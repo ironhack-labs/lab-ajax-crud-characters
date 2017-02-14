aeb5170c44536ad1e48c74d2b5c7ebfe6acb4bb7
ahuertam <ahuertam@yahoo.es>
2017-02-14 21:02:45 +0100

    Iteration 1 done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,69 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL+"/characters",
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    $.ajax({
+      url: this.BASE_URL+"/characters/"+id,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (object) {
+    $.ajax({
+      type: "POST",
+      url: this.BASE_URL+"/characters",
+      data: object,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,object) {
+    $.ajax({
+      type: "PATCH",
+      url: this.BASE_URL+"/characters/"+id,
+      data: object,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    $.ajax({
+      url: this.BASE_URL+"/characters/"+id,
+      method: "DELETE",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 }
