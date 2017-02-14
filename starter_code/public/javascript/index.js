631e60844ea6c11d1466e4a99c799cf5e63d42bc
Tomas Ruiz <tomastrg22@gmail.com>
2017-02-14 20:59:07 +0100

    it2 1 with error

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,25 +1,37 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+/*jshint esversion:6*/
 
 
-//
-// $(document).ready( () => {
-//   $('#fetch-all').on('click', (e) => {
-//
-//   })
-//
-//   $('#fetch-one').on('click', (e) => {
-//
-//   })
-//
-//   $('#delete-one').on('click', (e) => {
-//
-//   })
-//
-//   $('#edit-character-form').on('submit', (e) => {
-//
-//   })
-//
-//   $('#new-character-form').on('submit', (e) => {
-//
-//   })
-// })
+
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
+
+$(document).ready( () => {
+  let listOfCharacters = charactersAPI.getFullList();
+
+  $('#fetch-all').on('click', (e) => {
+    if(listOfCharacters.length !== 0){
+      $('.characters-container').remove();
+        charactersAPI.getFullList().map(item =>{
+        $('.container').append('<div>Character Name</div>').addClass('name').append(item.name);
+        $('.container').append('<div>Character Occupation</div>').addClass('occupation').append(item.occupation);
+        $('.container').append('<div>Character Debt</div>').addClass('debt').append(item.debt);
+        $('.container').append('<div>Character Weapon</div>').addClass('weapon').append(item.weapon);
+      });
+    }
+  });
+
+  $('#fetch-one').on('click', (e) => {
+
+  })
+
+  $('#delete-one').on('click', (e) => {
+
+  })
+
+  $('#edit-character-form').on('submit', (e) => {
+
+  })
+
+  $('#new-character-form').on('submit', (e) => {
+
+  })
+})
