c34af361ec41dd41067148621525f73b6dc033ab
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-02-14 23:42:50 +0100

    iteration 1 done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,55 @@
+/* jshint esversion: 6 */
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList (callback) {
+    this.ajaxCall('/characters', 'get', null, callback);
   }
 
-  getOneRegister () {
-
+  getOneRegister (id, callback) {
+    this.ajaxCall('/characters/' + id, 'get', null, callback);
   }
 
-  createOneRegister () {
-
+  createOneRegister (characterObj, callback) {
+    // verify that characterObj has all required fields.
+    let wrongFields = [];
+    if (!characterObj){
+      wrongFields.push('all');
+      return wrongFields;
+    }
+    if (!characterObj.name || typeof(characterObj.name) !== 'string')
+      wrongFields.push('name');
+    if (!characterObj.occupancy || typeof(characterObj.occupancy) !== 'string')
+      wrongFields.push('occupancy');
+    if (!characterObj.debt || typeof(characterObj.debt) !== 'boolean')
+      wrongFields.push('debt');
+    if (!characterObj.weapon || typeof(characterObj.weapon) !== 'string')
+      wrongFields.push('weapon');
+    if (wrongFields.length !== 0) return wrongFields;
+    this.ajaxCall('/characters', 'post', characterObj, callback);
+    return wrongFields;
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, characterInfo, callback) {
+    this.ajaxCall("/characters/" + id, "PUT", characterInfo, callback);
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (id, callback) {
+    this.ajaxCall("/characters/" + id, "DELETE", null, callback);
+  }
 
+  ajaxCall(url, method, data, callback){
+    $.ajax({
+        url: this.BASE_URL + url,
+        method,
+        data: "In case we need to send data**",
+        success: callback,
+        error: function (err){
+          console.log(err);
+        }
+    });
   }
 }
