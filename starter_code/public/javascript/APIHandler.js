ce134369616d0733255fc78ba889c4b7a07460c0
Tomas Ruiz <tomastrg22@gmail.com>
2017-02-14 18:06:07 +0100

    it1 done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,67 @@
+/*jshint esversion:6*/
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL+"/characters",
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    $.ajax({
+      url: this.BASE_URL+"/characters/"+id,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (characterInfo) {
+    $.ajax({
+      type:    'POST',
+      url:     this.BASE_URL+'/characters',
+      data:    characterInfo,
+      success: console.log(characterInfo),
+      error:   handleError
+    });
   }
 
-  updateOneRegister () {
+  updateOneRegister (id,updateInfo) {
+    $.ajax({
+      type: 'PATCH',
+      url: this.BASE_URL+'/characters/'+id,
+      data: updateInfo,
+      success: showFeedback,
+      error: handleError
+      });
+    }
 
+  deleteOneRegister (id) {
+    $.ajax({
+      method: 'DELETE',
+      url: this.BASE_URL+'/characters/'+id,
+      success: showFeedback,
+      error: handleError
+    });
   }
-
-  deleteOneRegister () {
-
-  }
+}
+function showFeedback () {
+  console.log('success');
+}
+function handleError (err) {
+  console.log('Oh no! Error:');
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,25 @@
 const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
 
-  })
-
-  $('#fetch-one').on('click', (e) => {
-
-  })
-
-  $('#delete-one').on('click', (e) => {
-
-  })
-
-  $('#edit-character-form').on('submit', (e) => {
-
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+//
+// $(document).ready( () => {
+//   $('#fetch-all').on('click', (e) => {
+//
+//   })
+//
+//   $('#fetch-one').on('click', (e) => {
+//
+//   })
+//
+//   $('#delete-one').on('click', (e) => {
+//
+//   })
+//
+//   $('#edit-character-form').on('submit', (e) => {
+//
+//   })
+//
+//   $('#new-character-form').on('submit', (e) => {
+//
+//   })
+// })
