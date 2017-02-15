074f1f34d1bcf278890fab6a398625e67c674bf6
Papu Arza <papuarza@MacBook-Pro-de-Papu.local>
2017-02-15 18:02:39 +0100

    done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,34 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+    printTheFullList = function(response){
+      console.log(response)
+    };
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister($('#character-id').val())
+    bringTheCharacter = function(response){
+      console.log(response.name);
+      $('.name').text(response.name);
+      $('.occupation').text(response.occupation);
+      $('.debt').text(response.debt);
+      $('.weapon').text(response.weapon);
+    };
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister();
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    charactersAPI.updateOneRegister();
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    e.preventDefault();
+   charactersAPI.createOneRegister();
+ });
+});
