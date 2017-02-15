708053a68013734454e04e1710b3f0894e97e2c8
juliapolbach <juliapolbach@gmail.com>
2017-02-15 09:00:09 +0100

    first iteration

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,83 @@
+/* jshint esversion:6 */
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList() {
+        $.ajax({
+            url: this.BASE_URL + "/characters/",
+            method: "GET",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+                return response;
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+    }
 
-  }
+    getOneRegister(id) {
+        $.ajax({
+            url: this.BASE_URL + "/characters/"+ id,
+            method: "GET",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+                return response;
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+    }
 
-  getOneRegister () {
+    createOneRegister() {
+        $.ajax({
+            url: this.BASE_URL + "/characters/",
+            method: "POST",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+                return response;
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+    }
 
-  }
+    updateOneRegister(id) {
+        $.ajax({
+            url: `http://ih-api.herokuapp.com/characters/${id}`,
+            method: "PATCH/PUT",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+                return response;
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+    }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(id) {
+        $.ajax({
+            url: `http://ih-api.herokuapp.com/characters/${id}`,
+            method: "DELETE",
+            dataType: "json",
+            success: function(response) {
+                console.log(response);
+                return response;
+            },
+            error: function(err) {
+                console.log(err);
+            },
+        });
+    }
 }
+
+var constructor = new APIHandler();
