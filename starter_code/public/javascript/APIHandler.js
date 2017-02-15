991829dda6294855dd937bc925970e3b78c40e4d
Michiel Barnaart <michielbarnaart@gmail.com>
2017-02-15 09:01:45 +0100

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,102 @@ class APIHandler {
   }
 
   getFullList () {
+    $.ajax({
+  url: "http://ih-api.herokuapp.com/characters",
+  method: "GET",
+  success: function (response) {
+    console.log(response);
+    response.forEach(function(character){
+      $('.name').html(character.name);
+      $('.occupation').html(character.occupation);
+      $('.debt').html(character.debt);
+      $('.weapon').html(character.weapon);
+    });
+  },
+  error: function (err) {
+  console.log(err);
+  },
+  });
 
   }
 
   getOneRegister () {
+    var idC = $('input').val();
+    console.log(idC);
+    $.ajax({
+      url: `http://ih-api.herokuapp.com/characters/${idC}`,
+      method: "GET",
+      success: function (response) {
+          $('.name').html(response.name);
+          $('.occupation').html(response.occupation);
+          $('.debt').html(response.debt);
+          $('.weapon').html(response.weapon);
+
+      },
+      error: function (err) {
+      console.log(err);
+      },
+    });
 
   }
 
   createOneRegister () {
+  const characterInfo = {
+    name: $("input[name='name']").val(),
+    occupation: $("input[name='occupation']").val(),
+    deb: $("input[name='deb']").is(":checked"),
+    weapon: $("input[name='weapon']").val()
 
+  };
+    $.ajax({
+    type: 'POST',
+    url: 'https://ih-api.herokuapp.com/characters',
+    data: characterInfo,
+    success: function (response) {
+      $( "#send-data" ).css( "background-color", "green" );
+    },
+    error: function (err) {
+    $( "#send-data" ).css( "background-color", "red" );}
+  });
   }
 
   updateOneRegister () {
+    const updateInfo = {
+      name: $("input[name='name']").val(),
+      occupation: $("input[name='occupation']").val(),
+      deb: $("input[name='deb']").is(":checked"),
+      weapon: $("input[name='weapon']").val()
+  };
 
+
+  const charId = $("input[name='chr-id']").val();
+
+  $.ajax({
+    type: 'PATCH',
+    url: `https://ih-api.herokuapp.com/characters/${charId}`,
+    data: updateInfo,
+    success: function (response) {
+      $( "#send-data" ).css( "background-color", "green" );
+    },
+    error: function (err) {
+    $( "#send-data" ).css( "background-color", "green" );
+    },
+  });
   }
 
   deleteOneRegister () {
+    var idC = $("input[name='character-id-delete']").val();
+    console.log(idC);
+    $.ajax({
+      type: 'DELETE',
+      url: `http://ih-api.herokuapp.com/characters/${idC}`,
+      success: function (response) {
+        $( "#delete-one" ).css( "background-color", "green" );
+      },
+      error: function (err) {
+      $( "#delete-one" ).css( "background-color", "red" );
+      },
+    });
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,27 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    charactersAPI.getOneRegister();
   })
 
   $('#delete-one').on('click', (e) => {
+    charactersAPI.deleteOneRegister();
 
   })
 
   $('#edit-character-form').on('submit', (e) => {
+      event.preventDefault();
+      charactersAPI.updateOneRegister();
 
   })
 
   $('#new-character-form').on('submit', (e) => {
+      event.preventDefault();
+    charactersAPI.createOneRegister();
 
   })
 })
