e39995d8144aebb78dd3e55872dd821c6a5af3ab
ManuLintz <manuel.lintz@gmail.com>
2017-02-15 08:54:29 +0100

    done til half of second iteration

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,15 +2,20 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
+    charactersAPI.getFullList();
 
   })
 
   $('#fetch-one').on('click', (e) => {
+    var character = $('#character-id').val();
+    charactersAPI.getOneRegister (character);
+
 
   })
 
   $('#delete-one').on('click', (e) => {
-
+    var character = $('#delete-one').val();
+    charactersAPI.deleteOneRegister (character);
   })
 
   $('#edit-character-form').on('submit', (e) => {
@@ -18,6 +23,14 @@ $(document).ready( () => {
   })
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    const characterInfo= {};
+    characterInfo.name = $('#new-name').val();
+    characterInfo.occupation = $('#new-occupation').val();
+    characterInfo.weapon = $('#new-weapon').val();
+    characterInfo.debt = $('#new-debt').prop('checked');
+    console.log(characterInfo);
+    charactersAPI.createOneRegister(characterInfo);
 
   })
 })
