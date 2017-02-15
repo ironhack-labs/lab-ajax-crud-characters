4530f91695870f3faaa4dedd2f35b5715fbee2f3
Peter Fantasia <peter.vittorio@gmail.com>
2017-02-15 08:26:00 +0100

    incomplete

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,17 +1,52 @@
+/* jshint esversion:6 */
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    $.ajax({
+  url: this.BASE_URL + "/characters",
+  method: "GET",
+  success: function (response) {
+    console.log(response);
+
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
 
   }
 
   getOneRegister () {
+    $.ajax({
+  url: this.BASE_URL + "/characters/:id",
+  method: "GET",
+  dataType: 'json',
+  success: function (response) {
+    console.log(response);
+  },
+  error: function (err) {
+    console.log(err);
+
+  },
+  });
 
   }
 
   createOneRegister () {
+    $.ajax({
+  url: this.BASE_URL + "/characters",
+  method: "POST",
+  success: function (response) {
+    console.log(response);
+
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
 
   }
 
