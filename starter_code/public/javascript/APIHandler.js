143166e30188e527cbdd93dd2de17603a51eaf54
Javier <jvrcueto@outlook.com>
2017-03-13 15:45:34 -0400

    basic setup

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,62 @@ class APIHandler {
   }
 
   getFullList () {
+    $.ajax({
+      url : 'https://ih-api.herokuapp.com/characters',
+      method : 'GET',
+      data : null,
+      error : (error) => console.log('getFullList error'),
+      success : (data) => {
 
+      }
+    });
   }
 
   getOneRegister () {
+    $.ajax({
+      url : 'https://ih-api.herokuapp.com/characters',
+      method : 'GET',
+      data : null,
+      error : (error) => console.log('getOneRegister error'),
+      success : (data) => {
 
+      }
+    });
   }
 
   createOneRegister () {
+    $.ajax({
+      url : 'https://ih-api.herokuapp.com/characters',
+      method : 'POST',
+      data : null,
+      error : (error) => console.log('createOneRegister error'),
+      success : (data) => {
 
+      }
+    });
   }
 
   updateOneRegister () {
+    $.ajax({
+      url : 'https://ih-api.herokuapp.com/characters',
+      method : 'PUT',
+      data : null,
+      error : (error) => console.log('updateOneRegister error'),
+      success : (data) => {
 
+      }
+    });
   }
 
   deleteOneRegister () {
+    $.ajax({
+      url : 'https://ih-api.herokuapp.com/characters',
+      method : 'DELETE',
+      data : null,
+      error : (error) => console.log('deleteOneRegister error'),
+      success : (data) => {
 
+      }
+    });
   }
 }
