45fc48059723b66569a585a9dca4071479b6b31b
Stephen Rifenburgh <Stephen@Rifenburgh.com>
2017-03-13 16:45:52 -0400

    First Commit

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,111 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL       = 'https://ih-api.herokuapp.com/characters/';
   }
 
   getFullList () {
+    for (let i = 1; i < 11; i ++) {
+      $.ajax({
+        url:            `${charactersAPI.BASE_URL}${i}`,
+        method:         `GET`,
+        success:        (data) => {
+          console.log(`Success: `);
+          console.log(`${data.name}`);
+          $('#characters-container').append(`
+            <div class="character-info" id="character-info">
+              <div class="name">Name: ${data.name}</div>
+              <div class="occupation">Occupation: ${data.occupation}</div>
+              <div class="debt">Debt: ${data.debt}</div>
+              <div class="weapon">Weapon: ${data.weapon}</div>
+            </div>
+            `);
+        },
+        error:          (err) => {
+          console.log(`Failure: `);
+        }
+      });
+    }
+  }
+
+  getOneRegister (id) {
+    const charId      = id;
+    console.log(`charId ${id}`);
+    $('#characters-container').empty();
+    $.ajax({
+      url:            `${charactersAPI.BASE_URL}${charId}`,
+      method:         `GET`,
+      success:        (data) => {
+        console.log(`Success: ${data.id}`);
+        $('#characters-container').append(`
+          <div class="character-info" id="character-info">
+            <div class="name">Name: ${data.name}</div>
+            <div class="occupation">Occupation: ${data.occupation}</div>
+            <div class="debt">Debt: ${data.debt}</div>
+            <div class="weapon">Weapon: ${data.weapon}</div>
+          </div>
+          `);
+
+      },
+      error:          (err) => {
+        console.log(`Error: ${err}`);
+      }
+
+    });
 
   }
 
-  getOneRegister () {
-
+  createOneRegister (info) {
+    $('#characters-container').empty();
+    const charInfo    = info;
+    console.log(`Create Info: ${charInfo.name}`);
+    $.ajax({
+      url:            `${charactersAPI.BASE_URL}`,
+      method:         'POST',
+      data:           charInfo,
+      success:        (data) => {
+        console.log(`Create Success: ${data}`);
+      },
+      error:          (err) => {
+        console.log(`Create Error: ${err}`);
+        console.log(err);
+        console.log(charInfo);
+      }
+    });
   }
 
-  createOneRegister () {
-
+  updateOneRegister (id, info) {
+    const charId        = id;
+    const charInfo      = info;
+    console.log(charId);
+    console.log(charInfo);
+    $.ajax({
+      url:                `${charactersAPI.BASE_URL}${charId}`,
+      method:             'PUT',
+      data:               charInfo,
+      success:            (data) => {
+        console.log(`Update Success`);
+        console.log(data);
+      },
+      error:              (err) => {
+        console.log(`Update Error`);
+        console.log(err);
+      }
+    });
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (id) {
+    const charId        = id;
+    $.ajax({
+      url:                `${charactersAPI.BASE_URL}${charId}`,
+      method:             `DELETE`,
+      success:            (data) => {
+        console.log(`Successfully deleted ID: ${charId}`);
+      },
+      error:              (err) => {
+        console.log(`There was an error.`);
+        console.log(err);
+      }
+    });
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,48 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    const id            = $('#character-id').val();
+    console.log(`IndexJS id: ${id}`);
+    charactersAPI.getOneRegister(id);
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    const id            = $('#deleteId').val();
+    console.log(id);
+    charactersAPI.deleteOneRegister(id);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    e.preventDefault();
+    const id            = $('#updateId').val();
+    const info          = {
+      name:               $('#updateName').val(),
+      occupation:         $('#updateOccupation').val(),
+      weapon:             $('#updateWeapon').val()
+    };
+    console.log('Edit Characters');
+    console.log(id);
+    console.log(info);
+    charactersAPI.updateOneRegister(id, info);
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    e.preventDefault();
+    // const id            = $('#').val();
+    // console.log(`Delete One ID: ${id}`);
+    const info = {
+      name:             $('#createName').val(),
+      occupation:       $('#createOccupation').val(),
+      weapon:           $('#createWeapon').val()
+    };
+    console.log(`Create Character: ${info}`);
+    charactersAPI.createOneRegister(info);
+  });
+});
