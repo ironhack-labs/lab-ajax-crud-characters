73644faf68e2dd73ba2113491efb5e55e03004f2
dsoraluz <d.soraluz@gmail.com>
2017-03-13 18:01:51 -0400

    done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,51 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
-$(document).ready( () => {
+$(document).ready(() => {
+  console.log("Connection Successful");
+
+
   $('#fetch-all').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
+    e.preventDefault();
+    charactersAPI.getOneRegister($('#get-by-id-input').val());
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    charactersAPI.deleteOneRegister($('#delete-by-id-input').val());
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
+    const id = $('#update-by-id-input').val();
+
+    const updateInfo = {
+      name: $('#update-name-input').val(),
+      occupation: $('#update-occupation-input').val(),
+      weapon: $('#update-weapon-input').val(),
+      debt: $('#update-debt-input').is(':checked')
+    };
+
+    charactersAPI.updateOneRegister(id, updateInfo);
+
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
-})
+    const charInfo = {
+      name: $('#create-name-input').val(),
+      occupation: $('#create-occupation-input').val(),
+      weapon: $('#create-weapon-input').val(),
+      debt: $('#create-debt-input').is(':checked')
+    };
+
+    charactersAPI.createOneRegister(charInfo);
+  });
+});
