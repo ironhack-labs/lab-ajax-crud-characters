2cb867a3b461535489d8acf34f460547fda9821c
Oleh Kolinko <oleh.kolinko@gmail.com>
2017-03-13 17:13:30 -0400

    done with exercise

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,37 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
+
+
+
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
+    const id = $('#character-id').val();
+    charactersAPI.getOneRegister(id);
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    const id = $('#character-id-delete').val();
+    charactersAPI.deleteOneRegister(id);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    const id = $('#id-edit').val();
+    e.preventDefault();
+    charactersAPI.updateOneRegister(id);
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    charactersAPI.createOneRegister();
+  });
+});
 
-  })
-})
+function getHtmlBluePrient(obj){
+
+  return bluePrient;
+}
