39a02a68e75660a6ed49dd9004c07c5e111acfa4
Fcozar <franciscodecozar@gmail.com>
2017-05-02 19:34:09 +0200

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,89 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList (url) {
+    $.ajax({
+      url: this.BASE_URL + url,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+        },
+      });
 
   }
 
-  getOneRegister () {
+  getOneRegister (url) {
+    $.ajax({
+      url: this.BASE_URL + url,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+        },
+      });
 
   }
 
-  createOneRegister () {
+  createOneRegister (name, occ, weap, debt) {
+    const characterInfo={
+      name : name,
+      occupation : occ,
+      weapon : weap,
+      debt : debt
+    };
+
+    $.ajax({
+      url: this.BASE_URL + "/characters",
+      method: "POST",
+      data: characterInfo,
+      success: function (postResponseresponse) {
+        console.log(postResponseresponse);
+      },
+      error: function (err) {
+        console.log(err);
+        },
+      });
+
 
   }
 
-  updateOneRegister () {
 
+  updateOneRegister(url, charId) {
+
+      $.ajax({
+        method: 'PATCH',
+        url: this.BASE_URL + url + charId,
+        data: updateInfo,
+        success: (patchResponse) => {
+        console.log('Update!');
+        console.log(patchResponse);
+            },
+        error: handleError
+        });
+    }
+
+  deleteOneRegister(url, charid) {
+    $.ajax ({
+        method: 'DELETE',
+        url: this.BASE_URL + url + charid,
+        success: (patchResponse) => {
+            console.log('Character has been successfully deleted');
+        },
+        error: handleError
+    });
   }
-
-  deleteOneRegister () {
-
-  }
+};
+function showFeedback(postResponse) {
+    console.log('post success');
+    console.log(postResponse);
 }
+
+function handleError(err) {
+    console.log('Oh no! Error:');
+    console.log(err);
+};
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,43 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
+    charactersAPI.getFullList("/characters");
 
   })
 
   $('#fetch-one').on('click', (e) => {
+    let id = $("[name=character-id]").val();
+    let url = "/characters/"+id;
+    charactersAPI.getOneRegister(url);
 
   })
 
   $('#delete-one').on('click', (e) => {
-
+    let characterid = $('[name=character-id-delete]').val();
+    charactersAPI.deleteOneRegister("/characters/", characterid);
   })
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
+    let updateInfo = {
+      name: $('#edit-character-form [name=name]').val(),
+      occupation:$('#edit-character-form [name=occupation]').val(),
+      weapon:$('#edit-character-form [name=weapon]').val(),
+      debt:$('#edit-character-form [name=debt]').prop( "checked" ),
+    };
+    const charId = $('[name=chr-id]').val();
+    console.log(updateInfo);
+    console.log(charId);
+  });
 
-  })
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    const name = $("#new-character-form [name='name']").val();
+    const occupation = $("#new-character-form [name='occupation']").val();
+    const weapon = $("#new-character-form [name='weapon']").val();
+    const debt = $("#new-character-form [name='debt']").prop("checked");
 
+    charactersAPI.createOneRegister(name, occupation, weapon, debt);
   })
 })
