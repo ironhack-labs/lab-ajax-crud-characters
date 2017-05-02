7ff3e96ddd3a29a19085d58316ca3e555dc91b95
Fernando Martinez Larrañaga <fernandomartinezlarranaga@MacBook-Air-de-Fernando.local>
2017-05-02 19:59:30 +0200

    done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,16 +1,18 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com/characters/")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    let charID = $('input[name ="character-id"]').val();
+    charactersAPI.getOneRegister(charID);
   })
 
   $('#delete-one').on('click', (e) => {
-
+    let charID = $('input[name="character-id-delete"]').val();s
+    charactersAPI.deleteOneRegister(charID);
   })
 
   $('#edit-character-form').on('submit', (e) => {
@@ -18,6 +20,7 @@ $(document).ready( () => {
   })
 
   $('#new-character-form').on('submit', (e) => {
-
+    
+    charactersAPI.createOneRegister();
   })
 })
