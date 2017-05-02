d9da07ee255baec786041a5514f27dfda119dc14
edsonjmv <edsonjmv@gmail.com>
2017-05-02 19:15:39 +0200

    first iteration

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,83 @@
+/*jshint esversion: 6*/
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL,
+      method: "get",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (charID) {
+    $.ajax({
+      url: this.BASE_URL + charID,
+      method: "get",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
   createOneRegister () {
-
+    let name = $('#name-new').val();
+    let occupation = $('#occupation-new').val();
+    let debt = $('#debt-new').prop('checked');
+    let weapon = $('#weapon-new').val();
+    const charNew = { name: name, occupation: occupation, debt: debt, weapon: weapon };
+    $.ajax({
+      url: this.BASE_URL,
+      method: "post",
+      data: charNew,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (charID) {
+    let name = $('#name-edit').val();
+    let occupation = $('#occupation-edit').val();
+    let debt = $('#debt-edit').prop('checked');
+    let weapon = $('#weapon-edit').val();
+    const charEdit = { name: name, occupation: occupation, debt: debt, weapon: weapon };
+    $.ajax({
+      url: this.BASE_URL + charID,
+      method: "patch",
+      data: charEdit,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (charID) {
+    $.ajax({
+      url: this.BASE_URL + charID,
+      method: "delete",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,29 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+/*jshint esversion: 6*/
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com/characters/");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    let charID = $('input[name="character-id"]').val();
+    charactersAPI.getOneRegister(charID);
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    let charID = $('input[name="character-id-delete"]').val();
+    charactersAPI.deleteOneRegister(charID);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    let charID = $('input[name="chr-id"]').val();
+    e.preventDefault();
+    charactersAPI.updateOneRegister(charID);
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    e.preventDefault();
+    charactersAPI.createOneRegister();
+  });
+});
