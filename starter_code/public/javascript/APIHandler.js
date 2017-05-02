b6957999a3d79f840309121f984c8c5dfd38fb19
Hery Martin <scorpionxii@gmail.com>
2017-05-02 18:30:58 +0200

    First Commit!

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList(callbackFunction) {
+        this.doAJAXRequest("GET", this.BASE_URL + "/characters", callbackFunction);
+    }
 
-  }
+    getOneRegister(id, callbackFunction) {
+        this.doAJAXRequest("GET", this.BASE_URL + "/characters/" + id, callbackFunction);
+    }
 
-  getOneRegister () {
+    createOneRegister() {
 
-  }
+    }
 
-  createOneRegister () {
+    updateOneRegister() {
 
-  }
+    }
 
-  updateOneRegister () {
+    deleteOneRegister(id, callbackFunction) {
+        this.doAJAXRequest("DELETE", this.BASE_URL + "/characters/" + id, callbackFunction);
+    }
 
-  }
+    doAJAXRequest(method, url, successCallback) {
+        $.ajax({
+            method: method,
+            url: url,
+            success: successCallback,
+            error: this.errorCallback
+        });
+    }
 
-  deleteOneRegister () {
-
-  }
+    errorCallback(error) {
+        console.log(error);
+        alert("Ups!!! We have a problem: " + JSON.parse(error.responseText).error);
+    }
 }
