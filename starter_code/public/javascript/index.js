a06a75419b19b6684ff4577d0c62970ad14c48b5
Rafael Melón <info@rafaelmelon.es>
2017-05-03 09:30:05 +0200

    Primer commit

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,29 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com/characters/")
+
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    var characterId = $("input[name='character-id']").val();
+    charactersAPI.getOneRegister(characterId);
   })
 
   $('#delete-one').on('click', (e) => {
-
+    // var deleteOne = $("input[name='character-id-delete']").val();
+    // charactersAPI.getOneRegister(deleteOne);
   })
 
   $('#edit-character-form').on('submit', (e) => {
 
   })
 
-  $('#new-character-form').on('submit', (e) => {
 
+
+  $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    charactersAPI.createOneRegister();
   })
 })
