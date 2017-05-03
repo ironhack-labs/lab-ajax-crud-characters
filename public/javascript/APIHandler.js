cbbbec8294f392ff67762ba2ea55313d86e90dfa
Isak La Fleur <isak.engdahl@gmail.com>
2017-05-03 21:30:28 +0200

    final commit

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,86 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
-  getFullList () {
-
+  getFullList(cb) {
+    $.ajax({
+      url: `${this.BASE_URL}`,
+      method: 'GET',
+      success(response) {
+        cb(response);
+      },
+      error(err) {
+        cb({ error: err });
+      },
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister(charId, cb) {
+    $.ajax({
+      url: `${this.BASE_URL}/${charId}`,
+      method: 'GET',
+      success(response) {
+        cb(response);
+      },
+      error(err) {
+        cb({ error: err });
+      },
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister(char, cb) {
+    /*    const characterInfo = {
+          name: 'WALL-E',
+          occupation: 'Waste Allocation Robot',
+          weapon: 'Head laser',
+        };*/
+    $.ajax({
+      url: `${this.BASE_URL}`,
+      method: 'POST',
+      data: char,
+      success(response) {
+        cb(response);
+      },
+      error(err) {
+        cb({ error: err });
+      },
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(charId, char, cb) {
+    $.ajax({
+      method: 'PATCH',
+      url: `${this.BASE_URL}/${charId}`,
+      data: char,
+      success(response) {
+        cb(response);
+      },
+      error(err) {
+        cb({ error: err });
+      },
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(charId, cb) {
+    $.ajax({
+      method: 'DELETE',
+      url: `${this.BASE_URL}/${charId}`,
+      success(response) {
+        cb(response);
+      },
+      error(err) {
+        cb({ error: err });
+      },
+    });
   }
+}
+
+function showFeedback(postResponse) {
+  console.log('post success');
+  console.log(postResponse);
+}
+
+function handleError(err) {
+  console.log('Oh no! Error:');
+  console.log(err);
 }
