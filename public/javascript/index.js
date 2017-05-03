985636bd98548e58e9f207ac1ee5f87bba15e756
Isak La Fleur <isak.engdahl@gmail.com>
2017-05-03 18:41:40 +0200

    inital commit

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,21 +3,21 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
 
-  })
+  });
 
   $('#fetch-one').on('click', (e) => {
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
 
-  })
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
