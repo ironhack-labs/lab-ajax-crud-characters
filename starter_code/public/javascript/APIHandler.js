528ec3d6c3ce1608d38a55ae03f0f0c76bceff9f
Sergi Garcia <sergigarciapena@gmail.com>
2017-05-03 18:47:09 +0200

    iteration1

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,105 @@
+/*jshint esversion: 6*/
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL+"/characters",
+      method: "GET",
+      //data: //if sending information
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
   getOneRegister () {
-
+      // const characterId: $('#the-name-input').val(),
+      const characterId = 2;
+    $.ajax({
+      url: this.BASE_URL+"/characters/"+characterId,
+      method: "GET",
+      //data: //if sending information
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
   createOneRegister () {
+    const characterInfo = {
+      // name: $('#the-name-input').val(),
+      // occupation: $('#the-occupation-input').val(),
+      // debt:  $('#the-weapon-input').val(),
+      // weapon: $('#the-weapon-input').val()
+      name: 'test1',
+      occupation: 'test1',
+      debt:  true,
+      weapon: 'test1'
+    };
+
+  $.ajax({
+      method: 'POST',
+      url: this.BASE_URL+"/characters",
+      data: characterInfo,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
 
   }
 
   updateOneRegister () {
+    // const characterId: $('#the-name-input').val(),
+    const characterId = 3;
+    const characterInfo = {
+      // name: $('#the-name-input').val(),
+      // occupation: $('#the-occupation-input').val(),
+      // debt:  $('#the-weapon-input').val(),
+      // weapon: $('#the-weapon-input').val()
+      name: 'test0',
+      occupation: 'test0',
+      debt:  true,
+      weapon: 'test0'
+    };
 
+  $.ajax({
+      method: 'PATCH',
+      url: this.BASE_URL+"/characters/"+characterId,
+      data: characterInfo,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
   }
 
   deleteOneRegister () {
-
+    // const characterId: $('#the-name-input').val(),
+    const characterId = 3;
+  $.ajax({
+    url: this.BASE_URL+"/characters/"+characterId,
+    method: "DELETE",
+    //data: //if sending information
+    success: function (response) {
+      console.log(response);
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,28 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
-
+/*jshint esversion: 6*/
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 $(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
 
-  })
+  $('#fetch-all').on('click', (e) => {
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister();
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister();
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    charactersAPI.updateOneRegister();
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    charactersAPI.createOneRegister();
+  });
+});
 
-  })
-})
+function generateInfo(){
+  $('.characters-container').append($('<div>').addClass('character-info').append($('<div>').addClass('name').html("Character Name")).append($('<div>').addClass('occupation').html("Character Occupation")).append($('<div>').addClass('debt').html("Character Debt")).append($('<div>').addClass('weapon').html("Character Weapon")));
+}
