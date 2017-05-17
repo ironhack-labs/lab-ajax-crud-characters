e4d33650cb24cb63fc1c02f79a098d07a17cf6ae
ogm710811 <ogm710811@gmail.com>
2017-05-17 17:58:49 -0400

    first commit -> Iteration 1 done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,81 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
+  //*************************************************************
+  // getFullList (): Gets all the characters info from API
+  //*************************************************************
   getFullList () {
-
+    $.ajax({
+      url     : this.BASE_URL + "/characters",
+      method  : "GET",
+      success : showFeedback,
+      error   : handleError,
+    });
   }
 
-  getOneRegister () {
-
+  //*************************************************************
+  // getOneRegister () : Get a single character info from API
+  //*************************************************************
+  getOneRegister (id) {
+    $.ajax({
+      url     : this.BASE_URL + "/characters/" + id,
+      method  : "GET",
+      success : showFeedback,
+      error   : handleError,
+    });
   }
 
-  createOneRegister () {
-
+  //*****************************************************************
+  // createOneRegister () : Create a single character info from API
+  //*****************************************************************
+  createOneRegister (characterInfo) {
+    console.log(characterInfo);
+    $.ajax({
+      url     : this.BASE_URL + "/characters",
+      data    : characterInfo,
+      method  : "POST",
+      success : showFeedback,
+      error   : handleError,
+    });
   }
 
-  updateOneRegister () {
-
+  //*****************************************************************
+  // updateOneRegister () : Edit a single character through his id
+  //*****************************************************************
+  updateOneRegister (characterInfo) {
+    console.log(characterInfo);
+    $.ajax({
+      url     : this.BASE_URL + "/characters/" + characterInfo.id,
+      data    : characterInfo,
+      method  : "PATCH",
+      success : showFeedback,
+      error   : handleError,
+    });
   }
 
-  deleteOneRegister () {
-
+  //******************************************************************
+  // deleteOneRegister () : Delete a single character through his id
+  //******************************************************************
+  deleteOneRegister (id) {
+    console.log(id);
+    $.ajax({
+      url     : this.BASE_URL + "/characters/" + id,
+      method  : "DELETE",
+      success : showFeedback,
+      error   : handleError,
+    });
   }
+}
+
+//******************************************************************
+// FUNCTIONS here ...
+//******************************************************************
+function showFeedback (postResponse) {
+  console.log('request success');
+  console.log(postResponse);
+}
+
+function handleError (err) {
+  console.log('Error !!!');
+  console.log(err);
 }
