7694e900ae7527666712aa84ec21a67c3d86fc66
Sherwin Garcia-Obregon <sherwination@gmail.com>
2017-05-16 21:17:34 -0400

    Initial

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,26 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    const charactersFetched = {
+      name: '',
+      occupation: '',
+      weapon: ''
+    };
+  });s
 
   $('#fetch-one').on('click', (e) => {
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
 
-  })
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
