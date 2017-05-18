b1926ba5e473faeb658d564bccfd262811a75162
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-17 21:41:06 -0400

    iteration one working on it easy cake

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,73 @@
+function showFeedback(postResponse) {
+    console.log('Oh YES works');
+    console.log(postResponse);
+}
+
+function handleError(err) {
+    console.log('Oh no! Error:');
+    console.log(err);
+}
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
 
-  }
+    getFullList() {
+        $.ajax({
+            method: 'GET',
+            url: 'http://ih-api.herokuapp.com/characters',
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  getOneRegister () {
+    getOneRegister(one) {
+        $.ajax({
+            method: 'GET',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  }
+    createOneRegister(nam,occu,deb,weap) {
+        $.ajax({
+            method: 'POST',
+            url: 'http://ih-api.herokuapp.com/characters',
+            data: {
+                name: nam,
+                occupation: occu,
+                debt: deb,
+                weapon: weap
+            },
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  createOneRegister () {
+    updateOneRegister(one,nam,occu,deb,weap) {
+        $.ajax({
+            method: 'PATCH',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            data: {
+                name: nam,
+                occupation: occu,
+                debt: deb,
+                weapon: weap
+            },
+            success: showFeedback,
+            error: handleError
+        });
+    }
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(one) {
+        $.ajax({
+            method: 'DELETE',
+            url: `http://ih-api.herokuapp.com/characters/${one}`,
+            success: showFeedback,
+            error: handleError
+        });
+    }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,29 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+charactersAPI.getOneRegister($('#searchByID').val());
+  });
 
   $('#delete-one').on('click', (e) => {
+    charactersAPI.deleteOneRegister($('#deleteOne').val());
+  });
+  $('#new-character-form').on('submit', (e) => {
+var
+name = $('name').val(),
+occupation = $('occupation').val(),
+debt = $('debt').val(),
+weapon = $('weapon').val();
 
-  })
+charactersAPI.createOneRegister(name,occupation,debt,weapon);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+charactersAPI.updateOneRegister();
+  });
 
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+});
