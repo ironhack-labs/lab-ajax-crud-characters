ee7b14580bdb808f31457cbe83a1ea2e1e2b229f
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-17 23:06:14 -0400

    done iteration 2

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,6 +1,38 @@
 function showFeedback(postResponse) {
     console.log('Oh YES works');
     console.log(postResponse);
+    var counter = 0;
+    if (postResponse.length > 0) {
+        postResponse.forEach((char) => {
+            counter++;
+            console.log(counter);
+            var info;
+if (counter === 1) {
+   info = 'characterinfo';
+   $('.name').append(`<label>  ${char.name}</label>`);
+   $('.occupation').append(`<label>  ${char.occupation}</label>`);
+   $('.debt').append(`<label>  ${char.debt}</label>`);
+   $('.weapon').append(`<label>  ${char.weapon}</label>`);
+   $('.id').append(`<label>  ${char.id}</label>`);
+}else {
+   info = 'characterinfo'+counter;
+   $('.characters-container').append(`<div class= ${info}></div>`);
+   $(`.${info}`).append(`<label class="name">Character Name:<label>  ${char.name}</label></label><br>`);
+   $(`.${info}`).append(`<label class="occupation">Character Occupation:<label>  ${char.occupation}</label></label><br>`);
+   $(`.${info}`).append(`<label class="debt">Character Debt:<label>  ${char.debt}</label></label><br>`);
+   $(`.${info}`).append(`<label class="weapon">Character Weapon:<label>  ${char.weapon}</label></label><br>`);
+   $(`.${info}`).append(`<label class="id">ID:<label>  ${char.id}</label></label><br>`);
+}
+
+
+        });
+    } else {
+        $('.name').append(`<label>  ${postResponse.name}</label>`);
+        $('.occupation').append(`<label>  ${postResponse.occupation}</label>`);
+        $('.debt').append(`<label>  ${postResponse.debt}</label>`);
+        $('.weapon').append(`<label>  ${postResponse.weapon}</label>`);
+        $('.id').append(`<label>  ${postResponse.id}</label>`);
+    }
 }
 
 function handleError(err) {
@@ -32,14 +64,14 @@ class APIHandler {
         });
     }
 
-    createOneRegister(nam,occu,deb,weap) {
-      
-      const char = {
-          name: nam,
-          occupation: occu,
-          debt: deb,
-          weapon: weap
-      };
+    createOneRegister(nam, occu, deb, weap) {
+
+        const char = {
+            name: nam,
+            occupation: occu,
+            debt: deb,
+            weapon: weap
+        };
         $.ajax({
             method: 'POST',
             url: 'http://ih-api.herokuapp.com/characters',
@@ -49,7 +81,7 @@ class APIHandler {
         });
     }
 
-    updateOneRegister(one,nam,occu,deb,weap) {
+    updateOneRegister(one, nam, occu, deb, weap) {
         $.ajax({
             method: 'PATCH',
             url: `http://ih-api.herokuapp.com/characters/${one}`,
