09065b2ff086d58798e5df3b79365d32298f14ab
ogm710811 <ogm710811@gmail.com>
2017-05-18 23:09:26 -0400

    ajax done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -10,7 +10,7 @@ class APIHandler {
     $.ajax({
       url     : this.BASE_URL + "/characters",
       method  : "GET",
-      success : showFeedback,
+      success : displayCharacterCards,
       error   : handleError,
     });
   }
@@ -22,7 +22,7 @@ class APIHandler {
     $.ajax({
       url     : this.BASE_URL + "/characters/" + id,
       method  : "GET",
-      success : showFeedback,
+      success : displayOneCharacter,
       error   : handleError,
     });
   }
@@ -36,8 +36,13 @@ class APIHandler {
       url     : this.BASE_URL + "/characters",
       data    : characterInfo,
       method  : "POST",
-      success : showFeedback,
-      error   : handleError,
+      // pass two parameters to the CB on success && error
+      success : (response) => {
+                  showFeedback( response, 'create');
+                },
+      error   : (response) => {
+                  handleError( response, 'create');
+                },
     });
   }
 
@@ -50,8 +55,13 @@ class APIHandler {
       url     : this.BASE_URL + "/characters/" + characterInfo.id,
       data    : characterInfo,
       method  : "PATCH",
-      success : showFeedback,
-      error   : handleError,
+      // pass two parameters to the CB on success && error
+      success : (response) => {
+                  showFeedback( response, 'update');
+                },
+      error   : (response) => {
+                  handleError( response, 'update');
+                },
     });
   }
 
@@ -63,21 +73,178 @@ class APIHandler {
     $.ajax({
       url     : this.BASE_URL + "/characters/" + id,
       method  : "DELETE",
-      success : showFeedback,
-      error   : handleError,
+      // pass two parameters to the CB on success && error
+      success : (response) => {
+                  showFeedback( response, 'delete');
+                },
+      error   : (response) => {
+                  handleError( response, 'delete');
+                },
     });
   }
 }
 
+
+//*****                  FUNCTIONS here ...                    *****
+
+
 //******************************************************************
-// FUNCTIONS here ...
+// displayCharacterCards()
+// display all characters from the API
 //******************************************************************
-function showFeedback (postResponse) {
-  console.log('request success');
-  console.log(postResponse);
+function displayCharacterCards(postResponse) {
+  // first empty any append html so get ready to display charcaters
+  clearCards();
+  //$('.characters-container').empty();
+
+  postResponse.forEach( (character, index) => {
+    // define the html to append for each character    
+    var htmlToAppend = 
+    "<div class=\"character-info\">" +
+      "<div class=\"block\">" + 
+          "<label id=\"id-" + index + "\">Id:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"name-" + index + "\">Name:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"occupation-" + index + "\">Occupation:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"debt-" + index + "\">Debt:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"weapon-" + index + "\">Weapon:</label>" + 
+      "</div>" +
+    "</div>";
+
+    $('.characters-container').append(htmlToAppend);
+    
+    // append values of each character
+    appendCharacterElements(character, index);
+    
+  }, this);
 }
 
-function handleError (err) {
+//******************************************************************
+// displayOneCharacter()
+// display one characters from the API
+// when it is called through his id
+//******************************************************************
+function displayOneCharacter(postResponse) {
+  let index = 0;
+  clearCards();
+
+  var htmlToAppend = 
+    "<div class=\"character-info\">" +
+      "<div class=\"block\">" + 
+          "<label id=\"id-" + index + "\">Id:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"name-" + index + "\">Name:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"occupation-" + index + "\">Occupation:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"debt-" + index + "\">Debt:</label>" + 
+      "</div>" + 
+      "<div class=\"block\">" + 
+          "<label id=\"weapon-" + index + "\">Weapon:</label>" + 
+      "</div>" +
+    "</div>";
+
+  $('.characters-container').append(htmlToAppend);
+
+  // append values of each character
+  appendCharacterElements(postResponse, index);
+
+}
+
+//******************************************************************
+// appendCharacterElements()
+// using jquery append all element of a character
+// call from displayCharacterCards()
+//******************************************************************
+function appendCharacterElements(character, index) {
+  $('#id-' + index).append(`<label class="id">${character.id}</label>`);
+  $('#name-' + index).append(`<label class="name">${character.name}</label>`);
+  $('#occupation-' + index).append(`<label class="occupation">${character.occupation}</label>`);
+  $('#debt-' + index).append(`<label class="debt">${character.debt}</label>`);
+  $('#weapon-' + index).append(`<label class="weapon">${character.weapon}</label>`);
+}
+
+//******************************************************************
+// clearCards()
+// remove html appended when displaying characters
+// so the cards are not overwrite
+//******************************************************************
+function clearCards() {
+  $('.characters-container').empty(); 
+}
+
+//******************************************************************
+// showFeedback()
+// log successful action from API
+//******************************************************************
+function showFeedback(postResponse, action) {
+  console.log('post success');
+  console.log(postResponse);
+
+  // change button background to green
+  switch (action) {
+    case 'delete':
+      $('#delete-one').css({
+        'background-color': 'green'    
+      });      
+      break;
+
+    case 'update':
+      $('#update-one').css({
+        'background-color': 'green'    
+      });   
+      break;
+
+      case 'create':
+      $('#create-one').css({
+        'background-color': 'green'    
+      });     
+      break;
+
+    default:
+      break;
+  }
+}
+
+//******************************************************************
+// handleError()
+// log any error from the API
+//******************************************************************
+function handleError (err, action) {
   console.log('Error !!!');
   console.log(err);
+
+  // change button background to green
+  switch (action) {
+    case 'delete':
+      $('#delete-one').css({
+        'background-color': 'red'    
+      });      
+      break;
+
+    case 'update':
+      $('#update-one').css({
+        'background-color': 'red'    
+      });   
+      break;
+
+      case 'create':
+      $('#create-one').css({
+        'background-color': 'red'    
+      });     
+      break;
+
+    default:
+      break;
+  }
 }
