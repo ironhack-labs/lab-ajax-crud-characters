6eb37a74288143c63bed2ebb4e0f5b8b089420f5
Lawrence Fitzgerald <lfitzmail@gmail.com>
2017-07-10 17:47:43 -0400

    Updated project.
    Finished step one of iteration one.

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,11 +4,29 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL + '/characters/',
+      method: 'GET',
+      success: (responseFromServer) => {
+        console.log("Grabbing list successful!");
+      },
+      error: (errorFromServer) => {
+        console.log("Unable to grab full list");
+      }
+    });
   }
 
   getOneRegister () {
-
+    $.ajax({
+      url: this.BASE_URL + '/characters/:id/',
+      method: 'GET',
+      success: (responseFromServer) => {
+        console.log("Found Id!");
+      },
+      error: (errorFromServer) => {
+        console.log("Could not find Id!");
+      }
+    });
   }
 
   createOneRegister () {
