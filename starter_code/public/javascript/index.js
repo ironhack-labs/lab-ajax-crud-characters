87a17f7eb4917446b34efdcca4a56b71e524587d
Deokjae Jeon <deokjj@github.com>
2017-07-10 18:40:10 -0400

    done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,46 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    var id = $("#idToSearch").val();
+    console.log(id);
+    charactersAPI.getOneRegister(id);
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    var id = $("#idToDelete").val();
+    charactersAPI.deleteOneRegister(id);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
+    var id = $("#idToEdit").val();
+
+    const editChar = {
+      name: $('#nameToEdit').val(),
+      occupation: $('#occToEdit').val(),
+      weapon: $('#weaponToEdit').val(),
+      debt: $('#debtToEdit').is(':checked')
+    };
+
+    charactersAPI.updateOneRegister(id, editChar);
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
-})
+    const newChar = {
+      name: $('#newName').val(),
+      occupation: $('#newOcc').val(),
+      weapon: $('#newWeapon').val(),
+      debt: $('#newDept').is(':checked')
+    };
+
+    charactersAPI.createOneRegister(newChar);
+  });
+});
