c8c6a350b1ceb6e51e85afc0044ad0cf0c0c243e
Claudia Duarte <Clau@MacBook-Pro-de-Claudia.local>
2017-07-11 19:35:33 +0200

    ITERATION1.4

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,34 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`,
+    });
 
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${id}`,
+    });
+  }
+
+  createOneRegister() {
 
   }
 
-  createOneRegister () {
+  updateOneRegister() {
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister(id) {
+    return $.ajax({
+      method: 'DELETE',
+      url: `${this.BASE_URL}/characters/${id}`,
+    });
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,53 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
-$(document).ready( () => {
+$(document).ready(() => {
   $('#fetch-all').on('click', (e) => {
+    charactersAPI.getFullList().then(figures => {
+      console.log(figures);
+      figures.forEach(figure => {
+        const $container = $('.characters-container');
+        const $figure = $('<div>').addClass('character-info');
+        const $name = $('<div>').addClass('name').text(figure.name);
+        const $occupation = $('<div>').addClass('occupation').text(figure.occupation);
 
-  })
+        const $weapon = $('<div>').addClass('weapon').text(figure.weapon);
+
+        $figure
+          .append($name)
+          .append($occupation)
+
+          .append($weapon);
+
+        $container.append($figure);
+
+      });
+    }, (err) => {
+      console.log(err);
+    });
+  });
 
   $('#fetch-one').on('click', (e) => {
+    const id = $('#characterID').val();
+    charactersAPI.getOneRegister(id).then(figure => {
+      console.log(figure);
+      $('.name').text(figure.name);
+      $('.occupation').text(figure.occupation);
+      $('.weapon').text(figure.weapon);
 
-  })
+    });
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister($('#character-id-delete').val()).then( figure => {
+      console.log(`Deleted ${$('#character-id-delete').val()}`);
+});
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
