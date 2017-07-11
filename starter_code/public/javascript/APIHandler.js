09ca710173e3843cb9f7229736d463a73e5cb859
Juan Macias <yo@soyjuanmacias.com>
2017-07-11 22:56:27 +0200

    You can draw characters and heroes

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = baseUrl
   }
 
   getFullList () {
-
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`
+    }).catch( err => {
+      console.log(err)
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister(idForSearch) {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${idForSearch}`
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister (data) {
+    return $.ajax({
+      method: "POST",
+      url: `${this.BASE_URL}/characters/`,
+      data: data
+    })
   }
 
   updateOneRegister () {
 
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(idForDelete) {
+    return $.ajax({
+      method: "DELETE",
+      url: `${this.BASE_URL}/characters/${idForDelete}`
+    })
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,16 +1,57 @@
 const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+$(document).ready(() => {
+  $('#fetch-all').on('click', (event) => {
+    event.preventDefault()
+    charactersAPI.getFullList().then(heroes => {
+      console.log(heroes)
+      heroes.forEach(heroe => {
+        const $generalContainer = $('.characters-container')
+        const $heroeContainer = $('<div>').addClass('character-info')
+        const $heroeId = $('<div>').addClass('id').text(`Id: ${heroe.id}`)
+        const $heroeName = $('<div>').addClass('name').text(`Name: ${heroe.name}`)
+        const $heroeOccupation = $('<div>').addClass('Occup:').text(`occupation: ${heroe.occupation}`)
+        const $heroeWeapon = $('<div>').addClass('weapon').text(`Weapon: ${heroe.weapon}`)
 
+        $heroeContainer
+          .append($heroeId)
+          .append($heroeName)
+          .append($heroeOccupation)
+          .append($heroeWeapon)
+        $generalContainer.append($heroeContainer)
+      })
+    })
   })
 
   $('#fetch-one').on('click', (e) => {
+    event.preventDefault()
+    const idForSearch = $("input[name='character-id']").val()
+    console.log(idForSearch)
+    charactersAPI.getOneRegister(idForSearch)
+      .then(heroe => {
+        const $searchById = $('.searchById')
+        const $heroeSearchContainer = $('<div>').addClass('character-info')
+        const $heroeSearchId = $('<div>').addClass('id').text(`Id: ${heroe.id}`)
+        const $heroeSearchName = $('<div>').addClass('name').text(`Name: ${heroe.name}`)
+        const $heroeSearchOccupation = $('<div>').addClass('Occup:').text(`occupation: ${heroe.occupation}`)
+        const $heroeSearchWeapon = $('<div>').addClass('weapon').text(`Weapon: ${heroe.weapon}`)
 
+        $heroeSearchContainer
+          .append($heroeSearchId)
+          .append($heroeSearchName)
+          .append($heroeSearchOccupation)
+          .append($heroeSearchWeapon)
+        $searchById.append($heroeSearchContainer)
+      })
   })
 
   $('#delete-one').on('click', (e) => {
-
+    event.preventDefault()
+    const idForDelete = $("input[name='character-id-delete']").val()
+    console.log(idForDelete)
+    charactersAPI.deleteOneRegister(idForDelete).then(heroe => {
+      console.log(`Deleted heroe with ID: ${idForDelete}`)
+    })
   })
 
   $('#edit-character-form').on('submit', (e) => {
@@ -18,6 +59,8 @@ $(document).ready( () => {
   })
 
   $('#new-character-form').on('submit', (e) => {
+    event.preventDefault()
+    const newCharacterName = $("input[name='character-id-delete']").val()
 
   })
 })
