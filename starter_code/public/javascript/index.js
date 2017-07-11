b5367713b808b5d450a7a1a8823664978ac24386
Jordi Manchon <jordimanchon@me.com>
2017-07-11 19:38:05 +0200

    fetch and fetch all done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,56 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList().then(characterList => {
+      characterList.forEach(character => {
+        const $container = $('.characters-container');
+        const $character = $('<div>').addClass('character-info');
+        const $name = $('<div>').addClass('name').text(character.name);
+        const $occupation = $('<div>').addClass('occupation').text(character.occupation);
+        const $debt = $('<div>').addClass('debt').text(character.debt);
+        const $weapon = $('<div>').addClass('weapon').text(character.weapon);
+        $character
+          .append($name)
+          .append($occupation)
+          .append($debt)
+          .append($weapon);
+        $container.append($character);
+      });
+    });
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    let id = $('#getOneRegister').val();
+    charactersAPI.getOneRegister(id).then(character => {
+      const $container = $('.characters-container');
+      const $character = $('<div>').addClass('character-info');
+      const $name = $('<div>').addClass('name').text(character.name);
+      const $occupation = $('<div>').addClass('occupation').text(character.occupation);
+      const $debt = $('<div>').addClass('debt').text(character.debt);
+      const $weapon = $('<div>').addClass('weapon').text(character.weapon);
+      $character
+        .append($name)
+        .append($occupation)
+        .append($debt)
+        .append($weapon);
+      $container.append($character);
+    });
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    let id = $('#deleteOneRegister').val();
+    charactersAPI.deleteOneRegister(id).then(character => {
+      const $container = $('.characters-container');
+      
+    });
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
