e1c6dd206dc649009d120cec96b20508a16eba7a
ferrodri <f.rodriguez.hervias@gmail.com>
2017-07-11 20:20:32 +0200

    CRUD done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,59 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`,
+      method: "GET",
+      success: response => {
+        return response;
+      }
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${$('#id').val()}`,
+      method: "GET",
+      success: response => {
+        return response;
+      }
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`,
+      method: "POST",
+      data: {
+        "name": `${$('#name').val()}`,
+        "occupation": `${$('#occupation').val()}`,
+        "debt": `${$('#debt').val()}`,
+        "weapon": `${$('#weapon').val()}`
+      },
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${$('#editing').val()}`,
+      method: 'PATCH',
+      data: {
+        "name": `${$('#ename').val()}`,
+        "occupation": `${$('#eoccupation').val()}`,
+        "debt": `${$('#edebt').val()}`,
+        "weapon": `${$('#eweapon').val()}`
+      },
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${$('#delete').val()}`,
+      type: 'DELETE',
+    });
   }
+
 }
