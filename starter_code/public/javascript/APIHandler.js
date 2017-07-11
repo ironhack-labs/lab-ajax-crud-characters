a7876f85059756f162e5d68ee7e2b3c567b338e8
sara <sara-cg@github.com>
2017-07-11 18:50:39 +0200

    first update

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        method: 'GET',
+        dataType: 'json',
+        url: `${this.BASE_URL}/characters`,
+        success: (data) => {
+          resolve(data);
+        }
+      });
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        method: 'GET',
+        dataType: 'json',
+        url: `${this.BASE_URL}/characters/:id`,
+        success: (data) => {
+          resolve(data);
+        }
+      });
+    });
   }
 
-  createOneRegister () {
+  createOneRegister() {
+    const characterInfo = {
+      name: $('create-name').val(),
+      occupation: $('create-occupation').val(),
+      weapon: $('create-weapon').val(),
+      debt: $('create-debt').val()
+    };
 
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        method: 'POST',
+        url: `${this.BASE_URL}/characters`,
+        data: characterInfo,
+        success: (data) => {
+          resolve(data);
+        }
+      });
+    });
   }
 
-  updateOneRegister () {
+  updateOneRegister() {
+    const updateInfo = {
+      name: $('edit-name').val(),
+      occupation: $('edit-occupation').val(),
+      weapon: $('edit-weapon').val(),
+      debt: $('edit-debt').val()
+    };
 
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        method: 'PATCH',
+        url: `${this.BASE_URL}/characters/:id`,
+        data: updateInfo,
+        success: (patchResponse) => {
+          resolve(patchResponse);
+        }
+      });
+    });
   }
 
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,33 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
-$(document).ready( () => {
+$(document).ready(() => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList().then(characterList => {
+      console.log(characterList);
+    });
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister().then(characterList => {
+      console.log(characterList);
+    });
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister().then(characterList => {
+      $(characterList).remove(characterInfo);
+    });
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    charactersAPI.updateOneRegister().then(characterList => {
+      $(characterList).append(updateInfo);
+    });
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    charactersAPI.createOneRegister().then(characterList => {
+      $(characterList).append(characterInfo);
+    });
+  });
+});
