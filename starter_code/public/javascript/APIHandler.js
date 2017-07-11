9cc8c62fa7f651f9b215eb370612e4a8e4baa0f4
Nik E <NikolasEchenique@gmail.com>
2017-07-11 10:25:15 -0400

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,110 @@ class APIHandler {
   }
 
   getFullList () {
+    $.ajax({ // 1st argument -> giant settings object
+          // Minimum 4 settings: url(actual url of the api), method (what method the api tells you to use), success & error(callbacks that do shit depending on what happened)
+    url: 'https://ih-api.herokuapp.com/characters',
+    method: 'GET',
 
+
+    // if successful, put some of the data on the screen (DOM Manipulation)
+    success: (responseFromApi) => {
+      // the first parameter of the 'success' callback will always be the data we get from the API
+      console.log('WOOT WOOT, MOTHERFUCKER!!');
+      console.log(responseFromApi);
+    },
+
+
+    // if error, show error feedback (DOM manipulation)
+    error: (errorFromApi) => {
+        alert('Sorry, shit got fucked up');
+        console.log(errorFromApi);
+    }
+  });
   }
 
-  getOneRegister () {
+  getOneRegister (myId) {
+    $.ajax({ // 1st argument -> giant settings object
+            // Minimum 4 settings: url(actual url of the api), method (what method the api tells you to use), success & error(callbacks that do shit depending on what happened)
+      url: 'https://ih-api.herokuapp.com/characters/' + myId ,
+      method: 'GET',
 
+
+      // if successful, put some of the data on the screen (DOM Manipulation)
+      success: (responseFromApi) => {
+        // the first parameter of the 'success' callback will always be the data we get from the API
+        console.log('WOOT WOOT, MOTHERFUCKER!!' + myId);
+        console.log(responseFromApi);
+
+      },
+
+
+      // if error, show error feedback (DOM manipulation)
+      error: (errorFromApi) => {
+          alert('Sorry, shit got fucked up');
+          console.log(errorFromApi);
+      }
+    });
   }
 
-  createOneRegister () {
+  createOneRegister (newCharacterDetails) {
+    $.ajax({ // 1st argument -> giant settings object
+          // Minimum 4 settings: url(actual url of the api), method (what method the api tells you to use), success & error(callbacks that do shit depending on what happened)
+    url: 'https://ih-api.herokuapp.com/characters',
+    method: 'POST',
 
+    // the "data" setting is only used when you need to send extra info to the API
+    data: newCharacterDetails,
+    // "newCharacterDetails" is an object that contains:
+    // 'name', 'occupation' & 'weapon' properties
+
+    // if successful, put some of the data on the screen (DOM Manipulation)
+    success: (responseFromApi) => {
+      // the first parameter of the 'success' callback will always be the data we get from the API
+      console.log('WOOT WOOT, MOTHERFUCKER!! THE POST WORKED!');
+    },
+
+
+    // if error, show error feedback (DOM manipulation)
+    error: (errorFromApi) => {
+        alert('Sorry, shit got fucked up');
+        console.log(errorFromApi);
+    }
+  });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (myId, newInfo) {
+    $.ajax({
+        url: ' https://ih-api.herokuapp.com/characters/' + myId,
+        method: 'Patch',
+        data: newInfo,
+        success: (responseFromApi) => {
+          console.log('SUCCESS' + myId);
+          alert('Great Job! Update worked!');
+          console.log(responseFromApi);
+        },
+        error: (errorFromApi) => {
+          console.log('SHIT' + myId);
+          alert('FUUUUUUUCK');
+          console.log(errorFromApi);
+        }
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (myId) {
+    $.ajax({
+        url: ' https://ih-api.herokuapp.com/characters/' + myId,
+        method: 'Delete',
+        success: (responseFromApi) => {
+          console.log('SUCCESS' + myId);
+          alert('Great Job! Update worked!');
+          console.log(responseFromApi);
+        },
+        error: (errorFromApi) => {
+          console.log('SHIT' + myId);
+          alert('FUUUUUUUCK');
+          console.log(errorFromApi);
+        }
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    const charNum = $('#charId').val();
+    charactersAPI.getOneRegister(charNum);
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    e.preventDefault();
+    const deleteCharacterId = $('#deleteCharId').val();
+    console.log('USER INPUTTED' + deleteCharacterId);
+    // call the function that makes the AJAX request
+    charactersAPI.deleteOneRegister(deleteCharacterId);
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
+    const updatedInfo = {
+    name: $('#updateName').val(),
+    weapon: $('#updateWeapon').val(),
+    occupation: $('#updateOccupation').val()
+  };
 
-  })
+  // retrieve the character id from the input
+  const characterId = $('#updateCharacterId').val();
+  console.log('USER INPUTTED' + characterId);
+  // call the function that makes the AJAX request
+  charactersAPI.updateOneRegister(characterId, updatedInfo);
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    const characterInfo = {
+      name: $('#postCharacterName').val(),
+      occupation: $('#postCharacterOccupation').val(),
+      weapon: $('#postCharacterWeapon').val()
+    };
+    charactersAPI.createOneRegister(characterInfo);
+  });
 
-  })
-})
+});
