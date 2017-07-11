7906dcae9089f0330767890d1aa771f5525149ca
Mario Martin <mariomarsan7@gmail.com>
2017-07-11 18:57:14 +0200

    iteration 1

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,44 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  // Requests to API
+  getFullList() {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`,
+      method: 'GET',
+      dataType: 'json',
+    });
+  }
+
+  getOneRegister(id) {
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${id}`,
+      method: 'GET',
+      dataType: 'json'
+    });
+  }
+
+  createOneRegister(obj, err) {
+    if (err){
+      console.log('err',err);
+      return 'Error. Cannot create new char';
+    }
+    else
+      return $.ajax({
+        url: `${this.BASE_URL}/characters/`,
+        method: 'POST',
+        dataType: 'json',
+        data: obj
+      });
+  }
+
+  updateOneRegister() {
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,78 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
-$(document).ready( () => {
+$(document).ready(() => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    console.log('fecht-all');
+    charactersAPI.getFullList().then(characterList => {
+      console.log(characterList);
+    });
+  });
 
   $('#fetch-one').on('click', (e) => {
+    console.log('fecht-one');
+    var charId = document.getElementById("char-id").value;
+    charactersAPI.getOneRegister(charId).then(character => {
+      console.log(character);
+    }, err => {
+      console.log(err, 'Oppss character not found');
+    });
+  });
 
-  })
+  function getNewCharFormData() {
+    let $name = $('#newName').val();
+    let $occupation = $('#newOccupation').val();
+    let $weapon = $('#newWeapon').val();
+    let $debt = $('#newDebt');
+    let debVal;
+    let error = { name: false, occupation: false, weapon: false};
 
-  $('#delete-one').on('click', (e) => {
+    if ($debt.is(":checked"))
+      debVal = true;
+    else
+      debVal = false;
 
-  })
+    if(!$name) {
+      error.name = true;
+      return error;
+    }else if(!$occupation){
+      error.occupation = true;
+      return error;
+    }else if(!$weapon){
+      error.weapon = true;
+      return error;
+    }else{
+      return {
+        name: $name,
+        occupation: $occupation,
+        debt: debVal,
+        weapon: $weapon
+      };
+    }
+
+
+  }
+
+  $('#new-character-form').on('submit', (event) => {
+    event.preventDefault();   /* stop form from submitting normally */
+    let data = getNewCharFormData(); // get data from form
+
+    charactersAPI.createOneRegister(data).then(newChar => {
+      console.log('New character created: ', newChar);
+    }, err => {
+      console.log(err);
+    });
+
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
-  $('#new-character-form').on('submit', (e) => {
+  $('#delete-one').on('click', (e) => {
 
-  })
-})
+  });
+
+
+
+
+});
