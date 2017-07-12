36de20c16a6bd34a3e9002641da32de618388596
Carlos Dueñas <carlosdss84@gmail.com>
2017-07-12 03:51:04 +0200

    parcial

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,45 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return $.ajax({
+      url: this.BASE_URL + "/characters",
+      method: "GET",
+    });
+  }
+
+  getOneRegister(id) {
+    return $.ajax({
+      url: this.BASE_URL + "/characters/" + id,
+      method: "GET",
+
+    });
+  }
+
+  createOneRegister(newCharacter) {
+    $.ajax({
+      method: 'POST',
+      url: this.BASE_URL + "/characters",
+      data: character,
+    });
+  }
+
+  updateOneRegister(id, character) {
+    $.ajax({
+      method: 'PATCH',
+      url: this.BASE_URL + "/characters/" + id,
+      data: character
+    });
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return $.ajax({
+      method: 'DELETE',
+      url: this.BASE_URL + "/characters/" + id,
+      error: 404
+    });
   }
 }
