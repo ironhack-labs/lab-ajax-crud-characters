69520b5133998c905f848ebe0825b448fe0dba73
Ana Milojevic <anamilojevicd@gmail.com>
2017-07-12 10:53:22 +0200

    wip

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,31 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return $.ajax({url: `${this.BASE_URL}/characters`})
   }
 
   getOneRegister () {
-
+    return $.ajax({url: `${this.BASE_URL}/characters/${id}`})
   }
 
   createOneRegister () {
-
+    return $.ajax({
+      url: `${this.BASE_URL}/characters`,
+      method: "POST"
+    })
   }
 
   updateOneRegister () {
-
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${id}`,
+      method: "PATCH"
+    })
   }
 
   deleteOneRegister () {
-
+    return $.ajax({
+      url: `${this.BASE_URL}/characters/${id}`,
+      method: "DELETE"
+    })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,57 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    var characters = [];
+    charactersAPI.getFullList().then(e => {
+      e.map((el, i) => {
+        var item = [
+          $("name").clone().append(' ' + el.name),
+          $("occupation").clone().append(' ' + el.occupation),
+          $("debt").clone().append(' ' + el.debt),
+          $("weapon").clone().append(' ' + el.weapon)
+        ]
+        characters.push($(".character-info").clone().html(item));
+      });
+      $('.characters-container').append(characters);
+      $('.character-info').first().hide();
+    });
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    charactersAPI.getOneRegister().then(e => {
+      var item = [
+        $("name").clone().append(' ' + e.name),
+        $("occupation").clone().append(' ' + e.occupation),
+        $("debt").clone().append(' ' + e.debt),
+        $("weapon").clone().append(' ' + e.weapon)
+      ]
+      $(".characters-container").append($(".character-info").clone().html(item));
+      $(".character-info").first().hide();
+    });
   })
 
   $('#delete-one').on('click', (e) => {
-
+    charactersAPI.deleteOneRegister($("input[name=character-id-delete]").val()).then(e => {
+      var item = [
+        $(".name").clone().append(' ' + e.name),
+        $(".occupation").clone().append(' ' + e.occupation),
+        $(".debt").clone().append(' ' + e.debt),
+        $(".weapon").clone().append(' ' + e.weapon)
+      ]
+      $(".characters-container").append($(".character-info").clone().html(item));
+      $(".character-info").first().remove();
+    });
   })
 
   $('#edit-character-form').on('submit', (e) => {
-
+    e.preventDefault();
+    var formInfo = $("#new-character-form").serialize();
+    charactersAPI.updateOneRegister($("input[name=chr-id]").val(), formInfo);
   })
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
+    e.preventDefault();
+    var formInfo = $("#new-character-form").serialize();
+    charactersAPI.deleteOneRegister(formInfo);
+  });
 })
