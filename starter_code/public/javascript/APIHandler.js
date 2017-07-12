4fd603e74ea78ec528eacc36f88147fca842b4c2
Mi6u3l <miguelbragagomes@gmail.com>
2017-07-12 20:47:51 +0200

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,92 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList (handleResponse) {
+    $.ajax({
+      url: this.BASE_URL + "/characters",
+      method: "GET",
+      success: function (response) {
+        //The callback function that will be executed if the request is completed succesfully
+        //This function will have a parameter with the server response.
+        handleResponse(response); 
+      },
+      error: function (err) {
+        //The callback function that will be executed if the request fails, whether it was a client or a server error
+        //It will have a parameter with error that caused the request to fail
+      },
+    });
+  }
+
+  getOneRegister (id, handleResponse) {
+    $.ajax({
+      url: this.BASE_URL + "/characters/" + id,
+      method: "GET",
+      success: function (response) {
+        //The callback function that will be executed if the request is completed succesfully
+        //This function will have a parameter with the server response.
+        handleResponse(response); 
+      },
+      error: function (err) {
+        //The callback function that will be executed if the request fails, whether it was a client or a server error
+        //It will have a parameter with error that caused the request to fail
+      },
+    });
 
   }
 
-  getOneRegister () {
+  createOneRegister (characterInfo, handleResponse) {
+    $.ajax({
+      url:     this.BASE_URL + "/characters/",
+      method:  'POST',
+      data: characterInfo,
+      success: function (response) {
+        //The callback function that will be executed if the request is completed succesfully
+        //This function will have a parameter with the server response.
+        handleResponse(response); 
+      },
+      error: function (err) {
 
+        handleResponse(err); 
+        //The callback function that will be executed if the request fails, whether it was a client or a server error
+        //It will have a parameter with error that caused the request to fail
+      },
+    });
   }
 
-  createOneRegister () {
+  updateOneRegister(id, characterInfo, handleResponse) {
+      $.ajax({
+      url:     this.BASE_URL + "/characters/" + id,
+      method:  'PUT',
+      data: characterInfo,
+      success: function (response) {
+        //The callback function that will be executed if the request is completed succesfully
+        //This function will have a parameter with the server response.
+        handleResponse(response); 
+      },
+      error: function (err) {
 
+        handleResponse(err); 
+        //The callback function that will be executed if the request fails, whether it was a client or a server error
+        //It will have a parameter with error that caused the request to fail
+      },
+    });
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (id, handleResponse) {
+    $.ajax({
+      url: this.BASE_URL + "/characters/" + id,
+      method: "DELETE",
+      success: function (response) {
+        //The callback function that will be executed if the request is completed succesfully
+        //This function will have a parameter with the server response.
+        handleResponse(response); 
+      },
+      error: function (err) {
+        //The callback function that will be executed if the request fails, whether it was a client or a server error
+        //It will have a parameter with error that caused the request to fail
+        handleResponse(err); 
+      },
+    });
 
   }
 }
