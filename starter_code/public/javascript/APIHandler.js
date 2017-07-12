2ce02e7e3cb0546c0f273580826112c88b61008e
Julius Suominen <julius.suominen@gmail.com>
2017-07-12 20:18:13 +0200

    done, no character cards

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,77 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      url: this.BASE_URL + '/characters',
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
   }
 
-  getOneRegister () {
-
+  getOne (id) {
+    $.ajax({
+      url: this.BASE_URL + '/characters/' + id,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    });
   }
 
-  createOneRegister () {
-
+  createOne (formdata) {
+    $.ajax({
+      url: this.BASE_URL + '/characters',
+      method: "POST",
+      data: formdata,
+      success: function (response) {
+        console.log(response);
+        $('#send-data').css('background-color', '#50f442');
+      },
+      error: function (err) {
+        console.log(err);
+        $('#send-data').css('background-color', '#f4414a');
+      }
+    });
   }
 
-  updateOneRegister () {
-
+  updateOne (formdata, id) {
+    $.ajax({
+      url: this.BASE_URL + '/characters/' + id,
+      method: "PUT",
+      data: formdata,
+      success: function (response) {
+        console.log(response);
+        $('#send-update').css('background-color', '#50f442');
+      },
+      error: function (err) {
+        console.log(err);
+        $('#send-update').css('background-color', '#f4414a');
+      }
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOne (id) {
+    $.ajax({
+      url: this.BASE_URL + '/characters/' + id,
+      method: "DELETE",
+      success: function (response) {
+        console.log(response);
+        $('#delete-one').css('background-color', '#50f442');
+        refresh();
+      },
+      error: function (err) {
+        console.log(err);
+        $('#delete-one').css('background-color', '#f4414a');
+        refresh();
+      }
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,64 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    const id = $("[name=character-id]").val();
+    charactersAPI.getOne(id);
+  });
 
   $('#delete-one').on('click', (e) => {
-
+    const id = $("[name=character-id-delete]").val();
+    charactersAPI.deleteOne(id);
   })
 
   $('#edit-character-form').on('submit', (e) => {
-
+    e.preventDefault();
+    const id = $("[name=chr-id]").val();
+    const inputs = $('#edit-character-form :input');
+    const values = {};
+    inputs.each(function() {
+      if (this.name === 'debt') {
+        if($(this).is(':checked')) {
+          values['debt'] = true;
+        } else {
+          values['debt'] = false;
+        }
+      }
+      if(this.name !== "" && this.name !== 'debt') {
+        values[this.name] = $(this).val();
+      }
+    });
+    charactersAPI.updateOne(values, id);
+    refresh();
   })
 
   $('#new-character-form').on('submit', (e) => {
-
+    e.preventDefault();
+    const inputs = $('#new-character-form :input');
+    const values = {};
+    inputs.each(function() {
+      if (this.name === 'debt') {
+        if($(this).is(':checked')) {
+          values['debt'] = true;
+        } else {
+          values['debt'] = false;
+        }
+      }
+      if(this.name !== "" && this.name !== 'debt') {
+        values[this.name] = $(this).val();
+      }
+    });
+    charactersAPI.createOne(values);
+    refresh();
   })
 })
+
+
+function refresh() {
+  var timeout = setTimeout( () => {
+    document.location.reload();
+  }, 1000);
+}
