a20687d03b748ee33b4e9c8c8277ee7e94c1d30d
Daniel Fernando Palma Lopez <palma@Daniels-MacBook-Pro.local>
2017-07-12 09:39:37 +0200

    not finish

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -2,17 +2,37 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+//OJO
+//Con AJAX NO se necesita new Promise
+//AJAX es una promesa que internamente
+//ya retorna una promesa por lo que tampoco
+//requiere success porque internamente
+//retorna lo resuelto.
   getFullList () {
-
+  return new Promise ((res, rej) => { //si se cumple es con res(resolve) y si no es rej(reject)
+    $.ajax({ //las promesas necesitan retornar algo
+        method: 'GET', //Get es por default
+        dataType: 'json', //json es por default
+        url: `${this.BASE_URL}/characters/`, //la barra aqui en /characters o en el api
+        success: (data) => { //success es una función que si se llama a ella en index.js puede ejecutar el then
+          res(data);
+        }
+      });
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return $.ajax({ //las promesas necesitan retornar algo
+        url: `${this.BASE_URL}/characters/${id}`
+      });
   }
 
   createOneRegister () {
-
+    return $.ajax({
+          method: 'POST', //post to create!!!!!
+          url: `${this.BASE_URL}/characters`,
+          data: e,
+        });
   }
 
   updateOneRegister () {
