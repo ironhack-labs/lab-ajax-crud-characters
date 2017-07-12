52f1a3aeca656ad9b76274205c8c6189ff49e3fa
Adrian Llerena <a.llerena001@gmail.com>
2017-07-12 15:18:18 -0400

    pull

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -2,14 +2,67 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
+// ---------------------------------------------------------------
 
-  getFullList () {
+
+
+  getFullList (characterId) {
+
+
+        $.ajax({
+
+            url: this.BASE_URL + '/characters',
+            method: 'GET',
+            success: (responseFromApi) => {
+                // The 1st parameter of the "success" callback
+                // will always be the data we get from the API.
+                console.log(responseFromApi);
+
+                // Add the information to the <p> tag
+
+            },
+
+            // if error, show error feedback (DOM manipulation)
+            error: (errorFromApi) => {
+                alert('Sorry! character data error. 😱');
+                console.log(errorFromApi);
+            }
+        });
+    }
+
+// -----------------------------------------------------------
+
+
+
+
+
+
+
+
+
+  getOneRegister (id) {
+
+    $.ajax({
+  url: this.BASE_URL + '/characters' + Id,
+  method: "GET",
+  success: function (response) {
+    console.log(response);
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
+
+
+
 
   }
 
-  getOneRegister () {
 
-  }
+
+
+
+
 
   createOneRegister () {
 
