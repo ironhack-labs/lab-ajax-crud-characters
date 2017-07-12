728a9d44695ae2fa0c2b5fba01b13afbf003d02d
valzie <skyla@users.noreply.github.com>
2017-07-12 11:12:11 -0400

    not done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,18 +1,73 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL =  "http:/ih-api.herokuapp.com";
   }
+
+
 
   getFullList () {
+    $.ajax({
+      url: this.BASE_URL + "/" + "characters",
+      method: "GET",
+      success: function (responseFromCharacterAPI){
+        // console.log('No Error, Hooray!');
+        console.log(responseFromCharacterAPI);
 
+      $('#characterInfo').html(`
+${responseFromCharacterAPI.name}
+${responseFromCharacterAPI.occupation}
+${responseFromCharacterAPI.debt}
+${responseFromCharacterAPI.weapon}`
+  );
+},
+  error: function (errorFromApi){
+    // console.log('Error, I\'m sad!');
+    console.log(errorFromApi);
+  },
+});
+}
+
+  getOneRegister (myId) {
+    $.ajax({
+      url: this.BASE_URL + "/characters/" + myId,
+      method: "GET",
+      success: function (responseFromCharacterAPI){
+        console.log('No Error, Hooray!');
+        console.log(responseFromCharacterAPI);
+
+    //   $('#characterInfo').html(`
+    // ${responseFromCharacterAPI.name}
+    // ${responseFromCharacterAPI.occupation}
+    // ${responseFromCharacterAPI.weapon}`
+    // );
+    },
+    error: function (errorFromApi){
+    console.log('Error, I\'m sad!');
+    console.log(errorFromApi);
+    },
+    });
   }
+//line 38 on index.js is calling createOneRegister
+  createOneRegister (newCharacterDetails) {
+    $.ajax({
+      url: this.BASE_URL + "/characters/",
+      method: "POST",
+      data: newCharacterDetails,
+      success: function (responseFromCharacterAPI){
+        console.log('No Error, Hooray!');
+        console.log(responseFromCharacterAPI);
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
+    //   $('#characterInfo').html(`
+    // ${responseFromCharacterAPI.name}
+    // ${responseFromCharacterAPI.occupation}
+    // ${responseFromCharacterAPI.weapon}`
+    // );
+    },
+    error: function (errorFromApi){
+    console.log('Error, I\'m sad!');
+    console.log(errorFromApi);
+    },
+    });
   }
 
   updateOneRegister () {
