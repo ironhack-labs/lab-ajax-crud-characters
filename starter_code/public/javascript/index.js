c2a8dbeecbb7ee183bac770b197003032490dfe0
zocha_sz <zofia.szczudlik@gmail.com>
2017-07-13 14:49:51 +0200

    Done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -52,103 +52,30 @@ $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
     event.preventDefault();
     resetButtonColor ();
-
-    $.ajax({
-      method: 'GET',
-      url: `https://ih-api.herokuapp.com/characters`,
-      success: showAllCharacters,
-      error: handleError
-    });
+    charactersAPI.getFullList ();
   })
 
   $('#fetch-one').on('click', (e) => {
     event.preventDefault();
     resetButtonColor ();
-
-    // The character ID that we will plug into the API's URL
-    const charId = $('#character-id').val();
-
-    $.ajax({
-      method: 'GET',
-      url: `https://ih-api.herokuapp.com/characters/${charId}`,
-      success: showOneCharacter ,
-      error: handleError
-    });
+    charactersAPI.getOneRegister ();
   })
 
   $('#delete-one').on('click', (e) => {
     event.preventDefault();
     resetButtonColor ();
-
-    // The character ID that we will plug into the API's URL
-    const charId = $('#character-id-delete').val();
-
-    $.ajax({
-      method: 'DELETE',
-      url: `https://ih-api.herokuapp.com/characters/${charId}`,
-      success: () => {
-        console.log('Delete SUCCESS!');
-        $('#delete-one').css("background-color", "green");
-      },
-      error: () => {
-        $('#delete-one').css("background-color", "red");
-        handleError
-      }
-    });
+    charactersAPI.deleteOneRegister ();
   })
 
   $('#edit-character-form').on('submit', (e) => {
     event.preventDefault();
     resetButtonColor ();
-
-    const updateInfo = {
-      name: $('#edit-name').val(),
-      occupation: $('#edit-occupation').val(),
-      weapon: $('#edit-weapon').val()
-      // debt: $('#edit-debt').val()
-    };
-
-    // The character ID that we will plug into the API's URL
-    const charId = $('#edit-id').val();
-
-    $.ajax({
-        // Notice that we are using PATCH. You could also use PUT.
-      method: 'PATCH',
-      url: `https://ih-api.herokuapp.com/characters/${charId}`,
-      data: updateInfo,
-      success: () => {
-        $('#send-edit-data').css("background-color", "green");
-      },
-      error: () => {
-        $('#send-edit-data').css("background-color", "red");
-        handleError
-      }
-    });
+    charactersAPI.updateOneRegister ();
   })
 
   $('#new-character-form').on('submit', (e) => {
     event.preventDefault();
     resetButtonColor ();
-
-    const characterInfo = {
-      name: $('#new-name').val(),
-      occupation: $('#new-occupation').val(),
-      weapon: $('#new-weapon').val(),
-      dept: $('#new-dept').val(),
-    };
-
-    $.ajax({
-      method: 'POST',
-      url: 'https://ih-api.herokuapp.com/characters',
-      data: characterInfo,
-      success: () => {
-        showFeedback;
-        $('#send-new-data').css("background-color", "green");
-      },
-      error: () => {
-        $('#send-new-data').css("background-color", "red");
-        handleError
-      }
-    });
+    charactersAPI.createOneRegister ()
   })
 })
