648f95932808389f5250149d3aadd65faf30aee0
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-07-26 10:55:09 +0200

    PAR Characters Octave -- some error with adding/updating with API, problem with object format being passed

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,87 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
+  /////////////////////////////////////////////////////////////////////////////
+  // Verb: GET, Route: "/characters"
+  // It receives NO parameters
+  // It returns the full characters list
+  // It returns JSON
+
   getFullList () {
 
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
+    return axios({
+      url: this.BASE_URL,
+      method: "GET"
+    })
 
   }
 
-  updateOneRegister () {
+  /////////////////////////////////////////////////////////////////////////////
+  // Verb: GET, Route: "/characters/:id"
+  // It receives the character ID as a parameter (route)
+  // It returns the character with the indicated id
+  // It returns JSON
+
+  getOneRegister (id) {
+
+    return axios({
+      url: this.BASE_URL + id,
+      method: "GET"
+    });
 
   }
 
-  deleteOneRegister () {
+  /////////////////////////////////////////////////////////////////////////////
+  // Verb: POST, Route: "/characters"
+  // It receives an object as a parameter, with the following format: { name: string, occupation: string, debt: boolean, weapon: string }
+  // It returns the created character if there are no errors
+  // It returns the wrong fields if there is some error
+  // It returns JSON
+
+  createOneRegister (character) {
+
+    return axios({
+      url: this.BASE_URL,
+      method: "POST",
+      data: character);
+    });
+    
+  }
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Verb: PATCH/PUT, Route: "/characters/:id"
+  // It receives the character id as a parameter (route)
+  // It receives an object as a parameter, with the following format: { name: string, occupation: string, debt: boolean, weapon: string }
+  // All the fields are optionals
+  // It returns the updated character if there are no errors
+  // It returns "Character not found" if there is no character with the indicated id
+  // It returns JSON / text
+
+  updateOneRegister (id, character) {
+
+    return axios({
+      url: this.BASE_URL + id,
+      method: "PATCH",
+      data: character
+    });
 
   }
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  // Verb: DELETE, Route: "/characters/:id"
+  // It receives the character id as a parameter (route)
+  // It returns "Character has been successfully deleted" if there are no errors
+  // It returns "Character not found" if there is no character with the indicated id
+  // It returns text
+
+  deleteOneRegister (id) {
+
+    return axios({
+      url: this.BASE_URL + id,
+      method: "DELETE"
+    });
+
+  }
+
 }
