b857c75c28f647c69c3407865b0f7f910e3d5423
raquel500c <raquel500c@gmail.com>
2017-09-13 01:15:19 +0200

    fetch-one and fetch-all ok

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,36 @@
 const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
-$(document).ready( () => {
+$(document).ready(() => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList().then(p => {
+      console.log(p)
+    })
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    const characterId = $('#character-id').val()
+    charactersAPI.getOneRegister(characterId).then( p => {
+      console.log(p)
+    })
   })
 
   $('#delete-one').on('click', (e) => {
+    const characterId = $('character-id-delete').val()
+    charactersAPI.deleteOneRegister(characterId).then( p => {
+       console.log(p) })
+  })
 
+  $('#new-character-form').on('submit', (e) => {
+    const name = $('input[name=name]').val();
+    const occupation = $('input[name=occupation]').val();
+    const weapon = $('input[name=weapon]').val();
+    const debt = $('checkbox[name=debt]').val();
+    //charactersAPI.createOneRegister().then(p => { console.log(p) })
   })
 
   $('#edit-character-form').on('submit', (e) => {
 
   })
 
-  $('#new-character-form').on('submit', (e) => {
 
-  })
 })
