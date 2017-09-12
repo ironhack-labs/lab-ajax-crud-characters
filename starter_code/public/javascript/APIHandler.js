4112249b5f0f1b0eaeafba7964c1ad2aebd51dfb
Lucas <lfbodereau@gmail.com>
2017-09-12 21:42:41 +0200

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -22,7 +22,6 @@ class APIHandler {
   createOneRegister () {
     return $.ajax({
       url: `${this.BASE_URL}/characters`,
-      data: { name: string, occupation: string, debt: boolean, weapon: string },
       dataType: 'JSON',
       method: 'POST',
       success: showRegisterSuccess,
@@ -33,8 +32,7 @@ class APIHandler {
   updateOneRegister (id) {
     return $.ajax({
       url: `${this.BASE_URL}/characters/:id`,
-      data: { name: string, occupation: string, debt: boolean, weapon: string },
-      dataType: 'JSON', text,
+      dataType: 'JSON',
       method: 'PATCH',
       success: showUpdateSuccess,
       error: showUpdateError
@@ -50,32 +48,4 @@ class APIHandler {
       error: showDeleteError
     });
   }
-}
-
-function showRegisterSuccess (postSuccess) {
-  console.log({});
-  console.log(postSuccess);
-}
-function showRegisterError (postError) {
-  console.log("Wrong fields");
-  console.log(postError);
-}
-
-function showUpdateSuccess (postSuccess) {
-  console.log({});
-  console.log(postSuccess);
-}
-function showUpdateError (postError) {
-  console.log("Character not found");
-  console.log(postError);
-}
-
-function showDeleteSuccess (postSuccess) {
-  console.log("Character has been successfully deleted");
-  console.log(postSuccess);
-}
-
-function showDeleteError (postError) {
-  console.log("Character not found");
-  console.log(postError);
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,67 @@
 const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
-    // $('#fetch-all').append(createListItems(characters));
+const $charContain = $(".characters-container");
+const addCharacter = (e) => {
+  const $name = $(`<div><p>Name: ${e.name}</p></div>`).addClass('name');
+  const $occupation = $(`<div><p>Occupation: ${e.occupation}</p></div>`).addClass('occupation');
+  const $debt = $(`<div><p>Debt: ${e.debt}</p></div>`).addClass('debt');
+  const $weapon = $(`<div><p>Weapon: ${e.weapon}</p></div>`).addClass('weapon');
+  const completedCharacter = $('<div>').addClass('character-info').append($name).append($occupation).append($debt).append($weapon);
+  $container.append(completedCharacter);
+};
+
+
+  $('.fetch-all').on('click', (e) => {
+    charactersAPI.getFullList().then(characterList => {
+      console.log(characterList);
+      characterList.forEach(e => e ? addCharacter(e) : console.log("Error"));
+    });
   });
 
-  $('#fetch-one').on('click', (e) => {
-
+  $('.fetch-one').on('click', (e) => {
+    charactersAPI.getOneRegister($('.character-id').val()).then(character => {
+      $('.name').text(character.name);
+      $('.occupation').text(character.occupation);
+      $('.debt').text(character.debt);
+      $('.weapon').text(character.weapon);
+    });
   });
 
-  $('#delete-one').on('click', (e) => {
-
+  $('.delete-one').on('click', (e) => {
+      charactersAPI.deleteOneRegister($('.character-id-delete').val());
+      console.log("Character has been successfully deleted");
   });
 
-  $('#edit-character-form').on('submit', (e) => {
-
+  $('.edit-character-form').on('submit', (e) => {
+    e.preventDefault();
+    let idCharacter = $('.update-chr-id').val();
+    let name = $('.update-name').val();
+    let occupation = $('.update-occupation').val();
+    let debt = $('.update-debt').val() === "on" ? true : false;
+    let weapon = $('.update-weapon').val();
+    const updatedCharacter = {
+      name: name,
+      occupation: occupation,
+      debt: debt,
+      weapon: weapon
+    };
+    console.log(idCharacter);
+    charactersAPI.updateOneRegister(idCharacter, updatedCharacter);
   });
 
-  $('#new-character-form').on('submit', (e) => {
-
+  $('.new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    let name = $('.create-name').val();
+    let occupation = $('.create-occupation').val();
+    let debt = $('.create-debt').val() === "on" ? true : false;
+    let weapon = $('.create-weapon').val();
+    const newCharacter = {
+      name: name,
+      occupation: occupation,
+      debt: debt,
+      weapon: weapon
+    };
+    charactersAPI.createOneRegister(newCharacter);
   });
 });
