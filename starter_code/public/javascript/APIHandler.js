081ff4a7db7a84f60db650b8940a60ddcb23ab22
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-09-12 19:58:03 +0200

    iteracion 2 casi terminada

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -22,6 +22,10 @@ class APIHandler {
       url: `http://ih-api.herokuapp.com/characters/${characterId}`,
       method: "GET",
       success: function(response) {
+        $(".character-info .name").html(JSON.stringify(response.name));
+        $(".character-info .occupation").html(JSON.stringify(response.occupation));
+        $(".character-info .debt").html(JSON.stringify(response.debt));
+        $(".character-info .weapon").html(JSON.stringify(response.weapon));
         console.log(response);
       },
       error: function(err) {
@@ -42,10 +46,12 @@ class APIHandler {
       method: "POST",
       data: characterInfo,
       success: function(response) {
+        $('#send-data').css('background','green');
         console.log(response);
       },
       error: function(err) {
         console.log(err);
+        $('#send-data').css('background','red');
       },
     });
   }
@@ -64,10 +70,12 @@ class APIHandler {
       data: characterInfo,
       success: (patchResponse) => {
       console.log('Update SUCCESS!');
+      $('#edit-data').css('background','green');
       console.log(patchResponse);
       },
       error: function(err) {
         console.log(err);
+        $('#edit-data').css('background','red');
       },
     });
   }
@@ -78,10 +86,12 @@ class APIHandler {
       url: `http://ih-api.herokuapp.com/characters/${characterId}`,
       method: "DELETE",
       success: function(response) {
+        $('#delete-one').css('background','green');
         console.log(response);
       },
       error: function(err) {
         console.log(err);
+          $('#delete-one').css('background','red');
       },
     });
   }
