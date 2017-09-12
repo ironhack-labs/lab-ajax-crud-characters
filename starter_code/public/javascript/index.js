04de23e70516af634f62b815755ef359886ccce8
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-12 19:23:14 +0200

    hacha parte de mostrar todos y uno

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,12 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList().then(p => {console.log(p)})
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    let posi = $('.selectID').val()
+    charactersAPI.getOneRegister(posi).then(p => {console.log(p)})
   })
 
   $('#delete-one').on('click', (e) => {
