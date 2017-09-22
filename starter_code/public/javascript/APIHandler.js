099324f837e7c92206d86fef057bc7010a95cd2f
Ryan Wang <qwang1993@gmail.com>
2017-09-22 18:10:33 -0400

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -5,21 +5,125 @@ class APIHandler {
 
   getFullList () {
 
-  }
+  $.ajax(
+    {
+      url: 'https://ih-api.herokuapp.com/characters/',
+      method: 'GET',
+
+      success: (characterList) => {
+        characterList.forEach((oneCharacter) => {
+        $('.characters-container').append(`
+            <div class="character-info">
+                <div class="id"> ID: ${oneCharacter.id} </div>
+                <div class="name">Name: ${oneCharacter.name} </div>
+                <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
+                <div class="debt">Debt: ${oneCharacter.debt}</div>
+                <div class="weapon">Weapon: ${oneCharacter.weapon}</div>
+            </div>
+              `);
+            });
+          },
+
+      error: (errorInfo) => {
+        console.log(errorInfo);
+      }
+
+  });
+
+}
+
+
 
   getOneRegister () {
 
+    const characterId = $('input[name=character-id]').val();
+
+    $.ajax(
+      {
+        url: 'https://ih-api.herokuapp.com/characters/' + characterId,
+        method: 'GET',
+
+        success: (oneCharacter) => {
+          $('.characters-container').append(`
+              <div class="character-info">
+                  <div class="id"> ID: ${oneCharacter.id} </div>
+                  <div class="name">Name: ${oneCharacter.name} </div>
+                  <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
+                  <div class="debt">Debt: ${oneCharacter.debt}</div>
+                  <div class="weapon">Weapon: ${oneCharacter.weapon}</div>
+              </div>
+                `);
+            },
+
+        error: (errorInfo) => {
+          console.log(errorInfo);
+        }
+
+    });
   }
 
-  createOneRegister () {
+  createOneRegister (characterSubmission) {
 
+    $.ajax({
+      url: 'https://ih-api.herokuapp.com/characters/',
+      method: 'POST',
+
+      data: characterSubmission,
+
+      success: (postResult) => {
+
+        $('#send-data').removeClass('failed');
+        $('#send-data').addClass('active');
+      },
+
+      error: (errorInfo) => {
+        $('#send-data').removeClass('active');
+        $('#send-data').addClass('failed');
+      }
+    });
   }
 
-  updateOneRegister () {
+  updateOneRegister (updateSubmission) {
 
+    const characterId = $('input[name=chr-id]').val();
+    $.ajax({
+      url: 'https://ih-api.herokuapp.com/characters/' + characterId,
+      method: 'PATCH',
+
+      data: updateSubmission,
+
+      success: (postResult) => {
+
+        $('#edit-data').removeClass('failed');
+        $('#edit-data').addClass('active');
+      },
+
+      error: (errorInfo) => {
+        $('#edit-data').removeClass('active');
+        $('#edit-data').addClass('failed');
+      }
+    });
   }
 
   deleteOneRegister () {
 
+    const characterId = $('input[name=character-id-delete]').val();
+
+    $.ajax(
+      {
+        url: 'https://ih-api.herokuapp.com/characters/' + characterId,
+        method: 'DELETE',
+
+        success: () => {
+          $('#delete-one').removeClass('failed');
+          $('#delete-one').addClass('active');
+            },
+
+        error: (errorInfo) => {
+          $('#delete-one').removeClass('active');
+          $('#delete-one').addClass('failed');
+        }
+
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,41 @@
-const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister();
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister();
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
+    const updateCharacterInfo = {
+      name: $('input[name=name]').val(),
+      occupation: $('input[name=occupation]').val(),
+      weapon: $('input[name=weapon]').val(),
+      debt: $('input[name=debt]').is(":checked")
+    };
+
+    charactersAPI.updateOneRegister(updateCharacterInfo);
+  });
 
   $('#new-character-form').on('submit', (e) => {
+      e.preventDefault();
 
-  })
-})
+      const newCharacterInfo = {
+        name: $('input[name=newName]').val(),
+        occupation: $('input[name=newOccupation]').val(),
+        weapon: $('input[name=newWeapon]').val(),
+        debt: $('input[name=newDebt]').is(":checked")
+      };
+
+      charactersAPI.createOneRegister(newCharacterInfo);
+    });
+});
