2f8dbd58f3796ae2f023a009b79de55db45d19a2
allanac <acole006@fiu.edu>
2017-09-25 10:02:58 -0400

    unfinished

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,14 @@ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+     getAllCharacters();
   })
 
   $('#fetch-one').on('click', (e) => {
+    e.preventDefault();
 
+    const idFromInput = $('input[name="character-id"]').val();
+    getOneCharacter(idFromInput);
   })
 
   $('#delete-one').on('click', (e) => {
@@ -21,3 +24,108 @@ $(document).ready( () => {
 
   })
 })
+
+function getAllCharacters(characters) {
+  // $.ajax() is a jQuery function
+  $.ajax(
+    // 1 argument -> settings object
+    {
+      url: 'https://ih-api.herokuapp.com/characters/',
+      method: 'GET',
+
+      data:characters,
+
+      //what to do when everything worked (we get the data)
+      success: (infoFromApi)=> {
+        // start by displaying variable you got from API
+        console.log('Character Fetch Success! 🙌🏽');
+        console.log(infoFromApi);
+        infoFromApi.forEach((oneCharacter) => {
+
+          $('.characters-container').append(`
+              <div class="character-info">
+                <div>Name: ${oneCharacter.name}</div>
+                <div>Occupation: ${oneCharacter.occupation}</div>
+                <div> Debt: ${oneCharacter.debt}</div>
+                <div> Weapon: ${oneCharacter.weapon}</div>
+              </div>
+          `);
+        })
+
+      //what to do when the request errors (we did )
+      error:(errorInfo) => {
+        console.log('Character fetch Error 🚨');
+        console.log(errorInfo);
+      }
+    }
+  }
+);
+}
+
+function getOneCharacter(charId) {
+  // $.ajax() is a jQuery function
+  $.ajax(
+    // 1 argument -> settings object
+    {
+      url: 'https://ih-api.herokuapp.com/characters/' + charId + '/',
+      method: 'GET',
+
+      //what to do when everything worked (we get the data)
+      success: (singleChar)=> {
+        // start by displaying variable you got from API
+        console.log('Character Fetch Success! 🙌🏽');
+        console.log(singleChar);
+
+
+        $('.characters-container').append(`
+          <div>
+            <div>${singleChar.name}</div>
+            <div>${singleChar.occupation}</div>
+            <div>${singleChar.debt}</div>
+            <div>${singleChar.weapon}</div>
+        </div>
+        `);
+      },
+
+      //what to do when the request errors (we did )
+      error:(errorInfo) => {
+        console.log('Character fetch Error 🚨');
+        console.log(errorInfo);
+      }
+    }
+  );
+}
+
+function deleteOneCharacter(charId) {
+  // $.ajax() is a jQuery function
+  $.ajax(
+    // 1 argument -> settings object
+    {
+      url: 'https://ih-api.herokuapp.com/characters/' + charId + '/',
+      method: 'DELETE',
+
+      //what to do when everything worked (we get the data)
+      success: (singleChar)=> {
+        // start by displaying variable you got from API
+        console.log('Character Fetch Success! 🙌🏽');
+        console.log(singleChar);
+
+
+        $('.characters-container').append(`
+          <div>
+            <div>${singleChar.name}</div>
+            <div>${singleChar.occupation}</div>
+            <div>${singleChar.debt}</div>
+            <div>${singleChar.weapon}</div>
+        </div>
+        `);
+      },
+
+      //what to do when the request errors (we did )
+      error:(errorInfo) => {
+        console.log('Character fetch Error 🚨');
+        console.log(errorInfo);
+      }
+    }
+  );
+}
