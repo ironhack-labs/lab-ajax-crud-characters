9e244166fd09aa62bdaf75187cb1032d14a1b505
Valentin Perot <perot.valentin@gmail.com>
2017-10-04 09:08:41 +0200

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,56 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get('https://ih-crud-api.herokuapp.com/characters').then
+    (response => {
+      return response.data
+      console.log(response.data)
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get("https://ih-crud-api.herokuapp.com/characters/" + id).then
+      (response => {
+        return response.data
+        console.log(response)
+      }
+    )
   }
 
-  createOneRegister () {
-
+  createOneRegister (name, occupation, debt, weapon) {
+    return axios.post("http://ih-crud-api.herokuapp.com/characters", {
+      name: name,
+      occupation: occupation,
+      debt: debt,
+      weapon: weapon
+    })
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister (id, name, occupation, debt, weapon) {
+    return axios.patch("http://ih-crud-api.herokuapp.com/characters/" + id, {
+      name: name,
+      occupation: occupation,
+      debt: debt,
+      weapon: weapon
+    })
+    .then(response => {
+      return response.data
+    })
+    .catch(error => {
+      console.log("Character not found")
+    })}
 
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete("http://ih-crud-api.herokuapp.com/characters/" + id).then
+    (response => {
+     return response.data
+      console.log(response)
+    })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,86 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler(
+  "http://ih-crud-api.herokuapp.com/characters"
+);
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+$(document).ready(() => {
+  $("#fetch-all").on("click", e => {
+    charactersAPI.getFullList().then(char => {
+      char.forEach(charac => {
+        appendChar(charac);
+      });
+    });
+  });
 
-  })
+  $("#fetch-one").on("click", e => {
+    charactersAPI
+      .getOneRegister($("input[name=character-id]").val())
+      .then(appendChar);
+  });
 
-  $('#fetch-one').on('click', (e) => {
+  $("#delete-one").on("click", e => {
+    charactersAPI
+      .deleteOneRegister($("input[name=character-id-delete]").val())
+      .then(response => {
+        $("#delete-one").css("background-color", "green");
+      })
+      .catch(err => {
+        $("#delete-one").css("background-color", "red");
+      });
+  });
 
-  })
+  $("#edit-character-form").on("submit", e => {
+    const id = $('input[name=chr-id]')
+    const name = $('input[name=name]').val()
+    const occupation = $('input[name=occupation]').val()
+    const weapon = $('input[name=weapon]').val()
+    let debt;
 
-  $('#delete-one').on('click', (e) => {
+    if ($("input[name=debt]").is(":checked")) {
+      debt = true;
+    } else {
+      debt = false;
+    }
+    
+    charactersAPI.updateOneRegister(id, name, occupation, debt, weapon)
+    .then(response => {
+      $("#add-btn").css("background-color", "green");
+    })
+    .catch(err => {
+      $("#add-btn").css("background-color", "red");
+    })
 
-  })
+  });
 
-  $('#edit-character-form').on('submit', (e) => {
+  $("#new-character-form").on("submit", (e) => {
+    event.preventDefault()
+    const name = $("#addName").val();
+    const occupation = $("#addOcc").val();
+    const weapon = $("#addWeap").val();
+    let debt;
+    if ($("#addDebt").is(":checked")) {
+      debt = true;
+    } else {
+      debt = false;
+    }
 
-  })
+    charactersAPI
+      .createOneRegister(name, occupation, debt, weapon)
+      .then(response => {
+        $("#add-btn").css("background-color", "green");
+      })
+      .catch(err => {
+        $("#add-btn").css("background-color", "red");
+      });
+  });
+});
 
-  $('#new-character-form').on('submit', (e) => {
+function appendChar(char) {
+  const newCharacterHtml = `<div class= "character-info">
+        <div class= "name"> ${char.name}</div>
+        <div class= "occupation"> ${char.occupation}</div>
+        <div class= "debt"> ${char.debt}</div>
+        <div class= "weapon"> ${char.weapon}</div>
+      </div>`;
 
-  })
-})
+  $(".characters-container").append(newCharacterHtml);
+}
