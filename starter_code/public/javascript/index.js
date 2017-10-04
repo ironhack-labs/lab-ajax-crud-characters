600fe485cd29377b291fa620d5a4c517ae18b349
ombline <ombline90@hotmail.com>
2017-10-04 10:04:14 +0200

    iteration 1

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,29 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+function appendCharacter(character) {
+  const newCharacterHtml = `<div class="character-info">
+      <div class="name">Name: ${character.name}</div>
+      <div class="occupation">Occupation: ${character.occupation}</div>
+      <div class="weapon">Weapon: ${character.weapon}</div>
+      </div>
+`;
+  $(".characters-container").append(newCharacterHtml);
+}
 
-  })
+$(document).ready(() => {
+  $("#fetch-all").on("click", e => {
+    charactersAPI.getFullList().then(function(characters) {
+      characters.forEach(obj => {
+        appendCharacter(obj);
+      });
+    });
+  });
 
-  $('#fetch-one').on('click', (e) => {
+  $("#fetch-one").on("click", e => {});
 
-  })
+  $("#delete-one").on("click", e => {});
 
-  $('#delete-one').on('click', (e) => {
+  $("#edit-character-form").on("submit", e => {});
 
-  })
-
-  $('#edit-character-form').on('submit', (e) => {
-
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+  $("#new-character-form").on("submit", e => {});
+});
