c37c72da60fb249035ac58eb1451592f26f9d8c1
Fred Magnin <frederic.magnin@gmail.com>
2017-10-04 09:19:30 +0200

    PAR- frederic magnin

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,45 @@
 const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+$(document).ready(() => {
+    $('#fetch-all').on('click', (e) => {
+        charactersAPI.getFullList().then(characters => {
+            characters.forEach(appendCharacter)
+        })
+    });
 
-  })
+    $('#fetch-one').on('click', (e) => {
+        charactersAPI.getOneRegister($("input[name=character-id]").val())
+            .then(appendCharacter)
+    });
 
-  $('#fetch-one').on('click', (e) => {
 
-  })
 
-  $('#delete-one').on('click', (e) => {
+    $('#delete-one').on('click', (e) => {
+        charactersAPI.deleteOneRegister($("input[name=character-id-delete]").val())
+            .then(Response => {
+                $('#delete-one').css("background-color", "green")
+            })
+            .catch(error => {
+                $('#delete-one').css("background-color", "red")
+            })
 
-  })
+    });
 
-  $('#edit-character-form').on('submit', (e) => {
+    $('#edit-character-form').on('submit', (e) => {
 
-  })
+    })
 
-  $('#new-character-form').on('submit', (e) => {
+    $('#new-character-form').on('submit', (e) => {
 
-  })
+    })
 })
+
+function appendCharacter(character) {
+    const newCharacterHtml = `<div class="character-info">
+    <div class="name">${character.name}</div>
+    <div class="occupation">${character.occupation}</div>
+    <div class="debt">${character.debt}</div>
+    <div class="weapon">${character.weapon}</div>
+    </div>`;
+    $('.characters-container').append(newCharacterHtml);
+};
