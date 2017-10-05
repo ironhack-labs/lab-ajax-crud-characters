a1b453887a132e1fe0640a41f4946b6cff3b893e
Thomas <tom.beguin@gmail.com>
2017-10-05 18:26:54 +0200

    have fun

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,16 +1,37 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("https://ih-crud-api.herokuapp.com")
 
-$(document).ready( () => {
+$(document).ready(() => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList()
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    charactersAPI.getOneRegister()
   })
 
   $('#delete-one').on('click', (e) => {
+    charactersAPI.deleteOneRegister()
+  })
 
+  $('#launch-shitstorm').on('click', (e) => {
+    charactersAPI.surpise()
+  })
+
+  $('#helloYou').on('click', (e) => {
+    e.preventDefault()
+    charactersAPI.havoc = true
+    charactersAPI.populate()
+    $(this).text('Oooops something went wrong...')
+  })
+
+  $('#ohShit').on('click', (e) => {
+    $('.mad').toggleClass("woop")
+    charactersAPI.havoc = true
+  })
+  $('#boom').on('click', (e) => {
+    e.preventDefault()
+    charactersAPI.havoc = true
+    charactersAPI.populate(parseInt($('#rage').val()))
   })
 
   $('#edit-character-form').on('submit', (e) => {
@@ -21,3 +42,9 @@ $(document).ready( () => {
 
   })
 })
+
+function log(arg) {
+  console.log('LOG FUNCTION RESUTLT =')
+  console.log(arg)
+  return arg
+}
