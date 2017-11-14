040a2c90079c91f77cd56ed6229b4e858253ea85
Eloi Gil <eloigil6@gmail.com>
2017-11-14 19:44:23 +0100

    first commit

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,53 @@
+'use strict';
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList() {
+        axios
+            .get(`${this.BASE_URL}/characters`);
+    }
 
-  }
+    getOneRegister() {
 
-  getOneRegister () {
+        const charId = 3;
+        axios
+            .get(`${this.BASE_URL}/characters/${charId}`);
+    }
 
-  }
+    createOneRegister() {
+        const characterInfo = {
+            name: 'eloithegoodboy',
+            occupation: 'peacefully smashing screens right now',
+            weapon: 'kame hame'
+        };
 
-  createOneRegister () {
+        const charId = 2;
 
-  }
+        axios
+            .post(`${this.BASE_URL}/characters/${charId}`, characterInfo);
+    }
 
-  updateOneRegister () {
+    updateOneRegister() {
+        const updateInfo = {
+            name: document.getElementById('update-name-input').value,
+            occupation: document.getElementById('update-occupation-input').value,
+            weapon: document.getElementById('update-weapon-input').value
+        };
 
-  }
+        const charId = 3;
 
-  deleteOneRegister () {
+        axios
+            .patch(`${this.BASE_URL}/characters/${charId}`, updateInfo);
 
-  }
+
+    }
+
+    deleteOneRegister() {
+        const charId = 3;
+        axios
+            .delete(`${this.BASE_URL}/characters/${charId}`);
+    }
 }
