2554e7da3419fe8d864503815278fab426ed0094
Eduardo Rodés <eduardorodes@MacBook-Air-de-Eduardo.local>
2017-11-14 23:13:20 +0100

    work in progress

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,65 @@ class APIHandler {
   }
 
   getFullList () {
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters",
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log("Ops! Something went wrong.");
+      },
+    });
+  };
 
-  }
+  getOneRegister (id) {
+    $.ajax({
+      url: "http://pokeapi.co/api/v2/pokemon/" + id,
+      method: "GET",
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    });
+  };
 
-  getOneRegister () {
+  createOneRegister (data) {
+    let name = $('#name').val();
+    let occupation = $('#occupation').val();
+    let weapon = $('#weapon').val();
+    let debt = $('#debt').val();
 
-  }
+    let characterData = {name, occupation, weapon, debt};
 
-  createOneRegister () {
-
-  }
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters",
+      method: "POST",
+      data: data,
+      success: function (response) {
+        console.log(response);
+      },
+      error: function (err) {
+        console.log("Ops! Something went wrong.");
+      },
+    });
+  };
 
   updateOneRegister () {
-
-  }
+    axios.get('http://ih-crud-api.herokuapp.com/characters/:id')
+      .then((response) => {
+        console.log(response);
+        var characters = response;
+      });
+  };
 
   deleteOneRegister () {
-
-  }
-}
+    axios.get('http://ih-crud-api.herokuapp.com/characters/:id')
+      .then((response) => {
+        console.log(response);
+        var characters = response;
+      });
+  };
+};
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,23 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    charactersAPI.getOneRegister();
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister ();
+  });
 
   $('#edit-character-form').on('submit', (e) => {
-
-  })
+    charactersAPI.updateOneRegister ();
+  });
 
   $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+    charactersAPI.createOneRegister (data);
+  });
+});
