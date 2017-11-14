aca32d7449a94fa25cf22a7d796f29b22352958f
popelini <ccastillon92@gmail.com>
2017-11-14 21:40:01 +0100

    Finished

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,96 @@ class APIHandler {
   }
 
   getFullList () {
-
+    let url = this.BASE_URL + '/characters';
+    $.ajax({
+      url: url,
+      success: (charList) => {
+        console.log(charList);
+        charList.forEach( (e, i) => {
+          let newDiv;
+          if (i > 0) {
+            newDiv = $('.character-info:first').clone().appendTo('.characters-container')
+          } else {
+            newDiv = $('.character-info:first');
+              newDiv.prepend(`<div class="id">Id: ${e.id}</div>`);
+          }
+            newDiv.children('.id').text(`Id: ${e.id}`);
+            newDiv.children('.name').text(`Character Name: ${e.name}`);
+            newDiv.children('.occupation').text(`Character Ocupation: ${e.occupation}`);
+            newDiv.children('.debt').text(`Character Debt: ${e.debt}`);
+            newDiv.children('.weapon').text(`Character Name: ${e.weapon}`)
+        })
+      }
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    let url = this.BASE_URL + '/characters/' + id;
+    $.ajax({
+      url: url,
+      success: (char) => {
+        console.log(char);
+        let newDiv = $('.character-info:first').clone();
+        $('.characters-container').empty().append(newDiv);
+        newDiv.children('.name').text(`Character Name: ${char.name}`);
+        newDiv.children('.occupation').text(`Character Ocupation: ${char.occupation}`);
+        newDiv.children('.debt').text(`Character Debt: ${char.debt}`);
+        newDiv.children('.weapon').text(`Character Name: ${char.weapon}`);
+        if(newDiv.children('.id')) {
+          newDiv.children('.id').text(`Id: ${char.id}`);
+        } else {
+          newDiv.prepend(`<div class="id">Id: ${char.id}</div>`);
+        }
+      }
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (newChar) {
+    let url = this.BASE_URL + '/characters';
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: newChar,
+      success: (newChar) => {
+        console.log(newChar);
+        $('#new-character-form button').css('background-color', 'green');
+      },
+      error : () => {
+        console.log("Input fields can't be empty!")
+        $('#new-character-form button').css('background-color', 'red');
+      },
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, newChar) {
+    let url = this.BASE_URL + '/characters/' + id;
+    $.ajax({
+      type: "PATCH",
+      url: url,
+      data: newChar,
+      success: (newChar) => {
+        console.log(newChar);
+        $('#edit-character-form button').css('background-color', 'green');
+      },
+      error : () => {
+        $('#edit-character-form button').css('background-color', 'red');
+      },
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    let url = this.BASE_URL + '/characters/' + id;
+    $.ajax({
+        type: "DELETE",
+        url: url,
+        success: () => {
+          console.log("Character has been successfully deleted");
+          $('#delete-one').css('background-color', 'green');
+        },
+        error : () => {
+          console.log("Character not found");
+          $('#delete-one').css('background-color', 'red');
+        },
+      });
+    }
   }
-}
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,66 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    let id = $('#fetch-one').siblings('input').val()
+    charactersAPI.getOneRegister(id);
   })
 
   $('#delete-one').on('click', (e) => {
-
+    let id = $('#delete-one').siblings('input').val()
+    // Empty the input value after getting it.
+    $('#delete-one').siblings('input').val('');
+    charactersAPI.deleteOneRegister(id);
   })
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
+    // Get input values.
+    let id = $( "#edit-character-form div:first input" ).val();
+    let name = $( "#edit-character-form div:nth-child(2) input" ).val();
+    let occupation = $( "#edit-character-form div:nth-child(3) input" ).val();
+    let weapon = $( "#edit-character-form div:nth-child(4) input" ).val();
+    let debt = $( "#edit-character-form div:nth-child(5) input" ).is(':checked');
 
+    // Create the character object.
+    let newChar = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      debt: debt,
+    }
+    // Clean the inputs.
+    $( "#edit-character-form div:first input" ).val("");
+    $( "#edit-character-form div:nth-child(2) input" ).val("");
+    $( "#edit-character-form div:nth-child(3) input" ).val("");
+    $( "#edit-character-form div:nth-child(4) input" ).val("");
+
+    charactersAPI.updateOneRegister (id, newChar);
   })
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
+    // Get input values.
+    let name = $( "#new-character-form div:first input" ).val();
+    let occupation = $( "#new-character-form div:nth-child(2) input" ).val();
+    let weapon = $( "#new-character-form div:nth-child(3) input" ).val();
+    let debt = $( "#new-character-form div:nth-child(4) input" ).is(':checked');
 
+    // Create the character object.
+    let newChar = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      debt: debt,
+    }
+    // Clean the inputs.
+    $( "#new-character-form div:first input" ).val("");
+    $( "#new-character-form div:nth-child(2) input" ).val("");
+    $( "#new-character-form div:nth-child(3) input" ).val("");
+
+    charactersAPI.createOneRegister (newChar);
   })
 })
