f3727296ad9bd4d3a8d31420ef25566e6e9e8759
Nikolaas Verlee <nikotomad@gmail.com>
2017-11-14 18:01:07 +0100

    Fetch all + Fetch one work

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,11 +4,44 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters",
+      success: function(result){
+        for(var i = 0; i < result.length; i++){
+          console.log(result[i]);
+          $(".characters-container").append(
+            "<div class='character-info'>" +
+                "<div class='name'>Name: " + result[i].name + "</div>" +
+                "<div class='occupation'>Occupation: " + result[i].occupation + "</div>" +
+                "<div class='debt'>Debt: " + result[i].debt + "</div>" +
+                "<div class='weapon'>Weapon: " + result[i].weapon + "</div>" +
+            "</div>"
+          );
+        }
+      }
+    });
   }
 
   getOneRegister () {
-
+    var id = $('#character-id').val();
+    if(id === "") {
+      alert('Please enter an id')
+    } else {
+      $.ajax({
+        url: "http://ih-crud-api.herokuapp.com/characters/"+ id,
+        success: function(result){
+          console.log(result);
+          $(".characters-container").append(
+            "<div class='character-info'>" +
+                "<div class='name'>Name: " + result.name + "</div>" +
+                "<div class='occupation'>Occupation: " + result.occupation + "</div>" +
+                "<div class='debt'>Debt: " + result.debt + "</div>" +
+                "<div class='weapon'>Weapon: " + result.weapon + "</div>" +
+            "</div>"
+          );
+        }
+      });
+    }
   }
 
   createOneRegister () {
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,11 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    charactersAPI.getOneRegister();
   })
 
   $('#delete-one').on('click', (e) => {
