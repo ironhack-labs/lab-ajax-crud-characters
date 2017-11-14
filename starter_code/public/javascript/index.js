77fb2baae744a9974778f3a1d3ac901c57809cff
Alberto Rivera <alberto.rivera.merida@gmail.com>
2017-11-14 22:40:12 +0100

    Error en Update

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,25 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+    charactersAPI.getOneRegister();
   })
 
   $('#delete-one').on('click', (e) => {
-
+    charactersAPI.deleteOneRegister();
   })
 
+  //NO FUNCIONA!!!!-->EN PROGRESO
   $('#edit-character-form').on('submit', (e) => {
-
+    e.preventDefault();
+    charactersAPI.updateOneRegister();
   })
 
   $('#new-character-form').on('submit', (e) => {
-
+    e.preventDefault();
+    charactersAPI.createOneRegister();
   })
 })
