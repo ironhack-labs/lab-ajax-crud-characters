e4eeb9ce726f05dcb87c1ea9ea28990903c2ac6a
Asier Cisneros <ascisneros14@gmail.com>
2017-11-14 20:33:39 +0100

    fetch all and fetch one

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,10 +4,50 @@ class APIHandler {
   }
 
   getFullList () {
+  $.ajax({
+    url: "http://ih-crud-api.herokuapp.com/characters",
+    method: "GET",
+    success: function (response) {
+      $('.character-info').remove();
+      for(let i = 0; i < response.length; i++){
+        console.log(response[i]);
+        $('.characters-container').append(
+          '<div class="character-info">' +
+            '<div class="name">Name: ' + response[i].name + '</div>' +
+            '<div class="occupation">occupation: ' + response[i].occupation + '</div>' +
+            '<div class="debt">Debt: ' + response[i].debt + '</div>' +
+            '<div class="weapon">weapon: ' + response[i].weapon + '</div>' +
+          '</div>'
+        )
+      }
+    },
+    error: function (err) {
+      console.log(err);
+    },
+  })
 
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters/" + id,
+      method: "GET",
+      success: function (response) {
+        $('.character-info').remove();
+          console.log(response);
+          $('.characters-container').append(
+            '<div class="character-info">' +
+              '<div class="name">Name: ' + response.name + '</div>' +
+              '<div class="occupation">occupation: ' + response.occupation + '</div>' +
+              '<div class="debt">Debt: ' + response.debt + '</div>' +
+              '<div class="weapon">weapon: ' + response.weapon + '</div>' +
+            '</div>'
+          )
+      },
+      error: function (err) {
+        console.log(err);
+      },
+    })
 
   }
 
@@ -23,3 +63,12 @@ class APIHandler {
 
   }
 }
+
+// function removeAndDrawDivs (response){
+//   let div = document.querySelector('.character-info');
+//     $('.character-info').remove();
+//     response.forEach(e => {
+//       let clone = div.cloneNode(true)
+//       clone.querySelector('.id').innerHTML = e.id;
+//     })
+// }
