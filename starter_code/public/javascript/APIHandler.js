e14ef446d062215fbf42e97180eaeefd45ff6500
Dominik Lasch <dl@exutec.de>
2017-11-14 21:27:39 +0100

    done first iteration

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,51 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
   }
 
-  getOneRegister () {
-
+  getOneRegister(characterId) {
+    const id = characterId;
+    axios
+      .get(`${this.BASE_URL}/characters/${id}`)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    axios
+      .post(`${this.BASE_URL}/characters/`, newCharacter)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(characterInfo) {
+    axios
+      .patch(`${this.BASE_URL}/characters/${characterInfo.id}`, characterInfo)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    const id = characterId;
+    axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(response => console.log(response.data))
+      .catch(error => console.log(error));
   }
 }
+
+// function getCoinDeskInfo(startDate, endDate, currency) {
+//   axios
+//     .get(
+//       `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
+//     )
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error));
+// }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,46 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+$(document).ready(() => {
+  document.getElementById("fetch-all").addEventListener("click", e => {
+    charactersAPI.getFullList();
+  });
 
-  })
+  document.getElementById("fetch-one").addEventListener("click", e => {
+    const characterId = document.getElementById("the-character-input").value;
+    charactersAPI.getOneRegister(characterId);
+  });
 
-  $('#fetch-one').on('click', (e) => {
+  document.getElementById("delete-one").addEventListener("click", e => {
+    const deleteCharacter = document.getElementById("delete-character").value;
+    charactersAPI.deleteOneRegister(deleteCharacter);
+  });
 
-  })
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", e => {
+      e.preventDefault();
+      const form = document.getElementById("edit-character-form");
+      const characterInfo = {
+        id: form[0].value,
+        name: form[1].value,
+        occupation: form[2].value,
+        weapon: form[3].value,
+        debt: form[4].checked
+      };
+      charactersAPI.updateOneRegister(characterInfo);
+    });
 
-  $('#delete-one').on('click', (e) => {
-
-  })
-
-  $('#edit-character-form').on('submit', (e) => {
-
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", e => {
+      e.preventDefault();
+      const form = document.getElementById("new-character-form");
+      const characterInfo = {
+        name: form[0].value,
+        occupation: form[1].value,
+        weapon: form[2].value,
+        debt: form[3].checked
+      };
+      charactersAPI.createOneRegister(characterInfo);
+    });
+});
