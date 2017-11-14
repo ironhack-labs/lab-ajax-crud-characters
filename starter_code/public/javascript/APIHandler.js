f69a398be739e7205219f6ea58092e0fb2dcc3ce
pablohk <pablusmmid@gmail.com>
2017-11-14 21:25:04 +0100

    Iteration 1 finished

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,82 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.item={
+      name:"",
+      occupation:"",
+      debt:false,
+      weapon:""
+    };
   }
 
   getFullList () {
-
+      $.ajax({
+        method: 'GET',
+        url:this.BASE_URL+"/characters",
+        dataType: 'json',
+        success: (response)=>{console.log(response);},
+        error: this.handleError
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    $.ajax({
+      method: 'GET',
+      url:this.BASE_URL+"/characters/"+id,
+      dataType: 'json',
+      success: (response)=>{console.log(response);},
+      error: this.handleError
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (item) {
+    this.fillItem(item);
+    $.ajax({
+      method: 'POST',
+      url:this.BASE_URL+"/characters",
+      dataType: 'json',
+      data: this.item,
+      success: (response)=>{console.log(response);},
+      error: this.handleError
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,item) {
+    this.fillItem(item);
+    $.ajax({
+      method: 'PUT',
+      url:this.BASE_URL+"/characters/"+id,
+      dataType: 'json',
+      data: this.item,
+      success: (response)=>{console.log(response);},
+      error: this.handleError
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+      $.ajax({
+      method: 'DELETE',
+      url:this.BASE_URL+"/characters/"+id,
+      dataType: 'text',
+      success: (response)=>{console.log(response);},
+      error: this.handleError
+    });
   }
+
+
+fillItem(item){
+  this.item.name=item.name;
+  this.item.occupation=item.occupation;
+  this.item.debt=item.debt,
+  this.item.weapon=item.weapon;
+}
+
+handleError(err){
+  console.log('Error');
+  console.log(err.responseText);
+}
+
+
+
+// End CLass
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,34 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
-  })
+    e.preventDefault();
+  });
 
   $('#fetch-one').on('click', (e) => {
+    e.preventDefault();
 
-  })
+  });
 
   $('#delete-one').on('click', (e) => {
+    e.preventDefault();
 
-  })
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
+    e.preventDefault();
 
-  })
-})
+  });
+
+
+// charactersAPI.getFullList();
+// charactersAPI.getOneRegister(17);
+// charactersAPI.createOneRegister({name:'jose',occupation:'otra',debt:false,weapon:'espada'});
+// charactersAPI.updateOneRegister(28,{name:'jose',occupation:'otra',debt:false,weapon:'trabuco'});
+charactersAPI.deleteOneRegister(28);
+});
