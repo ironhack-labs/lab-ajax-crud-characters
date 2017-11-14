486923442c1222a2bf293f70015b80b8594676a9
gemaderus <hi@gemaderus.com>
2017-11-15 00:17:05 +0100

    First iteration finished. Second iteration Fetch all, Fetch one and Delete one.

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,46 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
 
-  })
+  $('#fetch-all').on('click', (e) => {
+    charactersAPI.getFullList();
+  });
 
   $('#fetch-one').on('click', (e) => {
-
-  })
+    //let inputSearchVal = $('#character-id').val();
+    charactersAPI.getOneRegister($('#character-id').val());
+  });
 
   $('#delete-one').on('click', (e) => {
-
-  })
+    charactersAPI.deleteOneRegister($('#id-delete').val());
+  });
 
   $('#edit-character-form').on('submit', (e) => {
+    e.preventDefault();
+    var values = [];
+      $('.new-character').each(function() {
+          values.push($(this).val());
+      });
 
-  })
+      function check() {
+        if($('new-debt').is(":checked")) {
+          return "true";
+        } else {
+          return "false";
+        }
+      }
+
+      let newChar = {
+        name: values[0],
+        occupation: values[1],
+        weapon: values[2],
+        debt: check(),
+      };
+
+      charactersAPI.createOneRegister(newChar);
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
