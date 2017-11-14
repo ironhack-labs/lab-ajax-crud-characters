a9025d9e9c709b6056325e41d7fd0e64cda6b99f
acampos91 <acampos91@hotmail.com>
2017-11-14 20:37:15 +0100

    fetch all and fetch-one

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,11 +4,46 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters",
+      method: "GET",
+      success: function (response) {
+      $('.character-info').remove();
+      for(let i = 0; i < response.length; i++){
+        $(".characters-container").append(
+          "<div class='character-info'>" +
+              "<div class='name'>Name: " + response[i].name + "</div>" +
+              "<div class='occupation'>Occupation: " + response[i].occupation + "</div>" +
+              "<div class='debt'>Debt: " + response[i].debt + "</div>" +
+              "<div class='weapon'>Weapon: " + response[i].weapon + "</div>" +
+          "</div>"
+      )}
+      },
+      error: function (err) {
+      console.log(err)
+      },
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters/"+id,
+      method: "GET",
+      success: function (response) {
+        $('.character-info').remove();
+        $(".characters-container").append(
+          "<div class='character-info'>" +
+              "<div class='name'>Name: " + response.name + "</div>" +
+              "<div class='occupation'>Occupation: " + response.occupation + "</div>" +
+              "<div class='debt'>Debt: " + response.debt + "</div>" +
+              "<div class='weapon'>Weapon: " + response.weapon + "</div>" +
+          "</div>"
+      )
+      },
+      error: function (err) {
+      console.log(err)
+      },
+    })
   }
 
   createOneRegister () {
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,11 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
   $('#fetch-all').on('click', (e) => {
-
+charactersAPI.getFullList ();
   })
 
   $('#fetch-one').on('click', (e) => {
-
+charactersAPI.getOneRegister ($("#fetchOne").val());
   })
 
   $('#delete-one').on('click', (e) => {
