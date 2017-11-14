c64ac2188615445a60f4f3152d14f31e0249526d
Alejandro Dietta <alexdietta@hotmail.com>
2017-11-14 23:46:22 +0100

    Refactored

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,8 +1,13 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
   const container = $('.characters-container');
-  $('#fetch-all').on('click', (e) => {
+  const editButton = $('#submit-one');
+  const newButton = $('#create-one');
+  const deleteButton = $('#delete-one');
+  const fetchButton = $('#fetch-one');
+
+   $('#fetch-all').on('click', (e) => {
     charactersAPI.getFullList().then(responses => {
       container.empty();
       responses.forEach( (e) => {
@@ -11,61 +16,89 @@ $(document).ready( () => {
     }).catch(e => console.error(e));
   });
 
-  $('#fetch-one').on('click', (e) => {
-    charactersAPI.getOneRegister($('#character-id').val()).then(responses => {
-      container.empty();
-      showCharacter(responses);
-    }).catch(e => console.error(e))
-  })
+  fetchButton.on('click', (e) => {
+    let fetchId = $('#character-id');
+    if(fetchId.val() != ""){
+      resetButtons();
+      charactersAPI.getOneRegister(fetchId.val()).then(responses => {
+        container.empty();
+        fetchId.val("");
+        showCharacter(responses);
+      }).catch(e => { console.error(e); fetchButton.addClass('no-active'); });
+    };
+  });
 
-  $('#delete-one').on('click', (e) => {
-    let id = $('#character-id-delete').val();
-    let buttonDelete = $('#delete-one');
-    charactersAPI.deleteOneRegister(id).then(responses => {
-      console.log(responses);
-      buttonDelete.addClass('active');
-    }).catch(e => { console.error(e); buttonDelete.addClass('no-active'); })
-  })
+  deleteButton.on('click', (e) => {
+    let deleteId = $('#character-id-delete');
+    resetButtons();
+    charactersAPI.deleteOneRegister(deleteId.val()).then(responses => {
+      deleteId.val("");
+      deleteButton.addClass('active');
+    }).catch(e => { console.error(e); deleteButton.addClass('no-active'); });
+  });
 
   $('#edit-character-form').on('submit', (e) => {
     e.preventDefault();
-    let editButton = $('#submit-one');
     const update = {
       id: $('#edit-id').val(),
       name: $('#edit-name').val(),
       occupation: $('#edit-occupation').val(),
-      debt: $('#edit-debt').val(),
+      debt: $('#edit-debt').prop('checked'),
       weapon: $('#edit-weapon').val()
-    }
+    };
+
+    $('#edit-id').val("");
+    $('#edit-name').val("");
+    $('#edit-occupation').val("");
+    $('#edit-debt').prop('checked', false);
+    $('#edit-weapon').val("");
+
+    resetButtons();
     charactersAPI.updateOneRegister(update).then(responses => {
-      console.log(responses);
       editButton.addClass('active');
     }).catch(e => { console.error(e); editButton.addClass('no-active');  });
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
     e.preventDefault();
-    let newButton = $('#create-one');
     const newCharacter = {
       name: $('#new-name').val(),
       occupation: $('#new-occupation').val(),
-      debt: $('#new-debt').val(),
+      debt: $('#new-debt').prop('checked'),
       weapon: $('#new-weapon').val()
-    }
+    };
+
+    $('#new-name').val("");
+    $('#new-occupation').val("");
+    $('#new-debt').prop('checked', false);
+    $('#new-weapon').val("");
+
+    resetButtons();
     charactersAPI.createOneRegister(newCharacter).then(responses => {
-      console.log(responses);
       newButton.addClass('active');
     }).catch(e => { console.error(e); newButton.addClass('no-active'); });
-  })
+  });
 
   function showCharacter(char){
     let charInfo = $('<div>');
     charInfo.addClass('character-info');
-    charInfo.append($('<div>').addClass('name').text(`Character Name: ${char.name}`));
-    charInfo.append($('<div>').addClass('occupation').text(`Character Occupation: ${char.occupation}`));
-    charInfo.append($('<div>').addClass('debt').text(`Character Debt: ${char.debt}`));
-    charInfo.append($('<div>').addClass('weapon').text(`Character Weapon: ${char.weapon}`));
+    charInfo.append($('<div>').addClass('id').html(` Id: <span>${char.id}</span>`));
+    charInfo.append($('<div>').addClass('name').html(` Name: <span>${char.name}</span>`));
+    charInfo.append($('<div>').addClass('occupation').html(` Occupation: <span>${char.occupation}</span>`));
+    charInfo.append($('<div>').addClass('debt').html(` Debt: <span>${char.debt}</span>`));
+    charInfo.append($('<div>').addClass('weapon').html(` Weapon: <span>${char.weapon}</span>`));
     container.append(charInfo);
   }
 
-})
+  function resetButtons() {
+    editButton.removeClass('no-active');
+    newButton.removeClass('no-active');
+    deleteButton.removeClass('no-active');
+    fetchButton.removeClass('no-active');
+
+    editButton.removeClass('active');
+    newButton.removeClass('active');
+    deleteButton.removeClass('active');
+  }
+
+});
