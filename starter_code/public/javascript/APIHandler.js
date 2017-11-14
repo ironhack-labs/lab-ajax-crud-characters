94ce62dc1e53450fb9e462c59ad336ca09199e55
ClaraAmeller <claraameller7@gmail.com>
2017-11-14 20:04:20 +0100

    Iteration 1, update nor working

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,41 @@
+'use strict';
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = 'https://ih-crud-api.herokuapp.com/characters';
+    }
 
-  getFullList () {
+    getFullList() {
+        axios
+            .get(this.BASE_URL)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }
 
-  }
+    getOneRegister(registerId) {
+        axios
+            .get(`${this.BASE_URL}/${registerId}`)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }
 
-  getOneRegister () {
+    createOneRegister(characterInfo) {
+        axios
+            .post(this.BASE_URL, characterInfo)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }
 
-  }
+    updateOneRegister(registerId, updateInfo) {
+        axios
+            .patch(`${this.BASE_URL}/${registerId}`, updateInfo)
+            .then(response => console.log(response.data))
+            .catch(error => console.log(error));
+    }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(registerId) {
+        axios
+            .delete(`${this.BASE_URL}/${registerId}`)
+            .catch(error => console.log(error));
+    }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,47 @@
+'use strict';
+
 const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+window.onload = function() {
+    document.getElementById('fetch-all').addEventListener('click', (e) => {
+        e.preventDefault();
+        charactersAPI.getFullList();
+    });
 
-  })
+    document.getElementById('fetch-one').addEventListener('click', (e) => {
+        e.preventDefault();
+        const registerId = document.querySelectorAll('[name=character-id]')[0].value;
+        charactersAPI.getOneRegister(registerId);
+    });
 
-  $('#fetch-one').on('click', (e) => {
+    document.getElementById('delete-one').addEventListener('click', (e) => {
+        e.preventDefault();
+        const registerId = document.querySelectorAll('[name=character-id-delete]')[0].value;
+        charactersAPI.deleteOneRegister(registerId);
+    });
 
-  })
+    document.getElementById('edit-character-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const registerId = document.querySelectorAll('[name=chr-id]')[0].value;
+        const updateInfo = {
+            name: document.querySelectorAll('[name=name]')[0].value,
+            occupation: document.querySelectorAll('[name=occupation')[0].value,
+            weapon: document.querySelectorAll('[name=weapon]')[0].value,
+            debt: document.querySelectorAll('[name=debt]')[0].value
+        };
 
-  $('#delete-one').on('click', (e) => {
+        charactersAPI.updateOneRegister(registerId, updateInfo);
+    });
 
-  })
+    document.getElementById('new-character-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const characterInfo = {
+            name: document.querySelectorAll('[name=name]')[0].value,
+            occupation: document.querySelectorAll('[name=occupation')[0].value,
+            weapon: document.querySelectorAll('[name=weapon]')[0].value,
+            debt: document.querySelectorAll('[name=debt]')[0].value
+        };
 
-  $('#edit-character-form').on('submit', (e) => {
-
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+        charactersAPI.createOneRegister(characterInfo);
+    });
+};
