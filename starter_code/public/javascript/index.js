59ac6830098ff3c15e3ade013d58a78a00dfbc44
J. Gaspar Prieto <jgprieto00@gmail.com>
2017-11-15 00:07:35 +0100

    no muestra el total en pantalla

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,35 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+$(document).ready(() => {
+  $("#fetch-all").on("click", e => {
+    charactersAPI.getFullList();
+    printTheFullList = function(response) {
+      console.log(response);
+      $(".characters-info").text(response);
+    };
+  });
 
-  })
+  $("#fetch-one").on("click", e => {
+    charactersAPI.getOneRegister($("#character-id").val());
+    bringTheCharacter = function(response) {
+      console.log(response.name);
+      $(".name").text(response.name);
+      $(".occupation").text(response.occupation);
+      $(".debt").text(response.debt);
+      $(".weapon").text(response.weapon);
+    };
+  });
 
-  $('#fetch-one').on('click', (e) => {
+  $("#delete-one").on("click", e => {
+    charactersAPI.deleteOneRegister();
+  });
 
-  })
+  $("#edit-character-form").on("submit", e => {
+    charactersAPI.updateOneRegister();
+  });
 
-  $('#delete-one').on('click', (e) => {
-
-  })
-
-  $('#edit-character-form').on('submit', (e) => {
-
-  })
-
-  $('#new-character-form').on('submit', (e) => {
-
-  })
-})
+  $("#new-character-form").on("submit", e => {
+    e.preventDefault();
+    charactersAPI.createOneRegister();
+  });
+});
