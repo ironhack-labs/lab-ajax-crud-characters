0d56125899e721bca4562f1e4b5a1220b917ca83
Camila Maya <camilamaya21@gmail.com>
2017-11-15 09:53:24 +0100

    Delete completed

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,97 +2,29 @@ const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
 
-  function getCharacters() {
-    $.ajax({
-      url: "http://ih-crud-api.herokuapp.com/characters",
-      method: "GET",
-      success: showFeedback,
-      error:   handleError
-    })
-  }
-
-  function getCharacterById(id) {
-  $.ajax({
-    url: "http://ih-crud-api.herokuapp.com/characters/" + id,
-    method: "GET",
-    success: showFeedback,
-    error:   handleError
-  })
-}
-
-
   $('#fetch-all').on('click', (e) => {
-    getCharacters();
+    charactersAPI.getFullList();
   })
 
   $('#fetch-one').on('click', (e) => {
     let findById = $('#character-id').val();
-    getCharacterById(findById)
+    charactersAPI.getOneRegister(findById)
   })
 
   $('#delete-one').on('click', (e) => {
-
+    let findById = $('#character-id-delete').val()
+    charactersAPI.deleteOneRegister(findById);
   })
 
   $('#edit-character-form').on('submit', (e) => {
-    const characterInfo = {
-        name:       $('#character-edit-name').val('#the-name-input'),
-        occupation:  $('#character-edit-occupation').val('#the-occupation-input'),
-        weapon:      $('#character-edit-weapon').val('#the-weapon-input'),
-        debt:        $('#character-edit-debt').val()
-      };
-
-      const charId = $('#character-id-edit').val();
-
-    $.ajax({
-        // Notice that we are using PATCH. You could also use PUT.
-      method: 'PATCH',
-      url: `http://ih-crud-api.herokuapp.com/characters/${charId}`,
-      data: updateInfo,
-      success: (patchResponse) => {
-        console.log('Update SUCCESS!');
-        console.log(patchResponse);
-      },
-      error: handleError
-    });
+    let findById = $('')
+    charactersAPI.updateOneRegister(findById);
   })
 
 
   $('#new-character-form').on('submit', (e) => {
     event.preventDefault();
-     console.log('form submit');
-    const characterInfo = {
-        name:       $('#the-name-input').val(),
-        occupation:  $('#the-occupation-input').val(),
-        weapon:      $('#the-weapon-input').val(),
-        debth:        $('#the-debt-input').val()
-      };
-
-
-      $.ajax({
-        method:  'POST',
-        url:     'http://ih-crud-api.herokuapp.com/characters',
-        data:    characterInfo,
-        success: showFeedback,
-        error:   handleError
-      });
+    charactersAPI.createOneRegister();
   })
 
-  function showFeedback (postResponse) {
-  console.log('post success');
-  console.log(postResponse);
-  // const newCharacterHtml = `
-  //   <li>
-  //     <h3> ${postResponse.name} </h3>
-  //     <p> Id: ${postResponse.id} </p>
-  //   </li>
-  // `;
-  //
-  // $('#characters-list').append(newCharacterHtml);
-}
-
-function handleError (err) {
-  console.log('Oh no! Error:');
-  console.log(err);
-}
 })
