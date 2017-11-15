4830195516e201dbb849f52635e3e9dad07c7aad
Juan Vallejo-Nagera <juanvallejo-nagera@Juans-MacBook-Pro.local>
2017-11-15 09:49:51 +0100

    iteration2

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,24 @@
-const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com")
+const charactersAPI = new APIHandler("http://ih-crud-api.herokuapp.com");
 
 $(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
+  $('#fetch-all').on('click', () => {
+    charactersAPI.getFullList("/characters");
+  });
 
-  })
-
-  $('#fetch-one').on('click', (e) => {
-
-  })
+  $('#fetch-one').on('click', () => {
+    let characterId = $('#id-search').val();
+    charactersAPI.getOneRegister(characterId);
+  });
 
   $('#delete-one').on('click', (e) => {
 
-  })
+  });
 
   $('#edit-character-form').on('submit', (e) => {
 
-  })
+  });
 
   $('#new-character-form').on('submit', (e) => {
 
-  })
-})
+  });
+});
