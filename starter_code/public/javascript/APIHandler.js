07a068b2c5a6d497b473d9b78d1499921fccc4ba
Virginia Álvarez <virginiaalvarez@MacBook-Pro-de-Virginia.local>
2017-11-15 09:45:37 +0100

    something done withoutresults

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    $.ajax({
+      url: "http://ih-crud-api.herokuapp.com/characters",
+      success: function(response) {
+      for (var i = 0; i < response.length; i++) {
+      consola.log(response[i]);
+      $(" .characters-container ").append(
+        " <div class = 'character-info'> " +
+        " <div class = 'name'> Name: " + response[i].name + " </ div> " +
+        " <div class = 'occupation'> Occupation: " + response[i].occupation + " </ div> " +
+        " <div class = 'debt'> Debt: " + response[i].debt + " </ div> " +
+        " <div class = 'weapon'> Weapon: " + response[i].weapon + " </ div> " +
+        " </ div> "
+  );};
+},
+  error: function (err) {
+  console.log(err);
+},
+});
   }
 
-  getOneRegister () {
 
-  }
 
-  createOneRegister () {
 
-  }
 
-  updateOneRegister () {
+getOneRegister() {
 
-  }
+}
 
-  deleteOneRegister () {
+createOneRegister() {
 
-  }
+}
+
+updateOneRegister() {
+
+}
+
+deleteOneRegister() {
+
+}
 }
