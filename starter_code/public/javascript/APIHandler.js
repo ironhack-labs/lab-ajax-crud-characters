c0bf7da951b54a6d39903700f0b168ae463f8b40
Imran Syed <syed.imran2512@gmail.com>
2017-11-29 16:38:10 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -6,19 +6,19 @@ class APIHandler {
   getFullList () {
     $.ajax(
     {
-      url: 'https://ih-api.herokuapp.com/characters/',
+      url: 'https://ih-crud-api.herokuapp.com/characters/',
       method: 'GET',
-
       success: (characterList) => {
-        characterList.forEach((oneCharacter) => {
-        $('.characters-container').append(`
-            <div class="character-info">
-                <div class="id"> ID: ${oneCharacter.id} </div>
-                <div class="name">Name: ${oneCharacter.name} </div>
-                <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
-                <div class="debt">Debt: ${oneCharacter.debt}</div>
-                <div class="weapon">Weapon: ${oneCharacter.weapon}</div>
-            </div>
+              characterList.forEach((oneCharacter) => {
+              $('.characters-container').append(`
+                  <div class="character-info">
+                      <div class="id"> ID: ${oneCharacter.id} </div>
+                      <div class="name">Name: ${oneCharacter.name} </div>
+                      <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
+                      <div class="debt">Debt: ${oneCharacter.debt}</div>
+                      <div class="weapon">Weapon: ${oneCharacter.weapon}</div>
+                  </div>
+
               `);
             });
           },
