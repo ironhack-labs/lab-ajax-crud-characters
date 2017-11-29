701481e13d9f58db1fe9a82fbe27ebc0b3e5eed7
Imran Syed <syed.imran2512@gmail.com>
2017-11-29 15:43:54 -0500

    bit done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,6 +4,30 @@ class APIHandler {
   }
 
   getFullList () {
+    $.ajax(
+    {
+      url: 'https://ih-api.herokuapp.com/characters/',
+      method: 'GET',
+
+      success: (characterList) => {
+        characterList.forEach((oneCharacter) => {
+        $('.characters-container').append(`
+            <div class="character-info">
+                <div class="id"> ID: ${oneCharacter.id} </div>
+                <div class="name">Name: ${oneCharacter.name} </div>
+                <div class="occupation">Occupation: ${oneCharacter.occupation}</div>
+                <div class="debt">Debt: ${oneCharacter.debt}</div>
+                <div class="weapon">Weapon: ${oneCharacter.weapon}</div>
+            </div>
+              `);
+            });
+          },
+
+      error: (errorInfo) => {
+        console.log(errorInfo);
+      }
+
+  });
 
   }
 
