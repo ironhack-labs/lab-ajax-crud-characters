76be4d608493e5d45c213535497e9a9863a40ed6
Alejandro Poggi <apoggi419@gmail.com>
2017-11-29 20:53:36 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,76 @@ class APIHandler {
   }
 
   getFullList () {
-
+    $.ajax({
+      method: 'GET',
+      url: `${BASE_URL}`
+    }).then(allCharacters => {
+      return allCharacters;
+    }).catch(error => {
+      return error;
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (charId) {
+    $.ajax({
+      method: 'GET',
+      url: `${BASE_URL}/${charId}`,
+      data: {
+        id: charId
+      }
+    }).then(oneCharacter => {
+      return oneCharacter;
+    }).catch(error =>{
+      return error;
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (charObj) {
+    $.ajax({
+      method: 'POST',
+      url: `${BASE_URL}`,
+      data: {
+        name: charObj.name,
+        occupation: charObj.occupation,
+        debt: charObj.debt,
+        weapon: charObj.weapon
+      }
+    }).then(postedChar => {
+      return postedChar;
+    }).catch(error => {
+      return error;
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (charId, charObj) {
+    $.ajax({
+      method: 'PUT',
+      url: `${BASE_URL}/${charId}`,
+      data: {
+        id: charId,
+        name: charObj.name,
+        occupation: charObj.occupation,
+        debt: charObj.debt,
+        weapon: charObj.weapon
+      }
+    }).then(oneCharacter => {
+      return oneCharacter;
+    }).catch(error =>{
+      return "character not found.";
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (charId) {
+    $.ajax({
+      method: 'DELETE',
+      url: `${BASE_URL}/${charId}`,
+      data: {
+        id: charId
+      }
+    }).then(oneCharacter => {
+      return "character has been successfully deleted";
+    }).catch(error =>{
+      return "character not found.";
+    });
   }
 }
