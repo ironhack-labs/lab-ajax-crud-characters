b105c767751d1329529e7f1a7d96ec37b573f729
VivianSolide <vivian.sarazin@gmail.com>
2018-01-16 22:56:57 +0100

    API done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,66 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(`http://localhost:8000/characters`)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    axios.get(`http://localhost:8000/characters/` + $('#fetchOne').val())
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    axios.post(`http://localhost:8000/characters/`, {
+      "name": $('#name').val(),
+      "occupation": $('#occupation').val(),
+      "debt": $('#debt').val(),
+      "weapon": $('#weapon').val(),
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    console.log('yolo');
+    axios.put(`http://localhost:8000/characters/` + $('#chr-id').val(), {
+      "name": $('#nameuor').val(),
+      "occupation": $('#occupationuor').val(),
+      "debt": $('#debtuor').val(),
+      "weapon": $('#weaponuor').val()
+    })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    axios.delete(`http://localhost:8000/characters/` + $('#deleteOne').val())
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,25 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    charactersAPI.getOneRegister();
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+    charactersAPI.deleteOneRegister();
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function (event) {
+    event.preventDefault();
+    charactersAPI.updateOneRegister();
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function (event) {
+    charactersAPI.createOneRegister();
+    event.preventDefault();
   }
 })
