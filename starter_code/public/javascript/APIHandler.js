306bda62454db12636a081e4eb91552921f84e79
Charlottebtmn <c.bottemanne@gmail.com>
2018-01-18 16:20:05 +0100

    Charlotte B PAR Mid Iteration 3

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,54 @@
+const starWarsPerso = {
+  name:       'Padme',
+  occupation: 'Queen',
+  weapon:     'BRAIN',
+};
+
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    axios.get("http://localhost:8000/characters")
+      .then(response => {
+          console.log(response.data);
+          $('#characters-container').empty();
+          response.data.forEach(character => {
+            $('#characters-container').empty();
+            $('#characters-container').append('<div class="character-info"><div class="name">'+character.name+'</div><div class="occupation">'+character.occupation+'</div><div class="debt">'+character.debt+'</div><div class="weapon">'+character.weapon+'</div></div>');
+          });
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister (i) {
+    axios.get("http://localhost:8000/characters/"+i)
+      .then(response => {
+          console.log(response.data);
+      });
   }
 
   createOneRegister () {
+    axios.post("http://localhost:8000/characters/", starWarsPerso)
+      .then(response => {
+          console.log(response);
+      });
 
   }
 
-  updateOneRegister () {
+  updateOneRegister (i) {
+    axios.put("http://localhost:8000/characters/"+i)
+      .then(response => {
+          console.log(response.data);
+      });
 
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (i) {
+    axios.delete("http://localhost:8000/characters/"+i)
+      .then(response => {
+          console.log(response.data);
+      });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,23 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+  $('.fetch-all').on('click', (event) => {
+    charactersAPI.getFullList();
+});
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+$('.fetch-one').on('click', (event) => {
+  charactersAPI.getOneRegister($('.character-id').val());
+});
+
+  //document.getElementById('delete-one').onclick = function(){
+
+  //}
+
+  //document.getElementById('edit-character-form').onsubmit = function(){
+
+  //}
+
+  //document.getElementById('new-character-form').onsubmit = function(){
+
+  //}
+});
