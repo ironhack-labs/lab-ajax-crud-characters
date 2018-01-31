6554724e8d42d1e5dfe817ad44b8d7272fb4f2bf
LAURA MARCOS AGUILAR <Laura@MacBook-Pro-de-LAURA.local>
2018-01-31 17:03:11 +0100

    correccion jquery y metodos de apihandler

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,21 +1,27 @@
 var charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
+$(document).ready( () => {
+function handleError(err) {
+  console.log('Oh noooo! Error');
+  console.log(err);
+}
 
-  function handleError (err) {
-    console.log('Oh noooo! Error:');
-    console.log(err);
-  }
+function showFeedback(postResponse) {
+  console.log('oh lalaaa success');
+  console.log(postResponse);
+}
 
-  function createCharacter(character){
+$('#create-button').on('click', (event) => {
+  function createCharacter(character) {
     $('.character-form').on('submit', (event) => {
       event.preventDefault();
-    
+
       const characterInfo = {
         name: $('#name').val(),
         occupation: $('#occupation').val(),
         weapon: $('#weapon').val(),
         debt: $('#debt').val()
       };
-    
+
       $.ajax({
         method: 'POST',
         url: 'https://ih-crud-api.herokuapp.com/characters',
@@ -25,38 +31,42 @@ var charactersAPI = new APIHandler("http://ih-api.herokuapp.com")
       })
     });
   }
-  
-  function updateCharacter(character){
-    $('#edit-character-form').on('submit', (event) => {
-      event.preventDefault();
-    
-      const updateInfo = {
-        id: $('#id').val(),
-        name: $('#name').val(),
-        occupation: $('#occupation').val(),
-        weapon: $('#weapon').val(),
-        debt: $('#debt').val()
-      };
-      const charId = $('#id').val();
-    
-      $.ajax({
-        method: 'PATCH',
-        url: `https://ih-crud-api.herokuapp.com/characters/${charId}`,
-        data: updateInfo,
-        success: (patchResponse) => {
-          console.log('Oh lalaaaa SUCCESS!');
-          console.log(patchResponse);
-        },
-        error: handleError
-      });
-    });
-  }
-  
-  function deleteCharacter(id){
-    $('#edit-character-form').on('submit', (event) => {
-      event.preventDefault();
-    });
-  }
+});
+  $('#update-button').on('click', (event) => {
+    function updateCharacter(character) {
+      $('#edit-character-form').on('submit', (event) => {
+        event.preventDefault();
 
+        const updateInfo = {
+          id: $('#id').val(),
+          name: $('#name').val(),
+          occupation: $('#occupation').val(),
+          weapon: $('#weapon').val(),
+          debt: $('#debt').val()
+        };
+        const charId = $('#id').val();
+
+        $.ajax({
+          method: 'PATCH',
+          url: `https://ih-crud-api.herokuapp.com/characters/${charId}`,
+          data: updateInfo,
+          success: (patchResponse) => {
+            console.log('Oh lalaaaa SUCCESS!');
+            console.log(patchResponse);
+          },
+          error: handleError
+        });
+      });
+    }
+  });
+
+  $('#delete-one').on('click', (event) => {
+    function deleteCharacter(id) {
+      $('#edit-character-form').on('submit', (event) => {
+        event.preventDefault();
+      });
+    }
+  });
+})
 
 
