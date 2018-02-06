86d38c4342c7016fa96c18a51ecc0d8f6492e090
Myriam <bourbiamyriam@gmail.com>
2018-02-06 22:54:16 +0100

    Myriam

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,67 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.characterAPI = axios.create({
+      baseURL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.characterAPI
+      .get("/characters")
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(characterId) {
+    return this.characterAPI
+      .get(`/characters/${characterId}`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    return this.characterAPI
+      .post("/characters", newCharacter)
+      .then(res => {
+        console.log("post success");
+        return res.data;
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(characterId, editCharacter) {
+    return this.characterAPI
+      .patch(`/characters/${characterId}`, editCharacter)
+      .then(res => {
+        return res.data;
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    return this.characterAPI
+      .delete(`/characters/${characterId}`)
+      .then(console.log("Character deleted!"))
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,80 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+window.onload = () => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI
+      .getFullList()
+      .then(res => {
+        console.log(res);
+        $(".characters-container").empty();
+        res.forEach(element => {
+          $(".characters-container").append(
+            `<div class="character-info">
+            <div class="Id">Id: ${element.id}</div>
+          <div class="name">Name: ${element.name}</div>
+          <div class="occupation">Occupation: ${element.occupation}</div>
+          <div class="debt">Debt: ${element.debt}</div>
+          <div class="weapon">Weapon: ${element.weapon}</div> 
+          </div>`
+          );
+        });
+      })
+      .catch();
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    const characterId = $(".charID").val();
+    console.log(characterId);
+    charactersAPI
+      .getOneRegister(characterId)
+      .then(res => {
+        $(".characters-container").empty();
+        $(".characters-container").append(
+          `<div class="character-info">
+          <div class="Id">Id: ${res.id}</div>
+        <div class="name">Name: ${res.name}</div>
+        <div class="occupation">Occupation: ${res.occupation}</div>
+        <div class="debt">Debt: ${res.debt}</div>
+        <div class="weapon">Weapon: ${res.weapon}</div> 
+        </div>`
+        );
+      })
+      .catch();
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const characterId = $(".charDelete").val();
+    console.log(characterId);
+    charactersAPI
+      .deleteOneRegister(characterId)
+      .then($("#delete-one").css("background-color", "#208c3d"))
+      .catch($("#delete-one").css("background-color", "red"));
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+    const characterId = $(".editChar").val();
+    const editCharacter = {
+      name: $(".editName").val(),
+      occupation: $(".editOccupation").val(),
+      weapon: $(".editWeapon").val(),
+      debt: $(".editDebt").val()
+    };
+    charactersAPI
+      .updateOneRegister(characterId, editCharacter)
+      .then()
+      .catch();
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {
+    const newCharacter = {
+      name: $(".newName").val(),
+      occupation: $(".newOccupation").val(),
+      weapon: $(".newWeapon").val(),
+      debt: $(".newDebt").val()
+    };
+    charactersAPI
+      .createOneRegister(newCharacter)
+      .then()
+      .catch();
+  };
+};
