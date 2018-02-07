13aa9ef4c13da75eec4767c80cec1d0f8e2122dd
Arthur M <arthur.mossa@gmail.com>
2018-02-07 13:12:26 +0100

    PAR -Arthur Mossa - Done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,67 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.instanceAxios = axios.create({
+      baseURL: baseUrl
+    });
+  }
+  getFullList() {
+    return this.instanceAxios
+      .get(`/characters`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  getFullList () {
-
+  getOneRegister(id) {
+    return this.instanceAxios
+      .get(`/characters/${id}`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => {
+        $("#fetch-one").removeClass("green");
+        $("#fetch-one").addClass("red");
+      });
   }
 
-  getOneRegister () {
-
+  createOneRegister(creatInfo) {
+    return this.instanceAxios
+      .post("/characters", creatInfo)
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => {
+        $("#create-one").removeClass("green");
+        $("#create-one").addClass("red");
+      });
   }
 
-  createOneRegister () {
-
+  updateOneRegister(id, info) {
+    return this.instanceAxios
+      .patch(`/characters/${id}`, info)
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => {
+        $("#edit-one").removeClass("green");
+        $("#edit-one").addClass("red");
+        console.log(err);
+      });
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return this.instanceAxios
+      .delete(`/characters/${id}`)
+      .then(response => {
+        $("#delete-one").removeClass("red");
+        $("#delete-one").addClass("green");
+      })
+      .catch(err => {
+        $("#delete-one").removeClass("green");
+        $("#delete-one").addClass("red");
+      });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,96 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+document.getElementById("fetch-all").onclick = function() {
+  console.log("click");
+  charactersAPI.getFullList().then(response => {
+    $("#characters-container").html("");
+    for (let i = 0; i < response.length; i++) {
+      $("#characters-container").append(
+        ` <div class="character-info">
+          <div class="name"><i><b>Name:</i></b> ${response[i].name}</div>
+          <div class="occupation"><i><b>Occupation:</i></b> ${
+            response[i].occupation
+          }</div>
+          <div class="catchPhrase"><i><b>Phrase:</i></b> ${
+            response[i].catchPhrase
+          }</div>
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+          <div class="debt"><i><b>Debt:</i></b> ${response[i].debt}</div>
+          <div class="weapon"><i><b>Weapon:</i></b> ${response[i].weapon}</div>
+          <div class="id"><i><b>Character Id:</i></b> ${response[i].id}</div>
+
+        </div>`
+      );
+    }
+  });
+};
+
+document.getElementById("fetch-one").onclick = function() {
+  const selectOneId = document.getElementById("selectOneId").value;
+  charactersAPI.getOneRegister(selectOneId).then(response => {
+    $("#characters-container").html("");
+    $("#characters-container").append(
+      ` <div class="character-info">
+          <div class="name"><i><b>Name:</i></b> ${response.name}</div>
+          <div class="occupation"><i><b>Occupation:</i></b> ${
+            response.occupation
+          }</div>
+          <div class="catchPhrase"><i><b>Phrase:</i></b> ${
+            response.catchPhrase
+          }</div>
+          <div class="debt"><i><b>Debt:</i></b> ${response.debt}</div>
+          <div class="weapon"><i><b>Weapon:</i></b> ${response.weapon}</div>
+          <div class="id"><i><b>Character Id:</i></b> ${response.id}</div>
+        </div>`
+    );
+    $("#fetch-one").removeClass("red");
+    $("#fetch-one").addClass("green");
+  });
+};
+
+document.getElementById("delete-one").onclick = function() {
+  const deleteOneId = document.getElementById("deleteOneId").value;
+  charactersAPI.deleteOneRegister(deleteOneId);
+};
+
+document.getElementById("edit-character-form").onsubmit = function() {
+  event.preventDefault();
+  const updateId = document.getElementById("modifyOneId").value;
+  const updateInfo = {
+    name: document.getElementById("update-name-input").value
+      ? document.getElementById("update-name-input").value
+      : undefined,
+    occupation: document.getElementById("update-occupation-input").value
+      ? document.getElementById("update-occupation-input").value
+      : undefined,
+    catchPhrase: document.getElementById("update-catchPhrase-input").value
+      ? document.getElementById("update-catchPhrase-input").value
+      : undefined,
+    weapon: document.getElementById("update-weapon-input").value
+      ? document.getElementById("update-weapon-input").value
+      : undefined,
+    debt:
+      document.getElementById("update-debt-input").value === "on" ? true : false
+  };
+  console.log(updateInfo);
+  charactersAPI.updateOneRegister(updateId, updateInfo).then(response => {
+    console.log(response);
+  });
+};
+
+document.getElementById("new-character-form").onsubmit = function() {
+  event.preventDefault();
+  const hasDebt =
+    document.getElementById("creat-debt-input").value === "on" ? true : false;
+  const creatInfo = {
+    name: document.getElementById("creat-name-input").value,
+    occupation: document.getElementById("creat-occupation-input").value,
+    catchPhrase: document.getElementById("creat-catchPhrase-input").value,
+    weapon: document.getElementById("creat-weapon-input").value,
+    debt: hasDebt
+  };
+  charactersAPI.createOneRegister(creatInfo).then(response => {
+    $("#create-one").removeClass("red");
+    $("#create-one").addClass("green");
+  });
+};
