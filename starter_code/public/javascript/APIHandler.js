e205071567859ade0ab146e8fcd54177c077e29b
Frieda Bulnes <friedabulnes@gmail.com>
2018-02-21 08:42:41 -0600

    not done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,56 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.characterAPI=axios.create({
+      baseUrl:this.BASE_URL
+    })
   }
 
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
-}
+      //console.log("getFullList");
+      return axios.get(this.BASE_URL)
+      .then(r=>{
+        //console.log(r);
+        //console.log(r.data);
+        return r.data;
+      });
+    }
+  
+    getOneRegister (id) {
+      //console.log("getOneRegister");
+      return axios.get(this.BASE_URL+id)
+      .then(r=>{
+        //console.log(r);
+        //console.log(r.data);
+        return r.data;
+      });
+    }
+  
+    createOneRegister () {
+      //console.log("createOneRegister");
+      return axios.post(this.BASE_URL, element)
+      .then(r=>{
+        //console.log(r);
+        //console.log(r.data);
+        //console.log(r.statusText);
+        return r.data;
+      });
+    }
+  
+    updateOneRegister () {
+      return axios.put(this.BASE_URL+id, update)
+      .then(r=>{
+       // console.log(r);
+        //console.log(r.statusText);
+        return r.data;
+      });
+    }
+  
+    deleteOneRegister (id) {
+      return axios.delete(this.BASE_URL+id)
+      .then(r=>{
+        //console.log(r);
+        //console.log(r.statusText);
+        return r.data;
+      });
+    }
+  
