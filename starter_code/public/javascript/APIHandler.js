55052827b2156ee65661c1a6894e57c21516806d
Bet Mendoza <sbm2391@hotmail.com>
2018-02-20 21:58:12 -0600

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,3 +1,5 @@
+
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
@@ -6,18 +8,42 @@ class APIHandler {
   getFullList () {
     axios.get(this.BASE_URL+"characters")
     .then(response => {
-      return response.data;
+      var arrChar = response.data;
+      var content = ''
+      arrChar.forEach(char => { 
+        console.log(char)
+      content += `
+      <div class="character-info">
+        <div class="name">Name: ${char.name}</div>
+        <div class="occupation">Occupation: ${char.occupation}</div>
+        <div class="debt">Debt: ${char.debt}</div>
+        <div class="weapon">Weapon: ${char.weapon}</div>
+        <div class="id">Id: ${char.id}</div>
+      </div>`
+      });
+      
+      $('.characters-container').append(content);
   })
   .catch(err => {
     console.error(err)
   })
  
   }
+  
 
   getOneRegister (id) {
-    axios.get(this.BASE_URL+`characters/`+id)
+    axios.get(this.BASE_URL+"characters/"+id)
     .then(response => {
-      return response.data;
+      var idChar = response.data
+      var content = `
+      <div class="character-info">
+        <div class="name">Name: ${idChar.name}</div>
+        <div class="occupation">Occupation: ${idChar.occupation}</div>
+        <div class="debt">Debt: ${idChar.debt}</div>
+        <div class="weapon">Weapon: ${idChar.weapon}</div>
+        <div class="id">Id: ${idChar.id}</div>
+      </div>`
+      $('.characters-container').append(content);
   })
   .catch(err => {
     console.error(err)
@@ -25,7 +51,6 @@ class APIHandler {
   }
 
   createOneRegister () {
-    
     const characterInfo = {
       name: $("#name").val() ,
       occupation: $("#occupation").val(),
@@ -33,8 +58,7 @@ class APIHandler {
       debt: $("#debt").val() 
     };
 
-    axios.post(this.BASE_URL+`characters/`, characterInfo)
-    axios.post
+    axios.post(charactersAPI.BASE_URL+`characters/`, characterInfo)
     .then(response => {
         console.log('post success');
         console.log(response)
@@ -43,14 +67,37 @@ class APIHandler {
         console.log('Oh No! Error!');  
         console.log(error)
     })
+    
 
   }
 
   updateOneRegister () {
-
+    const updateInfo = {
+      id: $("#chr-id").val(),
+      name: $("#name2").val() ,
+      occupation: $("#occupation2").val(),
+      weapon: $("#weapon2").val(),
+      debt: $("#debt2").val()
+    };
+    const id = $("#chr-id").val()
+  
+    axios.patch(this.BASE_URL+"characters/"+id, updateInfo)
+    .then(response => {
+      console.log("Update SUCCESS!")
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL+"characters/"+id)
+    .then(response => {
+      console.log('delete success');
+    })
+    .catch(error => {
+        console.log('Oh No! Error!');  
+        console.log(error);
+    })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,45 +1,29 @@
 const charactersAPI = new APIHandler("http://localhost:8000/")
 
           
-  
-
 $(document).ready( () => {
+
   document.getElementById('fetch-all').onclick = function(){
-    
-   charactersAPI.getFullList
-   console.log(charactersAPI.getFullList)
+   charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    var id = $("#character-id").val()
-    console.log(id)
+    var id = $("#character-id").val();
+    charactersAPI.getOneRegister(id);
+   
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    var id = $("#character-id-delete").val();
+    console.log(id)
+    charactersAPI.deleteOneRegister(id)
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    charactersAPI.updateOneRegister();      
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-    const characterInfo = {
-      name: $("#name").val() ,
-      occupation: $("#occupation").val(),
-      weapon: $("#weapon").val(),
-      debt: $("#debt").val() 
-    };
-
-    axios.post(charactersAPI.BASE_URL+`characters/`, characterInfo)
-    axios.post
-    .then(response => {
-        console.log('post success');
-        console.log(response)
-    })
-    .catch(error => {
-        console.log('Oh No! Error!');  
-        console.log(error)
-    })
+    charactersAPI.createOneRegister();
   }
 })
