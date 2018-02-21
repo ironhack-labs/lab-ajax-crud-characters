f2e4efaa08bd3a090431757ab7edf2a220a821e0
stephjackson <steph.garrett.jackson@gmail.com>
2018-02-21 16:31:47 -0500

    Iteration 2

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL + "/characters")
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(characterInfo) {
+    axios
+      .post(this.BASE_URL + "/characters/", characterInfo)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, characterInfo) {
+    axios
+      .patch(this.BASE_URL + "/characters/" + id, characterInfo)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios
+      .delete(this.BASE_URL + "/characters/" + id)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
   }
 }
