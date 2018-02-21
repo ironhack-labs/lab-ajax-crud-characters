268b212e1daeaba03b974b54c1d7e1a18804115f
Bet Mendoza <sbm2391@hotmail.com>
2018-02-20 20:10:37 -0600

    functions

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,14 +4,45 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(this.BASE_URL+"characters")
+    .then(response => {
+      return response.data;
+  })
+  .catch(err => {
+    console.error(err)
+  })
+ 
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(this.BASE_URL+`characters/`+id)
+    .then(response => {
+      return response.data;
+  })
+  .catch(err => {
+    console.error(err)
+  })
   }
 
   createOneRegister () {
+    
+    const characterInfo = {
+      name: $("#name").val() ,
+      occupation: $("#occupation").val(),
+      weapon: $("#weapon").val(),
+      debt: $("#debt").val() 
+    };
+
+    axios.post(this.BASE_URL+`characters/`, characterInfo)
+    axios.post
+    .then(response => {
+        console.log('post success');
+        console.log(response)
+    })
+    .catch(error => {
+        console.log('Oh No! Error!');  
+        console.log(error)
+    })
 
   }
 
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,12 +1,18 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/")
+
+          
+  
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    
+   charactersAPI.getFullList
+   console.log(charactersAPI.getFullList)
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    var id = $("#character-id").val()
+    console.log(id)
   }
   
   document.getElementById('delete-one').onclick = function(){
@@ -18,6 +24,22 @@ $(document).ready( () => {
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    const characterInfo = {
+      name: $("#name").val() ,
+      occupation: $("#occupation").val(),
+      weapon: $("#weapon").val(),
+      debt: $("#debt").val() 
+    };
+
+    axios.post(charactersAPI.BASE_URL+`characters/`, characterInfo)
+    axios.post
+    .then(response => {
+        console.log('post success');
+        console.log(response)
+    })
+    .catch(error => {
+        console.log('Oh No! Error!');  
+        console.log(error)
+    })
   }
 })
