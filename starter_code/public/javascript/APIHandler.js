45a2502016ac3dbb586e66035bfb70a3732a1f6c
Enrique Garcia <rickyg218@gmail.com>
2018-02-21 09:12:39 -0600

    i made it through 3 iterations

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,15 +4,50 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(this.BASE_URL + "/characters")
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err)
+    })
   }
 
   getOneRegister () {
-
+    const id = document.getElementById("charatid").value;
+    axios.get(this.BASE_URL + `characters/${id}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err)
+    })
   }
-
-  createOneRegister () {
-
+ 
+  createOneRegister (e) {
+    const characterInfo = {
+        name: e.target.name.value,
+        occupation: e.target.occupation.value,
+        weapon: e.target.weapon.value,
+        debt: e.target.debt.value
+      };
+      console.log(e.target);
+    
+      axios.post(this.BASE_URL+"/characters", characterInfo)
+        .then(response => {
+          console.log(response.data)
+            // const newCharacterHtml = `
+            // <li>
+            //   <h3> ${response.data.name} </h3>
+            //   <p> Id: ${response.data.id} </p>
+            // </li>
+            // `;
+            //document.getElementById("character-info").innerHTML += newCharacterHtml;
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    
   }
 
   updateOneRegister () {
@@ -20,6 +55,14 @@ class APIHandler {
   }
 
   deleteOneRegister () {
-
+ const id = document.getElementById("delete").value;
+    axios.delete(this.BASE_URL + `characters/${id}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.error(err)
+    })
   }
-}
+  }
+
