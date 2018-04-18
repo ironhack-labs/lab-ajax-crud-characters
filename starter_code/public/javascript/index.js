16e56c511d8d390a1e7d34632e1a52648263afa6
thebinaryfelix <mateus.cfoliveira@gmail.com>
2018-04-18 21:53:04 +0200

    Almost done

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,55 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:3000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    console.log(charactersAPI.getFullList());
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
+  document.getElementById("fetch-one").onclick = function() {
+    const id = document.getElementById("character-id").value;
+    console.log(charactersAPI.getOneRegister(id));
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const id = document.getElementById("character-id-delete").value;
+    charactersAPI.deleteOneRegister(id);
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {
+    let cartoon = "";
+    if (document.getElementById("new-char-cartoon") == "on") {
+      cartoon = true;
+    } else {
+      cartoon = false;
+    }
     
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+    const newChar = {
+      name: document.getElementById("new-char-name").value,
+      occupation: document.getElementById("new-char-occ").value,
+      weapon: document.getElementById("new-char-weapon").value,
+      cartoon: cartoon
+    };
+
+    charactersAPI.createOneRegister(newChar);
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+    const id = document.getElementById("edit-char-id").value;
+
+    let cartoon = "";
+    if (document.getElementById("edit-char-cartoon") == "on") {
+      cartoon = true;
+    } else {
+      cartoon = false;
+    }
+
+    const editChar = {
+      name: document.getElementById("edit-char-name").value,
+      occupation: document.getElementById("edit-char-occ").value,
+      weapon: document.getElementById("edit-char-weapon").value,
+      cartoon: cartoon
+    };
+
+    charactersAPI.updateOneRegister(id, editChar);
+  };
+});
