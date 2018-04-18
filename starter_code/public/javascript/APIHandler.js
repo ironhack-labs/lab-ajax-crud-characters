4fd57c1d9e0d9bdc5d4f6bc2dc0372994ba8ab48
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-04-18 19:47:56 +0200

    first iteration

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,53 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(`${APIHandler}/characters`)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(`${APIHandler}/characters/${id}`)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   createOneRegister () {
-
+    axios.post(`${APIHandler}/characters`,data)
+    .then(response => {
+      console.log("post success");
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,newdata) {
+    axios.patch(`${APIHandler}/characters/${id}`,newdata)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catc(err => {
+      console.log(err)
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.put(`${APIHandler}/characters/${id}`)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
