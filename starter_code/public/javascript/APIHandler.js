0258b1c23518bd0c4272bf362e6ab0ede5254e30
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-18 20:07:30 +0200

    Fetch all, fetch one and delete

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -5,39 +5,26 @@ class APIHandler {
 
   getFullList () {
     return axios.get(`${this.BASE_URL}/characters`)
-      .then( characters => {
-        console.log(characters);
-        return character;
-      } )
+      .then( characters => characters.data )
       .catch( e => console.log(e) );
   }
 
   getOneRegister (id) {
     return axios.get(`${this.BASE_URL}/characters/${id}`)
-      .then( character => {
-        console.log(character);
-        return character;
-      } )
+      .then( character => character.data )
       .catch( e => console.log(e) );
   }
 
   createOneRegister (data) {
     return axios.post(`${this.BASE_URL}/characters`, data)
-      .then( character => {
-        console.log("Characted created");
-        return character;
-      })
+      .then( character => character.data )
       .catch( e => console.log(e) );
   }
 
   updateOneRegister (id, data) {
     return axios.patch(`${this.BASE_URL}/characters/${id}`, data)
-      .then( character => {
-        console.log("Character updated");
-        return character;
-      })
+      .then( character => character.data )
       .catch( e => console.log(`Character not found: ${e}`))
-
   }
 
   deleteOneRegister (id) {
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,21 +1,68 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  const reset = () => {
+    $(".character-info").empty();
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList().then(data => {
+      reset();
+      data.forEach(e => {
+        let container = $("<div class='character-info'>");
+        let name = `<div class="name">Character Name: ${e.name}</div>`;
+        let occupation = `<div class="occupation">Character Occupation: ${
+          e.occupation
+        }</div>`;
+        let cartoon = `<div class="cartoon">Is a Cartoon? ${e.cartoon}</div>`;
+        let weapon = `<div class="weapon">Character Weapon: ${e.weapon}</div>`;
+        container.append(name);
+        container.append(occupation);
+        container.append(cartoon);
+        container.append(weapon);
+        $(".characters-container").append(container);
+      });
+    });
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    let id = $("#character-id").val();
+
+    charactersAPI.getOneRegister(id)
+    .then(data => {
+      reset();
+      let container = $("<div class='character-info'>");
+      let name = `<div class="name">Character Name: ${data.name}</div>`;
+      let occupation = `<div class="occupation">Character Occupation: ${
+        data.occupation
+      }</div>`;
+      let cartoon = `<div class="cartoon">Is a Cartoon? ${data.cartoon}</div>`;
+      let weapon = `<div class="weapon">Character Weapon: ${data.weapon}</div>`;
+      container.append(name);
+      container.append(occupation);
+      container.append(cartoon);
+      container.append(weapon);
+      $(".characters-container").append(container);
+    });
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    let deleteButton = $("#delete-one");
+    let id = $("#character-id-delete").val();
+
+    charactersAPI.deleteOneRegister(id)
+    .then( () => {
+      reset();
+      deleteButton.css("background", "green");
+    })
+    .catch( () => {
+      deleteButton.css("background", "red");
+    })
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+    
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {};
+});
