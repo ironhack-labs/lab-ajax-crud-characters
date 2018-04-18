e2ab2d9df9e379f64e0ee0a10869c570b36f2d44
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-18 20:15:26 +0200

    Muestra todos los personajes

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,28 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList().then(data => {
+      data.forEach(e => {
+        let container = $("<div class='character-info'>");
+        let name = `<div class="name">Character Name: ${e.name}</div>`;
+        let occupation = `<div class="occupation">Character Occupation: ${e.occupation}</div>`;
+        let cartoon = `<div class="cartoon">Is a Cartoon? ${e.cartoon}</div>`;
+        let weapon = `<div class="weapon">Character Weapon: ${e.weapon}</div>`;
+        container.append(name);
+        container.append(occupation);
+        container.append(cartoon);
+        container.append(weapon);
+        $(".characters-container").append(container);
+      });
+    });
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {};
+
+  document.getElementById("delete-one").onclick = function() {};
+
+  document.getElementById("edit-character-form").onsubmit = function() {};
+
+  document.getElementById("new-character-form").onsubmit = function() {};
+});
