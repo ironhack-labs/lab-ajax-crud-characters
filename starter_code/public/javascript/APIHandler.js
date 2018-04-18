a14b0122ec10806588c2166804747a451138a940
A-Tr <alvarotrancon@gmail.com>
2018-04-18 17:10:53 +0200

    finalizado el APIHandler

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,55 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    axios
+      .get(`${this.BASE_URL}/characters/:${id}`)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    let newChar = {
+      name: String,
+      occupation: String,
+      cartoon: Boolean,
+      weapon: String
+    };
+    axios
+      .post(`${this.BASE_URL}/characters`, newChar)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    let updChar = {
+      name: String,
+      occupation: String,
+      cartoon: Boolean,
+      weapon: String
+    };
+    axios
+      .patch(`${this.BASE_URL}/characters/${id}`, updateInfo)
+      .then(response => {
+        console.log("Update SUCCESS!");
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(() => console.log("Character successfully deleted"))
+    .catch(err => console.log(err, "character not found"))
   }
 }
