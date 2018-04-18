f02f69342f6c686c1de91d001eb47d7696b3dc6a
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-18 17:28:30 +0200

    Ya trae todos los characters al index y los pinta

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+showCharacter(character) {
+  const newCharacterHtml = `
+    <div class="character-info">
+      <div class="id">id: ${character.id}</div>
+      <div class="name">Character Name: ${character.name}</div>
+      <div class="occupation">Character Occupation: ${character.occupation}</div>
+      <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+      <div class="weapon">Character Weapon: ${character.weapon}</div>
+    </div>
+    `;
+  document.getElementById("character-box").innerHTML += newCharacterHtml;
+}
+
+getFullList() {
+  axios.get(`${this.BASE_URL}/characters`)
+    .then(res => res.data)
+    .then(data => {
+      // console.log(data);
+      document.getElementById("character-box").innerHTML = '';
+      data.forEach(e => {
+        this.showCharacter(e);
+      })
+    })
+}
+
+  getOneRegister() {
 
   }
 
-  getOneRegister () {
+  createOneRegister() {
 
   }
 
-  createOneRegister () {
+  updateOneRegister() {
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
