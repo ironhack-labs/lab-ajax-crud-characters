3162d204d3254009303f384b67653ee35e508406
Juan Sanchez Mesa <sanchezmesaj@gmail.com>
2018-04-18 17:06:38 +0200

    Iteration 2 - APIHandler.js

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,45 @@ class APIHandler {
   }
 
   getFullList () {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then( characters => {
+        console.log(characters);
+        return character;
+      } )
+      .catch( e => console.log(e) );
+  }
+
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then( character => {
+        console.log(character);
+        return character;
+      } )
+      .catch( e => console.log(e) );
+  }
+
+  createOneRegister (data) {
+    return axios.post(`${this.BASE_URL}/characters`, data)
+      .then( character => {
+        console.log("Characted created");
+        return character;
+      })
+      .catch( e => console.log(e) );
+  }
+
+  updateOneRegister (id, data) {
+    return axios.patch(`${this.BASE_URL}/characters/${id}`, data)
+      .then( character => {
+        console.log("Character updated");
+        return character;
+      })
+      .catch( e => console.log(`Character not found: ${e}`))
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then( () => console.log("Character has been successfully deleted"))
+      .catch( e => console.log(`Character not found: ${e}`) );
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,11 +2,9 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
   }
   
   document.getElementById('delete-one').onclick = function(){
