05d351d6ad5addf42f112b359519ea5c53cc1cc4
Raul.dll <rdl.llave@gmail.com>
2018-04-19 00:07:28 +0200

    iteration2yfetch

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,17 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
-
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
-
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+$(document).ready(() => {
+  const charactersAPI = new APIHandler("http://localhost:8000");
+  document.getElementById("fetch-all").onclick = function() {
+    let container = document.getElementById("characters");
+    container.innerHTML = "";
+    charactersAPI.getFullList().then(data => {
+      data.forEach(e => {
+        container.innerHTML += `<div class="character-info col-sm-4">
+        <div class="name">Name: ${e.name}</div>
+        <div class="occupation">Occupation: ${e.occupation}</div>
+        <div class="cartoon">Cartoon: ${e.cartoon}</div>
+        <div class="weapon">Weapon: ${e.weapon}</div>
+      </div>`;
+      });
+    });
+  };
+});
