87ff34397896e2bd411f7bf9a1efe082fa918c0d
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-04-18 19:33:50 +0200

    ya ordenado lo del commit anterior

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,27 +4,11 @@ class APIHandler {
   }
 
 
-  showCharacter(character)  {
-    const newCharacterHtml = `
-        <div class="character-info" id="character-info">
-        <div class="id">${character.id}</div>
-        <div class="name">${character.name}</div>
-        <div class="occupation">${character.occupation}</div>
-        <div class="cartoon">${character.cartoon}</div>
-        <div class="weapon">${character.weapon}</div>
-      </div>
- `;
-    document.getElementById("character-container").innerHTML += newCharacterHtml;
-}
   getFullList () {
-    axios.get(`${this.BASE_URL}/characters`)
-        .then(res => res.data)
-        .then(data => {
-            console.log(data);
-            document.getElementById("character-container").innerHTML = '';
-            data.forEach(e => {
-                this.showCharacter(e);
-            })
+    return axios.get(`${this.BASE_URL}/characters`)
+        .then(res => {
+          console.log(res.data);
+          return res.data;
         })
   }
 
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,9 +1,36 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
+
+
+  showCharacter = function(character) {
+    const newCharacterHtml = `
+        <div class="character-info" id="character-info">
+        <div class="id">${character.id}</div>
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+      </div>
+ `;
+    document.getElementById("character-container").innerHTML += newCharacterHtml;
+};
+
   document.getElementById('fetch-all').onclick = function(){
-    charactersAPI.getFullList();
+
+    charactersAPI.getFullList()
+    .then((data)=> {
+    document.getElementById("character-container").innerHTML = '';
+    data.forEach(e => {
+        showCharacter(e);
+      })
+    })
+   
+
   }
+
+
+  
   
   document.getElementById('fetch-one').onclick = function(){
     
