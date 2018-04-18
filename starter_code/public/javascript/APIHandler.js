bbffef0a4ebb932856d0bdb5b18dab9b17230d01
Carlos Llorente <llorenteyuste@hotmail.com>
2018-04-18 18:09:46 +0200

    Hace el fecth all

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,9 +3,33 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  showCharacter (character){
+      const newCharacterHtml = `
+      <div class= "character-info>
 
+        <div class="id">id:${character.id}</div>
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+        
+      </div>
+      `;
+      document.getElementById("character-box").innerHTML += newCharacterHtml;
   }
+
+  getFullList () {
+    axios.get(`${this.BASE_URL}/characters`)
+    .then(res => res.data)
+    .then(data => {
+        console.log(data);
+        document.getElementById("character-box").innerHTML = '';
+        data.forEach(e => {
+            this.showCharacter(e);
+        })
+    })
+  }
+  
 
   getOneRegister () {
 
