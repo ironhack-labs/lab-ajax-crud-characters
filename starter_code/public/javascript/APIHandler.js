f6e71fdb57a7856043a1ff3d9055b69ef5b36274
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-18 20:12:23 +0200

    DONE

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,25 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister(newChar) {
+    return axios.post(`${this.BASE_URL}/characters`,newChar);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, update) {
+    return axios.put(`${this.BASE_URL}/characters/${id}`, update);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`);
   }
 }
