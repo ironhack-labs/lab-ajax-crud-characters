00370cf8099c876c0390417f668ab79772078099
Nacho Lopez <ignlopezsanchez@gmail.com>
2018-04-18 17:28:59 +0200

    muestra por pantalla characters pero tengo que reestructurar

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,8 +3,29 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
 
+  showCharacter(character)  {
+    const newCharacterHtml = `
+        <div class="character-info" id="character-info">
+        <div class="id">${character.id}</div>
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+      </div>
+ `;
+    document.getElementById("character-container").innerHTML += newCharacterHtml;
+}
+  getFullList () {
+    axios.get(`${this.BASE_URL}/characters`)
+        .then(res => res.data)
+        .then(data => {
+            console.log(data);
+            document.getElementById("character-container").innerHTML = '';
+            data.forEach(e => {
+                this.showCharacter(e);
+            })
+        })
   }
 
   getOneRegister () {
