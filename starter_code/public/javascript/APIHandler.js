4cf225484b3b5f0bbfb7526e813fffeaa70f6382
Borja Arana <barana1977@hotmail.com>
2018-04-19 09:25:14 +0200

    Definido los metodos de la API y recogido los elementos de la DOM

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,30 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(`http://localhost:8000/characters`)
+    .then(res => {res.data})
+    .catch(e => console.log(e));
+  }
+  getOneRegister (id) {
+    return axios.get(`http://localhost:8000/characters/${id}`)
+    .then(res =>{ res.data })
+    .catch(e => console.log(e));
+  }
+  createOneRegister (newData) {
+    return axios.post(`http://localhost:8000/characters`,{newData})
+      .then(res => { res.data })
+      .catch(e => console.log(e));
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id, update) {
+    return axios.patch(`http://localhost:8000/characters/${id}`,{update})
+      .then(res => { res.data })
+      .catch(e => console.log(e));
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`http://localhost:8000/characters/${id}`)
+    .then(res=>{console.log("funciona")})
+    
   }
 }
