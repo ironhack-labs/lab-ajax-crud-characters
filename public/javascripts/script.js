50a03bf25f8ae727b97b416012a59da9a8581ade
Tanis Pan <e.pan.gar@gmail.com>
2018-04-19 09:32:24 +0200

    i2

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+let api_url = "http://localhost:8000/characters";
 
-  console.log('IronGenerator JS imported successfully!');
+const charApi = axios({
+  method: GET,
+  url: "The url the server is going to receive.", 
+  params: "URL parameters to be sent with the request" ,
+})
 
-}, false);
+function getPokemonInfo(id) {
+pokeApi.get(id)
+.then(response => {
+  console.log(response.data)
+})
+.catch(err => {
+  console.error(err)
+})
+}
