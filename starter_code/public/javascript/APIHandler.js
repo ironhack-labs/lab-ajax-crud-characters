cc095dec84092d2d271d4ea2a0225f9c72b3d751
muflonex <muflonex@gmail.com>
2018-04-19 09:40:45 +0200

    Gracias a Pablo he conseguido entender que ambo json-server y http-server tienen que ir a la vez, porque si no, o la base servida por otro puerto me rechaza o la base servida en mismo lugar que la página no existía

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,42 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(`${this.BASE_URL}/characters`)
+          .then(response => {
+            console.log(response.data)
+            return response.data
+          })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+          .then(response => {
+            console.log(response.data)
+            return response.data
+          })
   }
 
   createOneRegister () {
-
+    axios.post(`${this.BASE_URL}/characters/${id}`)
+          .then(response => {
+            console.log("Item created")
+            return response.data
+          })
   }
 
   updateOneRegister () {
-
+    axios.patch(`${this.BASE_URL}/characters/${id}`)
+          .then(response => {
+            console.log("Item deleted")
+            return response.data
+          })
   }
 
   deleteOneRegister () {
-
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+          .then(response => {
+            console.log(response.data)
+            return response.data
+          })
   }
 }
