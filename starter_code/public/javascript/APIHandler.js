60ba74929023950febb22d6ddcd78ac070e7747e
Fernanda <mfernandasrmz8201@gmail.com>
2018-05-02 18:43:48 -0500

    Test

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,8 +3,16 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+getFullList () {
+      fetch("http://localhost:8000/characters")
+      .then(response=>{
+        if (!response.ok) return console.log(response)
+        return response.json();
+      })
+      .then(data=>{
+        console.log (data)
+      })
+      .catch(e=>console.log(e))
   }
 
   getOneRegister () {
