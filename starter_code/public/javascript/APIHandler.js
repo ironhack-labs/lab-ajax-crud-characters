7b2a5877dda7bb3d50acd6bcea4d733ee1ff7ef7
Alex <alejandrosg11@gmail.com>
2018-05-02 20:58:56 -0500

    Almost done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -2,24 +2,81 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
   getFullList () {
-
+    fetch("http://localhost:8000/characters")
+    .then(res=>{
+      if(!res.ok)return console.log(res)
+      return res.json()
+    })
+    .then(data=>{
+      console.log(data)
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    fetch("http://localhost:8000/characters")
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json()
+    })
+    .then(data=>{
+      console.log(data[id-1])
+    })
   }
 
-  createOneRegister () {
+  createOneRegister (data) {
+    fetch("http://localhost:8000/characters/", {
+      method:"POST",
+      body:JSON.stringify(data),
+      headers:{
+        "Content-Type":"application/json"
+      }
 
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
+
+    })
+    .then(data=>{
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister (luis , object) {
+    console.log(object)
+    fetch(`http://localhost:8000/characters/${luis}`,{
+      method:"PATCH",
+      body:JSON.stringify(object),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
 
+    })
+    .then(data=>{
+    })
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (luis) {
+    fetch(`http://localhost:8000/characters/${luis}`,{
+      method:"delete",
+      body:FormData,
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
+
+    })
+    .then(data=>{
+      console.log(data)
+    })
+
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,48 @@
+$( document ).ready(function() {
+  $(".inform").hide();
+});
+
+
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList();
 
+    $("#fetch-all").click(function(){
+      $(".inform").show(1000);
+  });
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.getElementsByName("character-id")[0].value;
+    charactersAPI.getOneRegister(id);
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let luis = document.getElementById("eleliminado").value;
+  charactersAPI.deleteOneRegister(luis); 
+
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    let object = {
+      name:document.getElementById("minombre2").value ,
+      occupation:document.getElementById("miocupacion2").value,
+      weapon:document.getElementById("miarma2").value,
+      cartoon:document.getElementById("caricatura2").checked
+    }
+    let luis = document.getElementById("elactualizado").value;
+    charactersAPI.updateOneRegister(luis,object); 
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    let object = {
+      name:document.getElementById("minombre").value ,
+      occupation:document.getElementById("miocupacion").value,
+      weapon:document.getElementById("miarma").value,
+      cartoon:document.getElementById("caricatura").checked
+    }
+    charactersAPI.createOneRegister(object);    
   }
 })
