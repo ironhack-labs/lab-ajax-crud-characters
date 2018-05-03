cec0ca269e12e0afdb9e1f05f4f71724c88be8a6
GermanOtniel <otnielgs94@gmail.com>
2018-05-02 20:54:01 -0500

    MEX-GERMAN-DAILY

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,83 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList(){
+    fetch("http://localhost:8000/characters")
+    .then(res=>{
+     if(!res.ok)return console.log(res)
+     return res.json()
+    })
+    .then(data=>{
+      console.log(data)
+    })
   }
 
-  getOneRegister () {
+  getOneRegister (luis) {
+    fetch(`http://localhost:8000/characters/${luis}`)
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
 
+    })
+    .then(data=>{
+    })
+    
   }
 
-  createOneRegister () {
+  createOneRegister (data) {
+    fetch("http://localhost:8000/characters/", {
+      method:"POST",
+      body:JSON.stringify(data),
+      headers:{
+        "Content-Type":"application/json"
+      }
 
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
+
+    })
+    .then(data=>{
+      console.log(data)
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister (luis , object) {
+    console.log(object)
+    fetch(`http://localhost:8000/characters/${luis}`,{
+      method:"PATCH",
+      body:JSON.stringify(object),
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
 
+    })
+    .then(data=>{
+    })
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (luis) {
+    fetch(`http://localhost:8000/characters/${luis}`,{
+      method:"delete",
+      body:FormData,
+      headers:{
+        "Content-Type":"application/json"
+      }
+    })
+    .then(res=>{
+      if(!res.ok) return console.log(res)
+      return res.json();
+
+    })
+    .then(data=>{
+      console.log(data)
+    })
+
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,39 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+  let luis = document.getElementById("characterr").value;
+  charactersAPI.getOneRegister(luis);
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let luis = document.getElementById("eleliminado").value;
+  charactersAPI.deleteOneRegister(luis); 
+
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    let object = {
+      name:document.getElementById("minombre2").value ,
+      occupation:document.getElementById("miocupacion2").value,
+      weapon:document.getElementById("miarma2").value,
+      cartoon:document.getElementById("caricatura2").checked
+    }
+    let luis = document.getElementById("elactualizado").value;
+    charactersAPI.updateOneRegister(luis,object); 
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    let object = {
+      
+      name:document.getElementById("minombre").value ,
+      occupation:document.getElementById("miocupacion").value,
+      weapon:document.getElementById("miarma").value,
+      cartoon:document.getElementById("caricatura").checked
+    }
+    charactersAPI.createOneRegister(object);    
   }
 })
