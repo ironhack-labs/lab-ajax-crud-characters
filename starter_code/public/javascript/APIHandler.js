cefcaee8ec9b38c10e78e7250604f0c1784c96a7
Lars Hansen <lars1702@gmail.com>
2018-06-21 14:07:30 +0200

    Not done! But finished with 2 iterations

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,17 +1,33 @@
-class APIHandler {
-  constructor (baseUrl) {
+/* jshint esversion: 6 */
+class APIHandler { //class
+  constructor (baseUrl) { //constructor
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList () { //This is a method. 
+    return axios.get(this.BASE_URL + "/characters")
+    .then(response => { 
+      console.log(response.data); //Why is this even an object. 
+        return response.data;
+    });
   }
 
   getOneRegister () {
-
+    let charId = document.getElementById("character-id").value;
+    return axios.get(this.BASE_URL + `/characters/${charId}`)
+    .then(response => {
+      console.log(response);
+        return response.data;
+    });
   }
 
   createOneRegister () {
+    // let newChar = {
+    //   name: $("#new-char-name").val(),occu: $("#new-char-occu").val(),
+    //   weap: $("#new-char-weap").val(),cartoon: $("#new-char-name").val(),
+    // }
+    newChar = axios.get(this.BASE_URL + `/characters/`, {name, occupation, weapon, cartoon})
+    return axios.post(this.BASE_URL + `/characters/`, {name, occupation, weapon, cartoon})
 
   }
 
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,60 @@
+/* jshint esversion: 6 */
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
 
-  }
+
+  let createElement = function (character) {
+    $(".characters-container").html("");
+    $(".characters-container").append(`
+      <div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="weapon">Is a cartoon: ${character.cartoon}</div>
+        <div class="cartoon">${character.weapon}</div>
+      </div>
+    `);
+  };
+
+
+
+
+
+  document.getElementById('fetch-all').onclick = function () {
+    console.log("fetch-all");
+    charactersAPI.getFullList().then(characters => {
+      $(".characters-container").html("");
+      for (let i = 0; i < characters.length; i++) {
+        $(".characters-container").append(`
+              <div class="character-info">
+                <div class="name">${characters[i].name}</div>
+                <div class="occupation">${characters[i].occupation}</div>
+                <div class="weapon">Is a cartoon: ${characters[i].cartoon}</div>
+                <div class="cartoon">${characters[i].weapon}</div>
+              </div>
+          `);
+      }
+    });
+  };
   
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById('fetch-one').onclick = () => {
+    console.log("fetch one");
+    charactersAPI.getOneRegister()
+    .then(character => {createElement(character); //character here is the object with the link and what not i think. We need to add the stuff from the ID-input. 
+    }); //This method takes a function that will be passed the resolved value of the Promise once it is fulfilled.
+  };
+
+  document.getElementById('delete-one').onclick = function () {
+    console.log("delete one");
+
+  };
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    console.log("edit char ")
+
+  };
+
+  document.getElementById('new-character-form').onsubmit = function () {
+    console.log("new char ")
+  };
+});
