13941bebae919d5888e2e4f4796199ff221bb595
Roberta Conrad <rjcnrd@gmail.com>
2018-06-21 10:43:53 +0200

    didnt have enough time, created the first two routes

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,74 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(this.BASE_URL + "/characters")
+    .then(response => {
+      console.log("response.data in API Handler",response.data);
+        for (let i = 0; i < response.data.length; i++) {
+          $(".characters-container").append(`
+            <div class="character-info ${response.data[i].id}">
+            <div class="name">${response.data[i].name}</div>
+            <div class="occupation">${response.data[i].occupation}</div>
+            <div class="cartoon">${response.data[i].cartoon}</div>
+            <div class="weapon">${response.data[i].weapon}</div>
+          </div>
+            `)
+        }
+      })
+     
   }
 
-  getOneRegister () {
+  getOneRegister(charId) {
+    console.log("chardID in getOneRegister",charId);
+    console.log("URL getOneRegister",this.BASE_URL + `/` + `${charId}`)
 
+    // axios.get(this.BASE_URL + `/1`)
+    axios.get(this.BASE_URL + `/characters/` + `${charId}`)
+    .then(response => {
+      console.log("getOneRegister response",response);
+      
+          $(".characters-container").append(`
+            <div class="character-info ${response.data.id}">
+            <div class="name">${response.data.name}</div>
+            <div class="occupation">${response.data.occupation}</div>
+            <div class="cartoon">${response.data.cartoon}</div>
+            <div class="weapon">${response.data.weapon}</div>
+          </div>
+            `)
+        
+      })
+    .catch(error => {
+      console.log('Oh No! Error!');  
+      console.log(error)
+  })
   }
 
-  createOneRegister () {
+  createOneRegister(charId) {
 
+    const characterInfo = {
+        name: $("#new-character-form").find('input[name="name"]').val(),
+        occupation: $("#new-character-form").find('input[name="occupation"]').val(),
+        weapon: $("#new-character-form").find('input[name="weapon"]').val(),
+        cartoon: true
+      
+    }
+    
+    axios.post(this.BASE_URL + `/characters/`,characterInfo)
+    
+    .then(response => {
+      console.log('post success');
+      console.log(response)
+    })
+    .catch(error => {
+      console.log('Oh No! Error!');  
+      console.log(error)
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister() {}
 
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,11 +1,23 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
-  }
+  document.getElementById('fetch-all').onclick = function(){
+//This should give us the full List 
+console.log("fetch all");
+charactersAPI.getFullList();
+
+}
   
   document.getElementById('fetch-one').onclick = function(){
+  //get the ID Number :) 
+  let charId = $(".operation").find('input[name="character-id"]').val();
+
+  console.log("fetch one charId",charId);
+  charactersAPI.getOneRegister(charId);
+
+    // charactersAPI.getOneRegister();
+
     
   }
   
