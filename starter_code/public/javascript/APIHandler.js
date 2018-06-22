981e86d1e0c45b475f926c3867c86eb83187fe15
steffriedrichs <steffi.friedrichs@web.de>
2018-06-22 13:20:02 +0200

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,42 @@
+// class APIHandler to deal with the Axios calls. 
+// only responsability of this class is to display the JSON result  from the API, 
+// or give the needed information to the API via a function argument.
+
+// START server: json-server --watch db.json --port 8000
+
+const charApi = axios.create({
+  baseURL: 'http://localhost:8000'
+})
+
+
+// Since Axios is Promise based, we need to add our .then() and .catch()
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
+// Get all the characters info from http://localhost:8000/characters
   getFullList () {
-
+    return charApi.get(`/characters`).then(response => response.data ).catch(err => toggleButtonColour('#fetch-all','red'));
   }
 
-  getOneRegister () {
-
+// Get a single character info from http://localhost:8000/characters/:id
+  getOneRegister (id) {
+    return charApi.get(`/characters/${id}`).then(response => response.data ).catch(err => toggleButtonColour('#fetch-one','red'));
   }
 
-  createOneRegister () {
+// Create a single character posting the data to http://localhost:8000/characters
+  createOneRegister (characterInfo) {
+    return charApi.post('/characters',characterInfo).then(response => response.data ).catch(err => toggleButtonColour('#send-data','red'));
+  } 
 
+// Edit a single character through his id in http://localhost:8000/characters/:id
+  updateOneRegister (updatedData) {
+    return charApi.put(`/characters/${updatedData.id}`,updatedData).then(response => toggleButtonColour('#send-data-update','green')).catch(err => toggleButtonColour('#send-data-update','red') );
   }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  
+// Delete a single character through his id in http://localhost:8000/characters/:id
+  deleteOneRegister (id) {
+    return charApi.delete(`/characters/${id}`,null).then(response => toggleButtonColour('#delete-one','green')).catch(err => toggleButtonColour('#delete-one','red') );
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,84 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+// display a character's information on the page:
+function createCharacter(resData){
+  let {name, occupation, weapon, cartoon} = resData;
+  $('#charactersField').append( 
+    `<div class="character-info">
+      <div class="name">${name}</div>
+      <div class="occupation">${occupation}</div>
+      <div class="cartoon">${weapon}</div>
+      <div class="weapon">${cartoon}</div>
+    </div>`
+  );
+}
 
+function toggleButtonColour(buttonId, color){
+  $(buttonId).toggleClass(color);
+  setTimeout(function(){ $(buttonId).toggleClass(color) }, 1000);
+}
+
+
+$(document).ready( () => {
+
+  document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList().then( response => {
+      toggleButtonColour('#fetch-all','green');
+      $('#charactersField').empty()
+      for(let i = 0; i < response.length ; i++){ 
+        createCharacter(response[i]) 
+      }
+    })
   }
-  
+
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister(document.getElementById('userInputId').value).then( response => {
+      $('#charactersField').empty();
+      toggleButtonColour('#fetch-one','green');
+      createCharacter(response)
+    })
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    charactersAPI.deleteOneRegister(document.getElementById('deleteID').value).then( response => {
+      $('#charactersField').empty();
+    })
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    // prevent form default behav. of redirectiong after input:
+    event.preventDefault();
+    // get user inputs to make a character:
+    const updatedData = {
+      id:         document.getElementById('newId').value,
+      name:       document.getElementById('newName').value,
+      occupation: document.getElementById('newOccupation').value,
+      weapon:     document.getElementById('newWeapon').value,
+      cartoon:    document.getElementById('newCartoon').checked
+    };
+    // add character to the database
+    charactersAPI.updateOneRegister(updatedData).then( response => {
+      $('#charactersField').empty();
+      createCharacter(updatedData);
+    })    
   }
-  
+
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    // prevent form default behav. of redirectiong after input:
+    event.preventDefault();
+    // get user input for new character:
+    const characterInfo = {
+      name:       document.getElementById('userInputNewName').value,
+      occupation: document.getElementById('userInputNewOccupation').value,
+      weapon:     document.getElementById('userInputNewWeapon').value,
+      cartoon:    document.getElementById('userInputNewIsCartoon').checked
+    };
+    // add character: 
+    charactersAPI.createOneRegister(characterInfo).then( response =>{
+      $('#charactersField').empty();
+      toggleButtonColour('#send-data','green');
+      createCharacter(characterInfo)
+      }
+    );
   }
 })
