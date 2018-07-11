ab0c6d671212397fe176c42c10565614d9b804fc
MateoReparaz <mateoreparaz1989@gmail.com>
2018-07-11 22:16:04 +0200

    Empezada iteración 3

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,26 +1,28 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
     charactersAPI.getFullList()
-    .then(data => console.log(data)
-    )}
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-    charactersAPI.getOneRegister($("#one").val())
-    .then(data => console.log(data)
-    )}
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+    .then(data => console.log(data));
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    let id = $("#character-id").val();
+    charactersAPI.getOneRegister(id)
+    .then(data => console.log(data));
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    let id = $("#character-id-delete").val();
+    charactersAPI.deleteOneRegister(id)
+    .then(data => console.log("Character deleted"))
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {
+
+  };
+});
