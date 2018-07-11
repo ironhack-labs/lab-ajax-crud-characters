d5b2ed3b9cb877b8ff6a361e937ff68094d7d37a
Sandra Najera <snajera@ucm.es>
2018-07-11 19:35:21 +0200

    Haciendo funcionar los formularios

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,27 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axios = axios;
   }
 
   getFullList () {
-
+    return axios.get(this.BASE_URL)
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/${id}`)
   }
 
   createOneRegister () {
-
+    return axios.post(`${this.BASE_URL}`)
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id) {
+    return axios.put(`${this.BASE_URL}/${id}`)
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/${id}`)
+    
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,51 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://ih-crud-api.herokuapp.com/characters")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+    .then(data => {
+      console.log(data.data);
+    })
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.querySelector("input[name='character-id']").value;
+    charactersAPI.getOneRegister(id)
+    .then(data => {
+      console.log(data)
+    })
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let id = document.querySelector("input[name='character-id-delete']").value;
+    charactersAPI.deleteOneRegister(id)
+    .then(data => {
+      console.log(data)
+    })
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    let id = document.querySelector("input[name='chr-id']").value;
+    let name = document.querySelector("input[name='name']").value;
+    let occupation = document.querySelector("input[name='occupation']").value;
+    let weapon = document.querySelector("input[name='weapon']").value;
+    let cartoon = document.querySelector("input[name='cartoon']").value;
+    charactersAPI.updateOneRegister(id)
+    .then( el => {
+      el.name = name;
+      el.occupation = occupation;
+      el.weapon = weapon;
+      el.debt = cartoon;
+    })
+
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    let name = document.querySelector("input[name='name']").value;
+    let occupation = document.querySelector("input[name='occupation']").value;
+    let weapon = document.querySelector("input[name='weapon']").value;
+    let cartoon = document.querySelector("input[name='cartoon']").value;
+    let object = {name, occupation, weapon, cartoon};
+    charactersAPI.createOneRegister(object);
   }
 })
