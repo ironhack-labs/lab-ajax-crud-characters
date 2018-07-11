158f73f47db8310542099a9dbef26837c8a37b9a
Dunnia <dunnia.alvarez@gmail.com>
2018-07-11 20:25:04 +0200

    apihandler.js

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,54 @@ class APIHandler {
   }
 
   getFullList () {
-
+      return axios.get(`${this.BASE_URL}/characters`)
+          .then(res => {
+              return res.data;
+              console.log(res)
+          })
+          .catch(error => {
+              console.log(error)
+          })
   }
 
   getOneRegister () {
+      return axios.get(`${this.BASE_URL}/characters/${id}`)
+          .then(res => {
+              return res.data;
+          })
+          .catch(error => {
+              console.log(error)
+          })
 
   }
 
   createOneRegister () {
-
+      return axios.post(`${this.BASE_URL}/characters`)
+          .then(res => {
+              return res.data;
+          })
+          .catch(error => {
+              console.log(error)
+          })
   }
 
   updateOneRegister () {
-
+      return axios.patch(`${this.BASE_URL}/characters/${id}`)
+          .then(res => {
+              return res.data;
+          })
+          .catch(error => {
+              console.log(error)
+          })
   }
 
   deleteOneRegister () {
-
+      return axios.delete(`${this.BASE_URL}/characters/${id}`)
+          .then(res => {
+              return res.data;
+          })
+          .catch(error => {
+              console.log(error)
+          })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,8 +1,16 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://ih-crud-api.herokuapp.com")
 
 $(document).ready( () => {
+  
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+      .then(res => {
+         display(res.data);  
+      
+      })
+      .catch(error=>{
+        console.log(error.message);
+      })
   }
   
   document.getElementById('fetch-one').onclick = function(){
@@ -21,3 +29,20 @@ $(document).ready( () => {
                 
   }
 })
+
+const display =characters =>{
+  const container = $(".characters-container");
+         container.html("");
+         characters.forEach((char) => {
+           const charCont = $("<div></div>").addClass("character-info");
+           const id = $("<div></div>").addClass("id").html(`ID: <span>${char.id}</span>`);
+           const name = $("<div></div>").addClass("name").html(`Name: <span>${char.name}`);
+           const occupation=$("<div></div>").addClass("occupation").html(`Occupation: <span>${char.occupation}</span>`);
+           const cartoon=$("<div></div>").addClass("cartoon").html(`Is a Cartoon?: <span>${char.cartoon}</span>`)
+           const weapon=$("<div></div>").addClass("weapon").htnml(`Weapon <span>${chan.weapon}</span>`)
+           
+           charCont.append(id,name,occupation,cartoon,weapon);
+           container.append(charCont);
+        
+  });
+}
