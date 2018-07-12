d55200665f5d1eb1e12102b45973ffe4393c6853
Beth Shook <beth.shook@gmail.com>
2018-07-11 23:35:50 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,85 @@ class APIHandler {
   }
 
   getFullList () {
+    let url = this.BASE_URL + '/characters';
+    fetch(url)
+    .then(result=>{
+        if(!result.ok) return Promise.reject(result.statusText);
+        return result.json()
+    })
+        .then(data=>{
+          console.log(data)
+        data.send();
+    })
+    .catch(e=>console.log(e))
+  }
+  
+
+  getOneRegister (id) {
+    let url = this.BASE_URL + `/characters/${id}`;
+    fetch(url)
+    .then(result=>{
+        if(!result.ok) return Promise.reject(result.statusText);
+        return result.json()
+    })
+        .then(data=>{
+          console.log(data)
+        data.send();
+    })
+    .catch(e=>console.log(e))
+  }
+
+  createOneRegister (newChar) {
+    let url = this.BASE_URL + '/characters';
+    fetch(url,{
+      method: 'POST',
+      headers: {
+          "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newChar)
+  })
+    .then(result=>{
+        if(!result.ok) return Promise.reject(result.statusText);
+        return result.json()
+    })
+        .then(data=>{
+        data.send();
+    })
+    .catch(e=>console.log(e))
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id,updatedChar) {
+    let url = this.BASE_URL + `/characters/${id}`;
+    fetch(url,{
+      method: 'PATCH',
+      headers: {
+          "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(updatedChar)
+  })
+    .then(result=>{
+        if(!result.ok) return Promise.reject(result.statusText);
+        return result.json()
+    })
+        .then(data=>{
+        data.send();
+    })
+    .catch(e=>{
+      console.log("Character not found")
+    })
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    let url = this.BASE_URL + `/characters/${id}`;
+    fetch(url,{
+      method: 'DELETE'
+  })
+    .then(result=>{
+      console.log("Character has been successfully deleted");
+    })
+    .catch(e=>{
+      console.log("Character not found")
+    })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,52 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
+  //needs to replace inner html on divs in character-info
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.getElementById('character-id').value;
+    charactersAPI.getOneRegister(id);
   }
   
-  document.getElementById('delete-one').onclick = function(){
-        
+  document.getElementById('delete-one').onclick = function(e){
+    e.preventDefault();
+    let id = document.getElementById('delete-char').value;
+    charactersAPI.deleteOneRegister(id);
+    document.getElementById('delete-one').reset();
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault();
+    let id = document.getElementById('edit-id').value;
+    let name = document.getElementById('edit-name').value;
+    let occupation = document.getElementById('edit-occ').value;
+    let weapon = document.getElementById('edit-weap').value;
+    let cartoon = document.getElementById('edit-cartoon').checked;    let updatedChar = {
+      "name" : name,
+      "occupation" : occupation,
+      "weapon" : weapon,
+      "cartoon" : cartoon
+    }  
+    charactersAPI.updateOneRegister(id,updatedChar);
+    document.getElementById("edit-character-form").reset();
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault();
+    let name = document.getElementById('newname').value;
+    let occupation = document.getElementById('newocc').value;
+    let weapon = document.getElementById('newweap').value;
+    let cartoon = document.getElementById('isCartoon').checked;
+    let newChar = {
+      "name" : name,
+      "occupation" : occupation,
+      "weapon" : weapon,
+      "cartoon" : cartoon
+    }
+    charactersAPI.createOneRegister(newChar);
+    document.getElementById("new-character-form").reset();
   }
 })
+
