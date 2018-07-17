71006833bb08469b1452abbf244238f765d2c582
Uriel Camargo Fierro <urielcamargofierro@iMac-de-Uriel.mshome.net>
2018-07-17 15:01:35 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,47 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(res => res.data);
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios
+      .get(`${this.BASE_URL}/characters/${id}`)
+      .then(res => res.data);
   }
 
-  createOneRegister () {
-
+  createOneRegister (character) {
+    return axios
+      .post(`${this.BASE_URL}/characters`, character)
+      .then(function(res){
+        console.log(res);
+      })
+      .catch(function(e){
+        console.log(e);
+      })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (character) {
+    return axios
+    .patch(`${this.BASE_URL}/character/${character.id}`, character)
+    .then(function(res){
+      console.log(res);
+    })
+    .catch(function(e){
+      console.log(e);
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios  
+      .delete(`${this.BASE_URL}/character/${id}`)
+      .then(function(res){
+        console.log(res);
+      })
+      .catch(function(e){
+        console.log(e);
+      })
   }
 }
