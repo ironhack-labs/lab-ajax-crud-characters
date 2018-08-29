9f5b52daaba82e1f5e5b0424a4e73ed93b4dfc7c
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-29 22:38:37 +0200

    implemented all functions, but there not any validation yet

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    console.log("get full list" + this.BASE_URL);
+    return axios
+      .get(`${this.BASE_URL}/characters`)
 
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    console.log("get one");
+    return axios.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister(inputs) {
+    console.log(inputs);
+    let allCharacters = {};
+    let highestId = 1;
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(result => {
+        console.log(result);
+        allCharacters = result.data.sort((a, b) => {
+          return a.id < b.id;
+        });
+        highestId = allCharacters[0].id;
+        console.log(allCharacters);
+        console.log(highestId);
+      })
+      .then(result => {
+        axios
+          .post(`${this.BASE_URL}/characters/`, {
+            id: highestId + 1,
+            name: inputs.name,
+            occupation: inputs.occupation,
+            weapon: inputs.weapon,
+            cartoon: inputs.cartoon
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    console.log("create one");
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(inputs) {
+    console.log("update one");
+    console.log(inputs);
+    axios
+      .patch(`${this.BASE_URL}/characters/${inputs["chr-id"]}`, {
+        id: inputs["chr-id"],
+        name: inputs.name,
+        occupation: inputs.occupation,
+        weapon: inputs.weapon,
+        cartoon: inputs.cartoon
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    console.log("delete one");
+    return axios.delete(`${this.BASE_URL}/characters/${id}`);
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,72 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList().then(response => {
+      $(".all-characters").text("");
+      response.data.forEach(character => {
+        $(".all-characters").append(getCard(character));
+      });
+    });
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    //TODO: validate
+    const id = $("#fetch-one")
+      .prev()
+      .val();
+
+    $(".all-characters").text("");
+
+    charactersAPI.getOneRegister(id).then(response => {
+      console.log(response);
+
+      $(".all-characters").append(getCard(response.data));
+    });
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const id = $("#delete-one")
+      .prev()
+      .val();
+
+    $(".all-characters").text("");
+    charactersAPI.deleteOneRegister(id);
+  };
+
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    e.preventDefault();
+
+    const inputs = $("#new-character-form :input");
+    let values = {};
+    $.each($("#new-character-form").serializeArray(), function(i, field) {
+      values[field.name] = field.value;
+    });
+
+    //TODO: validate form data
+
+    charactersAPI.createOneRegister(values);
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault();
+
+    const inputs = $("#edit-character-form :input");
+    let values = {};
+    $.each($("#edit-character-form").serializeArray(), function(i, field) {
+      values[field.name] = field.value;
+    });
+
+    charactersAPI.updateOneRegister(values);
+  };
+});
+
+function getCard(character) {
+  return `<div class='character-card'><p><span>Id:</span> ${
+    character.id
+  }</p><p><span>Name:</span> ${character.name}</p><p><span>Occupation:</span> ${
+    character.occupation
+  }</p><p><span>Is it a cartoon?</span> ${
+    character.cartoon
+  }</p><p><span>Weapon:</span> ${character.weapon}</p></div>`;
+}
