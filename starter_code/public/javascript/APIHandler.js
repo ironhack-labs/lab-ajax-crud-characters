2e39c6e56f02eef47fc1ebdccdb39e4a46269af0
Doug DeJong <dougdejong@gmail.com>
2018-09-11 17:07:30 -0400

    finished

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,6 +4,28 @@ class APIHandler {
   }
 
   getFullList () {
+    // const theResultDiv = document.getElementById('results')
+
+    // axios.get('https://ih-crud-api.herokuapp.com/characters')
+    // .then((response)=>{
+    //     const theArrayOfStuff = response.data;
+
+    //     theResultDiv.innerHTML = "";
+
+    //     response.data.forEach((eachThing)=>{
+    //         theResultDiv.innerHTML += `
+    //         <div class="char-component">
+    //         <h2> Name:  ${eachThing.name} </h2>
+    //         <h5> Occupation: ${eachThing.occupation} </h5>
+    //         <p> Weapon: ${eachThing.weapon} </p>
+    //         </div>
+    //         `
+    //     })
+    // })
+    // .catch((err)=>{
+    //     console.log(err)
+    // })
+
 
   }
 
