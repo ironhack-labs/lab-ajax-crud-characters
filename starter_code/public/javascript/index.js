076461d34de1fd0a5b586874828dbdb41f27e27f
Sat Gavassa <satgavassa@gmail.com>
2018-09-11 22:05:36 -0400

    everything works but no forms

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,8 +2,40 @@
 const myApiHandler = new APIHandler('https://ih-crud-api.herokuapp.com/characters')
 let charObject;
 
+$(document).ready( () => {
+  
+  document.getElementById('fetch-all').onclick = function(){
+    myApiHandler.getFullList(); // all functions must be inside promise
+   
+}
+  document.getElementById('fetch-one').onclick = function(){
+    myApiHandler.getOneRegister($('#character-id')[0].value)
+  }
+  
+  document.getElementById('delete-one').onclick = function(){
+    let inputID =$('#delete-character')[0].value
+    myApiHandler.deleteOneRegister(inputID)
+  }
+  
+  document.getElementById('update-data').onclick = function(){
+    myApiHandler.updateOneRegister($('#editId')[0].value, {
+      name: $('#editName')[0].value,
+      occupation: $('#editOccupation')[0].value,
+      weapon:$('#editWeapon')[0].value,
+    })
+  }
+  
+  document.getElementById('send-data').onclick = function(){
+    myApiHandler.createOneRegister({
+      name: $('#newName')[0].value,
+      occupation: $('#newOccupation')[0].value,
+      weapon:$('#newWeapon')[0].value,
+    });
+  }
+})
+
+
 function fetchAll(characterData) {
-  // console.log(characterData)
   const theResultDiv = document.getElementsByClassName('characters-container')[0];
   theResultDiv.innerHTML = "";
   characterData.forEach((eachThing)=>{
@@ -16,7 +48,6 @@ function fetchAll(characterData) {
     </div>`});
 }
 function printError() {
-  // console.log($('.characters-container'))
   $('.characters-container').html('<h2 style="color:red;">Sorry, no character matches this ID</h2>'); 
 }
 function fetchOne(characterData) {
@@ -31,46 +62,3 @@ function fetchOne(characterData) {
       <div class="weapon">Weapon: ${characterData.weapon}</div>
     </div>`;
 }
-
-$(document).ready( () => {
-  
-  document.getElementById('fetch-all').onclick = function(){
-    myApiHandler.getFullList(); // all functions must be inside promise
-   
-}
-  document.getElementById('fetch-one').onclick = function(){
-    let inputID =$('#character-id')[0].value
-    // console.log(inputID)
-    myApiHandler.getOneRegister(inputID)
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-    let inputID =$('#delete-character')[0].value
-    myApiHandler.deleteOneRegister(inputID)
-  }
-  
-  document.getElementById('update-data').onclick = function(){
-    let theId=$('#editId')[0].value;
-    let theName=$('#editName')[0].value;
-    let theOccupation =$('#editOccupation')[0].value;
-    let theWeapon =$('#editWeapon')[0].value
-    let charObject = {
-          name: theName,
-          occupation: theOccupation,
-          weapon:theWeapon,
-        }
-    myApiHandler.updateOneRegister(theId, charObject)
-  }
-  
-  document.getElementById('send-data').onclick = function(){
-    let theName=$('#newName')[0].value;
-    let theOccupation =$('#newOccupation')[0].value;
-    let theWeapon =$('#newWeapon')[0].value
-    let charObject = {
-      name: theName,
-      occupation: theOccupation,
-      weapon:theWeapon,
-    }
-    myApiHandler.createOneRegister(charObject);
-  }
-})
