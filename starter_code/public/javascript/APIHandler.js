44c1d93f3b5b06ee36e8947358d0d158afe0e508
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-12 10:55:52 -0400

    Finished 3 stage

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,61 @@
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
 
-  getFullList () {
+    constructor(url) {
+        this.baseAPI = url;
+    }
+   async getAllCharacters() {
+       try {
+           let characters = await axios.get(this.baseAPI + "/characters");
+           return characters;
 
-  }
+       } catch(ex) {
+           console.log(ex);
+       }
 
-  getOneRegister () {
+    }
 
-  }
+    async getCharacterInfo(id) {
+        try {
+            let character = await axios.get(thi.baseURL + `/characters/${id}`);
+            character = character.data;
+            console.log(character);
 
-  createOneRegister () {
+        } catch(ex) {
+            console.log(ex);
+        }
+    }
 
-  }
+    async createCharacter(opt) {
+        try {
+            let character = await axios.post(this.baseAPI + `/characters`, opt);
+            character = character.data;
+            console.log(character);
 
-  updateOneRegister () {
+        } catch(ex) {
+            console.log(ex);
+        }
+    }
 
-  }
+    async delCharacter(id) {
+        try {
+            let character = await axios.delete(this.baseAPI + `/characters/${id}`);
+            console.log("The character was deleted!");
 
-  deleteOneRegister () {
+        } catch(ex) {
+            console.log(ex);
+        }
+    }
 
-  }
+    async editCharacter({ id , ...opt}) {
+        try {
+            let character = await axios.put(this.baseAPI + `/characters/${id}`, opt);
+            character = character.data;
+            console.log(character);
+
+        } catch(ex) {
+            console.log(ex);
+        }
+    }
 }
+
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,7 +1,31 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+
+  document.getElementById('fetch-all').onclick = async function(){
+      let result = await charactersAPI.getAllCharacters();
+      result = result.data;
+
+      let characters = result.map( ch => {
+        return `<div class="char-info">
+                <div class="id"><span>ID:</span> <span>${ch.id}</span></div>
+                <div class="name"><span>NAME:</span> <span>${ch.name}</span></div>
+                <div class="occupation"><span>OCCUPATION:</span> <span>${ch.occupation}</span></div>
+                <div class="cartoon"><span>CARTOON:</span> <span>${ch.cartoon}</span></div>
+                <div class="weapon"><span>WEAPON:</span> <span>${ch.weapon}</span></div>
+              </div>`;
+      });
+      let div = document.createElement('div');
+      div.classList.add("wrapper");
+
+      for(let char of characters) {
+        div.innerHTML += char;
+      }
+      const $charSection = $(".characters");
+      $charSection.html(div);
+
+
+
 
   }
   
