a25108326303eb384666500575c97fb34cb20498
Celia Arnelas <Celia@simba.local>
2018-09-19 20:29:51 +0200

    primera iteracion

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -2,24 +2,40 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
   getFullList () {
+    return axios.get(`${this.BASE_URL}`)
+    .then( res => {
+      return res.data
+   }).catch(e => console.log(e));
+}
+  
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/${id}`)
+    .then( res => {
+      return res.data
+   }).catch(e => console.log(e));
+}
 
+  createOneRegister (newChar) {
+    axios.post(`${this.BASE_URL}/${id}`, newChar)
+    .then(res => {
+     return res.data;
+    }).catch(e => console.log(e));
   }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
+  
 
   updateOneRegister () {
-
+     axios.patch(`${this.BASE_URL}/${id}`)
+    .then(res => {
+      return res.data;
+    }) 
+    .catch(e => "Character not found");
   }
 
   deleteOneRegister () {
-
-  }
+    axios.delete(`${this.BASE_URL}/${id}`)
+    .then( () => {
+      return "Character has been successfully deleted"
+    })
+  .catch(e => "Character not found")}
 }
