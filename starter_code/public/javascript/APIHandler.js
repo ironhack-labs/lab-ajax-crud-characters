51b286bc31acffc12b766f2cdca9baab322585df
TeoDiaz <tdiazsan@gmail.com>
2018-09-19 20:46:40 +0200

    create, edit and delete

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios
+      .get("http://localhost:8000/characters")
+      .then(res => {
+        return res.data;
+      })
+      .catch(e => console.log(e));
   }
 
-  getOneRegister () {
-
+  getOneRegister(char) {
+    return axios
+      .get(`http://localhost:8000/characters/${char}`)
+      .then(res => {
+        console.log(res);
+        return res.data;
+      })
+      .catch(e => console.log(e));
   }
 
-  createOneRegister () {
-
+  createOneRegister(newChar) {
+    axios
+      .post("http://localhost:8000/characters", newChar)
+      .then(res => {
+        console.log("post success");
+        console.log(res);
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(edit) {
+    axios
+      .patch(`http://localhost:8000/characters/${edit.id}`, edit)
+      .then(res => {
+        console.log("patch success");
+        console.log(res);
+      })
+      .catch(error => {
+        console.log("Oh No! Error!");
+        console.log(error);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(del) {
+    axios.delete(`http://localhost:8000/characters/${del.id}`)
+    .then(res => {
+      console.log("patch success");
+      console.log(res);
+    })
+    .catch(error => {
+      console.log("Oh No! Error!");
+      console.log(error);
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList();
+    let printChar = $($(".character-info")[0]).clone();
+    $(".characters-container").append(printChar);
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function(e) {
+    let idChar = $(e.target).prev()[0].value;
+    charactersAPI.getOneRegister(idChar).then(res => {});
+  };
+
+  document.getElementById("delete-one").onclick = function(e) {
+    e.preventDefault;
+    const charDel = {
+      id: document.getElementById("delete").value
+    }
+    charactersAPI.deleteOneRegister(charDel);
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    const edit = {
+      id: document.getElementById("id").value,
+      name: document.getElementById("edit-name").value,
+      occupation: document.getElementById("edit-occupation").value,
+      weapon: document.getElementById("edit-weapon").value,
+      cartoon: document.getElementById("edit-cartoon").value
+    };
+    charactersAPI.updateOneRegister(edit);
+  };
+
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    const newChar = {
+      id: "",
+      name: document.getElementById("name").value,
+      occupation: document.getElementById("occupation").value,
+      weapon: document.getElementById("weapon").value,
+      cartoon: document.getElementById("cartoon").value
+    };
+
+    charactersAPI.createOneRegister(newChar);
+  };
+});
