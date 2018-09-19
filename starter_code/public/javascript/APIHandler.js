2a79b3af2fdda2c7d3b62a5aa717bc12770b69a5
Jacob Casado <jacob.casado.perez@gmail.com>
2018-09-19 21:42:46 +0200

    Falta Edit

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,54 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then((res) => {
+        let chrtInfo = "";
+        res.data.forEach((each) => {
+          chrtInfo += `<div class="character-info">`
+          chrtInfo += `<div class="id"><b>Id:</b> ${each.id}</div>`
+          chrtInfo += `<div class="name"><b>Name:</b> ${each.name}</div>`
+          chrtInfo += `<div class="occupation"><b>Occupation:</b> ${each.occupation}</div>`
+          chrtInfo += `<div class="cartoon"><b>Is it a cartoon?</b> ${each.cartoon}</div>`
+          chrtInfo += `<div class="weapon"><b>Weapon:</b> ${each.weapon}</div>`
+          chrtInfo += "</div>"
+        })
+        $('.characters-container').html(chrtInfo);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then (res => {
+      return res.data
+    })
+    .catch (e => console.log(e));
   }
 
-  createOneRegister () {
-
+  createOneRegister (newCharact) {
+    return axios.post(`${this.BASE_URL}/characters`, newCharact)
+    .then (res => {
+      return res.data
+    })
+    .catch (e => console.log(e));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,) {
+    return axios.patch(`${this.BASE_URL}/characters/${id}`)
+    .then (res => {
+      return res.data
+    })
+    .catch (e => "Character not found");
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then (() => {
+      return "Character has been successfully deleted"
+    })
+    .catch (() => "Character not found");
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,15 +2,26 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+    .then(res => {
+        console.log(res);
+    })
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.getElementById('fetch-one1').value
+    charactersAPI.getOneRegister(id)
+    .then(res => {
+        console.log(res);
+    })
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    let id = document.getElementById('delete-one1').value
+    charactersAPI.deleteOneRegister(id)
+    .then(res => {
+        console.log(res);
+    })
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
@@ -18,6 +29,18 @@ $(document).ready( () => {
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    let name = document.getElementById('name').value;
+    let occupation = document.getElementById('occupation').value;
+    let weapon = document.getElementById('weapon').value;
+    let cartoon = document.getElementById('cartoon').value;
+    
+    let newCharact = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon
+    };
+
+    charactersAPI.createOneRegister(newCharact)
   }
 })
