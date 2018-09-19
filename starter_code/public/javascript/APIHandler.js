25735477274e6e6f2ca711bbd33ddb002d66eb42
mfgomezb <mgomezb89@gmail.com>
2018-09-19 21:24:50 +0200

    DONE

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,49 @@ class APIHandler {
   }
 
   getFullList () {
+    return  axios.get(`${this.BASE_URL}/characters`)
 
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
 
   }
 
-  createOneRegister () {
-
+  createOneRegister (inputs) {
+    let id;
+    axios.get(`${this.BASE_URL}/characters`).then( chars => {
+      id = chars.data.length+1;
+    })
+    .then(chars => {
+      axios.post(`${this.BASE_URL}/characters`, {
+        id: id,
+        name: inputs.name,
+        occupation: inputs.occupation,
+        weapon: inputs.weapon,
+        isACartoon: inputs.isACartoon,
+      })
+    }).catch(e => {
+      console.log(e)
+    })
+    console.log('Created New Register')
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (inputs) {
+    axios.patch(`${this.BASE_URL}/characters/${inputs["chr-id"]}`, {
+      id: inputs["chr-id"],
+      name: inputs.name,
+      occupation: inputs.occupation,
+      weapon: inputs.weapon,
+      isACartoon: inputs.isACartoon,
+    }).catch(e => {
+      console.log(e)
+    })
+    console.log('Updated Register')
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
 
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,56 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
+    charactersAPI.getFullList().then( response => {
+      $(".characters-container").empty();
+      response.data.forEach(char => {
+        $(".characters-container").append(createCharCard(char));
+      });
+    })
     
   }
   
+  document.getElementById('fetch-one').onclick = function(){
+    let id = $('#fetch-one').prev().val()
+    console.log(id);
+    $(".character-info").empty();
+    charactersAPI.getOneRegister(id).then( response => {
+      $('.character-info').append(createCharCard(response.data))
+    });
+  }
+  
   document.getElementById('delete-one').onclick = function(){
-        
+    let id = $('#delete-one').prev().val()
+    charactersAPI.deleteOneRegister(id);
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault();
+    var inputs = {};
+    $("#edit-character-form").serializeArray().map(function(x){inputs[x.name] = x.value;});
+
+    if (!inputs.cartoon) { cartoon = false};
+
+    console.log(inputs)
+    charactersAPI.updateOneRegister(inputs);
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault();
+    var inputs = {};
+    $("#new-character-form").serializeArray().map(function(x){inputs[x.name] = x.value;});
+    console.log(inputs)
+    charactersAPI.createOneRegister(inputs);
+  }
+
+  let createCharCard = (char) => {
+    console.log(char)
+    return `<div class='character-info'>
+    <p>ID: ${char.id}</p>
+    <p>Name: ${char.name}</p>
+    <p>Occupation: ${char.occupation}</p>
+    <p>Is it a cartoon? ${char.cartoon}</p>
+    <p>Weapon: ${char.weapon}</p>
+    </div>`;
   }
 })
