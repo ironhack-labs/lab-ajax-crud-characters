2e6c3de774c6a81ece6ce4eb67e9b249a41a7cb7
agdwm <almudenaguerraswm@gmail.com>
2018-09-19 18:44:08 +0200

    Iteration 2 done - The APIHandler

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+	constructor(baseUrl) {
+		this.BASE_URL = baseUrl;
+		this.COLLECTION = 'characters/';
+	}
 
-  getFullList () {
+	getFullList() {
+		axios.get(`${this.BASE_URL}${this.COLLECTION}`)
+		.then(res => console.log(res.data))
+		.catch(err => console.log(err))
+	}
 
-  }
+	getOneRegister(id) {
+		axios.get(`${this.BASE_URL}${this.COLLECTION}/${id}`)
+		.then(res => console.log(res.data))
+		.catch(err => console.log(err))
+	}
 
-  getOneRegister () {
+	createOneRegister(character) {
+		axios.post(`${this.BASE_URL}${this.COLLECTION}`, character)
+		.then(res => console.log(res))
+		.catch(err => console.log(err))
+	}
 
-  }
+	updateOneRegister(id, characterToUpdate) {
+		axios.put(`${this.BASE_URL}${this.COLLECTION}/${id}`, characterToUpdate)
+		.then(res => console.log(res))
+		.catch(err => console.log(err))
+	}
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+	deleteOneRegister(id) {
+		axios.delete(`${this.BASE_URL}${this.COLLECTION}/${id}`)		
+		.then(res => console.log(res))
+		.catch(err => console.log(err))
+	}
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,40 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+	document.getElementById('fetch-all').onclick = function (e) {
+		e.preventDefault();
+		charactersAPI.getFullList();
+	}
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+	document.getElementById('fetch-one').onclick = function (e) {
+		e.preventDefault();
+		charactersAPI.getOneRegister('8');
+	}
+
+	document.getElementById('new-character-form').onsubmit = function (e) {
+		e.preventDefault();
+		const character = {
+			name: "Juana de Arco",
+			occupation: "Guerrera",
+			weapon: "Espada",
+			cartoon: false
+		}
+		charactersAPI.createOneRegister(character);
+	}
+
+	document.getElementById('edit-character-form').onsubmit = function (e) {
+		e.preventDefault();
+		const characterToUpdate = {
+			name: "Juana de Arco",
+			occupation: "Luchadora",
+			weapon: "Espada",
+			cartoon: false
+		}
+		charactersAPI.updateOneRegister(6,characterToUpdate);
+	}
+
+	document.getElementById('delete-one').onclick = function (e) {
+		e.preventDefault();
+		charactersAPI.deleteOneRegister('6');
+	}
 })
