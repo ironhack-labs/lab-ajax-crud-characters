f7802860a2b8eec1ca9d7e844d0f9e003374bfcb
Elvair <elvair@mountain>
2018-09-20 09:33:24 +0200

    Incompleto

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,15 +2,23 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+    .then(e => console.log(e.data));
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
-  }
+      const characterInfo = {
+        name: "name",
+        occupation: "occupation",
+        weapon: "weapon",
+        cartoon: "cartoon"
+      };
+      charactersAPI.getOneRegister(characterInfo);
+    };
+  })
   
   document.getElementById('delete-one').onclick = function(){
-        
+        //id.value
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
@@ -20,4 +28,4 @@ $(document).ready( () => {
   document.getElementById('new-character-form').onsubmit = function(){
                 
   }
-})
+
