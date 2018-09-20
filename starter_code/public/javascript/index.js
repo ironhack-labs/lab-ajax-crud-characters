430fdd5eaecffe8069c4d30c540c4193bc84d956
Cristopher Uriel <theledzed@gmail.com>
2018-09-19 20:52:02 -0500

    DE MEX Cris Uriel

--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,85 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+class APIHandler {
+  constructor (baseUrl) {
+    this.BASE_URL = baseUrl;
+  }
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+  getFullList() {
+    return fetch(this.BASE_URL + 'characters')
+    .then(result => result.json())
+    .then(characters => {
+      let list = ""
+      for (let i = 0; i < characters.length; i++) {
+        list+= `<div class="character-info">
+        <div class="id">ID: ${characters[i].id}</div>
+        <div class="name">Name: ${characters[i].name}</div>
+        <div class="occupation">Occupation: ${characters[i].occupation}</div>
+        <div class="cartoon">Cartoon: ${characters[i].cartoon}</div>
+        <div class="weapon">Weapon: ${characters[i].weapon}</div>
+        </div>`
+      }
+      document.getElementsByClassName("characters-container")[0].innerHTML=list;
+    })
+  }
 
+  getOneRegister(id) {
+    let url = this.BASE_URL + `characters/${id}`;
+    return fetch(url)
+    .then(result => result.json())
+    .then(char => {
+      $('.characters-container').empty();
+      $('.characters-container').append(`<div class="character-info">
+          <div class="id">ID: ${char.id}</div>
+          <div class="name">Name: ${char.name}</div>
+          <div class="occupation">Occupation: ${char.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${char.cartoon}</div>
+          <div class="weapon">Weapon: ${char.weapon}</div>
+          </div>`)
+    })
+    .catch(e => console.log(e))
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  createOneRegister(character) {
+    let url = this.BASE_URL + 'characters';
+    return fetch(url, { 
+      method: 'POST',
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(character)
+    })
+    .then(result => result.json())
+    .catch(e=>console.log(e))
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  updateOneRegister(id, update) {
+    let url = this.BASE_URL + `characters/${id}`
+    return fetch(url, { 
+      method: 'PATCH',
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(update)
+    })
+    .then(result => result.json())
+    .then(r => {
+      $('.characters-container').empty();
+      $('.characters-container').append(`<div class="character-info">
+          <div class="id">EDITED</div>
+          <div class="name">Name: ${r.name}</div>
+          <div class="occupation">Occupation: ${r.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${r.cartoon}</div>
+          <div class="weapon">Weapon: ${r.weapon}</div>
+          </div>`)
+        $('#send-data').attr('style','background:green')
+      })
+    .catch(e=> console.log(e))
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  deleteOneRegister(id) {
+    let url = this.BASE_URL + `characters/${id}`
+    return fetch(url, {
+      method: "DELETE"
+    })
+    .then(result => result)
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+}
