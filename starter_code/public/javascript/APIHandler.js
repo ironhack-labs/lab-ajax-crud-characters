4297e0d0933dd098957b74fe21ff4c51205336fe
David Sirit <davidsirit@yahoo.com>
2018-09-20 05:50:45 +0200

    iteration 2, APIHandler done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -4,22 +4,60 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(`http://localhost:8000/characters`)
+    .then(res => {
+      //console.log("get list success", res.data);
+      return res.data;
+    })
+    .catch(err => {
+      //console.log("Err");
+      console.log(err);
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`http://localhost:8000/characters/${id}`)
+    .then(res => {
+      //console.log("get one success", res.data);
+      return res.data;
+    })
+    .catch(err => {
+      //console.log("Err");
+      console.log(err);
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister (objChar) {
+    return axios.post(`http://localhost:8000/characters`, objChar)
+    .then(res => {
+      console.log("post success");
+      //return objChar;
+    })
+    .catch(err => {
+      console.log("Err");
+      console.log(err);
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, objChar) {
+    return axios.put(`http://localhost:8000/characters/${id}`, objChar)
+    .then(res => {
+      console.log("Character update success");
+    })
+    .catch(err => {
+      console.log("Character not found");
+      console.log(err);
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`http://localhost:8000/characters/${id}`)
+    .then(res => {
+      console.log(`Character ${id} has been successfully delete`);
+    })
+    .catch(err => {
+      console.log("Character not found");
+      console.log(err);
+    });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,12 +1,26 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
+  document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList().then(res => {
+      res.forEach(element => {
+        console.log(element.name);
+        console.log(element.occupation);
+        console.log(element.weapon);        
+      });
+    });
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    console.log(`hola mundo`);
+    getOneRegister ();
+    // const characterInfo = {
+    //   name: "Han Solo old",
+    //   occupation: "Smuggler",
+    //   weapon: "Blaster Pistol",
+    //   cartoon: true
+    // } ;
   }
   
   document.getElementById('delete-one').onclick = function(){
@@ -18,6 +32,15 @@ $(document).ready( () => {
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    event.preventDefault();
+    console.log(`ver esto`,document.getElementsByClassName(`character-info`)[0].name);
+
+    const characterInfo = {
+      name: document.getElementsByClassName(`character-info`)[0].getElementsByTagName('div')[0].value,
+      occupation: document.getElementsByClassName(`character-info`)[0].getElementsByTagName('div')[1].value,
+      weapon: document.getElementsByClassName(`character-info`)[0].getElementsByTagName('div')[2].value,
+      cartoon: document.getElementsByClassName(`character-info`)[0].getElementsByTagName('div')[3].value
+    }
+    //console.log('mi caractere', characterInfo);     
   }
 })
