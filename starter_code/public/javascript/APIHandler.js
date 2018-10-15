ba37eff9cc3bbaf00b04c321d73a73ae1709c395
Alberto Martinez <jose_alberto12216@hotmail.com>
2018-10-14 22:44:52 -0500

    Avances

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -3,23 +3,54 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(this.BASE_URL)
+    .then(res => {
+      console.log('data getFull ' + res.data[0].name);
+      return res.data;
+    })
+    .catch(err => {
+      console.log('Error: ' + err);
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(this.BASE_URL + id)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister(character) {
+    axios.post(this.BASE_URL,character)
+    .then(res => {
+      console.log('The character has been created');      
+    })
+    .catch(err => {
+      console.log('error ' + err);
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(character,id) {
+    axios.patch(this.BASE_URL+id, character )
+      .then(res => {
+        console.log(res.name + ' has been updated success');
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(this.BASE_URL + id )
+      .then(res => {
+        console.log("Element has been deleted success");
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/characters/");
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+  document.getElementById('fetch-all').onclick = function(){  
 
+    const characters = new Promise(function(resolve, reject) {  
+      let data = charactersAPI.getFullList();
+      if (data !== undefined ) {
+         resolve(data);  
+      }
+      else {
+         reject('Error en la consulta '+data);
+      }
+    });
+    characters.then(val => {
+      let mainDiv = document.getElementsByClassName('characters-container');
+      console.log('Entro al then '+val);
+      val.forEach(character => {
+        let div = document.createElement('div');
+        let card = '<div class="character-info">';
+        card +=  '<div class="name">Character Name</div>';
+        card +=  '<div class="occupation">Character Occupation</div>';
+        card +=  '<div class="cartoon">Is a Cartoon?</div>';
+        card +=  '<div class="weapon">Character Weapon</div>';
+        card +=  '</div>';
+        div.appendChild(card);
+        mainDiv.append(div);
+      });
+    })  
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    console.log('fetch-one');
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    console.log('delete-one');        
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    console.log('edit-character-form');            
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    console.log('new-character-form');                
   }
 })
