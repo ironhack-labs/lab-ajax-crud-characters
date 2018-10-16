d8a55825781cf9a7e0cd7a6a6cc3dce43c9e45ce
Miguel Tellez <miguel.tellez1997@gmail.com>
2018-10-16 17:44:30 -0500

    done

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,79 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`).then(res =>
+      res.data.forEach(character => {
+        createCard(
+          character.name,
+          character.occupation,
+          character.weapon,
+          character.cartoon
+        );
+      })
+    );
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`).then(res => {
+      createCard(
+        res.data.name,
+        res.data.occupation,
+        res.data.weapon,
+        res.data.cartoon
+      );
+    });
   }
 
-  createOneRegister () {
+  createOneRegister(name, occupation, weapon, cartoon) {
+    let newCharacter = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon
+    };
 
+    axios.post(`${this.BASE_URL}/characters`, newCharacter)
+      .then(() => {
+        $("#send-data").css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(err => {
+        $("#send-data").css("background-color", "red");
+        console.log(err);
+      });
   }
 
-  updateOneRegister () {
+  updateOneRegister(id, name, occupation, weapon, cartoon) {
+    let character = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      cartoon: cartoon
+    };
 
+    axios.patch(`${this.BASE_URL}/characters/${id}`, character)
+      .then(() => {
+        $("#send-data").last().css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(err => {
+        $("#send-data").last().css("background-color", "red");
+        console.log(err);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(() => {
+        $("#delete-one").css("background-color", "green");
+        this.getFullList();
+      })
+      .catch(() => {
+        $("#delete-one").css("background-color", "red");
+      });
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -2,22 +2,52 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    $(".character-info").remove();
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    $(".character-info").remove();
+    charactersAPI.getOneRegister($("input[name='character-id']").val());
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    $(".character-info").remove();
+    charactersAPI.deleteOneRegister($("input[name='character-id-delete']").val());
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    event.preventDefault();
+    $(".character-info").remove();
+
+    charactersAPI.updateOneRegister(
+      $("input[name='chr-id']").val(),
+      $("input[name='name']").last().val(),
+      $("input[name='occupation']").last().val(),
+      $("input[name='weapon']").last().val(),
+      $("input[name='cartoon']").last().prop("checked"));
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    event.preventDefault();
+    $(".character-info").remove();
+
+    charactersAPI.createOneRegister(
+      $("input[name='name']").first().val(),
+      $("input[name='occupation']").first().val(),
+      $("input[name='weapon']").first().val(),
+      $("input[name='cartoon']").first().prop("checked"));
   }
 })
+
+function createCard(name, occupation, cartoon, weapon) {
+  $(".characters-container")
+  .append(
+    `<div class="character-info">
+      <div class="name">Character Name: <span>${name}</span></div>
+      <div class="occupation">Character Occupation: <span>${occupation}</span></div>
+      <div class="cartoon">Is a Cartoon?: <span>${cartoon}</span></div>
+      <div class="weapon">Character Weapon: <span>${weapon}</span></div>
+    </div>`
+  );
+}
