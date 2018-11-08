ed360ddb42dc7e74c671f3ef62acc60a5cf51d32
Felicitas Deutges <deutges.ironhack@gmail.com>
2018-11-08 12:40:28 +0100

    Iteration 1 and 2

--- starter_code/public/javascript/APIHandler.js
+++ starter_code/public/javascript/APIHandler.js
@@ -1,25 +1,52 @@
+// const api = axios.create({
+//   baseURL: 
+// })
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(this.BASE_URL + "/characters")
+      .then(response => {
+        return response.data
+      })
+      .catch(err => console.log(err))
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(this.BASE_URL + "/characters/" + id)
+      .then(response => {
+        return response.data
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister(obj) {
+    // const info = {
+    //   name: obj.name,
+    //   occupation: obj.occupation,
+    //   cartoon: obj.cartoon,
+    //   weapon: obj.weapon
+    // };
+    return axios.post(this.BASE_URL + "/characters", obj)
+      .then(character => { return character.data })
+      .catch(err => { console.log(err) })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, obj) {
+    // axios.get(this.BASE_URL + "/characters/" + id)
+    //   .then(resp => {
+    //     if (resp.data === {}) { return "Character not found" }
+    //   })
+    return axios.patch(this.BASE_URL + "/characters/" + id, obj)
+      .then(character => { return character.data })
+      .catch(err => { return "Character not found" })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(this.BASE_URL + "/characters/" + id)
+      .then(sth => { return "Character has been successfully deleted" })
+      .catch(sth => { return "Character not found" })
   }
 }
--- starter_code/public/javascript/index.js
+++ starter_code/public/javascript/index.js
@@ -1,23 +1,26 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:7000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+      .then(characters => {
+        console.log(characters)
+      })
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
