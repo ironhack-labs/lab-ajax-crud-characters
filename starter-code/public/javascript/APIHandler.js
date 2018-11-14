6e0f1c7ecdce28a06ded6f2d602ac1ea13077d15
Ronaldo Rodrigues da Silva <neolondres@outlook.com>
2018-11-14 14:50:25 -0200

    feito ate o create new character

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,51 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get('http://localhost:8000/characters')
+    // .then(response => {
+    //   // console.log("teste");
+    //   return response;
+    // })
+    // .catch(error => {
+    //   console.log("The error is: ", error);
+    // });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`http://localhost:8000/characters/${id}`)
+    // .then(response => {
+    //   console.log(response);
+    //   return response;
+    // })
+    // .catch(error => {
+    //   console.log("The error is: ", error);
+    // });
   }
 
-  createOneRegister () {
-
+  createOneRegister (obj) {
+    console.log("xyz");
+    axios.post(`http://localhost:8000/characters`, obj)
+    .then(response => {
+      console.log(response);
+      return response;
+    })
+    .catch(error => {
+      console.log("The error is: ", error);
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (obj, id) {
+    axios.patch(`http://localhost:8000/characters/${id}`, obj)
+    .then(response => {
+      console.log(response);
+      return response;
+    })
+    .catch(error => {
+      console.log("The error is: ", error);
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`http://localhost:8000/characters/${id}`)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,15 +1,61 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
+let chars;
+const name = document.getElementsByClassName("name");
+const charsInfo = document.getElementById("character-info");
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList().then(response => {chars = response;
+      console.log(chars);
+      for (let i=0; i<chars.data.length; i++) {
+        const { id, name, occupation, cartoon, weapon } = chars.data[i];
+        const newCharsHtml = 
+             `
+               <div class="character-info">
+                 <p> Id: ${id} </p>
+                 <p> Name: ${name} </p>
+                 <p> Occupation: ${occupation} </p>
+                 <p> Is a Cartoon?: ${cartoon} </p>
+                 <p> Weapon: ${weapon} </p>
+               </div>
+             `;
+         document.getElementById("character-info").innerHTML += newCharsHtml;
+      }
+    });
   }
+});
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const id = document.getElementsByName("character-id")[0].value;
+    charactersAPI.getOneRegister(id)
+    .then (response => {
+      console.log(response);
+      const { id, name, occupation, cartoon, weapon } = response.data;
+      const newCharsHtml = 
+             `
+               <div class="character-info">
+                 <p> Id: ${id} </p>
+                 <p> Name: ${name} </p>
+                 <p> Occupation: ${occupation} </p>
+                 <p> Is a Cartoon?: ${cartoon} </p>
+                 <p> Weapon: ${weapon} </p>
+               </div>
+             `;
+         document.getElementsByClassName("operation")[0].innerHTML += newCharsHtml;
+    })
   }
   
   document.getElementById('delete-one').onclick = function(){
+    const id = document.getElementsByName("character-id-delete")[0].value;
+    charactersAPI.deleteOneRegister(id)
+    .then (() => {
+      let btnColor = document.getElementById("delete-one");
+      btnColor.style.backgroundColor = "green";
+    })
+    .catch (() => {
+      let btnColor = document.getElementById("delete-one");
+      btnColor.style.backgroundColor = "red";
+    })
         
   }
   
@@ -20,4 +66,3 @@ $(document).ready( () => {
   document.getElementById('new-character-form').onsubmit = function(){
                 
   }
-})
