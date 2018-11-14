5df8d8903c479169ba379e013b8c9bf55f860445
Mohsan <mohsanabbas>
2018-11-14 14:58:16 -0200

    Done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,33 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+     return axios.get(`${this.BASE_URL}/characters`)
+    .then(res => res.data)
+    
+  }
+
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => res.data)
+ 
+     
+  }
+
+  createOneRegister (newdata) {
+    axios.post(`${this.BASE_URL}/characters)`, {newdata})
+    .then(res => console.log({newdata}))
+  }
+
+  updateOneRegister (id, update) {
+    axios.patch(`${this.BASE_URL}/characters/${id}`, {update})
 
   }
 
-  getOneRegister () {
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+    // console.log(res);
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
 }
