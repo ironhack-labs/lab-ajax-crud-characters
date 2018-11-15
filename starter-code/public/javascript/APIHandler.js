c36cfc040a4ad4008cae0ecbf897d1dc31de28ad
Daniel Ferrari <danielferrari@live.com>
2018-11-15 10:38:35 -0200

    almost done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,15 +4,43 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(this.BASE_URL + '/characters').then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log("The error is: ", error);
+    });
   }
 
   getOneRegister () {
-
+    const theId = document.getElementsByName('character-id')[0].value;
+    axios.get(this.BASE_URL + `/characters/${theId}`).then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log("The error is: ", error);
+    });
   }
 
   createOneRegister () {
+    const theNames = document.getElementsByName('name');
+    const theOccupations = document.getElementsByName('occupation');
+    const theWeapons = document.getElementsByName('weapon');
+    const theCartoons = document.getElementsByName('cartoon');
 
+    const characterInfo = {
+      name: theNames[0].value,
+      occupation: theOccupations[0].value,
+      weapon: theWeapons[0].value,
+      isCartoon: theCartoons[0].checked
+    };
+
+    axios.post(this.BASE_URL + '/characters', characterInfo).then(response => {
+      console.log('post successful and the response is: ',response.data);
+    })
+    .catch(error => {
+      console.log("The error is: ", error);
+    });
   }
 
   updateOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,24 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister();
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    // charactersAPI.deleteOneRegister();
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    // event.preventDefault();
+    // charactersAPI.updateOneRegister();
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function(event){
+    event.preventDefault();
+    charactersAPI.createOneRegister();
   }
 })
