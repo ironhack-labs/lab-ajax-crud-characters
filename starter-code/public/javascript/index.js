54d2b2fbfbcace7d43de8efa8704487e6ae0f1fd
Rachel Hofer <rhofer3@gmail.com>
2018-11-19 14:43:43 -0500

    can fetch all and can fetch one

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,60 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
-  }
+// get all characters
+  function fetchAll(){
   
-  document.getElementById('fetch-one').onclick = function(){
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+    .then((response)=>{
+        $('.character-info').html("<ul id='list'></ul>")
+        response.data.forEach((eachCharacter)=>{
+
+            $('#list').append(`
+            <li>
+           name:        ${eachCharacter.name}
+           occupation:  ${eachCharacter.occupation}
+           weapon:      ${eachCharacter.weapon}
+           debt:        ${eachCharacter.debt}
+            </li>
+
+            <br>
+            `)
+        })
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
+$('#fetch-all').click(fetchAll)
+  
+
+// get ONE character
+function fetchOne(){
+
+  let input = $("#oneInput").val();
+
+  axios.get('https://ih-crud-api.herokuapp.com/characters/' + input)
+
+  .then((response)=>{
+    let oneChar = response.data;  
     
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+      $('.character-info').html("<ul id='list'></ul>")
+
+      $('#list').append(`
+        <li>
+        Id:          ${oneChar.id}<br>
+        name:        ${oneChar.name}<br>
+        occupation:  ${oneChar.occupation}<br>
+        weapon:      ${oneChar.weapon}<br>
+        debt:        ${oneChar.debt}
+        </li>
+
+        <br>
+        `)
+  })
+  .catch((err)=>{
+      console.log(err)
+  })
+}
+
+$('#fetch-one').click(fetchOne);
