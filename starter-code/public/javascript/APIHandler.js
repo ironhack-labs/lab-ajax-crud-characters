81f3367c0e0145c459750c141e6226abb22bd807
David Diez <ddiezr@gmail.com>
2018-11-21 16:42:39 +0100

    Firdt

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,63 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList() {
+        return axios.get("http://localhost:8000/characters")
+            .then((fullListCharacters) => {
+                return fullListCharacters.data;
+            })
+            .catch((error) => {
+                console.log(`getFullList of characters error:${error}`)
+            })
 
-  }
+    }
 
-  getOneRegister () {
+    getOneRegister(id) {
+        return axios.get(`http://localhost:8000/characters/${id}`)
+            .then((oneCharacter) => {
+                return oneCharacter.data;
+            })
+            .catch((error) => {
+                console.log(`get one  character error:${error}`)
+            })
 
-  }
+    }
 
-  createOneRegister () {
+    createOneRegister(newChar) {
+        console.log(newChar + 'newchar');
+        return axios.post(`http://localhost:8000/characters`, newChar)
+            .then((newCharacter) => {
+                console.log(newCharacter);
+                return newCharacter.data;
+            })
+            .catch((error) => {
+                console.log(`get one  character error:${error}`)
+            })
+    }
 
-  }
+    updateOneRegister(editChar) {
+        console.log(editChar);
+        return axios.put(`http://localhost:8000/characters/${editChar.id}`, editChar)
+            .then((editCharacter) => {
+                // console.log(editCharacter);
+                return editCharacter.data;
+            })
+            .catch((error) => {
+                console.log(`get one  character error:${error}`)
+            })
+    }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(id) {
+        console.log(id)
+        return axios.delete(`http://localhost:8000/characters/${id}`)
+            .then((deleteCharacter) => {
+                console.log(deleteCharacter);
+                return `Character has been successfully deleted`;
+            })
+            .catch((error) => {
+                console.log(`Character not found:${error}`)
+            })
+    }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,78 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+    document.getElementById('fetch-all').onclick = function () {
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
+        charactersAPI.getFullList().then((fullList) => {
+            //    return fullList;
+            console.log(fullList)
             
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+            document.querySelector('.character-info .id').innerText = `Id : ${fullList[0].id}`;
+            document.querySelector('.character-info .name').innerText = `Name : ${fullList[0].name}`;
+            document.querySelector('.character-info .occupation').innerText = `Occupation :${fullList[0].occupation}`;
+            document.querySelector('.character-info .cartoon').innerText = `Cartoon : ${fullList[0].cartoon}`;
+            document.querySelector('.character-info .weapon').innerText = `Weapon : ${fullList[0].weapon}`;
+
+        }).catch((err) => {
+            console.log(error)
+        })
+
+    }
+
+    document.getElementById('fetch-one').onclick = function () {
+        let characterId = document.querySelector('.operation input').value;
+        charactersAPI.getOneRegister(characterId).then((oneCharacter) => {
+                console.log(oneCharacter);
+                document.querySelector('.character-info .id').innerText = `Id : ${oneCharacter.id}`;
+                document.querySelector('.character-info .name').innerText = `Name : ${oneCharacter.name}`;
+                document.querySelector('.character-info .occupation').innerText = `Occupation :${oneCharacter.occupation}`;
+                document.querySelector('.character-info .cartoon').innerText = `Cartoon : ${oneCharacter.cartoon}`;
+                document.querySelector('.character-info .weapon').innerText = `Weapon : ${oneCharacter.weapon}`;
+            })
+            .catch((err) => {
+                console.log(error)
+            })
+    }
+
+    document.getElementById('delete-one').onclick = function () {
+        let characterId = document.querySelector('.delete input').value
+        console.log(characterId)
+        charactersAPI.deleteOneRegister(characterId).then((deleted) => {
+                console.log(deleted);
+            })
+            .catch((err) => {
+                console.log(error)
+            })
+    }
+
+    document.getElementById('edit-data').onclick = function () {
+        let editCharacter = {};
+        editCharacter.id = document.getElementsByName('chr-id')[0].value;
+        editCharacter.name = document.getElementsByName('name')[1].value;
+        editCharacter.occupation = document.getElementsByName('occupation')[1].value;
+        editCharacter.weapon = document.getElementsByName('weapon')[1].value;
+        editCharacter.cartoon = document.getElementsByName('cartoon')[1].value;
+        // console.log(editCharacter);
+        charactersAPI.updateOneRegister(editCharacter).then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(error)
+            })
+    }
+
+    document.getElementById('send-data').onclick = function () {
+        let newCharacter = {};
+        newCharacter.name = document.getElementsByName('name')[0].value
+        newCharacter.occupation = document.getElementsByName('occupation')[0].value
+        newCharacter.weapon = document.getElementsByName('weapon')[0].value
+        newCharacter.cartoon = document.getElementsByName('cartoon')[0].value
+
+        charactersAPI.createOneRegister(newCharacter).then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(error)
+            })
+    }
 })
