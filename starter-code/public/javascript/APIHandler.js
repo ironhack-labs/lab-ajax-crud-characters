39af6dd4380dfe07e4f1ced2b84159f9ff29d13a
aemartos <aemartos@gmail.com>
2018-11-25 20:32:34 +0100

    Finish exercise

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,79 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.charContainer = $(".characters-container");
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(this.BASE_URL).then(res => {
+      const chars = res.data;
+      this.charContainer.empty();
+      chars.forEach(char => {
+        this.printCharacter(char.id, char.name, char.occupation, char.cartoon, char.weapon);
+      });
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister(charID) {
+    this.charContainer.empty();
+    return axios.get(`${this.BASE_URL}/${charID}`).then(res => {
+      const char = res.data;
+      this.printCharacter(char.id, char.name, char.occupation, char.cartoon, char.weapon);
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister(data, e) {
+    return axios.post(this.BASE_URL, data)
+      .then(res => {
+        this.getOneRegister(res.data.id);
+        //this.getFullList();
+        this.buttonColor('right', e);
+      })
+      .catch(err => {
+        console.log(err, `Can't create character`);
+        this.buttonColor('wrong', e);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(data, charID, e) {
+    return axios.patch(`${this.BASE_URL}/${charID}`, data)
+      .then(res => {
+        this.getOneRegister(res.data.id);
+        //this.getFullList();
+        this.buttonColor('right', e);
+      })
+      .catch(err => {
+        console.log(err, `Can't edit character`);
+        this.buttonColor('wrong', e);
+      });
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(charID, e) {
+    return axios.delete(`${this.BASE_URL}/${charID}`).then(() => {
+      this.getFullList();
+      this.buttonColor('right', e);
+    })
+    .catch(err => {
+      console.log(err, `Can't delete character`);
+      this.buttonColor('wrong', e);
+    });
+  }
 
+  printCharacter(id, name, occupation, cartoon, weapon) {
+    const charBox =
+      `<div class="character-info">
+        <div class="id"><p>Character ID: <span class="color">${id}</span></p></div>
+        <div class="name"><p>Character name: <span class="color">${name}</span></p></div>
+        <div class="occupation"><p>Character occupation: <span class="color">${occupation}</span></p></div>
+        <div class="cartoon"><p>Is a cartoon? <span class="color">${cartoon}</span></p></div>
+        <div class="weapon"><p>Character weapon: <span class="color">${weapon}</span></p></div>
+      </div>`;
+    this.charContainer.append(charBox);
+  }
+
+  buttonColor(resp, e) {
+    let target = $(e.target).is('button') ? $(e.target) : $(e.target).children('button');
+    target.addClass(resp);
+    setTimeout(() => {$('button').removeClass(resp)}, 1500);
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,59 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/characters")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
 
+  //$('button').click(() => {$('input').val('')});
+
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+    $('input').val('');
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = function () {
+    const charID = $('input[name="character-id"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.getOneRegister(charID);
+    $('input').val('');
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault();
+    const data = getData('new');
+    if (data.name === '' || data.occupation === '' || data.weapon === '' || data.cartoon === '') alert('Empty ID field');
+    else charactersAPI.createOneRegister(data, e);
+    $('input').val('');
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    e.preventDefault();
+    const data = getData('edit');
+    const charID = $('input[name="chr-id"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.updateOneRegister(data, charID, e);
+    $('input').val('');
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('delete-one').onclick = function (e) {
+    const charID = $('input[name="character-id-delete"]').val();
+    if (charID === '') alert('Empty ID field');
+    else charactersAPI.deleteOneRegister(charID, e);
+    $('input').val('');
   }
-})
+
+  const getData = (action) => {
+    let data = {
+      name: $(`#${action}-character-form input[name="name"]`).val(),
+      occupation: $(`#${action}-character-form input[name="occupation"]`).val(),
+      weapon: $(`#${action}-character-form input[name="weapon"]`).val(),
+      cartoon: $(`#${action}-character-form input[name="cartoon"]`).is(':checked')
+      //cartoon: $(`#${action}-character-form input[name="cartoon"]`).is(':checked') ? 'YES' : 'NO'
+    };
+    return data;
+  }
+
+
+
+
+
+
+});
