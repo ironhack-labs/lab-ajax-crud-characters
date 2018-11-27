15687175a8d4ceb84e79f76002aafb53b450dc67
Alfredo Hinarejos <alfredo.hina@gmail.com>
2018-11-27 15:08:56 +0100

    no color changes

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,39 @@ class APIHandler {
   }
 
   getFullList () {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(res => {
+        return res.data
+      })
+  };
 
+
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(res => {
+        return res.data
+      })
+  };
+
+
+  createOneRegister (character) {
+    return axios.post(`${this.BASE_URL}/characters/`, character)
+      .then(res => {
+        return res.data
+      })
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id, character) {
+    return axios.patch(`${this.BASE_URL}/characters/${id}`, character)
+      .then(res => {
+        return res.data
+      })
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then(res => {
+        return res.data
+      })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,85 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList().then(elem => {
 
+      console.log(elem)
+
+      $('.characters-container').empty()
+      let container = document.querySelector(".characters-container");
+
+      elem.forEach(e => {
+        const chars = ` <div class="character-info">
+        <div class="name">${e.name}</div>
+        <div class="occupation">${e.occupation}</div>
+        <div class="cartoon">${e.cartoon}</div>
+        <div class="weapon">${e.weapon}</div>
+      </div>`;
+        container.innerHTML += chars;
+      });
+    })
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const searchID = document.querySelector("input[name=character-id]").value;
+    let container = document.querySelector(".characters-container");
+    container.innerHTML = "";
+
+    if (searchID === "") {
+      container.innerHTML = "ERROR: Write an ID"
+    } else {
+
+      charactersAPI.getOneRegister(searchID).then(e =>{
+        const chars = ` <div class="character-info">
+        <div class="name">${e.name}</div>
+        <div class="occupation">${e.occupation}</div>
+        <div class="cartoon">${e.cartoon}</div>
+        <div class="weapon">${e.weapon}</div>
+      </div>`;
+        container.innerHTML += chars;
+      });
+    }
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const deleteID = document.querySelector("input[name=character-id-delete]").value;
+    charactersAPI.deleteOneRegister(deleteID)
+    .then (e => {
+      console.log(e)
+    })
+    .catch (() => {
+      document.getElementById("bred");
+    });
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault();
+
+    const id = document.querySelector("input[name=chr-id]").value;
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+    const editChar = {
+      name: $("#edit-character-form input[name='name']" ).val(),
+      occupation: $("#edit-character-form input[name='occupation']" ).val(),
+      weapon: $("#edit-character-form input[name='weapon']" ).val(),
+      cartoon: $("#edit-character-form input[name='cartoon']" ).is(":checked") ? true : false,
+    }
+
+    charactersAPI.updateOneRegister(id, editChar)
+  }
+
+  
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault();
+
+    const newChar = {
+      name: $("#new-character-form input[name='name']" ).val(),
+      occupation: $("#new-character-form input[name='occupation']" ).val(),
+      weapon: $("#new-character-form input[name='weapon']" ).val(),
+      cartoon: $("#new-character-form input[name='cartoon']" ).is(":checked") ? true : false
+    }
+
+    charactersAPI.createOneRegister(newChar)
   }
 })
+
