cca25cd795ebe537e336175e5443434ecf172a27
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-28 14:58:47 -0600

    Done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,18 +2,27 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-    charactersAPI.getFullList()
-    .then(result=>{
-      console.log(result)
-    })
+    gettAll()
   }
   
   document.getElementById('fetch-one').onclick = function(){
     const input = document.querySelector("input[name='character-id']")
     charactersAPI.getOneRegister(input.value)
     .then(result=>{
-      console.log(result)
       input.value = ""
+      const container = document.querySelector(".cards-container")
+      container.innerHTML = ""
+      container.innerHTML = `
+        <div class="characters-container">
+          <div class="character-info">
+            <div class="charid">ID: ${result.data.id}</div>
+            <div class="name">Name: ${result.data.name}</div>
+            <div class="occupation">Occupation: ${result.data.occupation}</div>
+            <div class="cartoon">Cartoon: ${result.data.cartoon}</div>
+            <div class="weapon">Weapon: ${result.data.weapon}</div>
+          </div>
+        </div>
+        `
     })
   }
   
@@ -21,14 +30,38 @@ $(document).ready( () => {
     const input = document.querySelector("input[name='character-id-delete']")
     charactersAPI.deleteOneRegister(input.value)
     .then(result=>{
-      console.log(result)
       input.value = ""
+      gettAll()
     })
-        
   }
   
   document.getElementById('edit-character-form').onsubmit = function(e){
     e.preventDefault();
+    editCharacter(e)
+  }
+
+  function gettAll(){
+    charactersAPI.getFullList()
+    .then(result=>{
+      const container = document.querySelector(".cards-container")
+      container.innerHTML = ""
+      result.data.forEach(character => {
+        container.innerHTML += `
+        <div class="characters-container">
+          <div class="character-info">
+            <div class="charid">ID: ${character.id}</div>
+            <div class="name">Name: ${character.name}</div>
+            <div class="occupation">Occupation: ${character.occupation}</div>
+            <div class="cartoon">Cartoon: ${character.cartoon}</div>
+            <div class="weapon">Weapon: ${character.weapon}</div>
+          </div>
+        </div>
+        `
+      });
+    }).catch(e=>console.log(e))
+  }
+
+  function editCharacter(e){
     const form = document.querySelector("#edit-character-form")
     const id = form.chrid.value
     let character = {
@@ -39,11 +72,11 @@ $(document).ready( () => {
     }
     charactersAPI.updateOneRegister(id,character)
     .then(result=>{
-      console.log(result)
       form.name.value = ""
       form.weapon.value = ""
       form.occupation.value = ""
       form.cartoon.checked = false
+      gettAll()
     })
   }
   
@@ -58,11 +91,11 @@ $(document).ready( () => {
     }
     charactersAPI.createOneRegister(character)
     .then(result=>{
-      console.log(result)
       form.name.value = ""
       form.weapon.value = ""
       form.occupation.value = ""
       form.cartoon.checked = false
+      gettAll()
     })             
   }
 })
