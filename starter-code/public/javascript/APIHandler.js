ac97cb2d54a673be066854013e30a84827398e5c
MefitHp <mefitDev@gmail.com>
2018-11-28 17:53:51 -0600

    Missing update

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,65 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios.get(this.BASE_URL + '/characters')
+      .then(characters => {
+        $('.characters-container').html('')
+        characters.data.forEach(character => {
+          $('.characters-container').append(`
+          <div class="character-info">
+            <div class="name">Character Name: ${character.name}</div>
+            <div class="occupation">Character Occupation: ${character.occupation}</div>
+            <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+            <div class="weapon">Character Weapon: ${character.weapon}</div>
+          </div>
+          `)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  getOneRegister(id) {
+    axios.get(this.BASE_URL + '/characters/' + id)
+      .then(character => {
+        if (id === '') {
+          return $('.characters-container').html('<strong>Seleciona un ID</strong>')
+        }
+        $('.characters-container').html('')
+        $('.characters-container').append(` 
+          <div class="character-info">
+            <strong>${character._id}</strong>
+            <div class="name">Character Name: ${character.data.name}</div>
+            <div class="occupation">Character Occupation: ${character.data.occupation}</div>
+            <div class="cartoon">Is a Cartoon? ${character.data.cartoon}</div>
+            <div class="weapon">Character Weapon: ${character.data.weapon}</div>
+          </div>`)
+      }).catch(err => console.log(err))
+  }
+
+  createOneRegister(newCharacter) {
+    axios.post(this.BASE_URL + '/characters', newCharacter)
+      .then(character => {
+        $('.characters-container').html('')
+        this.getFullList()
+      }).catch(err => console.log(err))
+  }
+
+  updateOneRegister() {
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    console.log(id)
+    if (!confirm('Estás seguro que quieres borrar?')) return
+    axios.delete(this.BASE_URL + /characters/ + id)
+      .then(deleted => {
+        $('.characters-container').html('')
+        this.getFullList()
+      })
   }
 }
