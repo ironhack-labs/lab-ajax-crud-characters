443b26302e4580fb1aa7186cc3244d11ac5da673
MontseOrtiz <montse_2908@hotmail.com>
2018-11-28 22:44:51 -0600

    MEX PE MONTSE Y OMAR

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,76 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return axios
+      .get(this.BASE_URL + "/characters")
+      .then(result => {
+        $('.characters-container').html('')
+        return result;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  getOneRegister() {
+    const id = document.getElementById("valor-id").value;
+    return axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then(result => {
+        $('.characters-container').html('')
+        return result;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  createOneRegister(character) {
+    return axios.post(this.BASE_URL + "/characters", character)
+      .then(result => {
+        console.log("listo")
+        $('.characters-container').html('')
+        this.getFullList()
+      })
+      .catch(err => {
+        return err
+      })
+  }
+
+  updateOneRegister(e) {
+    e.preventDefault()
+    const character = {
+      name: e.target.name.value,
+      occupation: e.target.occupation.value,
+      weapon: e.target.weapon.value,
+      cartoon: e.target.cartoon.value
+    };
+    const boton = document.querySelector('#send-data-up')
+    const form = document.querySelector('#edit-character-form')
+    const id = document.getElementById("valor-id").value;
+    return axios.patch(this.BASE_URL + '/characters/' + id, character)
+    .then(result =>{
+      form.name.value =result.data.name
+      form.weapon.value =result.data.weapon
+      form.occupation.value =result.data.occupation
+    })
+
 
   }
 
-  getOneRegister () {
+  deleteOneRegister(id) {
+    const boton = document.querySelector('#delete-one')
+    if (!confirm("¿Seguro de borrar?")) {
+      return
+    }
+    return axios.delete(this.BASE_URL + '/characters/' + id)
+    .then(result => {
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+      this.getFullList()
+    })
+    .catch (err => console.log(err))
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,66 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList().then(result => {
+      result.data.forEach(element => {
+        const div = document.querySelector(".characters-container");
+        var newElem = document.createElement("div");
+        newElem.className= "character-info"
+        newElem.innerHTML = `
+        <div class="name">ID: ${element.id}</div>
+        <div class="name">Character Name: ${element.name}</div>
+        <div class="occupation">Character Occupation: ${
+          element.occupation
+        }</div>
+        <div class="cartoon">Is a Cartoon?: ${element.cartoon}</div>
+        <div class="weapon">Character Weapon: ${element.weapon}</div>
+        `;
+        div.appendChild(newElem);
+      });
+    });
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    charactersAPI.getOneRegister().then(result => {
+      result.data;
+      const div = document.querySelector(".characters-container");
+      var newElem = document.createElement("div");
+      newElem.className= "character-info"
+      newElem.innerHTML = `
+      <div class="name">ID: ${result.data.id}</div>
+      <div class="name">Character Name: ${result.data.name} </div>
+      <div class="occupation">Character Occupation: ${
+        result.data.occupation
+      }</div>
+      <div class="cartoon">Is a Cartoon?: ${result.data.cartoon}</div>
+      <div class="weapon">Character Weapon: ${result.data.weapon}</div>
+        `;
+      div.appendChild(newElem);
+    });
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const id = document.getElementById("delete-char").value
+    charactersAPI.deleteOneRegister(id)
+      
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault()
+    charactersAPI.updateOneRegister(e)
+  };
+
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    // tengo que obtener la data 
+    // convertirla en un objeto
+    e.preventDefault()
+    const character = {
+      name: e.target.name.value,
+      occupation: e.target.occupation.value,
+      weapon: e.target.weapon.value,
+      cartoon: e.target.cartoon.value
+    };
+    charactersAPI.createOneRegister(character)
+  };
+});
