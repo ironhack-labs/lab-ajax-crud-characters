52aea1628382975717b360ae4442b679f0c35171
Gabriel Pacheco <pacheco.sga@gmail.com>
2018-11-29 02:01:08 -0600

    MEX - PP - Gabriel Pacheco y Omar Vizarreta

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,107 @@ class APIHandler {
   }
 
   getFullList () {
+    const container = document.querySelector('.characters-container')
+    container.innerHTML= ""
 
+    axios.get(this.BASE_URL+'/characters')
+    .then(characters=>{
+      //console.log(characters.data)
+      characters.data.forEach(character=>{
+       
+       var newContainer = document.createElement("div")
+       newContainer.classList.add("character-info")
+       newContainer.innerHTML= `
+       <div class="name">${character.name}</div>
+       <div class="occupation">${character.occupation}</div>
+       <div class="cartoon">${character.cartoon}</div>
+       <div class="weapon">${character.weapon}</div>
+       `
+       container.appendChild(newContainer)
+     })
+    })
+    .catch(e=>{
+      console.log(e)
+    })
   }
 
   getOneRegister () {
+    const container = document.querySelector('.characters-container')
+    const id = document.getElementsByName('character-id')[0].value
+    //console.log(id)
 
+    axios.get(this.BASE_URL+'/characters/' + id)
+    .then(character=>{
+      //console.log(character.data)
+      container.innerHTML = ""
+      var newContainer = document.createElement("div")
+      newContainer.classList.add("character-info")
+      newContainer.innerHTML= `
+       <div class="name">${character.data.name}</div>
+       <div class="occupation">${character.data.occupation}</div>
+       <div class="cartoon">${character.data.cartoon}</div>
+       <div class="weapon">${character.data.weapon}</div>
+       `
+      container.appendChild(newContainer)
+      })
+      .catch(e=>{
+        console.log(e)
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister (e) {
+    e.preventDefault()
+    const char = {
+      name: e.target.name.value,
+      occupation: e.target.occupation.value,
+      cartoon: e.target.cartoon.value,
+      weapon:e.target.weapon.value
+    }
+    axios.post(this.BASE_URL+'/characters',char)
+    .then(result=>{
+      e.target.name.value =''
+      e.target.cartoon.value=''
+      e.target.weapon.value=''
+      console.log('Character added')
+    })
+    .catch(e=>{
+      console.log(e)
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (e) {
+    e.preventDefault()
+    const character = {
+      name: e.target.name.value,
+      occupation: e.target.occupation.value,
+      cartoon: e.target.cartoon.value,
+      weapon:e.target.weapon.value
+    }
+    const form = document.querySelector('#edit-character-form')
+    const id= e.target.children[0].children[1].value
+    //console.log(id)
+    return axios.patch(this.BASE_URL+'/characters/' + id,character)
+    .then(result=>{
+      this.getFullList()
+      form.chrid.value = ''
+      form.name.value =''
+      form.weapon.value =''
+      form.occupation.value =''
+    })
+ 
   }
-
+ 
   deleteOneRegister () {
-
+    const id = document.getElementsByName('character-id-delete')[0].value
+    if(!confirm('¿Quieres borrarlo?')){
+      return
+    } 
+    return axios.delete(this.BASE_URL+'/characters/' + id)
+    .then(result=>{
+      this.getFullList()
+    })
+    .catch(e=>{
+      console.log(e)
+    })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,24 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister()
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    charactersAPI.deleteOneRegister()
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault()
+    charactersAPI.updateOneRegister(e)
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault()
+    charactersAPI.createOneRegister(e)
   }
 })
