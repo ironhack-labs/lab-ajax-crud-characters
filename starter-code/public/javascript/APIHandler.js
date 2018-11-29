c19b8003dc9cab649d35959668dcb32ffe3ee277
Omar Avelar <omar.avelar.f@gmail.com>
2018-11-29 02:13:08 -0600

    fetch all fetch one

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,7 +4,12 @@ class APIHandler {
   }
 
   getFullList () {
-
+      return axios.get(this.BASE_URL + '/characters')
+      .then(result =>{
+        return result
+      }).catch(err=>{
+        console.log(err)
+      })
   }
 
   getOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,13 +1,70 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
+function fetchAll(){
+    //charactersAPI.getFullList()
+     // .then(result=>{
+      //  console.log(result);
+      //})
+
+
+      axios.get('http://localhost:8000/characters')
+      .then((response)=>{
+          response.data.forEach((eachCharacter)=>{
+            $('.characters-container').append(`
+            <div class="character-info">
+            <div class="name">Character Name: ${eachCharacter.name}</div>
+            <div class="occupation">Character Occupation: ${eachCharacter.occupation}</div>
+            <div class="cartoon">Is a Cartoon? ${eachCharacter.cartoon}</div>
+            <div class="weapon">Character Weapon: ${eachCharacter.weapon}</div>
+          </div>
+              `)
+          })
+      })
+      .catch((err)=>{
+          console.log(err)
+      })
   }
+ 
+  $('#fetch-all').click(fetchAll)
+
+
+
+
   
-  document.getElementById('fetch-one').onclick = function(){
-    
+  
+  function fetchOne(){
+
+    let input = $("#oneInput").val();
+ 
+    axios.get('https://ih-crud-api.herokuapp.com/characters/' + input)
+ 
+    .then((response)=>{
+      let oneChar = response.data;  
+      
+        $('.character-info').html("<ul id='list'></ul>")
+ 
+        $('#list').append(`
+          <li>
+          Id:          ${oneChar.id}<br>
+          name:        ${oneChar.name}<br>
+          occupation:  ${oneChar.occupation}<br>
+          weapon:      ${oneChar.weapon}<br>
+          debt:        ${oneChar.debt}
+          </li>
+ 
+          <br>
+          `)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
   }
+ 
+  $('#fetch-one').click(fetchOne);
+
+
+  
   
   document.getElementById('delete-one').onclick = function(){
         
@@ -19,5 +76,4 @@ $(document).ready( () => {
   
   document.getElementById('new-character-form').onsubmit = function(){
                 
-  }
-})
+    }
