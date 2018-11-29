acf1a19e6fb42473d226b81e00f861ff18ed7dc1
Migu8 <lopezcamiro.m8@gmail.com>
2018-11-28 19:39:21 -0600

    done iteration 1  iteration 2 a medias

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,60 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
+  
+
   getFullList () {
+  
+  return axios.get(this.BASE_URL + '/characters')
+    .then(results => {
+      return results.data
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  getOneRegister (id) {
+
+    return axios.get(this.BASE_URL + '/characters/' + id )
+      .then(results =>{
+        return results.data
+      })
+      .catch(err=>console.log(err))
+
 
   }
 
-  getOneRegister () {
+  createOneRegister (character) {
+
+    
+    return axios.post(this.BASE_URL +'/characters/',character )
+      .then(result =>{
+        return result
+      })
+     .catch(e=>console.log(e))
+
 
   }
 
-  createOneRegister () {
+  updateOneRegister (id,character) {
+
+    return axios.patch(this.BASE_URL + '/characters/' + id,character)
+    .then(result =>{
+      return result
+      })
+    .catch(e=>console.log(e))
+
+
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (id,character) {
+    return axios.delete(this.BASE_URL + '/characters/' + id)
+    .then(result =>{
+      return result
+      })
+    .catch(e=>console.log(e))
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,87 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
+
+function Push(element){
+  const contenedor = document.getElementsByClassName('characters-container')
+  var info = document.querySelector('.character-info:nth-child('+element.id+')')
+
+  info.style.display = "none"
+  var div = document.createElement("div")
+  div.className ="character-info"
+  div.innerHTML += `
+    <div class="name">id:  ${element.id} </div>
+    <div class="name">Name:  ${element.name} </div>
+    <div class="occupation">Occupation:  ${element.occupation}</div>
+    <div class="cartoon">Is a Cartoon? ${element.cartoon}</div>
+    <div class="weapon">Weapon: ${element.weapon}</div>
+`
+
+if(element.name){
+info.style.display = "initial"
+//contenedor[0](:nth-child:).style.display = "none"
+contenedor[0].append(div)
+}
+}
+
+
+
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+    .then(result => { console.log(result)
+      result.forEach(element => {
+            Push(element)
+        
+      });
+      
+    })
+    .catch(e=>console.log(e))
   }
   
   document.getElementById('fetch-one').onclick = function(){
     
+    var id = document.querySelector("input[name='character-id']").value
+
+    charactersAPI.getOneRegister(id)
+    .then(result =>{
+        Push(result)
+    }) 
+  
+    
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+
+    var id = document.querySelector("input[name='character-id-delete']").value
+    console.log(id)
+    charactersAPI.deleteOneRegister(id)
+
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
+
+    const character ={
+      name:"name",
+      occupation:"occupation",
+      weapon:"pon",
+      cartoon:true
+    }
+
+    charactersAPI.updateOneRegister (4,character)
+  
             
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
                 
+    const character ={
+      name:"name",
+      occupation:"occupation",
+      weapon:"weapon",
+      cartoon:true
+    }
+
+
+        charactersAPI.createOneRegister(character)
   }
 })
