595a1712f2c185caf9c568dfe31da1cd673e42bb
Axel Heinz <axelmheinz@gmail.com>
2019-01-12 15:32:21 +0100

    done show all show one delete one

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,26 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
+
   getFullList () {
+    return axios.get("http://localhost:8000/characters/")
 
   }
 
-  getOneRegister () {
-
+  getOneRegister (Id) {
+    console.log(Id);
+    return axios.get("http://localhost:8000/characters/" + Id)
   }
 
   createOneRegister () {
-
+return axios.post("http://localhost:8000/characters")
   }
 
   updateOneRegister () {
-
+    return axios.patch("http://localhost:8000/characters/${Id}")
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (Id) {
+    return axios.delete("http://localhost:8000/characters/"  + Id)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,50 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList().then(response => {
+      const data = response.data; // data from Axios
+      const htmlCharacters = response.data.map(character => {
+        return `
+        <div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.cartoon}</div>
+      </div>
+        `;
+      });
+      $(".characters-container").html(htmlCharacters);
+    });
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    const characterId = document.getElementById("character-id").value;
+    console.log(characterId);
+    charactersAPI.getOneRegister(characterId).then(response => {
+      const character = response.data
+      const htmlCharacter = `
+        <div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.cartoon}</div>
+      </div>
+        `;
+      $(".characters-container").html(htmlCharacter);
+    });
+  };
+
+
+  document.getElementById("delete-one").onclick = function() {
+    const deleteId = document.getElementById("delete-id").value;
+    console.log(deleteId);
+    charactersAPI.deleteOneRegister(deleteId);
+    document.getElementById("delete-message").innerHTML = "ID DELETED: " + deleteId;
+
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {};
+
+  document.getElementById("new-character-form").onsubmit = function() {};
+});
