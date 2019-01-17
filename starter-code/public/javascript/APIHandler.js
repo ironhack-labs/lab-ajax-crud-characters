8b005a4d8e7ebeff7ce8121371b5e19b49b8d623
“Archana” <archana.villalba@gmail.com>
2019-01-17 10:45:08 +0100

    iteration 1 done - Iteration 2 WIP

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,49 @@
+
+
+function renderCharacter(json){
+  return `<div class="character-info">
+  <div class="name">${json.name}</div>
+  <div class="occupation">${json.occupation}</div>
+  <div class="cartoon">${json.cartoon}</div>
+  <div class="weapon">${json.weapon}</div>
+</div>`;
+}
+
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  getFullList() {
+    axios.get('http://localhost:8000/characters')
+      .then(response => {
+        console.log (response.data, response.data.length) // [ {}, {},{} ]
+        let str = "";
+        response.data.forEach(character => {
+          str += renderCharacter(character);
+        })
+        console.log (str);
+      })
+      .catch(error => {
+        console.log('Oh No! Error is: ', error);
+      })
   }
 }
+
+// getOneRegister () {
+
+// }
+
+// createOneRegister () {
+
+// }
+
+// updateOneRegister () {
+
+// }
+
+// deleteOneRegister () {
+
+// }
+// }
