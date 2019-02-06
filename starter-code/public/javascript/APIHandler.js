f5b1ee154037b555bfbdbdfd25295bed2a14ee21
Thilo Sperber <t.sperber@gmx.net>
2019-02-06 19:40:53 +0100

    first commit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,59 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL + "characters")
+      .then(persons => {
+        /*  console.log(persons); */
+        persons.data.forEach(person => {
+          console.log(person.name);
+          /*   document.getElementsByClassName("name").innerHTML = person.name */
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    axios
+      .get(this.BASE_URL + "characters")
+      .then(persons => {
+        let output = persons.data.filter(function(person) {
+          return person.id === id;
+        })[0];
 
+        console.log(output);
+        /*  person.data.forEach(person => {
+         console.log(person.name)
+       document.getElementsByClassName("name").innerHTML = person.name
+       }) */
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    axios.post(this.BASE_URL + "characters", newCharacter).then(argument => {
+      console.log(newCharacter);
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(controlID, editCharacter) {
+    /* if (persons.data.includes(controlID.id)) */
+      axios.put(this.BASE_URL + "characters/" + controlID.id, editCharacter).then(argument => {
+        console.log(+controlID.id, editCharacter);
+       /*  console.log("ha entrado") */
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(this.BASE_URL + "characters/" + id).then(persons => {
+      console.log(persons);
+    });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,71 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList()
 
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const selectedId = +document.querySelector("input[name=character-id]").value
+
+    charactersAPI.getOneRegister(selectedId)
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const selectedId = +document.querySelector("input[name=character-id-delete]").value
+
+    charactersAPI.deleteOneRegister(selectedId)
+   /*  console.log(selectedId) */
+    
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault();
+   /*  let controlID = {
+      id: +document.querySelector("input[name=chr-id]").value,
+      }
+      console.log(controlID.id)
+
+    let editCharacter = {
+      name: document.querySelector("input[name=name]").value,
+      occupation: document.querySelector("input[name=occupation]").value,
+      weapon: document.querySelector("input[name=weapon]").value,
+      cartoon: document.querySelector("input[name=cartoon]").checked
+    }
+    console.log(document.querySelector("input[name=name]").value)
+    console.log(editCharacter)
+
+    charactersAPI.updateOneRegister(controlID, editCharacter) */
+    document.getElementById('edit-character-form').onsubmit = function () {
+      let updateID = document.getElementById("updateID").value
+      let editName = document.getElementById("editName").value
+      let editOccupation = document.getElementById("editOccupation").value
+      let editWeapon = document.getElementById("editWeapon").value
+      let editCartoon = document.getElementById("editCartoon").value
+   
+      let editCharacter = {
+        name: editName,
+        occupation: editOccupation,
+        weapon: editWeapon,
+        cartoon: editCartoon
+      }
+      charactersAPI.updateOneRegister(updateID, editCharacter)
+        .then((character) => console.log(character))
+        .catch(error => console.log(error))
+    }
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault();
+    let newCharacter = {
+      name: document.querySelector("input[name=name]").value,
+      occupation: document.querySelector("input[name=occupation]").value,
+      weapon: document.querySelector("input[name=weapon]").value,
+      cartoon: document.querySelector("input[name=cartoon]").checked
+    }
+   /*  console.log(newCharacter) */
+
+    charactersAPI.createOneRegister(newCharacter)
   }
 })
