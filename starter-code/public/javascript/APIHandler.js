b81732f8ac8b8dede9bb9b5e0bb867971f0d6212
murillorulkowski@gmail.com <murillorulkowski@gmail.com>
2019-02-06 21:10:21 -0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,86 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then((response) => {
+        const containerPrincipal = document.getElementsByClassName('characters-container');
+        containerPrincipal[0].innerHTML = '';
+        response.data.forEach((element) => {
+          const text = `
+       <div class="character-info">
+       <div class="id">ID: ${element.id}</div>
+       <div class="name">Name: ${element.name}</div>
+       <div class="occupation">Occupation: ${element.occupation}</div>
+       <div class="cartoon">Cartoon: ${element.cartoon}</div>
+       <div class="weapon">Weapon: ${element.weapon}</div>
+     </div>`;
+          containerPrincipal[0].innerHTML += text;
+        });
+        console.log('Response from API is: ',
+          response.data);
+      })
+      .catch((err) => {
+        console.log('Error is: ', err);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    const searchID = document.getElementById('searchID').value;
+    axios.get(`${this.BASE_URL}/characters/${searchID}`)
+      .then((response) => {
+        const containerPrincipal = document.getElementsByClassName('characters-container');
+        containerPrincipal[0].innerHTML = '';
+        const text = `
+       <div class="character-info">
+       <div class="name">Name: ${response.data.name}</div>
+       <div class="occupation">Occupation: ${response.data.occupation}</div>
+       <div class="cartoon">Cartoon: ${response.data.cartoon}</div>
+       <div class="weapon">Weapon: ${response.data.weapon}</div>
+     </div>`;
+        containerPrincipal[0].innerHTML += text;
+        console.log(response.data);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    const name = document.getElementById('createName').value;
+    const occupation = document.getElementById('createOccupation').value;
+    const weapon = document.getElementById('createWeapon').value;
+    const cartoon = document.getElementById('createCartoon').checked;
+    console.log(cartoon);
+    axios.post(`${this.BASE_URL}/characters`, { name, occupation, weapon, cartoon })
+      .then(() => {
+        this.getFullList();
+      })
+      .catch((err) => {
+        console.log('Error is: ', err);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    const id = document.getElementById('updateID').value;
+    const name = document.getElementById('updateName').value;
+    const occupation = document.getElementById('updateOccupation').value;
+    const weapon = document.getElementById('updateWeapon').value;
+    const cartoon = document.getElementById('updateCartoon').checked;
+    axios.put(`${this.BASE_URL}/characters/${id}`, { name, occupation, weapon, cartoon })
+      .then(() => {
+        this.getFullList();
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    const deleteID = document.getElementById('deleteID').value;
+    axios.delete(`${this.BASE_URL}/characters/${deleteID}`)
+      .then((response) => {
+        this.getFullList();
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log('Error is: ', err);
+      });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,27 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
+    charactersAPI.getOneRegister();
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+    charactersAPI.deleteOneRegister();
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function (event) {
+    event.preventDefault();
+    charactersAPI.updateOneRegister();
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('new-character-form').onsubmit = function (event) {
+    event.preventDefault();
+    charactersAPI.createOneRegister();
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+});
