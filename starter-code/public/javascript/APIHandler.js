f5c94a35390a9ada2f96d42a53c223777d235444
Sofía <sofianombela@gmail.com>
2019-02-06 18:43:44 +0100

    iteration 1

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,59 @@
-class APIHandler {
-  constructor (baseUrl) {
+class APIHandler 
+{
+  constructor (baseUrl) 
+  {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() 
+  {
+    axios.get(this.BASE_URL)
+    .then(response => 
+    {
+      console.log(this.BASE_URL)
+      console.log('post successful and the response is: ', response.data)
+    })
+    .catch(error => 
+    {
+    console.log('Oh No! Error is: ', error)
+    })
   }
 
   getOneRegister () {
+    axios.get(this.BASE_URL)
+    .then(response => 
+    {
+      const output = response.data.filter((data) => {
+        return data.id === +document.querySelector("input[name=character-id]").value
+      })[0]
 
+      console.log('post successful and the response is: ', output)
+    })
+    .catch(error => 
+    {
+      console.log('Oh No! Error is: ', error)
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister (elbicho) {
+    axios.post(this.BASE_URL, elbicho)
+    .then(response => 
+    {
+      console.log("+++++",response)
+    })
+    .catch(error => 
+    {
+    console.log('Oh No! Error is: ', error)
+    })
   }
 
   updateOneRegister () {
-
+    
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + "/" + id).then(persons => {
+      console.log(persons)
+    });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,35 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/characters")
 
 $(document).ready( () => {
+
   document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList()
 
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister()
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const selectedId = document.querySelector("input[name=character-id-delete]").value
+    charactersAPI.deleteOneRegister(selectedId)
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+      charactersAPI.updateOneRegister()
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    const mybichito = {
+      name: document.getElementById("name").value,
+      occupation: document.getElementById('occupation').value,
+      weapon: document.getElementById('weapon').value,
+      cartoon: document.getElementById('cartoon').value
+    }
+
+    charactersAPI.createOneRegister(mybichito)
   }
 })
+
+// "input[name=character-id]"
