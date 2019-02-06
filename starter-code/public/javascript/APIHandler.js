254bffc8be973a289aefee5d9f7c8b0e199ab231
Inés Guerra <inesguerra@MacBook-Air-de-Ines.local>
2019-02-06 16:38:08 +0100

    end points

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,71 @@ class APIHandler {
   }
 
   getFullList () {
+    axios.get("/characters")
+  .then(data => {
+    console.log(data)
+  }) 
+  .catch(err => {
+    console.log(err)
+  })
 
   }
 
   getOneRegister () {
+    axios.get("/characters/:id")
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 
   }
 
   createOneRegister () {
+  axios.post("/characters", {
+    name: "string",
+    occupation: "string",
+    cartoon: "string",
+    weapon: "string"
+  })
+  .then(data => {
+    console.log(data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
 
   }
 
   updateOneRegister () {
+    axios.put("/characters/:id", {
+    name: "string",
+    occupation: "string",
+    cartoon: "string",
+    weapon: "string"
+      
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
 
   }
 
   deleteOneRegister () {
+    axios.delete("/characters/:id")
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
 
   }
 }
