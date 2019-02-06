4bf6e09ce8f5af8ab2d65b02835a86b4551de53c
Marianna <mddobal@gmail.com>
2019-02-06 21:50:52 -0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(response => response.data)
+      .catch(error => console.log(error));
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    const id = document.getElementById('fetch-id').value;
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(response => response.data)
+      .catch(error => console.log(error));
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    const name = document.getElementById('new-name').value;
+    const occupation = document.getElementById('new-occupation').value;
+    const weapon = document.getElementById('new-weapon').value;
+    const cartoon = document.getElementById('new-cartoon').value;
+    const obj = {
+      name,
+      weapon,
+      cartoon,
+      occupation
+    };
+    const button = document.getElementById('delete-one');
+    return axios.post(`${this.BASE_URL}/characters`, obj)
+      .then((response) => {
+        console.log('post successful and the response is: ', response);
+        button.css('background-color', 'green');
+      })
+      .catch((error) => { console.log(error), button.css('background-color', 'red'); });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    const id = document.getElementById('edit-id').value;
+    const name = document.getElementById('edit-name').value;
+    const occupation = document.getElementById('edit-occupation').value;
+    const weapon = document.getElementById('edit-weapon').value;
+    const cartoon = document.getElementById('edit-cartoon').value;
+    const obj = {
+      name,
+      weapon,
+      cartoon,
+      occupation
+    };
+    return axios.put(`${this.BASE_URL}/characters/${id}`, obj)
+      .then(response => console.log('post successful and the response is: ', response))
+      .catch(error => console.log(error));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    const id = document.getElementById('delete-id').value;
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then(response => response.data)
+      .catch(error => console.log(error));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,49 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000');
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    let htmlInfo = '';
+    charactersAPI.getFullList().then((response) => {
+      console.log(response);
+      response.forEach((element) => {
+        htmlInfo 
+      += `<div class="character-info">
+        <div class="id">Id: ${element.id}</div>
+        <div class="name">Name: ${element.name}</div>
+        <div class="occupation">Occupation: ${element.occupation}</div>
+        <div class="cartoon">Is it a cartoon? ${element.cartoon}</div>
+        <div class="weapon">Weapon: ${element.weapon}</div>
+      </div>`;
+      });
+      document.getElementById('characters-container').innerHTML = htmlInfo;
+    });
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById('fetch-one').onclick = function () {
+      let htmlInfo = '';
+      charactersAPI.getOneRegister().then((element) => {
+        // console.log(element);
+          htmlInfo 
+        += `<div class="character-info">
+          <div class="id">Id: ${element.id}</div>
+          <div class="name">Name: ${element.name}</div>
+          <div class="occupation">Occupation: ${element.occupation}</div>
+          <div class="cartoon">Is it a cartoon? ${element.cartoon}</div>
+          <div class="weapon">Weapon: ${element.weapon}</div>
+        </div>`;
+        document.getElementById('characters-container').innerHTML = htmlInfo;
+      });
+  };
+
+  document.getElementById('delete-one').onclick = function () {
+    charactersAPI.deleteOneRegister();
+  };
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    charactersAPI.updateOneRegister();
+  };
+
+  document.getElementById('new-character-form').onsubmit = function () {
+    charactersAPI.createOneRegister();
+  };
+});
