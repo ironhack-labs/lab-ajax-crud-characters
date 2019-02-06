5312de4e91b8a51f6738cb4f0a60704ad1258636
Ruben Villar <ruben@rubenvillar.net>
2019-02-06 18:14:27 +0100

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,100 @@
+function selectContainer(containerClass) {
+  const container = document.querySelector(containerClass);
+  container.innerHTML = "";
+  return container;
+}
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios.get(this.BASE_URL + "/characters").then(characters => {
+      const container = selectContainer(".characters-container");
 
+      characters.data.forEach(character => {
+        const charactersInfo = document.createElement("div");
+        charactersInfo.classList.add("character-info");
+
+        const charactersName = document.createElement("div");
+        charactersInfo.classList.add("name");
+        charactersName.innerHTML = character.name;
+        charactersInfo.appendChild(charactersName);
+
+        const charactersOccupation = document.createElement("div");
+        charactersInfo.classList.add("occupation");
+        charactersOccupation.innerHTML = character.occupation;
+        charactersInfo.appendChild(charactersOccupation);
+
+        const charactersWeapon = document.createElement("div");
+        charactersInfo.classList.add("weapon");
+        charactersWeapon.innerHTML = character.cartoon;
+        charactersInfo.appendChild(charactersWeapon);
+
+        const charactersCartoon = document.createElement("div");
+        charactersInfo.classList.add("cartoon");
+        charactersCartoon.innerHTML = character.weapon;
+        charactersInfo.appendChild(charactersCartoon);
+
+        container.appendChild(charactersInfo);
+      });
+    });
   }
 
-  getOneRegister () {
+  getOneRegister(characterId) {
+    axios
+      .get(this.BASE_URL + "/characters" + "/" + characterId)
+      .then(character => {
+        const container = selectContainer(".characters-container");
+        const charactersInfo = document.createElement("div");
+        charactersInfo.classList.add("character-info");
 
+        const charactersName = document.createElement("div");
+        charactersInfo.classList.add("name");
+        charactersName.innerHTML = character.data.name;
+        charactersInfo.appendChild(charactersName);
+
+        const charactersOccupation = document.createElement("div");
+        charactersInfo.classList.add("occupation");
+        charactersOccupation.innerHTML = character.data.occupation;
+        charactersInfo.appendChild(charactersOccupation);
+
+        const charactersCartoon = document.createElement("div");
+        charactersInfo.classList.add("cartoon");
+        charactersCartoon.innerHTML = character.data.weapon;
+        charactersInfo.appendChild(charactersCartoon);
+
+        const charactersWeapon = document.createElement("div");
+        charactersInfo.classList.add("weapon");
+        charactersWeapon.innerHTML = character.data.cartoon;
+        charactersInfo.appendChild(charactersWeapon);
+
+        container.appendChild(charactersInfo);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(payload) {
+    axios
+      .post(this.BASE_URL + "/characters", payload)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(payload) {
+    console.log(payload);
+    axios
+      .put(this.BASE_URL + "/characters" + "/" + payload.id, payload)
+      .then(response => console.log(response))
+      .catch(error => console.log(error));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(characterId) {
+    axios
+      .delete(this.BASE_URL + "/characters" + "/" + characterId)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log("No hay"));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,53 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList();
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    const characterId = document.querySelector("input[name=character-id").value;
+    charactersAPI.getOneRegister(characterId);
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const characterId = document.querySelector("input[name=character-id-delete")
+      .value;
+    charactersAPI.deleteOneRegister(characterId);
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    let cartoonCheck = document.querySelector("input[name=cartoon-edit");
+    let cartoonValue = false;
+    if (cartoonCheck.checked) {
+      cartoonValue = true;
+    }
+
+    let payload = {
+      id: document.querySelector("input[name=chr-id").value,
+      name: document.querySelector("input[name=name-edit").value,
+      occupation: document.querySelector("input[name=occupation-edit").value,
+      weapon: document.querySelector("input[name=weapon-edit").value,
+      cartoon: cartoonValue
+    };
+    charactersAPI.updateOneRegister(payload);
+  };
+
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    e.preventDefault();
+    let cartoonCheck = document.querySelector("input[type=checkbox");
+    let cartoonValue = false;
+    if (cartoonCheck.checked) {
+      cartoonValue = true;
+    }
+
+    let payload = {
+      name: document.querySelector("input[name=name").value,
+      occupation: document.querySelector("input[name=occupation").value,
+      weapon: document.querySelector("input[name=weapon").value,
+      cartoon: cartoonValue
+    };
+    charactersAPI.createOneRegister(payload);
+  };
+});
