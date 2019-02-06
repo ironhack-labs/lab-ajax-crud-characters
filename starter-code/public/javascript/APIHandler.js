df251cc30bf67df0a829c08c2bed206d0b1e8404
Malte <m.felmy@googlemail.com>
2019-02-06 13:21:10 +0100

    BER - Emmanuel & Malte

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,29 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(this.BASE_URL + "/characters").then(response => response.data);
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(this.BASE_URL + "/characters/" + id).then(response => response.data);
   }
 
-  createOneRegister () {
-
+  createOneRegister(newInfo) {
+    return axios
+      .post(this.BASE_URL + "/characters", newInfo)
+      .then(response => response.data);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updatedInfo) {
+    return axios
+      .patch(this.BASE_URL + "/characters/" + id, updatedInfo)
+      .then(response => response.data);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(this.BASE_URL + "/characters/" + id).then(response => response.data);
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,109 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    let parent = document.getElementById("characters-container");
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
 
-  }
+    charactersAPI
+      .getFullList()
+      .then(characters => {
+        for (let i = 0; i < characters.length; i++) {
+          const { id, name, occupation, cartoon, weapon } = characters[i];
+          const newHtml = `
+            <div class="character-info">
+                <div class="name">${id}: ${name}</div>
+                <div class="occupation">${occupation}</div>
+                <div class="cartoon">${cartoon}</div>
+                <div class="weapon">${weapon}</div>
+            </div>
+          `;
+          document.getElementById("characters-container").innerHTML += newHtml;
+        }
+      })
+      .chatch(console.log);
+
+    // try {
+    //   charactersAPI
+    //     .getFullList() // should be a promis
+    //     .then(characters => {
+    //       console.log("TCL: document.getElementById -> characters", characters);
+    //     });
+    // }
+    // catch(err) {
+    //   // ERROR HANDLING
+    //   // good, if the error is in a subfunction
+    // }
+
+    // charactersAPI
+    //   .getFullList() // should be a promis
+    //   .then(characters => {
+    //     console.log("TCL: document.getElementById -> characters", characters);
+    //   });
+
+    // axios
+    //   .get("http://localhost:8000/characters")
+    //   .then(response => response.data)
+    //   .then(characters => {});
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    let parent = document.getElementById("characters-container");
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+
+    const id = document.getElementsByName("character-id")[0].value;
+
+    if (id !== "") {
+      charactersAPI.getOneRegister(id).then(character => {
+        console.log("TCL: document.getElementById -> character", character);
+        const { name, occupation, cartoon, weapon } = character;
+        const newHtml = `
+              <div class="character-info">
+              <div class="name">${id}: ${name}</div>
+                  <div class="occupation">${occupation}</div>
+                  <div class="cartoon">${cartoon}</div>
+                  <div class="weapon">${weapon}</div>
+              </div>
+            `;
+        document.getElementById("characters-container").innerHTML += newHtml;
+      });
+    }
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function(event) {
+    event.preventDefault();
+    const id = document.getElementById("edit-id").value;
+    const updatedInfo = {
+      name: document.getElementById("edit-name").value,
+      occupation: document.getElementById("edit-occupation").value,
+      cartoon: document.getElementById("edit-cartoon").checked,
+      weapon: document.getElementById("edit-weapon").value
+    };
+    charactersAPI.updateOneRegister(id, updatedInfo).then(character => {
+      console.log("TCL: document.getElementById -> character", character);
+    });
+  };
   
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("new-character-form").onsubmit = function() {
+    event.preventDefault();
+    const newInfo = {
+      name: document.getElementById("new-name").value,
+      occupation: document.getElementById("new-occupation").value,
+      cartoon: document.getElementById("new-cartoon").checked,
+      weapon: document.getElementById("new-weapon").value
+    };
+    charactersAPI.createOneRegister(newInfo).then(character => {
+      console.log("TCL: document.getElementById -> character", character);
+    });
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const id = document.getElementsByName("character-id-delete")[0].value;
+    charactersAPI.deleteOneRegister(id);
+  };
+
+});
