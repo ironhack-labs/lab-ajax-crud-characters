90f2908de2630be7eaec884d7896dde523874d10
Diego <diegoramos.media@gmail.com>
2019-02-06 18:50:21 -0200

    Lab Ajax Crud Characters - done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,70 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then((response) => {
+        const containerPrincipal = document.getElementsByClassName('characters-container');
+        containerPrincipal[0].innerHTML = '';
+        response.data.forEach((element) => {
+          const text = `
+          <div class="character-info">
+          <div class="name">Name: ${element.name}</div>
+          <div class="occupation">Occupation: ${element.occupation}</div>
+          <div class="cartoon">Is a Cartoon?</div>
+          <div class="weapon">Weapon: ${element.weapon}</div>
+          </div>`;
+          containerPrincipal[0].innerHTML += text;
+        });
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    const inputId = $('input[name=character-id]').val();
+    axios.get(`${this.BASE_URL}/characters/${inputId}`)
+      .then((response) => {
+        const containerPrincipal = document.getElementsByClassName('characters-container');
+        containerPrincipal[0].innerHTML = '';
+        const text = `
+        <div class="character-info">
+        <div class="name">Name: ${response.data.name}</div>
+        <div class="occupation">Occupation: ${response.data.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${response.data.cartoon}</div>
+        <div class="weapon">Weapon: ${response.data.weapon}</div>
+        </div>`;
+        containerPrincipal[0].innerHTML = text;
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    const name = $('input[name=name-create]').val();
+    const occupation = $('input[name=occupation-create]').val();
+    const weapon = $('input[name=weapon-create]').val();
+    const cartoon = $('input[name=cartoon-update]').val();
+    axios.post(`${this.BASE_URL}/characters`, { name, occupation, weapon, cartoon })
+      .then(() => {
+        console.log('created!');
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    const name = $('input[name=name-update]').val();
+    const occupation = $('input[name=occupation-update]').val();
+    const weapon = $('input[name=weapon-update]').val();
+    const cartoon = $('input[name=cartoon-update]').val();
+    const id = $('input[name=chr-id]').val();
+    axios.put(`${this.BASE_URL}/characters/${id}`, { name, occupation, weapon, cartoon }).then((response) => {
+      console.log('updated!');
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    const inputId = $('input[name=character-id-delete]').val();
+    axios.delete(`${this.BASE_URL}/characters/${inputId}`)
+      .then((response) => {
+        console.log(response.data);
+      });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,23 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000');
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = () => {
+    charactersAPI.getFullList();
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById('fetch-one').onclick = () => {
+    charactersAPI.getOneRegister();
+  };
+
+  document.getElementById('delete-one').onclick = () => {
+    charactersAPI.deleteOneRegister();
+  };
+
+  document.getElementById('edit-character-form').onsubmit = () => {
+    charactersAPI.updateOneRegister();
+  };
+
+  document.getElementById('new-character-form').onsubmit = () => {
+    charactersAPI.createOneRegister();
+  };
+});
