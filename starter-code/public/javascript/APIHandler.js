9f5bef114867d9ccb1ae26ca84325bfece0302a7
Daniela Lopez <danielalopez@MacBook-Air-de-Daniela.local>
2019-02-13 22:15:12 -0600

    PP MEX Daniela y Andrés

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,86 @@
-class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+
+  class APIHandler {
+    constructor(baseUrl) {
+      this.BASE_URL = baseUrl;
+    }
+  
+    getFullList() {
+      axios.get(this.BASE_URL + '/characters')
+        .then(characters => {
+          $('.characters-container').html('')
+          characters.data.forEach(character => {
+            $('.characters-container').append(`
+            <div class="character-info">
+              <div class="name">Character Name: ${character.name}</div>
+              <div class="occupation">Character Occupation: ${character.occupation}</div>
+              <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+              <div class="weapon">Character Weapon: ${character.weapon}</div>
+            </div>
+            `)
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  // The .append() method inserts the specified content as the last child of each element in the jQuery collection 
+  // (To insert it as the first child, use .prepend()).
+
+  
+    getOneRegister(id) {
+      axios.get(this.BASE_URL + '/characters/' + id)
+        .then(character => {
+          if (id === '') {
+            return $('.characters-container').html('<strong>Seleciona un ID</strong>')
+          }
+          $('.characters-container').html('')
+          $('.characters-container').append(` 
+            <div class="character-info">
+              <strong>${character._id}</strong>
+              <div class="name">Character Name: ${character.data.name}</div>
+              <div class="occupation">Character Occupation: ${character.data.occupation}</div>
+              <div class="cartoon">Is a Cartoon? ${character.data.cartoon}</div>
+              <div class="weapon">Character Weapon: ${character.data.weapon}</div>
+            </div>`)
+        }).catch(err => console.log(err))
+    }
+  
+    createOneRegister(newCharacter) {
+      axios.post(this.BASE_URL + '/characters', newCharacter)
+        .then(character => {
+          $('.characters-container').html('')
+          this.getFullList()
+        }).catch(err => console.log(err))
+    }
+  
+    updateOneRegister() {
+  
+    }
+  
+    deleteOneRegister(id) {
+      console.log(id)
+      if (!confirm('Estás seguro que quieres borrar?')) return
+      axios.delete(this.BASE_URL + /characters/ + id)
+        .then(deleted => {
+          $('.characters-container').html('')
+          this.getFullList()
+        })
+    }
   }
 
-  getFullList () {
 
-  }
+// axios.request(config)
 
-  getOneRegister () {
+// axios.get(url[, config])
 
-  }
+// axios.delete(url[, config])
 
-  createOneRegister () {
+// axios.head(url[, config])
 
-  }
+// axios.options(url[, config])
 
-  updateOneRegister () {
+// axios.post(url[, data[, config]])
 
-  }
+// axios.put(url[, data[, config]])
 
-  deleteOneRegister () {
-
-  }
-}
+// axios.patch(url[, data[, config]])
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,40 @@
+
+//.ready para indicar que estás con jquery
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    const characterId = document.getElementById('character-id').value
+    charactersAPI.getOneRegister(characterId)
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+    var deleteId = $('#character-id-delete').val()
+    charactersAPI.deleteOneRegister(deleteId)
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function () {
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault()
+    newCharacter = {
+      id: e.target.id.value,
+      name: e.target.name.value,
+      occupation: e.target.occupation.value,
+      weapon: e.target.weapon.value,
+      cartoon: e.target.cartoon.value
+    }
+    charactersAPI.createOneRegister(newCharacter)
+
+    e.target.id.value = ''
+    e.target.name.value = ''
+    e.target.occupation.value = ''
+    e.target.weapon.value = ''
+    e.target.cartoon.value = ''
   }
 })
