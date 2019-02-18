d1ddb83d6474dd100a772f9aa348035e0f37367a
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-18 12:59:40 -0500

    First commit of ajax-crud-characters lab

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,44 @@
-class APIHandler {
-  constructor (baseUrl) {
+class APIHandler 
+{
+  constructor (baseUrl) 
+  {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList () 
+  {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);           
+        })
+    .catch(err => {
+      console.log('Error is: ', err);
+    })
+  }
+
+  getOneRegister () 
+  {
+    axios.get(`${this.BASE_URL}/characters/:id`)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);           
+    })
+    .catch(err => {
+      console.log('Error is: ', err);
+      })
+  }
+
+  createOneRegister () 
+  {
 
   }
 
-  getOneRegister () {
+  updateOneRegister () 
+  {
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister () 
+  {
 
   }
 }
