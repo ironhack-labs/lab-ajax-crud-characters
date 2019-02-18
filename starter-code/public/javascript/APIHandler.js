acb0cd0530a08c2097d5b813401f1380412e7d01
Mike <troianello@protonmail.com>
2019-02-18 17:42:50 -0500

    finished

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,137 @@ class APIHandler {
   }
 
   getFullList () {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+        console.log('Response from API is:', responseFromAPI.data)
+        responseFromAPI.data.forEach((obj)=> {
 
+          // $(".id").html("Id: " + obj.id)
+          // $(".name").html("Name: " + obj.name)
+          // $(".occupation").html("Occupation: " + obj.occupation)
+          // if(obj.cartoon == "on"){
+          //   $(".cartoon").html("Is a Cartoon?: true")
+          // }
+          // else{
+          //   $(".cartoon").html("Is a Cartoon?: false")
+          // }       
+          // $(".weapon").html("Weapon: " + obj.weapon)
+
+          let is=""
+
+          if(obj.cartoon == "on"){
+            is = true
+          }
+          else{
+            is = false
+          }
+
+          let html = 
+          
+          `
+          <div class="character-info">
+          <div class="id">Id: ${obj.id}</div>
+          <div class="name">Name: ${obj.name}</div>
+          <div class="occupation">Occupation: ${obj.occupation}</div>
+          <div class="cartoon">Is a Cartoon?: ${is}</div>
+          <div class="weapon">Weapon: ${obj.weapon}</div>
+          </div>
+          `
+
+          $('.characters-container').append(html)
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    console.log(id)
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(responseFromAPI => {
+      let choice = responseFromAPI.data
+      console.log('Response from API is: ', responseFromAPI.data);
+      // responseFromAPI.data.forEach((obj)=> {
+        $(".id").html("Id: " + choice.id)
+        $(".name").html("Name: " + choice.name)
+        $(".occupation").html("Occupation: " + choice.occupation)
+        if(choice.cartoon == "on"){
+          $(".cartoon").html("Is a Cartoon?: true")
+        }
+        else{
+          $(".cartoon").html("Is a Cartoon?: false")
+        }       
+        $(".weapon").html("Weapon: " + choice.weapon)
 
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   createOneRegister () {
-
+    axios.post(`${this.BASE_URL}/characters`, {
+      "name": $('#name').val(),
+      "occupation": $('#occupation').val(),
+      "debt": $('#debt').val(),
+      "weapon": $('#weapon').val(),
+      "cartoon": $('#cartoon').val(),
+    })
+    .then(responseFromAPI => {
+      console.log('Response from API is:', responseFromAPI.data)
+      $(".1st-send").removeClass("red")
+      $(".1st-send").addClass("green")
+    })
+    .catch(err => {
+      $(".1st-send").removeClass("green")
+      $(".1st-send").addClass("red")
+      console.log(err);
+    })
   }
 
-  updateOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(responseFromAPI => {
+      console.log('Response from API is:', responseFromAPI.data)
+      $("#delete-one").removeClass("red")
+      $("#delete-one").addClass("green")
+    })
+    .catch(err => {
+      console.log(err);
+      $("#delete-one").removeClass("green")
+      $("#delete-one").addClass("red")
+    })
   }
 
-  deleteOneRegister () {
-
+  updateOneRegister (id) {
+    axios.put(`${this.BASE_URL}/characters/${id}`, {
+      "name": $('#name-edit').val(),
+      "occupation": $('#occupation-edit').val(),
+      "debt": $('#debt-edit').val(),
+      "weapon": $('#weapon-edit').val(),
+      "cartoon": $('#cartoon-edit').val(),
+    })
+    .then(responseFromAPI => {
+      console.log('Response from API is:', responseFromAPI.data)
+      console.log(responseFromAPI.name, "deleted")
+      $(".2nd-send").removeClass("red")
+      $(".2nd-send").addClass("green")
+    })
+    .catch(err => {
+      console.log(err);
+      $(".2nd-send").removeClass("green")
+      $(".2nd-send").addClass("red")
+    })
   }
 }
+
+
+
+// for(let i=0; i < responseFromAPI; i++){
+//   $(".id").html(responseFromAPI[0].id)
+//   $(".name").html(responseFromAPI[0].name)
+//   $(".occupation").html(responseFromAPI[0].occupation)
+//   $(".cartoon").html(responseFromAPI[0])
+//   $(".weapon").html(responseFromAPI[0].weapon)
+// }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,28 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList ()
   }
   
-  document.getElementById('fetch-one').onclick = function(){
+  document.getElementById('fetch-one').onclick = function(e){
+    let id = document.getElementById('fetch-one-input').value;
+    charactersAPI.getOneRegister (id)
+  }
+  
+  document.getElementById('delete-one').onclick = function(e){
+    let id = document.getElementById('character-id-delete').value;
+    charactersAPI.deleteOneRegister (id)
+  }
+  
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault()
+    let id = document.getElementById('chr-id').value;
+    charactersAPI.updateOneRegister (id)
+  }
+  
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault()
+    charactersAPI.createOneRegister ()
     
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
   }
 })
