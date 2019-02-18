37b239aa1bda33a985d9053dda09ea1e5eb3835d
LiranGib <liran.gibor@gmail.com>
2019-02-18 14:36:23 -0500

    first commit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,65 @@ class APIHandler {
   }
 
   getFullList () {
-
+    // const charactersApi = axios.create({
+    //   baseURL: 'http://localhost:8000/characters/'
+    // });
+    
+    //function getCharactersInfo(theName) {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);           
+    })
+    .catch(err => {
+      console.log('Error is: ', err);
+      })
+    //}
   }
 
-  getOneRegister () {
+  getOneRegister(e) {
+   
+    axios.get(`${this.BASE_URL}/characters/`+ e)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);           
+    })
+    .catch(err => {
+      console.log('Error is: ', err);
+      })
 
   }
 
   createOneRegister () {
-
-  }
+    axios.post(`${this.BASE_URL}/characters`, characterInfo)
+      .then(response => {
+          const {id, name, occupation, weapon, cartoon } = response.data;
+          const newCharacterHtml = `
+            <li>
+              <p> Charecter Id: ${id} </p>
+              <p> Character Name: ${name} </p>
+              <p> Character Occupation: ${occupation} </p>
+              <p> Character Weapon: ${weapon} </p>
+              <p> Is a Cartoon?: ${cartoon} </p>
+            </li>
+            `;
+            document.getElementById("edit-character-form").innerHTML += newCharacterHtml;
+            console.log('post successfull and the response is: ',response );
+       })
+       .catch(error => {
+        console.log('Oh No! Error is: ', error);  
+    })
+}
 
   updateOneRegister () {
 
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (e) {
+    axios.get(`${this.BASE_URL}/characters/`+ e)
+      .then(responseFromAPI => {
+          console.log('Response from API is: ', responseFromAPI.data);           
+    })
+    .catch(err => {
+      console.log('Error is: ', err);
+      })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,34 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    // const characters = document.getElementById("fetch-all").value;       
+    // getCharactersInfo(characters);
+    charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
+    // $('character-id').keyup(function(e){
+    //   let charId =  e.target.value;
+    //   return charId
+    // })
+    let charId = $('input[name="character-id"]').val()
+    charactersAPI.getOneRegister(charId)
     
   }
   
   document.getElementById('delete-one').onclick = function(){
+    let charId = $('input[name="character-id-delete"]').val()
+    charactersAPI.deleteOneRegister(charId)
         
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    createOneRegister () ;   
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+      
   }
 })
+
+
