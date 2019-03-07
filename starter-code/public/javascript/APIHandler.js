17f5506d94ff0a16ea6032ae27c289423f40a775
kikeanaya <luiseanayam@gmail.com>
2019-03-07 21:45:51 +0100

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,30 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = baseUrl
   }
 
   getFullList () {
+    return axios.get(this.BASE_URL + "/characters")
+    .then(response => response.data)
+  }
+
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL + "/characters/" + id)
+    .then(response => response.data)
+  }
+
+  createOneRegister (obj) {
+    axios.post(this.BASE_URL + "/characters", obj)
+    .then(response => response.data)
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister (obj) {
+    axios.put(this.BASE_URL + "/characters/" + obj.id, obj)
+    .then(response => response.data)
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + "/characters/" + id)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,6 +2,21 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    let html = ""
+    charactersAPI.getFullList()
+    .then(res => {
+      res.forEach(elm => {
+          html += 
+          `<div class="character-info">
+            <div class="id">Id: <span>${elm.id}</span></div>
+            <div class="name">Name: <span>${elm.name}</span></div>
+            <div class="occupation">Occupation: <span>${elm.occupation}</span></div>
+            <div class="cartoon">Is a Cartoon? <span>${elm.cartoon}</span></div>
+            <div class="weapon">Weapon: <span>${elm.weapon}</span></div>
+          </div>`
+        })
+      document.querySelector(".characters-container").innerHTML = html
+    })
 
   }
   
