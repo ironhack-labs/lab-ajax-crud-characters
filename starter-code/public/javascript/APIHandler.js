05e966cd96759909c00b19a6a0ee05be3d45c91e
Fedde <feddetilma@hotmail.com>
2019-04-06 15:36:24 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,76 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  // -- DISPLAY ALL -- //
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`)
+      .then(chars => {
+        $('.characters-container-main').empty();
+        chars.data.forEach(item => {
+          displayOneCharacter(item); // (function in index.js)
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
 
+  // -- DISPLAY ONE -- //
+  getOneRegister(charId) {
+    axios.get(`${this.BASE_URL}/characters/${charId}`)
+      .then(char => {
+        $('.characters-container-main').empty();
+        displayOneCharacter(char.data); // (function in index.js)
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
+  // -- CREATE ONE -- //
+  createOneRegister(characterObject) {
+    $('#send-data-create').removeClass('green-button red-button'); // reset status
+    axios.post(`${this.BASE_URL}/characters`, characterObject)
+      .then(result => { 
+        $('#send-data-create').addClass('green-button');
+      })
+      .catch(err => {
+        console.log(err);
+        $('#send-data-create').addClass('red-button');
+      })
   }
 
-  getOneRegister () {
-
+  // -- UPDATE ONE -- //
+  updateOneRegister(characterObject) {
+    $('#send-data-edit').removeClass('green-button red-button'); // reset status
+    axios.patch(`${this.BASE_URL}/characters/${characterObject.id}`, {
+      name: characterObject.name,
+      occupation: characterObject.occupation,
+      weapon: characterObject.weapon,
+      cartoon: characterObject.cartoon
+    })
+      .then(char => { 
+        $('#send-data-edit').addClass('green-button');
+      })
+      .catch(err => {
+        $('#send-data-edit').addClass('red-button');
+        console.log(err);
+      })
   }
 
-  createOneRegister () {
+  // -- DELETE ONE -- //
+  deleteOneRegister(charId) {
+    $('#delete-one').removeClass('green-button red-button'); // reset status
+    axios.delete(`${this.BASE_URL}/characters/${charId}`)
+      .then(char => {
+        $('#delete-one').addClass('green-button');
+      })
+      .catch(err => {
+        $('#delete-one').addClass('red-button');
+        console.log(err);
+      })
+  };
+};
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
-}
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,66 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  // START - dom manipulation
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    let charId = document.getElementById('fetch-one-id').value;
+    charactersAPI.getOneRegister(charId);
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+    let charId = document.getElementById('delete-one-id').value;
+    charactersAPI.deleteOneRegister(charId);
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    e.preventDefault(); 
+    let id = $("#edit-character-form .field input[name='chr-id']")[0].value;
+    let name = $("#edit-character-form .field input[name='name']")[0].value;
+    let occupation = $("#edit-character-form .field input[name='occupation']")[0].value;
+    let weapon = $("#edit-character-form .field input[name='weapon']")[0].value;
+    let cartoon = $("#edit-character-form .field input[name='cartoon']")[0].checked;
+
+    let editCharacter = {id, name, occupation, weapon, cartoon}; 
+    charactersAPI.updateOneRegister(editCharacter);
   }
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault(); 
+    let name = $("#new-character-form .field input[name='name']")[0].value;
+    let occupation = $("#new-character-form .field input[name='occupation']")[0].value;
+    let weapon = $("#new-character-form .field input[name='weapon']")[0].value;
+    let cartoon = $("#new-character-form .field input[name='cartoon']")[0].checked;
+
+    let newCharacter = {name, occupation, weapon, cartoon}; 
+    charactersAPI.createOneRegister(newCharacter);
+  }
+  // END - dom manipulation
+
   
-  document.getElementById('edit-character-form').onsubmit = function(){
+  // DISPLAY CARD WITH PROVIDED DATA //
+  displayOneCharacter = function(item) {
+    var content = `
+            <div class="characters-container">
+              <div class="character-info">
+                <div class="id">Id</div>
+                <div class="name">Name</div>
+                <div class="occupation">Occupation</div>
+                <div class="cartoon">Is a Cartoon?</div>
+                <div class="weapon">Weapon</div>
+              </div>
+              <div class="character-data">
+                <div class="id">${item.id}</div>
+                <div class="name">${item.name}</div>
+                <div class="occupation">${item.occupation}</div>
+                <div class="cartoon">${item.cartoon}</div>
+                <div class="weapon">${item.weapon}</div>
+              </div>
+            </div>`
             
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+            $('.characters-container-main').append(content);
   }
 })
