ac244ef26a191d1d4aca4b646b1f907c711676f2
Jelena Jukic <jelena.dj.pg@gmail.com>
2019-04-07 12:36:03 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,67 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
+
+  
+
+
   getFullList () {
-
+    document.getElementsByClassName('characters-container')[0].innerHTML="";
+      axios.get(`${this.BASE_URL}/characters`)
+      .then(characters=> characters.data.map(character=>{
+        document.getElementsByClassName('characters-container')[0].innerHTML+=`<div class="character-info">
+        <div class="name">Character Name: ${character.name}</div>
+        <div class="occupation">Character Occupation:${character.occupation} </div>
+        <div class="cartoon">Is a Cartoon?:${character.cartoon} </div>
+        <div class="weapon">Character Weapon:${character.weapon} </div>
+      </div>`
+      }))
+      .catch(error=>console.log(error));
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    document.getElementsByClassName('characters-container')[0].innerHTML="";
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(character=> {
+      document.getElementsByClassName('characters-container')[0].innerHTML+=`<div class="character-info">
+        <div class="name">Character Name: ${character.data.name}</div>
+        <div class="occupation">Character Occupation:${character.data.occupation} </div>
+        <div class="cartoon">Is a Cartoon?:${character.data.cartoon} </div>
+        <div class="weapon">Character Weapon:${character.data.weapon} </div>
+      </div>`
+  })
+  .catch(error=>console.log(error));
   }
 
-  createOneRegister () {
-
+  createOneRegister (ourObject) {
+    axios.post(`${this.BASE_URL}/characters/`, ourObject)
+    .then(()=> document.getElementById('create-data').style.backgroundColor="green")
+    .catch(error=>{
+      document.getElementById('create-data').style.backgroundColor="red";
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, ourObject) {
+    axios.put(`${this.BASE_URL}/characters/${id}`, ourObject )
+    // .then(character=> console.log(character))
+    .then(()=> document.getElementById('edit-data').style.backgroundColor="green")
+    .catch(error=>{
+      document.getElementById('edit-data').style.backgroundColor="red";
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(character=> console.log('Deleted char is:',character))
+    .then(()=> document.getElementById('delete-one').style.backgroundColor="green")
+    .catch(error=>{
+      document.getElementById('delete-one').style.backgroundColor="red";
+    })
   }
 }
+
+
+// connection = new APIHandler('http://localhost/');
+
+
+// connection.getFullList ();
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,78 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = function () {
+    let id = document.getElementById('charToSearch').value;
+    charactersAPI.getOneRegister(id);
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('delete-one').onclick = function () {
+    let id = document.getElementById('charToDelete').value;
+    charactersAPI.deleteOneRegister(id);
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    let id = document.getElementById('charToEdit').value;
+    let name = document.getElementsByClassName("name")[2].value;
+    let occupation = document.getElementsByClassName("occupation")[2].value;
+    let weapon = document.getElementsByClassName("weapon")[2].value;
+    let cartoon = document.getElementsByClassName("cartoon")[2].checked;
+
+    let obj = {
+      "name": name,
+      "occupation": occupation,
+      "weapon": weapon,
+      "cartoon": cartoon
+    }
+    charactersAPI.updateOneRegister(id, obj)
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function (event) {
+    event.preventDefault();
+    console.log(document.getElementsByClassName("name")[1].value);
+    let name = document.getElementsByClassName("name")[1].value;
+    let occupation = document.getElementsByClassName("occupation")[1].value;
+    let weapon = document.getElementsByClassName("weapon")[1].value;
+    let cartoon = document.getElementsByClassName("cartoon")[1].checked
+    console.log(cartoon);
+
+    let obj = {
+      "name": name,
+      "occupation": occupation,
+      "weapon": weapon,
+      "cartoon": cartoon
+    }
+    charactersAPI.createOneRegister(obj);
+
+
   }
+
+  document.getElementById("charToEdit").addEventListener('input', (event) => {
+    event.preventDefault();
+    let id = document.getElementById('charToEdit').value;
+    if (id == "") {
+      document.getElementsByClassName('name')[2].value = "";
+      document.getElementsByClassName('occupation')[2].value = "";
+      document.getElementsByClassName('weapon')[2].value = "";
+      document.getElementsByClassName('cartoon')[2].checked = false;
+    } else {
+      console.log("change");
+      axios.get(`http://localhost:8000/characters/${id}`).then((character) => {
+          console.log(character.data.name);
+          document.getElementsByClassName('name')[2].value = character.data.name;
+          document.getElementsByClassName('occupation')[2].value = character.data.occupation;
+          document.getElementsByClassName('weapon')[2].value = character.data.weapon;
+          if (character.data.cartoon)
+            document.getElementsByClassName('cartoon')[2].checked = true;
+        })
+        .catch(error => console.log(error));
+    }
+
+
+  })
 })
