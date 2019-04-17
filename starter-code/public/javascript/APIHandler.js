b6d56562538a3612152aa4bdd4825e858294b72a
Alberto González <algorinetportfolio@gmail.com>
2019-04-17 20:15:22 +0200

    [MAD-PP] Laura y Alberto

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,58 @@
+document.querySelector(".characters-container").innerHTML = "";
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios
+      .get(this.BASE_URL + "/characters")
 
+      .then(response => {
+        response.data.forEach(element => {
+          document.querySelector(
+            ".characters-container"
+          ).innerHTML += `<div class="character-info">
+        <div class="id"> Id: ${element.id}</div>
+        <div class="name"> Name: ${element.name}</div>
+        <div class="occupation"> Occupation: ${element.occupation}</div>
+        <div class="weapon"> Weapon: ${element.weapon}</div>
+        <div class="cartoon"> Cartoon: ${element.cartoon}</div>
+        </div>       `;
+        });
+      })
+      .catch(err => {});
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    let numero = document.querySelector(".numero").value;
+    axios.get(this.BASE_URL + `/characters/${numero}`).then(response => {
+      document.querySelector(
+        ".characters-container"
+      ).innerHTML += `<div class="character-info">
+      <div class="id"> Id: ${response.data.id}</div>
+      <div class="name"> Name: ${response.data.name}</div>
+      <div class="occupation"> Occupation: ${response.data.occupation}</div>
+      <div class="weapon"> Weapon: ${response.data.weapon}</div>
+      <div class="cartoon"> Cartoon: ${response.data.cartoon}</div>
+      </div>       `;
+    });
   }
 
-  createOneRegister () {
+  createOneRegister() {}
 
-  }
+  updateOneRegister() {}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    let numeroDelete = document.querySelector(".numeroDelete").value;
+   
+    axios.delete(this.BASE_URL + '/characters/' + numeroDelete).then(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,16 +1,17 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister();
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    
+    charactersAPI.deleteOneRegister();
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
