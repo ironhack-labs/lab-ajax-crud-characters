e29b8f86a9115086fed36ff6314b6bf1e780a2ff
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-17 18:37:51 +0200

    salen los personajes

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,29 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios
+      .get("http://localhost:8000/characters")
+      .then(todos => {
+         return todos.data
+         
+      })
+      .catch(error => {
+        return error;
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    return axios.get(`/characters/${id}`).then(uno => {
+      return uno;
+    });
   }
 
-  createOneRegister () {
+  createOneRegister() {}
 
-  }
+  updateOneRegister() {}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,11 +2,34 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    document.querySelector(".characters-container").innerHTML = ""
+    var todos = charactersAPI.getFullList();
+    todos
+      .then( todos =>{
 
+        todos.forEach(element => {
+          console.log(element)
+          document.querySelector(".characters-container").innerHTML += 
+          `<div class="character-info">
+          <div class="name">${element.name}</div>
+          <div class="occupation">${element.occupation}</div>
+          <div class="cartoon">${element.cartoon}</div>
+          <div class="weapon">${element.weapon}</div>
+        </div>`
+        })
+      })
+      .catch(err =>{
+        return err;
+      })
+
+      
+        
+      
+      
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    var uno=charactersAPI.getFullList()
   }
   
   document.getElementById('delete-one').onclick = function(){
