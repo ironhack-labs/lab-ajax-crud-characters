3480a5250fc2f0d19d633905d695fd98b199fd60
JlAviles <jlavilesmolina@gmail.com>
2019-04-17 20:23:38 +0200

    First function, completed

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,14 +1,29 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    
   }
 
   getFullList () {
-
+    return axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(allCharacters => {
+        return allCharacters
+      })
+      .catch(error => {
+        return "There aren't characters";
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios
+    .get(`${this.BASE_URL}/characters/:id`)
+    .then(oneCharacter => {
+      return oneCharacter
+    })
+    .catch(error => {
+      return "That isn't a character with this id";
+    })
   }
 
   createOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,20 +2,46 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
+    document.querySelector(".characters-container").innerHTML = ''
+    charactersAPI.getFullList()
+    .then(allCharacters => {
+      allCharacters.data.forEach(element => {
+        const {id, name, occupation, cartoon, weapon} = element;
+        document.querySelector(".characters-container").innerHTML += 
+        `<div class="character-info">
+        <div class="name"><b>Id:</b> ${id}</div>
+        <div class="name"><b>Name:</b> ${name}</div>
+        <div class="occupation"><b>Ocuppation:</b> ${occupation}</div>
+        <div class="cartoon"><b>Is a Cartoon?</b> ${cartoon}</div>
+        <div class="weapon"><b>Character Weapon:</b> ${weapon}</div>
+      </div>`
+    })
+  })
+    .catch(error => {
+      return "There aren't characters data"
+  })
+},
 
-  }
-  
   document.getElementById('fetch-one').onclick = function(){
-    
-  }
+    charactersAPI.getOneRegister()
+    const {id} = element;
+    document.querySelector(".characters-container").innerHTML += 
+        `<div class="character-info">
+        <div class="name"><b>Id:</b> ${id}</div>
+        <div class="name"><b>Name:</b> ${name}</div>
+        <div class="occupation"><b>Ocuppation:</b> ${occupation}</div>
+        <div class="cartoon"><b>Is a Cartoon?</b> ${cartoon}</div>
+        <div class="weapon"><b>Character Weapon:</b> ${weapon}</div>
+      </div>`
+  },
   
   document.getElementById('delete-one').onclick = function(){
         
-  }
+  },
   
   document.getElementById('edit-character-form').onsubmit = function(){
             
-  }
+  },
   
   document.getElementById('new-character-form').onsubmit = function(){
                 
