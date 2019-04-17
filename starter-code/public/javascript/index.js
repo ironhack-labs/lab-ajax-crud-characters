9a283d93e692ab2642f7b1e8c4f1d6bb52dfb3be
Francisco Vaca <fcovacarh@gmail.com>
2019-04-17 17:58:07 +0200

    Fetch one by id implemented on front end

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -3,25 +3,44 @@ const charactersList = document.querySelector("#characters-list");
 const template = document.querySelector("#character-card-template");
 
 $(document).ready(() => {
-  document.getElementById("fetch-all").onclick = function() {
+  document.getElementById("fetch-all").onclick = function(e) {
+    e.preventDefault();
     axios.get("/characters").then(characters => {
+      charactersList.innerHTML = '';
       characters.data.forEach(character => {
-        let card = template.content.cloneNode(true);
-        card.querySelector(".character-id").innerText = character._id;
-        card.querySelector(".character-name").innerText = character.name;
-        card.querySelector(".character-occupation").innerText = character.occupation;
-        card.querySelector(".character-is-cartoon").innerText = character.cartoon;
-        card.querySelector(".character-weapon").innerText = character.weapon;
+        let card = createCharacterCard(character);
         charactersList.appendChild(card);
       });
     });
   };
 
-  document.getElementById("fetch-one").onclick = function() {};
+  document.getElementById("fetch-one").onclick = function(e) {
+    e.preventDefault();
+    let characterId = document.querySelector('#search-input').value;
+    axios.get(`/characters/${characterId}`)
+    .then( character => {
+      charactersList.innerHTML = '';
+      let card = createCharacterCard(character.data);
+      charactersList.appendChild(card);
+    })
+  };
 
-  document.getElementById("delete-one").onclick = function() {};
+  document.getElementById("delete-one").onclick = function(e) {
+    e.preventDefault();
+  };
 
   document.getElementById("edit-character-form").onsubmit = function() {};
 
   document.getElementById("new-character-form").onsubmit = function() {};
 });
+
+function createCharacterCard(character) {
+  let card = template.content.cloneNode(true);
+  card.querySelector(".character-id").innerText = character._id;
+  card.querySelector(".character-name").innerText = character.name;
+  card.querySelector(".character-occupation").innerText = character.occupation;
+  card.querySelector(".character-is-cartoon").innerText = character.cartoon;
+  card.querySelector(".character-weapon").innerText = character.weapon;
+
+  return card;
+}
