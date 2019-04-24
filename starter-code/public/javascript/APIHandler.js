4951dc0b07f63e52a34ca3e7f463e4d38e5af860
Christian <christiankhawam@outlook.com>
2019-04-24 16:58:02 -0400

    initial commit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,42 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`).then(characters => {
+      return characters.data
+    })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`).then(characters => {
+      console.log(characters.data)
+    })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister(all) {
+    axios.post(`${this.BASE_URL}/characters`, all)
+      .then(characters => {
+        console.log(characters.data)
+        return characters.data
+      })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, all) {
+    axios.patch(`${this.BASE_URL}/characters/${id}`, all).then().catch(err => console.error(err))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
   }
 }
