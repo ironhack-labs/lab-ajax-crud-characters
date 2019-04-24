21fb2af0ab06bc25208d65a539fe098187181d8e
Katy Brown <thisis.katybrown@gmail.com>
2019-04-24 17:13:16 -0400

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,87 @@ class APIHandler {
   }
 
   getFullList () {
+    axios.get('http://localhost:8000/characters')
+    .then((res) => {
 
+      let characterArr = []
+    
+      res.data.forEach((character) => {
+
+        let info =
+       `
+        ${character.name}
+        ${character.weapon}
+        ${character.occupation}
+        ${character.cartoon}
+
+        `
+
+        console.log(info)
+        characterArr.push(info)
+        document.getElementById("list-of-all").innerHTML = characterArr
+      })
+    });
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    console.log('in here now ',id)
+    //http://localhost:8000/characters/3
+    axios.get(`http://localhost:8000/characters/${id}`)
+    .then(character=>{
 
+      console.log(character) 
+    
+    })
+    /*let oneCharacter = []//put that into html 
+      let info =
+       `
+        ${character.name}
+        ${character.weapon}
+        ${character.occupation}
+        ${character.cartoon}
+
+        `
+        console.log(info)
+    Onecharacter.push(info)
+    document.getElementById("one-character").innerHTML = Onecharacter
+    */
   }
 
-  createOneRegister () {
+  createOneRegister (characterInfo) {
+    axios.post("http://localhost:8000/characters/",characterInfo)
+    .then ( character => {
+      console.log(character)
+    })
+    .catch(error => {
+      console.log("The error is:", error)
+    })
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
 }
+  
+
+  updateOneRegister (editCharacterInfo) {
+    axios.patch(`http://localhost:8000/characters/${editCharacterInfo.id}`, editCharacterInfo)
+    .then (character => {
+      document.getElementById("edit-character-form").reset();
+      console.log(character.data)
+    })
+    .catch(error => {
+      console.log("The error is:", error)
+    })
+  }
+  
+
+  deleteOneRegister (deleteId) {
+     axios.delete(`http://localhost:8000/characters/${deleteId}`)
+          .then (character => {
+          })
+          .catch(error => {
+            console.log("The error is:", error)
+          })
+        
+    }
+  }
+
+  
+
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,46 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI.getFullList();
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    const id = document.querySelector("#theId").value;
+    console.log("does the id work?", id);
+    charactersAPI.getOneRegister(id);
+  };
+
+  document.getElementById("delete-one").onclick = function() {
+    const deleteId = document.querySelector("#deleteId").value;
+    console.log("does this work?", deleteId)
+        charactersAPI.deleteOneRegister(deleteId)
 
   }
   
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
+
+  document.getElementById("edit-character-form").onsubmit = function(e) {
+    e.preventDefault()
+    const editCharacterInfo = {
+      id: document.querySelector('#edit-character-form > div:nth-child(1) > input[type="text"]').value,
+      name: document.querySelector('#edit-character-form > div:nth-child(2) > input[type="text"]').value,
+      occupation: document.querySelector('#edit-character-form > div:nth-child(3) > input[type="text"]').value,
+      weapon: document.querySelector('#edit-character-form > div:nth-child(4) > input[type="text"]').value,
+      cartoon: document.querySelector('#edit-character-form > div:nth-child(5) > input[type="checkbox"]').value
+    }
+
+    charactersAPI.updateOneRegister(editCharacterInfo);
+  };
+
+  document.getElementById("new-character-form").onsubmit = function(e) {
+    e.preventDefault()
+    const characterInfo = {
+      name: document.querySelector('#new-character-form > div:nth-child(1) > input[type="text"]').value,
+      occupation:document.querySelector('#new-character-form > div:nth-child(2) > input[type="text"]').value,
+      weapon: document.querySelector('#new-character-form > div:nth-child(3) > input[type="text"]').value,
+      cartoon:document.querySelector('#new-character-form > div:nth-child(4) > input[type="checkbox"]').value
+    }
+    charactersAPI.createOneRegister(characterInfo);      
   
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  };
+});
