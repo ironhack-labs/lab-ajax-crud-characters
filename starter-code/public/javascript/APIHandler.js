14145e326356bb93f7169d080d03469a634b6884
jvelez523 <justin.velez0523@gmail.com>
2019-04-24 15:52:51 -0400

    Finished

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,65 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(characters => {
+        console.log(characters.data);
+        return characters.data
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
+  getOneRegister(id) {
+    axios
+    .get(`${this.BASE_URL}/characters/${id}`)
+    .then(character => {
+      console.log(character.data);
+      return character.data
+    })
+    .catch(err => {
+      alert(err);
+    });
 
   }
 
-  getOneRegister () {
-
+  createOneRegister(attributes) {
+    axios
+    .post(`${this.BASE_URL}/characters`, attributes)
+    .then(response => {
+      console.log('character created',response);
+      return newcharacter.data
+    })
+    .catch(err => {
+      alert(err);
+    });
   }
 
-  createOneRegister () {
+  updateOneRegister(id, attributes) {
 
+    axios.patch(`${this.BASE_URL}/characters/${id}`, attributes)
+    .then(response => {
+          console.log('update successful: ', response);
+          return response;
+    })
+    .catch(error => {
+        console.log(error);
+    })
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(response => {
+        console.log('delete successful', response);
+        return response;
+      })
+      .catch(err => 
+        console.log(err));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,36 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:3000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    var a = document.getElementsByName('character-id')[0].value
+    charactersAPI.getOneRegister(a)
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+        var a = document.querySelector('body > section:nth-child(1) > section > div.operation.delete > input[type="text"]').value
+        charactersAPI.deleteOneRegister(a)
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    var id = document.querySelector('#edit-character-form > div:nth-child(1) > input[type="text"]').value
+    var name = document.querySelector('#edit-character-form > div:nth-child(2) > input[type="text"]').value
+    var occupation = document.querySelector('#edit-character-form > div:nth-child(3) > input[type="text"]').value
+    var weapon = document.querySelector('#edit-character-form > div:nth-child(4) > input[type="text"]').value
+    var cartoon = document.querySelector('#edit-character-form > div:nth-child(5) > input[type="checkbox"]').checked
+    var attributes = {name, occupation, weapon, cartoon}
+    charactersAPI.updateOneRegister(id, attributes)
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    var name = document.querySelector('#new-character-form > div:nth-child(1) > input[type="text"]').value
+    var occupation = document.querySelector('#new-character-form > div:nth-child(2) > input[type="text"]').value
+    var weapon = document.querySelector('#new-character-form > div:nth-child(3) > input[type="text"]').value
+    var cartoon = document.querySelector('#new-character-form > div:nth-child(4) > input[type="checkbox"]').checked
+    var attributes = {name, occupation, weapon, cartoon, attributes}
+    charactersAPI.createOneRegister(attributes)
   }
 })
