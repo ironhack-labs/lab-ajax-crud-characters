c8eefd033c819be672a07d9a09ae89cc973b7cfa
saulscs <saulcoronasantos@ymail.com>
2019-04-24 18:56:00 -0500

    almost done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
-class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
 
-  getFullList () {
+const buttonFetchAll = document.getElementById('fetch-all')
+const buttonFetchOne = document.getElementById('fetch-one')
+const buttonDelete = document.getElementById('delete-one')
+const buttonSendData = document.getElementById('send-data')
+const buttonEditCharacter = document.getElementById('edit-character-form')
 
-  }
+const endpoint = 'http://localhost:8000/characters'
 
-  getOneRegister () {
 
-  }
+buttonFetchAll.onclick = () => {
+  
+  axios.get(endpoint) 
+  .then(data => {
+    console.log(data)
+  })
+  .catch (err => console.log(err))
+}
 
-  createOneRegister () {
 
-  }
+buttonFetchOne.onclick = () => {
+ id = document.getElementById('fetch-one-input').value
+ 
+ console.log(id)
+ axios.get (`${endpoint}/${id}`)
+ .then(({data}) => console.log(data))
+ .catch(err => console.log(err))
+}
 
-  updateOneRegister () {
 
-  }
+buttonDelete.onclick = () =>{
+  id= document.getElementById('delete-one-input').value
 
-  deleteOneRegister () {
+  axios.delete (`${endpoint}/${id}`)
+}
 
-  }
+
+buttonSendData.onclick = () => {
+  id = document.getElementById('send-data').value
+  
+  name = document.querySelector('input[name="name"]').value
+  occupation = document.querySelector('input[name="occupation"]').value
+  cartoon = document.querySelector('input[name="cartoon"]').value
+  weapon = document.querySelector('input[name="weapon"]').value
+  
+  axios
+  .post(`${endpoint}`, {name,occupation,cartoon,weapon})
+  .then(({data})=> console.log(data))
+  .catch(err => console.log(err))
+}
+
+
+buttonEditCharacter.onsubmit = (e) =>{
+  e.preventDefault()
+  id = document.getElementById('chr-id-edit').value
+  console.log(id)
+  name = document.getElementById('name-edit').value
+  occupation = document.getElementById('occupation-edit').value
+  cartoon = document.getElementById('cartoon-edit').value
+  weapon = document.getElementById('weapon-edit').value
+  console.log(id,name,occupation,cartoon,weapon)
+  axios 
+  .patch (`${endpoint}/${id}`, {name,occupation,cartoon,weapon} )
+  .then(({data}) => console.log(data))
+  .catch(err => console.log(err))
+}
+
+buttonDelete.onclick =()=>{
+  alert('sí sirve el botón')
+  id= document.getElementById('delete-one-input').value
+  console.log(id)
+  axios
+  .delete (`${endpoint}/${id}`)
+  
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,27 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+// const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
-
-  }
+// $(document).ready( () => {
+//   document.getElementById('fetch-all').onclick = function(){
+//     axios.get(charactersAPI) 
+//     .then(data => {
+//       console.log(data)
+//     })
+//     .catch (err => console.log(err))
+//   }
   
-  document.getElementById('fetch-one').onclick = function(){
+//   document.getElementById('fetch-one').onclick = function(){
     
-  }
+//   }
   
-  document.getElementById('delete-one').onclick = function(){
+//   document.getElementById('delete-one').onclick = function(){
         
-  }
+//   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
+//   document.getElementById('edit-character-form').onsubmit = function(){
             
-  }
+//   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
+//   document.getElementById('new-character-form').onsubmit = function(){
                 
-  }
-})
+//   }
+// })
