2310f6737a52bd1b333307450aaef9dc6eaa0ac5
Chris Cardelle <chris.cardelle@gmail.com>
2019-04-24 14:59:50 -0400

    Structure and fetch all added

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,15 +1,75 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    
   }
-
+  
   getFullList () {
+    const fetchCharacters = axios.create({
+      baseURL: this.BASE_URL
+  });
+    fetchCharacters.get(`/characters`)
+    .then(responseFromAPI => {
+      let i = 0;
+      responseFromAPI.data.forEach( () => {
+        let charId = responseFromAPI.data[i].id;
+        let charName = responseFromAPI.data[i].name;
+        let charOcc = responseFromAPI.data[i].occupation;
+        let charWep = responseFromAPI.data[i].weapon;
+        let charCartoon = responseFromAPI.data[i].cartoon;
 
-  }
 
-  getOneRegister () {
+        console.log(`Character Information: 
+        ID: ${charId} 
+        Name: ${charName} 
+        Occupation: ${charOcc} 
+        Weapon: ${charWep} 
+        Cartoon: ${charCartoon}`);
+        
+        i++;
+      });
+    }
+  )};
 
-  }
+  getOneRegister (requestId) {
+    
+    const fetchCharacters = axios.create({
+      baseURL: this.BASE_URL
+  });
+    fetchCharacters.get(`/characters`)
+    
+    
+    .then(responseFromAPI => {
+      let i = 0;
+      responseFromAPI.data.forEach( () => {
+        if (responseFromAPI.data[i].id == requestId) {
+
+          let charId = responseFromAPI.data[i].id;
+          let charName = responseFromAPI.data[i].name;
+          let charOcc = responseFromAPI.data[i].occupation;
+          let charWep = responseFromAPI.data[i].weapon;
+          let charCartoon = responseFromAPI.data[i].cartoon;
+          
+
+          console.log(`Charater Information: 
+          ID: ${charId} 
+          Name: ${charName} 
+          Occupation: ${charOcc} 
+          Weapon: ${charWep} 
+          Cartoon: ${charCartoon}`);
+
+          i++;
+
+        } else {
+          
+          
+        i++;
+
+        }
+      });
+    }
+  )};
+  
 
   createOneRegister () {
 
@@ -19,7 +79,12 @@ class APIHandler {
 
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (requestId) {
+    const fetchCharacters = axios.create({
+      baseURL: this.BASE_URL
+  });
+    fetchCharacters.delete(`/characters/${requestId}`)
+    
+     
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,15 +2,17 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+      let requestedId = document.getElementsByName('character-id')[0].value;
+      charactersAPI.getOneRegister(requestedId);
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+      let requestedId = document.getElementsByName('character-id-delete')[0].value;
+      charactersAPI.deleteOneRegister(requestedId);
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
