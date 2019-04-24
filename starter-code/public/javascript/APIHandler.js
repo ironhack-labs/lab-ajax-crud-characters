cacde63788984225975a0308c9a9a9921af54c18
Daniela Olano <danolan@Danielas-MacBook-Pro-2.local>
2019-04-24 15:37:14 -0400

    commit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,62 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get('http://localhost:8000/characters').then(characters=>{ 
+      console.log(characters.data) 
+    }).catch(err=>{
+      alert(err)
+    })
   }
-
-  getOneRegister () {
-
+  getOneRegister() {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(character => {
+      console.log(character.data);
+    })
+    .then(err => {
+      console.log(err);
+    });
   }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  createOneCharacter (characterInfo) {
+      // const characterInfo = {
+      //   name:       'Dan',
+      //   occupation: 'fdgdg',
+      //   weapon:     'dpgjpfgj',
+      //   cartoon: 'no'
+      //  };
+    axios.post('http://localhost:8000/characters',characterInfo)
+    .then(response => {
+         const { name, occupation, cartoon, weapon } = response.data;
+         const newCharacterHtml = 
+            `<div class="name"> ${name}</div>
+            <div class="occupation">${occupation}</div>
+            <div class="cartoon">${cartoon}</div>
+            <div class="weapon">${weapon}</div>`;
+        document.getElementsByClassName("character-info").innerHTML += newCharacterHtml;
+        console.log('post successful and the response is: ',response );
+    })
+    .catch(error => {
+        console.log('Oh No! Error is: ', error);  
+    })
 }
+
+  updateOneRegister (id, characterInfo) {
+    axios
+      .patch(this.BASE_URL + "/characters" + "/" + id, characterInfo)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log("The error is: ", error);
+      });
+  }
+
+  deleteOneRegister (id) {
+      axios
+        .delete(this.BASE_URL + "/characters" + "/" + id)
+        .then(character => {
+          console.log(character.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+  }}
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,56 @@
+
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+    charactersAPI.getFullList()
   }
 })
+  
+  document.getElementById('fetch-one').onclick = function(){
+      let value = document.querySelector('body > section:nth-child(1) > section > div:nth-child(2) > input[type="text"]').value;
+      charactersAPI.getOneRegister(value);
+    };
+  
+  
+  document.getElementById('delete-one').onclick = function(){
+    let id = document.querySelector(
+      'body > section:nth-child(1) > section > div.operation.delete > input[type="text"]'
+    ).value;
+    charactersAPI.deleteOneRegister(id);
+  };
+  
+  document.getElementById('edit-character-form').onsubmit = function(e){
+      e.preventDefault();
+      let id = document.querySelector(
+        '#edit-character-form > div:nth-child(1) > input[type="text"]'
+      ).value;
+      console.log(id);
+      let name = document.querySelector(
+        '#edit-character-form > div:nth-child(2) > input[type="text"]'
+      ).value;
+      let occupation = document.querySelector(
+        '#edit-character-form > div:nth-child(3) > input[type="text"]'
+      ).value;
+      let weapon = document.querySelector(
+        '#edit-character-form > div:nth-child(4) > input[type="text"]'
+      ).value;
+      let cartoon = document.querySelector(
+        '#edit-character-form > div:nth-child(5) > input[type="checkbox"]'
+      ).checked;
+      let characterInfo = {
+        name,
+        occupation,
+        weapon,
+        cartoon
+      };
+      if (id) charactersAPI.updateOneRegister(id, characterInfo);
+    };
+    
+
+  
+  document.getElementById('new-character-form').onsubmit = function(e){
+        e.preventDefault();
+        charactersAPI.createOneCharacter()
+  }
+
