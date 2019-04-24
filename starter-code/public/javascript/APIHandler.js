c0564fd6fc95dca5f3315e1abda06fd3afbc6f57
“Anghy <anghyf27@hotmail.com>
2019-04-24 15:43:02 -0400

    initial commit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,65 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+
   }
 
   getFullList () {
+    axios.get(`${this.BASE_URL}/characters`)
+        .then(character => {
+          console.log(character.data)
+          // return characters.data
+        })
+        .catch(error => {
+          console.log("The error is:", error)
+        })
+  }
+
+
+
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+        .then( character => {
+          console.log(character.data)
+          //  return character.data
+        })
+        .catch(error => {
+          console.log("The error is:", error)
+        })
 
   }
 
-  getOneRegister () {
+  createOneRegister (characterInfo) {
+    axios.post(`${this.BASE_URL}/characters`, characterInfo)
+        .then ( character => {
+          console.log(character.data)
+          return character.data
+        })
+        .catch(error => {
+          console.log("The error is:", error)
+        })
 
   }
 
-  createOneRegister () {
-
+  updateOneRegister (editCharacterInfo) {
+    axios.patch(`${this.BASE_URL}/characters/${editCharacterInfo.id}`, editCharacterInfo)
+        .then (character => {
+          document.getElementById("edit-character-form").reset();
+          return character.data
+        })
+        .catch(error => {
+          console.log("The error is:", error)
+        })
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister (deleteId) {
+    axios.delete(`${this.BASE_URL}/characters/${deleteId}`)
+        .then (character => {
+          return character.data
+        })
+        .catch(error => {
+          console.log("The error is:", error)
+        })
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,41 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList();
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const id= document.querySelector('#theInput').value;
+    charactersAPI.getOneRegister(id)
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+        const deleteId = document.querySelector('body > section:nth-child(1) > section > div.operation.delete > input[type="text"]').value;
+        charactersAPI.deleteOneRegister(deleteId)
+
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
-            
+    
+    const editCharacterInfo = {
+      id: document.querySelector('#edit-character-form > div:nth-child(1) > input[type="text"]').value,
+      name: document.querySelector('#edit-character-form > div:nth-child(2) > input[type="text"]').value,
+      occupation: document.querySelector('#edit-character-form > div:nth-child(3) > input[type="text"]').value,
+      weapon: document.querySelector('#edit-character-form > div:nth-child(4) > input[type="text"]').value,
+      cartoon: document.querySelector('#edit-character-form > div:nth-child(5) > input[type="checkbox"]').value
+    }
+
+    charactersAPI.updateOneRegister(editCharacterInfo);
+          
   }
   
   document.getElementById('new-character-form').onsubmit = function(){
-                
+    const characterInfo = {
+      name: document.querySelector('#new-character-form > div:nth-child(1) > input[type="text"]').value,
+      occupation:document.querySelector('#new-character-form > div:nth-child(2) > input[type="text"]').value,
+      weapon: document.querySelector('#new-character-form > div:nth-child(3) > input[type="text"]').value,
+      cartoon:document.querySelector('#new-character-form > div:nth-child(4) > input[type="checkbox"]').value
+    }
+    charactersAPI.createOneRegister(characterInfo);      
   }
 })
