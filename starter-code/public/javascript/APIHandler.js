4440fe8f6f63187bb6a8bfdf71dacb06dc953a18
Jairo Garcia <jgarcia@indratek.com>
2019-04-25 09:31:41 -0500

    2 Iteration

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,8 +4,8 @@ class APIHandler {
   }
 
   getFullList () {
-
-  }
+    
+}
 
   getOneRegister () {
 
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,60 @@
+const buttonFetchAll = document.getElementById('fetch-all')
+const buttonFetchOne = document.getElementById('fetch-one')
+const buttonDelete = document.getElementById('delete-one')
+const buttonEdit = document.getElementById('send-data-edit')
+const buttonNew = document.getElementById('send-data-create')
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
+buttonFetchAll.onclick = () => {
+  axios
+  .get(charactersAPI.BASE_URL + '/characters')
+  .then(({ data }) => {
+    console.log(data)
+  })
+  .catch(err => console.log(err))
+}
+
+buttonFetchOne.onclick = () => {
+  id = document.getElementById('character-id').value
+  axios
+  .get(charactersAPI.BASE_URL + `/characters/${id}`)
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err))
+}
+
+buttonDelete.onclick = () => {
+  id = document.getElementById('character-id-delete').value
+  axios
+  .delete(charactersAPI.BASE_URL + `/characters/${id}`)
+  .then(({ data }) => console.log(data))
+  .catch(err => console.log(err))   
+}
+
+buttonNew.onclick = () => {
+  id = document.getElementById('new-character-form').value
+  name = document.querySelector('input[name="name"]').value
+  occupation = document.querySelector('input[name="occupation"]').value
+  weapon = document.querySelector('input[name="weapon"]').value
+  cartoon = document.querySelector('input[name="cartoon"]').checked
+  axios
+  .post(charactersAPI.BASE_URL + `/characters/`, {name, occupation, weapon })
+  .then(({ data }) => {
+    console.log(data)
+  })
+      .catch(err => console.log(err))
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+buttonEdit.onclick = () => {
+  id = document.getElementById('edit-one-input').value
+  name = document.querySelector('input[name="name-edit"]').value
+  occupation = document.querySelector('input[name="occupation-edit"]').value
+  weapon = document.querySelector('input[name="weapon-edit"]').value
+  cartoon = document.querySelector('input[name="cartoon-edit"]').checked
+  axios
+  .patch(charactersAPI.BASE_URL + `/characters/`, { name, occupation, weapon })
+  .then(({ data }) => {
+    console.log(data)
+  })
+      .catch(err => console.log(err))
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
