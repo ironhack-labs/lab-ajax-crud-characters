44da7edeba10fa07a69a68497016262557e7d25a
YeahGab <gabba.prado@gmail.com>
2019-04-24 20:23:37 -0500

    2a iteración

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,39 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+    getAllRegiter() {
+      return axios
+      .get(this.BASE_URL)
+      .then(response => response.data)
+      .catch(err => console.log(err))
+      
+    }
 
+    getOneRegister(id) {
+      const endpoint = `${this.BASE_URL}/${id}`
+      return axios
+      .get(endpoint)
+      .then(response => response.data)
+      .catch(err => console.log(err))
+    }
+    
+    createOneRegister(data) {
+      return axios
+      .post(`${this.BASE_URL}`, data)
+      .then(response => response.data)
+      .catch(err => console.log(err))
   }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    updateOneRegister(data) {
+      return axios
+      .patch(`${this.BASE_URL}/${data.id}`, data)
+      .then(response => response.data)
+      .catch(err => console.log(err))
 }
+   deleteOneRegister(id) {
+     return axios
+     .delete(`${this.BASE_URL}/${id}`)
+     .then(response => response.data)
+     .catch(err => console.log(err))
+   }
+  }
+  
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,56 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000/characters")
+//const endpoint = "http://localhost:8000/characters"
+const buttonFetchAll = document.getElementById("fetch-all")
+const buttonFetchOne = document.getElementById("fetch-one")
+const buttonCreate = document.getElementById("new-character-form")
+const buttonEditOne = document.getElementById("edit-character-form")
+const buttonDeleteOne = document.getElementById("delete-one")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+window.onload =  () => {
+  buttonFetchAll.onclick = () => {
+      charactersAPI.getAllRegiter() 
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
 
+ buttonFetchOne.onclick = () => {
+   const id = document.querySelector('input[name="character-id"]').value
+    charactersAPI.getOneRegister(id)
+    .then(data => console.log(data))
+    .catch(err =>  console.log(err))
   }
   
-  document.getElementById('fetch-one').onclick = function(){
+  buttonCreate.onsubmit = (e) => {
+    e.preventDefault()
+    const name = document.querySelector('input[name="name"]').value
+    const occupation = document.querySelector('input[name="occupation"]').value
+    const weapon = document.querySelector('input[name="weapon"]').value
+    const cartoon = document.querySelector('input[name="cartoon"]').checked
+
+    charactersAPI.createOneRegister({name, occupation, weapon, cartoon})
+    .then(() => console.log('You created a new character'))
+    .catch(err =>  console.log(err)) 
+  }
+  
+  buttonEditOne.onsubmit = (e) => {
+    e.preventDefault()
+    const id = buttonEditOne.querySelector('input[name="chr-id"]').value
+    const name = buttonEditOne.querySelector('input[name="name"]').value
+    const occupation = buttonEditOne.querySelector('input[name="occupation"]').value
+    const weapon = buttonEditOne.querySelector('input[name="weapon"]').value
+    const cartoon = buttonEditOne.querySelector('input[name="cartoon"]').checked
     
+    charactersAPI.updateOneRegister({id, name, occupation, weapon, cartoon})
+    .then(() => console.log('You updated a character'))
+    .catch(err =>  console.log(err))
   }
+
+  buttonDeleteOne.onclick = () => {
+    const id = document.querySelector('input[name="character-id-delete"]').value
+    charactersAPI.deleteOneRegister(id)
+    .then(() => console.log("You deleted a character :("))
+    .catch(err =>  console.log(err))
+  }
+}
+
   
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
