5488b8bdc440d3df223335869138463873b01d1e
Nathaniel Carvajal <nathanielcarvajal@gmail.com>
2019-04-26 10:29:45 -0400

    complete

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
 class APIHandler {
-  constructor (baseUrl) {
+    constructor(baseUrl){
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
 
+  //Fetch ALL--------------------------------------------------------
+  getFullList() {
+    axios.get(`${this.BASE_URL}/characters`).then(characters => {
+
+      console.log(characters.data)
+      return characters.data
+
+    })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  getOneRegister () {
 
+
+  //Fetch by ID--------------------------------------------------------
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`).then(characters => {
+      document.querySelector('.name').innerHTML = `${characters.data.name}`
+      document.querySelector('.occupation').innerHTML = `${characters.data.occupation}`
+      document.querySelector('.weapon').innerHTML = `${characters.data.weapon}`
+      document.querySelector('.cartoon').innerHTML = `Cartoon: ${characters.data.cartoon}`
+    })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  createOneRegister () {
 
+
+
+  //Create a character--------------------------------------------------------
+  createOneRegister(all) {
+    axios.post(`${this.BASE_URL}/characters`, all)
+      .then(characters => {
+        console.log(characters.data)
+        return characters.data
+      })
+      .catch(error => {
+        console.log('ERRRORRRR', error)
+      })
   }
 
-  updateOneRegister () {
 
+
+  //Edit a character--------------------------------------------------------
+  updateOneRegister(id, all) {
+    axios.path(`${this.BASE_URL}/characters/${id}`, all).then().catch(err => console.error(err))
   }
 
-  deleteOneRegister () {
 
+  //Delete a character--------------------------------------------------------
+  deleteOneRegister(id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,53 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+  }
+
+
+  document.getElementById('fetch-one').onclick = function () {
+    let a = document.getElementsByName('character-id')[0].value
+    charactersAPI.getOneRegister(a);
+
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+
+
+  document.getElementById('delete-one').onclick = function () {
+    let b = document.querySelector('body > section:nth-child(1) > section > div.operation.delete > input[type="text"]').value
+    charactersAPI.deleteOneRegister(b);
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    e.preventDefault()
+    let id = document.querySelector('#edit-character-form > div:nth-child(1) > input[type="text"]').value
+    let name = document.querySelector('#edit-character-form > div:nth-child(2) > input[type="text"]').value
+    let occupation = document.querySelector('#edit-character-form > div:nth-child(3) > input[type="text"]').value
+    let weapon = document.querySelector('#edit-character-form > div:nth-child(4) > input[type="text"]').value
+    let cartoon = document.querySelector('#edit-character-form > div:nth-child(5) > input[type="checkbox"]').checked
+
+    let all = { name, occupation, weapon, cartoon }
+    charactersAPI.updateOneRegister(id, all);
+
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    e.preventDefault()
+    let name = document.querySelector('#new-character-form > div:nth-child(1) > input[type="text"]').value
+    let occupation = document.querySelector('#new-character-form > div:nth-child(2) > input[type="text"]').value
+    let weapon = document.querySelector('#new-character-form > div:nth-child(3) > input[type="text"]').value
+    let cartoon = document.querySelector('#new-character-form > div:nth-child(4) > input[type="checkbox"]').checked
+    let all = { name, occupation, weapon, cartoon }
+    charactersAPI.createOneRegister(all)
+
+
   }
 })
