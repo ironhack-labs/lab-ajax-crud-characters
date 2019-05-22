91375dfab3d7f75cc585528d4b7df24f76cfd59e
Alessio Marinoni <alessio.marinoni1@gmail.com>
2019-05-22 18:25:16 +0200

    Done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios.get("https://minions-api.herokuapp.com/characters")
+      .then(response => printAll(response.data))
+      .catch(error => console.log(error))
+  }
+
+  getOneRegister(id) {
+    axios.get(`https://minions-api.herokuapp.com/characters/${id}`)
+
+      .then(response => printOne(response.data))
+      .catch(error => console.log(error))
+  }
+
+  getTheOneToEdit(id) {
+    return axios.get(`https://minions-api.herokuapp.com/characters/${id}`)
+
+      .then(response => {
+        console.log(response.data)
+        return response.data
+      })
+      .catch(error => console.log(error))
+  }
+
+  createOneRegister(newCharacter) {
+
+    axios.post('https://minions-api.herokuapp.com/characters', newCharacter)
+      .then(response => {
+        console.log('post successful and the response is: ', response);
+      })
+      .catch(error => console.log(error))
 
   }
 
-  getOneRegister () {
+  updateOneRegister(id, updatedCharacter) {
+
+    axios.put(` https://minions-api.herokuapp.com/characters/${id}`, updatedCharacter)
+      .then(response => {
+        document.getElementById("send-update-data").style.color = "#00ff16"
+        printOne(response.data)
+      })
+      .catch(error => {
+        document.getElementById("send-update-data").style.color = "red"
+        console.log(error)
+      })
 
   }
 
-  createOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`https://minions-api.herokuapp.com/characters/${id}`)
+      .then(x => charactersAPI.getFullList())
+      .catch(error => console.log(error))
   }
+  // https://minions-api.herokuapp.com/characters/:id
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,103 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
+    const id = document.getElementById("fetch-one-input").value
+    charactersAPI.getOneRegister(id)
+  }
+
+  document.getElementById('delete-one').onclick = function () {
+    const id = document.getElementById("delete-one-input").value
+    charactersAPI.deleteOneRegister(id)
+  }
+
+
+
+
+
+
+
+  document.getElementById("edit-id").onkeyup = function () {
+
+    const idtoEdit = document.getElementById("edit-id").value
+    const editName = document.getElementById("edit-name")
+    const editOcc = document.getElementById("edit-occupation")
+    const editWeap = document.getElementById("edit-weapon")
+    const editCart = document.getElementById("edit-cartoon")
+
+    charactersAPI.getTheOneToEdit(idtoEdit)
+      .then(data => {
+        editName.value = data.name
+        editOcc.value = data.occupation
+        editWeap.value = data.weapon
+        editCart.checked = data.cartoon
+      })
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('edit-character-form').onsubmit = function (e) {
+    const idtoEdit = document.getElementById("edit-id").value
+    const editName = document.getElementById("edit-name").value
+    const editOcc = document.getElementById("edit-occupation").value
+    const editWeap = document.getElementById("edit-weapon").value
+    const editCart = document.getElementById("edit-cartoon").checked
+    const updatedCharacter = { name: editName, occupation: editOcc, weapon: editWeap, cartoon: editCart }
+
+    e.preventDefault()
+    charactersAPI.updateOneRegister(idtoEdit, updatedCharacter)
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('new-character-form').onsubmit = function (e) {
+    const newName = document.getElementById("create-name").value
+    const newOcc = document.getElementById("create-occupation").value
+    const newWeap = document.getElementById("create-weapon").value
+    const newCart = document.getElementById("create-cartoon").checked
+    const newCharacter = { name: newName, occupation: newOcc, weapon: newWeap, cartoon: newCart }
+
+    e.preventDefault()
+    charactersAPI.createOneRegister(newCharacter)
+
+    const formCreate = document.querySelectorAll("#new-character-form input")
+    formCreate.forEach(input => input.value = "")
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+
 })
+
+const container = document.getElementsByClassName("characters-container")[0]
+
+const printAll = (arg) => {
+  console.log(arg)
+  // <div class="characters-container">
+  container.innerHTML = ""
+  for (let i = 0; i < arg.length; i++) {
+    container.innerHTML += `
+    <div class="character-info">
+
+      <div class="name"> Name:&nbsp&nbsp&nbsp&nbsp ${arg[i].name}</div>
+      <div class="occupation">Occupation: &nbsp&nbsp&nbsp&nbsp${arg[i].occupation}</div>
+      <div class="cartoon">Is a Cartoon?: &nbsp&nbsp&nbsp&nbsp${arg[i].cartoon}</div>
+      <div class="weapon">Weapon: &nbsp&nbsp&nbsp&nbsp${arg[i].weapon}</div>
+
+    </div>`
+
+  }
+
+}
+
+const printOne = (arg) => {
+  console.log(arg)
+  container.innerHTML = `
+    <div class="character-info">
+
+      <div class="name"> Name:&nbsp&nbsp&nbsp&nbsp ${arg.name}</div>
+      <div class="occupation">Occupation: &nbsp&nbsp&nbsp&nbsp${arg.occupation}</div>
+      <div class="cartoon">Is a Cartoon?: &nbsp&nbsp&nbsp&nbsp${arg.cartoon}</div>
+      <div class="weapon">Weapon: &nbsp&nbsp&nbsp&nbsp${arg.weapon}</div>
+
+    </div>`
+}
