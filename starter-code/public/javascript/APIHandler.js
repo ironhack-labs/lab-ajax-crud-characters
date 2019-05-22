a86cf13996eff4b0c1b3c28cb422a0a3659a953b
letiescanciano <leticia.escanciano@gmail.com>
2019-05-22 16:09:14 +0200

    Iteration 2

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
+    this.minionAPI = axios.create({
+      baseURL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
+  getFullList() {
+    this.minionAPI.get('/characters')
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
 
   }
 
-  deleteOneRegister () {
+  getOneRegister(id) {
+    this.minionAPI.get(`/characters/${id}`)
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
+  }
 
+  createOneRegister(name, occupation, weapon, cartoon) {
+
+    this.minionAPI.post('/characters', {
+        name,
+        occupation,
+        weapon,
+        cartoon
+      })
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
+
+  }
+
+  updateOneRegister(id) {
+    this.minionAPI.put(`/characters/${id}`)
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
+  }
+
+  deleteOneRegister(id) {
+    this.minionAPI.delete(`/characters/${id}`)
+      .then(response => console.log(response.data))
+      .catch(err => console.log(err))
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = () => {
+    charactersAPI.getFullList()
+  }
 
+  document.getElementById('fetch-one').onclick = () => {
+    const id = document.getElementById('character-id').value
+    console.log(id)
+    charactersAPI.getOneRegister(id)
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = () => {
+    const id = document.getElementById('character-id-delete').value
+    console.log(id)
+    charactersAPI.deleteOneRegister(id)
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = () => {
+    e.preventDefault()
+
+    const characterData = document.querySelectorAll('#edit-character-form input')
+    console.log(characterData)
+    const name = characterData[0].value
+    const occupation = characterData[1].value
+    const weapon = characterData[2].value
+    const cartoon = characterData[3].checked
+
+    console.log(cartoon)
+    charactersAPI.updateOneRegister(name, occupation, weapon, cartoon)
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = e => {
+    e.preventDefault()
+    const characterData = document.querySelectorAll('#new-character-form input')
+    console.log(characterData)
+    const name = characterData[0].value
+    const occupation = characterData[1].value
+    const weapon = characterData[2].value
+    const cartoon = characterData[3].checked
+
+    console.log(cartoon)
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon)
+
   }
 })
