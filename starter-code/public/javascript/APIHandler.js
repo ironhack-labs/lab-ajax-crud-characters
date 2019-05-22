91c2ef7fb0b12c2f4b5191e695a8914ca284c41f
gabaldonrico <gabaldon.1617@gmail.com>
2019-05-22 21:12:58 +0200

    ok

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -2,24 +2,73 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
+  
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`)
+    .then(response => response.data)
   }
 
   getOneRegister () {
+    const characterId = document.getElementById('character-id').value
+    return axios.get(`${this.BASE_URL}/characters/${characterId}`)
+    .then(response => {
+      const { id, name, occupation, weapon } = response.data
+      createContainer(response.data)
+    })
 
   }
 
-  createOneRegister () {
+createOneRegister () {
+  const newCharacterInputs = document.querySelectorAll('#new-character-form input')
 
+  const character = {
+      name: newCharacterInputs[0].value,
+      occupation: newCharacterInputs[1].value,
+      weapon: newCharacterInputs[2].value,
+      cartoon: newCharacterInputs[3].checked,
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  console.log("esponse")
+  axios.post(`${this.BASE_URL}/characters/`, character)
+    .then(response => {
+        const { id, name, occupation, weapon } = response.data
+        createContainer(response.data)
+    })
+    .catch(error => console.log('¡ops! error:', error))
 }
+
+updateOneRegister () {
+  const updateCharacterInputs = document.querySelectorAll('#edit-character-form input')
+  console.log(updateCharacterInputs)
+  const character = {
+    id: updateCharacterInputs[0].value,
+    name: updateCharacterInputs[1].value,
+    occupation: updateCharacterInputs[2].value,
+    weapon: updateCharacterInputs[3].value,
+    cartoon: updateCharacterInputs[4].checked,
+}
+    axios.put(`${this.BASE_URL}/characters/${character.id}`,character)
+    .then(response => {
+      const { id, name, occupation, weapon } = response.data
+      createContainer(response.data)
+  })
+
+}
+
+deleteOneRegister () {
+
+  const deleteCharacterId = document.getElementById('delete').value
+  console.log(deleteCharacterId)
+  return axios.delete(`${this.BASE_URL}/characters/${deleteCharacterId}` )
+  .then(response => {
+    const { id, name, occupation, weapon } = response.data
+    createContainer(response.data)
+  })
+
+}
+
+  }
+
+  
+
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,68 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com")
 
+
+const updateCharacterInputs = document.querySelectorAll('#edit-character-form input')
+
+//CREATE CONTAINER
+
+const createContainer = (arg)=>{
+  const charactersContainer = document.getElementsByClassName('characters-container')[0]
+  charactersContainer.innerHTML= ``
+      const ChildCharactersContainer1 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer1).innerHTML = `Name:${arg.name}`
+      
+      const ChildCharactersContainer2 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer2).innerHTML = `Occupation:${arg.occupation}`
+      
+      const ChildCharactersContainer3 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer3).innerHTML = `Weapon:${arg.weapon}`
+}
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
-
+  //DELETE CHARACTER FORM
+  document.getElementById('fetch-all').onclick = e =>{
+    charactersAPI.getFullList()
+    .then(allCharacters => {
+      const charactersContainer = document.getElementsByClassName('characters-container')[0]
+      charactersContainer.innerHTML= ``
+      allCharacters.forEach(elm => {
+        const ChildCharactersContainer1 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer1).innerHTML = `Name:${elm.name}`
+      
+      const ChildCharactersContainer2 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer2).innerHTML = `Occupation:${elm.occupation}`
+      
+      const ChildCharactersContainer3 = document.createElement("div")
+      charactersContainer.appendChild(ChildCharactersContainer3).innerHTML = `Weapon:${elm.weapon}`
+      
+      })
+    })
+    .catch(error => console.log('¡ops! error:', error))
   }
+  //SEARCH CHARACTER FORM
   
   document.getElementById('fetch-one').onclick = function(){
+    charactersAPI.getOneRegister()
     
   }
+
+  //DELETE CHARACTER FORM
   
   document.getElementById('delete-one').onclick = function(){
-        
+    charactersAPI.deleteOneRegister () 
   }
+
+  //EDIT CHARACTER FORM
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(event){
+    event.preventDefault()
+    charactersAPI.updateOneRegister()
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  //NEW CHARACTER FORM
+
+  document.getElementById('send-data').onclick = function(event){
+    event.preventDefault()
+    charactersAPI.createOneRegister()
   }
 })
+
