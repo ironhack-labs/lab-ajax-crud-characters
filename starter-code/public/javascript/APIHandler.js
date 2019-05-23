1d43ad3a3f574134281a3495f109ecf897b03dbd
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-23 09:03:37 +0200

    IH characters

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,66 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.name = document.getElementById('name')
+    this.occupation = document.getElementById('occupation')
+    this.weapon = document.getElementById('weapon')
+    this.cartoon = document.getElementById('cartoon')
+    this.charactersDiv = document.getElementById('characters')
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios.get(this.BASE_URL)
+    .then(response => {
+      response.data.forEach(element => {
+        const listPoint = `<li>Personaje ${element.name} con ID ${element.id}, trabaja como ${element.occupation}, su arma es ${element.weapon} y es cartoon: ${element.cartoon}</li>`
+        this.charactersDiv.innerHTML += listPoint
+      });
+    })
+    .catch(error => console.log('¡ops! error:', error)) 
   }
 
-  getOneRegister () {
-
+  clearAll() {
+    this.charactersDiv.innerHTML = ""
   }
 
-  createOneRegister () {
+  getOneRegister(searchCharacterID) {
 
+    axios.get(`${this.BASE_URL}/${searchCharacterID}`)
+    .then(response => {
+      console.log(response.data)
+        const { id, name, occupation, weapon, cartoon } = response.data
+        this.name.innerHTML = name
+        this.occupation.innerHTML = occupation
+        this.weapon.innerHTML = weapon
+        this.cartoon.innerHTML = cartoon
+    })
+    .catch(error => console.log('¡ops! error:', error))
   }
 
-  updateOneRegister () {
-
+  createOneRegister(character) {
+    axios.post(this.BASE_URL, character)
+    .then(response => {
+        const { id, name, occupation, weapon, cartoon } = response.data
+        console.log("Created:", id, name, occupation, weapon, cartoon)
+    })
+    .catch(error => console.log('¡ops! error:', error))
   }
 
-  deleteOneRegister () {
+  updateOneRegister(updatedCharacter) {
+    axios.put(`${this.BASE_URL}/${updatedCharacter.id}`, updatedCharacter)
+        .then(response => {
+            const { id, name, occupation, weapon, cartoon } = response.data
+            console.log("Updated:", id, name, occupation, weapon, cartoon)
+         })
+        .catch(error => console.log('¡ops! error:', error))
+  }
 
+  deleteOneRegister(deleteCharacterID) {
+    axios.delete(`${this.BASE_URL}/${deleteCharacterID}`)
+    .then(response => {
+        const { id, name, occupation, weapon, cartoon } = response.data
+        console.log("Borrado:", id, name, occupation, weapon, cartoon)
+     })
+    .catch(error => console.log('¡ops! error:', error))
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com/characters")
+const newCharacterInputs = document.querySelectorAll('#new-character-form input')
+const editCharacterInputs = document.querySelectorAll('#edit-character-form input')
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
+  }
+  document.getElementById('clear').onclick = function(){
+    charactersAPI.clearAll()    
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const searchCharacterID = document.getElementById('search-id').value
+    charactersAPI.getOneRegister(searchCharacterID)
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const deleteCharacterID = document.getElementById('delete-id').value
+    charactersAPI.deleteOneRegister(deleteCharacterID)
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = e => {
+    e.preventDefault()
+
+    const updatedCharacter = {
+      id: editCharacterInputs[0].value,
+      name: editCharacterInputs[1].value,
+      occupation: editCharacterInputs[2].value,
+      weapon: editCharacterInputs[3].value,
+      cartoon: editCharacterInputs[3].checked
+    }
+    charactersAPI.updateOneRegister(updatedCharacter)
+         
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = e => {
+    e.preventDefault()
+    const character = {
+        name: newCharacterInputs[0].value,
+        occupation: newCharacterInputs[1].value,
+        weapon: newCharacterInputs[2].value,
+        cartoon: newCharacterInputs[3].checked
+    }
+    charactersAPI.createOneRegister(character)   
   }
 })
