7f3227e2a270587c41d8e6ecdf8186d519a860c4
Teo <teodorolg130@gmail.com>
2019-05-23 10:12:39 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,3 +1,4 @@
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
@@ -5,21 +6,45 @@ class APIHandler {
 
   getFullList () {
 
+
+ return axios.get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+
+          console.log('Response from API is: ', responseFromAPI.data)
+          
+          return responseFromAPI.data
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+            .then( responseFromAPI => {
+              console.log(`${this.BASE_URL}/characters/${id}`)
+              console.log(responseFromAPI.data)
+              return responseFromAPI.data
+            })
   }
 
-  createOneRegister () {
-
+  createOneRegister (character) {
+    return axios.post(`${this.BASE_URL}/characters`, character)
+              .then( responseFromAPI => responseFromAPI.data)
   }
 
-  updateOneRegister () {
+  updateOneRegister (id, character) {
 
+    return axios.put(`${this.BASE_URL}/characters/${id}`, character)
+    .then( responseFromAPI => {
+      return responseFromAPI.data
+    })
+    
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`, id)
+    .then( responseFromAPI => {
+      return responseFromAPI.data
+    })
+    
+    
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,84 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com')
+
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+  let container = document.getElementsByClassName("characters-container")[0]
+  function replaceContainer(extraDiv, name, occupation, cartoon, weapon) {
+    const div = `<div>${extraDiv}</div></div><div class="character-info"><div class="name">Character Name: ${name}</div><div class="occupation">Character Occupation: ${occupation}</div><div class="cartoon">Is a Cartoon? ${cartoon}</div><div class="weapon">Character Weapon: ${weapon}</div></div>`      
+    container.innerHTML += div
+  }
 
+
+
+  document.getElementById('fetch-all').onclick = function(){
+    charactersAPI.getFullList()
+      .then( fullList => {
+        container.innerHTML = ""
+        fullList.forEach(elm => {          
+         replaceContainer("",elm.name, elm.occupation, elm.cartoon, elm.weapon)
+        })
+      })
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    const id = document.getElementsByName("character-id")[0]
+    charactersAPI.getOneRegister(id.value)
+    .then( elm => {
+      container.innerHTML = ""
+      replaceContainer("", elm.name, elm.occupation, elm.cartoon, elm.weapon)
+    } )
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    const id = document.getElementsByName("character-id-delete")[0]
+    
+    setInterval( () => {
+      let nuevoID = 300
+      charactersAPI.deleteOneRegister(nuevoID)
+      .then(elm => {
+        container.innerHTML = ""
+        replaceContainer("Deleted Minion",elm.name, elm.occupation, elm.cartoon, elm.weapon)})
+      
+      nuevoID++
+      
+      }, 10)
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(event){
+    event.preventDefault()
+    const newCharacterInputs = document.querySelectorAll('#edit-character-form input')
+    const id = newCharacterInputs[0].value
+    const character = {
+      name: newCharacterInputs[1].value,
+      occupation: newCharacterInputs[2].value,
+      weapon: newCharacterInputs[3].value,
+      cartoon: newCharacterInputs[4].checked,
+    }
+
+    charactersAPI.updateOneRegister(id, character)
+      .then(elm => {
+      container.innerHTML = ""
+      replaceContainer("Edited Minion",elm.name, elm.occupation, elm.cartoon, elm.weapon)
+
+      })
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(event){
+    event.preventDefault()
+    const newCharacterInputs = document.querySelectorAll('#new-character-form input')
+    
+    
+    const character = {
+      name: newCharacterInputs[0].value,
+      occupation: newCharacterInputs[1].value,
+      weapon: newCharacterInputs[2].value,
+      cartoon: newCharacterInputs[3].checked
+    }
+    
+    charactersAPI.createOneRegister(character)
+    .then(elm => {
+      container.innerHTML = ""
+      replaceContainer("New Minion",elm.name, elm.occupation, elm.cartoon, elm.weapon)      
+    })
   }
 })
