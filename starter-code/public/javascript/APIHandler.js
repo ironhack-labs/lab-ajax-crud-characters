67542604f431da35cdd50b1ada0a8e69445dd2b2
“Anthony <anthonym.gs@gmail.com>
2019-05-23 16:11:35 +0200

    half

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,44 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
 
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(response => response.data)
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
 
+    axios.get(`${this.BASE_URL}/characters` + id)
+      .then(response => response.data)
+      .catch(error => console.log(error))
   }
 
-  createOneRegister () {
 
+  createOneRegister(name, occupation, cartoon, weapon) {
+
+    const character = { name, occupation, cartoon, weapon }
+
+    axios.post(`${this.BASE_URL}/characters`, character)
+      .then(response => response.data)
+      .catch(error => console.log(error))
   }
 
-  updateOneRegister () {
+  updateOneRegister(id, name, occupation, cartoon, weapon) {
 
+    const character = { name, occupation, cartoon, weapon }
+
+    axios.put/* NO PATCH :D */(`${this.BASE_URL}/characters` + id, character)
+      .then(response => response.data)
+      .catch(error => "No character with that id#")
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(id) {
 
+    axios.delete(`${this.BASE_URL}/characters` + id)
+      .then(response => "Character destroyed!")
+      .catch(error => "No character with that id#")
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,106 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+
+    const container = document.querySelector('.characters-container')
+    const charID = document.querySelector('#character-id')
+
+    charactersAPI.getFullList()
+      .then(allCharacters => {
+        container.innerHTML = ""
+
+        allCharacters.map(character => {
+
+          const createDiv = document.createElement('createDiv')
+
+          createDiv.setAttribute('class', 'character-info')
+          for (const key in character) {
+            const createSmallDiv = document.createElement('createDiv')
+
+            createSmallDiv.setAttribute("class", `${key}`)
+            createSmallDiv.innerHTML = `<p>${key}: ${character[key]}</p>`
+            div.appendChild(createSmallDiv)
+          }
+          container.appendChild(createDiv)
+
+
+
+
+          // if (object.hasOwnProperty(key)) {
+          //   const element = object[key];
+
+          // }
+        })
+      })
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
+
+    charactersAPI.getOneRegister(characterId.value)
+      .then(character => {
+
+        if (character) {
+          container.innerHTML = ""
+          const createDiv = document.createElement("createDiv")
+          div.setAttribute("class", "character-info")
+          for (let key in character) {
+
+
+            const createSmallDiv = document.createElement("createDiv")
+            createSmallDiv.setAttribute("class", `${key}`)
+            createSmallDiv.innerHTML = `<p>${key}: <p class="color">${character[key]}</p> </p>`
+            div.appendChild(createSmallDiv)
+            document.getElementById('fetch-one').style.backgroundColor = "green"
+            setTimeout(() => {
+              document.getElementById('fetch-one').style.backgroundColor = ""
+              characterId.value = ""
+            }, 500)
+
+
+          }
+          container.appendChild(div)
+
+        } else {
+          container.innerHTML = ""
+
+          const createDiv = document.createElement("createDiv")
+          createSmallDiv.setAttribute("class", "character-info")
+
+          const createSmallDiv = document.createElement("createDiv")
+          createSmallDiv.innerText = "No character with selected #id"
+          createSmallDiv.appendChild(createSmallDiv)
+          container.appendChild(createSmallDiv)
+          document.getElementById('fetch-one').style.backgroundColor = "red"
+          setTimeout(() => {
+            document.getElementById('fetch-one').style.backgroundColor = ""
+            characterId.value = ""
+          }, 500)
+        }
+      })
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
+
+
+
+
+
+
+
+
+
+
+// const delCharID = document.querySelector(#character - id)
