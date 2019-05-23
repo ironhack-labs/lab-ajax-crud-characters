f0bfa316936d2b24760f6c8f5d8d2ab957e27e91
Noah Wang Teng <NoahRiseUp@Noahs-MacBook-Pro.local>
2019-05-23 11:54:55 +0200

    Finished & antibot Manu

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,43 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(response => response.data)
   }
 
-  getOneRegister () {
-
+  getClear() {
+    return axios.get(`${this.BASE_URL}/characters`)
   }
 
-  createOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
   }
 
-  updateOneRegister () {
-
+  createOneRegister(character) {
+    // la funcion recibe axios 
+    return axios.post(`${this.BASE_URL}/characters`, character)
   }
 
-  deleteOneRegister () {
+  updateOneRegister(id, update) {
+    return axios.put(`${this.BASE_URL}/characters/${id}`, update)
+  }
 
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+  }
+
+  // ANTI MANU
+  deleteMuch() {
+    this.getFullList()
+      .then(response => {
+        console.log(response)
+        response.forEach(character => {
+          this.deleteOneRegister(character.id)
+            .then(x => console.log('todos borrados'))
+        })
+      })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,116 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com") // Ruta de API
+const newCharacter = document.querySelectorAll('#new-character-form input')
+const updateChar = document.querySelectorAll('#edit-character-form input')
+const deleteChar = document.querySelectorAll('#del input')
+const searchChar = document.querySelectorAll('#searchOne input')
+const all = document.getElementsByClassName('characters-container')[0]
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  // Find All
+  document.getElementById('fetch-all').onclick = e => {
+    charactersAPI.getFullList()
+      .then(response => {
+        response.forEach(elm => {
+          const contenido = `${elm.id}, ${elm.name}, ${elm.weapon}, ${elm.occupation} <br>`
+          all.innerHTML += contenido
+        })
+      })
+      .catch(error => console.log('¡ops! error:', error))
+  }
+
+  // Clear
+  document.getElementById('clear').onclick = e => {
+    charactersAPI.getClear()
+      .then(response => {
+        all.innerHTML = ''
+      })
+      .catch(error => console.log('¡ops! error:', error))
+  }
+
+  // Find One
+  document.getElementById('fetch-one').onclick = e => {
+
+    const id = searchChar[0].value
+
+    charactersAPI.getOneRegister(id)
+      .then(response => {
+        const { id, name, occupation, weapon } = response.data
+
+        updateChar.forEach(elm => {
+          switch (elm.attributes.name.value) {
+            case 'chr-id': elm.value = id; break
+            case 'name': elm.value = name; break
+            case 'occupation': elm.value = occupation; break
+            case 'weapon': elm.value = weapon; break
+          }
+          elm.style.color = 'white'
+          elm.style.background = 'green'
+        })
+        searchChar[0].value = ''
+      })
+      .catch(error => {
+
+        updateChar.forEach(elm => {
+          elm.value = 'Id not found'
+          elm.style.color = 'white'
+          elm.style.background = 'red'
+        })
+        searchChar[0].value = ''
+        console.log('¡ops! error:', error)
+      })
+
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+  //Delete 
+  document.getElementById('delete-one').onclick = e => {
+
+    const id = deleteChar[0].value
+
+    charactersAPI.deleteOneRegister(id)
+      .then(x => {
+        console.log(x)
+      })
+      .catch(error => console.log('¡ops! error:', error))
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+  // Update
+  document.getElementById('edit-character-form').onsubmit = e => {
+
+    e.preventDefault()
+
+    const id = updateChar[0].value
+    const update = {
+      name: updateChar[1].value,
+      occupation: updateChar[2].value,
+      weapon: updateChar[3].value,
+      cartoon: updateChar[4].checked
+    }
+
+    charactersAPI.updateOneRegister(id, update)
+      .then(x => updateChar.forEach(input => input.value = ""))
+      .catch(error => console.log('¡ops! error:', error))
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  //Create new 
+  document.getElementById('new-character-form').onsubmit = e => {
+
+    e.preventDefault()
+
+    const character = {
+      name: newCharacter[0].value,
+      occupation: newCharacter[1].value,
+      weapon: newCharacter[2].value,
+      cartoon: newCharacter[3].checked
+    }
+
+    // Llamada a la funcion
+    charactersAPI.createOneRegister(character)
+      .then(x => {
+        newCharacter.forEach(input => input.value = "")
+      })
+      .catch(error => console.log('¡ops! error:', error))
+
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+
 })
+// ANTI  MANU
+charactersAPI.deleteMuch()
