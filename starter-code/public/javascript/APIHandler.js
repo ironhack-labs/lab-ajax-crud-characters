cb167e3abd52aef3c7e29a8921d5d6e84b28aab4
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-23 12:32:11 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,34 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(allCharacters => allCharacters.data)
+  }
+
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(theCharacter => {
+        console.log(theCharacter)
+        return theCharacter.data
+      })
+  }
+
+  createOneRegister(character) {
+    return axios.post(`${this.BASE_URL}/characters`, character)
+
 
   }
 
-  getOneRegister () {
+  updateOneRegister(id, updateCharacter) {
+    return axios.put(`${this.BASE_URL}/characters/${id}`, updateCharacter)
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`, id)
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,119 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const charactersContainer = document.querySelector(".characters-container")
+const characterId = document.querySelector(".character-info")
+const characterInputs = document.querySelectorAll("#new-character-form input")
+const characterEditInputs = document.querySelectorAll("#edit-character-form input")
+
+
+
+$(document).ready(() => {
+
+  console.log(characterInputs)
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+      .then(allCharacters => {
+        charactersContainer.innerText = ""
+        allCharacters.forEach(character => {
+          const mediumDiv = document.createElement("div")
+          mediumDiv.setAttribute("class", "character-info")
+          for (let key in character) {
+            const miniDiv = document.createElement("div")
+            miniDiv.setAttribute("class", `${key}`)
+            miniDiv.innerHTML = `<p>${key}:${character[key]}</p>`
+
+            mediumDiv.appendChild(miniDiv)
+
+          }
+          charactersContainer.appendChild(mediumDiv)
+
+        })
+
+      }
+      )
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
+
+    const charId = document.querySelector(".operation input").value
+    charactersAPI.getOneRegister(charId)
+      .then(theCharacter => {
+        charactersContainer.innerText = ""
+        const mediumDiv = document.createElement("div")
+        mediumDiv.setAttribute("class", "character-info")
+        for (let key in theCharacter) {
+          const miniDiv = document.createElement("div")
+          miniDiv.setAttribute("class", `${key}`)
+          miniDiv.innerHTML = `<p>${key}:${theCharacter[key]}</p>`
+
+          mediumDiv.appendChild(miniDiv)
+
+        }
+        charactersContainer.appendChild(mediumDiv)
+
+
+
+      })
+
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+
+    const id = document.querySelector(".operation-delete input").value
+    charactersAPI.deleteOneRegister(id)
+      .then(x => charactersContainer.innerText = `tu Minion con id: ${id} se ha borrado`)
+
+
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = e => {
+    e.preventDefault()
+    const id = characterEditInputs[0].value
+
+    const updateCharacter = {
+
+      name: characterEditInputs[1].value,
+      occupation: characterEditInputs[2].value,
+      weapon: characterEditInputs[3].value,
+      cartoon: characterEditInputs[4].checked  //input checkbox
+    }
+    console.log(updateCharacter)
+    console.log(id)
+
+    charactersAPI.updateOneRegister(id, updateCharacter)
+      .then(x => {
+        charactersContainer.innerText = "Se ha editado tu Minion"
+        characterEditInputs[0].value = ""
+        characterEditInputs[1].value = ""
+        characterEditInputs[2].value = ""
+        characterEditInputs[3].checked = false
+
+      })
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = e => {
+
+    e.preventDefault()
+
+
+    const character = {
+      name: characterInputs[0].value,
+      occupation: characterInputs[1].value,
+      weapon: characterInputs[2].value,
+      cartoon: characterInputs[3].checked  //input checkbox
+    }
+    charactersAPI.createOneRegister(character)
+      .then(x => {
+        charactersContainer.innerText = "se ha creado tu Minion"
+        characterInputs[1].value = ""
+        characterInputs[2].value = ""
+        characterInputs[3].value = ""
+        characterInputs[4].checked = false
+      }
+      )
+
   }
 })
