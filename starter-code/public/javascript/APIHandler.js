093546c6e20818bf8ae6e0c2c4a6aa60e9b0f01f
Sachin Tekchandani <sachin6291@gmail.com>
2019-05-23 09:06:49 +0200

    iteration1 incomplete for now beacause of the lack of time

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,55 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return axios.get(`https://minions-api.herokuapp.com/characters`);
+    // .then(response => {
+    //   const { id, name, occupation, cartoon, weapon } = response.data;
+    // })
+    // .catch(error => console.log(error));
 
+    // It receives NO parameters
+    // It returns the full characters list
+    // It returns JSON
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    console.log("entro en one register");
+    return axios.get(`https://minions-api.herokuapp.com/characters/${id}`);
 
+    // It receives the character ID as a parameter(route)
+    // It returns the character with the indicated id
+    // It returns JSON
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    axios.post(`https://minions-api.herokuapp.com/characters`, newCharacter);
+    // It receives an object as a parameter, with the following format: { name: string, occupation: string, cartoon: boolean, weapon: string }
+    // It returns the created character if there are no errors
+    // It returns the wrong fields if there is some error
+    // It returns JSON
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(character) {
+    axios.put(
+      `https://minions-api.herokuapp.com/characters/${character_id}`,
+      character
+    );
+    // It receives the character id as a parameter(route)
+    // It receives an object as a parameter, with the following format: { name: string, occupation: string, cartoon: boolean, weapon: string }
+    // All the fields are optional
+    // It returns the updated character if there are no errors
+    // It returns "Character not found" if there is no character with the indicated id
+    // It returns JSON / text
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`https://minions-api.herokuapp.com/characters/${id}`, id);
+    // It receives the character id as a parameter(route)
+    // It returns "Character has been successfully deleted" if there are no errors
+    // It returns "Character not found" if there is no character with the indicated id
+    // It returns text
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,39 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = () => {
+    charactersAPI
+      .getFullList()
+      .then(response => {
+        const { id, name, occupation, cartoon, weapon } = response.data;
+        response.data.forEach((x, index) => {
+          if (response.data[index].name !== undefined) {
+            document.getElementById(
+              "allContainer"
+            ).innerHTML += ` <div class="characters-container">
+           <div class="character-info">
+             <div class="name">${response.data[index].name}</div>
+             <div class="occupation">${response.data[index].occupation}</div>
+             <div class="cartoon">${response.data[index].cartoon}</div>
+             <div class="weapon">${response.data[index].weapon}</div>
+           </div>
+         </div>`;
+          }
+        });
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+        console.log(response.data);
+      })
+      .catch(error => console.log(error));
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    const id = document.getElementById("fetch-one-input").value;
+    charactersAPI.getOneRegister(id).then(response => {});
+  };
+
+  document.getElementById("delete-one").onclick = function() {};
+
+  document.getElementById("edit-character-form").onsubmit = function() {};
+
+  document.getElementById("new-character-form").onsubmit = function() {};
+});
