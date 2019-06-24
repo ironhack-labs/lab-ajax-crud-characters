b30b7d3d30adb95c4bbad89d35f16b77f8b052cb
Cecile Pham <sheshiru.van@gmail.com>
2019-06-24 23:53:05 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,34 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister(data) {
+    return axios.post(`${this.BASE_URL}/characters`, {
+      name: data.name,
+      occupation: data.occupation,
+      weapon: data.weapon,
+      cartoon: data.cartoon
+    });
+  }
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`);
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  updateOneRegister(id, data) {
+    return axios.patch(`${this.BASE_URL}/characters/${id}`, {
+      name: data.name,
+      occupation: data.occupation,
+      weapon: data.weapon,
+      cartoon: data.cartoon
+    });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,133 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+// import APIHandler from ".APIHandler.js";
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    charactersAPI
+      .getFullList()
+      .then(apiCharac => {
+        let characContainer = document.querySelector(".characters-container");
+        characContainer.innerHTML = "";
+        apiCharac.data.forEach(charac => {
+          characContainer.innerHTML += `<div class="character-info">
+          <div class="id">Id: <span>${charac.id}</span></div>
+          <div class="name">Name: <span>${charac.name}</span></div>
+          <div class="occupation">Occupation: <span>${
+            charac.occupation
+          }</span></div>
+          <div class="cartoon">Is a cartoon? <span>${
+            charac.cartoon
+          }</span></div>
+          <div class="weapon">Weapon: <span>${
+            charac.weapon
+          }</span></div></div>`;
+        });
+      })
+      .catch(apiErr => console.log(apiErr));
+  };
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  document.getElementById("fetch-one").onclick = function() {
+    const userInput = document.querySelector(`[name=character-id]`);
+    // console.log(userInput);
+    let characContainer = document.querySelector(".characters-container");
+    charactersAPI
+      .getOneRegister(userInput.value)
+      .then(apiCharac => {
+        characContainer.innerHTML = "";
+        characContainer.innerHTML += `<div class="character-info">
+        <div class="id">Id: <span>${apiCharac.data.id}</span></div>
+        <div class="name">Name: <span>${apiCharac.data.name}</span></div>
+        <div class="occupation">Occupation: <span>${
+          apiCharac.data.occupation
+        }</span></div>
+        <div class="cartoon">Is a cartoon? <span>${
+          apiCharac.data.cartoon
+        }</span></div>
+        <div class="weapon">Weapon: <span>${
+          apiCharac.data.weapon
+        }</span></div></div>`;
+      })
+      .catch(apiErr => console.log(apiErr));
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {
+    const userNameInput = document.querySelector(`[name=name]`);
+    const userOccupationInput = document.querySelector(`[name=occupation]`);
+    const userWeaponInput = document.querySelector(`[name=weapon]`);
+    const userCartoonInput = document.querySelector(`[name=cartoon]`);
+    charactersAPI
+      .createOneRegister({
+        name: userNameInput.value,
+        occupation: userOccupationInput.value,
+        weapon: userWeaponInput.value,
+        cartoon: userCartoonInput.checked
+      })
+      .then(apiRes => {
+        document.getElementById("send-data").style.backgroundColor = "green";
+        console.log(apiRes.data);
+      })
+      .catch(apiErr => {
+        document.getElementById("send-data").style.backgroundColor = "red";
+        setTimeout(() => {
+          document.getElementById("send-data").style.backgroundColor =
+            "initial";
+        }, 1100);
+        console.log(apiErr);
+      });
+  };
+  document.getElementById("delete-one").onclick = function() {
+    const userInput = document.querySelector(`[name=character-id-delete]`);
+    charactersAPI
+      .deleteOneRegister(userInput.value)
+      .then(apiRes => {
+        document.getElementById("delete-one").style.backgroundColor = "green";
+        console.log(apiRes.data);
+      })
+      .catch(apiErr => {
+        document.getElementById("delete-one").style.backgroundColor = "red";
+        setTimeout(() => {
+          document.getElementById("delete-one").style.backgroundColor =
+            "initial";
+        }, 1100);
+        console.log(apiErr);
+      });
+  };
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+    const updateId = document.querySelector(`[name=chr-id]`);
+    const updateName = document.querySelector(
+      "#edit-character-form > div:nth-child(2) > input[type=text]"
+    );
+    const updateOccupation = document.querySelector(
+      "#edit-character-form > div:nth-child(3) > input[type=text]"
+    );
+    const updateWeapon = document.querySelector(
+      "#edit-character-form > div:nth-child(4) > input[type=text]"
+    );
+    const updateCartoon = document.querySelector(
+      "#edit-character-form > div:nth-child(5) > input[type=checkbox]"
+    );
+
+    // console.log(evt.target.children);
+
+    charactersAPI
+      .updateOneRegister(updateId.value, {
+        name: updateName.value,
+        occupation: updateOccupation.value,
+        weapon: updateWeapon.value,
+        cartoon: updateCartoon.checked
+      })
+      .then(apiRes => {
+        document.getElementById("update-data").style.backgroundColor = "green";
+        console.log(apiRes.data);
+      })
+      .catch(apiErr => {
+        document.getElementById("update-data").style.backgroundColor = "red";
+        setTimeout(() => {
+          document.getElementById("update-data").style.backgroundColor =
+            "initial";
+        }, 1100);
+        console.log(apiErr);
+      });
+  };
+});
