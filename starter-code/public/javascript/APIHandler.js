b428811b8555c91f3ff73d7d1515bf1f77a6f7d8
Majdi Chargui <mejdichergui@gmail.com>
2019-06-24 19:55:37 +0100

    done at 40%

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,19 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(this.BASE_URL + "/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(this.BASE_URL + "/characters/" + id);
   }
 
-  createOneRegister () {
+  createOneRegister() {}
 
-  }
+  updateOneRegister() {}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,23 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+document.getElementById("fetch-all").onclick = function() {
+  charactersAPI
+    .getFullList()
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+};
+document.getElementById("fetch-one").onclick = function() {
+  const input = document.getElementById("charactId");
+  const id = input.value;
+  console.log(id);
+  charactersAPI
+    .getOneRegister(id)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+};
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+document.getElementById("delete-one").onclick = function() {};
+$;
+document.getElementById("edit-character-form").onsubmit = function() {};
+
+document.getElementById("new-character-form").onsubmit = function() {};
