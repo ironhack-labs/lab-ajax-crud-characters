a45c69f158bec5b7ca8a518579cb616c70ab2a35
Rafael Ruiz <rafaelruizgallardon@hotmail.com>
2019-07-03 19:46:42 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,61 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+     axios
+      .get("http://localhost:8000/Characters")
+      .then(allCharacters => {
+       let hiddenInfo = document.querySelector(".character-info")
+       hiddenInfo.style.display = 'none';
+        allCharacters.data.forEach(element => {
+          document.querySelector(".characters-container")
+          .innerHTML += `<div class="character-info">
+        <div class="id"> Id: ${element.id}</div>
+        <div class="name"> Name: ${element.name}</div>
+        <div class="occupation"> Occupation: ${element.occupation}</div>
+        <div class="weapon"> Weapon: ${element.weapon}</div>
+        <div class="cartoon"> Cartoon: ${element.cartoon}</div>
+        </div>`;
+        });
+      })
+      .catch(err => err);
+
 
   }
 
-  getOneRegister () {
+  getOneRegister() {
+    let id = document.querySelector(".characterId").value 
+    axios
+    .get(`http://localhost:8000/characters/${id}`)
+    .then(character => {
+      document.querySelector(".characters-container").innerHTML = ""
+      document.querySelector(".characters-container")
+    .innerHTML += `<div class="character-info">
+  <div class="id"> Id: ${character.data.id}</div>
+  <div class="name"> Name: ${character.data.name}</div>
+  <div class="occupation"> Occupation: ${character.data.occupation}</div>
+  <div class="weapon"> Weapon: ${character.data.weapon}</div>
+  <div class="cartoon"> Cartoon: ${character.data.cartoon}</div>
+  </div>`;
+  });
+  }
+
+  createOneRegister() {
+axios
+.post("http://localhost:8000/characters")
 
   }
 
-  createOneRegister () {
-
+  updateOneRegister() {
+    axios
+    .put("http://localhost:8000/characters/:id")
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    axios
+    .delete("http://localhost:8000/characters/:id")
+    
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,11 +1,19 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
+const characterContainerDOMEL = document.querySelector(".character-info")
+const characterName = document.querySelector(".name")
+const characterOccupation = document.querySelector(".occupation")
+const characterCartoon = document.querySelector(".cartoon")
+const characterWeapon = document.querySelector(".weapon")
+
+
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+   charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
+    charactersAPI.getOneRegister()
     
   }
   
