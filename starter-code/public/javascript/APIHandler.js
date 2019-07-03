3e35558c025d3667745769455216c51dbfa880d7
Miriloper <miriam.mad.ml@gmail.com>
2019-07-03 20:29:39 +0200

    a tope till the middle

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+   // this.pepe = +document.getElementById('pepito').value;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters/`)
+    .then((data)=>{
+      return data.data
+    })
   }
 
-  getOneRegister () {
+  
 
+  getOneRegister() {
+    this.pepe = +document.getElementById('pepito').value;
+    return axios.get(`${this.BASE_URL}/characters/${this.pepe}`)
+    .then((data)=>{
+      document.querySelector(`.name`).innerHTML = data.data.name
+      document.querySelector(`.occupation`).innerHTML = data.data.occupation
+      document.querySelector(`.cartoon`).innerHTML = data.data.cartoon
+      document.querySelector(`.weapon`).innerHTML = data.data.weapon
+      return data.data
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    axios.get("/characters/:id");
   }
 
-  updateOneRegister () {
+  updateOneRegister() {}
 
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    this.felipe = +document.getElementById('pepita').value;
+    console.log(this.felipe)
+    return axios.put(`${this.BASE_URL}/characters/${this.felipe}`)
+    .then((data)=>{
+      data.data.name = ""
+      data.data.occupation = ""
+      data.data.cartoon = ""
+      data.data.weapon = ""
+      return data.data
+    })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,15 +2,15 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister()
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    charactersAPI.deleteOneRegister()
   }
   
   document.getElementById('edit-character-form').onsubmit = function(){
