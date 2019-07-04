5a317b541dbf8dae5a0ef59c5f35fd5ce04a4e74
jose1987barajas <jose.1987.barajas@gmail.com>
2019-07-03 23:51:57 -0500

    first

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,25 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(this.BASE_URL)
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(this.BASE_URL + `/${id}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister(valor) {
+    return axios.post(this.BASE_URL, valor)
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, valor) {
+    return axios.patch(this.BASE_URL + `/${id}`, valor)
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(this.BASE_URL + `/${id}`)
   }
 }
