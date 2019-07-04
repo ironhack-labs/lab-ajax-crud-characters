19bc7c9e8d517ad718f7ab4724247cd60d1c726b
Adri Gispen <aegispen@gmail.com>
2019-07-04 17:58:05 +0200

    done together

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,69 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL + "/characters")
+      .then(response => {
+        console.log(response.data);
+        let parent = document.getElementById("chars-container");
+        for (var i = 0; i < response.data.length; i++) {
+          let div = document.createElement("div");
+          div.innerHTML = `
+            <div class="character-info">
+              <div class="id">Id: ${response.data[i].id}</div>
+              <div class="name">Name: ${response.data[i].name}</div>
+              <div class="occupation">Occupation: ${response.data[i].occupation}</div>
+              <div class="cartoon">Is a cartoon? ${response.data[i].cartoon}</div>
+              <div class="weapon">Weapon: ${response.data[i].weapon}</div>
+            </div>`;
+          parent.appendChild(div);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios
+      .get(this.BASE_URL + `/characters/${id}`)
+      .then(character => {
+        return character;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  createOneRegister () {
-
+  createOneRegister(character) {
+    axios
+      .post(this.BASE_URL + "/characters", character)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, character) {
+    axios
+      .put(this.BASE_URL + `/characters/${id}`, character)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    axios
+      .delete(this.BASE_URL + `/characters/${id}`)
+      .then()
+      .catch();
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,47 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const newForm = document.getElementById("new-character-form");
+const editForm = document.getElementById("edit-character-form");
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+$(document).ready(() => {
+  document.getElementById("fetch-all").onclick = function() {
+    let chars = charactersAPI.getFullList();
+    let parent = document.getElementById("chars-container");
+    chars.foreach(char => {
+      let div = document.createElement("div");
+      div.innerHTML = `
+        <div class="character-info">
+          <div class="name">${char.name}</div>
+          <div class="occupation">${char.occupation}</div>
+          <div class="cartoon">${char.cartoon}</div>
+          <div class="weapon">${char.weapon}</div>
+        </div>`;
+      parent.appendChild(div);
+    });
+  };
+
+  document.getElementById("fetch-one").onclick = function() {
+    let id = document.getElementById("find-by-id").value;
+    console.log(id);
+    console.log(charactersAPI.getOneRegister(id));
+  };
+
+  document.getElementById("delete-one").onclick = function() {};
+
+  document.getElementById("edit-character-form").onsubmit = function() {
+    let name = editForm.elements["name"].value;
+    let occupation = editForm.elements["occupation"].value;
+    let weapon = editForm.elements["weapon"].value;
+    let cartoon = editForm.elements["cartoon"].checked;
+    let id = editForm.elements["chr-id"].value;
+    charactersAPI.updateOneRegister(id, { name, occupation, weapon, cartoon });
+  };
+
+  document.getElementById("new-character-form").onsubmit = function() {
+    let name = newForm.elements["name"].value;
+    let occupation = newForm.elements["occupation"].value;
+    let weapon = newForm.elements["weapon"].value;
+    let cartoon = newForm.elements["cartoon"].checked;
+    charactersAPI.createOneRegister({ name, occupation, weapon, cartoon });
+  };
+});
