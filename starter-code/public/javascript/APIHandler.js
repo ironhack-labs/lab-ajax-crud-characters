50035c4005aa6d6fbcbb8dfd24bdd96ee03b8ced
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-07-04 10:37:11 -0500

    sin terminar

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    getFullList() {
+        return axios.get(this.BASE_URL)
 
-  }
+    }
 
-  getOneRegister () {
+    getOneRegister(id) {
+        return axios.get(this.BASE_URL + `/${id}`)
 
-  }
+    }
 
-  createOneRegister () {
+    createOneRegister(valor) {
+        return axios.post(this.BASE_URL, valor)
+    }
 
-  }
+    updateOneRegister(id, valor) {
+        return axios.patch(this.BASE_URL + `/${id}`, valor)
+    }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(id) {
+        return axios.delete(this.BASE_URL + `/${id}`)
+    }
 }
+// Get all the characters info from http://localhost:8000/characters
+// Get a single character info from http://localhost:8000/characters/:id
+// Create a single character posting the data to http://localhost:8000/characters
+// Delete a single character through his id in http://localhost:8000/characters/:id
+// Edit a single character through his id in http://localhost:8000/characters/:id
