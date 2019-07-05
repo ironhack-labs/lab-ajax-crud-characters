fa730b2ae54a41b6d42e19e1a9d76549271e816d
sofia <sofleos@gmail.com>
2019-07-05 09:40:06 -0500

    PP Web FT MEX Sofía De la Cueva

---
+++ starter-code/api/APIHandler.js
@@ -0,0 +1,25 @@
+class APIHandler {
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl;
+  }
+
+  getFullList() {
+    return axios.get(this.BASE_URL);
+  }
+
+  getOneRegister(id) {
+    return axios.get(this.BASE_URL + `/${id}`);
+  }
+
+  createOneRegister(valor) {
+    return axios.post(this.BASE_URL, valor);
+  }
+
+  updateOneRegister(id, valor) {
+    return axios.patch(this.BASE_URL + `/${id}`, valor);
+  }
+
+  deleteOneRegister(id) {
+    return axios.delete(this.BASE_URL + `/${id}`);
+  }
+}
---
+++ starter-code/index.js
@@ -0,0 +1,111 @@
+
+const charactersAPI = new APIHandler("http://localhost:8000/characters")
+
+$(document).ready(() => {
+    document.getElementById('fetch-all').onclick = function (e) {
+        e.preventDefault()
+
+        charactersAPI.getFullList()
+            .then(data => {
+                console.log(data)
+
+                cont chars = document.querySelector('.characters-container')
+                data.array.forEach(element => {
+                    const newDiv = document.createElement('div');
+                    newDiv.style.classList.toggle('character-info')
+                    newDiv.innerHTML = `<div class="character-info">
+                    <div class="name">${element.name}</div>
+                    < div class = "occupation" > ${
+                        element.occupation
+                        } < /div>
+                    < div class = "cartoon" > ${element.cartoon}
+                    </div>
+                    <div class = "weapon" > ${element.weapon}
+   < /div>
+                </div>`
+                    chars.
+                });
+
+
+
+                document.querySelector('.name').innerHTML = `
+        <div class="name">Character Name: ${data[0].name}</div>`
+
+            })
+            .catch(err => console.log(err))
+    }
+
+    document.getElementById('fetch-one').onclick = function (e) {
+        e.preventDefault()
+
+        const id = document.querySelector('#character-id').value
+        charactersAPI.getOneRegister(id)
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }
+
+    document.getElementById('delete-one').onclick = function (e) {
+        e.preventDefault()
+
+        const id = document.querySelector('#deleteById').value
+        charactersAPI.deleteOneRegister(id)
+            .then(() => {
+                console.log('deleted one character')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    document.getElementById('edit-character-form').onsubmit = function (e) {
+        e.preventDefault()
+
+        const id = document.querySelector('#edit-id').value
+        const name = document.querySelector('#edit-name').value
+        const occupation = document.querySelector('#edit-occupation').value
+        const weapon = document.querySelector('#edit-weapon').value
+        const cartoon = document.querySelector('#edit-checkbox').checked
+
+
+        const character = {
+            name,
+            occupation,
+            weapon,
+            cartoon
+        }
+        charactersAPI.updateOneRegister(id, character)
+            .then(data => {
+                console.log("las quesadillas ni de pedllevan queso Alex  NEvERRRR")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    document.getElementById('new-character-form').onsubmit = function (e) {
+        e.preventDefault()
+        const name = document.querySelector('#create-name').value
+        const occupation = document.querySelector('#create-occupation').value
+        const weapon = document.querySelector('#create-weapon').value
+        const cartoon = document.querySelector('#cartoon-checkbox').checked
+
+        const character = {
+            name,
+            occupation,
+            weapon,
+            cartoon
+        }
+        charactersAPI.createOneRegister(character)
+            .then(data => {
+                console.log("Created character")
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+})
