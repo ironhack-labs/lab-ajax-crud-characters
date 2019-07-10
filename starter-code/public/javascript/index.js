059ce1150e606d226543d0105dced722e227f574
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-10 13:33:25 -0400

    Fetch all characters done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,9 +1,32 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
 
-  }
+  
+    let listOfCharacters = document.querySelector('.characters-container');
+  
+    document.getElementById('fetch-all').onclick = function(){
+      // Make a GET request for all
+      axios.get('https://ih-crud-api.herokuapp.com/characters')
+        .then(response => {
+          listOfCharacters.innerHTML = '';
+            
+          response.data.forEach((each)=>{
+            let character = document.createElement('div');
+            character.setAttribute('class', 'character-info')
+            character.innerHTML = `<div class="name">${each.name}</div>
+                                  <div class="occupation">${each.occupation}</div>
+                                  <div class="debt">${each.debt}</div>
+                                  <div class="weapon">${each.weapon}</div>`;
+            
+            listOfCharacters.appendChild(character);
+          });
+            console.log('post successful and the response is: ',response );
+        })
+        .catch(error => {
+            console.log('Oh No! Error is: ', error);  
+        })
+    }
   
   document.getElementById('fetch-one').onclick = function(){
     
