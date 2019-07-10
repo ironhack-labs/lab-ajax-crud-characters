b2f384ae9dff1c73cd2c0cf9ddc984f60e4bb460
Andre Cabrera <cabrerandre@gmail.com>
2019-07-10 14:28:01 -0400

    working on the edit function, others completed

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -3,21 +3,141 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
 
+    let charBox = document.getElementById('charBox')
+    // console.log(charBox)
+    
+
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+    .then((response)=>{
+      console.log(response.data);
+        charBox.innerHTML = "";
+
+        response.data.forEach((eachOne)=>{
+
+            let newChar = document.createElement('div');
+            newChar.className = "character-info"
+            newChar.innerHTML = `
+            <div class="name">${eachOne.name}</div>
+            <div class="occupation">${eachOne.occupation}</div>
+            <div class="cartoon">${eachOne.debt}</div>
+            <div class="weapon">${eachOne.weapon}</div>
+            `
+
+            charBox.appendChild(newChar);
+
+        })
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
   }
   
   document.getElementById('fetch-one').onclick = function(){
+
+    let charBox = document.getElementById('charBox')
+    let charID = document.getElementById('idSearch').value
+    console.log(charID)
+    
+
+    axios.get('https://ih-crud-api.herokuapp.com/characters/' + charID)
+    .then((response)=>{
+      console.log(response.data);
+        charBox.innerHTML = "";
+        document.getElementById('idSearch').value = ""  ;      
+        // response.data.forEach((eachOne)=>{
+
+            let newChar = document.createElement('div');
+            newChar.className = "character-info"
+            newChar.innerHTML = `
+            <div class="name">${response.data.name}</div>
+            <div class="occupation">${response.data.occupation}</div>
+            <div class="cartoon">${response.data.debt}</div>
+            <div class="weapon">${response.data.weapon}</div>
+            `
+
+            charBox.appendChild(newChar);
+          
+
+        // })
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
     
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
+  document.getElementById('edit-character-form').onsubmit = function(e){
+
+    e.preventDefault();
+
+    let name = document.getElementById('new-name').value;
+    let occupation = document.getElementById('new-occupation').value;
+    let weapon = document.getElementById('new-weapon').value;
+    let debt = document.getElementById('new-debt').checked;
+    console.log(name, occupation, weapon, debt)
+
+    axios.put('https://ih-crud-api.herokuapp.com/characters',  {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      debt: debt
+  })
+    .then(()=>{
+      
+      document.getElementById('new-name').value = "";
+      document.getElementById('new-occupation').value = "";
+      document.getElementById('new-weapon').value = "";
+      document.getElementById('new-debt').checked = false;
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
             
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
+  document.getElementById('new-character-form').onsubmit = function(e){
+
+    e.preventDefault();
+
+    let name = document.getElementById('new-name').value;
+    let occupation = document.getElementById('new-occupation').value;
+    let weapon = document.getElementById('new-weapon').value;
+    let debt = document.getElementById('new-debt').checked;
+    console.log(name, occupation, weapon, debt)
+
+    axios.post('https://ih-crud-api.herokuapp.com/characters',  {
+      name: name,
+      occupation: occupation,
+      weapon: weapon,
+      debt: debt
+  })
+    .then(()=>{
+      
+      document.getElementById('new-name').value = "";
+      document.getElementById('new-occupation').value = "";
+      document.getElementById('new-weapon').value = "";
+      document.getElementById('new-debt').checked = false;
+
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
                 
   }
+
+
+
+
+
+
+
+
+
 })
