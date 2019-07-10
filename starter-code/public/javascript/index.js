5638c5b1d44c8e529bbc7d36d15ddd6939971205
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-10 14:14:53 -0400

    Edit character form

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -54,13 +54,73 @@ $(document).ready( () => {
         })
     }
   
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
+    document.getElementById('delete-one').onclick = function(){
+      let characterId = Number(document.getElementById('del-char').value);
+      console.log(characterId);
   
-  document.getElementById('edit-character-form').onsubmit = function(){
+      // Make a DELETE request for one
+      axios.delete(`https://ih-crud-api.herokuapp.com/characters/${characterId}` )
+        .then(response => {
+          listOfCharacters.innerHTML = '';
+          
+          let character = document.createElement('div');
+          character.setAttribute('class', 'character-info')
+          character.innerHTML = `<div class="name">${response.data.name}</div>
+                                <div class="occupation">${response.data.occupation}</div>
+                                <div class="weapon">${response.data.weapon}</div>`;
+          
+          listOfCharacters.appendChild(character);
+  
+          console.log('get successful and the response is: ',response );
+        })
+        .catch(error => {
+            console.log('Oh No! Error is: ', error);  
+        }) 
+    }
+  
+    document.getElementById('edit-character-form').onsubmit = function(e){
+    
+      e.preventDefault();
+  
+      let updateName = document.getElementById('update-name').value;
+      let updateOccupation = document.getElementById('update-occupation').value;
+      let updateWeapon = document.getElementById('update-weapon').value;
+      let updateDebt = document.getElementById('update-debt').checked;
+  
+      let updatedCharacter = {
+        name: updateName,
+        occupation: updateOccupation,
+        weapon: updateWeapon,
+        debt: updateDebt
+      };
+  
+      let theId = document.getElementById("id-char").value;
+  
+      axios.put(`https://ih-crud-api.herokuapp.com/characters/${theId}`, updatedCharacter)
+          .then(response => {
+            document.getElementById('id-char').value = '';
+            document.getElementById('update-name').value = '';
+            document.getElementById('update-occupation').value = '';
+            document.getElementById('update-weapon').value = '';
+            document.getElementById('update-debt').checked = '';
+  
+            listOfCharacters.innerHTML = '';
             
-  }
+            let character = document.createElement('div');
+            character.setAttribute('class', 'character-info')
+            character.innerHTML = `<div class="name">${response.data.name}</div>
+                                  <div class="occupation">${response.data.occupation}</div>
+                                  <div class="debt">${response.data.debt}</div>
+                                  <div class="weapon">${response.data.weapon}</div>`;
+            
+            listOfCharacters.appendChild(character);
+            console.log('Response from the API is: ', response);
+          })
+          .catch(error => {
+              console.log("The error is: ", error);
+          });
+  
+    }
   
   document.getElementById('new-character-form').onsubmit = function(){
                 
