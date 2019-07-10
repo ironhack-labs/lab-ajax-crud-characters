697da4186ea8b1d18b4dfec6864b16c154713084
Alfredo-Rojas <alfredot84@gmail.com>
2019-07-10 13:57:33 -0400

    Fetch one character done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -28,9 +28,31 @@ $(document).ready( () => {
         })
     }
   
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
+    document.getElementById('fetch-one').onclick = function(){
+
+      let characterId = Number(document.getElementById('char-id').value);
+      console.log(characterId);
+  
+      // Make a GET request for one
+      axios.get(`https://ih-crud-api.herokuapp.com/characters/${characterId}` )
+        .then(response => {
+          listOfCharacters.innerHTML = '';
+          
+          let character = document.createElement('div');
+          character.setAttribute('class', 'character-info')
+          character.innerHTML = `<div class="name">${response.data.name}</div>
+                                <div class="occupation">${response.data.occupation}</div>
+                                <div class="debt">${response.data.debt}</div>
+                                <div class="weapon">${response.data.weapon}</div>`;
+          
+          listOfCharacters.appendChild(character);
+  
+          console.log('get successful and the response is: ',response );
+        })
+        .catch(error => {
+            console.log('Oh No! Error is: ', error);  
+        })
+    }
   
   document.getElementById('delete-one').onclick = function(){
         
