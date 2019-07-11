8ef41d8ec3de2a36e43344c5875e2e34031138a8
Gabriel Holanda <gabriel_ebot@hotmail.com>
2019-07-11 00:23:10 -0300

    Project Done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,40 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this._id = 1;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
   }
 
-  getOneRegister () {
-
+  getOneRegister(_id) {
+    this._id = _id;
+    return axios.get(`${this.BASE_URL}/characters/${this._id}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister(name, occupation, cartoon, weapon) {
+    return axios.post(`${this.BASE_URL}/characters`, {
+      'name': name,
+      'occupation': occupation,
+      'cartoon': cartoon,
+      'weapon': weapon
+    })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(_id, name, occupation, cartoon, weapon) {
+    this._id = _id;
+    return axios.put(`${this.BASE_URL}/characters/${this._id}`, { name, occupation, cartoon, weapon })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(_id) {
+    this._id = _id;
+    return axios.delete(`${this.BASE_URL}/characters/${this._id}`)
+      .then(response => {
+        console.log('Character has been successfully deleted ');
+      })
+      .catch(error => {
+        console.log('Character not found', error);
+      })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,55 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
+let fetchAll = document.getElementsByClassName('characters-container')[0]
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+      .then(res => res.data.forEach(element => {
+        fetchAll.innerHTML += `<div class="character-info">
+          <div class="name">${element.name}</div>
+          <div class="occupation">${element.occupation}</div>
+          <div class="cartoon">${element.cartoon}</div>
+          <div class="weapon">${element.weapon}</div>
+          </div>`
+      }))
+      .catch(err => console.log(err));
+  }
 
+  document.getElementById('fetch-one').onclick = function () {
+    charactersAPI.getOneRegister(document.getElementsByName('character-id')[0].value)
+      .then(res => fetchAll.innerHTML = `<div class="character-info">
+      <div class="name">${res.data.name}</div>
+      <div class="occupation">${res.data.occupation}</div>
+      <div class="cartoon">${res.data.cartoon}</div>
+      <div class="weapon">${res.data.weapon}</div>
+      </div>`)
+      .catch(err => console.log(err));
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+  document.getElementById('delete-one').onclick = function () {
+    charactersAPI.deleteOneRegister(document.getElementsByName('character-id-delete')[0].value)
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    event.preventDefault();
+    const name = document.getElementsByName('name')[1].value;
+    const occupation = document.getElementsByName('occupation')[1].value;
+    const weapon = document.getElementsByName('weapon')[1].value;
+    const cartoon = document.getElementsByName('cartoon')[1].checked;
+
+    charactersAPI.updateOneRegister(document.getElementsByName('chr-id')[0].value, name, occupation, cartoon, weapon)
+      .then(() => console.log('Edited with success!'))
+      .catch(err => console.log(err))
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+    event.preventDefault();
+    const name = document.getElementsByName('name')[0].value;
+    const occupation = document.getElementsByName('occupation')[0].value;
+    const weapon = document.getElementsByName('weapon')[0].value;
+    const cartoon = document.getElementsByName('cartoon')[0].checked;
+
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon)
+      .then(() => console.log('Created character with success!'))
+      .catch(err => console.log(err));
   }
 })
