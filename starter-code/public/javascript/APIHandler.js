98811c4613df9d247764458e0469c165486c4b15
David Dornelles <daviddornelles@gmail.com>
2019-07-11 02:03:06 -0300

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,166 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  //
+  // .methods() by CRUD order
+  //
 
+  // Create a single character posting the data to this.BASE_URL
+  createOneRegister(name, occupation, weapon, cartoon) {
+    const newCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon,
+    };
+
+    return axios.post(`${this.BASE_URL}/characters`, newCharacter)
+      .then((response) => {
+        const okCreate = document.getElementById('delete-one');
+        okCreate.style.backgroundColor = 'green';
+        console.log(response.data);
+      })
+      .catch(err => {
+        const failCreate = document.getElementById('delete-one');
+        failCreate.style.backgroundColor = 'red';
+        console.log(err)
+      });
   }
 
-  getOneRegister () {
+  // Get all the characters info from this.BASE_URL
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then((response) => {
+        const card = document.querySelector('.characters-container');
+        card.innerHTML = '';
 
+        response.data.forEach((element) => {
+          card.innerHTML += `
+            <div class="character-info">
+              <div class="name">Id: <span>${element.id}</span></div>
+              <div class="name">Name: <span>${element.name}</span></div>
+              <div class="occupation">Occupation: <span>${element.occupation}</span></div>
+              <div class="cartoon">Is a Cartoon?: <span>${element.cartoon}</span></div>
+              <div class="weapon">Weapon: <span>${element.weapon}</span></div>
+            </div>
+          `;
+        });
+      })
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
+  // Get a single character info from this.BASE_URL/:id
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then((response) => {
+        const findOne = document.getElementById('fetch-one-input').value;
+        if (findOne === '') return;
 
+        const card = document.querySelector('.characters-container');
+        card.innerHTML = '';
+
+        card.innerHTML += `
+          <div class="character-info">
+            <div class="name">Id: <span>${response.data.id}</span></div>
+            <div class="name">Name: <span>${response.data.name}</span></div>
+            <div class="occupation">Occupation: <span>${response.data.occupation}</span></div>
+            <div class="cartoon">Is a Cartoon?: <span>${response.data.cartoon}</span></div>
+            <div class="weapon">Weapon: <span>${response.data.weapon}</span></div>
+          </div>
+        `;
+
+        const edit = document.getElementById('edit-character-form');
+        edit.innerHTML = '';
+
+        if (response.data.cartoon === true) {
+          edit.innerHTML = `
+            <div class="field">
+                <label for="id">ID: </label>
+                <input type="text" name="chr-id" value="${response.data.id}" disabled />
+              </div>
+              <div class="field">
+                <label for="name">Name: </label>
+                <input type="text" name="name" value="${response.data.name}" />
+              </div>
+              <div class="field">
+                <label for="occupation">Occupation: </label>
+                <input type="text" name="occupation" value="${response.data.occupation}" />
+              </div>
+              <div class="field">
+                <label for="weapon">Weapon: </label>
+                <input type="text" name="weapon" value="${response.data.weapon}" />
+              </div>
+              <div class="field">
+                <label for="cartoon">Is a Cartoon: </label>
+                <input name="cartoon" type="checkbox" checked />
+              </div>
+              <button id="send-data">Update</button>
+            `;
+        } else {
+          edit.innerHTML = `
+            <div class="field">
+                <label for="id">ID: </label>
+                <input type="text" name="chr-id" value="${response.data.id}" disabled />
+              </div>
+              <div class="field">
+                <label for="name">Name: </label>
+                <input type="text" name="name" value="${response.data.name}" />
+              </div>
+              <div class="field">
+                <label for="occupation">Occupation: </label>
+                <input type="text" name="occupation" value="${response.data.occupation}" />
+              </div>
+              <div class="field">
+                <label for="weapon">Weapon: </label>
+                <input type="text" name="weapon" value="${response.data.weapon}" />
+              </div>
+              <div class="field">
+                <label for="cartoon">Is a Cartoon: </label>
+                <input name="cartoon" type="checkbox" />
+              </div>
+              <button id="send-data">Update</button>
+            `;
+        }
+      })
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
+  // Edit a single character through his id in this.BASE_URL/:id
+  updateOneRegister(id) {
+    const editCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon,
+    };
 
+    return axios.post(`${this.BASE_URL}/characters/${id}`, editCharacter)
+      .then((response) => {
+        const okUpdate = document.getElementById('delete-one');
+        okUpdate.style.backgroundColor = 'green';
+      })
+      .catch(err => {
+        const failUpdate = document.getElementById('delete-one');
+        failUpdate.style.backgroundColor = 'red';
+        console.log(err)
+      });
   }
 
-  deleteOneRegister () {
-
+  // Delete a single character through his id in this.BASE_URL/:id
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then((response) => {
+        const deleteOne = document.getElementById('delete-one-input').value;
+        if (deleteOne === '') return;
+        const okDelete = document.getElementById('delete-one');
+        okDelete.style.backgroundColor = 'green';
+      })
+      .catch(err => {
+        const failDelete = document.getElementById('delete-one');
+        failDelete.style.backgroundColor = 'red';
+        console.log(err)
+      });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,36 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000');
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+  //
+  // actions by CRUD order
+  //
+
+  document.getElementById('new-character-form').onsubmit = function () {
+    const name       = document.getElementById('newChar-name').value;
+    const occupation = document.getElementById('newChar-occupation').value;
+    const weapon     = document.getElementById('newChar-weapon').value;
+    const cartoon    = document.getElementById('newChar-cartoon').checked;
+
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon);
+
+  };
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList();
+  };
+
+  document.getElementById('fetch-one').onclick = function () {
+    const findOne = document.getElementById('fetch-one-input').value;
+    charactersAPI.getOneRegister(findOne);
+  };
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    const deleteOne = document.getElementById('delete-one-input').value;
+    charactersAPI.updateOneRegister(deleteOne);
+  };
+
+  document.getElementById('delete-one').onclick = function () {
+    const deleteOne = document.getElementById('delete-one-input').value;
+    charactersAPI.deleteOneRegister(deleteOne);
+  };
+});
