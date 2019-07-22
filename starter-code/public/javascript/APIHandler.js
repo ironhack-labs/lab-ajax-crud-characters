e96f916a7a54e9d5d91ed125eeb4a656a319d98a
Daniel Orio <danielislas3@live.com>
2019-07-21 22:30:23 -0500

    donde :D

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,26 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    constructor (baseUrl) {
+      this.BASE_URL = baseUrl;
+    }
+  
+    getFullList () {
+      return axios.get(this.BASE_URL);
+    }
+  
+    getOneRegister (id) {
+      return axios.get(`${this.BASE_URL}/${id}`);
+    }
+  
+    createOneRegister (nuevo) {
+      return axios.post(`${this.BASE_URL}`, nuevo);
+    }
+  
+    updateOneRegister (id, actualizado) {
+      return axios.patch(`${this.BASE_URL}/${id}`, actualizado);
+    }
+  
+    deleteOneRegister (id) {
+      return axios.delete(`${this.BASE_URL}/${id}`);
+    }
   }
-
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
-}
+  
