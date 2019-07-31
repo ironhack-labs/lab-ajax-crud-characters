cbc5c6c192079e83c1f250f49d9c058c56f7f85b
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-31 18:03:10 +0200

    minions

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl, fetchOneName, fetchOneOccupation, fetchOneCartoon, fetchOneWeapon, fetchOneButton, searchCharId) {
+    this.BASE_URL = 'https://minions-api.herokuapp.com'
+
+    this.fetchOneName = fetchOneName
+    this.fetchOneOccupation = fetchOneOccupation
+    this.fetchOneCartoon = fetchOneCartoon
+    this.fetchOneWeapon = fetchOneWeapon
+
+    this.fetchOneButton = fetchOneButton
+    this.searchCharId = searchCharId
   }
 
-  getFullList () {
+  getFullList() {
+  }
+
+  getOneRegister() {
+    APIHandler.get(`/characters/${}`)
+      .then(theChar => {
+
+      })
 
   }
 
-  getOneRegister () {
+  createOneRegister() {
 
   }
 
-  createOneRegister () {
+  updateOneRegister() {
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,33 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const fetchOneName = document.getElementsByClassName('name-p')[0]
+const fetchOneOccupation = document.getElementsByClassName('occupation-p')[0]
+const fetchOneCartoon = document.getElementsByClassName('cartoon-p')[0]
+const fetchOneWeapon = document.getElementsByClassName('weapon-p')[0]
+
+const fetchOneButton = document.getElementById('fetch-one')
+const searchCharId = document.getElementById('char-id').value
+
+$(document).ready(() => {
+
+  document.getElementById('fetch-all').onclick = function () {
+
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = function () {
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('delete-one').onclick = function () {
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
