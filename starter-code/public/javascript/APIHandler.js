52cc1cb8b3bc31ed72837ef5d486e9cde6cc9088
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-07-31 21:42:51 +0200

    minions

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,28 +1,81 @@
 class APIHandler {
-  constructor(baseUrl, fetchOneName, fetchOneOccupation, fetchOneCartoon, fetchOneWeapon, fetchOneButton, searchCharId) {
-    this.BASE_URL = 'https://minions-api.herokuapp.com'
+  constructor(baseUrl) {
+    this.BASE_URL = 'https://minions-api.herokuapp.com/'
+    this.minions = axios.create({
+      baseURL: this.BASE_URL
+    })
 
-    this.fetchOneName = fetchOneName
-    this.fetchOneOccupation = fetchOneOccupation
-    this.fetchOneCartoon = fetchOneCartoon
-    this.fetchOneWeapon = fetchOneWeapon
+    // const fetchOneButton = document.getElementById('fetch-one')
+    // const searchCharId = document.getElementById('char-id').value
 
-    this.fetchOneButton = fetchOneButton
-    this.searchCharId = searchCharId
+    // const theName = document.getElementsByClassName("the-name")
+    // const theOccupation = document.getElementsByClassName("the-occupation")
+    // const theWeapon = document.getElementsByClassName("the-weapon")
+
   }
 
   getFullList() {
+    this.minions.get("characters")
+      .then(response => {
+
+        response.data.forEach(minion => {
+          document.getElementsByClassName('name-p')[0].innerHTML += `${minion.name}<br>`
+          document.getElementsByClassName('occupation-p')[0].innerHTML += `${minion.occupation}<br>`
+          document.getElementsByClassName('cartoon-p')[0].innerHTML += `${minion.cartoon}<br>`
+          document.getElementsByClassName('weapon-p')[0].innerHTML += `${minion.weapon}<br>`
+        })
+      })
   }
 
   getOneRegister() {
-    APIHandler.get(`/characters/${}`)
-      .then(theChar => {
+    const searchCharId = document.getElementById('char-id').value
+    this.minions.get(`characters/${searchCharId}`)
+      .then(response => {
+        document.getElementsByClassName('name-p')[0].innerHTML = response.data.name
+        document.getElementsByClassName('occupation-p')[0].innerHTML = response.data.occupation
+        document.getElementsByClassName('cartoon-p')[0].innerHTML = response.data.cartoon
+        document.getElementsByClassName('weapon-p')[0].innerHTML = response.data.weapon
 
       })
-
   }
 
   createOneRegister() {
+    const theName = document.getElementsByClassName("the-name")[0].value
+    const theOccupation = document.getElementsByClassName("the-occupation")[0].value
+    const theCartoon = document.getElementById('is-cartoon').value
+    const theWeapon = document.getElementsByClassName("the-weapon")[0].value
+
+
+    const charsModel = {
+      name: theName,
+      occupation: theOccupation,
+      weapon: theWeapon,
+      cartoon: theCartoon
+    }
+    console.log(charsModel)
+    this.minions.post("characters", charsModel)
+
+
+
+
+
+    //   const characterToCreate = {
+    //     name: theName[0].value,
+    //     occupation: theOccupation[0].value,
+    //     weapon: theWeapon[0].value
+    // }
+
+    // charactersApp.post('/', characterToCreate)
+    //     .then(response => {
+    //         const { id, name, occupation } = response.data
+    //         const characterLi = `<li><strong>Personaje creado</strong><br>Nombre: ${name} (id ${id}), ocupación: ${occupation}`
+    //         document.getElementById('characters-list').innerHTML += characterLi
+
+    //         document.getElementById("character-form").reset()
+    //         updateAllCharacters()
+    //     })
+    //     .catch(err => console.log(err))
+
 
   }
 
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,33 +1,29 @@
-const charactersAPI = new APIHandler("")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com")
 
-const fetchOneName = document.getElementsByClassName('name-p')[0]
-const fetchOneOccupation = document.getElementsByClassName('occupation-p')[0]
-const fetchOneCartoon = document.getElementsByClassName('cartoon-p')[0]
-const fetchOneWeapon = document.getElementsByClassName('weapon-p')[0]
 
-const fetchOneButton = document.getElementById('fetch-one')
-const searchCharId = document.getElementById('char-id').value
+
 
 $(document).ready(() => {
 
-  document.getElementById('fetch-all').onclick = function () {
-
-
-  }
-
-  document.getElementById('fetch-one').onclick = function () {
+  document.getElementById('fetch-all').onclick = function (event) {
+    charactersAPI.getFullList()
 
   }
 
-  document.getElementById('delete-one').onclick = function () {
+  document.getElementById('fetch-one').onclick = function (event) {
+    charactersAPI.getOneRegister()
 
   }
 
-  document.getElementById('edit-character-form').onsubmit = function () {
+  document.getElementById('delete-one').onclick = function (event) {
 
   }
 
-  document.getElementById('new-character-form').onsubmit = function () {
+  document.getElementById('edit-character-form').onsubmit = function (event) {
 
+  }
+
+  document.getElementById('new-character-form').onsubmit = function (event) {
+    charactersAPI.createOneRegister()
   }
 })
