02fc7fe93791761631f4f8cc861b0de43c2a7269
Lu Suelves <luciasuelves@gmail.com>
2019-08-01 09:35:41 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,65 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = baseUrl
+    this.axios = axios.create({baseURL:this.BASE_URL})
   }
 
   getFullList () {
-
+    this.axios.get('/')
+     .then(response => {
+        response.data.forEach(elm => {
+         document.getElementsByClassName("container")[0].innerHTML += `<div class="characters-container">
+         <div class="character-info">
+         <div class="name" id="character-name">Id: ${elm.id}</div>
+           <div class="name" id="character-name">Name: ${elm.name}</div>
+           <div class="occupation">Occupation: ${elm.occupation}</div>
+           <div class="cartoon">Is a Cartoon?: ${elm.cartoon}</div>
+           <div class="weapon">Character Weapon: ${elm.weapon}</div>
+         </div>
+       </div> `
+       })
+    })
   }
 
   getOneRegister () {
+    const charId = document.getElementsByName('character-id')[0].value
+    this.axios.get(`/${charId}`)
+      .then(response => {
+        document.getElementsByClassName("name")[0].innerHTML += ': ' +response.data.name
+        document.getElementsByClassName("occupation")[0].innerHTML += ': ' +response.data.occupation
+        document.getElementsByClassName("weapon")[0].innerHTML += ': ' +response.data.weapon
+        document.getElementsByClassName("cartoon")[0].innerHTML += ': ' +response.data.cartoon
 
+      })
   }
 
   createOneRegister () {
-
+    const characterToCreate = {
+        name: document.getElementsByName("name")[0].value, 
+        occupation: document.getElementsByName("occupation")[0].value,
+        weapon: document.getElementsByName("weapon")[0].value,
+        cartoon: document.getElementsByName("cartoon")[0].checked
+    }
+    this.axios.post('/', characterToCreate)
   }
 
   updateOneRegister () {
+    const charId = document.getElementsByName('chr-id')[0].value
+    const updatedCharacter = {
+        name: document.getElementsByName("name")[1].value, 
+        occupation: document.getElementsByName("occupation")[1].value,
+        weapon: document.getElementsByName("weapon")[1].value,
+        cartoon: document.getElementsByName("cartoon")[1].checked
+    }
+    this.axios.put(`/${charId}`, updatedCharacter)
+    .then(()=> document.getElementsByClassName('update-data')[0].innerHTML += `<style>.update-data{color:green; border-color:green}</style>`)
 
   }
 
   deleteOneRegister () {
-
+    const charId = document.getElementsByName('character-id-delete')[0].value
+    this.axios.delete(`/${charId}`)
+      .then(()=> document.getElementsByName('character-id-delete')[0].innerHTML += `<style>#delete-one{color:green; border-color:green}</style>`)
+      .catch(err =>  document.getElementsByName('character-id-delete')[0].innerHTML += `<style>#delete-one{color:red; border-color:red}</style>`)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,27 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://minions-api.herokuapp.com/characters")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+      charactersAPI.getFullList()
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    charactersAPI.getOneRegister()
   }
   
   document.getElementById('delete-one').onclick = function(){
-        
+    charactersAPI.deleteOneRegister()
   }
   
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+  document.getElementById('edit-character-form').onsubmit = function(e){
+    e.preventDefault()
+    charactersAPI.updateOneRegister()
+    document.getElementById("edit-character-form").reset()
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  document.getElementById('new-character-form').onsubmit = function(e){
+    e.preventDefault()
+    charactersAPI.createOneRegister()
+    document.getElementById("new-character-form").reset()
   }
 })
