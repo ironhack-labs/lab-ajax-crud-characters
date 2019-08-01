1ffe1962d4b0fa99ffbf854316157668cc97d2a3
Josue Martin <biojosue@hotmail.com>
2019-08-01 15:42:28 +0200

    api

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,66 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
+    // this.minions = axios.create({ baseURL: baseUrl });
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}characters`)
+      .then(theMinion => {
+        $(".characters-container").empty();
+        theMinion.data.forEach(allTheMinions => {
+          const theHtml = $(
+            `<div class="character-info">
+            <div class="id">Id: ${allTheMinions.id}</div>
+            <div class="name">Name: ${allTheMinions.name}</div>
+            <div class="occupation">Occupation: ${
+              allTheMinions.occupation
+            }</div>
+            <div class="cartoon">Cartoon: ${allTheMinions.cartoon}</div>
+            <div class="weapon">Weapon: ${allTheMinions.weapon}</div>
+            </div>`
+          );
+          $(".characters-container").append(theHtml);
+        });
+      })
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    axios
+      .get(`${this.BASE_URL}characters/:id`)
+      .then(theMinion => {
+        $(".characters-container").empty();
+        const theHtml = $(
+          `<div class="character-info">
+            <div class="id">Id: ${theMinion.data.id}</div>
+            <div class="name">Name: ${theMinion.data.name}</div>
+            <div class="occupation">Occupation: ${
+              theMinion.data.occupation
+            }</div>
+            <div class="cartoon">Cartoon: ${theMinion.data.cartoon}</div>
+            <div class="weapon">Weapon: ${theMinion.data.weapon}</div>
+            </div>`
+        );
+        $(".characters-container").append(theHtml);
+      })
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
+  createOneRegister() {
+    const theName = document.getElementsByClassName("the-name");
+    const theOccupation = document.getElementsByClassName("the-occupation");
+    const theWeapon = document.getElementsByClassName("the-weapon");
 
+    const characterToCreate = {
+      name: theName[0].value,
+      occupation: theOccupation[0].value,
+      weapon: theWeapon[0].value
+    };
   }
 
-  updateOneRegister () {
+  updateOneRegister() {}
 
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
