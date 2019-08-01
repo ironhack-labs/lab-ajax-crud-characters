a761b03294c6913338800e5ef1bcb8a08ac5653d
laanayam333 <laanayam333@gmail.com>
2019-08-01 10:38:22 +0200

    shit

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,26 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseURL) {
+    this.characters = axios.create({ baseURL: baseURL });
   }
 
-  getFullList () {
+  getFullList() {
+    this.characters
+      .get("/characters")
+      .then(characters => {
+        console.log(characters);
 
+        characters.data.forEach(elm => {
+          const { id, name, occupation, weapon } = elm;
+          const characterLi = `<li><strong>Character created</strong><br>Name: ${name} (id ${id}), occupation: ${occupation}, weapon: ${weapon}`;
+          document.getElementById("character-info").innerHTML += characterLi;
+        });
+      })
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
+  createOneRegister() {}
 
-  }
+  updateOneRegister() {}
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
