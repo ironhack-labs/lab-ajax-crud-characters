8ad1fb46e28b014f6b1dbef7acdfe3520553a99c
Maria Pelaez <maria.shecodes@gmail.com>
2019-08-01 09:32:23 +0200

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.minionsapi = axios.create({baseURL: baseUrl})
   }
 
   getFullList () {
-
+    this.minionsapi.get('characters')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   }
 
   getOneRegister () {
-
+    this.minionsapi.get('characters/:id')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   }
 
   createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
+    this.minionsapi.post('characters')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
   }
 
   deleteOneRegister () {
+    this.minionsapi.get('characters/:id')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
+
+  updateOneRegister () {
+    this.minionsapi.put('characters/:id')
+    .then(result => console.log(result))
+    .catch(err => console.log(err))
+  }
 
   }
-}
