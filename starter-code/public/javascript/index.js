751751ed21d4525b622fbb2681d057b6bfb9b959
Ousmane Yade <oyade3@gmail.com>
2019-08-01 09:34:43 +0200

    empty

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,13 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000')
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function() {}
+  mm
+  document.getElementById('fetch-one').onclick = function() {}
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+  document.getElementById('delete-one').onclick = function() {}
+
+  document.getElementById('edit-character-form').onsubmit = function() {}
+
+  document.getElementById('new-character-form').onsubmit = function() {}
 })
