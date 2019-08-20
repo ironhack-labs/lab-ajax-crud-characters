7face761ec0f51d85b0c45e5ffbb516f40170b8d
Alexander Hoinville <ahoin001@gmail.com>
2019-08-20 18:24:39 -0400

    Add fetch single character by ID

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -15,8 +15,6 @@ $(document).ready(() => {
         let theContainer = $('.characters-container');
         theContainer.html('')
 
-        let counter = 0;
-
         characterList.forEach(currentItem => {
 
           // For each character, create a new box with character-info class 
@@ -32,10 +30,6 @@ $(document).ready(() => {
 
           theContainer.append(newBox);
 
-          // counter++;
-
-
-
         })
 
       })
@@ -47,6 +41,39 @@ $(document).ready(() => {
   }
 
   document.getElementById('fetch-one').onclick = function () {
+
+    // connect to container div then clear it of all character boxes
+    let theContainer = $('.characters-container');
+    theContainer.html('')
+
+    // Get value inside the textbox we want to get value from 
+    let characterID = $(`#character-search-id`).val();
+
+    // use API request to get 1 character using the id from user
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${characterID}`)
+      .then((result) => {
+
+        // TODO Why the .data? To get back what kind of useable object?
+        let character = result.data;
+        
+        // create new box and give it proper css class then append it too container
+        let newBox = $('<div></div>');
+        newBox.addClass('character-info');
+        newBox.append(`<h2>ID: ${character.id}</h2>`)
+        newBox.append(`<h2> Name: ${character.name}  </h2>`);
+        newBox.append('</br>');
+        newBox.append(`<h2> Occupation: ${character.occupation}  </h2>`);
+        newBox.append('</br>');
+        newBox.append(`<h2> Weapon: ${character.weapon}  </h2>`);
+        newBox.append('</br>');
+
+        theContainer.append(newBox);
+
+      })
+      .catch((err) => {
+        console.log(`Error pulling single character`,err);
+      })
+
 
   }
 
