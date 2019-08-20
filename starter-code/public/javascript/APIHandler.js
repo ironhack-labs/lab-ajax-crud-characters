b183379b04447bfb1f398daa1ea51bcd022da025
Alexander Hoinville <ahoin001@gmail.com>
2019-08-20 08:26:57 -0400

    Add feature to return all chracters with fetch all

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,41 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+
+    axios.get(this.BASE_URL)
+      .then((result) => {
+
+        //we make an axios request to the url + the searchterm
+
+        let characterList = result.data;
+
+        console.log(characterList);
+
+        return characterList;
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
 
   }
 
-  getOneRegister () {
+  getOneRegister() {
 
   }
 
-  createOneRegister () {
+  createOneRegister() {
 
   }
 
-  updateOneRegister () {
+  updateOneRegister() {
 
   }
 
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,64 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler("https://ih-crud-api.herokuapp.com/characters")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+      .then((result) => {
+
+        //we make an axios request to the url + the searchterm
+
+        let characterList = result.data;
+
+        console.log(characterList);
+
+        let theContainer = $('.characters-container');
+        theContainer.html('')
+
+        let counter = 0;
+
+        characterList.forEach(currentItem => {
+
+          // For each character, create a new box with character-info class 
+
+          let newBox = $('<div></div>');
+          newBox.addClass('character-info');
+          newBox.append(`<h2> Name: ${currentItem.name}  </h2>`);
+          newBox.append('</br>');
+          newBox.append(`<h2> Occupation: ${currentItem.occupation}  </h2>`);
+          newBox.append('</br>');
+          newBox.append(`<h2> Weapon: ${currentItem.weapon}  </h2>`);
+          newBox.append('</br>');
+
+          theContainer.append(newBox);
+
+          // counter++;
+
+
+
+        })
+
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = function () {
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('delete-one').onclick = function () {
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
