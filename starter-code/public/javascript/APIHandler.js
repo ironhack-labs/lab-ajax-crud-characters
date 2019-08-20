265fea408043e7f72f1af822a6801c26e930d8d2
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-08-19 22:03:56 -0400

    almost done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,68 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+
+
+  getFullList() {
+
+    axios.get('https://ih-crud-api.herokuapp.com/characters')
+      .then(result => {
+        let container = $(".characters-container")
+        result.data.map(char => {
+          let list = ` <div class="character-info">
+        <div class="name">${char.name}</div>
+        <div class="occupation">${char.occupation}</div>
+        <div class="cartoon">${char.debt}</div>
+        <div class="weapon">${char.weapon}</div>
+      </div>`
+          container.append(list)
+        });
+      })
 
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${id}`)
+      .then(
+        result => {
+          let container = $(".characters-container")
+          container.html(' ')
+          let list = ` <div class="character-info">
+        <div class="name">${result.data.name}</div>
+        <div class="occupation">${result.data.occupation}</div>
+        <div class="cartoon">${result.data.debt}</div>
+        <div class="weapon">${result.data.weapon}</div>
+      </div>`
+          container.append(list)
+        })
+      .catch(err => console.log(err))
+  }
+
+  createOneRegister() {
+    const name = $('#createName').val()
+    const occupation = $('#createOccupation').val()
+    const weapon = $('createWeapon').val()
+    const charInfo = {
+      name: name,
+      occupation: occupation,
+      weapon: weapon
+    }
+
+
+
 
   }
 
-  createOneRegister () {
+  updateOneRegister() {
 
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`https://ih-crud-api.herokuapp.com/characters/${id}`)
+      .then(response => console.log('deleted ' + response.data))
+      .catch(err => console.log(err))
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,25 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+    charactersAPI.getFullList()
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
+    const id = $('[name= "character-id"]').val()
+    charactersAPI.getOneRegister(id)
+  }
+
+  document.getElementById('delete-one').onclick = function () {
+    const id = $('[name= "character-id-delete"]').val()
+    charactersAPI.deleteOneRegister(id)
+  }
+
+  document.getElementById('edit-character-form').onsubmit = function () {
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
