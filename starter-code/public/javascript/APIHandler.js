9cacd14ad182772149b2818a30eab5fdc0bcdb90
Ant-Quanique Dancy <antdancy95@gmail.com>
2019-08-23 09:46:15 -0400

    created an API to handle crud operations for characters

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,61 @@ class APIHandler {
   }
 
   getFullList () {
+   return axios.get(this.BASE_URL)
+    .then((response) => {
+      console.log(response.data)
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+      return error;
+    })
+  }
+
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL + `/${id}`)
+    .then((response) => {
+      console.log(response.data)
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  createOneRegister (itemToAdd) {
+    return axios.post(this.BASE_URL, itemToAdd)
+    .then((response) => {
+      console.log(response.data)
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id, updatedItem) {
+    return axios.put(this.BASE_URL + `/${id}`, updatedItem )
+    .then((response) => {
+      console.log(response.data)
+      return response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+    
   }
 
-  createOneRegister () {
+  deleteOneRegister (id) {
+    return axios.delete(this.BASE_URL + `/${id}`)
+    .then((response) => {
+      console.log(response.data)
+      return response.data
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   }
 }
