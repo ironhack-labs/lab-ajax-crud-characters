f3ea6ede212c7ec679c4326f060c64e2786afaf0
Lauren Laskerr <llask@me.com>
2019-08-23 11:56:19 -0400

    pushing everything

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -2,24 +2,45 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+  //Get all the characters info from http://localhost:8000/characters
   getFullList () {
+    axios.get(`${BASE_URL}/characters`).then(() => {
+      //array
+      
 
+    })
   }
+  //Get a single character info from http://localhost:8000/characters/:id
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`).then(data => {
+      let html = `<div class="characters-container">
+      <div class="character-info">
+        <div class="name">${data.data.name}</div>
+        <div class="occupation">${data.data.occupation}</div>
+        <div class="cartoon">${data.data.cartoon}</div>
+        <div class="weapon">${data.data.weapon}</div>
+      </div>`
 
-  getOneRegister () {
-
+      document.body.innerHTML += html
+    })
   }
-
+  //Create a single character posting the data to http://localhost:8000/characters
   createOneRegister () {
+    axios.post(`${baseUrl}/characters`).then(() => {
+
+    })
 
   }
-
+  //Edit a single character through his id in http://localhost:8000/characters/:id
   updateOneRegister () {
+    axios.patch(`${baseUrl}/characters/:id`).then(() => {
 
+    })
   }
-
+  //Delete a single character through his id in http://localhost:8000/characters/:id
   deleteOneRegister () {
-
+    axios.delete(`${baseUrl}/characters/:id`).then(() => {
+      
+    })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,11 +2,12 @@ const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
   document.getElementById('fetch-all').onclick = function(){
-
+    charactersAPI.getFullList;
   }
   
   document.getElementById('fetch-one').onclick = function(){
-    
+    let id = document.querySelector('.operation input[name="character-id"]').value
+    charactersAPI.getOneRegister(id);
   }
   
   document.getElementById('delete-one').onclick = function(){
