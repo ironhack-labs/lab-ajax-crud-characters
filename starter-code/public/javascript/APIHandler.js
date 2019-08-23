674a5b24229b2dd9837c165a3352e4b8b55a6daf
Andres Weber <andresmweber@gmail.com>
2019-08-23 13:09:17 -0400

    Done. (CSS is unfinished)

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,61 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
+    console.log(`Starting axios with base URL: ${this.BASE_URL}`)
   }
 
-  getFullList () {
-
+  getFullList() {
+    console.log(`GET request at URL: ${this.BASE_URL}/characters`)
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => {
+        return err
+      })
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    console.log(`GET:id request at URL: ${this.BASE_URL}/characters/${id}`)
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => {
+        return err
+      })
   }
 
-  createOneRegister () {
-
+  createOneRegister(charData) {
+    console.log(`PUT request at URL: ${this.BASE_URL}/characters`, "Data: ", charData)
+    return axios.post(`${this.BASE_URL}/characters`, charData)
+      .then(res => {
+        return res.data
+      })
+      .catch(err => {
+        return err
+      })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updatedCharacterInfo) {
+    console.log(`PATCH request at URL: ${this.BASE_URL}/characters/${id}`, "Data: ", updatedCharacterInfo)
+    return axios.patch(`${this.BASE_URL}/characters/${id}`, updatedCharacterInfo)
+      .then(res => {
+        return res
+      })
+      .catch(err => {
+        return err
+      })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    console.log(`DELETE request at URL: ${this.BASE_URL}/characters/${id}`)
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then(res => {
+        return res
+      })
+      .catch(err => {
+        return err
+      })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,118 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
+var characters = {}
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+function clearCharacters() {
+  for (key of Object.keys(characters)) {
+    characters[key].remove()
+  }
+  characters = {}
+}
 
+function clearFields() {
+  let inputs = document.getElementsByTagName('input')
+  for (index = 0; index < inputs.length; ++index) {
+    inputs[index].value = ""
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+}
+function charViewFromData(data) {
+  return new CharacterView(data.id, data.name, data.cartoon, data.occupation, data.weapon)
+}
+
+function processCharData(data) {
+  for (var propName in data) {
+    if (data[propName] === null || data[propName] === undefined || data[propName] === "") {
+      delete data[propName]
+    }
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+  return data
+}
+
+async function showAll() {
+  let charactersData = await charactersAPI.getFullList()
+  clearCharacters()
+  for (key of Object.keys(charactersData)) {
+    characters[key] = charViewFromData(charactersData[key])
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+}
+class CharacterView {
+  constructor(id, name, cartoon, occupation, weapon) {
+    this.div = document.createElement('DIV')
+    this.idDiv = document.createElement('DIV')
+    this.nameDiv = document.createElement('DIV')
+    this.occupationDiv = document.createElement('DIV')
+    this.cartoonDiv = document.createElement('DIV')
+    this.weaponDiv = document.createElement('DIV')
+
+    this.children = [this.idDiv, this.nameDiv, this.occupationDiv, this.cartoonDiv, this.weaponDiv]
+    this.children.map(c => this.div.appendChild(c))
+
+    this.div.className = 'character-info'
+    this.idDiv.className = 'id'
+    this.nameDiv.className = 'name'
+    this.occupationDiv.className = 'occupation'
+    this.cartoonDiv.className = 'cartoon'
+    this.weaponDiv.className = 'weapon'
+
+    this.idDiv.innerHTML = `ID: ${id}`
+    this.nameDiv.innerHTML = `Name: ${name}`
+    this.cartoonDiv.innerHTML = `Is a Cartoon?: ${cartoon}`
+    this.occupationDiv.innerHTML = `Occupation: ${occupation}`
+    this.weaponDiv.innerHTML = `Weapon: ${weapon}`
+
+    document.querySelector("body > section:nth-child(1) > div").appendChild(this.div)
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+  remove() {
+    this.div.remove()
   }
+}
+
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = async function () {
+    showAll()
+  }
+
+  document.getElementById('fetch-one').onclick = async function () {
+    let id = Number(document.querySelector("body > section:nth-child(1) > section > div:nth-child(2) > input[type=text]").value)
+    let charData = await charactersAPI.getOneRegister(id)
+    clearCharacters()
+    characters[id] = charViewFromData(charData)
+  }
+
+  document.getElementById('delete-one').onclick = async function () {
+    let id = Number(document.querySelector("body > section:nth-child(1) > section > div.operation.delete > input[type=text]").value)
+    await charactersAPI.deleteOneRegister(id)
+  }
+
+  document.getElementById('edit-character-form').onsubmit = async function (e) {
+    let characterData = {
+      name: document.querySelector("#edit-character-form > div:nth-child(2) > input[type=text]").value,
+      occupation: document.querySelector("#edit-character-form > div:nth-child(3) > input[type=text]").value,
+      weapon: document.querySelector("#edit-character-form > div:nth-child(4) > input[type=text]").value,
+      cartoon: document.querySelector("#edit-character-form > div:nth-child(5) > input[type=checkbox]").checked
+    }
+    let id = Number(document.querySelector("#edit-character-form > div:nth-child(1) > input[type=text]").value)
+    await charactersAPI.updateOneRegister(id, processCharData(characterData))
+    clearFields()
+    showAll()
+  }
+
+  document.getElementById('new-character-form').onsubmit = async function (e) {
+    let characterData = {
+      name: document.querySelector("#new-character-form > div:nth-child(1) > input[type=text]").value,
+      occupation: document.querySelector("#new-character-form > div:nth-child(2) > input[type=text]").value,
+      weapon: document.querySelector("#new-character-form > div:nth-child(3) > input[type=text]").value,
+      cartoon: document.querySelector("#new-character-form > div:nth-child(4) > input[type=checkbox]").checked
+    }
+    await charactersAPI.createOneRegister(processCharData(characterData))
+    clearFields()
+    showAll()
+  }
+
+  $("#edit-character-form").submit(function (e) {
+    e.preventDefault();
+  });
+
+  $("#new-character-form").submit(function (e) {
+    e.preventDefault();
+  });
 })
