abbe0209aff5af85550fede31bb5697f29303c70
Osheit <chang.osheit@gmail.com>
2019-08-27 18:34:36 -0400

    push draft- working on features

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,7 +4,6 @@ class APIHandler {
   }
 
   getFullList () {
-
   }
 
   getOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,13 +1,39 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
-
+  // var data = axios.get("http://localhost:8000/characters").then(function(elements) {
+  //   elements.data
+  })
+  //console.log(document.getElementsByClassName('name')[0].innerText) =  
+  document.getElementById('fetch-all').onclick = function getFullList (){
+    axios.get("http://localhost:8000/characters").then(function(elements) {
+     for(e=0; e<elements.data.length;e++){
+          document.getElementsByClassName('name')[0].innerText +=` ${elements.data[e].name}`
+          document.getElementsByClassName('occupation')[0].innerText += ` ${elements.data[e].occupation}`
+          document.getElementsByClassName('cartoon')[0].innerText +=` ${elements.data[e].cartoon}`
+          document.getElementsByClassName('weapon')[0].innerText +=` ${elements.data[e].weapon}`
+    //console.log(document.getElementsByClassName('characters-container'))
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
+})
+}
+    //for(e=1;e<characters-ServiceWorkerContainer.length;e++) {
+    //console.log(axios.get("http://localhost:8000/characters"))
+  //}
+  document.getElementById('fetch-one').onclick = function getOneRegister(){
+    //for(e=0; e<elements.length;e++) {
+      let id = document.getElementsByClassName("operation")[1].children[1].value
+      console.log(id)
+      axios.get("http://localhost:8000/characters").then(function(character){
+        console.log(character.data[id]);
+        document.getElementsByClassName('name')[0].innerText =` ${character.data.name}`
+          document.getElementsByClassName('occupation')[0].innerText = ` ${characters.data.occupation}`
+          document.getElementsByClassName('cartoon')[0].innerText =` ${characters.data.cartoon}`
+          document.getElementsByClassName('weapon')[0].innerText =` ${characters.data.weapon}`
+      //  console.log(elements)
+       })
+    }
     
-  }
+  //}
   
   document.getElementById('delete-one').onclick = function(){
         
@@ -20,4 +46,4 @@ $(document).ready( () => {
   document.getElementById('new-character-form').onsubmit = function(){
                 
   }
-})
+//})
