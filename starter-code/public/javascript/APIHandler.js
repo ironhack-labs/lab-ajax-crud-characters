2e1fbd4cbde4c374f6d3c0b14b6c7507ef03a691
Loup A <loup.aparicio@gmail.com>
2019-09-02 18:51:12 +0200

    get ok

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
-class APIHandler {
-  constructor (baseUrl) {
+// const axios = require("axios");
+
+export default class APIHandler {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.handler = axios.create({
+      baseURL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
+  getFullList(url, clbk) {
+    console.log("bordel");
+    return this.handler.get(url);
+    // this.handler
+    //   .get(`${url}`)
+    //   .then(APIRes => {
+    //     console.log(APIRes);
+    //     clbk("2");
+    //   })
+    //   .catch(err => console.log(err));
   }
 
-  getOneRegister () {
+  getOneRegister() {}
 
-  }
+  createOneRegister() {}
 
-  createOneRegister () {
+  updateOneRegister() {}
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
+
+// module.exports = APIHandler;
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,40 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+import APIHandler from "./APIHandler.js";
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const charactersAPI = new APIHandler("http://localhost:8000");
 
+const target = document.querySelector(".characters-container");
+
+function displayCharacter(chars) {
+  function setTemplate(char) {
+    const tpl = `
+    <div class="character-info">
+      <div class="name">${char.name}</div>
+      <div class="occupation">Character Occupation</div>
+      <div class="cartoon">Is a Cartoon?</div>
+      <div class="weapon">Character Weapon</div>
+    </div>`;
+    return tpl;
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+
+  target.innerHTML = "";
+
+  chars.forEach(char => {
+    console.log(char);
+    target.innerHTML += setTemplate(char);
+  });
+}
+
+document.getElementById("fetch-all").onclick = function() {
+  charactersAPI
+    .getFullList("/characters")
+    .then(res => displayCharacter(res.data))
+    .catch(APIErr => console.error(APIErr));
+};
+
+document.getElementById("fetch-one").onclick = function() {};
+
+document.getElementById("delete-one").onclick = function() {};
+
+document.getElementById("edit-character-form").onsubmit = function() {};
+
+document.getElementById("new-character-form").onsubmit = function() {};
