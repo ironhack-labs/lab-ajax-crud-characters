d4f2ec9fac25cedb7864b29a2ccafacdd00f1f5e
Anaïs Lorteau <anaislor@orange.fr>
2019-09-02 17:56:06 +0200

    need to be fixed

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,42 @@
-class APIHandler {
-  constructor (baseUrl) {
+export class APIHandler {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.handler = axios.create({
+      BASE_URL: this.BASE_URL
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.handler.get();
+    // .then(APIres => console.log(APIres))
+    // .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    this.handler
+      .get(id)
+      .then(APIres => console.log(APIres))
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister(id) {
+    this.handler
+      .post(id)
+      .then(APIres => console.log(APIres))
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, modification) {
+    this.handler
+      .patch(id, modification)
+      .then(APIres => console.log(APIres))
+      .catch(err => console.log(err));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    this.handler
+      .delete(id)
+      .then(APIres => console.log(APIres))
+      .catch(err => console.log(err));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,33 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+import { APIHandler } from "./APIHandler.js";
+const characterContainer = document.querySelector("characters-container");
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const charactersAPI = new APIHandler("http://localhost:8000/characters");
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+document.getElementById("fetch-all").onclick = function() {
+  charactersAPI
+    .getFullList()
+    .then(APIRes => {
+      console.log(APIRes);
+      APIRes.data.forEach(character => {
+        const div = document.createElement("div");
+        characterContainer.appendChild(div);
+        div.classList.add("character-info");
+        div.innerHTML = `<div class="name">Id: ${character.id} </div>
+      <div class="name">Name: ${character.name} </div>
+      <div class="occupation">Occupation:  ${character.occupation} </div>
+      <div class="cartoon">Is a Cartoon?: ${character.cartoon} </div>
+      <div class="weapon">Weapon: ${character.weapon} </div>`;
+      });
+    })
+    .catch(APIErr => {
+      console.log(APIErr, "yo");
+    });
+};
+
+document.getElementById("fetch-one").onclick = function() {};
+
+document.getElementById("delete-one").onclick = function() {};
+
+document.getElementById("edit-character-form").onsubmit = function() {};
+
+document.getElementById("new-character-form").onsubmit = function() {};
