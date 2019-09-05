1cfc8c76a5fe5c41b4665dfea00022dc2b43b764
Adriana Saty Bertelli <Adria@Adria-2.local>
2019-09-05 19:18:18 -0300

    interation2

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
-  }
-
-  getFullList () {
 
   }
 
-  getOneRegister () {
-
+  async getFullList () {
+    const characters = await axios.get(`${this.BASE_URL}/characters`);
+    return characters.data;
   }
 
-  createOneRegister () {
+  async getOneRegister (id) {
+    const register = await axios.get(`${this.BASE_URL}/characters/${id}`);
+    return register.data;
+  }
 
+  async createOneRegister (body) {
+    const create = await axios.post(`${this.BASE_URL}/characters/`, body);
+    return create.data;
+  }
+
+  deleteOneRegister (id) {
+    // const deleteC = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+    // return deleteC.data;
   }
 
   updateOneRegister () {
 
   }
 
-  deleteOneRegister () {
 
-  }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,12 +1,49 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
 $(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+  document.getElementById('fetch-all').onclick = async function(){
+    const fullList = await charactersAPI.getFullList();
+      const charactersContainer = document.getElementsByClassName('characters-container');
+      charactersContainer[0].innerHTML = '';
+      fullList.forEach(element => {
+        charactersContainer[0].innerHTML += `
+            <div class="character-info">
+              <div class="name">Character Name: ${element.name}</div>
+              <div class="occupation">Character Occupation: ${element.occupation}</div>
+              <div class="cartoon">Is a Cartoon? ${element.cartoon}</div>
+              <div class="weapon">Character Weapon: ${element.weapon}</div> 
+            </div>
+            `;
+    });
 
   }
   
-  document.getElementById('fetch-one').onclick = function(){
-    
+  document.getElementById('fetch-one').onclick = async function(){
+    let indexId = document.getElementById('character-id').value;
+      const oneRegister = await charactersAPI.getOneRegister(indexId);
+      const charactersContainer = document.getElementsByClassName('characters-container');
+      charactersContainer[0].innerHTML = '';
+      charactersContainer[0].innerHTML += `
+      <div class="character-info">
+        <div class="name">Character Name: ${oneRegister.name}</div>
+        <div class="occupation">Character Occupation: ${oneRegister.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${oneRegister.cartoon}</div>
+        <div class="weapon">Character Weapon: ${oneRegister.weapon}</div> 
+      </div>
+      `;
+  }
+
+  document.getElementById('new-character-form').onsubmit = async function(event){
+    const body = {
+      name: event.target[0].value,
+      occupation: event.target[1].value,
+      weapon: event.target[2].value,
+      cartoon: event.target[3].checked
+    }
+    await charactersAPI.createOneRegister(body)
+
+    // event.preventDefault();
+            
   }
   
   document.getElementById('delete-one').onclick = function(){
@@ -17,7 +54,5 @@ $(document).ready( () => {
             
   }
   
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
+
 })
