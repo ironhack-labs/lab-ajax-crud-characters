bba7ea321e0ae4e7d2b1f858fa8c650f0e5cedc6
nadia nami abe <abe.nami0510@gmail.com>
2019-09-05 21:53:35 -0300

    done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,72 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+const charactersAPI = new APIHandler('http://localhost:8000');
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = async function () {
+    const response = await charactersAPI.getFullList();
+    const data = await response.data;
 
-  }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
-  }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
-  }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
-  }
-})
+    const all = data
+      .map(
+        item => `
+        <div class="character-info">
+          <div class="name">${item.name}</div>
+          <div class="occupation">${item.occupation}</div>
+          <div class="cartoon">${item.cartoon}</div>
+          <div class="weapon">${item.weapon}</div>
+        </div>
+      `,
+      )
+      .join('');
+
+    document.querySelector('.characters-container').innerHTML = all;
+  };
+
+  document.getElementById('fetch-one').onclick = async function () {
+    const id = document.getElementById('character-id').value;
+
+    const response = await charactersAPI.getOneRegister(id);
+    const data = await response.data;
+
+    const one = `
+        <div class="character-info" key=${data.id}>
+          <div class="name">${data.name}</div>
+          <div class="occupation">${data.occupation}</div>
+          <div class="cartoon">${data.cartoon}</div>
+          <div class="weapon">${data.weapon}</div>
+        </div>
+      `;
+
+    document.querySelector('.characters-container').innerHTML = one;
+  };
+
+  document.getElementById('delete-one').onclick = async function () {
+    try {
+      const id = document.getElementById('character-id-delete').value;
+      await charactersAPI.deleteOneRegister(id);
+
+      $('#delete-one').css('background', 'green');
+    } catch (err) {
+      console.log(err);
+      $('#delete-one').css('background', 'red');
+    }
+  };
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+    const id = $('#edit-id').val();
+    const name = $('#edit-name').val();
+    const occupation = $('#edit-occupation').val();
+    const cartoon = $('#edit-cartoon').val();
+    const weapon = $('#edit-weapon').val();
+
+    charactersAPI.updateOneRegister(id, name, occupation, cartoon, weapon);
+  };
+
+  document.getElementById('new-character-form').onsubmit = function () {
+    const name = $('#create-name').val();
+    const occupation = $('#create-occupation').val();
+    const cartoon = $('#create-cartoon').val();
+    const weapon = $('#create-weapon').val();
+
+    charactersAPI.createOneRegister(name, occupation, cartoon, weapon);
+  };
+});
