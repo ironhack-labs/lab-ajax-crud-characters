d5802aa69c51a3e5af2151f0ce33067578f7652a
Imanol Sanchez <you@example.com>
2019-09-11 20:02:55 -0500

    DONE

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,42 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get('http://localhost:8000/characters').then(response => {
+      return response
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`http://localhost:8000/characters/${id}`).then(response => {
+      return response
+    })
   }
 
-  createOneRegister () {
-
+  createOneRegister(character) {
+    return axios
+      .post(`http://localhost:8000/characters/`, character)
+      .then(response => {
+        return response
+      })
+      .catch(error => {
+        document.getElementById('send-data').style.backgroundColor = 'red'
+        console.log(error)
+        //red
+      })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, character) {
+    return axios.patch(`http://localhost:8000/characters/${id}`, character).then(response => {
+      return response
+    })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(`http://localhost:8000/characters/` + id).then(response => {
+      return response
+    })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,119 @@
-const charactersAPI = new APIHandler("http://localhost:8000")
+//rgba(88, 98, 123, 0.95);
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+const charactersAPI = new APIHandler('http://localhost:8000')
 
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = async function(e) {
+    e.preventDefault()
+    try {
+      const result = await charactersAPI.getFullList()
+      document.getElementById('newlist').innerHTML = ''
+
+      for (let index = 0; index < result.data.length; index++) {
+        theid = result.data[index].id
+        name = result.data[index].name
+        occupation = result.data[index].occupation
+        cartoon = result.data[index].cartoon
+        weapon = result.data[index].weapon
+
+        const newCharacterHTML = `
+        <div class="character-info">
+          <div class="weapon">${theid}</div>
+          <div class="name">${name}</div>
+          <div class="occupation">${occupation}</div>
+          <div class="cartoon">${cartoon}</div>
+          <div class="weapon">${weapon}</div>
+        </div>
+        `
+
+        document.getElementById('newlist').innerHTML += newCharacterHTML
+        document.querySelector('#fetch-all').style.backgroundColor = 'green'
+      }
+    } catch {
+      document.querySelector('#fetch-all').style.backgroundColor = 'red'
+    }
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('fetch-one').onclick = async function(e) {
+    e.preventDefault()
+    const theId = document.getElementById('theId').value
+
+    try {
+      const result = await charactersAPI.getOneRegister(theId)
+      document.getElementById('newlist').innerHTML = ''
+      console.log(result)
+      name = result.data.name
+      occupation = result.data.occupation
+      cartoon = result.data.cartoon
+      weapon = result.data.weapon
+
+      const newCharacterHTML = `
+      <div class="character-info">
+        <div class="name">${name}</div>
+        <div class="occupation">${occupation}</div>
+        <div class="cartoon">${cartoon}</div>
+        <div class="weapon">${weapon}</div>
+      </div>
+      `
+      console.log(newCharacterHTML)
+      document.getElementById('newlist').innerHTML += newCharacterHTML
+      document.querySelector('#fetch-one').style.backgroundColor = 'green'
+    } catch {
+      document.querySelector('#fetch-one').style.backgroundColor = 'red'
+    }
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = async function(e) {
+    e.preventDefault()
+    const id = document.getElementById('editId').value
+    const name = document.getElementById('editName').value
+    const occupation = document.getElementById('editOccupation').value
+    const cartoon = document.getElementById('editCartoon').checked
+    const weapon = document.getElementById('editWeapon').value
+
+    const character = {
+      name,
+      occupation,
+      cartoon,
+      weapon
+    }
+    try {
+      const result = await charactersAPI.updateOneRegister(id, character)
+      document.querySelector('#send-data-edit').style.backgroundColor = 'green'
+    } catch {
+      document.querySelector('#send-data-edit').style.backgroundColor = 'red'
+    }
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  document.getElementById('new-character-form').onsubmit = async function(e) {
+    e.preventDefault()
+    const name = document.getElementById('newName').value
+    const occupation = document.getElementById('newOccupation').value
+    const cartoon = document.getElementById('newCartoon').checked
+    const weapon = document.getElementById('newWeapon').value
+
+    const character = {
+      name,
+      occupation,
+      cartoon,
+      weapon
+    }
+    try {
+      const result = await charactersAPI.createOneRegister(character)
+      document.querySelector('#send-data-new').style.backgroundColor = 'green'
+    } catch {
+      document.querySelector('#send-data-new').style.backgroundColor = 'red'
+    }
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('delete-one').onclick = async function(e) {
+    e.preventDefault()
+    const theId = document.getElementById('theDeleteId').value
+    try {
+      const result = await charactersAPI.deleteOneRegister(theId)
+      document.querySelector('#delete-one').style.backgroundColor = 'green'
+    } catch {
+      document.querySelector('#delete-one').style.backgroundColor = 'red'
+    }
   }
 })
