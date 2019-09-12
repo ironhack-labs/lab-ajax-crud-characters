16d1d1f60944e832c70d1b805693a4242a7f5e86
Choskas <choskasdelta@hotmail.com>
2019-09-11 22:02:40 -0500

    donasbimbotengohambre

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,55 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  async getFullList() {
+    try {
+      const res = await axios.get(`${this.BASE_URL}/characters`)
 
+      return res
+
+    } catch {
+      return false
+    }
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    try {
+      const res = await axios.get(`${this.BASE_URL}/characters/${id}`)
+      console.log("res:" + res)
+      return res
+    } catch {
+      return false
+    }
   }
 
-  createOneRegister () {
-
+  async createOneRegister(elobjeto) {
+    try {
+      const res = await axios.post(`${this.BASE_URL}/characters/`, elobjeto)
+      return true
+    } catch {
+      return false
+    }
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(elobjeto, id) {
+    try {
+      const res = await axios.patch(`${this.BASE_URL}/characters/${id}`, elobjeto)
+      return true
+    } catch {
+      return false
+    }
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    try {
+      const res = await axios.delete(`${this.BASE_URL}/characters/${id}`)
+      return true
+    } catch {
+      return false
+    }
   }
+
+
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,154 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
+const id = document.querySelector('#character-id')
+const updateId = document.querySelector('#chr-id')
+const newName = document.querySelector('#name')
+const newOccupation = document.querySelector('#occupation')
+const newWeapon = document.querySelector('#weapon')
+const newCartoon = document.querySelector('#cartoon')
+const deleteOne = document.querySelector('#delete-one')
+const deleteInput = document.querySelector('#delete-input')
+const updateName = document.querySelector('#update-name')
+const updateWeapon = document.querySelector('#update-weapon')
+const updateCartoon = document.querySelector('#update-checkbox')
+const updateOccupation = document.querySelector('#update-occupation')
+const name = document.querySelector('.name')
+const occupation = document.querySelector('.occupation')
+const weapon = document.querySelector('.weapon')
+const cartoon = document.querySelector('.cartoon')
+const characterInfo = document.querySelector('.character-info')
+const characterContainer = document.querySelector('.characters-container')
+const sendData = document.querySelector('#send-data')
+const createData = document.querySelector('#create-data')
+const fetchOne = document.querySelector('#fetch-one')
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+characterInfo.style.display = 'none'
 
+//$(document).ready(() => {
+document.getElementById('fetch-all').onclick = async function (e) {
+  e.preventDefault()
+  console.log('pto')
+  const {
+    data
+  } = await charactersAPI.getFullList()
+  await showAll(data)
+
+}
+
+
+const showAll = (data) => {
+  characterContainer.innerHTML = ''
+  console.log('ladata:' + data.name)
+  if (data.length) {
+    for (let i = 0; i < data.length; i++) {
+      const cloneCharacter = characterInfo.cloneNode(true)
+      cloneCharacter.children[0].innerHTML = `Name: ${data[i].name}`
+      cloneCharacter.children[1].innerHTML = `Occupation: ${data[i].occupation}`
+      cloneCharacter.children[2].innerHTML = `Weapon: ${data[i].weapon}`
+      cloneCharacter.children[3].innerHTML = `Is a Cartoon?: ${data[i].cartoon}`
+      cloneCharacter.style.display = 'inline'
+      characterContainer.appendChild(cloneCharacter)
+    }
+  } else {
+    console.log('objetos putos')
+    const cloneCharacter = characterInfo.cloneNode(true)
+    cloneCharacter.children[0].innerHTML = `Name: ${data.name}`
+    cloneCharacter.children[1].innerHTML = `Occupation: ${data.occupation}`
+    cloneCharacter.children[2].innerHTML = `Weapon: ${data.weapon}`
+    cloneCharacter.children[3].innerHTML = `Is a Cartoon?: ${data.cartoon}`
+    cloneCharacter.style.display = 'inline'
+    characterContainer.appendChild(cloneCharacter)
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+}
+
+document.getElementById('fetch-one').onclick = async function (e) {
+  e.preventDefault()
+
+  const {
+    data
+  } = await charactersAPI.getOneRegister(id.value)
+  if (!data){
+    fetchOne.style.backgroundColor = 'red'
+  } else {
+  console.log("un resultado pto" + data.name)
+  await showAll(data)
+  fetchOne.style.backgroundColor = 'green'
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+}
+
+document.getElementById('delete-one').onclick = async function (e) {
+  e.preventDefault()
+
+  const result = await charactersAPI.deleteOneRegister(deleteInput.value)
+  console.log(result)
+  if (!result) {
+    deleteOne.style.backgroundColor = 'red'
+  } else {
+    deleteOne.style.backgroundColor = 'green'
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
+
+  //const {
+  //  data
+  //} = await charactersAPI.getFullList()
+  //showAll(data)
+
+}
+
+document.getElementById('edit-character-form').onsubmit = async function (e) {
+  e.preventDefault()
+
+  const agarrar = await charactersAPI.getOneRegister(updateId.value)
+  if (agarrar) {
+    const {
+      data: {
+        id
+      }
+
+    } = agarrar
+    console.log(updateCartoon)
+    const empujar = await charactersAPI.updateOneRegister({
+      name: updateName.value,
+      occupation: updateOccupation.value,
+      weapon: updateWeapon.value,
+      cartoon: updateCartoon.checked
+
+    }, id)
+    const {
+      data
+    } = await charactersAPI.getFullList()
+    showAll(data)
+    sendData.style.backgroundColor = 'green'
+  } else {
+    sendData.style.backgroundColor = 'red'
   }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+}
+
+document.getElementById('new-character-form').onsubmit = async function (e) {
+  e.preventDefault()
+  console.log('pend')
+  const newCharacter = {
+    name: newName.value,
+    occupation: newOccupation.value,
+    weapon: newWeapon.value,
+    cartoon: newCartoon.checked,
   }
-})
+
+  const laCreacion = await charactersAPI.createOneRegister(newCharacter)
+  if (laCreacion) {
+    const {
+      data
+    } = await charactersAPI.getFullList()
+    showAll(data)
+    document.getElementById('new-character-form').reset()
+    createData.style.backgroundColor = 'green'
+  } else {
+    createData.style.backgroundColor = 'red'
+  }
+}
+
+
+
+
+//})
