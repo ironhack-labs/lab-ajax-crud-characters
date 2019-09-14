b92f1e908e0429ec31bda6a8991ad0b86cc9cbc9
Christoph Butenschön <c.butenschoen@gmail.com>
2019-09-14 14:57:51 +0200

    displays single characters by ID, deletes single characters by ID

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,14 +2,39 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    axios.get('http://localhost:8000/characters').then(data => {
+      console.log(data)
+    })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    let char_id = document.getElementById('fetchOneID').value
+
+    axios.get(`http://localhost:8000/characters/${char_id}`).then(data => {
+      let newNode = document.getElementById('characterCard')
+
+      newNode.getElementsByClassName('name')[0].innerText = `Character Name: ` + data.data.name
+      newNode.getElementsByClassName('occupation')[0].innerText = `Occupation: ` + data.data.occupation
+      newNode.getElementsByClassName('cartoon')[0].innerText = 'Cartoon: ' + data.data.cartoon
+      newNode.getElementsByClassName('weapon')[0].innerText = 'Weapon: ' + data.data.weapon
+    })
+
+    {/* <div class="name">Character Name</div>
+        <div class="occupation">Character Occupation</div>
+        <div class="cartoon">Is a Cartoon?</div>
+        <div class="weapon">Character Weapon</div> */}
+
+
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+
+    let char_id = document.getElementById('character-id-delete').value
+
+    axios.delete(`http://localhost:8000/characters/${char_id}`).then(data => {
+      console.log(data)
+    })
 
   });
 
