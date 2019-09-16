95cd855ae22188faba32ab08799c9a14f8096673
banahagos <bana.hagos@yahoo.de>
2019-09-16 10:36:29 +0200

    error messages added

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -15,11 +15,11 @@ window.addEventListener('load', () => {
 
         document.getElementsByClassName('characters-container')[0].append(newNode)
       })
-
     })
+      .catch(err => console.log("something went wrong with getting the list of characters", err))
   });
 
-  document.getElementById('fetch-one').onclick =  function (event) {
+  document.getElementById('fetch-one').onclick = function (event) {
     let searchID = document.getElementById('id-input-search').value
     axios.get(`http://localhost:8000/characters/${searchID}`).then(result => {
       console.log(result)
@@ -28,63 +28,63 @@ window.addEventListener('load', () => {
       document.getElementById('cartoon-info').innerHTML = result.data.cartoon
       document.getElementById('weapon-info').innerHTML = result.data.weapon
     })
+      .catch(err => console.log("something went wrong with getting the character", err))
   };
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
     let deleteID = document.getElementById('id-input-delete').value
-    console.log(deleteID)
     axios.delete(`http://localhost:8000/characters/${deleteID}`).then(result => {
       console.log(result)
       document.getElementById('delete-one').style.background = "green"
-      
+
+    })
+      .catch(err => {
+        console.log("something went wrong with deleting the character", err)
+        document.getElementById('delete-one').style.background = "red"
+      })
   })
-  .catch(err => {
-    console.log(err)
-    document.getElementById('delete-one').style.background = "red"
-  })
-})
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
     event.preventDefault();
     let characterID = document.getElementById('edit-id').value
 
-    axios.put(`http://localhost:8000/characters/${characterID}`, 
-    {
-      name: document.getElementById('edit-name').value,
-      occupation: document.getElementById('edit-occupation').value,
-      weapon: document.getElementById('edit-weapon').value,
-      cartoon: document.getElementById('edit-cartoon').value
-    })
-    .then(result => {
-      console.log(result)
-      document.getElementById('send-update').style.background = "green"
-    })
-    .catch(err => {
-      console.log(err)
-      document.getElementById('send-update').style.background = "red"
-    })
+    axios.put(`http://localhost:8000/characters/${characterID}`,
+      {
+        name: document.getElementById('edit-name').value,
+        occupation: document.getElementById('edit-occupation').value,
+        weapon: document.getElementById('edit-weapon').value,
+        cartoon: document.getElementById('edit-cartoon').value
+      })
+      .then(result => {
+        console.log(result)
+        document.getElementById('send-update').style.background = "green"
+      })
+      .catch(err => {
+        console.log("something went wrong with editing the character", err)
+        document.getElementById('send-update').style.background = "red"
+      })
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
     event.preventDefault();
-  
 
-    axios.post('http://localhost:8000/characters', 
-    {
-      name: document.getElementById('newName').value,
-      occupation: document.getElementById('newOccupation').value,
-      weapon: document.getElementById('newWeapon').value,
-      cartoon: document.getElementById('newCartoon').value
 
-    })
-    .then(result => {
-      console.log(result)
-      document.getElementById('send-data').style.background = "green"
-    })
-    .catch(err => {
-      console.log(err)
-      document.getElementById('send-data').style.background = "red"
-    })
+    axios.post('http://localhost:8000/characters',
+      {
+        name: document.getElementById('newName').value,
+        occupation: document.getElementById('newOccupation').value,
+        weapon: document.getElementById('newWeapon').value,
+        cartoon: document.getElementById('newCartoon').value
+
+      })
+      .then(result => {
+        console.log(result)
+        document.getElementById('send-data').style.background = "green"
+      })
+      .catch(err => {
+        console.log("something went wrong with creating a new character", err)
+        document.getElementById('send-data').style.background = "red"
+      })
 
   });
 });
