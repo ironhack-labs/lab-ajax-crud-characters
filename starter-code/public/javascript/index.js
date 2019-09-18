4c7a52d1ffb66b6269848e8866c474cc74b9c9c4
Maximiliano Sussini <maxsus60@gmail.com>
2019-09-18 16:28:38 +0200

    done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,109 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler(`http://localhost:8000/characters`);
+const characterContainer = document.querySelector(".characters-container");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      printAllCharacters();
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      const id = document.getElementById("fetch-one-input").value;
+      charactersAPI.getOneRegister(id).then(res => {
+        const { data } = res;
+        characterContainer.innerHTML = ``;
+        characterContainer.innerHTML += `
+      <div class="character-info">
+        <div class="name">${data.name}</div>
+        <div class="occupation">${data.occupation}</div>
+        <div class="cartoon">${data.cartoon}</div>
+        <div class="weapon">${data.weapon}</div>
+      </div>
+      `;
+      });
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      const id = document.getElementById("delete-one-input").value;
+      charactersAPI.deleteOneRegister(id).then(() => {
+        printAllCharacters();
+      });
+    });
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      const id = document.getElementById("chr-id").value;
+      const name = document.getElementById("chr-name").value;
+      const occupation = document.getElementById("chr-occupation").value;
+      const weapon = document.getElementById("chr-weapon").value;
+      const cartoon = document.getElementById("chr-cartoon").checked;
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+      charactersAPI
+        .updateOneRegister(
+          id,
+          {name,
+            occupation,
+            weapon,
+            cartoon}
+        )
+        .then(() => {
+          printAllCharacters();
+        })
+        .catch(error => console.log(error));
+    });
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function(event) {
+      event.preventDefault();
+      event.stopPropagation();
+      const name = document.getElementById("createCharacter-name").value;
+      const occupation = document.getElementById("createCharacter-occupation")
+        .value;
+      const weapon = document.getElementById("createCharacter-weapon").value;
+      const cartoon = document.getElementById("createCharacter-cartoon").checked;
+      console.log(name)
+      console.log(occupation)
+      console.log(weapon)
+      console.log(cartoon)
+      charactersAPI
+        .createOneRegister({
+          name,
+          occupation,
+          weapon,
+          cartoon
+        })
+        .then(() => {
+          printAllCharacters();
+        })
+        .catch(error => console.log(error));
+    });
 });
+
+const printAllCharacters = () => {
+  charactersAPI.getFullList().then(res => {
+    const { data } = res;
+    characterContainer.innerHTML = ``;
+    data.forEach(character => {
+      characterContainer.innerHTML += `
+      <div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+      </div>
+      `;
+    });
+  });
+};
