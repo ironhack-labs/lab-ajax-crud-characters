5c7f81c35b73ad15f4978ee46dacf35d09bd554e
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-09-18 15:58:01 +0200

    Pepe Valderrama y Pablo Giral

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,80 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler(
+  "https://ih-crud-api.herokuapp.com/characters"
+);
+const charactersContainer = document.querySelector(".characters-container");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all").addEventListener("click", function(event) {
+      printCharacters();
+    });
 
-  });
+  document
+    .getElementById("fetch-one").addEventListener("click", function(event) {
+      charactersAPI.getOneRegister(document.querySelector('[name="character-id"]').value)
+      .then(character => {
+        
+        charactersContainer.innerHTML = printOneCharacter(character.data);
+      })
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one").addEventListener("click", function(event) {
+      charactersAPI.deleteOneRegister(document.querySelector('[name="character-id-delete"]').value)
+      .then(deletedCharacter => {
+        document.getElementById("delete-one").classList = "success"
+        console.log("Deleted character");
+      })
+      .catch(error => { document.getElementById("delete-one").classList = "error"})
+    });
 
-  });
+  document
+    .getElementById("edit-character-form").addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      charactersAPI.updateOneRegister(document.querySelector('[name="chr-id"]').value, {
+        name: document.querySelector('#updateName').value,
+        occupation: document.querySelector('#updateOccupation').value,
+        weapon: document.querySelector('#updateWeapon').value,
+        debt: document.querySelector('#updateCartoon').checked
+      }).then(updatedCharacter => {
+        document.getElementById("send-data-update").classList = "success"
+        console.log("updated character")
+      })
+      .catch(error => { document.getElementById("send-data-update").classList = "error"})
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("new-character-form").addEventListener("submit", function(event) {
+      event.preventDefault();
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+      charactersAPI.createOneRegister({
+        name: document.querySelector('#newName').value,
+        occupation: document.querySelector('#newOccupation').value,
+        weapon: document.querySelector('#newWeapon').value,
+        debt: document.querySelector('#newCartoon').checked
+      }).then(createdCharacter => {
+        document.getElementById("send-data").classList = "success"
+        console.log("character created")
+      })
+      .catch( error => {document.getElementById("send-data").classList = "error"})
+    });
 });
+
+const printCharacters = () => {
+  charactersAPI.getFullList().then(characters => {
+    charactersContainer.innerHTML = "";
+    characters.data.forEach(character => {
+      charactersContainer.innerHTML += printOneCharacter(character)
+    });
+  });
+};
+
+const printOneCharacter = (character) => {
+  return `<div class="character-info">
+  <div class="name">Name: <span>${character.name}</span></div>
+  <div class="occupation">Occupation: <span>${character.occupation}</span></div>
+  <div class="cartoon">Cartoon: <span>${character.debt}</span></div>
+  <div class="weapon">Weapon: <span>${character.weapon}</span></div>
+</div>`
+}
