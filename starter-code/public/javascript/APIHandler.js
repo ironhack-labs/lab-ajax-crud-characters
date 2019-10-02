563d59481e750bb0393ec6240d49d4dc06be42b3
Anedom <marco.modena@tutanota.com>
2019-10-02 17:41:50 -0300

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,79 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios
+      .create({ baseURL: `${this.BASE_URL}/characters`})
+      .get()
+      .then((res) => {
+        let container = document.getElementsByClassName("characters-container")[0];
+        container.innerHTML = "";
+        res.data.forEach((item) => {
+          let infoDiv = document.createElement("div");
+          infoDiv.className = "character-info";
+          let nameDiv = document.createElement("div");
+          let occupationDiv = document.createElement("div");
+          let cartoonDiv = document.createElement("div");
+          let weaponDiv = document.createElement("div");
+          nameDiv.innerHTML = `Character Name: ${item.name}`;
+          occupationDiv.innerHTML = `Character Occupation: ${item.occupation}`;
+          cartoonDiv.innerHTML = `Is a Cartoon? ${item.cartoon}`;
+          weaponDiv.innerHTML = `Character Weapon: ${item.weapon}`;  
+          infoDiv.appendChild(nameDiv);
+          infoDiv.appendChild(occupationDiv);
+          infoDiv.appendChild(cartoonDiv);
+          infoDiv.appendChild(weaponDiv);
+          container.appendChild(infoDiv);
+        })
+      })
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios
+      .create({ baseURL: `${this.BASE_URL}/characters/${id}`})
+      .get()
+      .then((res) => {
+        document.getElementsByClassName("name")[0].innerHTML = `Character Name: ${res.data.name}`;
+        document.getElementsByClassName("occupation")[0].innerHTML = `Character Occupation: ${res.data.occupation}`;
+        document.getElementsByClassName("cartoon")[0].innerHTML = `Is a Cartoon? ${res.data.cartoon}`;
+        document.getElementsByClassName("weapon")[0].innerHTML = `Character Weapon: ${res.data.weapon}`;
+      })
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister (obj) {
+    axios
+      .post(`${this.BASE_URL}/characters`, obj)
+      .then(() => {
+        document.getElementById("send-data").style.backgroundColor = "green";
+      })
+      .catch(() => {
+        document.getElementById("send-data").style.backgroundColor = "red" ;
+        console.log(err);
+      });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, obj) {
+    axios
+      .put(`${this.BASE_URL}/characters/${id}`, obj)
+      .then(() => {
+        document.getElementById("send-data").style.backgroundColor = "green";
+      })
+      .catch(() => {
+        document.getElementById("send-data").style.backgroundColor = "red" ;
+        console.log(err);
+      });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(() => {
+        document.getElementById("send-data").style.backgroundColor = "green";
+      })
+      .catch(() => {
+        document.getElementById("send-data").style.backgroundColor = "red" ;
+        console.log(err);
+      });
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,34 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister(document.getElementsByName("character-id")[0].value);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(document.getElementsByName("character-id-delete")[0].value)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    let obj = {
+      "name": document.getElementsByName("name")[1].value,
+      "occupation": document.getElementsByName("occupation")[1].value,
+      "weapon": document.getElementsByName("weapon")[1].value,
+      "cartoon": document.getElementsByName("cartoon")[1].value
+    };
+    charactersAPI.updateOneRegister(document.getElementsByName("chr-id")[0].value, obj);
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    let obj = {
+      "name": document.getElementsByName("name")[0].value,
+      "occupation": document.getElementsByName("occupation")[0].value,
+      "weapon": document.getElementsByName("weapon")[0].value,
+      "cartoon": document.getElementsByName("cartoon")[0].value
+    };
+    charactersAPI.createOneRegister(obj);
   });
 });
