b59120dbdafa6ed6c1b1a68b149b08c7a9a347f2
marciano <mcnvrodrigues@gmail.com>
2019-11-09 17:01:33 -0300

    inicial

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,58 @@ class APIHandler {
   }
 
   getFullList () {
+    // let data = undefined;
+    return axios.get(this.BASE_URL + '/characters')
+      // .then(responseFromApi => {
+        // console.log(responseFromApi.data)
+        // responseFromApi.data.forEach(element => {
+        //   const {name, occupation, weapon, cartoon} = element;
+        //   console.log(name);
+        // });
+        // return responseFromApi;
+      // })
+      // .catch(err => console.log("Error while getting the data: ", err));
+
+      // return data;
+  }
+
+  getOneRegister (id) {
+    axios.get(this.BASE_URL + '/characters/' + id)
+      .then(responseFromApi => {
+        console.log(responseFromApi.data)
+      })
+      .catch(err => console.log("Error while getting the data: ", err));
 
   }
 
-  getOneRegister () {
+  createOneRegister (characterInfo) {
+    // const characterInfo = {
+    //          name:       'WALL-E',
+    //          occupation: 'Waste Allocation Robot',
+    //          weapon:     'Head laser'
+    //        };
+     axios.post(this.BASE_URL + '/characters/', characterInfo)
+      .then(responseFromApi => {
+        console.log(responseFromApi.data)
+      })
+      .catch(err => console.log("Error while getting the data: ", err));
 
   }
 
-  createOneRegister () {
-
+  updateOneRegister (id, characterInfo) {
+    axios.put(this.BASE_URL + '/characters/' + id, characterInfo)
+      .then(responseFromApi => {
+        console.log(responseFromApi.data)
+      })
+      .catch(err => console.log("Character not found"));
+    
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL + '/characters/' + id)
+      .then(responseFromApi => {
+        console.log("Character has been successfully deleted ", responseFromApi.data)
+      })
+      .catch(err => console.log("Character not found "));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,7 +2,61 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    document.querySelectorAll('.character-info').forEach(x => {
+      x.parentNode.removeChild(x);
+      console.log('entrou aqui');
+    });
+    
+    charactersAPI.getFullList()
+      .then(responseFromApi =>{
+          responseFromApi.data.forEach(element => {
+          let {name, occupation, weapon, cartoon} = element;
 
+          let newCharacter = document.createElement("div");
+          newCharacter.setAttribute("class", "character-info");
+
+          let name_n = document.createElement("div");
+          name_n.setAttribute("class", "name");
+          name_n.innerHTML = `<div class = "campo">Name:</div> <div class = "valor">${name}</div>`;
+
+          let occupation_n = document.createElement("div");
+          occupation_n.setAttribute("class", "occupation");
+          occupation_n.innerHTML = `<div class = "campo">Occupation:</div> <div class = "valor">${occupation}</div>`;
+
+          if(!cartoon){
+            cartoon = false;            
+          }
+
+          let carton_n = document.createElement("div");
+          carton_n.setAttribute("class", "cartoon");
+          carton_n.innerHTML = `<div class = "campo">Is a cartoon?:</div> <div class = "valor">${cartoon}</div>`;
+          
+          let weapon_n = document.createElement("div");
+          weapon_n.setAttribute("class", "weapon");
+          weapon_n.innerHTML = `<div class = "campo">Weapon:</div> <div class = "valor">${weapon}</div>`;
+
+          newCharacter.appendChild(name_n);
+          newCharacter.appendChild(occupation_n);
+          newCharacter.appendChild(carton_n);
+          newCharacter.appendChild(weapon_n);
+
+          document.querySelector('.characters-container').appendChild(newCharacter);
+
+        });
+      })
+      .catch(err => console.log("Error while getting the data: ", err));
+    // let newCharacter = document.createElement("div");
+    // newCharacter.setAttribute("class", "character-info");
+
+
+    // const {name, occupation, weapon, cartoon} = getFullList();
+
+    // charactersAPI.getFullList().forEach(x => {
+    //   const {name, occupation, weapon, cartoon} = x;
+    //   console.log(name);
+    // });
+
+    
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
@@ -20,4 +74,10 @@ window.addEventListener('load', () => {
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
   });
+
+  // charactersAPI.getFullList();
+  // charactersAPI.getOneRegister(1);
+  // charactersAPI.createOneRegister({name:'WALL - E', occupation: 'Waste Allocation Robot', weapon: 'Head laser'});
+  // charactersAPI.deleteOneRegister(1);
+  // charactersAPI.updateOneRegister(2, {name:'Marciano'})
 });
