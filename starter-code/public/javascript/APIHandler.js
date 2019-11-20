a1777d9547070f67fcfdae2e19aeedf1d6a00d0a
María Simó <maria@coponstudio.es>
2019-11-20 19:04:28 +0100

    Done some

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,56 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}/characters`)
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
   }
 
-  createOneRegister () {
+  createOneRegister(e) {
+    e && e.preventDefault()
 
+    axios({
+      method: 'post',
+      url: `${this.BASE_URL}/characters`,
+      data: {
+
+        name: "Sito",
+        occupation: "Perro flauta",
+        weapon: "Dibusitos",
+        cartoon: true
+
+      },
+    })
   }
 
-  updateOneRegister () {
+  updateOneRegister(e, id) {
+    e && e.preventDefault()
 
+    axios({
+      method: 'put',
+      url: `${this.BASE_URL}/characters/${id}`,
+      data: {
+        id : id,
+        name: "Sito",
+        occupation: "Perro flauta",
+        weapon: "Dibusitos",
+        cartoon: true
+      },
+    }).then(() => {
+      console.log("Updated. Bg green")
+    })
+    .catch(err => console.log(`${err} the bg needs to be red`))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`).then(oneCharacter => {
+      console.log(oneCharacter.data)
+    })
   }
 }
+
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,63 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  let card = document.querySelector('.characters-container')
+  let cardHTML;
 
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+      .then((characterList) => {
+        card.innerHTML = ''
+        characterList.data.forEach(el => {
+
+          cardHTML = `<div class="character-info">
+          <div class="id">Id:  <span>${el.id}</span></div>
+          <div class="name">Name: <span>${el.name}</span></div>
+          <div class="occupation">Occupation: <span>${el.occupation}</span></div>
+          <div class="cartoon">Is a Cartoon? <span>${el.cartoon}</span></div>
+          <div class="weapon">Weapon <span>${el.weapon}</span></div>
+        </div>`
+
+          card.innerHTML += cardHTML;
+        })
+      })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    let characterID = document.querySelector('input[name="character-id"]').value
+    charactersAPI.getOneRegister(characterID)
+      .then(oneCharacter => {
 
+        card.innerHTML = ''
+        let el = oneCharacter.data
+
+          cardHTML = `<div class="character-info">
+          <div class="id">Id:  <span>${el.id}</span></div>
+          <div class="name">Name: <span>${el.name}</span></div>
+          <div class="occupation">Occupation: <span>${el.occupation}</span></div>
+          <div class="cartoon">Is a Cartoon? <span>${el.cartoon}</span></div>
+          <div class="weapon">Weapon <span>${el.weapon}</span></div>
+        </div>`
+
+          card.innerHTML += cardHTML;
+      
+      })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    let characterID = document.querySelector('input[name="character-id-delete"]').value
+    charactersAPI.deleteOneRegister(characterID)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    let characterID = document.querySelector('input[name="chr-id"]').value
+    charactersAPI.updateOneRegister(event, characterID)
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.createOneRegister(event)
+    .then((createdCharacter) => {
+      console.log(createdCharacter.data)
+    })
   });
 });
