310581a9c8b7783c240e94ba99827f04f15a571a
Javier Agudo <javieragudo3d@gmail.com>
2019-11-20 19:30:05 +0100

    Fckng masters

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,20 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+     return axios.get(`${this.BASE_URL}`)}
 
-  }
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/${id}`)}
 
-  getOneRegister () {
+  createOneRegister(character) {
+    return axios.post(`${this.BASE_URL}`, character)}
 
-  }
+  updateOneRegister(id, character) {
+    return axios.put(`${this.BASE_URL}/${id}`,character)}
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/${id}`)}
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,76 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000/characters");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+window.addEventListener("load", () => {
+  document.getElementById("fetch-all").addEventListener("click", function(event) {
+    charactersAPI.getFullList()
+    .then(characters => {
+      document.querySelector(".characters-container").innerHTML = "";
+      let { data } = characters;
+      data.forEach((character) => {
+      document.querySelector(".characters-container").innerHTML += `<div class="characters-container">
+      <div class="character-info">
+      <div class="name">${character.name}</div>
+      <div class="occupation">${character.occupation}</div>
+      <div class="cartoon">${character.cartoon}</div>
+      <div class="weapon">${character.weapon}</div>
+    </div>
+  </div>`
+        
+      })
+    })
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById("fetch-one").addEventListener("click", function(event) {
+    let id = document.querySelector('input[name="character-id"]').value;
+    charactersAPI.getOneRegister(id)
+    .then(character => {
+      console.log(character)
+      document.querySelector(".characters-container").innerHTML = "";
+      document.querySelector(".characters-container").innerHTML += `<div class="characters-container">
+      <div class="character-info">
+      <div class="name">${character.data.name}</div>
+      <div class="occupation">${character.data.occupation}</div>
+      <div class="cartoon">${character.data.cartoon}</div>
+      <div class="weapon">${character.data.weapon}</div>
+    </div>
+  </div>`
 
+    })
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function(event) {
+      let id = document.querySelector('input[name="character-id-delete"]').value;
+      charactersAPI.deleteOneRegister(id)
+    });
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function(event) {
+      let id = document.querySelector('input[name="chr-id"]').value;
+      let character = {
+        name : document.querySelector('input[name="name2"]').value,
+        occupation : document.querySelector('input[name="occupation2"]').value,
+        weapon : document.querySelector('input[name="weapon2"]').value,
+        cartoon : document.querySelector('input[name="cartoon2"]').value,
+      }
+      charactersAPI.updateOneRegister(id,character)
+    });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function(event) {
 
-  });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+      let character = {
+        name : document.querySelector('input[name="name"]').value,
+        occupation : document.querySelector('input[name="occupation"]').value,
+        weapon : document.querySelector('input[name="weapon"]').value,
+        cartoon : document.querySelector('input[name="cartoon"]').value,
+      }
+      charactersAPI.createOneRegister(character)
+    });
 });
