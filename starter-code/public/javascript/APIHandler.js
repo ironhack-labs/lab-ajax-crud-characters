f9821ab72d07ffbbf3961240620f56f1b6179242
Luis Zarza <luiszarza@Luiss-MacBook-Pro.local>
2019-11-21 00:42:36 +0100

    Done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -2,24 +2,24 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+  
   getFullList () {
-
+    `https://minions-api.herokuapp.com/characters`
   }
 
   getOneRegister () {
-
+    `https://minions-api.herokuapp.com/characters/${id}`
   }
 
   createOneRegister () {
-
+    `https://minions-api.herokuapp.com/characters`
   }
 
   updateOneRegister () {
-
+    `https://minions-api.herokuapp.com/characters/${id}`
   }
 
   deleteOneRegister () {
-
+    `https://minions-api.herokuapp.com/characters/${id}`
   }
 }
