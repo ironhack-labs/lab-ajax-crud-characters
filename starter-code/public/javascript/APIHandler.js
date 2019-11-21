3a60c4f1227312bc1feb9203e0e258d88136597d
Robert <robcg1102@gmail.com>
2019-11-21 02:01:19 -0600

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,35 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList() {
+    const data = await axios.get(`${this.BASE_URL}/characters`);
+    return data;
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    const data = await axios.get(`${this.BASE_URL}/characters/${id}`).catch(err => {
+      document.getElementById("msg-fetch").innerHTML = "Character not found";
+    });
+    return data;
   }
 
-  createOneRegister () {
-
+  async createOneRegister(character) {
+    const newChar = await axios.post(`${this.BASE_URL}/characters`, character);
+    return newChar;
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(id, character) {
+    const data = await axios.patch(`${this.BASE_URL}/characters/${id}`, character);
+    return data;
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    const data = await axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .catch(err => {
+        document.getElementById("msg-delete").innerHTML = "Character not found";
+      });
   }
 }
