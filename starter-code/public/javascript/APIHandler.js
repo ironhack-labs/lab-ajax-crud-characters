551263802d61ab7dc7f5af47948bd5ce54bcfa8b
EChColorado <edxz7c@gmail.com>
2019-11-20 18:45:12 -0600

    Iteration 2

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,18 +1,21 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.charactersService = axios.create({
+      baseURL: `${baseUrl}`
+    })
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`)
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return this.charactersService.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister (name, occupation, weapon, cartoon) {
+    this.charactersService.post('/characters', { name, occupation, weapon, cartoon})
   }
 
   updateOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,12 +1,44 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    const card = document.querySelector('.characters-container');
+    card.innerHTML ="";
+    const {data} = await charactersAPI.getFullList()
+    data.forEach(character => {
+      const newCard = document.createElement("div");
+      newCard.className = "character-info";
+      
+      const name = document.createElement("div");
+      name.className="name";
+      name.innerHTML =  `Name: ${character.name}`;
+      newCard.appendChild(name);
 
+      const occupation = document.createElement("div");
+      occupation.className="occupation";
+      occupation.innerHTML = `Occupation: ${character.occupation}`;
+      newCard.appendChild(occupation);
+
+      const cartoon = document.createElement("div");
+      cartoon.className = "cartoon";
+      cartoon.innerHTML = `Is a cartoon?: ${character.cartoon}`;
+      newCard.appendChild(cartoon);
+
+      const weapon = document.createElement("div");
+      weapon.className = "weapon";
+      weapon.innerHTML =  `Weapon: ${character.weapon}`;
+      newCard.appendChild(weapon);
+
+      card.appendChild(newCard);
+    });
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    const card = document.querySelector('.characters-container');
+    card.innerHTML ="";
+    const id = document.querySelector('[name = character-id]').value
+    const {data} = await charactersAPI.getOneRegister(id);
+    console.log(data)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
