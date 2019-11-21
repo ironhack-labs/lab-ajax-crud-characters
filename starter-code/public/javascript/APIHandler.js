47bd91bcbe4e2bc4619c5e06420f87397347be7a
Jessica <54844929+jessica-git@users.noreply.github.com>
2019-11-21 09:25:44 +0100

    not done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,33 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.minionApi = axios.create(
+      {
+        baseURL: baseUrl
+      }
+    )
   }
 
   getFullList () {
+    return axios.get(`${this.BASE_URL}/characters`)     //-----------
 
   }
 
   getOneRegister () {
-
+    return axios.get(`${this.BASE_URL}/characters/${id}`)          //-------------
   }
 
-  createOneRegister () {
+  createOneRegister () {  
+    return axios.post(`${this.BASE_URL}/characters`, character)           //------------
 
   }
 
   updateOneRegister () {
+    return axios.put(`${this.BASE_URL}/characters/${id}`, character)            //-----------
 
   }
 
   deleteOneRegister () {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)            //-----------
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,16 +1,19 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com/');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    getAllChar();   //-----------
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    const id = document.querySelector("#character-id").value;
 
+    getOneChar(id)                  //-----------------
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const idDelete = document.querySelector("#deleteOne").value;
+    charactersAPI.deleteOneRegister(idDelete)               //---------------
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
