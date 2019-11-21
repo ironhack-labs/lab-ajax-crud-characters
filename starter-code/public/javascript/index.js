1cf3b078c8ee35295a2eb8b456da83c9652f6b6f
keidu <guille.kfb@gmail.com>
2019-11-21 09:28:33 +0100

    subiendo

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,13 +1,25 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com/')
+
+const inputs = document.querySelectorAll('input')
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
-
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
 
   });
+
+  document.getElementById('fetch-one').onclick = () => {
+    const id = inputs[0].value
+    axios.get(`https://minions-api.herokuapp.com/characters/${id}`)
+      .then(response => {
+        const { name, occupation, weapon } = response.data
+        console.log(name, occupation, weapon)
+      })
+      .catch(error => console.log('error:', error))
+  }
+
+
+
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
