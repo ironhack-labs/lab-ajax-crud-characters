49334823cabbe66cd0ae7021e7bee9511ad403c9
pyrrhos-msits <menzelf@mediamarktsaturn.com>
2020-01-10 17:29:51 +0100

    lasst Stand

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,33 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(this.BASE_URL + '/characters')
+    .then(response => response.data );
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL+'/characters/'+id)
+    .then(response => console.log(response.data));
   }
 
-  createOneRegister () {
-
+  createOneRegister (body) {
+    return  axios.post(this.BASE_URL+'/characters/', body)
+    .then(response => response.data);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, data) {
+    return axios.patch(this.BASE_URL + '/characters/'+ id, data)
+    .then(response => response.data);
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (id) {
+    return axios.delete(this.BASE_URL + '/characters/'+id)
+    .then(response => response.data)
+    .catch(error => {
+      console.log(error);
+    });
+
 
   }
 }
+
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,20 +1,37 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const inputSearchID = document.querySelector("input[name=character-id]");
+const idToDelete = document.querySelector("input[name=character-id-delete]");
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+      charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+      // funktioniert weil im html ein leeres 'value' definiert ist
+      charactersAPI.getOneRegister(inputSearchID.value );
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+        const id = idToDelete.value;
+        charactersAPI.deleteOneRegister(id);
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+        event.preventDefault();
+        const idToChange = document.querySelector("input[name=chr-id]");
+        const id = idToChange.value;
+        const name = document.querySelector("#edit-character-form [name=name]");
+        const nameVal = name.value;
+        console.log(nameVal)
+        const occupation = document.querySelector("#edit-character-form [name=occupation]");
+        const weapon = document.querySelector("#edit-character-form[name=weapon]");
+        const cartoon = document.querySelector("#edit-character-form [name=cartoon]");
+        let data = {name: nameVal  , occupation: occupation.value , weapon: weapon.value, cartoon: cartoon.value};
+        
+        console.log(data);
+        charactersAPI.updateOneRegister(id, data );
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
