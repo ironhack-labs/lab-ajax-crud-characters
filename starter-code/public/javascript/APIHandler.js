235453304896af422c76d5bba35d10e297e555be
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2020-01-10 16:55:17 +0100

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,22 +4,32 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios
+      .get( `${this.BASE_URL}/characters` )
+      .then( response => { return response.data; } );
   }
 
-  getOneRegister () {
-
+  getOneRegister ( id ) {
+    return axios
+      .get( `${this.BASE_URL}/characters/${id}` )
+      .then( response => { return response.data; } );
   }
 
-  createOneRegister () {
-
+  createOneRegister ( character ) {
+    return axios
+      .post( `${this.BASE_URL}/characters`, character )
+      .then( response => { return response.data; } );
   }
 
-  updateOneRegister () {
-
+  updateOneRegister ( id, character ) {
+    return axios
+      .patch( `${this.BASE_URL}/characters/${id}`, character )
+      .then( response => { console.log( "UPD", response.data ); return response.data; } );
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister ( id ) {
+    return axios
+      .delete( `${this.BASE_URL}/characters/${id}` )
+      .then( response => { return response.data; } );
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,22 +2,105 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
+      .then( characters => {
+        console.log( "getFullList RESPONSE", characters );
+        createCharacterInfos( characters );
+      })
+      .catch( err => {
+        
+      });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    let oneRegisterId = document.getElementById('fetch-one-input').value;
 
+    if( oneRegisterId !== "" ){
+      charactersAPI.getOneRegister(oneRegisterId)
+        .then( character => {
+          console.log( "getOneRegister RESPONSE", character );
+          createCharacterInfos( [character] );
+        })
+        .catch( err => {
+        
+        });
+    }
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    let oneRegisterId = document.getElementById('fetch-one-input').value;
+
+    if( oneRegisterId !== "" ){
+      charactersAPI.getOneRegister(oneRegisterId)
+        .then( character => {
+          console.log( "getOneRegister RESPONSE", character );
+        })
+        .catch( err => {
+        
+        });
+    }
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    let updateCharacter = {
+      "name": "Dirk",
+      "occupation": "Actor",
+      "weapon": "Hats",
+      "cartoon": true
+    };
+    let id = 1;
+    
+    console.log("UPD");
 
+    charactersAPI.updateOneRegister(id, updateCharacter)
+      .then( character => {
+        console.log( "updateOneRegister RESPONSE", character );
+      })
+      .catch( err => {
+      
+      });
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    let newCharacter = {
+      "name": "Susanne",
+      "occupation": "Actress",
+      "weapon": "Shoes",
+      "cartoon": false
+    };
 
+    charactersAPI.createOneRegister(newCharacter)
+      .then( character => {
+        console.log( "createOneRegister RESPONSE", character );
+      })
+      .catch( err => {
+      
+      });
   });
 });
+
+function createCharacterInfos( characters ){
+  let charCont = document.getElementById('characterContainer');
+  charCont.innerHTML = "";
+
+  characters.forEach(element => {
+    let contInfoDiv = document.createElement("DIV");
+    contInfoDiv.classList.add("character-info");
+
+    contInfoDiv.appendChild( createInfoDiv( "DIV", "ID", element.id, "name" ) );
+    contInfoDiv.appendChild( createInfoDiv( "DIV", "NAME", element.name, "name" ) );
+    contInfoDiv.appendChild( createInfoDiv( "DIV", "OCCUPATION", element.occupation, "occupation" ) );
+    contInfoDiv.appendChild( createInfoDiv( "DIV", "IS CARTOON", element.cartoon, "cartoon" ) );
+    contInfoDiv.appendChild( createInfoDiv( "DIV", "WEAPON", element.weapon, "weapon" ) );
+
+    charCont.appendChild(contInfoDiv);
+  });
+};
+
+function createInfoDiv( tag, name, data, className ){
+  let infoDiv = document.createElement(tag);
+  infoDiv.innerHTML = name + ": " + data;
+  infoDiv.classList.add(className);
+  return infoDiv;
+}
