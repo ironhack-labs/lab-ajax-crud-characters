2a6236877af65cacf928d5baf0daae32c58caa3a
Bialas <bialaska@media-saturn.com>
2020-01-10 17:37:33 +0100

    done until iteration 3

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,25 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl){
+      this.baseUrl = baseUrl;
   }
-
-  getFullList () {
-
+  getAllCharacters(){
+      return axios.get(this.baseUrl + "/characters")
+      .then(response => response.data)
   }
-
-  getOneRegister () {
-
+  getSingleCharacter(id){
+      return axios.get(this.baseUrl + "/characters/" + id)
+      .then(response => response.data)
   }
-
-  createOneRegister () {
-
+  createSingleCharacter(obj){
+      return axios.post(this.baseUrl + "/characters", obj)
+      .then(response => response.data)
   }
-
-  updateOneRegister () {
-
+  deleteSingleCharacter(id){
+      return axios.delete(this.baseUrl + "/characters/" + id)
+      .then(response => response.data)
   }
-
-  deleteOneRegister () {
-
+  editSingleCharacter(id, obj){
+      return axios.patch(this.baseUrl + "/characters/" + id, obj)
+      .then(response => response.data)
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,7 +2,24 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getAllCharacters()
+    .then(allChar => {
+      console.log(allChar);
+      
+        let allCharHtml = "";
+        for (let i=0; i<allChar.length; i++) {
+          allCharHtml += `
+            <div class="character-info">
+              <div class="name">Id: ${allChar[i].id}</div>
+              <div class="name">Name: ${allChar[i].name}</div>
+              <div class="occupation">Occupation: ${allChar[i].occupation}</div>
+              <div class="cartoon">Is a Cartoon?: ${allChar[i].cartoon}</div>
+              <div class="weapon">Weapon: ${allChar[i].weapon}</div>
+            </div>
+          `;
+        }
+        document.querySelector('.characters-container').innerHTML = allCharHtml;  
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
