9ca64b252b4df0f3e610ba0f9eccdf598323cc7e
Bialas <bialaska@media-saturn.com>
2020-01-13 23:55:49 +0100

    update with edit-character

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -20,7 +20,7 @@ window.addEventListener('load', () => {
       document.querySelector('.characters-container').innerHTML = allCharHtml;  
     })
     .catch(error => {
-      console.log(error);
+      console.log("The error is: ", error);
     })
   });
 
@@ -29,7 +29,7 @@ window.addEventListener('load', () => {
     charactersAPI.getSingleCharacter(oneId)
     .then(oneChar => {
       //console.log(oneChar);
-      removeError();
+      removeErrorDivChar();
       let oneCharHtml = "";
       if(oneId !== ""){
         oneCharHtml = `
@@ -44,82 +44,153 @@ window.addEventListener('load', () => {
       document.querySelector('.characters-container').innerHTML = oneCharHtml;
       }
       else{
-        createErrorDiv()
+        createErrorDivChar()
         const missInputError = document.createTextNode("Please enter an ID");
         errDiv.appendChild(missInputError);
       }      
       
     })
     .catch(error => {
-      console.log(error);
+      console.log("The error is: ", error);
     })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {    
     //event.preventDefault();
     const oneDeleteId = document.getElementById("characterIdDelete").value;
-    if(oneDeleteId !== ""){
-      removeError();
+    if(oneDeleteId !== ""){      
       charactersAPI.deleteSingleCharacter(oneDeleteId)
-        .then(oneDelChar => {      
-          console.log(oneDelChar)
+        .then(oneDelChar => {      /* oneDelChar not needed --> .then(() => {})*/
+          //console.log(oneDelChar)
           var elementToColor = document.getElementById("delete-one");        
           elementToColor.classList.add("green");
-          console.log(elementToColor)          
+          //console.log(elementToColor)    /* console.log is displayed shortly & disappears immediately*/      
         })
         .catch(error => {
           console.log(error);
           var elementToColor = document.getElementById("delete-one");        
           elementToColor.classList.add("red");
-          console.log(elementToColor)
+          //console.log(elementToColor)
         })      
     }
     else{
-      createErrorDiv()
+      createErrorDivChar()
       const missInputError = document.createTextNode("Please enter an ID");
       errDiv.appendChild(missInputError);
     }      
 
   });
   
+  const theNames = document.getElementsByClassName("the-name");  
+  const theOccupations = document.getElementsByClassName("the-occupation");
+  const theWeapons = document.getElementsByClassName("the-weapon");
+  const theCartoons = document.getElementsByClassName("is-cartoon");
+
+  
+  /* document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const oneEditId = document.getElementById("characterIdEdit").value;
+    charactersAPI.getSingleCharacter(oneEditId)
+      .then(response => {
+        console.log('Response from the API is: ', response);
+        theNames[1].value = response.name;
+        theOccupations[1].value = response.occupation;
+        theWeapons[1].value = response.weapon;
+        theCartoons[1].checked = response.cartoon
+        
+      })
+      .catch(error => {
+          console.log("The error is: ", error);
+      });
+  }); */
+
+  document.getElementById('get-data').addEventListener('click', function (event) {
+    event.preventDefault();
+    const oneEditId = document.getElementById("characterIdEdit").value;
+    charactersAPI.getSingleCharacter(oneEditId)
+      .then(response => {
+        console.log('Response from the API is: ', response);
+        theNames[1].value = response.name;
+        theOccupations[1].value = response.occupation;
+        theWeapons[1].value = response.weapon;
+        theCartoons[1].checked = response.cartoon
+        
+      })
+      .catch(error => {
+          console.log("The error is: ", error);
+      });
+  });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const oneEditId = document.getElementById("characterIdEdit").value;
+    const updatedCharacterInfo = {
+      name: theNames[1].value,
+      occupation: theOccupations[1].value,
+      weapon: theWeapons[1].value,      
+      cartoon: theCartoons[1].checked
+    };
     
+    charactersAPI.editSingleCharacter(oneEditId, updatedCharacterInfo)
+      .then(response => {
+        console.log('update successful: ', response);
+        var elementToColor = document.getElementById("send-data-updated");        
+        elementToColor.classList.add("green");       
+      })
+      .catch(error => {
+          console.log("The error is: ", error);
+          var elementToColor = document.getElementById("send-data-updated");        
+          elementToColor.classList.add("red");
+      });
   });
+
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
     event.preventDefault();
-    const theNames = document.getElementsByClassName("the-name");
-    const theOccupations = document.getElementsByClassName("the-occupation");
-    const theWeapons = document.getElementsByClassName("the-weapon");
-    const theCartoons = document.getElementsByClassName("is-cartoon");
-
     const characterInfo = {
       name: theNames[0].value,
       occupation: theOccupations[0].value,
-      weapon: theWeapons[0].value,
-      cartoon: theCartoons[0].value
+      weapon: theWeapons[0].value,      
+      cartoon: theCartoons[0].checked
     };
-    charactersAPI.createSingleCharacter(characterInfo)
-        .then(oneNewChar => {      
-          console.log(oneNewChar)                    
-        })
-        .catch(error => {
-          console.log(error);          
-        })
+    if(characterInfo.name !== ""){
+      charactersAPI.createSingleCharacter(characterInfo)
+          .then(() => {            
+            var elementToColor = document.getElementById("send-data");        
+            elementToColor.classList.add("green");                    
+          })
+          .catch(error => {
+            console.log("The error is: ", error);
+            var elementToColor = document.getElementById("send-data");        
+            elementToColor.classList.add("red");          
+          })
+    }
+    else{
+      createErrorDivForm()
+      const missInputError = document.createTextNode("Please enter at least a name");
+      errDiv.appendChild(missInputError);
+
+    }
   });
+
 });
 
 
-function removeError() {
+function removeErrorDivChar() {
   if (document.getElementById("error")) {
     const error = document.getElementById("error");
     error.parentNode.removeChild(error);
   }
 }
 
-function createErrorDiv(){
+function createErrorDivChar(){
   errDiv = document.createElement("div");
   errDiv.setAttribute("id", "error");
-  document.querySelector('.container').appendChild(errDiv);
+  document.querySelector('.operations').appendChild(errDiv);
+}
+
+function createErrorDivForm(){
+  errDiv = document.createElement("div");
+  errDiv.setAttribute("id", "error");
+  document.querySelector('.form-container').appendChild(errDiv);
 }
