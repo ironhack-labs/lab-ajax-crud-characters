6790a8acba1fbadcc9d3b4b74841d6c2aa61ab84
Tonalli <tonalli@xanum.uam.mx>
2020-01-16 17:51:30 -0600

    First attempt to conect handler with index.js

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -4,17 +4,19 @@ class APIHandler {
   }
 
   getFullList () {
-    axios.get(`${this.BASE_URL}/characters`)
+    return axios.get(`${this.BASE_URL}/characters`)
     .then(response => {
-      console.log(`Response from the API is :`, response.data);
+      // console.log(`Response from the API is :`, response.data);
+      return response.data;
     })
     .catch(error => console.log('The error is: ', error));
   }
 
   getOneRegister (theId) {
-    axios.get(`${this.BASE_URL}/characters/${theId}`)
+    return axios.get(`${this.BASE_URL}/characters/${theId}`)
     .then(response => {
-      console.log(`Response from the API is :`, response.data);
+      // console.log(`Response from the API is :`, response.data);
+      return response.data;
     })
     .catch(error => {
       if (error.response.status == 404){
@@ -26,22 +28,22 @@ class APIHandler {
   }
 
   createOneRegister (characterInfo) {
-    axios.post(`${this.BASE_URL}/characters`, characterInfo)
+    return axios.post(`${this.BASE_URL}/characters`, characterInfo)
     .then(response => {
-      console.log(`Response from the API is :`, response.data);
+      // console.log(`Response from the API is :`, response.data);
+      return response.data;
     })
     .catch(error => console.log('The error is: ', error));
   }
-// { name: 'Batman', occupation: 'hero', cartoon: true, weapon: 'A lot' }
-// { name: 'Robin', occupation: 'hero', cartoon: true, weapon: 'some' }
 
   updateOneRegister (theId, characterInfo) {
-    axios.get(`${this.BASE_URL}/characters/${theId}`)
+    return axios.get(`${this.BASE_URL}/characters/${theId}`)
     .then(response => {
       const data = Object.assign({}, response.data, characterInfo);
-      axios.put(`${this.BASE_URL}/characters/${theId}`, data)
+      return axios.put(`${this.BASE_URL}/characters/${theId}`, data)
       .then(res => {
-        console.log(`Response from the API is :`, res.data);
+        // console.log(`Response from the API is :`, res.data);
+        return res.data;
       })
       .catch(err => console.log('The error is: ', err));
     })
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,27 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    const fullList = await charactersAPI.getFullList();
+    console.log(fullList);
+  });
+
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    const oneElement = await charactersAPI.updateOneRegister(1);
+    console.log(oneElement);
+  });
+
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    // { name: 'Batman', occupation: 'hero', cartoon: true, weapon: 'A lot' }
+    // { name: 'Robin', occupation: 'hero', cartoon: true, weapon: 'some' }
 
   });
 });
