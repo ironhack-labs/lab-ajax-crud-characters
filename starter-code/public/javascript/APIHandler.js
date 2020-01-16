03ec8ebc9681afecab94701cccb86f114e25d825
Ardash <56517452+ardache@users.noreply.github.com>
2020-01-16 10:29:59 -0600

    progress Iteration 2
    :aerial_tramway:

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,14 +1,18 @@
+import axios from "axios";
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    return axios.get(this.BASE_URL + '/characters')
+     .then(response => response.data );
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL+'/characters/'+id)
+    .then(response => console.log(response.data));
   }
 
   createOneRegister () {
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,12 +1,13 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const inputSearchID = document.querySelector("input[name=character-id]");
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister(inputSearchID.value );
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
