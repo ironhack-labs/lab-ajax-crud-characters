a91b10cdf6e36554687949d78ab556e79801d096
Tonalli <tonalli@xanum.uam.mx>
2020-01-17 11:31:26 -0600

    fetch-all

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -3,7 +3,20 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', async function (event) {
     const fullList = await charactersAPI.getFullList();
-    console.log(fullList);
+    // console.log(fullList);
+    let container = document.querySelector('.characters-container');
+    container.innerHTML = '';
+    fullList.forEach(character => {
+      container.innerHTML += `
+      <div class="character-info">
+        <div class="name">Id: ${character.id}</div>
+        <div class="name">Name: ${character.name}</div>
+        <div class="occupation">Occupation: ${character.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+        <div class="weapon">Weapon: ${character.weapon}</div>
+      </div>
+      `;
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', async function (event) {
