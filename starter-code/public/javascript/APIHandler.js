24440d0877e4483acc6cad3dfc6c3a6c79e1d270
unknown <tayssire@hotmail.com>
2020-02-05 22:05:55 +0100

    done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
+console.log("hey");
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api.get("/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister(theId) {
+    return this.api.get("/characters/" + theId);
   }
 
-  createOneRegister () {
-
+  createOneRegister(newChar) {
+    return this.api.post("/characters", newChar);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(TheId, updateChar) {
+    return this.api.patch("/characters/" + TheId, updateChar);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(theId) {
+    return this.api.delete("/characters/" + theId);
   }
 }
+
+export default APIHandler;
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,150 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+import APIHandler from "./APIHandler.js";
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+function cleanTheContainer() {
+  document.getElementById("characters-container-id").innerHTML = "";
+  
+}
+function createAnHtmlChar(newChar) {
+  const newCharacterHtml = `
+          <div class="character-info">
+          <div class="name">${newChar.id}</div>
+          <div class="name">${newChar.name}</div>
+          <div class="occupation">${newChar.occupation}</div>
+          <div class="cartoon">${newChar.cartoon}</div>
+          <div class="weapon">${newChar.weapon}</div>
+        </div>
+          `;
+  document.getElementById(
+    "characters-container-id"
+  ).innerHTML += newCharacterHtml;
+}
 
-  });
+function printanHtmlMessage(message) {
+  const newCharacterHtml = `
+        <div class="character-info">
+        <div class="name">${message}</div>
+      </div>
+        `;
+  document.getElementById(
+    "characters-container-id"
+  ).innerHTML += newCharacterHtml;
+}
+function changeButtonStyle(buttonId, state) {
+  if (state == "suceeded") {
+    document.getElementById(buttonId).style.backgroundColor = "green";
+  } else {
+    document.getElementById(buttonId).style.backgroundColor = "red";
+  }
+}
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  event.preventDefault();
 
-  });
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function(event) {
+      cleanTheContainer();
+      charactersAPI
+        .getFullList()
+        .then(dbRes => {
+          for (let i = 0; i < dbRes.data.length; i++) {
+            createAnHtmlChar(dbRes.data[i]);
+          }
+        })
+        .catch(dbErr => {
+          console.log(dbErr);
+        });
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      cleanTheContainer();
+      const theId = document.getElementById("theCharId").value;
+      charactersAPI
+        .getOneRegister(theId)
+        .then(dbRes => {
+          createAnHtmlChar(dbRes.data);
+        })
+        .catch(dbErr => {
+          console.log(dbErr);
+        });
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
+      cleanTheContainer();
+      const theId = document.getElementById("theCharIdDelete").value;
+      charactersAPI
+        .deleteOneRegister(theId)
+        .then(dbRes => {
+          printanHtmlMessage("Character has been successfully deleted");
+          changeButtonStyle("delete-one", "suceeded");
+        })
+        .catch(dbErr => {
+          printanHtmlMessage("Character not found");
+          changeButtonStyle("delete-one", "failed");
+          console.log(dbErr);
+        });
+    });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function(event) {
+      cleanTheContainer();
+      event.preventDefault();
+      var checkedValue = false;
+      if (document.getElementById("cartoon-update").checked)
+        checkedValue = true;
+      const updateChar = {
+        name: document.getElementById("name-update").value,
+        occupation: document.getElementById("occupation-update").value,
+        cartoon: checkedValue,
+        weapon: document.getElementById("weapon-update").value
+      };
+      const theId = document.getElementById("id-update").value;
+      charactersAPI
+        .updateOneRegister(theId, updateChar)
+        .then(dbRes => {
+          createAnHtmlChar(dbRes.data);
+          changeButtonStyle("update-data", "suceeded");
+        })
+        .catch(dbErr => {
+          printanHtmlMessage(dbErr);
+          changeButtonStyle("update-data", "failed");
 
-  });
+          console.log(dbErr);
+        });
+    });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function(event) {
+      event.preventDefault();
+      cleanTheContainer();
+      var checkedValue = false;
+      if (document.getElementById("cartoon").checked) checkedValue = true;
+      const newChar = {
+        name: document.getElementById("name").value,
+        occupation: document.getElementById("occupation").value,
+        cartoon: checkedValue,
+        weapon: document.getElementById("weapon").value
+      };
+      charactersAPI
+        .createOneRegister(newChar)
+        .then(dbRes => {
+          createAnHtmlChar(dbRes.data);
+          changeButtonStyle("send-data", "suceeded");
+        })
+        .catch(dbErr => {
+          printanHtmlMessage(dbErr);
+          changeButtonStyle("send-data", "failed");
 
-  });
+          console.log(dbErr);
+        });
+    });
 });
