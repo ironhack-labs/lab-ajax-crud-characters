2f496ac23232e1b616a872e584d6ffa8e78e0a8c
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-05 16:01:21 +0100

    Fetch all - Fetch one

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -2,24 +2,19 @@ import APIHandler from './APIHandler.js'
 const charactersAPI = new APIHandler('http://localhost:8000');
 const charactersContainer = document.body.querySelector(".characters-container");
 
-function generateCharacter(character) {
+function clearCharactersContainer() {
+  charactersContainer.innerHTML = '';
+}
 
+function generateCharacter(character) {
   const template = `
   <div class="character-info">
     <div class="id"><span>ID : </span>${character.id}</div>
     <div class="name"><span>Nom : </span>${character.name}</div>
     <div class="occupation"><span>Occupation : </span>${character.occupation}</div>
-    <div class="cartoon"><span>Cartoon : </span>${character.cartoon}</div>
+    <div class="cartoon"><span>Is a Cartoon ? : </span>${character.cartoon}</div>
     <div class="weapon"><span>Weapon : </span>${character.weapon}</div>
   </div>`;
-
-  // <div class="character-info">
-  //   <div class="name">Character Name</div>
-  //   <div class="occupation">Character Occupation</div>
-  //   <div class="cartoon">Is a Cartoon?</div>
-  //   <div class="weapon">Character Weapon</div>
-  // </div>
-
   charactersContainer.innerHTML += template;
 }
 
@@ -30,11 +25,15 @@ document.getElementById('fetch-all').addEventListener('click', function (event) 
         generateCharacter(character)
       })
     })
-
 });
 
 document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  const charId = parseInt(document.getElementById('fetch-one-input').value, 10);
+  charactersAPI.getOne(charId)
+    .then(response => {
+      clearCharactersContainer();
+      generateCharacter(response.data)
+    }).catch(err => console.log(err))
 });
 
 document.getElementById('delete-one').addEventListener('click', function (event) {
