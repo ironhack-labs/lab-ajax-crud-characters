94017065b2ef5dafe90223753589722bf00c1cb1
Alex Meyer <alexandre.meyer78@gmail.com>
2020-02-05 17:08:57 +0100

    Create and Edit a character

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,9 +1,40 @@
 import APIHandler from './APIHandler.js'
 const charactersAPI = new APIHandler('http://localhost:8000');
+
 const charactersContainer = document.body.querySelector(".characters-container");
-const btnFetchAll = document.getElementById('fetch-all');
-const btnFetchOne = document.getElementById('fetch-one');
+
+const btnFetchAll  = document.getElementById('fetch-all');
+const btnFetchOne  = document.getElementById('fetch-one');
 const btnDeleteOne = document.getElementById('delete-one');
+const btnCreateOne = document.getElementById('new-character-btn');
+const btnEditOne   = document.getElementById('edit-character-btn');
+const createForm   = document.getElementById('new-character-form');
+const editForm     = document.getElementById('edit-character-form');
+
+// ID GENERATION
+let generatedIds = null;
+function generateId() {
+  // First time
+  if ( generatedIds == null ) {
+
+    // Start with last id in db.json
+    return charactersAPI.getAll()
+            .then(response => {
+              let lastId = response.data.pop().id;
+              generatedIds = lastId + 1;
+              return generatedIds;
+            })
+  } else {
+    // Increment and return
+    return ++generatedIds;
+  }
+}
+
+function setButtonState(buttonElement, successful) {
+  buttonElement.classList.remove('success');
+  buttonElement.classList.remove('error');
+  buttonElement.classList.add(successful ? 'success' : 'error');
+}
 
 function clearCharactersContainer() {
   charactersContainer.innerHTML = '';
@@ -16,8 +47,8 @@ function generateCharacter(character) {
     <div class="id"><span>ID : </span>${character.id}</div>
     <div class="name"><span>Nom : </span>${character.name}</div>
     <div class="occupation"><span>Occupation : </span>${character.occupation}</div>
-    <div class="cartoon"><span>Is a Cartoon ? : </span>${character.cartoon}</div>
     <div class="weapon"><span>Weapon : </span>${character.weapon}</div>
+    <div class="cartoon"><span>Is a Cartoon ? </span>${character.cartoon}</div>
   </div>`;
 
   charactersContainer.innerHTML += template;
@@ -25,6 +56,8 @@ function generateCharacter(character) {
 }
 
 btnFetchAll.addEventListener('click', function (event) {
+
+  clearCharactersContainer();
 
   charactersAPI.getAll()
     .then(response => {
@@ -39,14 +72,15 @@ btnFetchOne.addEventListener('click', function (event) {
 
   const charId = parseInt(document.getElementById('fetch-one-input').value, 10);
 
+  clearCharactersContainer();
+
   charactersAPI.getOne(charId)
     .then(response => {
-      btnFetchOne.classList.remove('error');
-      clearCharactersContainer();
+      setButtonState(btnFetchOne, true);
       generateCharacter(response.data)
     }).catch(err => {
       console.log(err);
-      btnFetchOne.classList.add('error');
+      setButtonState(btnFetchOne, false);
     })
 
 });
@@ -56,17 +90,58 @@ btnDeleteOne.addEventListener('click', function (event) {
   charactersAPI.deleteOne(charId)
     .then(response => {
       console.log("Successfuly deleted char id : ", charId);
-      btnDeleteOne.classList.remove('error');
+      setButtonState(btnDeleteOne, true);
     }).catch(err => {
       console.log(err);
-      btnDeleteOne.classList.add('error');
+      setButtonState(btnDeleteOne, false);
     })
 });
 
-document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+createForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
+  const formData = new FormData(createForm);
+  const newId    = await generateId();
+
+  const newChar = {
+    id         : newId,
+    name       : formData.get('name'),
+    occupation : formData.get('occupation'),
+    weapon     : formData.get('weapon'),
+    cartoon    : !!formData.get('cartoon') ? true : false
+  }
+
+  console.log("New character : ", newChar)
+
+  charactersAPI.createOne(newChar)
+    .then(response => {
+      console.log("Successfuly created character");
+      setButtonState(btnCreateOne, true);
+    }).catch(err => {
+      console.log(err);
+      setButtonState(btnCreateOne, false);
+    })
 
 });
 
-document.getElementById('new-character-form').addEventListener('submit', function (event) {
+editForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const formData = new FormData(editForm);
+  const charId   = formData.get('charId');
+
+  const updateValues = {
+    name       : formData.get('name'),
+    occupation : formData.get('occupation'),
+    weapon     : formData.get('weapon'),
+    cartoon    : !!formData.get('cartoon') ? true : false
+  }
+
+  charactersAPI.updateOne(charId, updateValues)
+    .then(response => {
+      console.log("Successfuly updated character");
+      setButtonState(btnEditOne, true);
+    }).catch(err => {
+      console.log(err);
+      setButtonState(btnEditOne, false);
+    })
 
 });
