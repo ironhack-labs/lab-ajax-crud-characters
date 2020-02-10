9e6a1ddfc09194e4e678a4d6b53fe866713d2609
Nora Nicklis <nora.nicklis@gmail.com>
2020-02-10 08:35:59 +0100

    done, missing frontend for edit and add

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,63 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    const url = this.BASE_URL;
+    return axios
+      .get(`${url}/characters`)
+      .then(response => {
+        // console.log(response.data);
+        let result = response.data;
+        return result;
+      })
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios
+      .get(`http://localhost:8000/characters/${id}`)
+      .then(response => {
+        return response.data;
+      })
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister(name, occupation, weapon, cartoon) {
+    axios
+      .post("http://localhost:8000/characters", {
+        name: name,
+        occupation: occupation,
+        weapon: weapon,
+        cartoon: cartoon
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, name, occupation, weapon, cartoon) {
+    axios
+      .patch(`http://localhost:8000/characters/${id}`, {
+        name: name,
+        occupation: occupation,
+        weapon: weapon,
+        cartoon: cartoon
+      })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios
+      .delete(`http://localhost:8000/characters/${id}`)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => console.log(err));
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,64 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function(event) {
+      // console.log(charactersAPI.getFullList());
+      charactersAPI
+        .getFullList()
+        .then(response => {
+          document.querySelector(".characters-container").innerHTML = "";
+          response.forEach(element => {
+            let div = document.createElement("div");
+            div.innerHTML = `<p>Name: ${element.name}</p>
+            <p>Occupation: ${element.occupation}</p>
+            <p>Weapon: ${element.weapon}</p>
+            <p>Cartoon: ${element.cartoon}</p>`;
+            div.classList.add("character-info");
+            document.querySelector(".characters-container").appendChild(div);
+          });
+        })
+        .catch(err => console.log(err));
 
-  });
+      // IF I HAVE TIME YOUTUBE ASYNC AWAIT
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function(event) {
+      charactersAPI
+        .getOneRegister(document.getElementById("character-id").value)
+        .then(response => {
+          document.querySelector(".characters-container").innerHTML = "";
+          let div = document.createElement("div");
+          div.innerHTML = `<p>Name: ${response.name}</p>
+            <p>Occupation: ${response.occupation}</p>
+            <p>Weapon: ${response.weapon}</p>
+            <p>Cartoon: ${response.cartoon}</p>`;
+          div.classList.add("character-info");
+          document.querySelector(".characters-container").appendChild(div);
+        })
+        .catch(err => console.log(err));
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function(event) {});
+  charactersAPI
+    .deleteOneRegister(document.getElementById("character-delete-id").value)
+    .then(response => {
+      let p = document.createElement("p");
+      p.innerText = "The character was deleted";
+      document.querySelector(".operations").appendChild(p);
+    })
+    .catch(err => console.log(err));
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function(event) {});
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function(event) {});
 });
