29dd0aa52f2a33b09abff581356aa7455c4c63ac
America Zurita <americazurita@gmail.com>
2020-02-10 00:01:43 -0600

    2nd iteration

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -3,23 +3,30 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  //Get all the characters info from http://localhost:8000/characters
+  async getFullList () {
+    const characters = await axios.get(`${this.BASE_URL}/characters`)
+    return characters.data
 
   }
-
-  getOneRegister () {
+  //Get a single character info from http://localhost:8000/characters/:id
+  async getOneRegister () {
+    const character = await axios.get(`${this.BASE_URL}/characters/${id}`)
+      return character.data
+  }
+//Create a single character posting the data to http://localhost:8000/characters
+  async createOneRegister () {
+    const createCharacter = await axios.post(`${this.BASE_URL}/characters`, character)
+      return createCharacter
+  }
+  //Delete a single character through his id in http://localhost:8000/characters/:id
+  async updateOneRegister () {
+    const editCharacter = await axios.put(`${this.BASE_URL}/characters/${id}`, char)
+    return editCharacter
 
   }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+//Edit a single character through his id in http://localhost:8000/characters/:id
+  async deleteOneRegister () {
+    await axios.delete(`${this.BASE_URL}/characters/${id}`)    
   }
 }
