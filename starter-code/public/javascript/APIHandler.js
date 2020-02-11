158c421a24ea156650abf1dc29149b16bef13418
Paul Carillion <pcarillion@yahoo.fr>
2020-02-11 23:52:09 +0100

    it's not working but I 'll finish later on

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,31 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl
+    })
   }
+
 
   getFullList () {
-
+    return this.api.get("/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return console.log(this.api.get(`/characters/${id}`));
   }
 
   createOneRegister () {
-
+    return this.api.post("/characters");
   }
 
   updateOneRegister () {
-
+    return this.api.patch("/characters/:id")
   }
 
   deleteOneRegister () {
-
+    return this.api.delete("/characters/:id")
   }
 }
+
+export default APIHandler;
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,13 +1,48 @@
+import APIHandler from "./APIHandler.js"
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+
 window.addEventListener('load', () => {
+
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+    document.querySelector(".characters-container").innerHTML = "";
+    charactersAPI  
+      .getFullList()
+      .then(res => {
+        res.data.forEach(data => {
+            console.log("hey")
+            document.querySelector(".characters-container").innerHTML += 
+            `<div class='character-info'>
+                <div class='name'>name: ${data.name}</div>
+                <div class='occupation'>occupation: ${data.occupation}</div>
+                <div class='cartoon'>Is it a cartoon? ${data.cartoon}</div>
+                <div class='weapon'>Character Weapon: ${data.weapon}</div>
+            </div>`
+        });
+      })
+      .catch(err => {console.log(err)})
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    document.querySelector(".characters-container").innerHTML = "";
+    charactersAPI  
+      .getOneRegister(document.querySelector("#fetch-one-input").value)
+      .then(res =>  {
+            // console.log(getOneRegister(document.querySelector("#fetch-one-input").value))
+            document.querySelector(".characters-container").innerHTML += 
+            `<div class='character-info'>
+                <div class='id'>name: ${id.id}</div>
+                <div class='name'>name: ${id.name}</div>
+                <div class='occupation'>occupation: ${id.occupation}</div>
+                <div class='cartoon'>Is it a cartoon? ${id.cartoon}</div>
+                <div class='weapon'>Character Weapon: ${id.weapon}</div>
+            </div>`
+      })
+      .catch(err => {console.log(err)})
   });
+  
+
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
