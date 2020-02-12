a0eb4614db3a0d24532452b9d08754eaf5155d1d
Luz Divina Delgado Castañeda <luzdivdelg@gmail.com>
2020-02-12 23:05:39 +0100

    hecho partes

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,51 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    console.log("Listado de personajes")
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(data => {
+        return data.data
+      })
+  }
+
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(data => {
+        return data.data
+      })
+  }
+
+  createOneRegister(info) {
+    return axios.post(`${this.BASE_URL}/characters`, info)
+      .then(data => {
+        return data.data
+      })
+      .catch(err=>{
+        return "no se ahora mismo como devolver si hubo un error como pide el ejercicio"
+      })
+  }
+
+  updateOneRegister(id, info) {
+    return axios.put(`${this.BASE_URL}/characters/${id}`, info)
+      .then(data => {
+        return data.data
+      })
+      .catch(err=>{
+        return "Character not found"
+      })
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+return axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(data => {
+      return "Character has been successfully deleted"
+    })
+    .catch(err=>{
+      return "Character not found"
+    })
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,38 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    event.preventDefault()
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    event.preventDefault()
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const characterInfo = document.querySelectorAll('#new-character-form input')
+    const info = {
+      name: characterInfo[0].value,
+      occupation: characterInfo[1].value,
+      weapon: characterInfo[2].value
+    }
+    const api = new APIHandler('https://minions-api.herokuapp.com');
+    api.createOneRegister(info)
+      .then(data => {
+        console.log(data)
+      })
 
   });
 });
