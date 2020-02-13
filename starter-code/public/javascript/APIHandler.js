94ebb85841779bc3d736d06373e61a6f7814b76e
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-02-13 09:20:23 +0100

    hecho

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,96 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApp = axios.create({ baseURL: this.BASE_URL })
   }
 
-  getFullList () {
+  getFullList() {
+    this.axiosApp.get(`/`)
+      .then(allCharacters => {
+
+        allCharacters.data.forEach(char => {
+
+          const minion = `<li>Nombre: ${char.name} || Trabajo: ${char.occupation} || id: ${char.id}</li>`
+
+          document.querySelector("#complete-list").innerHTML += minion
+
+        })
+
+        console.log(allCharacters.data)
+      })
+  }
+
+  getOneRegister() {
+
+    const minionId = document.getElementById("search-id").value
+
+    this.axiosApp.get(`/${minionId}`)
+
+      .then(theMinion => {
+        document.querySelector(".name").innerText = `Character Name ${theMinion.data.name}`
+        document.querySelector(".occupation").innerText = `Character occupation ${theMinion.data.occupation}`
+        document.querySelector(".cartoon").innerText = `Character is cartoon? ${theMinion.data.cartoon}`
+        document.querySelector(".weapon").innerText = `character weapon is ${theMinion.data.weapon}`
+      })
 
   }
 
-  getOneRegister () {
+  createOneRegister() {
+
+    const minionInfo = document.getElementById("new-character-form").querySelectorAll("input")
+
+    const newMinion = {
+
+      name: minionInfo[0].value,
+      occupation: minionInfo[1].value,
+      weapon: minionInfo[2].value,
+      cartoon: minionInfo[3].checked,
+    }
+
+
+    this.axiosApp.post("/", newMinion)
+      .then(created => console.log(created))
+
 
   }
 
-  createOneRegister () {
+  updateOneRegister() {
 
+    const minionInfo = document.getElementById("edit-character-form").querySelectorAll("input")
+
+    const newMinion = {
+
+      id: minionInfo[0].value,
+      name: minionInfo[1].value,
+      occupation: minionInfo[2].value,
+      weapon: minionInfo[3].value,
+      cartoon: minionInfo[4].checked,
+    }
+
+    this.axiosApp.put(`/${newMinion.id}`, newMinion)
+      .then(created => console.log(created))
+      .catch(err => next(err)
+
+
+
+  
   }
 
-  updateOneRegister () {
+  deleteOneRegister() {
 
-  }
+    const minionId = document.getElementById("delete-id").value
 
-  deleteOneRegister () {
+    console.log(minionId)
+
+    this.axiosApp.delete(`/${minionId}`)
+      .then(erased => {
+        document.getElementById("delete-id").value = `${erased.data.id} ha sido borrado`
+      })
+      .cacth(err => {
+
+        document.getElementById("delete-id").value = `ha habido un problema borrando al minion ${erased.data.id}`
+      })
 
   }
 }
+
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,42 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const baseUrlMinion = "https://minions-api.herokuapp.com/characters"
+
+const charactersAPI = new APIHandler(baseUrlMinion);
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+    charactersAPI.getFullList()
+
+
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    charactersAPI.getOneRegister()
+
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    charactersAPI.deleteOneRegister()
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
+    event.preventDefault()
+
+    charactersAPI.updateOneRegister()
+
+
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+    event.preventDefault()
+
+    charactersAPI.createOneRegister()
+
 
   });
 });
