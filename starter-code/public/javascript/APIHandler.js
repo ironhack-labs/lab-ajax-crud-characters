dbfcc8c503ea4f6a0b17c058a87d10658a5a44e3
Carlos González <cargonamt@gmail.com>
2020-02-13 09:33:58 +0100

    not done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,42 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = axios.create({
+      baseURL: "https://minions-api.herokuapp.com"
+    });
   }
 
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
+  getFullList() {
+    return this.BASE_URL.get('https://minions-api.herokuapp.com/characters')
+      .then(allCharacters => allCharacters.data)
+      .catch(error => console.log('Oh No! Error is: ', error))
 
   }
 
-  updateOneRegister () {
+  getOneRegister() {
+    // return this.BASE_URL.get('https://minions-api.herokuapp.com/characters/:id')
+    //   .then(OneCharacter => OneCharacter.data)
+    //   .catch(error => console.log('Oh No! Error is: ', error))
+    let id = document.querySelector('character-id').value
 
+    this.axiosApp.get(`characters/${id}`)
+      .then(response => {
+        document.querySelector('name').value = response.data.name
+        document.querySelector('ocupation').value = response.data.occupation
+        document.querySelector('weapon').value = response.data.weapon
+        document.querySelector('cartoon').checked = response.data.cartoon
+      })
+  }
+  createOneRegister() {
+    this.BASE_URL.post('https://minions-api.herokuapp.com/characters')
   }
 
-  deleteOneRegister () {
+  updateOneRegister() {
+    this.BASE_URL.put('https://minions-api.herokuapp.com/characters/:id')
+  }
 
+  deleteOneRegister() {
+    this.BASE_URL.delete('https://minions-api.herokuapp.com/characters/:id')
+    this.app.delete(`characters/${id}`)
+      .catch(err => console.log("Ha habido un error: ", err))
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,42 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
-
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com');
+const inputs = document.querySelectorAll('input')
+const name = document.getElementsByClassName("name")[0]
+const occupation = document.getElementsByClassName("occupation")[0]
+const cartoon = document.getElementsByClassName("cartoon")[0]
+const weapon = document.getElementsByClassName("weapon")[0]
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault()
+    charactersAPI.getFullList()
+      .then(allCharacters => {
+        allCharacters.forEach(element => {
+          list.innerHTML += `<li>${element.name}</li>`
+        });
+      })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    event.preventDefault()
+    charactersAPI.getOneRegister()
+    reset()
 
-  });
+  })
+  // //.then(x => resetInputs())
+  // .catch(err => console.log("ha habido un error", err))
+});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+document.getElementById('delete-one').addEventListener('click', function (event) {
+  event.preventDefault()
+  charactersAPI.deleteOneRegister()
+    .then(OneCharacters => {
+      list2.innerHTML += `<li>${console.log(OneCharacters)}</li>`
+    })
+});
 
-  });
+document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+});
 
-  });
+document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
 });
