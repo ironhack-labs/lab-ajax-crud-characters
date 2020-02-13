3968a9237b86474aa9fd0acd0509caa5b60eb96c
de_Mora <miguel.uni.demora@gmail.com>
2020-02-13 09:37:41 +0100

    donete

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,75 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApp = axios.create({ baseURL: this.BASE_URL})
+    
   }
 
   getFullList () {
+    this.axiosApp.get("/characters")
+      .then(allMinions => {
+        console.log(allMinions)
+        allMinions.data.forEach(element => {
+        document.querySelector('.characters-container').innerHTML += 
 
+        `<div class="character-info">
+        <div class="name">Character Name : ${element.name}</div>
+        <div class="occupation">Character Occupation : ${element.occupation} </div>
+        <div class="cartoon">Is a Cartoon? : ${element.cartoon}</div>
+        <div class="weapon">Character Weapon : ${element.weapon}</div>
+      </div>`
+      
+      })})
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
+    this.axiosApp.get(`/characters/${id}`)
+      .then(theMinion => document.querySelector('.characters-container').innerHTML =
 
-  }
+      `<div class="character-info">
+        <div class="name">Character Name : ${theMinion.data.name}</div>
+        <div class="occupation">Character Occupation : ${theMinion.data.occupation} </div>
+        <div class="cartoon">Is a Cartoon? : ${theMinion.data.cartoon}</div>
+        <div class="weapon">Character Weapon : ${theMinion.data.weapon}</div>
+      </div>`
+    )}
 
   createOneRegister () {
 
+    const newCharacter = {
+      name: document.getElementById("addName").value,
+      occupation: document.getElementById("addOccupation").value,
+      cartoon: document.getElementById("addCartoon").checked,
+      weapon: document.getElementById("addWeapon").value
+    }
+    console.log(newCharacter)
+
+    this.axiosApp.post(`/characters`,newCharacter)
+    .then((response)=> console.log(response))
+    .then(console.log(newCharacter))
+    .then(document.getElementById("new-character-form").reset())
   }
 
   updateOneRegister () {
 
+    const newCharacter = {
+      id: document.getElementById("editID").value,
+      name: document.getElementById("editName").value,
+      occupation: document.getElementById("editOccupation").value,
+      cartoon: document.getElementById("editCartoon").checked,
+      weapon: document.getElementById("editWeapon").value
+    }
+
+    this.axiosApp.put(`characters/${newCharacter.id}`, newCharacter)
+    .then(response => console.log(response))
+    .then(document.getElementById("new-character-form").reset())
+    .catch(err => console.log(err))
+    
   }
 
-  deleteOneRegister () {
+  deleteOneRegister (id) {
+    this.axiosApp.delete(`/characters/${id}`)
+    .then(a => console.log(a))
 
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,28 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', (e) =>{
+    charactersAPI.getFullList()
 
-  });
+  })
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const charID = document.querySelector('#character-id').value
+    charactersAPI.getOneRegister(charID)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const charID = document.querySelector('#character-id-delete').value
+    charactersAPI.deleteOneRegister(charID)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    charactersAPI.updateOneRegister()
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    charactersAPI.createOneRegister()
   });
 });
