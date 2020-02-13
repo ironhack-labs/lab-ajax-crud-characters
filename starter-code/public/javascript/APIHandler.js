950e8295f5bff829634af5147a3b8988fa0fe2a6
Gonzalo lledo <gonzalo_lledo@outlook.com>
2020-02-13 09:03:09 +0100

    Pete

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,10 +1,15 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApp = axios.create({ baseUrl: this.BASE_URL})
   }
 
   getFullList () {
-
+    this.axiosApp.get("https://minions-api.herokuapp.com/characters")
+    .then(allMinions => {
+      
+      
+    })
   }
 
   getOneRegister () {
