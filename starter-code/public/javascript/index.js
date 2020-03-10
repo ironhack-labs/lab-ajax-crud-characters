8509ed4d7fec6f603aa77dfb7fcc9eeb2ef012f3
Michael Mueller <michael@miniguide.co>
2020-03-10 23:58:43 +0100

    add fetch-all and fetch-one

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,32 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', function(event) {
+    charactersAPI
+      .getFullList()
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log('Error fetching all', error);
+      });
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', function(event) {
+    const id = document.getElementsByName("character-id")[0].value;
+    charactersAPI
+      .getOneRegister(id)
+      .then(res => {
+        console.log(res.data);
+      })
+      .catch(error => {
+        console.log('Error fetching one character', error);
+      });
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', function(event) {});
 
-  });
+  document.getElementById('edit-character-form').addEventListener('submit', function(event) {});
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document.getElementById('new-character-form').addEventListener('submit', function(event) {});
 });
