3198b080698a886131e1eac6a2327e9814d7ca36
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-04-02 19:11:27 -0300

    Done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,54 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor( baseUrl ) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios
+      .get( this.BASE_URL + `/characters` )
+      .then( charactersFromAPI => charactersFromAPI.data )
+      .catch( error => console.log( error ) )
   }
 
-  getOneRegister () {
-
+  getOneRegister( id ) {
+    return axios
+      .get( this.BASE_URL + `/characters/${id}` )
+      .then( characterFromAPI => characterFromAPI.data )
+      .catch( error => console.log( error ) )
   }
 
-  createOneRegister () {
+  createOneRegister( name, occupation, weapon, cartoon ) {
+    const newCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    }
 
+    return axios
+      .post( this.BASE_URL + `/characters`, newCharacter )
+      .then( characterFromAPI => characterFromAPI.data )
+      .catch( error => console.log( error ) )
   }
 
-  updateOneRegister () {
+  updateOneRegister( id, name, occupation, weapon, cartoon ) {
+    const updateCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    }
 
+    return axios
+      .put( this.BASE_URL + `/characters/${id}`, updateCharacter )
+      .then( characterFromAPI => characterFromAPI.data )
+      .catch( error => console.log( error ) )
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios
+    .delete( this.BASE_URL + `/characters/${id}`)
+    .then( charactersFromAPI => charactersFromAPI.data )
+    .catch( error => console.log( error ) )
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,130 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler( 'http://localhost:8000' );
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener( 'load', () => {
+  const charactersContainer = document.querySelector( '.characters-container' )
+  const clear = () => charactersContainer.innerHTML = ''
+  
 
-  });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  let eventChecker = false
+  const update = () => {
+    charactersAPI.getFullList()
+      .then( characters => {
+        characters.forEach( character => {
+          const {
+            id,
+            name,
+            occupation,
+            weapon,
+            cartoon
+          } = character
+          renderComponent( id, name, occupation, weapon, cartoon )
+        } )
+      } )
+      .catch( error => {
+        charactersContainer.innerHTML = '<h1>No characters to display</h1>'
+        console.log( error )
+      } )
+  }
 
-  });
+  const createComponent = ( id, name, occupation, cartoon, weapon ) => (
+    `<div class="id">id: <span>${id}</span></div>
+    <div class="name">Name: <span>${name}</span></div>
+    <div class="occupation">Occupation: <span>${occupation}</span></div>
+    <div class="cartoon">Cartoon: <span>${cartoon}</span></div>
+    <div class="weapon">Weapon: <span>${weapon}</span></div>`
+  )
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  const renderComponent = ( id, name, occupation, weapon, cartoon ) => {
+    const div = document.createElement( 'div' )
+    div.classList.add( 'character-info' )
+    div.innerHTML = createComponent( id, name, occupation, weapon, cartoon )
+    charactersContainer.appendChild( div )
+  }
 
-  });
+  document.getElementById( 'fetch-all' ).addEventListener( 'click', function ( event ) {
+    clear()
+    eventChecker = true
+    update()
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  } );
 
-  });
+  document.getElementById( 'fetch-one' ).addEventListener( 'click', function ( event ) {
+    clear()
+    
+    const inputSearchId = document.getElementById( 'character-id' )
+    charactersAPI.getOneRegister( inputSearchId.value )
+      .then( character => {
+        const {
+          id,
+          name,
+          occupation,
+          weapon,
+          cartoon
+        } = character
+        renderComponent( id, name, occupation, weapon, cartoon )
+      } )
+      .catch( error => {
+        charactersContainer.innerHTML = '<h1>No characters to display</h1>'
+        console.log( error )
+      } )
+  } );
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
-  });
-});
+  document.getElementById( 'delete-one' ).addEventListener( 'click', function ( event ) {
+    clear()
+    const inputDeleteId = document.getElementById( 'character-id-delete' )
+    if ( eventChecker )
+      charactersAPI.deleteOneRegister( inputDeleteId.value )
+    charactersContainer.innerHTML = '<h1>Character deleted</h1>'
+
+  } );
+
+  document.getElementById( 'edit-character-form' ).addEventListener( 'submit', function ( event ) {
+
+    const editId = document.querySelector( '#edit-id' ).value
+    const editName = document.querySelector( '#edit-name' ).value
+    const editOccupation = document.querySelector( '#edit-occupation' ).value
+    const editWeapon = document.querySelector( '#edit-weapon' ).value
+    const editCartoon = document.querySelector( '#edit-cartoon' ).value
+
+    charactersAPI.updateOneRegister( editId, editName, editOccupation, editWeapon, editCartoon )
+      .then( character => {
+        const {
+          id,
+          name,
+          occupation,
+          weapon,
+          cartoon
+        } = character
+        renderComponent( id, name, occupation, weapon, cartoon )
+        charactersContainer.innerHTML = '<h1>Character edited</h1>'
+      } )
+      .catch( () => charactersContainer.innerHTML = '<h1>Character not created</h1>' )
+  } );
+} );
+
+document.getElementById( 'new-character-form' ).addEventListener( 'submit', function ( event ) {
+  clear()
+  const newName = document.querySelector( '#new-name' ).value
+  const newOccupation = document.querySelector( '#new-occupation' ).value
+  const newWeapon = document.querySelector( '#new-weapon' ).value
+  const newCartoon = document.querySelector( '#new-cartoon' ).value
+
+  if ( ( !newName || !newOccupation || !newWeapon || !newCartoon ) ) return
+
+  charactersAPI
+    .createOneRegister( newName, newOccupation, newWeapon, newCartoon )
+    .then( character => {
+      const {
+        id,
+        name,
+        occupation,
+        weapon,
+        cartoon
+      } = character
+      renderComponent( id, name, occupation, weapon, cartoon )
+      charactersContainer.innerHTML = '<h1>Character created</h1>'
+    } )
+    .catch( () => charactersContainer.innerHTML = '<h1>Character not created</h1>' )
+} );
