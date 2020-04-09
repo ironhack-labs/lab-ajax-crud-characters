1afea8277ccc265d58a666a220ed4b48c3a0dde8
Andre <andre.talpo@gmail.com>
2020-04-08 21:06:31 -0300

    Funcionalidades prontas

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,47 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList() {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters`);
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  getOneRegister () {
-
+  async getOneRegister(id) {
+    try {
+      const response = await axios.get(`${this.BASE_URL}/characters/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  createOneRegister () {
-
+  async createOneRegister(body) {
+    try {
+      await axios.post(`${this.BASE_URL}/characters`, body);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister(id, body) {
+    try {
+      await axios.put(`${this.BASE_URL}/characters/${id}`, body);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    try {
+      await axios.delete(`${this.BASE_URL}/characters/${id}`);
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,103 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    try {
+      cleanCharacterInfo();
 
+      const characters = await charactersAPI.getFullList();
+      characters.forEach(element => {
+        addCharacterInfo(element);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    try {
+      const id = document.querySelector('#input-id-search').value;
+      const character = await charactersAPI.getOneRegister(id);
+      cleanCharacterInfo();
+      addCharacterInfo(character);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    try {
+      const id = document.querySelector('#input-id-delete').value;
+      await charactersAPI.deleteOneRegister(id);
+      colorElement('green', event.target);
+    } catch (error) {
+      colorElement('red', event.target);
+      console.log(error);
+    }
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    try {
+      event.preventDefault();
 
+      const formElements = event.currentTarget.elements;
+      const bodyRequest = {};
+
+      for (let i = 0; i < formElements.length; i++) {
+        const { name, value } = formElements[i];
+        if (name) bodyRequest[name] = value;
+        if (name === 'cartoon') bodyRequest[name] = formElements[i].checked;
+      }
+
+      await charactersAPI.updateOneRegister(bodyRequest.id, bodyRequest);
+
+      colorElement('green', document.querySelector('#edit-data'));
+    } catch (error) {
+      colorElement('red', document.querySelector('#edit-data'));
+      console.log(error);
+    }
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    try {
+      event.preventDefault();
 
+      const formElements = event.currentTarget.elements;
+      const bodyRequest = {};
+
+      for (let i = 0; i < formElements.length; i++) {
+        const { name, value } = formElements[i];
+        if (name) bodyRequest[name] = value;
+        if (name === 'cartoon') bodyRequest[name] = formElements[i].checked;
+      }
+
+      await charactersAPI.createOneRegister(bodyRequest);
+
+      colorElement('green', document.querySelector('#send-data'));
+    } catch (error) {
+      colorElement('red', document.querySelector('#send-data'));
+      console.log(error);
+    }
   });
 });
+
+function cleanCharacterInfo() {
+  document.querySelector('.characters-container').innerHTML = "";
+}
+
+function addCharacterInfo(element) {
+  const div = document.createElement("div");
+  div.classList.add('character-info');
+  div.innerHTML = `
+          <div class="id">id: <span>${element.id}</span></div>
+          <div class="name">Name: <span>${element.name}</span></div>
+          <div class="occupation">Occupation: <span>${element.occupation}</span></div>
+          <div class="cartoon">Is a Cartoon?: <span>${element.cartoon}</span></div>
+          <div class="weapon">Weapon: <span>${element.weapon}</span></div>`;
+
+  document.querySelector('.characters-container').appendChild(div);
+}
+
+function colorElement(color, element) {
+  element.style.backgroundColor = color;
+}
