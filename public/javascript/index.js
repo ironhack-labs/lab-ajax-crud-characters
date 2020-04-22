2cd9c7e996e41eebe86df6e836120da9a5c9eebe
TheShua <g.deglisezinski@gmail.com>
2020-04-22 20:37:14 +0200

    Fix to add button colorization

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,9 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 const container = document.querySelector('.characters-container');
 
 window.addEventListener('load', () => {
+	/**
+	 * FETCH ALL CHARACTERS LISTENER
+	 */
 	document.getElementById('fetch-all').addEventListener('click', function (event) {
 		// Request on the API to get all the characters in the database
 		charactersAPI
@@ -19,6 +22,9 @@ window.addEventListener('load', () => {
 			.catch((e) => console.log(e));
 	});
 
+	/**
+	 * FETCH ONE CHARACTER LISTENER
+	 */
 	document.getElementById('fetch-one').addEventListener('click', function (event) {
 		const id = getId(event.target);
 		// Request on the API to get only the specified character
@@ -32,13 +38,21 @@ window.addEventListener('load', () => {
 				character.cartoon = textIsCartoon(character.cartoon);
 				// Add the character bloc in the container
 				container.innerHTML += createBlock(character);
+				event.target.classList.remove('oops-i-did-it-again');
+				event.target.classList.add('all-green');
 			})
-			.catch((e) => console.log(e));
+			.catch((e) => {
+				console.log(e);
+				event.target.classList.add('oops-i-did-it-again');
+			});
 
 		// Clean the input
 		resetInput(getInput(event.target));
 	});
 
+	/**
+	 * DELETE ONE CHARACTER LISTENER
+	 */
 	document.getElementById('delete-one').addEventListener('click', function (event) {
 		// Request on the API to delete the specified character
 		charactersAPI
@@ -46,13 +60,21 @@ window.addEventListener('load', () => {
 			.then((APIresult) => {
 				// Well, hum... worcestershire sauce ?
 				// HAN ! Just googled it ! First try !
+				event.target.classList.remove('oops-i-did-it-again');
+				event.target.classList.add('all-green');
 			})
-			.catch((e) => console.log(e));
+			.catch((e) => {
+				console.log(e);
+				event.target.classList.add('oops-i-did-it-again');
+			});
 
 		// Clean the input
 		resetInput(getInput(event.target));
 	});
 
+	/**
+	 * EDIT CHARACTER LISTENER
+	 */
 	document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 		// Don't submit the form please master DOM
 		event.preventDefault();
@@ -86,13 +108,21 @@ window.addEventListener('load', () => {
 					character.querySelector('.cartoon span').textContent = textIsCartoon(upgradedCharacter.cartoon);
 					character.querySelector('.weapon span').textContent = upgradedCharacter.weapon;
 				}
+				event.target.querySelector('button').classList.remove('oops-i-did-it-again');
+				event.target.querySelector('button').classList.add('all-green');
 			})
-			.catch((e) => console.log(e));
+			.catch((e) => {
+				console.log(e);
+				event.target.querySelector('button').classList.add('oops-i-did-it-again');
+			});
 
 		// Clean the input
 		resetInput(form, true);
 	});
 
+	/**
+	 * CREATE CHARACTER LISTENER
+	 */
 	document.getElementById('new-character-form').addEventListener('submit', function (event) {
 		// DOMinic Monaghan, please ! I'm your Billy Boyd ! Don't do that ! Don't submit...
 		event.preventDefault();
@@ -119,8 +149,13 @@ window.addEventListener('load', () => {
 					newCharacter.cartoon = textIsCartoon(newCharacter.cartoon);
 					container.innerHTML += createBlock(newCharacter);
 				}
+				event.target.querySelector('button').remove('oops-i-did-it-again');
+				event.target.querySelector('button').classList.add('all-green');
 			})
-			.catch((e) => console.log(e));
+			.catch((e) => {
+				console.log(e);
+				event.target.querySelector('button').classList.add('oops-i-did-it-again');
+			});
 
 		// Clean the input
 		resetInput(form, true);
