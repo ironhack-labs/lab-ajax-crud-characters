1fa2e8ab9ffd9dc2a1f0408ee147f3fa0dc55810
Ruben Narvaez <rubnarvaez@gmail.com>
2020-04-22 20:42:49 +0200

    SemiDone

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
+const axiosApp = axios.create({ baseUrl: 'https://minions-api.herokuapp.com' })
+
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
+    return axios.get(this.BASE_URL + '/characters')
+      // .then(response => console.log(response))
+    
+  }
+
+  getOneRegister (id) {
+    return axios.get(this.BASE_URL + `/characters/${id}`)
+      // .then(response => console.log(response))
+  }
+
+  createOneRegister (character) {
+    return axios.post(this.BASE_URL + `/characters/`, character)
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id, character) {
+    return axios.put(this.BASE_URL + `/characters/${id}`, character)
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return axios.delete(this.BASE_URL + `/characters/${id}`)
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,92 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+ const charactersAPI = new APIHandler('https://minions-api.herokuapp.com');
+
+let charactersDiv = document.querySelector('.characters-container')
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+      .then(res => {
+        let characterArr = res.data.slice(20, 40)
+        charactersDiv.innerHTML = ' '
 
+
+        characterArr.forEach(elm => {
+
+          const divCharInfo = document.createElement('div')
+          const name = document.createElement('div')
+          const occupation = document.createElement('div')
+          const cartoon = document.createElement('div')
+          const weapon = document.createElement('div')
+
+          divCharInfo.setAttribute('class', 'character-info')
+          name.setAttribute('class', 'name')
+          occupation.setAttribute('class', 'occupation')
+          cartoon.setAttribute('class', 'cartoon')
+          weapon.setAttribute('class', 'weapon')
+
+          name.innerHTML = `name: ${elm.name}`
+          occupation.innerHTML = `occupation: ${elm.occupation}`
+          cartoon.innerHTML = `cartoon: ${elm.cartoon}`
+          weapon.innerHTML = `weapon: ${elm.weapon}`
+          divCharInfo.appendChild(name)
+          divCharInfo.appendChild(occupation)
+          divCharInfo.appendChild(cartoon)
+          divCharInfo.appendChild(weapon)
+
+          charactersDiv.appendChild(divCharInfo)
+        });
+        console.log(characterArr)
+      })
+    
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    event.preventDefault()
+    const id = document.getElementById('search-one').value
+    charactersAPI.getOneRegister(id)
+      .then(res => {
+        let characterArr = res.data
+        charactersDiv.innerHTML = ' '
+
+          const divCharInfo = document.createElement('div')
+          const name = document.createElement('div')
+          const occupation = document.createElement('div')
+          const cartoon = document.createElement('div')
+          const weapon = document.createElement('div')
+
+          divCharInfo.setAttribute('class', 'character-info')
+          name.setAttribute('class', 'name')
+          occupation.setAttribute('class', 'occupation')
+          cartoon.setAttribute('class', 'cartoon')
+          weapon.setAttribute('class', 'weapon')
+
+          name.innerHTML = `name: ${characterArr.name}`
+          occupation.innerHTML = `occupation: ${characterArr.occupation}`
+          cartoon.innerHTML = `cartoon: ${characterArr.cartoon}`
+          weapon.innerHTML = `weapon: ${characterArr.weapon}`
+         
+          divCharInfo.appendChild(name)
+          divCharInfo.appendChild(occupation)
+          divCharInfo.appendChild(cartoon)
+          divCharInfo.appendChild(weapon)
+
+          charactersDiv.appendChild(divCharInfo)
+       
+       
+      })
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister('character-id-delete')
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.updateOneRegister(id, character)
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    charactersAPI.createOneRegister(character)
 
   });
 });
