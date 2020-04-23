9d62580f87da6be25efc53fb711a858ea18a5bf1
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-04-23 00:02:18 -0500

    MEX WEB-DEV Emma Solano y Eduardo Z.

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,30 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  async getFullList () {
+    const {data} = await axios.get(this.BASE_URL)
+        return data
+       
+      }
 
+  async getOneRegister (id) {
+    const {data} = await axios.get(`${this.BASE_URL}${id}`)
+      return data
+    }
+
+  async createOneRegister() {
+    const {data} = await axios.post(this.BASE_URL)
+      return data
   }
 
-  getOneRegister () {
-
+  async updateOneRegister() {
+    const {data} = await axios.put(this.BASE_URL)
+      return data
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  async deleteOneRegister() {
+   const {data} = await axios.delete(`${this.BASE_URL}${id}`)
   }
 }
