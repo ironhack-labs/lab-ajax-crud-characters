a46edaa6a124e8fe11f506a1876feab88bebc9e7
Andre R <andre.rabaca94@gmail.com>
2020-04-28 20:46:33 +0100

    Working on itearation 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,72 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(this.BASE_URL+"/characters")
+    .then(characters =>{
+      console.log(characters.data)
+      characters.data.forEach(character => {
+      console.log(character.name)    
+      const nameHTML=document.createElement("H1")
+      const text =document.createTextNode(`${character.name}`)
+      nameHTML.appendChild(text)
+      console.log("nice until here")
+      document.getElementsByClassName("name")[0].appendChild(nameHTML)
+      })
+    })
+    .catch(err =>{
+      console.log("Something went wrong when getting the characters")
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios.get(this.BASE_URL+`/characters/${id}`)
+    .then(character =>{
+      console.log(character)
+    })
+    .catch(err =>{
+      console.log("Something went wrong when getting the character")
+    })    
   }
 
-  createOneRegister () {
-
+  createOneRegister (character) {
+    axios.post(this.BASE_URL+`/characters`, character)
+    .then(character =>{
+      console.log(character)
+    })
+    .catch(err =>{
+      console.log("Something went wrong when creating the character")
+    })    
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id , character) {
+    axios.put(this.BASE_URL+`/characters/${id}`, character)
+    .then(character =>{
+      console.log(character)
+    })
+    .catch(err =>{
+      console.log("Something went wrong when updating the character")
+    })    
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+  axios.delete(this.BASE_URL+`/characters/${id}`)
+    .then(character =>{
+      console.log("Character deleted")
+    })
+    .catch(err =>{
+      console.log("Something went wrong when updating the character")
+    })    
   }
 }
+
+/*document.getElementById('postCharacter').onclick = () => {
+    const character = {
+        name: "WALL-E",
+        occupation: "Waste Allocation Robot",
+        weapon: "Head laser"
+    };
+    axios
+      .post('https://ih-crud-api.herokuapp.com/characters', character)
+      .then(response => console.log("post success, response is", response))
+      .catch(err => console.log("error occurred", err));
+};*/
