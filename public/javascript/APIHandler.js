952def9712dd1a17d48e0c5e4d089bb3680361ea
Catarina Laranjeiro <ctlaranjeiro@gmail.com>
2020-04-29 03:02:55 +0100

    Done until delete character

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,106 +4,74 @@ class APIHandler {
   }
 
   getFullList() {
-    axios
-      .get(this.BASE_URL + '/characters')
-      .then(response => {
-        console.log('response', response.data);
-      })
-      .catch(err => console.log('error occurred', err));
+    return axios.get(this.BASE_URL + '/characters');
+      // .then(response => {
+      //   console.log('response', response.data);
+      // })
+      // .catch(err => console.log('error occurred', err));
   }
 
   getOneRegister(characterId) {
-    
-    axios
-      .get(this.BASE_URL + `/characters/${characterId}`)
-      .then(response => {
-        console.log('response', response.data);
-      })
-      .catch(err => console.log('error occurred', err));
+    return axios.get(this.BASE_URL + `/characters/${characterId}`);
+      // .then(response => {
+      //   console.log('response', response.data);
+      // })
+      // .catch(err => console.log('error occurred', err));
   }
 
   createOneRegister(character) {
-    // character = {
-    //   name: String,
-    //   occupation: String,
-    //   cartoon: Boolean,
-    //   weapon: String
-    // };
+    return axios.post(this.BASE_URL + '/characters', character);
+      // .then(response => {
+      //   const {
+      //     name,
+      //     occupation,
+      //     weapon,
+      //     cartoon
+      //   } = response.data;
 
-    if(typeof character.name !== 'string'){
-      console.log('name is not a string. Try again.');
-      return;
-    }
-    if(typeof character.occupation !== 'string'){
-      console.log('occupation is not a string. Try again.');
-      return;
-    }
-    if(typeof character.cartoon !== 'boolean'){
-      console.log('cartoon is not boolean. Try again.');
-      return;
-    }
-    if(typeof character.weapon !== 'string'){
-      console.log('weapon is not a string. Try again.');
-      return;
-    }
+      //   console.log(`New character created - Name: ${response.data.name} ID: ${response.data.id}`);
 
-    axios
-      .post(this.BASE_URL + '/characters', character)
-      .then(response => {
-        const {
-          name,
-          occupation,
-          weapon,
-          cartoon
-        } = response.data;
-        const newCharacterDiv = document.createElement('div');
-        newCharacterDiv.innerHTML = `<p>New character created - Name: ${response.data.name} ID: ${response.data.id}</p>`;
-        document.body.appendChild(newCharacterDiv);
-
-        console.log(`New character created - Name: ${response.data.name} ID: ${response.data.id}`);
-      })
-      .catch(err => {
-        console.log('error occurred:', err);
-      });
+      //   //--------------------------
+      //   //COLOCAR NO INDEX.JS   ----
+      //   // const newCharacterDiv = document.createElement('div');
+      //   // newCharacterDiv.innerHTML = `<p>New character created - Name: ${response.data.name} ID: ${response.data.id}</p>`;
+      //   // document.body.appendChild(newCharacterDiv);
+      //   //--------------------------
+      // })
+      // .catch(err => {
+      //   console.log('error occurred:', err);
+      // });
   }
 
   updateOneRegister(characterId, updatedCharacter) {
-    
-    updatedCharacter = {
-      name: String,
-      occupation: String,
-      cartoon: Boolean,
-      weapon: String
-    };
+    return axios.patch(this.BASE_URL + `/characters/${characterId}`, updatedCharacter);
+      // .then(response => {
+      //   console.log('Character updated!');
 
-    axios
-      .patch(this.BASE_URL + `/characters/${characterId}`, updatedCharacter)
-      .then(response => {
-        console.log('Character updated!');
-
-        const updatedCharacterDiv = document.createElement('div');
-        updatedCharacterDiv.innerHTML = `<p>Character updated!</p>`;
-        document.body.appendChild(updatedCharacterDiv);
-      })
-      .catch(err => {
-        if (err.response.status === 404) {
-          const errorDiv = document.createElement('div');
-          errorDiv.innerHTML = `<p>There's no character with id: ${characterId}</p>`;
-          document.body.appendChild(errorDiv);
-        }
-      });
+      //   //--------------------------
+      //   //COLOCAR NO INDEX.JS   ----
+      //   // const updatedCharacterDiv = document.createElement('div');
+      //   // updatedCharacterDiv.innerHTML = `<p>Character updated!</p>`;
+      //   // document.body.appendChild(updatedCharacterDiv);
+      //   //--------------------------
+      // })
+      // .catch(err => {
+      //   if (err.response.status === 404) {
+      //     const errorDiv = document.createElement('div');
+      //     errorDiv.innerHTML = `<p>There's no character with id: ${characterId}</p>`;
+      //     document.body.appendChild(errorDiv);
+      //   }
+      // });
   }
 
 
   deleteOneRegister(characterId) {
-    
-    axios
-      .delete(this.BASE_URL + `/characters/${characterId}`)
-      .then(response => {
-        console.log('Character has been successfully deleted.');
-      })
-      .catch(err => {
-        console.log('Character not found.');
-      });
+    return axios.delete(this.BASE_URL + `/characters/${characterId}`);
+      // .then(response => {
+      //   console.log('Character has been successfully deleted.');
+      // })
+      // .catch(err => {
+      //   console.log('Character not found.');
+      // });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,22 +1,127 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-charactersAPI.getFullList();
+// charactersAPI.getFullList();
 //charactersAPI.getOneRegister(2);
-charactersAPI.createOneRegister({name: 4, occupation:'unknown', cartoon: 'true', weapon: 'knife'});
+// charactersAPI.createOneRegister({name: 'Hey', occupation:'unknown', cartoon: true, weapon: 'jokes'});
 //charactersAPI.deleteOneRegister(11);
-charactersAPI.getFullList();
+// charactersAPI.getFullList();
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-    
+    charactersAPI.getFullList()
+      .then(response => {
+        const data = response.data;
+        console.log('getFullList() > response from API: ', data);
+        
+        document.querySelector('.characters-container').innerHTML = '';
+
+        data.forEach(characterAPI => {
+          const characterInfo = document.createElement('div');
+
+          characterInfo.setAttribute("class", "character-info flex");
+
+          characterInfo.innerHTML = `
+            <div class="tags">
+              <div class="id">Id: </div>
+              <div class="name">Name: </div>
+              <div class="occupation">Occupation: </div>
+              <div class="cartoon">Is a Cartoon?:</div>
+              <div class="weapon">Weapon:</div>
+            </div>
+            <div class="detail">
+              <div class="getName">${characterAPI.id}</div>
+              <div class="getName">${characterAPI.name}</div>
+              <div class="getOccupation">${characterAPI.occupation}</div>
+              <div class="getCartoon">${characterAPI.cartoon}</div>
+              <div class="getWeapon">${characterAPI.weapon}</div>
+            </div>
+          `;
+          document.querySelector('.characters-container').appendChild(characterInfo);
+        });
+      })
+      .catch(err => console.log('error occurred', err));
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    const characterId = document.getElementById('character-id').value;
 
+    charactersAPI.getOneRegister(characterId)
+      .then(response => {
+        const characterData = response.data;
+        console.log('getOneRegister() > response from API: ', characterData);
+
+        document.querySelector('.characters-container').innerHTML = '';
+
+        const characterInfo = document.createElement('div');
+
+          characterInfo.setAttribute("class", "character-info flex");
+
+          characterInfo.innerHTML = `
+            <div class="tags">
+              <div class="id">Id: </div>
+              <div class="name">Name: </div>
+              <div class="occupation">Occupation: </div>
+              <div class="cartoon">Is a Cartoon?:</div>
+              <div class="weapon">Weapon:</div>
+            </div>
+            <div class="detail">
+              <div class="getId">${characterData.id}</div>
+              <div class="getName">${characterData.name}</div>
+              <div class="getOccupation">${characterData.occupation}</div>
+              <div class="getCartoon">${characterData.cartoon}</div>
+              <div class="getWeapon">${characterData.weapon}</div>
+            </div>
+          `;
+          document.querySelector('.characters-container').appendChild(characterInfo);
+
+          document.getElementById('character-id').value = '';
+
+      })
+      .catch(err => {
+        console.log('error occurred', err);
+
+        document.getElementById("fetch-one").style.backgroundColor = "red";
+        document.getElementById("fetch-one").innerText = "Id doesn't exist!";
+
+        setTimeout(() => {
+          document.getElementById('character-id').value = '';
+          document.getElementById("fetch-one").style.background = "none";
+          document.getElementById("fetch-one").innerText = "Fetch one";
+        }, 2000);
+      });
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    const characterId = document.getElementById('character-id-delete').value;
 
+    charactersAPI.deleteOneRegister(characterId)
+      .then(response => {
+        const characterData = response.data;
+        console.log('Character has been successfully deleted.');
+
+        document.getElementById("delete-one").style.backgroundColor = "green";
+        document.getElementById("delete-one").innerText = "Success!";
+
+        document.querySelector('.characters-container').innerHTML = '';
+
+        setTimeout(() => {
+          document.getElementById('character-id-delete').value = '';
+          document.getElementById("delete-one").style.background = "none";
+          document.getElementById("delete-one").innerText = "Delete one";
+        }, 2000);
+      })
+      .catch(err => {
+        console.log('Character not found.');
+        
+        document.getElementById("delete-one").style.backgroundColor = "red";
+        document.getElementById("delete-one").innerText = "Id doesn't exist!";
+
+        setTimeout(() => {
+          document.getElementById('character-id-delete').value = '';
+          document.getElementById("delete-one").style.background = "none";
+          document.getElementById("delete-one").innerText = "Delete one";
+        }, 2000);
+      });
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
