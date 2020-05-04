3c83328641569415dcd87e73369cd69b7bf8f286
Margarida Pascoal <amppascoal@gmail.com>
2020-05-04 19:06:24 +0100

    unfinished lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,71 @@
+// Get all the characters info from http://localhost:8000/characters
+// Get a single character info from http://localhost:8000/characters/:id
+// Create a single character posting the data to http://localhost:8000/characters
+// Delete a single character through his id in http://localhost:8000/characters/:id
+// Edit a single character through his id in http://localhost:8000/characters/:id
+
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    return axios
+      .get(this.BASE_URL + `/characters`)
+      .then(response => {
+        return response.data;
+    })
+    .catch(err => {
+      console.log(`There was some sort of error: ${err}`);
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    
+    return axios
+      .get(this.BASE_URL + `/characters/${id}`)
+      .then(response => {
+        console.log(response.data)
+        return response.data;
+    })
+    .catch(err => {
+      console.log(`There was some sort of error: ${err}`);
+    });
   }
 
   createOneRegister () {
 
+    return axios
+    .post(this.BASE_URL + `/characters`, character)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(`There was some sort of error: ${err}`);
+    });
   }
 
   updateOneRegister () {
 
+    axios
+    .patch(`http://localhost:8000/characters/${id}`, updatedRegister)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(`There was some sort of error: ${err}`);
+    });
   }
 
   deleteOneRegister () {
-
+    axios
+    .delete(`http://localhost:8000/characters/${id}`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(`There was some sort of error: ${err}`);
+    });
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,55 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+      
+  document.getElementById('here').innerHTML = '';
 
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    event.preventDefault;
+    charactersAPI.getFullList()
+      .then(response => {
+        //console.log("response", response);
+
+        for (let i=0; i<response.length; i++) {
+
+          const {id, name, occupation, cartoon, weapon} = response[i]; 
+            
+          const characterCard = `
+            <div style="text-align:left">
+                <span>Id: ${id}</span> <br>
+                <span>Name: ${name}</span> <br>
+                <span>Occupation: ${occupation}</span> <br>
+                <span>Is a Cartoon?: ${cartoon}</span> <br >
+                <span>Weapon: ${weapon}</span>
+            </div>
+          `
+          document.getElementById('here').innerHTML += characterCard;
+        }
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    event.preventDefault;
 
+    charactersAPI.getOneRegister(id)
+    .then(response => {
+      
+
+      document.getElementById('here').innerHTML = '';
+        const {id, name, occupation, isAcartoon, weapon} = response;
+        const id = document.getElementsByName('character-id').value;
+
+        const characterCard = `
+          <div style="text-align:left">
+              <span>Id: ${id}</span> <br>
+              <span>Name: ${name}</span> <br>
+              <span>Occupation: ${occupation}</span> <br>
+              <span>Is a Cartoon?: ${isAcartoon}</span> <br>
+              <span>Weapon: ${weapon}</span>
+          </div>
+          `
+        document.getElementById('here').innerHTML += characterCard;
+    })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
