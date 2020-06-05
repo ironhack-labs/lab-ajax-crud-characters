6414856d77cc7304d8ab7c9ef867ceec7ed35aed
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-06-05 18:36:12 +0200

    Iteration 3 done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,58 +1,73 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-    axios
-    .get(`${this.BASE_URL}/characters`)
-    .then(response => {
-      console.log('Response from the API: ', response);
-      const data = response.data;
-      console.log('The array of characters: ', data);
-    })
-    .catch(err => console.log(`Error while getting the list of characters: ${err}`));
-
+  getFullList() {
+    return axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const data = response.data;
+        console.log('The array of characters: ', data);
+        return data;
+      })
+      .catch(err => {
+        console.log(`Error while getting the list of characters: ${err}`);
+        throw err;
+      });
   }
 
-  getOneRegister (id) {
-    axios
-    .get(`${this.BASE_URL}/characters/${id}`)
-    .then(response => {
-      console.log('Response from the API: ', response);
-      const data = response.data;
-      console.log('The array of characters: ', data);
-    })
-    .catch(err => console.log(`Error while getting the list of characters: ${err}`));
+  getOneRegister(id) {
+    return axios
+      .get(`${this.BASE_URL}/characters/${id}`)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const data = response.data;
+        console.log('The array of the character: ', data);
+        return data;
+      })
+      .catch(err => {
+        console.log(`Error while getting the character: ${err}`);
+        throw err;
+      });
   }
 
-  createOneRegister (newCharacterInfo) {
-    axios
-    .post(`${this.BASE_URL}/characters`, newCharacterInfo)
-    .then(() => {
-     this.getFullList();
-    
-    })
-    .catch(err => console.log(`Error while creating new character: ${err}`));
+  createOneRegister(newCharacterInfo) {
+    return axios
+      .post(`${this.BASE_URL}/characters`, newCharacterInfo)
+      .then(() => {
+        this.getFullList();
+      })
+      .catch(err => {
+        console.log(`Error while creating new character: ${err}`);
+        throw err;
+      });
   }
 
-  updateOneRegister (updateCharacterInfo,id) {
-    axios
-    .put(`${this.BASE_URL}/characters/${id}`, updateCharacterInfo)
-    .then(response => {
-      console.log(response);
-      this.getFullList();
-    })
-    .catch(error => console.log(`Error while updating a character: ${error}`));
-
+  updateOneRegister(updateCharacterInfo, id) {
+    return axios
+      .put(`${this.BASE_URL}/characters/${id}`, updateCharacterInfo)
+      .then(response => {
+        console.log(response);
+        this.getFullList();
+      })
+      .catch(err => {
+        console.log(`Error while updating new character: ${err}`);
+        throw err;
+      });
   }
 
-  deleteOneRegister (id) {
-    axios
-    .delete(`${this.BASE_URL}/characters/${id}`)
-    .then(response => {
-      this.getFullList();
-    })
-    .catch(err => console.log(`Err while deleting character: ${err}`));
+  deleteOneRegister(id) {
+    return axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(response => {
+        this.getFullList();
+      })
+      .catch(err => {
+        console.log(`Error while deleting character: ${err}`);
+        throw err;
+      });
+
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,15 +2,53 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-    charactersAPI.getFullList();
+    charactersAPI.getFullList().then(data => {
+      let str = '';
+      data.forEach(character => {
+        str += `
+       <div class="character-info">
+         <div class="name"><b>Name: </b>${character.name}</div>
+         <div class="occupation"><b>Occupation: </b>${character.occupation}</div>
+         <div class="cartoon"><b>Is a Cartoon? </b>${character.cartoon}</div>
+         <div class="weapon"><b>Weapon: </b>${character.weapon}</div>
+       </div>
+   `;
+      });
+      document.getElementsByClassName('characters-container')[0].innerHTML = str;
+      document.getElementById('fetch-all').className = "green-button";
+    }).catch(err => {
+      document.getElementById('fetch-all').className = "red-button";
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-    charactersAPI.getOneRegister(document.getElementsByName("character-id")[0].value);
+    charactersAPI.getOneRegister(document.getElementsByName("character-id")[0].value).then(character => {
+      let str = '';
+      str += `
+        <div class="character-info">
+          <div class="name"><b>Name: </b>${character.name}</div>
+          <div class="occupation"><b>Occupation: </b>${character.occupation}</div>
+          <div class="cartoon"><b>Is a Cartoon? </b>${character.cartoon}</div>
+          <div class="weapon"><b>Weapon: </b>${character.weapon}</div>
+        </div>
+    `;
+      document.getElementsByClassName('characters-container')[0].innerHTML = str;
+      document.getElementById('fetch-one').className = "green-button";
+    }).catch(err => {
+      document.getElementById('fetch-one').className = "red-button";
+    });
+    document.getElementById('char-id').value="";
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-    charactersAPI. deleteOneRegister(document.getElementsByName("character-id-delete")[0].value);
+    charactersAPI.deleteOneRegister(document.getElementsByName("character-id-delete")[0].value)
+      .then(data => {
+        document.getElementById('delete-one').className = "green-button";
+        document.getElementsByName('character-id-delete')[0].innerText = "";
+      }).catch(err => {
+        document.getElementById('delete-one').className = "red-button";
+      });
+      document.getElementById('del-char-id').value="";
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
@@ -19,7 +57,7 @@ window.addEventListener('load', () => {
     const name = document.getElementById('update-name').value;
     const occupation = document.getElementById('update-occupation').value;
     const weapon = document.getElementById('update-weapon').value;
-    const cartoon =  document.getElementById('update-cartoon').checked;
+    const cartoon = document.getElementById('update-cartoon').checked;
     const updateCharacterInfo = {
       id,
       name,
@@ -28,19 +66,23 @@ window.addEventListener('load', () => {
       cartoon
     };
     console.log('Update character: ', updateCharacterInfo);
-    charactersAPI.updateOneRegister(updateCharacterInfo, id);
+    charactersAPI.updateOneRegister(updateCharacterInfo, id)
+    .then(data => {
+      document.getElementById('update-data').className = "green-button";
+    }).catch(err => {
+      document.getElementById('update-data').className = "red-button";
+    });
     document.getElementById('edit-character-form').reset();
 
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
     event.preventDefault();
     const name = document.getElementsByName('name')[0].value;
     const occupation = document.getElementsByName('occupation')[0].value;
     const weapon = document.getElementsByName('weapon')[0].value;
-    const cartoon =  document.getElementsByName('cartoon')[0].checked;
+    const cartoon = document.getElementsByName('cartoon')[0].checked;
 
     const newCharacterInfo = {
       name,
@@ -50,8 +92,12 @@ window.addEventListener('load', () => {
     };
 
     console.log('New character: ', newCharacterInfo);
-    charactersAPI.createOneRegister(newCharacterInfo);
+    charactersAPI.createOneRegister(newCharacterInfo)
+    .then(data => {
+      document.getElementById('send-data').className = "green-button";
+    }).catch(err => {
+      document.getElementById('send-data').className = "red-button";
+    });
     document.getElementById('new-character-form').reset();
-
   });
 });
