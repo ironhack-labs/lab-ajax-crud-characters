52aeb6b5fd4cf53c2514fc246014697ef3749638
JesusSantana <jesus@esmarketingdigital.com>
2020-06-07 17:06:25 +0200

    Iteration 1-2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,40 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
 
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(characters => characters)
+      .catch(err => console.log(`Error while getting the list of characters: ${err}`));
   }
 
-  getOneRegister () {
+  getOneRegister(id) {
 
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(character => character)
+      .catch(err => console.log(`Error while getting the list of characters: ${err}`));
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    
+    return axios.post(`${this.BASE_URL}/characters`, newCharacter)
+      .then()
+      .catch(err => console.log(`Error while creating new character: ${err}`));
   }
 
-  updateOneRegister () {
+  updateOneRegister(updateCharacter, id) {
 
+    return axios.put(`${this.BASE_URL}/characters/${id}`, updateCharacter)
+      .then(character => character)
+      .catch(error => console.log(`Error while updating a character: ${error}`));
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(id) {
 
+    return axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then(character => character)
+      .catch(err => console.log(`Err while deleting character: ${err}`));
   }
 }
