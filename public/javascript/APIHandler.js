1da14193fa86fe5e1c8261c96460b259557fe3a7
Natalia López <natalialopezyeste@gmail.com>
2020-06-08 19:51:24 +0200

    Iteration 1 and 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,47 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(responseFromAPI => {
+        console.log(responseFromAPI.data); 
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios
+      .get(`${this.BASE_URL}/characters/${id}`)
+      .then(responseFromAPI => {
+        console.log(responseFromAPI.data); 
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
   }
 
-  createOneRegister () {
-
+  createOneRegister (obj) {
+    axios
+      .post(`${this.BASE_URL}/characters`, obj)
+      .then(responseFromAPI => {
+        console.log("Personaje creado", responseFromAPI); 
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, obj) {
+    axios
+      .put(`${this.BASE_URL}/characters/${id}`, obj)
+      .then(responseFromAPI => {
+        console.log("Personaje editado", responseFromAPI); 
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(responseFromAPI => {
+        console.log("Personaje eliminado", responseFromAPI); 
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,41 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const id = document.getElementById('search-id').value;
+    charactersAPI.getOneRegister(id);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const id = document.getElementById('delete-id').value;
+    charactersAPI.deleteOneRegister(id);
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const id = document.getElementById('edit-id').value;
 
+    const name = document.getElementById('edit-name').value;
+    const occupation = document.getElementById('edit-occupation').value;
+    const cartoon = document.getElementById('edit-cartoon').value;
+    const weapon = document.getElementById('edit-weapon').value;
+
+    const obj = {name, occupation, cartoon, weapon};
+
+    charactersAPI.updateOneRegister(id, obj);
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    const name = document.getElementById('create-name').value;
+    const occupation = document.getElementById('create-occupation').value;
+    const cartoon = document.getElementById('create-cartoon').value;
+    const weapon = document.getElementById('create-weapon').value;
+
+    const obj = {name, occupation, cartoon, weapon};
+
+    charactersAPI.createOneRegister(obj);
 
   });
 });
