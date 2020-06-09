3c75fc74868f3eead44fbb0ec343f639462ac545
Robert Ledezma <Robert@MacBook-Pro-de-Robert-2.local>
2020-06-09 11:23:04 +0200

    Done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,47 @@
+//import Axios from "axios";
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(res => {
+        console.log('RESPUESTA DE API', res)
+      })
+      .catch(error => console.log('ERROR AL CONSULTAR LA DATA', error))
+  }
+
+  getOneRegister(id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+      .then(res => {
+        const characterRegister = res.data
+        console.log(`ID DEL CARACTER:`, characterRegister)
+      })
+      .catch(error => console.log('ERROR AL VER AL CHARACTER', error))
+  }
+
+  createOneRegister(newCharacterInfo) {
+    axios.post(`${this.BASE_URL}/characters`, newCharacterInfo)
+      .then(res => {
+        console.log('New Character: ', res.data)
+        //getFullList()
+      })
+      .catch(error => console.log('ERROR AL CREAR AL CHARACTER', error))
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister(id, update) {
+    axios.put(`${this.BASE_URL}/characters/${id}`, update)
+      .then(res => console.log(res.data))
+      .catch(error => console.log(error))
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+      .then(res => console.log("Character has been successfully deleted", res.data))
+      .catch(error => console.log(error))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,38 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    //const charatecrinput = document.getElementById('fecthById').value
+    charactersAPI.getOneRegister(document.getElementById('fecthById').value)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(document.getElementById('deleteCharacter').value)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const editCharacter = {
+      id: document.getElementById('editId').value,
+      name: document.getElementById('editName').value,
+      occupation: document.getElementById('editOccupation').value,
+      weapon: document.getElementById('editWeapon').value,
+      cartoon: document.getElementById('editCartoon').checked
 
+    }
+    charactersAPI.updateOneRegister(editCharacter.id, editCharacter)
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    const objCharacter = {
+      name: document.getElementById('newName').value,
+      occupation: document.getElementById('newOcuppation').value,
+      weapon: document.getElementById('newWeapon').value,
+      cartoon: document.getElementById('newCartoon').checked
 
+    }
+    charactersAPI.createOneRegister(objCharacter)
   });
 });
