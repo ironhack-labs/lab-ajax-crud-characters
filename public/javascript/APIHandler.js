789e27a7635758b4b29284350eeadc1602f5d1eb
Luciana Vina <lucianasvina@gmail.com>
2020-06-30 22:00:22 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL + "/characters")
+      .then((response) => console.log(response.data))
+      .catch((err) => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    const id = document.querySelector(".operation input").value;
+    axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then((oneCharacter) => console.log(oneCharacter.data))
+      .catch((err) => console.log("err", err));
   }
 
-  createOneRegister () {
+  createOneRegister() {
+    const input = document.querySelectorAll("#new-character-form input");
 
+    const theCharacter = {
+      name: input[0].value,
+      occupation: input[1].value,
+      weapon: input[2].value,
+      cartoon: input[3].checked,
+    };
+
+    axios
+      .post(this.BASE_URL + "/characters", theCharacter)
+      .then((input) => console.log(input.data))
+      .catch((err) => console.log("err", err));
   }
 
-  updateOneRegister () {
+  updateOneRegister() {
+    // const idUpd = document.querySelector("#edit-character-form input").value;
+    // const input = document.querySelectorAll("#edit-character-form input");
 
+    // const newCharInfo = {
+    //   name: input[1].value,
+    //   occupation: input[2].value,
+    //   weapon: input[3].value,
+    //   cartoon: input[4].checked,
+    // }
+
+    // axios
+    //   .put((this.BASE_URL + "/characters/" + idUpd), newCharInfo)
+    //   .then(console.log(newCharInfo))
+    //   .catch((err) => console.log("err", err));
   }
 
-  deleteOneRegister () {
+  deleteOneRegister(id) {
+    const idDel = document.querySelector(".delete input").value;
 
+    axios
+      .delete(this.BASE_URL + "/characters/" + idDel)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log("err", err));
   }
 }
