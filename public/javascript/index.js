52ea482edc2e073f7f01226537e44ad9e1063b13
andresbr92 <andresbr92@gmail.com>
2020-06-30 20:52:00 +0200

    done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,35 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    event.preventDefault()
+
+    charactersAPI.getFullList()
+    
+    
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    charactersAPI.getOneRegister()
+    event.preventDefault()
+    charactersAPI.printOne ()
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    
+    charactersAPI.deleteOneRegister()
+    event.preventDefault()
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    charactersAPI.updateOneRegister ()
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    charactersAPI.createOneRegister ()
   });
 });
