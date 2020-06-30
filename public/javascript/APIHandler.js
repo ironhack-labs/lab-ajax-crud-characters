50bd89eb82e8aa73491702050df5d218a11db589
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-06-30 23:16:24 +0200

    done without dom manipulation

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,49 @@
+
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
+    this.axiosApp = axios.create({ baseURL: baseUrl })
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.axiosApp.get('/characters')
+      .then(res => {
+        console.log('Get full list: ', res.data)
+        return res.data
+      })
+      .catch(error => console.log('Error getting character list', error))
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.axiosApp.get(`/characters/${id}`)
+      .then(res => {
+        console.log('Get one register: ', res.data)
+        return res.data
+      })
+      .catch(error => console.log('Error fetching one character', error))
   }
 
-  createOneRegister () {
+  createOneRegister(createdChar) {
+    return this.axiosApp.post(`/characters`, createdChar)
+      .then(res => {
+        console.log('Create one register:', res.data)
+        return res.data
 
+      })
+      .catch(error => console.log('Error creating new character', error))
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updatedChar) {
+    return this.axiosApp.put(`/characters/${id}`, updatedChar)
+      .then(res => console.log('Update one register:', res.data))
+      .catch(error => console.log(error))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return this.axiosApp.delete(`/characters/${id}`)
+      .then(res => console.log('Delete one register: ', res.data))
+      .catch(error => console.log(error))
   }
 }
+
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,56 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8000')
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    event.preventDefault()
+    charactersAPI.getFullList()
+      .then(res => {
+        console.log('Char list', res.data)
+      })
 
-  });
+    document.getElementById('fetch-one').addEventListener('click', function (event) {
+      event.preventDefault()
+      const id = document.querySelector('.fetchOne').value
+      console.log(id)
+      charactersAPI.getOneRegister(id)
+    })
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+    document.getElementById('delete-one').addEventListener('click', function (event) {
+      event.preventDefault()
+      const id = document.querySelector('.deleteOne').value
+      console.log(id)
+      charactersAPI.deleteOneRegister(id)
+    })
 
-  });
+    document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+      event.preventDefault()
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+      const id = document.querySelector('.updateOne').value
+      const input = document.querySelectorAll('#edit-character-form input')
+      const newChar = {
+        name: input[1].value,
+        occupation: input[2].value,
+        weapon: input[3].value,
+        cartoon: input[4].checked
+      }
+      charactersAPI.updateOneRegister(id, newChar)
 
-  });
+    })
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    document.getElementById('new-character-form').addEventListener('submit', function (event) {
+      event.preventDefault()
 
-  });
+      const input = document.querySelectorAll('#new-character-form input')
+      const newChar = {
+        name: input[0].value,
+        occupation: input[1].value,
+        weapon: input[2].value,
+        cartoon: input[3].checked
+      }
+      charactersAPI.createOneRegister(newChar)
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
-  });
-});
+    })
+  })
+})
