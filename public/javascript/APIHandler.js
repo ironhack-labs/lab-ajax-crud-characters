b78c13e59a2f9c8b71a6a81c405ef31713c1d162
Diego Ortiz <diegortiz988@gmail.com>
2020-06-30 21:02:49 +0200

    iteraciones 1 y 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,68 @@
+
+const axiosApp = axios.create({
+  baseURL: 'http://localhost:8000'
+
+})
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axiosApp
+    .get(`${this.BASE_URL}/characters`)
+    .then(allCharacters => console.log(allCharacters.data))
+    .catch(error => console.log(error))
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    // const id = document.querySelector('#fetch-one').value
+    return axiosApp
+    .get(`${this.BASE_URL}/characters/${id}`)
+    .then(oneCharacter => console.log(oneCharacter.data))
+    .catch(error => console.log(error))
   }
 
-  createOneRegister () {
+  createOneRegister() {
 
+    const input = document.querySelectorAll('#new-character-form input')
+    const newCharacter = {
+      name: input[0].value,
+      occupation: input[1].value,
+      weapon: input[2].value,
+      cartoon: input[3].checked
   }
 
-  updateOneRegister () {
-
+    return axiosApp.
+      post(`${this.BASE_URL}/characters`, newCharacter)
+      .then(newCharacterCheck => console.log(newCharacterCheck.data))
+      .catch(error => console.log('oh no error', error))
   }
 
-  deleteOneRegister () {
+  updateOneRegister() {
+   
+    const inputEdit = document.querySelectorAll('#edit-character-form input')
+    const characterEdit = {
+      id: inputEdit[0].value,
+      name: inputEdit[1].value,
+      occupation: inputEdit[2].value,
+      weapon: inputEdit[3].value,
+      cartoon: inputEdit[4].checked
+    }
 
+    return axiosApp
+    .put(`${this.BASE_URL}/characters/:id`, characterEdit)
+    .then(oneCharacter => console.log(oneCharacter.data))
+    .catch(error => console.log(error))
   }
+
+  deleteOneRegister() {
+    return axiosApp
+    .delete(`${this.BASE_URL}/characters/:id`)
+    .then(deleteCharacter => console.log(deleteCharacter.data))
+    .catch(error => console.log(error))
+  }
+
+  
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,39 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    
+    charactersAPI.getFullList()
+    //hay que poner algo para que se imprima, pero me sale en la consola//
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    let id = document.getElementById('character-id').value
 
+    event.preventDefault()
+
+    charactersAPI.getOneRegister(id)
+
+    //dice que value no se puede usar, nodevalue tampoco; pero todo correcto en postman//
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister()
+    event.preventDefault()
+    
+    //se me borra en POSTMAN, pero no en el html, dice 404, como si no hay nada//
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
+    charactersAPI.updateOneRegister()
+
+
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+  event.preventDefault()
+  charactersAPI.createOneRegister(newCharacter)
 
   });
 });
