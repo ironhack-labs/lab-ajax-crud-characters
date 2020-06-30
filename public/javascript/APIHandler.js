ec19740d0b31e5324008680d3411e3a7aa2993d9
Lovemetal14 <franj.form019@gmail.com>
2020-07-01 01:05:50 +0200

    done til update one

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,57 @@ class APIHandler {
   }
 
   getFullList () {
+    return axios.get(`${this.BASE_URL}/characters`)
+      .then(res => {
+        const char = res.data
+        console.log(`Character id:`, char)
+      })
+      .catch(error => console.log('Error getting character', error))
 
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
+      const char = res.data
+      console.log(`Character id:`, char)
+    })
+    .catch(error => console.log('Error getting character', error))
+
 
   }
 
-  createOneRegister () {
+  createOneRegister (newCharacter) {
 
+    console.log("inputs: ", newCharacter)
+
+    return axios.post(`${this.BASE_URL}/characters`, newCharacter)
+    .then(() => updateCharacters())
+    .catch(err => console.log('Error creating new Char', err))
+    
+    
   }
 
   updateOneRegister () {
 
+    const id = document.querySelector()
+    const inputs = document.querySelectorAll('#edit-character-form input')
+
+    const Character = {
+      
+      id: inputs[0].value,
+      name: inputs[1].value,
+      occupation: inputs[2].value,
+      weapon: inputs[3].value,
+      cartoon: inputs[4].checked,
+    } 
+
+    axios.put('characters/${id}')
   }
 
   deleteOneRegister () {
 
   }
 }
+
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,38 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    const id = document.querySelector('#searchone input').value
+    charactersAPI.getOneRegister(id)
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
+    
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    charactersAPI.updateOneRegister()
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+
+    const inputs = document.querySelectorAll('#new-character-form input')
+
+    const newCharacter = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].checked
+    } 
+    charactersAPI.createOneRegister(newCharacter)
 
   });
 });
