bd2a6e09d6439b1131e0d2a67a3049d569538c02
EFdez <escarlafdez@gmail.com>
2020-06-30 17:06:33 +0200

    iteration2 done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,90 @@
+// Axios app
+const axiosApp = axios.create({
+  baseURL: 'http://localhost:8000'
+})
+
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axiosApp
+      .get('/characters')
+      .then(response => {
+        console.log('Characters:', response.data)
+      })
+      .catch(err => console.log(err))
+
+    const cardOriginal = document.getElementById('char-info')
 
   }
 
-  getOneRegister () {
 
+
+  //     < div class="characters-container" >
+  //       <div id="char-info" class="character-info">
+  //         <div class="name">Character Name</div>
+  //         <div class="occupation">Character Occupation</div>
+  //         <div class="cartoon">Is a Cartoon?</div>
+  //         <div class="weapon">Character Weapon</div>
+  //       </div>
+  // </div >
+
+
+
+
+  getOneRegister() {
+    let id = document.getElementById("character-id").value
+    axiosApp
+      .get('/characters/' + id)
+      .then(response => {
+        console.log('Characters:', response.data)
+        // document.querySelector('#character-id').reset()
+      })
+      .catch(err => console.log(err))
   }
 
-  createOneRegister () {
 
+  createOneRegister() {
+    const inputNew = document.querySelectorAll("#new-character-form input")
+    const characterNew = {
+      name: inputNew[0].value,
+      occupation: inputNew[1].value,
+      weapon: inputNew[2].value,
+      cartoon: inputNew[3].checked
+    }
+
+    axiosApp
+      .post('/characters', characterNew)
+      .catch(err => console.log('err', err))
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    console.log("entras asquí?")
+    const inputEdit = document.querySelectorAll("#edit-character-form input")
+    const characterEdit = {
+      id: inputEdit[0].value,
+      name: inputEdit[1].value,
+      occupation: inputEdit[2].value,
+      weapon: inputEdit[3].value,
+      cartoon: inputEdit[4].checked
+    }
+    axiosApp
+      .put(`/characters/${characterEdit.id}`, characterEdit)
+      .catch(err => console.log('err', err))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    let id = document.getElementById("character-id-delete").value
+    console.log(id)
+    axiosApp
+      .delete('/characters/' + id)
+      .then(response => {
+        console.log('Characters:', response.data)
+        // document.querySelector('#character-id').reset()
+      })
+      .catch(err => console.log(err))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,33 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault()
+    charactersAPI.getFullList()
   });
 
+
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    event.preventDefault()
+    charactersAPI.getOneRegister()
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    event.preventDefault()
 
+    charactersAPI.deleteOneRegister()
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    console.log("te estoy clicando locamenti")
+    event.preventDefault()
+    charactersAPI.updateOneRegister()
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    charactersAPI.createOneRegister()
   });
 });
+
+
