71e251890c92306b2eafe9bb8adc866003cb2c63
David <drgomez92@gmail.com>
2020-06-30 23:28:41 +0200

    Lab ajax CRUD: FINALIZADO.

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,70 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApp = axios.create({baseURL: baseUrl})
   }
 
   getFullList () {
-
+    return this.axiosApp.get(`/characters`)
+      .then(res => res.data.forEach(elm => newCardCharacter(elm)))
+      .catch(console.log(err => console.log('Error al acceder a la BD: ', err)))
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return this.axiosApp.get(`/characters/${id}`)
+      .then(res => newCardCharacter(res.data))
+      .catch(err => console.log('Error al acceder al ID: ', err))
   }
 
-  createOneRegister () {
-
+  createOneRegister (character) {
+    return this.axiosApp.post('/characters', character)
+      .then(res =>{
+        document.querySelector('#new-character-form').reset()
+        document.querySelector('#create-data').classList.toggle('btn-green')
+      })
+      .catch(err => {
+        console.log('Error al crear el registro: ', err)
+        document.querySelector('#create-data').classList.toggle('btn-green')
+      })
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (newData, id) {
+    return this.axiosApp.put(`/characters/${id}`, newData)
+      .then(res => {
+        document.querySelector('#edit-character-form').reset()
+        document.querySelector('#update-data').classList.toggle('btn-green')
+      })
+      .catch(err => {
+        console.log('Error al actualizar el registro: ', err)
+        document.querySelector('#update-data').classList.toggle('btn-red')
+      })
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    return this.axiosApp.delete(`/characters/${id}`)
+      .then(res => document.querySelector('#delete-one').classList.toggle('btn-green'))
+      .catch(err => {
+        console.log('Error al eliminar el registro: ', err)
+        document.querySelector('#delete-one').classList.toggle('btn-red')
+      })
   }
+}
+
+function newCardCharacter(elm) {
+  let newCard = document.getElementById('ch-info').cloneNode(true)
+  newCard.classList.toggle('char-display')
+        
+  let clonedID = newCard.querySelector('.id')
+  let clonedName = newCard.querySelector('.name')
+  let clonedOccupation = newCard.querySelector('.occupation')
+  let clonedIsCartoon = newCard.querySelector('.cartoon')
+  let clonedWeapon = newCard.querySelector('.weapon')
+
+  clonedID.innerHTML = `ID: ${elm.id}` 
+  clonedName.innerHTML = `Character Name: ${elm.name}`
+  clonedOccupation.innerHTML = `Character Occupation: ${elm.occupation}`
+  clonedIsCartoon.innerHTML = `Is a Cartoon?: ${elm.cartoon}`
+  clonedWeapon.innerHTML = `Character Weapon: ${elm.weapon}`
+
+  document.querySelector('.characters-container').appendChild(newCard)
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,46 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  event.preventDefault()
 
-  });
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+  })
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
+    const id = document.querySelector('input[name="character-id"]').value
+    charactersAPI.getOneRegister(id)
+  })
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
+    const id = document.querySelector('input[name="character-id-delete"]').value
+    charactersAPI.deleteOneRegister(id)
+  })
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const id = document.querySelector('input[name="chr-id"]').value
+    const inputs = document.querySelectorAll('#edit-character-form input')
 
-  });
+    const newData = {
+      id: inputs[0].value,
+      name: inputs[1].value,
+      occupation: inputs[2].value,
+      weapon: inputs[3].value,
+      cartoon: inputs[4].checked
+    }
+    charactersAPI.updateOneRegister(newData, id)
+  })
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    const inputs = document.querySelectorAll('#new-character-form input')
 
-  });
-});
+    const character = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].checked
+    }
+    console.log(character)
+    charactersAPI.createOneRegister(character)
+  })
+})
