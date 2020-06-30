5f52369ec18c93a6a54dd3c1e05d9ee50b8358e6
Laura del Toro <lauradeltoro.arq@gmail.com>
2020-06-30 20:37:17 +0200

    Status: Done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,50 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.axiosApp = axios.create({
+      baseURL: baseUrl
+    })
   }
-
-  getFullList () {
-
+  getFullList() {
+    return this.axiosApp.get("/characters")
+      .then(response => response.data)
+      .catch(err => {
+        throw new Error(err)
+      })
   }
-
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.axiosApp.get(`/characters/${id}`)
+      .then(response => response.data)
+      .catch(err => {
+        throw new Error(err)
+      })
   }
-
-  createOneRegister () {
-
+  createOneRegister(character) {
+    return this.axiosApp.post("/characters", character)
+      .then(response => response.data)
+      .catch(err => {
+        throw new Error(err)
+      })
   }
-
-  updateOneRegister () {
-
+  updateOneRegister(id, characterToUpdate) {
+    return this.axiosApp.put(`/characters/${id}`, characterToUpdate)
+      .then(response => response.data)
+      .catch(err => {
+        if (err.response.status === 404) {
+          throw new Error("Character not found")
+        } else {
+          throw new Error("Unknown error")
+        }
+      })
   }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return this.axiosApp.delete(`/characters/${id}`)
+      .then(() => "Character has been successfully deleted")
+      .catch(err => {
+        if (err.response.status === 404) {
+          throw new Error("Character not found")
+        } else {
+          throw new Error("Unknown error")
+        }
+      })
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,115 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+// New characters API
+const charactersAPI = new APIHandler('http://localhost:8000')
 
+//Selectors 
+const mainCardCointainer = document.querySelector(".characters-container")
+const editButton = document.querySelector("#edit-character-form #send-data")
+const createButton = document.querySelector("#new-character-form #send-data")
+const editForm = document.querySelector("#edit-character-form")
+const createForm = document.querySelector("#new-character-form")
+const searchInput = document.querySelector("input[name='character-id']")
+const deleteInput = document.querySelector("input[name='character-id-delete']")
+
+//Helper functions
+const createElm = (elm, className, textToUpdate) => {
+  const elem = document.createElement(elm);
+  elem.setAttribute("class", className)
+  elem.innerHTML = textToUpdate
+  return elem;
+}
+
+const drawCharacterCard = (character) => {
+  const cardsContainer = createElm("div", "character-info", "")
+  cardsContainer.appendChild(createElm("div", "name", `Character Name: ${character.name}`))
+  cardsContainer.appendChild(createElm("div", "occupation", `Character Ocupation: ${character.occupation}`))
+  cardsContainer.appendChild(createElm("div", "weapon", `Character weapon: ${character.weapon}`))
+  cardsContainer.appendChild(createElm("div", "cartoon", `Is a cartoon? ${character.cartoon}`))
+  mainCardCointainer.appendChild(cardsContainer)
+}
+
+const drawCharactersCard = (characters) => {
+  characters.forEach(character => {
+    drawCharacterCard(character)
+  })
+}
+
+const emptyCardContainer = () => mainCardCointainer.innerHTML = ""
+
+//Event listeners
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', () => {
+    charactersAPI.getFullList()
+      .then(characters => {
+        emptyCardContainer()
+        drawCharactersCard(characters)
+      })
+      .catch(err => console.log(err))
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', () => {
+    const id = document.querySelector(".operation input").value
+    charactersAPI.getOneRegister(id)
+      .then(character => {
+        emptyCardContainer()
+        drawCharacterCard(character)
+        searchInput.value = ""
+      })
+      .catch(err => {
+        emptyCardContainer()
+        console.log(err)
+      })
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', (e) => {
+    const id = document.querySelector(".operation.delete input").value
+    charactersAPI.deleteOneRegister(id)
+      .then(() => {
+        e.target.style.backgroundColor = "green"
+        deleteInput.value = ""
+      })
+      .catch(err => {
+        e.target.style.backgroundColor = "red"
+        console.log(err)
+      })
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('edit-character-form').addEventListener('submit', () => {
+    event.preventDefault()
+    const inputsValues = [...document.querySelectorAll("#edit-character-form input")].map(elm => elm.value)
+    const [id, name, occupation, weapon, cartoon] = inputsValues
+    charactersAPI.updateOneRegister(id, {
+        name,
+        occupation,
+        weapon,
+        cartoon
+      })
+      .then(() => {
+        editButton.style.backgroundColor = "green"
+        editForm.reset()
+      })
+      .catch(err => {
+        editButton.style.backgroundColor = "red"
+        console.log(err)
+      })
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    const inputsValues = [...document.querySelectorAll("#new-character-form input")].map(elm => elm.value)
+    const [name, occupation, weapon, cartoon] = inputsValues
+    charactersAPI.createOneRegister({
+        name,
+        occupation,
+        weapon,
+        cartoon
+      })
+      .then(() => {
+        createButton.style.backgroundColor = "green"
+        createForm.reset()
+      })
+      .catch(err => {
+        createButton.style.backgroundColor = "red"
+        console.log(err)
+      })
   });
 });
