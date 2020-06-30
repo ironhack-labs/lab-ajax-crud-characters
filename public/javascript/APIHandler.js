2aa84b166fa786d78fd3269392ed1c1061495408
Paola Martin <paolamartinme@gmail.com>
2020-06-30 21:06:48 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,47 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axios = axios.create({
+      baseUrl 
+    })
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.axios
+      .get('/characters')
+      .catch((error) => console.log(error));
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.axios
+      .get('/characters/${id}')
+      .then((res) => {
+        const charId = res.data;
+        console.log(`character ID:`, charId);
+      })
+      .catch((error) => console.log("Error character", error));
   }
 
-  createOneRegister () {
-
+  createOneRegister(newChar) {
+    this.axios
+      .post('/characters', newChar)
+      .then((res) => {
+        console.log("New character:", res.data);
+      })
+      .catch((error) => console.log("Error new character", error));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updateOneChar) {
+    this.axios
+      .put('/characters/${id}', updateOneChar)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    this.axios
+      .delete('/characters/${id}')
+      .then((res) => console.log("Character deleted", res.dara))
+      .catch((error) => console.log(error));
   }
 }
