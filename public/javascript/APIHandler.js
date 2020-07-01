35898a30506e7d6fd2a2a4eaea5c7808aff641e8
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-07-01 08:45:38 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -2,24 +2,45 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
-  getFullList () {
-
+  
+  getFullList() {
+    axios
+    .get(`${this.BASE_URL}/characters`)
+    .then(res => {
+      console.log('API Response', res)
+    })
+    .catch(error => console.log('Error geting character list', error))
   }
-
-  getOneRegister () {
-
+  
+  getOneRegister (id) {
+    axios
+    .get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
+      const singleCharacter = res.data
+      console.log('Character ID', singleCharacter)
+    })
+    .catch(error => console.log('Error gettingn single character', error))
   }
-
-  createOneRegister () {
-
+  
+  createOneRegister(obj) {
+    axios
+    .post(`${this.BASE_URL}/characters`, obj)
+    .then(res => console.log('New Character: ', res.data))
+    .catch(error => console.log('Error creating character', error))
   }
-
-  updateOneRegister () {
-
+  
+  updateOneRegister (id, update){
+    axios
+    .put(`${this.BASE_URL}/characters/${id}`, update)
+    .then(res => console.log(res.data))
+    .catch(error => console.log('Error editing character', error))
+    
   }
-
+  
   deleteOneRegister () {
-
+    axios
+    .delete(`${this.BASE_URL}/characters/${id}`)
+    .then(res => console.log("Character has been successfully deleted", res.data))
+    .catch(error => console.log('Error editing character', error))  
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,20 +3,24 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+    
   });
-
+  
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    
+    
+    
   });
-
+  
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    
   });
-
+  
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
   });
-
+  
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
   });
