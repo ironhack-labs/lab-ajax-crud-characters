fbbf4d6a4bcb50d753f75ff511c9dd6ac665d701
Setti <setti@leqoqo.com>
2020-07-08 23:08:08 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,25 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios.get(`${this.BASE_URL}` + "/characters");
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister(body) {
+    return axios.post(`${this.BASE_URL}/characters/`, body);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, body) {
+    return axios.patch(`${this.BASE_URL}/characters/${id}`, body);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete(`${this.BASE_URL}/characters/${id}`);
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,103 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+function eraseAll() {
+  document.querySelector(".characters-container").innerHTML = "";
+}
 
-  });
+// function eraseOne(id) {
+//   document.getElementById(`id-${id}`).remove();
+// }
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+function displayOneCharacter(res) {
+  document.querySelector(
+    ".characters-container"
+  ).innerHTML += `<div class="character-info" id="id-${res.id}">
+  <div class="id">Id: ${res.id}</div>
+    <div class="name">Name: ${res.name}</div>
+    <div class="occupation">Occupation: ${res.occupation}</div>
+    <div class="cartoon">Is cartoon?:${res.cartoon}</div>
+    <div class="weapon">Weapon:${res.weapon}</div></div>`;
+}
 
-  });
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      eraseAll();
+      charactersAPI
+        .getFullList()
+        .then((result) => {
+          const allCharacters = result.data;
+          allCharacters.forEach((e) => displayOneCharacter(e));
+        })
+        .catch((err) => console.log(err));
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      const id = event.target.previousElementSibling.value;
+      eraseAll();
+      charactersAPI
+        .getOneRegister(id)
+        .then((result) => displayOneCharacter(result.data))
+        .catch((err) => console.log(err));
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      const id = event.target.previousElementSibling.value;
+      charactersAPI.deleteOneRegister(id);
+      document.querySelector(".character-id-delete");
+    });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const id = document.getElementById("edit-id").value;
+      const name = document.getElementById("edit-name").value;
+      const occupation = document.getElementById("edit-occupation").value;
+      const weapon = document.getElementById("edit-weapon").value;
+      const cartoon = document.getElementById("edit-cartoon").checked
+        ? true
+        : false;
+      const body = {
+        name: name,
+        occupation: occupation,
+        weapon: weapon,
+        cartoon: cartoon,
+      };
+      charactersAPI.updateOneRegister(id, body).then((result) => {
+        displayOneCharacter(result);
+      });
+    });
 
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      alert("coucou Tonin");
+      const name = document.getElementById("new-name").value;
+      const occupation = document.getElementById("new-occupation").value;
+      const weapon = document.getElementById("new-weapon").value;
+      const cartoon = document.getElementById("new-cartoon").checked
+        ? true
+        : false;
+      const body = {
+        name: name,
+        occupation: occupation,
+        weapon: weapon,
+        cartoon: cartoon,
+      };
+      console.log(body);
+      charactersAPI
+        .createOneRegister(body)
+        .then((body) => {
+          displayOneCharacter(body.data);
+        })
+        .catch((err) => console.log(err));
+    });
 });
