38ee40f9484b07f6f7e108c6958ec9c4b937e2f3
Aude Richon <auderichon@gmail.com>
2020-07-08 23:32:28 +0200

    CRD

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,93 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000/characters");
+const charactersContainer = document.getElementById("container");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI
+        .getFullList()
+        .then((dbRes) => {
+          console.log(dbRes.data);
+          if (dbRes !== []) charactersContainer.innerHTML = "";
+          for (let i = 0; i < dbRes.data.length; i++) {
+            charactersContainer.innerHTML += `<div class="character-info">
+                <div class="name">Id: <span>${dbRes.data[i].id}</span></div>
+                <div class="name">Name: <span>${dbRes.data[i].name}</span></div>
+                <div class="occupation">Occupation: <span>${dbRes.data[i].occupation}</span></div>
+                <div class="cartoon">Cartoon: <span>${dbRes.data[i].cartoon}</span></div>
+                <div class="weapon">Weapon: <span>${dbRes.data[i].weapon}</span></div>
+                </div>`;
+          }
+        })
+        .catch((err) => console.log(err));
+    });
 });
+
+document
+  .getElementById("fetch-one")
+  .addEventListener("click", function (event) {
+    const userInput = document.getElementById("character-id").value;
+    console.log(userInput);
+    charactersAPI
+      .getOneRegister(userInput)
+      .then((dbRes) => {
+        charactersContainer.innerHTML = `<div class="character-info">
+                <div class="name">Id: <span>${dbRes.data.id}</span></div>
+                <div class="name">Name: <span>${dbRes.data.name}</span></div>
+                <div class="occupation">Occupation: <span>${dbRes.data.occupation}</span></div>
+                <div class="cartoon">Cartoon: <span>${dbRes.data.cartoon}</span></div>
+                <div class="weapon">Weapon: <span>${dbRes.data.weapon}</span></div>
+                </div>`;
+      })
+      .catch((err) => console.log(err));
+  });
+
+document
+  .getElementById("delete-one")
+  .addEventListener("click", function (event) {
+    let userInput = document.getElementById("character-id-delete").value;
+    if (userInput)
+      charactersAPI
+        .deleteOneRegister(userInput)
+        .then(() => {
+          document.getElementById("delete-one").classList.add("success");
+        })
+        .catch((err) => {
+          document.getElementById("delete-one").classList.add("problem");
+          console.log(err);
+        });
+  });
+
+// document
+//   .getElementById("edit-character-form")
+//   .addEventListener("submit", function (event) {});
+
+document
+  .getElementById("new-character-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const occupation = document.getElementById("occupation").value;
+    const weapon = document.getElementById("weapon").value;
+    let cartoon;
+    document.getElementById("cartoon") ? (cartoon = true) : (cartoon = false);
+
+    const newCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon,
+    };
+
+    if (newCharacter.name)
+      charactersAPI
+        .createOneRegister(newCharacter)
+        .then(() => {
+          document.getElementById("send-create").classList.add("success");
+        })
+        .catch((err) => {
+          document.getElementById("send-create").classList.add("problem");
+          console.log(err);
+        });
+  });
