ca7aa59f7b7a15343db7d480fe3d9efd36ef2e7a
Andres Martinez <andresmartgonz@gmail.com>
2020-07-18 14:10:15 +0200

    iteration3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,76 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.BASE_URL = baseUrl
   }
 
-  getFullList () {
+  getFullList() {
+    return axios
+      .get(this.BASE_URL + '/characters')
+      .then((list) => {
+        list.data
+        console.log(list.data)
 
+        let str = ''
+        list.data.forEach((el) => {
+          str += ` 
+          <div class="character-info">
+            <div class="id"><strong>Id:</strong> ${el.id}</div>
+            <div class="name"><strong>Name:</strong> ${el.name}</div>
+            <div class="occupation"><strong>Ocuppation:</strong> ${el.occupation}</div>
+            <div class="cartoon"><strong>Is a cartoon?:</strong> ${el.cartoon}</div>
+            <div class="weapon"><strong>Weapon:</strong> ${el.weapon}</div>
+          </div>`
+        })
+
+        const charactersContainer = document.querySelector(
+          '.characters-container'
+        )
+
+        charactersContainer.innerHTML = str
+      })
+      .catch((error) => console.log('getFullList error: ', error))
   }
 
-  getOneRegister () {
+  getOneRegister() {
+    const id = document.querySelector('[name="character-id"]')
+    return axios
+      .get(this.BASE_URL + `/characters/${id.value}`)
+      .then((list) => {
+        list.data
+        console.log(list.data)
 
+        let str = ''
+        str += ` 
+          <div class="character-info">
+            <div class="id"><strong>Id:</strong> ${list.data.id}</div>
+            <div class="name"><strong>Name:</strong> ${list.data.name}</div>
+            <div class="occupation"><strong>Ocuppation:</strong> ${list.data.occupation}</div>
+            <div class="cartoon"><strong>Is a cartoon?:</strong> ${list.data.cartoon}</div>
+            <div class="weapon"><strong>Weapon:</strong> ${list.data.weapon}</div>
+          </div>`
+
+        const charactersContainer = document.querySelector(
+          '.characters-container'
+        )
+        charactersContainer.innerHTML = ''
+        charactersContainer.innerHTML = str
+        id.value = ''
+      })
+      .catch((error) => console.log('getFullList error: ', error))
   }
 
-  createOneRegister () {
+  createOneRegister() {}
 
-  }
+  updateOneRegister() {}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    const id = document.querySelector('[name="character-id-delete"]')
+    return axios
+      .delete(this.BASE_URL + `/characters/${id.value}`)
+      .then((list) => {
+        this.getFullList()
+        id.value = ''
+      })
+      .catch((error) => console.log('getFullList error: ', error))
   }
 }
