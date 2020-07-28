ce5b6a4213bed0df4ee9b289ca083c76ab8cde34
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-07-28 16:53:47 +0200

    Iteracion 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,49 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+
+    this.axiosApi = axios.create({
+      baseURL: this.BASE_URL
+    })
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.axiosApi
+      .get('/characters')
+      .then(responseFromApi => {
+        console.log('Response from API is:', responseFromApi.data)
+      })
+      .catch(err => console.log('Error while getting full list', err))
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return this.axiosApi
+      .get(`/characters/${id}`)
+      .then(responseFromApi => {
+        console.log('The single character is:', responseFromApi)
+      })
+      .catch(err => console.log('Error while getting one register', err))
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacter) {
+    return this.axiosApi
+      .post('/characters', newCharacter)
+      .then(responseFromApi => {
+        console.log('New character created', responseFromApi.data)
+      })
+      .catch(err => console.log('Error while creating the character', err))
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updatedCharacterInfo) {
+    return this.axiosApi
+      .patch(`/characters/${id}`, updatedCharacterInfo)
+      .catch(err => console.log('Error while editing the character', err))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return this.axiosApi
+      .delete(`/characters/${id}`)
+      .then(responseFromApi => responseFromApi.data)
+      .catch(err => console.log('Error while deleting the character'))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,36 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister();
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(4);
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const updatedCharacterInfo = {
+      name: 'Batman',
+      occupation: "test",
+      weapon: "test",
+      cartoon: true
+    };
 
+    charactersAPI.updateOneRegister(1, updatedCharacterInfo)
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    const newCharacter = {
+      name: "Spider-man",
+      occupation: "Making webs",
+      weapon: "webs",
+      cartoon: true
+    };
 
+    charactersAPI.createOneRegister(newCharacter);
   });
 });
