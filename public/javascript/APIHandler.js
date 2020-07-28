ee1df0abed4970061da15873d73c391a8ddfa6e8
Jean-Baptiste Castillo <castillo.jbaptiste@gmail.com>
2020-07-28 17:58:59 +0200

    Iteracion 3 hasta Delete One Character

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,26 +3,18 @@ class APIHandler {
     this.BASE_URL = baseUrl;
 
     this.axiosApi = axios.create({
-      baseURL: this.BASE_URL
+      baseURL: `http://localhost:8000`
     })
   }
 
   getFullList() {
     return this.axiosApi
       .get('/characters')
-      .then(responseFromApi => {
-        console.log('Response from API is:', responseFromApi.data)
-      })
-      .catch(err => console.log('Error while getting full list', err))
   }
 
   getOneRegister(id) {
     return this.axiosApi
       .get(`/characters/${id}`)
-      .then(responseFromApi => {
-        console.log('The single character is:', responseFromApi)
-      })
-      .catch(err => console.log('Error while getting one register', err))
   }
 
   createOneRegister(newCharacter) {
@@ -43,7 +35,5 @@ class APIHandler {
   deleteOneRegister(id) {
     return this.axiosApi
       .delete(`/characters/${id}`)
-      .then(responseFromApi => responseFromApi.data)
-      .catch(err => console.log('Error while deleting the character'))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,29 +1,64 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
+
+  //FETCH ALL CHARACTERS
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-    charactersAPI.getFullList();
+
+    charactersAPI.getFullList()
+      .then((reponses) => {
+        const charactersContainer = document.getElementsByClassName('characters-container')[0];
+        charactersContainer.innerHTML = ''; //Vidage de la div
+
+        reponses.data.forEach(reponse => {
+          const divElts = document.createElement('div');
+          divElts.innerHTML = `<div class="character-info"><div class="name">name: ${reponse.name}</div><div class="occupation">occupation: ${reponse.occupation}</div><div class="cartoon">cartoon: ${reponse.cartoon}</div><div class="weapon">weapon: ${reponse.weapon}</div></div>`;
+
+          charactersContainer.appendChild(divElts);
+        })
+
+      })
+      .catch(err => console.log('Error while fetching', err))
   });
 
+  //FETCH ONE CHARACTER
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-    charactersAPI.getOneRegister();
+    const fetchOneId = document.getElementsByName('character-id')[0].value;
+    const charactersContainer = document.getElementsByClassName('characters-container')[0];
+
+    charactersAPI.getOneRegister(fetchOneId)
+      .then((reponse) => {
+        charactersContainer.innerHTML = ''; //Vidage de la div
+        const answerId = reponse.data;
+        const divElt = document.createElement('div');
+        divElt.innerHTML = `<div class="character-info"><div class="name">name: ${answerId.name}</div><div class="occupation">occupation: ${answerId.occupation}</div><div class="cartoon">cartoon: ${answerId.cartoon}</div><div class="weapon">weapon: ${answerId.weapon}</div></div>`
+
+        charactersContainer.appendChild(divElt);
+      })
   });
 
+  //DELETE ONE CHARACTER
   document.getElementById('delete-one').addEventListener('click', function (event) {
-    charactersAPI.deleteOneRegister(4);
+    const deleteOneId = document.getElementsByName('character-id-delete')[0].value;
+    const buttonDeleteOne = document.getElementById('delete-one');
+    charactersAPI.deleteOneRegister(deleteOneId);
+
+    buttonDeleteOne.style.backgroundColor = 'green';
   });
 
+  //EDIT ONE CHARACTER
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
     const updatedCharacterInfo = {
       name: 'Batman',
-      occupation: "test",
-      weapon: "test",
+      occupation: "Saving the world",
+      weapon: "His brain (and money...)",
       cartoon: true
     };
 
     charactersAPI.updateOneRegister(1, updatedCharacterInfo)
   });
 
+  //CREATE ONE CHARACTER
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
     const newCharacter = {
       name: "Spider-man",
