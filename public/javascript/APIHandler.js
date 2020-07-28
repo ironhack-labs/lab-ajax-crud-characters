b21703f758c34985890998b32936c2ed4eb176e8
Alejandro Abad <alejandro.abad.noriega@gmail.com>
2020-07-28 16:40:54 +0200

    sin crear cada character

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,54 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList () {
+    try {
+      const data =  await axios.get(`${this.BASE_URL}/characters`)
+      console.log(data.data);
+      return data.data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  
+  async getOneRegister (id) {
+    try {
+      const data = await axios.get(`${this.BASE_URL}/characters/${id}`)
+      console.log('GetOneRegiser ', data.data)
+      if(data.data.id){
+        console.log('El data es: ', data.data);
+        console.log('El data id es: ', data.data.id);
+        return data.data.id;
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  getOneRegister () {
-
+  async createOneRegister (infoPost) {
+    try {
+      await axios.post(`${this.BASE_URL}/characters/${infoPost}`);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  createOneRegister () {
-
+  async updateOneRegister (infoUpdate) {
+    try {
+      await axios.patch(`${this.BASE_URL}/characters/${infoUpdate.id}`, infoUpdate);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  async deleteOneRegister (id) {
+    try {
+      await axios.delete(`${this.BASE_URL}/characters/${id}`);
+    } catch (error) {
+      console.log(error) 
+    }
   }
 }
+
+const charactersAPI2 = new APIHandler('http://localhost:8000');
+charactersAPI2.getFullList();
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,46 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+
+
+
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
-
+  
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const id = document.getElementsByName('character-id')[0];
+    charactersAPI.getOneRegister(id.value)
+    
   });
-
+  
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    //porque son P**O submits!
+    event.preventDefault();
+    const deleteId = document.getElementsByName('character-id-delete')[0];
+    charactersAPI.deleteOneRegister(deleteId.value);
   });
-
+  
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const editCharacter = {
+      id: event.target[0].value,
+      name: event.target[1].value,
+      occupation: event.target[2].value,
+      weapon: event.target[3].value,
+      cartoon: event.target[4].checked
+    }
+    charactersAPI.updateOneRegister(editCharacter);
   });
-
+  
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const newCharacter = {
+      name: event.target[0].value,
+      occupation: event.target[1].value,
+      weapon: event.target[2].value,
+      cartoon: event.target[3].checked,
+    }
+    charactersAPI.createOneRegister(newCharacter);
   });
 });
