cfca155cfab9a7d224e6a98645923d5446e0a6b5
krankie <foresvives10@gmail.com>
2020-07-29 08:56:46 +0200

    'lab'

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,36 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axiosApi = axios.create({
+      baseURL: `http://localhost:8000`
+    })
+    
   }
+
 
   getFullList () {
-
+    return this.axiosApi
+    .get('/characters')
+  }
+  
+  getOneRegister (id) {
+  axios.get(`${this.BASE_URL}/characters/${id}`)
+  console.log(id)
+    
   }
 
-  getOneRegister () {
-
+  createOneRegister (newCharacter) {
+    axios.post(`${this.BASE_URL}/characters`, newCharacter)
+   
   }
 
-  createOneRegister () {
-
+  updateOneRegister (id, updateChar) {
+    console.log(updateChar)
+    axios.patch(`${this.BASE_URL}/characters/${id}`,updateChar)
+    
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,59 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+    .then((responses)=>{
+      const box = document.getElementsByClassName('characters-container')[0];
+      box.innerHTML = '';
 
+      responses.data.forEach(response =>{
+        const elem = document.createElement('div');
+        elem.innerHTML = `<div class = 'character-info'>
+                          <div class = 'name'>Name: ${response.name}</div>
+                          <div class = 'occupation'>Ocupattion: ${response.occupation}</div>
+                          <div class = 'cartoon'>cartoon: ${response.cartoon}</div>
+                          <div class = 'weapon'>weapon: ${response.weapon}</div>
+                          </div>`
+
+                          box.appendChild(elem)
+      })
+    })
+    .catch(error=> console.log('error',error))
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const id = document.getElementsByName('character-id')[0];
+    charactersAPI.getOneRegister(id.value)
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    const idD = document.getElementsByName('character-id-delete')[0];
+    charactersAPI.deleteOneRegister(idD.value)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
 
+    const id = document.getElementsByName('chr-id')[0].value
+    const updateChar = {
+      name:event.target.name.value,
+      occupation: event.target.occupation.value,
+      weapon:event.target.weapon.value,
+      cartoon:event.target.cartoon.checked
+    }
+    
+    charactersAPI.updateOneRegister(id, updateChar)
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const newChar = {
+      name:event.target.name.value,
+      occupation: event.target.occupation.value,
+      weapon:event.target.weapon.value,
+      cartoon:event.target.cartoon.checked
 
+    }
+    charactersAPI.createOneRegister(newChar)
   });
 });
