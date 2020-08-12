6b6d0b92cf2f69e0c30d1181adcb185c5043176f
Arianna Valle <ariannamarbella@gmail.com>
2020-08-12 16:30:03 -0400

    Update APIHandler.js

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,57 +4,27 @@ class APIHandler {
 	}
 
 	getFullList() {
-		axios
+		return axios
 			.get(`${this.BASE_URL}/characters`)
-			.then((response) => {
-				console.log("Response from the API: ", response.data);
-			})
-			.catch((error) => {
-				console.log("Unable to retrieve characters.");
-			});
 	}
 
 	getOneRegister(characterId) {
-		axios
+		return axios
 			.get(`${this.BASE_URL}/characters/${characterId}`)
-			.then((response) => {
-				console.log("Response from the API: ", response.data);
-			})
-			.catch((error) => {
-				console.log(`Unable to retrieve character with ID ${characterId}.`);
-			});
 	}
 
 	createOneRegister() {
 		axios
 			.post(`${this.BASE_URL}/characters`, newCharacter)
-			.then((response) => {
-				this.getFullList();
-			})
-			.catch((error) => {
-				console.log("Unable to create character.");
-			});
 	}
 
 	updateOneRegister(characterId, editedCharacter) {
 		axios
 			.put(`${this.BASE_URL}/characters/${characterId}`, editedCharacter)
-			.then((response) => {
-				console.log("Response from the API: ", response.data);
-			})
-			.catch((error) => {
-				console.log(`Unable to update character with ID ${characterId}.`);
-			});
 	}
 
 	deleteOneRegister(characterId) {
-		axios
-			.delete(`${this.BASE_URL}/character/${characterId}`)
-			.then((response) => {
-				console.log("Response from the API: ", response.data);
-			})
-			.catch((error) => {
-				console.log(`Unable to delete character with ID ${characterId}.`);
-			});
+		return axios
+			.delete(`${this.BASE_URL}/characters/${characterId}`)
 	}
 }
