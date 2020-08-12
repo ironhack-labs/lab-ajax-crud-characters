da96d57acfce70d50dd63c204a51208cfcd9f4ff
Arianna Valle <ariannamarbella@gmail.com>
2020-08-11 22:09:15 -0400

    Complete iteration 2: The APIHandler.js file

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+	constructor(baseUrl) {
+		this.BASE_URL = baseUrl;
+	}
 
-  getFullList () {
+	getFullList() {
+		axios
+			.get(`${this.BASE_URL}/characters`)
+			.then((response) => {
+				console.log("Response from the API: ", response.data);
+			})
+			.catch((error) => {
+				console.log("Unable to retrieve characters.");
+			});
+	}
 
-  }
+	getOneRegister(characterId) {
+		axios
+			.get(`${this.BASE_URL}/characters/${characterId}`)
+			.then((response) => {
+				console.log("Response from the API: ", response.data);
+			})
+			.catch((error) => {
+				console.log(`Unable to retrieve character with ID ${characterId}.`);
+			});
+	}
 
-  getOneRegister () {
+	createOneRegister() {
+		axios
+			.post(`${this.BASE_URL}/characters`, newCharacter)
+			.then((response) => {
+				this.getFullList();
+			})
+			.catch((error) => {
+				console.log("Unable to create character.");
+			});
+	}
 
-  }
+	updateOneRegister(characterId, editedCharacter) {
+		axios
+			.put(`${this.BASE_URL}/characters/${characterId}`, editedCharacter)
+			.then((response) => {
+				console.log("Response from the API: ", response.data);
+			})
+			.catch((error) => {
+				console.log(`Unable to update character with ID ${characterId}.`);
+			});
+	}
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+	deleteOneRegister(characterId) {
+		axios
+			.delete(`${this.BASE_URL}/character/${characterId}`)
+			.then((response) => {
+				console.log("Response from the API: ", response.data);
+			})
+			.catch((error) => {
+				console.log(`Unable to delete character with ID ${characterId}.`);
+			});
+	}
 }
