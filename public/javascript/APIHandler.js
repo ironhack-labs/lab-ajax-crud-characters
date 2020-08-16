7a8527b45da90faf873f0a52198e01cb13936b97
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-16 18:45:50 -0400

    working on iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -5,9 +5,53 @@ class APIHandler {
 
   getFullList () {
 
+    axios.get(this.BASE_URL + "/characters")
+      .then(reponseFromApi => {
+
+        let charactersContainer = document.querySelector(".characters-container").innerHTML;
+        charactersContainer = "";
+
+        reponseFromApi.data.forEach(ele => {
+
+          charactersContainer += `<div class="character-info">
+                                    <div class="id">Id: ${ele.id}</div>
+                                    <div class="name">Name: ${ele.name}</div>
+                                    <div class="occupation">Occupation: ${ele.occupation}</div>
+                                    <div class="cartoon">Is a Cartoon?: ${ele.cartoon}</div>
+                                    <div class="weapon">Weapon: ${ele.weapon}</div>
+                                  </div>`;
+
+        })
+
+        // console.log(charactersContainer)
+        console.log(reponseFromApi.data);
+
+      })
+      .catch(error => console.log(error))
+
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+
+    axios.get(this.BASE_URL + "/characters/" + id)
+      .then(reponseFromApi => {
+
+        // console.log(reponseFromApi.data.name);
+
+        const data = reponseFromApi.data;
+
+        let charactersContainer = document.querySelector('.characters-container').innerHTML;
+        charactersContainer = "";
+
+        charactersContainer = `<div class="character-info">
+                                  <div class="id">Id: ${data.id}</div>
+                                  <div class="name">Name: ${data.name}</div>
+                                  <div class="occupation">Occupation: ${data.occupation}</div>
+                                  <div class="cartoon">Is a Cartoon?: ${data.cartoon}</div>
+                                  <div class="weapon">Weapon: ${data.weapon}</div>
+                                </div>`;
+      })
+      .catch(error => console.log(error));
 
   }
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,29 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', () => {
+
+    charactersAPI.getFullList();
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', (event) => {
+
+    const characterId = Number(event.path[1].children[1].value);
+
+    charactersAPI.getOneRegister(characterId);
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', (event) => {
 
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', (event) => {
 
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', (event) => {
 
   });
 });
