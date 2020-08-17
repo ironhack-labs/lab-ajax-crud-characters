1d0aae790a0545bed15b1c597355bc6d01306a5d
Alejandro Garcia <agarc371@gmail.com>
2020-08-17 19:16:08 -0400

    initial commit, complete iteration 1 and add first few GET routes for iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,35 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    axios
+      .get(`${BaseUrl}/characters`)
+      .then(response => {
+        console.log('The response from the API is: ', response.data);
+      })
+      .catch(err => console.log('error while getting the data: ', err));
+  }
+
+  getOneRegister() {
+    axios
+      .get(`${baseUrl}/:id`)
+      .then(response => {
+        console.log('The response from the API is: ', response.data);
+      })
+      .catch(err => console.log('error while getting the character: ', err));
+  }
+
+  createOneRegister() {
 
   }
 
-  getOneRegister () {
+  updateOneRegister() {
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  deleteOneRegister() {
 
   }
 }
