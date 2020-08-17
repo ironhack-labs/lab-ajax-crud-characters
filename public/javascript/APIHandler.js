d4e4218bb6abaf01b74ec10b5151e5fd4466f300
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-16 20:11:56 -0400

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,66 +4,76 @@ class APIHandler {
   }
 
   getFullList () {
-
     axios.get(this.BASE_URL + "/characters")
       .then(reponseFromApi => {
 
-        let charactersContainer = document.querySelector(".characters-container").innerHTML;
-        charactersContainer = "";
+        let charactersContainer = document.querySelector(".characters-container");
+        charactersContainer.innerHTML = "";
 
         reponseFromApi.data.forEach(ele => {
-
-          charactersContainer += `<div class="character-info">
-                                    <div class="id">Id: ${ele.id}</div>
-                                    <div class="name">Name: ${ele.name}</div>
-                                    <div class="occupation">Occupation: ${ele.occupation}</div>
-                                    <div class="cartoon">Is a Cartoon?: ${ele.cartoon}</div>
-                                    <div class="weapon">Weapon: ${ele.weapon}</div>
-                                  </div>`;
-
-        })
-
-        // console.log(charactersContainer)
-        console.log(reponseFromApi.data);
-
+          const {id, name, occupation, cartoon, weapon} = ele;
+          charactersContainer.innerHTML += `<div class="character-info">
+                                              <div class="id">Id: ${id}</div>
+                                              <div class="name">Name: ${name}</div>
+                                              <div class="occupation">Occupation: ${occupation}</div>
+                                              <div class="cartoon">Is a Cartoon?: ${cartoon}</div>
+                                              <div class="weapon">Weapon: ${weapon}</div>
+                                            </div>`;
+        });
+        // console.log(reponseFromApi.data);
       })
-      .catch(error => console.log(error))
-
+      .catch(error => console.log(error));
   }
 
   getOneRegister (id) {
-
     axios.get(this.BASE_URL + "/characters/" + id)
       .then(reponseFromApi => {
 
         // console.log(reponseFromApi.data.name);
 
-        const data = reponseFromApi.data;
+        const {id, name, occupation, cartoon, weapon} = reponseFromApi.data;
 
-        let charactersContainer = document.querySelector('.characters-container').innerHTML;
-        charactersContainer = "";
+        let charactersContainer = document.querySelector('.characters-container');
+        charactersContainer.innerHTML = "";
 
-        charactersContainer = `<div class="character-info">
-                                  <div class="id">Id: ${data.id}</div>
-                                  <div class="name">Name: ${data.name}</div>
-                                  <div class="occupation">Occupation: ${data.occupation}</div>
-                                  <div class="cartoon">Is a Cartoon?: ${data.cartoon}</div>
-                                  <div class="weapon">Weapon: ${data.weapon}</div>
-                                </div>`;
+        charactersContainer.innerHTML = `<div class="character-info">
+                                          <div class="id">Id: ${id}</div>
+                                          <div class="name">Name: ${name}</div>
+                                          <div class="occupation">Occupation: ${occupation}</div>
+                                          <div class="cartoon">Is a Cartoon?: ${cartoon}</div>
+                                          <div class="weapon">Weapon: ${weapon}</div>
+                                        </div>`;
+                                
+        console.log(charactersContainer);
       })
       .catch(error => console.log(error));
 
   }
 
-  createOneRegister () {
-
+  createOneRegister (newCharacter) {
+    axios
+      .post(this.BASE_URL + "/characters/", newCharacter)
+      .then(() => this.getFullList())
+      .catch(error => console.log(error));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, updatedCharacter) {
+    axios
+      .put(this.BASE_URL + "/characters/" + id, updatedCharacter)
+      .then(() => {
+        this.getOneRegister(id);
+      })
+      .catch(error => console.log(error));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios
+      .delete(this.BASE_URL + "/characters/" + id)
+      .then(() => {
+        alert("character was deleted");
+        this.getFullList();
+      })
+      .catch(error => console.log(error));
   }
+
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -10,20 +10,53 @@ window.addEventListener('load', () => {
   document.getElementById('fetch-one').addEventListener('click', (event) => {
 
     const characterId = Number(event.path[1].children[1].value);
-
     charactersAPI.getOneRegister(characterId);
 
   });
 
   document.getElementById('delete-one').addEventListener('click', (event) => {
 
+    const characterId = Number(event.path[1].children[1].value);
+    charactersAPI.deleteOneRegister(characterId);
+
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const id = document.getElementById('idEdit').value;
+    const name = document.getElementById('nameEdit').value;
+    const occupation = document.getElementById('occupationEdit').value;
+    const weapon = document.getElementById('weaponEdit').value;
+    const cartoon = document.getElementById('cartoonEdit').value;
+
+    const updatedCharacter = {
+      id,
+      name,
+      occupation,
+      weapon,
+      cartoon
+    };
+
+    charactersAPI.updateOneRegister(id, updatedCharacter);
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', (event) => {
+    event.preventDefault();
 
+    const name = document.getElementById('nameInput').value;
+    const occupation = document.getElementById('occupationInput').value;
+    const weapon = document.getElementById('weaponInput').value;
+    const cartoon = document.getElementById('cartoonInput').value;
+
+    const newCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    };
+
+    charactersAPI.createOneRegister(newCharacter);
   });
 });
