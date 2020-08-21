4422936511a0291e36a10c8086561d01495c92f7
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-08-21 10:55:10 +0200

    fetchAll, fetchOne comepleted

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,82 @@
 class APIHandler {
-  constructor (baseUrl) {
+  /**
+   * Constructor
+   * @param {*} baseUrl
+   */
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  /**
+   * Requests the URL to fetch all the characters
+   */
+  getFullList() {
+    const url = this.BASE_URL + `/characters`;
+    console.log(url);
+    axios
+      .get(url)
+      .then((response) => {
+        this.response = response.data;
+        this.updateFullList();
+      })
+      .catch((error) => (this.error = error));
   }
 
-  getOneRegister () {
-
+  /**
+   *  Updates the fetched characters  on the forntend
+   */
+  updateFullList() {
+    let str = "";
+    this.response.forEach((element) => {
+      str += this.formCharacterElement(element);
+    });
+    document.querySelector(".characters-container").innerHTML = str;
   }
 
-  createOneRegister () {
-
+  /**
+   *  Returns the character information in HTML form
+   * @param {*} element
+   */
+  formCharacterElement(element) {
+    return `<div class="character-info"> 
+                <div class="name"> Id: <span> ${element.id} <span> </div>\
+                <div class="name"> Name: <span> ${element.name} <span> </div>
+                <div class="name"> Occupation: <span> ${element.occupation} <span> </div>
+                <div class="name"> Is a Cartoon?: <span> ${element.cartoon} <span> </div>
+                <div class="name"> Weapon: <span> ${element.weapon} <span> </div>                
+              </div>`;
   }
 
-  updateOneRegister () {
-
+  /**
+   *  Requests the url to get one characer information
+   */
+  getOneRegister() {
+    const id = document.getElementsByName("character-id")[0].value;
+    const url = this.BASE_URL + `/characters/${id}`;
+    // console.log(url);
+    axios
+      .get(url)
+      .then((response) => {
+        this.response = response.data;
+        this.updateOneCharacterInfo();
+      })
+      .catch((error) => (this.error = error));
   }
 
-  deleteOneRegister () {
-
+  /**
+   *  Updates the fetched characters  on the forntend
+   */
+  updateOneCharacterInfo() {
+    document.querySelector(
+      ".characters-container"
+    ).innerHTML = this.formCharacterElement(this.response);
   }
+
+  createOneRegister() {}
+
+  updateOneRegister() {}
+
+  deleteOneRegister() {}
 }
+
+//npm install -g json-server
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,36 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  /**
+   * When "FETCH ALL" button clicked : Get all the charactrs information
+   */
 
-  });
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      charactersAPI.getFullList();
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  /**
+   * When "FETCH ONE" button clicked : Get all the charactrs information
+   */
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      charactersAPI.getOneRegister();
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
 });
