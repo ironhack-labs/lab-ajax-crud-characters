984cd96c779e353c4c0995dec67a52b8edf410b6
Sunitha Mannam <mannam.sunitha@gmail.com>
2020-08-21 18:05:27 +0200

    all iterations completed

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -72,11 +72,122 @@ class APIHandler {
     ).innerHTML = this.formCharacterElement(this.response);
   }
 
-  createOneRegister() {}
+  createOneRegister() {
+    const url = this.BASE_URL + `/characters`;
+    const formElements = document.getElementById("new-character-form").elements;
+    console.log(formElements[3]);
+    const newCharacterInfo = {
+      name: formElements[0].value,
+      occupation: formElements[1].value,
+      weapon: formElements[2].value,
+      cartoon: formElements[3].checked,
+    };
+    console.log(newCharacterInfo);
+    axios
+      .post(url, newCharacterInfo)
+      .then((response) => {
+        const { name, id } = response.data;
+        document.getElementById("send-data").className = "btn-del-success";
+        alert(`The charcter with ID: ${id} is created successfully`);
+        document.getElementById("new-character-form").reset();
+      })
+      .catch((err) => {
+        document.getElementById("send-data").className = "btn-del-error";
+        alert(`Error in creating new  character Information`);
+        console.log(`Error while creating a new character: ${err}`);
+      });
+  }
 
-  updateOneRegister() {}
+  /**
+   *  Fetch the character information basedon the ID entered
+   */
+  fetchCharacterInfo() {
+    event.preventDefault();
+    const id = document.querySelector("#edit-character-form input[name=chr-id]")
+      .value;
+    const url = this.BASE_URL + `/characters/${id}`;
+    axios
+      .get(url)
+      .then((response) => {
+        this.response = response.data;
 
-  deleteOneRegister() {}
+        document.querySelector(
+          "#edit-character-form input[name=chr-id]"
+        ).value = this.response.id;
+        document.querySelector(
+          "#edit-character-form input[name=name]"
+        ).value = this.response.name;
+        document.querySelector(
+          "#edit-character-form input[name=occupation]"
+        ).value = this.response.occupation;
+        document.querySelector(
+          "#edit-character-form input[name=weapon]"
+        ).value = this.response.weapon;
+        document.querySelector(
+          "#edit-character-form input[name=cartoon]"
+        ).checked = this.response.cartoon;
+
+        document.querySelector(
+          "#edit-character-form input[name=chr-id]"
+        ).disabled = true;
+      })
+      .catch((error) => (this.error = error));
+  }
+
+  /**
+   *  Update the character information
+   */
+  updateOneRegister() {
+    const updatedCharacter = {
+      name: document.querySelector("#edit-character-form input[name=name]")
+        .value,
+      occupation: document.querySelector(
+        "#edit-character-form input[name=occupation]"
+      ).value,
+      weapon: document.querySelector("#edit-character-form input[name=weapon]")
+        .value,
+      cartoon: document.querySelector(
+        "#edit-character-form input[name=cartoon]"
+      ).checked,
+    };
+    const id = document.querySelector("#edit-character-form input[name=chr-id]")
+      .value;
+    const url = this.BASE_URL + `/characters/${id}`;
+    // alert(url);
+    axios
+      .put(url, updatedCharacter)
+      .then((response) => {
+        console.log(response);
+        document.getElementById("update-data").className = "btn-del-success";
+        alert(`The charcter with ID: ${id} is updated successfully`);
+        document.getElementById("edit-character-form ").reset();
+      })
+      .catch((error) => {
+        document.getElementById("update-data").className = "btn-del-error";
+        alert(`Error while updating the character information : ${error}`);
+      });
+  }
+
+  /**
+   *  Requests the url to delete one characer information
+   */
+  deleteOneRegister() {
+    const id = document.getElementsByName("character-id-delete")[0].value;
+    const url = this.BASE_URL + `/characters/${id}`;
+    console.log(url);
+    axios
+      .delete(url)
+      .then((response) => {
+        this.response = response.data;
+        document.getElementById("delete-one").className = "btn-del-success";
+        alert(`The charcter with ID: ${id} is deleted successfully`);
+      })
+      .catch((error) => {
+        this.error = error;
+        document.getElementById("delete-one").className = "btn-del-error";
+        alert(`Error in deleting the charcter with ID: ${id}`);
+      });
+  }
 }
 
 //npm install -g json-server
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
   /**
    * When "FETCH ALL" button clicked : Get all the charactrs information
    */
-
   document
     .getElementById("fetch-all")
     .addEventListener("click", function (event) {
@@ -13,7 +12,7 @@ window.addEventListener("load", () => {
     });
 
   /**
-   * When "FETCH ONE" button clicked : Get all the charactrs information
+   * When "FETCH ONE" button clicked : Get the character with the ID specified
    */
   document
     .getElementById("fetch-one")
@@ -22,15 +21,42 @@ window.addEventListener("load", () => {
       charactersAPI.getOneRegister();
     });
 
+  /**
+   * When "DELETE ONE" button clicked : Delete the character
+   */
   document
     .getElementById("delete-one")
-    .addEventListener("click", function (event) {});
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      charactersAPI.deleteOneRegister();
+    });
 
+  /**
+   * Fetch the character information when the "Fetch character" is clicked
+   */
+  document
+    .getElementById("fetch-data")
+    .addEventListener("click", function (event) {
+      charactersAPI.fetchCharacterInfo();
+    });
+
+  /**
+   * When "Update " button clicked : Update the  character
+   */
   document
     .getElementById("edit-character-form")
-    .addEventListener("submit", function (event) {});
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      charactersAPI.updateOneRegister();
+    });
 
+  /**
+   * When "CREATE ONE" button clicked : Create a new character
+   */
   document
     .getElementById("new-character-form")
-    .addEventListener("submit", function (event) {});
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      charactersAPI.createOneRegister();
+    });
 });
