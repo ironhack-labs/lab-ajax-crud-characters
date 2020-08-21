ea54481eafb44358c577b182cb4e5613f53e231b
Michel Ribbens <michelribbens@gmail.com>
2020-08-21 23:24:44 +0100

    iteration 1&2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,46 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios
+			.get(`${this.BASE_URL}/characters`)
+      .then(response => console.log('Response from API is:', response.data))
+      .catch(err => console.log(`Error while getting the list of characters: ${err}`));
   }
 
-  getOneRegister () {
-
+  getOneRegister(charId) {
+		axios
+      .get(`${this.BASE_URL}/characters/${charId}`)
+      .then(response => console.log('Response from API is:', response.data))
+      .catch(err => console.log(`Error while getting character: ${err}`));
   }
 
-  createOneRegister () {
+  createOneRegister(newChar) {
+		axios
+			.post(`${this.BASE_URL}/characters`, newChar)
+			.then(response => {
+        console.log('Created a new character', response.data)
+        this.getFullList();
+      })
+      .catch(err => console.log(`Error while getting character: ${err}`));
+	}
 
-  }
+	updateOneRegister(charId, editChar) {
+		axios
+			.put(`${this.BASE_URL}/characters/${charId}`, editChar)
+			.then(response => {
+        console.log("Response from the API: ", response.data)
+        this.getFullList();
+      })
+			.catch(err => console.log(`Error while updating a character: ${err}`));
+	}
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+	deleteOneRegister(charId) {
+		axios
+      .delete(`${this.BASE_URL}/characters/${charId}`)
+      .then(response => {
+        console.log('A character has been deleted', response.data)
+        this.getFullList();
+      })
+      .catch(err => console.log(`Error while saving a new character: ${err}`));
+	}
 }
