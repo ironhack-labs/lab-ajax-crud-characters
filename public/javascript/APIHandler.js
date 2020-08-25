ac70746df92c29097ca73d804db7ca80d05aec94
Lynn W-B <l.welshboucard@yahoo.co.uk>
2020-08-25 16:37:45 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,60 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL)
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("Unable to access data from DB");
+      });
   }
-
-  getOneRegister () {
-
+​  getOneRegister(chrId) {
+    axios
+      .get((this.BASE_URL += `/${chrId}`))
+      .then((response) => {
+        const data = response.data;
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(`Unable to retrieve character with id ${chrId}`);
+      });
   }
-
-  createOneRegister () {
-
+  createOneRegister() {
+    axios
+      .post(this.BASE_URL, newCharacterInfo)
+      .then((response) => {
+        this.getFullList();
+        document.getElementById("new-character-form").reset();
+      })
+      .catch((error) => console.log(`Error while saving a new character`));
   }
-
-  updateOneRegister () {
-
+  updateOneRegister(charId, updatedCharacter) {
+    axios
+      .put((this.BASE_URL += `/${charId}`), updatedCharacter)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(`Unable to update character with id ${charId}`);
+      });
   }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(deleteId) {
+    const toDelete = confirm("Are you sure you want to delete?");
+    if (toDelete) {
+      axios
+        .delete((this.BASE_URL += `/${deleteId}`))
+        .then((response) => {
+          alert(response.data);
+          getFullList();
+        })
+        .catch((error) =>
+          console.log(`Error while deleting character: ${error}`)
+        );
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,97 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8000/characters')
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
-  });
+    charactersAPI.getFullList()
+    .then (characters => {
+      let str = '';
+        characters.data.forEach(character => {
+          str += `
+              <div class="character-info">
+              <div class="chr-id">Character ID:${character.id}</div>
+              <div class="name">Character Name:${character.name}</div>
+              <div class="occupation">Character Occupation:${character.occupation}</div>
+              <div class="weapon">Character Weapon:${character.weapon}</div>
+              <div class="cartoon">Is a cartoon?:${character.cartoon}</div>
+              </div>`;
+        });
+  
+        document.querySelector('.characters-container').innerHTML = str;
+      });
+    })
+    })
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister(document.getElementById('chr-id').value)
+    .then(character => {
+     const { id, name, occupation, weapon, cartoon } = character.data;
+     document.querySelector('.characters-container').innerHTML = `
+     <div class="character-info">
+     <div class="chr-id">Character ID:${id}</div>
+     <div class="name">Character Name:${name}</div>
+     <div class="occupation">Character Occupation:${occupation}</div>
+     <div class="weapon">Character Weapon:${weapon}</div>
+     <div class="cartoon">Is a cartoon?:${cartoon}</div>
+     </div>`
+    })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(document.getElementById('delete-id').value)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    const charName = document.getElementById('update-name-input');
+    const charOccupation = document.getElementById('update-occupation-input');
+    const charWeapon = document.getElementById('update-weapon-input');
+    const charId = document.getElementById('char-id');
 
-  });
+  const { id, name, occupation, weapon } = response.data;
+          charName.value = name;
+          charOccupation.value = occupation;
+          charWeapon.value = weapon;
+          charCartoon.value = cartoon;
+          charId.value = id;
+          document.getElementById('update-form-div').style.display = 'block';
+
+document.getElementById('edit-character-form').addEventListener('submit', event => {
+      event.preventDefault();
+      const charId = document.getElementById('chr-id').value;
+      const charName = document.getElementById('update-name-input').value;
+      const charOccupation = document.getElementById('update-occupation-input').value;
+      const charWeapon = document.getElementById('update-weapon-input').value;
+      const charCartoon = document.getElementById('update-cartoon-input').value;
+      
+      const updatedCharacter = {
+        name: charName,
+        occupation: charOccupation,
+        weapon: charWeapon,
+        cartoon: charCartoon
+      };
+
+      charactersAPI.updateOneRegister(charId, updatedCharacter);
+
+      document.getElementById('edit-character-form').reset();
+      document.getElementById('edit-character-form').style.display = 'none';
+  })
+      .catch(error => console.log(`Error while updating a character: ${error}`));
+    })
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name-input').value;
+    const occupation = document.getElementById('occupation-input').value;
+    const weapon = document.getElementById('weapon-input').value;
+    const cartoon = document.getElementById('cartoon-input').value;
+    
+    const newCharacterInfo = {
+        name,
+        occupation,
+        weapon, 
+        cartoon
+      };
+    
+    console.log('New character: ', newCharacterInfo);
 
-  });
-});
+    charactersAPI.createOneRegister(newCharacterInfo);
+  })
