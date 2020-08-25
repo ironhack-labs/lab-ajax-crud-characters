0cd10d2af386fbf3eb966e16441a4fa75f61bd86
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-08-25 17:59:12 +0200

    Iteration 3 complete

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,57 +4,22 @@ class APIHandler {
   }
 
   getFullList() {
-    axios
-      .get(`${this.BASE_URL}/characters`)
-      .then((response) => {
-        console.log("Response from the API: ", response.data);
-      })
-      .catch((error) => {
-        console.log("Unable to retrieve characters.");
-      });
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
-  getOneRegister() {
-    axios
-      .get(`${this.BASE_URL}/characters/${characterId}`)
-      .then((response) => {
-        console.log("Response from the API: ", response.data);
-      })
-      .catch((error) => {
-        console.log(`Unable to retrieve character with ID ${characterId}.`);
-      });
+  getOneRegister(characterId) {
+    return axios.get(`${this.BASE_URL}/characters/${characterId}`);
   }
 
-  createOneRegister() {
-    axios
-      .post(`${this.BASE_URL}/characters`, newCharacter)
-      .then((response) => {
-        this.getFullList();
-      })
-      .catch((error) => {
-        console.log("Unable to create character.");
-      });
+  createOneRegister(newCharacter) {
+    axios.post(`${this.BASE_URL}/characters`, newCharacter);
   }
 
-  updateOneRegister() {
-    axios
-      .put(`${this.BASE_URL}/characters/${characterId}`, editedCharacter)
-      .then((response) => {
-        console.log("Response from the API: ", response.data);
-      })
-      .catch((error) => {
-        console.log(`Unable to update character with ID ${characterId}.`);
-      });
+  updateOneRegister(characterId, editedCharacter) {
+    axios.put(`${this.BASE_URL}/characters/${characterId}`, editedCharacter);
   }
 
-  deleteOneRegister() {
-    axios
-      .delete(`${this.BASE_URL}/character/${characterId}`)
-      .then((response) => {
-        console.log("Response from the API: ", response.data);
-      })
-      .catch((error) => {
-        console.log(`Unable to delete character with ID ${characterId}.`);
-      });
+  deleteOneRegister(characterId) {
+    axios.delete(`${this.BASE_URL}/characters/${characterId}`);
   }
 }
