1babe54d5a308812f90cd12916d7898ec79f1a65
ViktorJT <viktor.jensentorp@hyperisland.se>
2020-08-27 18:21:40 +0200

    :sparkles: initial commit
    done, except for displaying characters in promises

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,54 @@ class APIHandler {
   }
 
   getFullList () {
-
+    axios.get(`${this.BASE_URL}/characters`)
+    .then(() => {
+      
+    })
+    .catch(err => console.error(err));
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
 
+      document.getElementById('edit-id').value = res.data.id;
+      document.getElementById('edit-name').value = res.data.name;
+      document.getElementById('edit-occupation').value = res.data.occupation;
+      document.getElementById('edit-weapon').value = res.data.weapon;
+      document.getElementById('edit-cartoon').checked = res.data.cartoon;
+
+    })
+    .catch(err => console.error(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister (name, occupation, weapon, cartoon) {
+    axios.post(`${this.BASE_URL}/characters`, {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    })
+    .then(what => {
+      console.log(what);
+    })
+    .catch(err => console.error(err))
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id, name, occupation, weapon, cartoon) {
+    axios.put(`${this.BASE_URL}/characters/${id}`, {
+      name, 
+      occupation, 
+      weapon, 
+      cartoon
+    })
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
+    .then(res => console.log(`deleted ${id}`))
+    .catch(err => console.log(err))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,22 +2,40 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    const id = document.getElementById('character-id').value
+    charactersAPI.getOneRegister(id);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    event.preventDefault();
+    const id = document.getElementById('delete-id').value;
+    console.log(id);
+    charactersAPI.deleteOneRegister(id);
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const id = document.getElementById('edit-id').value
+    const name = document.getElementById('edit-name').value
+    const occupation = document.getElementById('edit-occupation').value
+    const weapon = document.getElementById('edit-weapon').value
+    const cartoon = document.getElementById('edit-cartoon').checked
+    charactersAPI.updateOneRegister(id, name, occupation, weapon, cartoon);
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('new-name').value;
+    const occupation = document.getElementById('new-occupation').value;
+    const weapon = document.getElementById('new-weapon').value;
+    const cartoon = document.getElementById('new-cartoon').checked ? true : false;
 
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon);
   });
 });
