900b56c4a906258fa7046ce48a08add9e58a8219
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-27 15:45:50 +0200

    event listener iteration 3

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,22 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList().then((characters) => {
+      let characterInfo = ''
+      characters.data.forEach(character => {
+        characterInfo += `
+        <div class="character-info">
+        <div class="name">Character ID: ${character.id}</div>
+        <div class="name">Character Name: ${character.name}</div>
+        <div class="occupation">Character Occupation: ${character.occupation}</div>
+        <div class="cartoon">Is a Cartoon?: ${character.cartoon}</div>
+        <div class="weapon">Character Weapon: ${character.weapon}</div>
+      </div>
+      `
+      });
+
+      document.querySelector('.characters-container').innerHTML = characterInfo;
+    })  
 
   });
 
