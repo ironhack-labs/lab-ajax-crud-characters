63838bb711e09ea18f2fe88072ae9b7d65621a08
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-02 13:37:25 -0500

    initiated project

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,27 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
 
-  getFullList () {
+    async getFullList() {
+        const { data: characters } = await axios.get(`${this.BASE_URL}/characters`)
+        return characters;
+    }
 
-  }
+    async getOneRegister(id) {
+        const character = await axios.get(`${this.BASE_URL}/characters/${id}`)
+        return character;
+    }
 
-  getOneRegister () {
+    createOneRegister() {
 
-  }
+    }
 
-  createOneRegister () {
+    updateOneRegister() {
 
-  }
+    }
 
-  updateOneRegister () {
+    deleteOneRegister() {
 
-  }
-
-  deleteOneRegister () {
-
-  }
+    }
 }
