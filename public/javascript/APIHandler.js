80aca20490ed151ae0e433bff60c69bed988f91c
Pedro Conde <condepedro968@gmail.com>
2020-09-15 18:16:47 +0200

    casi terminado

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,33 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor () {
+    this.app = axios.create({
+        baseURL: 'https://minions-api.herokuapp.com/'
+    });
+    console.log('Instancia creada y aplicación inicializada:')
   }
 
-  getFullList () {
+  getFullList = () => this.app.get('characters')
 
+  getOneRegister = charId => this.app.get(`characters/${charId}`)
+
+  createOneRegister(name, occupation, weapon, cartoon){
+      this.app.post(`characters`,{
+          name,
+          occupation,
+          weapon,
+          cartoon
+      })
   }
 
-  getOneRegister () {
+  updateOneRegister (id, name, occupation, weapon, cartoon) {
+      this.app.put(`characters/${id}` ,{
+          id,
+          name,
+          occupation,
+          weapon,
+          cartoon
+      })
+    }
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister = charId => this.app.delete(`characters/${charId}`)
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,55 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
 
-window.addEventListener('load', () => {
+window.addEventListener('load', () => {  // fetch all characters
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+      
+    charactersAPI.getFullList()
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+    
   });
-
+                // Fetch only 1 character
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    event.preventDefault()
+    const id = document.getElementById("character-id").value
+    
+    //console.log(id)
+    charactersAPI.getOneRegister(id)
+        .then(res => console.log(res.data))
+        .catch(err => console.log(err))
+    
   });
-
+                // Delete 1 character
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
+    event.preventDefault();
+    const id = document.getElementById('character-id-delete').value;
+    console.log(id)
+
+    charactersAPI.deleteOneRegister(id)
+        .then(res => console.log(`id ${id} Deleted`))
+        .catch(err => console.log(err))
   });
 
+                // Edit 1 character
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
   });
 
+                // Create new character
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+    event.preventDefault()
+    const name = document.getElementById("new-name").value
+    const occupation = document.getElementById("new-occupation").value
+    const weapon = document.getElementById("new-weapon").value
+    const cartoon = document.getElementById("new-checkbox").value
+    console.log(name, occupation, weapon, cartoon)
+
+    charactersAPI.createOneRegister(name, occupation, weapon, cartoon)
+        .then(res => console.log(`one register created`))
+        .catch(err => console.log(err))
 
   });
 });
