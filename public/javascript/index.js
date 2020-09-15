a2bf58526c43174d7df23ecd472cd467feb37f8c
anabermudezmonteagudo <anabermudezmonteagudo@gmail.com>
2020-09-15 19:50:07 +0200

    done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,125 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+//Listar todos los minions
+document.querySelector('#fetch-all').onclick = e => {
+ 
+  e.preventDefault()
 
-  });
+    function showCurrentMinions(){
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  
+    charactersAPI
+      .getFullList()
+      .then(allMinions => {
 
-  });
+        const allCharacters = allMinions.data.slice(-6)
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+        const infoMinion = document.querySelector('.characters-container')
 
-  });
+        let text = ''
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+        allCharacters.forEach(elm => infoMinion.innerHTML += 
+        `<div class="character-info">
+        <div class="id">Character ID: ${elm.id}</div>
+        <div class="name">Name: ${elm.name}</div>
+        <div class="occupation">Occupation: ${elm.occupation}</div>
+        <div class="cartoon">Is a Cartoon?: ${elm.cartoon}</div>
+        <div class="weapon">Weapon: ${elm.weapon}</div>
+      </div>
+    </div>`
+  )})
+  
+     .catch(err => console.log('error', err))
+  }
 
-  });
+  showCurrentMinions()
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+}
 
-  });
-});
+
+
+//Encontrar minion por ID
+document.querySelector('#fetch-one').onclick = e => {
+  e.preventDefault()
+
+  const minionId = document.querySelector('.operation input').value
+
+  charactersAPI
+  .getOneRegister(minionId)
+  .then(minion => {
+    
+      const { id, name, occupation, cartoon, weapon } = minion.data
+
+      const infoMinion = document.querySelector('.characters-container')
+     
+       infoMinion.innerHTML = 
+        `<div class="character-info">
+        <div class="id">Character ID: ${id}</div>
+        <div class="name"> Name: ${name}</div>
+        <div class="occupation">Occupation: ${occupation}</div>
+        <div class="cartoon">Is a Cartoon?: ${cartoon}</div>
+        <div class="weapon">Weapon: ${weapon}</div>
+    </div>`
+
+    })
+
+  .catch(err => console.log('Hubo un error!', err))
+}
+
+
+
+//Eliminar un minion
+document.querySelector('#delete-one').onclick = e => {
+     e.preventDefault()
+
+  const minionId = document.querySelector('input[name=character-id-delete]').value
+
+  charactersAPI
+    .deleteOneRegister(minionId)
+    .then(() => {
+    document.querySelector('input[name=character-id-delete]').reset()
+  })
+   .catch(err => console.log('Hubo un error!', err))
+}
+
+
+//Crear un nuevo minion
+
+document.querySelector('#new-character-form').onsubmit = e => {
+  e.preventDefault()
+  const infoMinions = document.querySelectorAll('#new-character-form input')
+
+
+  const minion = {
+      name: infoMinions[0].value,
+      occupation: infoMinions[1].value,
+      weapon: infoMinions[3].value,
+      cartoon: infoMinions[4].ckecked
+    }
+  
+  //infoMinions[2].value === "on" ? minion.cartoon = true : minion.cartoon = false
+  charactersAPI
+    .createOneRegister(minion)
+
+}
+
+//Editar minion
+
+document.querySelector('#edit-character-form').onsubmit = e => {
+  e.preventDefault()
+  const infoMinions = document.querySelectorAll('#edit-character-form input')
+
+  const minionId = infoMinions[0].value
+
+  const minionInfo = {
+    name: infoMinions[1].value,
+    occupation: infoMinions[2].value,
+    weapon: infoMinions[3].value,
+    cartoon: infoMinions[4].ckecked
+  }
+  //infoMinions[3].value === "on" ? minion.cartoon = true : minion.cartoon = false
+
+  charactersAPI
+    .updateOneRegister(minionId, minionInfo)
+
+}
