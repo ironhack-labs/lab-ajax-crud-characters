129fafa3d53c53d07c5f3b3af674b07eada64f66
Zalillo18 <gmcano85@gmail.com>
2020-09-15 20:36:14 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,34 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor () {
+    this.app = axios.create({
+      baseURL: 'https://minions-api.herokuapp.com'
+    })
   }
 
   getFullList () {
-
+    return this.app.get('/characters')
   }
 
-  getOneRegister () {
-
+  getOneRegister (characterId) {
+    return  this.app.get(`/characters/${characterId}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister(characterInfo) {
+    console.log(characterInfo)
+    return this.app.post('/characters', characterInfo)
   }
 
-  updateOneRegister () {
-
+   updateOneRegister (characterId, characterInfo) {
+    return this.app.put(`/characters/${characterId}`, characterInfo)
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (characterId) {
+    return  this.app.delete(`/characters/${characterId}`)
   }
 }
+
+
+// getAllCharacters = () => this.app.get('/characters')
+// getOneCharacter = characterId => this.app.get(`/characters/${characterId}`)
+// createCharacter = characterInfo => this.app.post('/characters', characterInfo)
+// editCharacter = (characterId, characterInfo) => this.app.put(`/characters/${characterId}`, characterInfo)
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,128 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('https://minions-api.herokuapp.com')
 
 window.addEventListener('load', () => {
+
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
+    charactersAPI
+      .getFullList()
+      .then(result => {
+        let text = ''
+        result.data.forEach(elm => {
+          
+          
+          text += `
+          <div  class="character-info">
+          <div class="name">Character Name: ${elm.name} </div>
+          <div class="occupation">Character Occupation: ${elm.occupation}</div>
+          <div class="cartoon">Is a Cartoon: ${elm.cartoon}</div>
+          <div class="weapon">Character Weapon ${elm.weapon}</div>
+          </div>
+          `
+          
+        })
+        document.querySelector('#all').innerHTML = text
+      })
+        .catch(err => console.log(err))
+      });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    const characterId = document.getElementsByName("character-id")[0].value
+
+    charactersAPI
+      .getOneRegister(characterId)
+      .then((result) => { 
+
+        const inputs = document.querySelectorAll('#edit-character-form input')
+        if(result.data === null)
+          inputs[0].value = 'CHARACTER NOT FOUND'
+        else {
+          inputs[0].value = result.data.id
+          inputs[1].value = result.data.name
+          inputs[2].value = result.data.occupation
+          inputs[3].value = result.data.weapon
+          
+        }
+
+        let text = ''
+        if (!result.data)
+          text = `USER NOT FOUND, RETRY!`
+        else {
+          text += `
+        <div  class="character-info">
+        <div class="name">Character Name: ${result.data.name} </div>
+        <div class="occupation">Character Occupation: ${result.data.occupation}</div>
+        <div class="cartoon">Is a Cartoon: ${result.data.cartoon}</div>
+        <div class="weapon">Character Weapon ${result.data.weapon}</div>
+        </div>
+        `
+        }
+        document.querySelector('#all').innerHTML = text
+      })
+      .catch(err => console.log(err))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
+    const characterId = document.getElementsByName("character-id-delete")[0].value
+    charactersAPI
+      .deleteOneRegister(characterId)
+      .then((result) => {
+        if (result)
+          document.getElementById('delete-one').style.backgroundColor = 'green'
+      })
+      .catch(() => document.getElementById('delete-one').style.backgroundColor = 'red')
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').onsubmit = e => {
+    e.preventDefault()
 
-  });
+    const characterId = document.getElementsByName("chr-id")[0].value
+    const inputs = document.querySelectorAll('#edit-character-form input')
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    if (inputs[4].value === 'on')
+      inputs[4].value = true
+    else
+      inputs[4].value = false
+    
+    const characterInfo = {
+      id: inputs[0].value,
+      name: inputs[1].value,
+      occupation: inputs[2].value,
+      weapon: inputs[3].value,
+      cartoon: inputs[4].value
+    }
+    charactersAPI
+      .updateOneRegister(characterId, characterInfo)
+      .then((myCharacter) => {
+        document.getElementById('send-data').style.backgroundColor = 'green'
+        document.querySelector('#new-character-form').reset()
+      })
+      .catch(err => document.getElementById('send-data').style.backgroundColor = 'red')
+    }
 
-  });
-});
+  document.getElementById('new-character-form').onsubmit = e => {
+    e.preventDefault()
+
+    const inputs = document.querySelectorAll('#new-character-form input')
+
+    if (inputs[3].value === 'on')
+      inputs[3].value = true
+    else
+      inputs[3].value = false
+    const myCharacter = {
+      name: inputs[0].value,
+      occupation: inputs[1].value,
+      weapon: inputs[2].value,
+      cartoon: inputs[3].value
+    }
+    charactersAPI
+      .createOneRegister(myCharacter)
+      .then((myCharacter) => {
+            document.getElementById('send-data').style.backgroundColor = 'green'
+            document.querySelector('#new-character-form').reset()
+        })
+      .catch(err => document.getElementById('send-data').style.backgroundColor = 'red')
+    }
+})
