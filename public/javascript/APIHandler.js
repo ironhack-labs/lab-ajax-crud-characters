19f69eeeb685f54cf78d93ec7d22bcddf9888110
anna dodo <anna.dorenskaya@gmail.com>
2020-09-17 20:30:36 +0200

    1 and 2 iterations

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,62 @@
+/* const axiosAPI = axios.create({
+  baseUrl: 'http://localhost:8000'
+}) */
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    
   }
 
   getFullList () {
-
+      axios.get(this.BASE_URL + "/characters")
+        .then(data => {
+          console.log(data.data)
+        })
+        .catch(err => {
+          console.log(err)
+        })
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+     axios.get(this.BASE_URL  + `/characters/${id}`)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    }) 
   }
 
   createOneRegister () {
-
+    axios.post(this.BASE_URL, {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    })
+    .then(data => {
+      console.log(data)
+    })
   }
 
   updateOneRegister () {
-
+    axios.patch(this.BASE_URL + `/characters/${id}`)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 
   deleteOneRegister () {
-
+    axios.delete(this.BASE_URL + `/characters/${id}`)
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,19 @@
+
+/* 
+axios.get('localhost:8000/characters')
+.then(data => {
+  console.log(data)
+})
+.catch(err => {
+  console.log(err)
+}) */
+
 const charactersAPI = new APIHandler('http://localhost:8000');
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
