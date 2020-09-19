53d8f512c665c33bba2d8a724d697785471831aa
Lisette Venema <lisettevenema@hotmail.com>
2020-09-19 14:11:10 +0200

    getting the gest of this assignment

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,38 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+
+  getFullList() {
+    return axios.get(this.BASE_URL + '/characters')
+  }
+
+  getOneRegister(id) {
+    return axios.get(`${this.BASE_URL}/characters/${id}`)
+  }
+
+  createOneRegister(name,occupation,weapon,cartoon) {
+    axios.post(`${this.BASE_URL}/characters/`,
+    {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    })
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  updateOneRegister() {
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios.delete (`${this.BASE_URL}/characters/${id}`)
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,15 +2,62 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
+      .then(response => {
+        console.log(response.data)
+        let showCharacters = ""
+        response.data.forEach(character => {
+          console.log(character)
+          showCharacters += `
+          <div class="character-info">
+          <div class="name">Character Name:${character.name}</div>
+          <div class="occupation">Character Occupation:${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+          <div class="weapon">Character Weapon: ${character.weapon}</div>
+        </div>`
+        })
+        document.getElementById('characters-container').innerHTML = showCharacters
+      })
+      .catch(err => {
+        console.log(err)
+      })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const nameOfCharacter = document.getElementById('fetching-one')
+    console.log(nameOfCharacter.value)
+    charactersAPI.getOneRegister(nameOfCharacter.value)
+      .then(response => {
+        console.log(response.data)
+        let showOnlyOneCharacter = ""
+        showOnlyOneCharacter += `
+      <div class="character-info">
+          <div class="name">Character Name:${response.data.name}</div>
+          <div class="occupation">Character Occupation:${response.data.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${response.data.cartoon}</div>
+          <div class="weapon">Character Weapon: ${response.data.weapon}</div>
+        </div>`
+        document.getElementById('characters-container').innerHTML = showOnlyOneCharacter
+      })
+      .catch(err => {
+        console.log(err)
+      })
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    event.preventDefault()
+    let deleteOneCharacter = document.getElementById('deleting-this-one')
+    console.log(deleteOneCharacter.value)
+    charactersAPI.deleteOneRegister(deleteOneCharacter.value)
+    .then(response => {
+      console.log(response.data)
 
+      document.getElementById('delete-one').style.backgroundColor = "green"
+    })
+    .catch(err => {
+      document.getElementById('delete-one').style.backgroundColor = "red"
+      console.log(err)
+    })
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
@@ -18,6 +65,6 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.createOneRegister()
   });
 });
