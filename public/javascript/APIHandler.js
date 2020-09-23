07d75aec3c79fe58e823190aff1a61f399098c18
sechs <philippe.buteau@skema.edu>
2020-09-23 16:37:48 +0200

    iteration 2 completed

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,27 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.service = axios.create({
+      baseURL: baseUrl
+    })
   }
 
   getFullList () {
-
+return this.service.get("/characters")
   }
 
   getOneRegister () {
-
+    return this.service.get("/characters/" + id)
   }
 
   createOneRegister () {
-
+    return this.service.post("/characters/", data)
   }
 
   updateOneRegister () {
-
+    return this.service.patch("/characters/" + id, data)
   }
 
   deleteOneRegister () {
-
+    return this.service.delete("/characters/" +id)
   }
 }
