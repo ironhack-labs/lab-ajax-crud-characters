25df0592346a3e7fb51c94bf89eddc5e1bc7e91a
Leonardo Mezzanotti <leomezza@gmail.com>
2020-09-22 23:37:10 -0300

    Iteration 2 done, missing 3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,61 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const { data } = response;
+        console.log('The array of characters: ', data);
+        return data;
+      })
+      .catch(err => console.log(`Error while getting the list of characters: ${err}`));
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    axios
+      .get(`${this.BASE_URL}/characters/${id}`)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const { data } = response;
+        console.log('The character: ', data);
+      })
+      .catch(err => console.log(`Error while getting the character: ${err}`));
   }
 
-  createOneRegister () {
-
+  createOneRegister(newCharacterInfo) {
+    axios
+      .post(`${this.BASE_URL}/characters`, newCharacterInfo)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const { data } = response;
+        console.log('The new character: ', data);
+      })
+      .catch(err => console.log(`Error while saving a new character: ${err}`));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister(id, updatedCharacter) {
+    axios
+      .put(`${this.BASE_URL}/characters/${id}`, updatedCharacter)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const { data } = response;
+        console.log('The updated character: ', data);
+      })
+      .catch(err => console.log(`Error while updating a character: ${err}`));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    axios
+      .delete(`${this.BASE_URL}/characters/${id}`)
+      .then(response => {
+        console.log('Response from the API: ', response);
+        const { data } = response;
+        console.log('The deleted character: ', data);
+      })
+      .catch(err => console.log(`Err while deleting character: ${err}`));
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,23 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', event => {
+    const responseFromAPI = charactersAPI.getFullList();
+    console.log(responseFromAPI);
+    let str = '';
 
+    document.getElementsByClassName('characters-container').innerHTML = str;
+
+    responseFromAPI.forEach(character => {
+      str += `<div class="character-info">
+          <div class="id">id:<span class="float-right">${character.id}</span></div>
+          <div class="name">Name:<span class="float-right">${character.name}</span></div>
+          <div class="occupation">Occupation:<span class="float-right">${character.occupation}</span></div>
+          <div class="cartoon">Is a Cartoon?:<span class="float-right">${character.cartoon}</span></div>
+          <div class="weapon">Weapon:<span class="float-right">${character.weapon}</span></div>
+          </div>`;
+      document.getElementById('characters-container').innerHTML = str;
+    });
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
