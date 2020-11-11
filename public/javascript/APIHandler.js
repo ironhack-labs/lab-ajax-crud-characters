55dfd478042a1409b813176eff8ec77bd261870c
Maria Reyna <mereynag@gmail.com>
2020-11-11 18:01:22 -0400

    Ejercicio

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,90 @@
+const baseURL = "http://localhost:8000/characters"
+const singleChar = "http://localhost:8000/characters/:id"
+const $charList = document.querySelector(".character-info")
+
+const $createForm = document.querySelector("#new-character-form")
+const $updateForm = document.querySelector("#edit-character-form")
+const $fetchAll = document.querySelector("fetch-all")
+const $getCharInput = document.querySelector("#charid")
+const $getChatBtn = document.querySelector("#get-char-data")
+
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor (baseUrl, singleChar) {
     this.BASE_URL = baseUrl;
+    this.singleChar = singleChar;
   }
 
-  getFullList () {
-
+  getFullList (arr) {
+    $charList.innerHTML = ""
+    arr.forEach(el => {
+      $charList.innerHTML += `
+      Character Name: ${name}
+      Occupation: ${occupation}
+      Is a Cartoon: ${cartoon}
+      Character Weapon: ${weapon}`
+    })
   }
 
   getOneRegister () {
-
+    function getCharacters() {
+      axios.get(baseURL).then(({ data }) => {
+        getFullList(data)
+      })
+    }
   }
 
   createOneRegister () {
-
+    $createForm.onsubmit = async event => {
+      event.preventDefault()
+    
+      const { target } = event
+      const name = target[0].value
+      const occupation = target[1].value
+      const weapon = target[2].value
+    
+      await axios.post(baseURL, {
+        name,
+        occupation,
+        is a cartoon,
+        weapon
+      })
+    
+      target[0].value = ""
+      target[1].value = ""
+      target[2].value = ""
+    
+      getCharacters()
+    }
   }
 
   updateOneRegister () {
-
+    $updateForm.onsubmit = async e => {
+      e.preventDefault()
+    
+      const { target } = e
+      const name = target[0].value
+      const occupation = target[1].value
+      const weapon = target[2].value
+      const isacartoon = target[3].value
+      const id = $getCharInput.value
+    
+      await axios.patch(`${singleChar}`, { name, occupation, weapon, isacartoon })
+    
+      $getCharInput.value = ""
+      target[0].value = ""
+      target[1].value = ""
+      target[2].value = ""
+      target[3].value = ""
+    
+      getCharacters()
+    }
   }
 
   deleteOneRegister () {
-
+    async function deleteElement(id) {
+      await axios.delete(`${singleChar}`)
+      getCharacters()
+    }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,16 +1,79 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const baseURL = "http://localhost:8000/characters"
+const singleChar = "http://localhost:8000/characters/:id"
+const $charList = document.querySelector(".character-info")
+const $createForm = document.querySelector("#new-character-form")
+const $updateForm = document.querySelector("#edit-character-form")
+const $fetchAll = document.querySelector("fetch-all")
+const $getCharInput = document.querySelector("#charid")
+const $getChatBtn = document.querySelector("#get-char-data")
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    event.preventDefault()
+    function printChars(arr) {
+      $charList.innerHTML = ""
+      arr.forEach(element => {
+        $charList.innerHTML += `
+          <div class='col-12'>
+            <div class="card text-white bg-success mb-3">
+              <div class="card-body">
+                <p class="card-text">
+                Id: ${$getCharInput}
+                <br/>
+                Name: ${element.name}
+                <br/>
+                occupation: ${element.occupation}
+                <br/>
+                is a cartoon: ${element.cartoon}
+                <br/>
+                weapon: ${element.weapon}
+                </p>
+              </div>
+              <div class="card-body">
+              </div>
+            </div>
+          </div>
+        `
+      })
+    }    
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    event.preventDefault()
+    function printSingleChar() {
+      singleChar.innerHTML = ""
+      singleChar.innerHTML = `
+          <div class='col-12'>
+            <div class="card text-white bg-success mb-3">
+              <div class="card-body">
+                <p class="card-text">
+                Id: ${$getCharInput}
+                <br/>
+                Name: ${element.name}
+                <br/>
+                occupation: ${element.occupation}
+                <br/>
+                is a cartoon: ${element.cartoon}
+                <br/>
+                weapon: ${element.weapon}
+                </p>
+              </div>
+              <div class="card-body">
+              </div>
+            </div>
+          </div>
+        `
+      })  
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    event.preventDefault()
+    function deleteOne(id) {
+     const id = $getCharInput
+     id.delete
+     return `Character has been successfully deleted`
+    }
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
