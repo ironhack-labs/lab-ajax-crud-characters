53d20e89881ad0e5b400fe0c96aedd51592d810c
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-11 20:49:17 -0600

    naa

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,24 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
+      .then(response => {
+        console.log(document.querySelector('#characters').innerHTML)
+        let showCharacters = ""
+        response.data.forEach(character => {
+          showCharacters += `
+          <div class="character-info">
+          <div class="name">Character Name:${character.name}</div>
+          <div class="occupation">Character Occupation:${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
+          <div class="weapon">Character Weapon: ${character.weapon}</div>
+        </div>`
+        })
+        document.querySelector('#characters').innerHTML = showCharacters
+      })
+      .catch(err => {
+        console.log(err)
+      })
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
