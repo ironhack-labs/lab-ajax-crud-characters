b1a9d5be342fae8140b444ef9dafbc104b30f9c8
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-11 19:55:23 -0600

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -9,46 +9,44 @@ class APIHandler {
 
   getFullList() {
     return axios.get(this.BASE_URL + '/characters')
-      .then(() => console.log(this.BASE_URL))
-      .catch(error => console.log(error))
+      
+}
 
+getOneRegister() {
+  return axios.get(`${this.BASE_URL}/characters/${id}`)
+    .then()
+}
+
+createOneRegister(name, occupation, weapon, cartoon) {
+  axios.post(`${this.BASE_URL}/characters/`, {
+      name,
+      occupation,
+      weapon,
+      cartoon
+    })
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+updateOneRegister() {
+  async () => {
+    await axios.patch(`${this.BASE_URL}/${id}`, {
+      name,
+      occupation,
+      weapon
+    })
+
+
+    // expected output: "resolved"
   }
 
-  getOneRegister() {
-    return axios.get(`${this.BASE_URL}/characters/${id}`)
-      .then()
-  }
+}
 
-  createOneRegister(name, occupation, weapon, cartoon) {
-    axios.post(`${this.BASE_URL}/characters/`, {
-        name,
-        occupation,
-        weapon,
-        cartoon
-      })
-      .then(response => {
-        console.log(response.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  updateOneRegister() {
-    async () => {
-      await axios.patch(`${this.BASE_URL}/${id}`, {
-        name,
-        occupation,
-        weapon
-      })
-
-
-      // expected output: "resolved"
-    }
-
-  }
-
-  deleteOneRegister(id) {
-    return axios.delete(`${this.BASE_URL}/characters/${id}`)
-  }
+deleteOneRegister(id) {
+  return axios.delete(`${this.BASE_URL}/characters/${id}`)
+}
 }
