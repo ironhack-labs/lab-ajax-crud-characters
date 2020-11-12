3c3e12d69c1948761a5d320a9cda782e357801e8
Mauricio Figueroa <mfigueroapa@hotmail.com>
2020-11-11 22:40:48 -0600

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,86 @@
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  async getFullList() {
+    console.log("llamado a getFullList()")
+    const {data} = await axios.get(this.BASE_URL)
+    return data
+  }
+
+  async getOneRegister(id) {
+    try{
+      const {data: {name, occupation, weapon, isCartoon}} = await axios.get(`${this.BASE_URL}/${id}`)
+    console.log(`data: ${name}, ${occupation}, ${weapon}, ${isCartoon}`)
+    return {name, occupation, weapon, isCartoon}
+    }
+    catch {
+      return 
+    }
+  }
+
+  async createOneRegister(name, occupation, weapon, isCartoon) {
+    await axios.post(this.BASE_URL, {
+      name, occupation, weapon, isCartoon
+    })
+  }
+
+  async updateOneRegister(id,name,occupation,weapon, isCartoon) {
+    console.log(id)
+    await axios.patch(`${this.BASE_URL}/${id}`, {name, occupation, weapon,isCartoon})
 
   }
 
-  getOneRegister () {
-
+  async deleteOneRegister(id) {
+    console.log("this is the id" + id)
+    await axios.delete(`${this.BASE_URL}/${id}`)
   }
 
-  createOneRegister () {
 
-  }
+displayCharacters(elements) {
+  const $charactersContainer = document.querySelector('.characters-container')
+  $charactersContainer.innerHTML = ""
+  elements.forEach(element => {
+    console.log(element)
+    $charactersContainer.innerHTML += `
+      <div class="character-info">
+        <div class="id">    
+          <div style="display: inline"><span>ID: </span> ${element.id}</div>
+        </div>
+        <div class="name">
+          <div style="display: inline"><span>Name: </span>  ${element.name}</div>
+        </div>
+        <div class="occupation">
+          <div style="display: inline"><span>Occupation: </span>  ${element.occupation}</div>
+        </div>
+        <div class="cartoon">
+          <div style="display: inline"><span>Cartoon: </span>  ${element.isCartoon}</div>
+        </div>
+        <div class="weapon">
+          <div style="display: inline"><span>Weapon:</span> ${element.weapon}</div>
+        </div>
+      </div>
+    `
+  })
+}
+// displayCharacters(elements) {
+//   const $charactersContainer = document.querySelector('.characters-container')
+//   $charactersContainer.innerHTML = ""
+//   elements.forEach(element => {
+//     console.log(element)
+//     $charactersContainer.innerHTML += `
+//     <div class="characters-container">
+//       <div class="character-info">
+//         <div class="name">${element.name}</div>
+//         <div class="occupation"> ${element.occupation}</div>
+//         <div class="cartoon">${element.isCartoon}</div>
+//         <div class="weapon">${element.weapon}</div>
+//       </div>
+//     </div>
+//     `
+//   })
+// }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,67 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8000/characters');
+let $charactersContainer = document.querySelector('.characters-container')
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    const data = await charactersAPI.getFullList()
+    charactersAPI.displayCharacters(data)
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    const $inputID = document.querySelector('#character-id')
+    const data = await charactersAPI.getOneRegister($inputID.value)
+    document.querySelector('#fetch-one').classList.remove('active', 'wrong')
+    if (data) document.querySelector('#fetch-one').classList.add('active');
+    else {
+      document.querySelector('#fetch-one').classList.add('wrong');
+      $inputID.value = ''
+      return
+    }
+    $inputID.value = ''
+    charactersAPI.displayCharacters([data])
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    const $inputID = document.querySelector('#character-id-delete')
+    charactersAPI.deleteOneRegister($inputID.value)
+    $inputID.value = ''
+    const data = await charactersAPI.getFullList()
+    charactersAPI.displayCharacters(data)
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    const $createForm = document.querySelector('#edit-character-form')
+    event.preventDefault()
+    const {target} = event
+    const id = target[0].value
+    const name = target[1].value
+    const occupation = target[2].value
+    const weapon = target[3].value
+    const isCartoon = target[4].checked
+    charactersAPI.updateOneRegister(id,name,occupation,weapon,isCartoon)
+    event.target[0].value = ''
+    event.target[1].value = ''
+    event.target[2].value = ''
+    event.target[3].value = ''
+    event.target[4].checked = false
+    const data = await charactersAPI.getFullList()
+    charactersAPI.displayCharacters(data)
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    const $createForm = document.querySelector("#new-character-form")
+    event.preventDefault()
+    const { target } = event
+    const name = target[0].value
+    const occupation = target[1].value
+    const weapon = target[2].value
+    const isCartoon = target[3].checked
+    charactersAPI.createOneRegister(name, occupation, weapon, isCartoon)
+    event.target[0].value = '';
+    event.target[1].value = '';
+    event.target[2].value = '';
+    event.target[3].checked = false;
+    const data = await charactersAPI.getFullList()
+    charactersAPI.displayCharacters(data)
   });
 });
