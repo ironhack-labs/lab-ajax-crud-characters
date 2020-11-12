e3c26a7b5393222c458ca2b8a1dac52a6764291d
KarenRoth <karenrothc@gmail.com>
2020-11-11 23:22:34 -0300

    done APIHandler structure

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,32 @@
+const baseUrl = "http://localhost:8000/characters"
+const oneChar = "http://localhost:8000/characters/:id"
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    const {data : characters} = axios.get(`${this.BASE_URL}/characters`)
+    console.log (characters)
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    const {data : character} = axios.get(`${this.BASE_URL}/characters/${id}`)
+    console.log (character)
   }
 
-  createOneRegister () {
-
+  createOneRegister (name,occupation,weapon,cartoon) {
+    axios.post(`${this.BASE_URL}/characters/`, {name, occupation, weapon, cartoon})
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (id,name,occupation,weapon,cartoon) {
+    axios.put(`${this.BASE_URL}/characters/${id}`, {name, occupation, weapon, cartoon})
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/characters/${id}`)
   }
 }
+
+
