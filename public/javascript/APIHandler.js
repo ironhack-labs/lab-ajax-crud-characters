9ade58ebadde4a796fb3a28040eafdfe45443090
Isabel Ferrara <ifm95@hotmail.com>
2020-11-11 22:55:21 -0600

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,37 @@
+//llamar al contenedor donde se mandarán los datos de todos nuestros personajes
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
+
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`)
+    // .then((response)=> {
+    //   console.log('the data is ', data);
+    //   // printChars(data)
+    // })
+    // .catch(err=>console.log('Error', err))
   }
 
-  getOneRegister () {
+  getOneRegister (characterId) {
+    return axios.get(`${this.BASE_URL}/characters/${characterId}`)
+   } 
 
+  createOneRegister (char) {
+    const {name, occupation, cartoon, weapon } = char
+    return axios.post(`${this.BASE_URL}/characters`, {name, occupation, cartoon, weapon })
   }
 
-  createOneRegister () {
-
+  updateOneRegister (characterId, char) {
+    const { name, occupation, cartoon, weapon } = char
+    return axios.patch(`${this.BASE_URL}/characters/${characterId}`, {name, occupation, cartoon, weapon })
+      
   }
 
-  updateOneRegister () {
-
+  deleteOneRegister (characterId) {
+    return axios.delete(`${this.BASE_URL}/characters/${characterId}`) 
   }
 
-  deleteOneRegister () {
-
-  }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,93 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+function printChars(arr) {
+  //limpias el texto de ese contenedor 
+  const $charList= document.querySelector(`.characters-container`);
+  $charList.innerHTML = ''
+  //
+  arr.forEach(element => {
+    $charList.innerHTML += `
+    <div class="character-info" style="width:200px">
+      <span class="id">Id: ${element.id}</span>
+      <br>
+      <span class="name" >Name: ${element.name}</span><br>
+      <span class="occupation"> Occupation: ${element.occupation}</span><br>
+      <span class="cartoon">Cartoon: ${element.cartoon}</span><br>
+      <span class="weapon"> Weapon: ${element.weapon}</span><br>
+  </div>`
+   })
+  }
+
+
+
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click',  async (event)=> 
+  {
+    event.preventDefault()
+    const {data}= await charactersAPI.getFullList();
+    printChars(data)
+});
+
+
+  document.getElementById('fetch-one').addEventListener('click', async (event)=> {
+    event.preventDefault()
+    const $charID= document.querySelector('#character-id').value
+    console.log($charID) //1
+    //promise{
+    //result: {data: Object, status: 200, statusText: "OK", headers: Object, config: Object, …}
+    //data{cartoon: true, id: 1, name: "Han Solo", occupation: "Smuggler",weapon: "Blaster Pistol"}
+    const {data}= await charactersAPI.getOneRegister($charID)
+    console.log({data})
+    //{data:object}
+    console.log([data])
+     //[object] 
+    printChars([data]) 
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async (event) => {
+    event.preventDefault()
+    //busca el value del id que estas escribiendo en el input
+    const $delChar = document.querySelector(`#character-id-delete`).value
+//espera a que se cumpla la promesa
+    await charactersAPI.deleteOneRegister($delChar)
+    //borra el valor de value
+    $delChar.value=""
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    console.log(event)
+    //let target= event.target
+    const { target } = event
+    const id = target[0].value
+    const name = target[1].value
+    const occupation = target[2].value
+    const weapon = target[3].value
+    const cartoon = target[4].value
+    const edited = { name, occupation, weapon, cartoon }
 
+    charactersAPI.updateOneRegister(id, edited)
+    //borrar los espacios
+    target[0].value = ''
+    target[1].value = ''
+    target[2].value = ''
+    target[3].value = ''
+    target[4].value = ''
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault()
+    const { target } = event
+        const name = target[0].value
+        const occupation = target[1].value
+        const weapon = target[2].value
+        const cartoon = target[3].value
+        //igual que update pero mandas a llamar create one
+        charactersAPI.createOneRegister({ name, occupation, weapon, cartoon })
+        target[0].value = ''
+        target[1].value = ''
+        target[2].value = ''
+        target[3].value = ''
   });
 });
