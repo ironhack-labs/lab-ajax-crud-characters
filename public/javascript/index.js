558e85970e47dffc84408f87a11a4589d8307efc
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-11 21:02:08 -0600

    add characttr inifo

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,40 +1,36 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', function(event) {
     charactersAPI.getFullList()
       .then(response => {
-        console.log(document.querySelector('#characters').innerHTML)
-        let showCharacters = ""
+        var div = document.createElement("div")
+        div.classList.add("character-info")
+        // var p = document.createElement("p");
+        // div.append("Some text", p);
         response.data.forEach(character => {
-          showCharacters += `
-          <div class="character-info">
-          <div class="name">Character Name:${character.name}</div>
-          <div class="occupation">Character Occupation:${character.occupation}</div>
-          <div class="cartoon">Is a Cartoon? ${character.cartoon}</div>
-          <div class="weapon">Character Weapon: ${character.weapon}</div>
-        </div>`
+
+          console.log(document.querySelector('#characters').appendChild(div));
         })
-        document.querySelector('#characters').innerHTML = showCharacters
       })
       .catch(err => {
         console.log(err)
       })
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', function(event) {
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', function(event) {
 
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', function(event) {
 
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', function(event) {
 
   });
 });
