776173a6f2941186691ce860a6f26ce8446bd03e
MLuisa Santos <mls_mansell@outlook.com>
2020-11-17 22:07:08 +0100

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,13 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor () {
+    this.axiosApp = axios.create({baseUrl:'https://minions-api.herokuapp.com'})
   }
 
-  getFullList () {
 
-  }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  getFullList = () => this.axiosApp.get('/characters')
+  getOneRegister = charId => this.axiosApp.get(`/characters/${charId}`)
+  createOneRegister = charInfo => this.axiosApp.post('/characters', charInfo) 
+  updateOneRegister = (charId, charInfo) => this.axiosApp.put(`/characters/${charId}`, charInfo) 
+  deleteOneRegister = charId => this.axiosApp.delete(`/characters/${charId}`)
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,17 +1,76 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+
+    charactersAPI
+
+     .getFullList()
+      .then(response => {
+        const allChar = response.data
+        const allCharHtml = ''
+
+        allChar.forEach(elm => {
+          allCharHtml += `<div class="character-info">
+                            <div class="name">Character Name: ${elm.name}</div>
+                            <div class="occupation">Character Occupation: ${elm.occupation}</div>
+                            <div class="cartoon">Is a Cartoon?: ${elm.cartoon}</div>
+                            <div class="weapon">Character Weapon: ${elm.weapon}</div>
+                          </div>`
+        })
+
+        document.querySelector('.character-info').innerHTML = allCharHtml
+
+      })
+      .catch(err => console.log('HUBO UN ERROR!', err))
+
   });
+
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+
+const charIDvalue = document.querySelector('.operation input').value
+
+     charactersAPI
+       .getOneRegister(charIDvalue)
+       .then(response => {
+         const oneChar = response.data
+         const oneCharHtml = ''
+          oneCharHtml += `<div class="character-info">
+                            <div class="name">Character Name: ${oneChar.name}</div>
+                            <div class="occupation">Character Occupation: ${oneChar.occupation}</div>
+                            <div class="cartoon">Is a Cartoon?: ${oneChar.cartoon}</div>
+                            <div class="weapon">Character Weapon: ${oneChar.weapon}</div>
+                          </div>`
+
+
+         document.querySelector('.character-info').innerHTML = oneCharHtml
+
+       })
+       .catch(err => console.log('HUBO UN ERROR!', err))
+
   });
+
+
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
 
+const deleteCharIDvalue = document.querySelector('.operation delete').value //??????
+
+    charactersAPI
+      .deleteOneRegister(deleteCharIDvalue)
+      .then(() => {
+         document.querySelector('#delete-one')
+      })
+      .catch(document.querySelector('#delete-one'))
+
   });
+
+
+
+
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
