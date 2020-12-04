8d17f60c843c9967b6f49b0b2edc28336c3b91c5
Dirk Kelderman <dirkkelderman@gmail.com>
2020-12-04 11:11:50 +0100

    iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,43 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList (baseUrl) {
+    axios.get(baseUrl + '/characters')
+      .then(fullListFromApi => {
+        console.log(fullListFromApi)
+      })
+      .catch(err => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister (baseUrl) {
+    axios.get(baseUrl + `/characters/${id}`)
+      .then(oneRegisterFromApi => {
+        console.log(oneRegisterFromApi)
+      })
+      .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
+  createOneRegister (baseUrl) {
+    axios.post(baseUrl + '/characters', {name, occupation, weapon, cartoon})
+      .then(createOneApi => {
+        console.log(createOneApi);
+      })
+      .catch(err => console.log(err));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (baseUrl) {
+    axios.put(baseUrl + `/characters/${id}`)
+      .then(updateOneApi => {
+        console.log(updateOneApi)
+      })
+      .catch(err => console.log(err));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (baseUrl) {
+    axios.delete(baseUrl + `/characters/${id}`)
+      .then(deleteOneApi => {
+        console.log(deleteOneApi)
+      })
+      .catch(err => console.log(err));
   }
 }
