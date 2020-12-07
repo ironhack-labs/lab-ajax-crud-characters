c61d19e174861a8e201b8cff58218e45cec10cc5
Thrashskin <jmontegonz@gmail.com>
2020-12-07 08:04:39 +0100

    iteration 2 ongoing - list characters complete

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,74 @@ class APIHandler {
   }
 
   getFullList () {
+    axios
+    .get(this.BASE_URL + '/characters')
+    .then( response => {
+      console.log(response.data);
 
+      let str = '';
+
+      response.data.forEach(character => {
+        str +=`
+        <li class="list-group-item">
+          <div class="character-info">
+              <div>Id: ${character.id}</div>
+              <div>Name: ${character.name}</div>
+              <div>Occupation: ${character.occupation}</div>
+              <div>Cartoon?: ${character.cartoon}</div>
+              <div>Weapon: ${character.weapon}</div>
+          </div>
+        </li>`
+      });
+
+      document.getElementById('characters-list').innerHTML = str;
+
+    } )
+    .catch(err => {
+      console.log(err);
+    })
+    
   }
 
   getOneRegister () {
-
+    axios
+    .get(this.BASE_URL + '/characters/:id')
+    .then( response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   createOneRegister () {
-
+    axios
+    .post(this.BASE_URL + '/characters', {})
+    .then( () => {
+      //this should redirect to the list of all the characters.
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   updateOneRegister () {
+    axios
+    .put(this.BASE_URL + '/characters/:id', {})
+    .then( () => {
 
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 
   deleteOneRegister () {
-
+    axios
+    .delete(this.BASE_URL + '/characters/:id')
+    .then()
+    .catch(err => {
+      console.log(err);
+    })
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,7 +2,7 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
