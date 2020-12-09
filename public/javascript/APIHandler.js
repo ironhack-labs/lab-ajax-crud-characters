fb0a35f94ebe730b6d23a982b64d4f5f975426d2
Marine Sanjuan <marine@sanjuan.eu>
2020-12-09 19:36:10 +0100

    lab not done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,51 @@
+//const charactersAPI = require(".");
+
 class APIHandler {
   constructor (baseUrl) {
+    this.service = axios.create({baseUrl:'http://localhost:8000'});
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return axios
+      .get(this.BASE_URL + '/characters')
+      .then(response => response.data)
+      .catch(err => console.log(err));
+    // return this.service.get('/characters');
+
+  }
+  
+
+  getOneRegister (id) {
+    return axios
+      .get(this.BASE_URL + `/characters/${id}`)
+      .then(response => response.data) // ça doit être un JSON:id
+      .catch(err => console.log(err));
+
+    
+  }
+
+  createOneRegister (object) {
+    
+    return axios
+      .post(this.BASE_URL + `/characters`)
+      .then(response => response.data) // ça doit être un JSON:id
+      .catch(err => console.log(err));
 
   }
 
-  getOneRegister () {
-
+  updateOneRegister (id) {
+    return axios
+    .patch(this.BASE_URL + `/characters/${id}`)
+    .then(response => response.data) // ça doit être un JSON:id
+    .catch(err => console.log(err));
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    
+    return axios
+    .delete(this.BASE_URL + `/characters/${id}`)
+    .then(response => response.data) // ça doit être un JSON:id
+    .catch(err => console.log(err));
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,72 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+
+// FETCH ALL
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", async function (event) {
+      try {
+        const apiRes = await charactersAPI.getFullList(); // retourne JSON avec tous elts
+        const container = document.querySelector(".characters-container");
+        container.innerHTML = ""; // vide l'innerhtml de son container
+        //const arr = apiRes.data; // tranforme le JSON en array!!!
+        //console.log(arr); // être sur qu'on a un array //undifines
+        apiRes.forEach((character) => { // loop où l'on ajoute les divs
+          container.innerHTML += `<div class="character-info">
+          <div class="id">Id: ${character.id}</div>
+          <div class="name">Name: ${character.name}</div>
+          <div class="occupation">Occupation: ${character.occupation}</div>
+          <div class="cartoon">Is a Cartoon?: ${character.cartoon}</div>
+          <div class="weapon">Weapon: ${character.weapon}</div>`;
+        });
+        //console.log(apiRes.data);
+      } catch (err) {
+        console.error(err);
+      }
+    });
+
+
+// FETCH ONE
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    try {
+      const container = document.querySelector(".characters-container");
+      const fetchone = document.querySelector('.operation > input')
+      console.log(fetchone.value); // ça nous retourne un objet qui correpond à la value (1, 2)
+      container.innerHTML = "";
+      const getOne = await charactersAPI.getOneRegister(fetchone) 
+      console.log(getOne);
+      // getOne(character) { 
+      //   container.innerHTML += `<div class="character-info">
+      //   <div class="id">Id: ${character.id}</div>
+      //   <div class="name">Name: ${character.name}</div>
+      //   <div class="occupation">Occupation: ${character.occupation}</div>
+      //   <div class="cartoon">Is a Cartoon?: ${character.cartoon}</div>
+      //   <div class="weapon">Weapon: ${character.weapon}</div>`;
+      // };
+      //console.log(apiRes.data);
+    } catch (err) {
+      console.error(err);
+    }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
 
-  });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
   });
+
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
   });
+
+
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    
+  })
 
-  });
-});
+
+
+  
+    });
