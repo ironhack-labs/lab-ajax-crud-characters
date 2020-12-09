96c021c2674e3095dfae9860b9f5cb2077f98b91
Menhaj safi <minhaj.sahil01@gmail.com>
2020-12-09 21:52:33 +0100

    lab  done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,37 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    return axios
+      .get(this.BASE_URL + "/characters")
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
   }
 
-  getOneRegister () {
-
+  getOneRegister(id) {
+    return axios
+      .get(this.BASE_URL + "/characters/" + id)
+      .then((response) => response.data)
+      .catch((err) => console.log(err));
   }
-
-  createOneRegister () {
-
+  createOneRegister(newChar) {
+    return axios
+      .post(this.BASE_URL + "/characters", newChar)
+      .then()
+      .catch((err) => console.log(err));
   }
-
-  updateOneRegister () {
-
+  updateOneRegister(id, modifChar) {
+    return axios
+      .patch(this.BASE_URL + "/characters/" + id, modifChar)
+      .then()
+      .catch((err) => console.log(err));
   }
-
-  deleteOneRegister () {
-
+  deleteOneRegister(id) {
+    return axios
+      .delete(this.BASE_URL + "/characters/" + id)
+      .then()
+      .catch((err) => console.log(err));
   }
 }
