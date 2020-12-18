18eef95ee4952d28608dee2d0df8fb2fc76a33b0
Tomy Tubert <tomytubert@gmail.com>
2020-12-18 13:22:51 +0100

    Done but I don't like the update function

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,85 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.baseUrl = baseUrl;
+    this.APIRequest = axios.create({
+      baseURL: this.baseUrl
+    })
   }
 
-  getFullList () {
+  async getFullList () {
+    try{
+      const{data} = await this.APIRequest.get("/characters");
+      return data
+      //console.log(data);
+    }catch(err){
+      console.error(err);
+    }
+  }
+
+  async getOneRegister (id) {
+  try{
+    const {data} = await this.APIRequest.get(`/characters/${id}`)
+    return data
+  }catch(err){
+    console.error(err);
+  }
+  }
+
+  async createOneRegister () {
+  try{
+
+    const {value:name}=document.querySelector("#name")
+    const {value:occupation}=document.querySelector("#occupation")
+    const {value:weapon}=document.querySelector("#weapon")
+    const {checked:cartoon}=document.querySelector("#cartoon")
+
+    const newOne = {
+      name:name,
+      occupation:occupation,
+      weapon:weapon,
+      cartoon:cartoon
+    }
+    
+    const {data: newCharacter } = await this.APIRequest.post("/characters",newOne)
+    console.log(newCharacter)
+    return newCharacter
+    
+  }catch(err){
+    console.error(err);
+  }
+  }
+
+  async updateOneRegister (id) {
+    try{
+      
+      const {value:name}=document.querySelector("#nameEditCharacter")
+      const {value:occupation}=document.querySelector("#occupationEditCharacter")
+      const {value:weapon}=document.querySelector("#weaponEditCharacter")
+      const {checked:cartoon}=document.querySelector("#cartoonEditCharacter")
+
+      const newOne = {
+        name:name,
+        occupation:occupation,
+        weapon:weapon,
+        cartoon:cartoon
+      }
+  
+      const updateNew = await this.APIRequest.patch(`/characters/${id}`,newOne)     
+      
+    
+    }catch(err){
+      console.error(err);
+    }
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
+  async deleteOneRegister (id) {
+    try{
+      const deleteOne = await this.APIRequest.delete(`/characters/${id}`)
+  
+    }catch(err){
+      console.error(err);
+    }
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,112 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+function renderCharacters(characters){
+
+  const container = document.querySelector(".characters-container")
+  container.innerHTML="";
+
+    if(characters.length == undefined){
+      console.log("Undefined");
+      container.innerHTML =
+      `<div class="character-info">
+      <div class="name">Character Name
+      ${characters.name}
+      </div>
+      <div class="occupation">Character Occupation
+      ${characters.occupation}
+      </div>
+      <div class="cartoon">Is a Cartoon?
+      ${characters.cartoon}
+      </div>
+      <div class="weapon">Character Weapon
+      ${characters.weapon}
+      </div>
+      <div class="id">Character Id
+      ${characters.id}
+      </div>
+    </div>`
+    } else {
+      characters.forEach(character =>{
+        container.innerHTML +=
+          `<div class="character-info">
+          <div class="name">Character Name
+          ${character.name}
+          </div>
+          <div class="occupation">Character Occupation
+          ${character.occupation}
+          </div>
+          <div class="cartoon">Is a Cartoon?
+          ${character.cartoon}
+          </div>
+          <div class="weapon">Character Weapon
+          ${character.weapon}
+          </div>
+          <div class="id">Character Id
+          ${character.id}
+          </div>
+        </div>`
+        })
+    }
+
+
+}
+
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+//-------------------------------All fetch-----------------------//
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    const charactersArr = await charactersAPI.getFullList()
+  
+    renderCharacters(charactersArr)
+    
+  });
+//------------------One character render-------------------------------//
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+  
+  const inputIdValue = document.querySelector(".operation > input").value
+
+  const character = await charactersAPI.getOneRegister(inputIdValue)
+ 
+  renderCharacters(character)
+
+  });
+//------------------------Delete One-----------------------------------------//
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    event.preventDefault()
+
+ const inputIdValue = document.querySelector(".operation-delete > input").value
+
+ const deleteOne = await charactersAPI.deleteOneRegister(inputIdValue)
+
+ const charactersArr = await charactersAPI.getFullList()
+  
+  renderCharacters(charactersArr)
+
+
+  });
+//------------------------Edit One-----------------------------------------//
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+  event.preventDefault()
+  
+  const{value:id}=document.querySelector("#idEditCharacter")
+
+  const editCharacter = await charactersAPI.updateOneRegister(id)
+
+  const charactersArr = await charactersAPI.getFullList()
+  
+  renderCharacters(charactersArr)
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+//------------------------New One-----------------------------------------//
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault()
+    
+    const newCharacter = await charactersAPI.createOneRegister()
 
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    const charactersArr = await charactersAPI.getFullList()
+  
+    renderCharacters(charactersArr)
+    
   });
 });
