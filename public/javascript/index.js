cd9b7fd47f6859818436a03e3fbb6da577459fe1
shtesh <vshtefanuka@gmail.com>
2020-12-18 23:18:21 +0100

    Done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,33 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI.getFullList();
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      charactersAPI.getOneRegister();
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+      charactersAPI.deleteOneRegister();
+    });
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      charactersAPI.updateOneRegister();
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {
+      charactersAPI.createOneRegister();
+    });
 });
