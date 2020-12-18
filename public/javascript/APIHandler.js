73ed46b832f56b3c4e62ad9b94ba9ffee98eb19d
Pablo Fernandez <pablo_fz@hotmail.com>
2020-12-18 19:15:47 +0100

    fetch all

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,26 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.charactersAPI = axios.create({
+      baseURL: this.BASE_URL,
+    });
   }
 
-  getFullList () {
-
+  async getFullList() {
+    try {
+      // const characters  =  await axios.get(`${this.BASE_URL}/characters`);
+      const { data: characters } = await this.charactersAPI.get(`/characters`);
+      return characters;
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  getOneRegister () {
+  getOneRegister() {}
 
-  }
+  createOneRegister() {}
 
-  createOneRegister () {
+  updateOneRegister() {}
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister() {}
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,8 +1,9 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+window.addEventListener('load', () => {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    addCharacter(await charactersAPI.getFullList());
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
@@ -21,3 +22,32 @@ window.addEventListener('load', () => {
 
   });
 });
+
+
+function addCharacter(arrayOrNot){
+
+  const character = document.querySelector(".characters-container");
+  character.innerHTML="";
+  if(arrayOrNot instanceof Array){
+    arrayOrNot.forEach((obj)=>{
+      character.innerHTML+=`<div class="character-info">
+      <div class="id">Id: ${obj.id}</div>
+      <div class="name">Name: ${obj.name}</div>
+      <div class="occupation">Occupation: ${obj.occupation}</div>
+      <div class="cartoon">Is a Cartoon? ${obj.cartoon}</div>
+      <div class="weapon">Weapon: ${obj.weapon}</div>
+    </div>`
+    })
+  }else{
+    character.innerHTML+=`<div class="character-info">
+    <div class="id">Id: ${obj.id}</div>
+    <div class="name">Name: ${obj.name}</div>
+    <div class="occupation">Occupation: ${obj.occupation}</div>
+    <div class="cartoon">Is a Cartoon? ${obj.cartoon}</div>
+    <div class="weapon">Weapon: ${obj.weapon}</div>
+  </div>`
+  }
+
+
+
+}
