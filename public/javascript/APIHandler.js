7efa223dd1f78460184e092931cc8cc2405ed619
LucasFeli <felipe298@gmail.com>
2020-12-21 01:00:11 +0100

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,18 +1,45 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+      this.BASE_URL= baseUrl;
+      this.APIRequest = axios.create({
+      baseURL : this.BASE_URL
+    })
   }
 
-  getFullList () {
-
+  async getFullList () {
+    try {
+      const { data: allCharacters } = await this.APIRequest.get('/characters')
+       console.log('response :>> ', allCharacters);
+      return allCharacters;
+    } catch (error) {
+      console.log(error);
+      
+    }
+  
   }
 
-  getOneRegister () {
-
+  async getOneRegister () {
+    try {
+      const { data: oneCharacter } = await this.APIRequest.get('characters/:id')
+      console.log('response :>> ', oneCharacter);
+      return oneCharacter;
+    } catch (error) {
+      console.log(error);
+      
+    }
+  
   }
 
-  createOneRegister () {
-
+  async createOneRegister () {
+    try {
+      const { body: oneCharacter } = await this.APIRequest.post('/characters')
+      console.log('response :>> ', oneCharacter);
+      return oneCharacter;
+    } catch (error) {
+      console.log(error);
+      
+    }
+  
   }
 
   updateOneRegister () {
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,12 +1,12 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    const characters = await charactersAPI.getFullList()
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    const charactersId = await charactersAPI.getOneRegister()
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
