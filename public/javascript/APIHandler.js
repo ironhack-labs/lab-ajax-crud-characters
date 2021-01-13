cd6f8cdc0783820d15c1177a165502723b67e954
Ashley Vermaas <ashleyvermaas1@gmail.com>
2021-01-13 14:01:48 +0100

    complete iteration 1 and 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,45 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList() {
+    axios
+      .get(this.BASE_URL + '/characters')
+      .then((responseFromApi) => {
+        const data = responseFromApi.data;
+      })
+      .catch((error) => console.log(`There was an error while getting all of the characters: ${error}`));
   }
 
-  getOneRegister () {
-
+  getOneRegister() {
+    axios
+      .get(this.BASE_URL + `/characters/${id}`)
+      .then((responseFromApi) => {
+        const data = responseFromApi.data;
+      })
+      .catch((error) => console.log(`There was an error while getting the character: ${error}`));
   }
 
-  createOneRegister () {
-
+  createOneRegister() {
+    axios
+    .post(this.BASE_URL + '/characters', newCharacter)
+    .then()
+    .catch((error) => console.log(`There was an error while creating the character: ${error}`));
   }
 
-  updateOneRegister () {
-
+  updateOneRegister() {
+    axios
+    .put(this.BASE_URL + `/characters/${id}`)
+    .then()
+    .catch((error) => console.log(`There was an error while updating the character: ${error}`));
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    axios
+    .delete(this.BASE_URL + `/characters/${id}`)
+    .then()
+    .catch((error) => console.log(`There was an error while deleting the character: ${error}`));
   }
 }
+
