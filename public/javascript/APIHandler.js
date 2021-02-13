93a7968cc16a67e304871382135ec0933882f5fa
Ines de Palma <ipalmamasaveu@gmail.com>
2021-02-13 11:40:43 +0100

    First Commit

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,10 +1,16 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = baseUrl
+    axios.defaults.baseURL = this.BASE_URL
+    
+   
   }
 
   getFullList () {
-
+    axios.get(`/characters`)
+    .then((charactersList) => {
+      console.log(charactersList)
+    })
   }
 
   getOneRegister () {
