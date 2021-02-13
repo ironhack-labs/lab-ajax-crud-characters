eeeb687460f2ad26030137e0ec77ebe66e721f50
Ángela Herrador <angelaherrador1991@gmail.com>
2021-02-13 14:46:58 +0100

    Done Fetch all characters

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,3 +1,29 @@
+const drawCard = (character) => {
+  const {id, name, occupation, cartoon, weapon} = character
+  const element = document.querySelector(`.characters-container`)
+  element.innerHTML += `<div>
+  <p><b>Id:</b> ${id}<p>
+  <p><b>Name:</b> ${name}<p>
+  <p><b>Occupation:</b> ${occupation}<p>
+  <p><b>Is a Cartoon?:</b> ${cartoon}<p>
+  <p><b>Weapon:</b> ${weapon}<p>
+  </div>
+  `
+  const elementDelete = document.querySelector(`.character-info`)
+  elementDelete.classList.add("hide")
+}
+
+const drawCards = (items) => {
+  items.forEach(element => {
+    drawCard(element)
+  });
+}
+
+// const charName = document.querySelector('.name')
+// const charOccupation = document.querySelector('.occupation')
+// const charCartoon = document.querySelector('.cartoon')
+// const charWeapon = document.querySelector('.weapon')
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
@@ -5,11 +31,13 @@ class APIHandler {
 
   getFullList () {
     axios
-      .get(`${this.BASE_URL} / characters`)
+      .get(`${this.BASE_URL}/characters`)
       .then((response) => {
-      console.log(response.data)
+        const characters = response.data
+        //console.log (characters)
+        drawCards(characters)
       })
-      .catch((e) => showPopup(e))
+      .catch((e) => console.log (e))
   }
 
   getOneRegister (id) {
