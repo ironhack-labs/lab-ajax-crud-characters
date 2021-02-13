18a2d3bc3e198d5a7d314033edf5ffa27b58d677
Manuel Carrillo Almoguera <manucaralmo@gmail.com>
2021-02-13 12:11:43 +0100

    First commit

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -5,21 +5,64 @@ class APIHandler {
 
   getFullList () {
 
+    const charactersContainer = document.querySelector('.characters-container')
+
+    axios
+      .get(`${this.BASE_URL}/characters`)
+      .then(res => {
+
+        // return res.data
+
+        res.data.forEach(e => {
+          
+          let element = document.createElement("div");
+          element.className = "character-info";
+          element.innerHTML = `
+            <div class="name"><b>Name:</b> ${e.name}</div>
+            <div class="occupation"><b>Occupation:</b> ${e.occupation}</div>
+            <div class="cartoon"><b>Cartoon:</b> ${e.cartoon}</div>
+            <div class="weapon"><b>Weapon:</b> ${e.weapon}</div>
+          `
+          charactersContainer.appendChild(element)
+
+        });
+      })
+      .catch(e => console.log(e))
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    axios
+    .get(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
   }
 
-  createOneRegister () {
-
+  createOneRegister (body) {
+    axios
+    .post(`${this.BASE_URL}/characters`, body)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (body) {
+    axios
+    .put(`${this.BASE_URL}/characters`, body)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios
+    .delete(`${this.BASE_URL}/characters/${id}`)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,15 +2,15 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister(1) // pasarle el valor real
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(1) // pasarle el valor real
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
@@ -18,6 +18,6 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    
   });
 });
