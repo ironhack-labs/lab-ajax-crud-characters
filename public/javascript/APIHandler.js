8b7f0f23a332e2dd2cdc9cb10dd7f42b4725b69b
luisga32 <luisga32@hotmail.com>
2021-02-16 20:57:25 +0100

    fetch all done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,46 @@ class APIHandler {
   }
 
   getFullList () {
+    
+      return axios.get(`${this.BASE_URL}/characters`)
+   
+  }
+
+  getOneRegister (id) {
+    axios.get(`http://localhost:8000/characters/${id}`)
+    .then((character) => {
+
+    })
+    .catch(err => console.log(err))
 
   }
 
-  getOneRegister () {
+  createOneRegister (newCharacter) {
+    
+    axios.post('http://localhost:8000/characters',newCharacter)
+    .then((character) => {
+     // console.log(characters)
+
+    })
+    .catch(err => console.log(err))
 
   }
 
-  createOneRegister () {
+  
+
+  updateOneRegister (character) {
+    axios.put(`http://localhost:8000/characters/${character.id}`,character)
+    .then((character) => {
+      
+    })
 
   }
 
-  updateOneRegister () {
+  deleteOneRegister (id) {
+    axios.delete(`http://localhost:8000/characters/${id}`)
+    .then()
+    .catch(err => console.log(err))
 
-  }
-
-  deleteOneRegister () {
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,33 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
+
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const charactersContainer = document.querySelector(".characters-container");
+    charactersAPI.getFullList()
+      .then((characters) => {
+        charactersContainer.innerHTML = ""
+        //console.log(characters.data);
+    
+        characters.data.forEach(character => {
+        let  characterInfo = document.createElement('div')
+        characterInfo.className = 'character-info'
+          characterInfo.innerHTML = `
+      <div class="name">Name: ${character.name}</div>
+      <div class="occupation">occupation:${character.occupation}</div>
+      <div class="cartoon">Cartoon: ${character.cartoon}</div>
+      <div class="weapon">weapon: ${character.weapon}</div>
+     `
+    
+           charactersContainer.appendChild(characterInfo)
+
+        })
+        console.log(`al final de getFullList`)
+      
+      })
+      .catch((e) => console.log(e))
 
   });
 
