5ce4caae4f413e77326a3b48bf0c40a6fd6cf217
luisga32 <luisga32@hotmail.com>
2021-02-16 22:54:03 +0100

    Create done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -70,5 +70,24 @@ window.addEventListener('load', () => {
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
 
+    event.preventDefault()
+
+    const newCharacter = {
+      name: event.target[0].value,
+      occupation: event.target[1].value,
+      weapon: event.target[2].value,
+      cartoon: event.target[3].checked
+    }
+
+    charactersAPI
+      .createOneRegister(newCharacter)
+      .then(res => {
+        const buttonCreate = document.getElementById('send-data');
+        buttonCreate.style.background = "Green"
+       
+      })
+      .catch(e => {  const buttonCreate = document.getElementById('send-data');
+      buttonCreate.style.background = "Red" })
+
   });
 });
