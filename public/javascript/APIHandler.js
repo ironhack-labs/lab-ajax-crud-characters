425bda5c0308ee18b9c8ce27d7fdf896b27a0019
Alexis Verthuy <alexis.verthuy@gmail.com>
2021-03-04 18:41:12 +0100

    lab to be completed

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,26 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList() {
+    return axios.get(this.BASE_URL + "/characters")
+  }
+
+  getOneRegister() {
+    return axios.get(this.BASE_URL + "/characters/:id")
+  }
+
+  createOneRegister() {
+    return axios.post(this.BASE_URL + "/characters")
+  }
+
+  updateOneRegister() {
+    return axios.patch(this.BASE_URL + "/characters/:id")
 
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister() {
+    return axios.delete(this.BASE_URL + "/characters/:id")
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,43 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  window.addEventListener('load', () => {
+    document
+      .getElementById('fetch-all')
+      .addEventListener('click', function (event) {
+        charactersAPI.getFullList()
+          .then((apiRes) => {
+            console.log(apiRes.data);
+            // insert apiRes.data into index.html ==> characters-container
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      });
 
+
+    document
+      .getElementById('fetch-one')
+      .addEventListener('click', function (event) {
+        charactersAPI.getOneRegister()
+        .then((apiRes2) => {
+          console.log(apiRes2.data);
+          // insert apiRes.data into index.html ==> characters-container
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      });
+
+    document.getElementById('delete-one').addEventListener('click', function (event) {
+
+    });
+
+    document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+
+    });
+
+    document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+    });
   });
-
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
-});
