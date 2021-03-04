40f8beb48aa314e532fe5788f4d3d43027b0569a
Amalia Zebi <amalia.zebi@b4finance.com>
2021-03-04 19:42:09 +0100

    Difficult to understand the dom manipulation with methods

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,22 @@ class APIHandler {
   }
 
   getFullList () {
-
+return axios.get(this.BASE_URL + "/characters")
   }
 
   getOneRegister () {
-
+    return axios.get(this.BASE_URL + "/characters/:id")
   }
 
   createOneRegister () {
-
+    return axios.post(this.BASE_URL + "/characters")
   }
 
   updateOneRegister () {
-
+    return axios.patch(this.BASE_URL + "/characters/:id")
   }
 
   deleteOneRegister () {
-
+    return axios.delete(this.BASE_URL + "/characters/:id")
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,85 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
+const btnFetchAll = document.getElementById("fetch-all");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      const characterContainer = document.getElementsByClassName(
+        "characters-container"
+      );
+      charactersAPI
+        .getFullList()
+        .then((apiRes) => {
+          //console.log(apiRes.data)
+          characterContainer.innerHTML = "";
 
-  });
+          apiRes.data.forEach((character) => {
+            data = document.createElement(
+              "div"
+            ).innerHTML += `<div class="character-info">
+    <div class="name">${character.name}</div>
+    <div class="occupation">${character.occupation}</div>
+    <div class="cartoon">Cartoon:${character.cartoon}</div>
+    <div class="weapon">${character.weapon}</div>
+    </div>`;
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      charactersAPI.getOneRegister()
+        .then((apiResForOne) => {
+          //console.log(apiResForOne.data);
 
-  });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {
+      charactersAPI.deleteOneRegister()
+        .then((apiResDeleteOne) => {
+          //console.log(apiResDeleteOne.data);
 
-  });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {
+      charactersAPI.updateOneRegister()
+        .then((apiResEditOne) => {
+          //console.log(apiResEditOne.data);
 
-  });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {
+      charactersAPI.createOneRegister()
+        .then((apiResCreateOne) => {
+          //console.log(apiResCreateOne.data);
 
-  });
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    });
 });
