ef2ce8ecdb11e8c1ef3658ea5309a09e15d10c1a
Melisa KMNV <melisakmnv@gmail.com>
2021-05-17 13:46:26 +0200

    not don

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,14 +1,25 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.api = axios.create({baseUrl})
   }
 
   getFullList () {
+    return this.api.get("/characters")
+  
+  
+
+    // axios({
+    //   method: 'get',
+    //   url: 'http://localhost:8000/characters',
+    // })
+    // .then( res => console.log(res.data))
+    // .catch(err => console.error(err))
 
   }
 
   getOneRegister () {
-
+// I want to do this 
+    //   http://localhost:8000/characters/1...
   }
 
   createOneRegister () {
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,15 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+console.log(charactersAPI);
+
+// function displa
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+    
+    // I want to click and display all the info of Character 
+    // I want to display them on HTML body
+    // charactersAPI.getFullList()
 
   });
 
