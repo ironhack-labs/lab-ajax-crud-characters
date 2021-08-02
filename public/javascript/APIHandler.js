90e120242dc0cea4421cfa3454b3fe7c08900113
Luana <luana.nadai.santos@gmail.com>
2021-08-02 14:35:27 -0300

    lab done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,38 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`).then((result) => {
+      return result.data
+    })
   }
 
-  getOneRegister () {
-
+  getOneRegister = async (id) => {
+    const result = await axios.get(`${this.BASE_URL}/characters/${id}`)
+    return result.data
   }
 
-  createOneRegister () {
-
+  createOneRegister = async (character) => {
+    if(!character.name || !character.occupation || !character.weapon){
+      throw "Missing Fields"
+    }
+    if(!character.hasOwnProperty('cartoon')){
+      character.cartoon = false
+    }
+    const result = await axios.post(`${this.BASE_URL}/characters`, character)
+    return result.data
   }
 
-  updateOneRegister () {
-
+  updateOneRegister = async (id, updateCharacter) => {
+    try {
+    const result = await axios.patch(`${this.BASE_URL}/characters/${id}`, updateCharacter)
+    return result.data
+    } catch (error) {
+      throw error
+    }
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister = async (id) => {
+    const result = await axios.delete(`${this.BASE_URL}/characters/${id}`)
+    return result.data
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,101 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+  
+
+  const buildCharacterBox = (character) => {
+
+    const characterCartoonValue = character.cartoon === true ? 'Yes' : 'No'
+
+    const newElement = `
+      <div class="character-info">
+        <div class="name">${character.name} - ID ${character.id}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">Is a Cartoon? ${characterCartoonValue}</div>
+        <div class="weapon">${character.weapon}</div>
+      </div>
+    `
+    document.querySelector('.characters-container').innerHTML += newElement
+  }
+
+  const fetchAll = () => {
+    document.querySelector('.characters-container').innerHTML = ''
+    charactersAPI.getFullList().then(result => {
+      result.forEach(element => {
+        buildCharacterBox(element)
+      })
+    })
+  }
+
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    fetchAll()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
+    document.querySelector('.characters-container').innerHTML = ''
+    const id = document.querySelector('input[name="character-id"]').value
+ 
+    charactersAPI.getOneRegister(id).then((result) => {
+      buildCharacterBox(result)
+    })
 
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    event.preventDefault();
+    
+    // document.querySelector('.characters-container').innerHTML = ''
+    
+    const idDelete = document.querySelector('input[name="character-id-delete"]').value
+    charactersAPI.deleteOneRegister(idDelete).then((result) => {
+      document.querySelector('#delete-one').className='active';
+
+      result = fetchAll();
+    })
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    let id = document.querySelector('#edit-character-form input[name="chr-id"]').value
+    let occupation = document.querySelector('#edit-character-form input[name="occupation"]').value
+    let name = document.querySelector('#edit-character-form input[name="name"]').value
+    let weapon = document.querySelector('#edit-character-form input[name="weapon"]').value
+    let cartoon = document.querySelector('#edit-character-form input[name="cartoon"]').checked
+
+
+    const updatedCharacter = {
+      name, 
+      occupation,
+      weapon,
+      cartoon
+    }
+
+    charactersAPI.updateOneRegister(id, updatedCharacter).then((result) => {
+      return result
+    })
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
 
+    let occupation = document.querySelector('#new-character-form input[name="occupation"]').value
+    let name = document.querySelector('#new-character-form input[name="name"]').value
+    let weapon = document.querySelector('#new-character-form input[name="weapon"]').value
+    let cartoon = document.querySelector('#new-character-form input[name="cartoon"]').checked
+
+    let newCharacter = {
+      name,
+      occupation,
+      weapon,
+      cartoon,
+    }
+
+    charactersAPI.createOneRegister(newCharacter).then((result) => {
+      return result
+    })
   });
 });
