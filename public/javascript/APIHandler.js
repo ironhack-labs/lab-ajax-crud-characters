956aed4f10a8236c439bf5883141abe797b69d7a
John Doe <gustavo.sena0@gmail.comGustavo Barbosa Sena>
2021-08-02 10:07:31 -0300

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList = async () => {
+
+    const result = await axios.get(`${this.BASE_URL}/characters`)
+    return result.data
 
   }
 
-  getOneRegister () {
+  getOneRegister = async (id) => {
+    const result = await axios.get(`${this.BASE_URL}/characters/${id}`)
+    return result.data
+  }
+
+  createOneRegister = async (newCharacter) => {
+    if (!newCharacter.name) {
+      throw 'error'
+    }
+    if (!newCharacter.hasOwnProperty('cartoon')) {
+      newCharacter.cartoon = false
+    }
+
+    const result = axios.post(`${this.BASE_URL}/characters`, newCharacter)
+    return result.data
+  }
+
+  updateOneRegister = async (id, updatedCharacter) => {
+
+    try {
+      const result = axios.put(`${this.BASE_URL}/characters/${id}`, updatedCharacter)
+      return result.data
+    } catch (error) {
+      throw error
+    }
 
   }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister = async (id) => {
+    const result = await axios.delete(`${this.BASE_URL}/characters/${id}`)
+    return result.data
   }
 }
