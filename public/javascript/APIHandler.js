696c402170faebbd33d32e9e211e42bdc934be33
thsc47 <thsc47@gmail.com>
2021-08-30 21:37:53 -0300

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,48 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList = async () => {
+    const {data} = await axios.get(`${this.BASE_URL}/characters`);
+    return data;
+  };
 
-  }
+  getOneRegister = async (characterid) => {
+    const {data} = await axios.get(`${this.BASE_URL}/characters/${characterid}`);
+    return data;
+  };
 
-  getOneRegister () {
+  createOneRegister = async (newCharacter) => {
+    if (
+      !newCharacter.name ||
+      !newCharacter.occupation ||
+      !newCharacter.weapon
+    ) {
+      throw "Missing Fields";
+    }
+    if (!newCharacter.hasOwnProperty("cartoon")) {
+      newCharacter.cartoon = false;
+    }
+    const {data} = await axios.post(
+      `${this.BASE_URL}/characters`,
+      newCharacter
+    );
+    return data;
+  };
 
-  }
+  updateOneRegister = async (id, updatedCharacter) => {
+    try {
+      const {data} = await axios.put(
+        `${this.BASE_URL}/characters/${id}`,
+        updatedCharacter
+      );
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  };
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister = async (id) => {
+    const {data} = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+    return data;
+  };
 }
