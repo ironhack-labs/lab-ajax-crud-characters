b7fd481638acfd8609b2eb777122f0682b1c1825
Juandi Mena <juandimena34@gmail.com>
2021-09-09 22:43:32 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,16 @@
+const axios = require("axios");
+
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({baseUrl : this.BASE_URL});
   }
 
-  getFullList () {
-
-  }
-
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  getFullList = () => this.api.get("/characters");
+  getOneRegister = (id) => this.api.get(`/characters/${id}`);
+  createOneRegister = (body) => this.api.post("/characters", body);
+  updateOneRegister = (id) => this.api.put(`/characters/${id}`);
+  deleteOneRegister = (id) => this.api.delete(`/characters/${id}`);
 }
+
+module.exports = APIHandler;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,56 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const APIHandler = require("./APIHandler");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-  });
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      const container = document.getElementsByClassName("characters-container");
+      charactersAPI
+        .getFullList()
+        .then((response) => {
+          console.log(response);
+          const characterData = response.data;
+          console.log(characterData);
+          for (const element of characterData) {
+            let character = document.createElement("div");
+            let charName = document.createElement("div");
+            charName.innerText = element.name;
+            console.log(element.name);
+            let charOccupation = document.createElement("div");
+            charOccupation.innerText = element.occupation;
+            let charCartoon = document.createElement("div");
+            charCartoon.innerText = element.cartoon;
+            let charWeapon = document.createElement("div");
+            charWeapon.innerText = element.weapon;
+            character.append(charName, charOccupation, charCartoon, charWeapon);
+          }
+          container.appendChild(character);
+        })
+        .catch((err) => console.log(err));
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      //   const inputId = document.getElementsByName("character-id")
+      //  const id = inputId[0].value
+      // charactersAPI.getOneRegister(id)
+      //   .then(response => {
+      //     const charName = document.getElementsByClassName("name")
+      //     charName[0].innerText += response.data.name
+    });
 
-  });
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
 });
