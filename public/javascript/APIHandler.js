d9102450b61ed40f40ba5915e91c28cc1f86f4f5
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-09-10 00:45:34 +0200

    Features: Fighting with create

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,18 +1,36 @@
+
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    this.api = axios.create({ baseURL: baseUrl });  }
 
   getFullList () {
-
+  /*     GET, Route: "/characters"
+  It receives NO parameters
+  It returns the full characters list
+  It returns JSON */
+  return this.api.get('/characters')
+    .then((response) => {
+    const allCharInfo = response.data;
+    return allCharInfo
+    }) 
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+  /*   Verb: GET, Route: "/characters/:id"
+    It receives the character ID as a parameter (route)
+    It returns the character with the indicated id
+    It returns JSON  */
+    return this.api.get(`/characters/${id}`)
+    .then((response) => {
+    console.log(response);
+    return ("ok")
+    //const oneCharInfo = response.data;
+    //return oneCharInfo
+    }) 
   }
 
-  createOneRegister () {
-
+  createOneRegister (character) {
+    character => this.api.post(`/characters`, character)
   }
 
   updateOneRegister () {
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,11 +2,14 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    console.log ("hi")
+    console.log (charactersAPI.getFullList ()) 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    let id = document.getElementById('one').value;
+    //console.log("id: " , id)
+    console.log (charactersAPI.getOneRegister(id))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
@@ -18,6 +21,11 @@ window.addEventListener('load', () => {
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    const name = document.getElementById(`new-name`).value
+    const occupation = document.getElementById(`new-occupation`).value
+    const weapon = document.getElementById(`new-weapon`).value
+    const cartoon = document.getElementById('new-cartoon').checked;
+    const char = {name, occupation, weapon, cartoon}
+    charactersAPI.createOneRegister(char)
   });
 });
