658d9acd247006a244823958cf04a4a563737ae3
karlajaramillo <karlav.jaramillo@gmail.com>
2021-11-30 17:03:31 +0100

    Iteration 2 and 3 - only console log and postman ok

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,44 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    // this.BASE_URL = baseUrl;
+    this.api = axios.create({ baseURL: baseUrl });  }
+  
+  //function declaration
+  // It receives NO parameters
+  // It returns the full characters list
+  // It returns JSON
+  // Get all the characters info from http://localhost:8000/characters
+  // getFullList () {
+  //   return this.api.get('/characters');
+  // }
+  // arrow function
+  getFullList = () => this.api.get('/characters');
 
-  getFullList () {
+  //Get a single character info from http://localhost:8000/characters/:id
+  //   Verb: GET, Route: "/characters/:id"
+  // It receives the character ID as a parameter (route)
+  // It returns the character with the indicated id
+  // It returns JSON
+  // getOneRegister (charId) {
+  // }
+  getOneRegister = (id) => this.api.get(`/characters/${id}`);
 
-  }
+  // Verb: POST, Route: "/characters"
+  // It receives an object as a parameter, with the following format: { name: string, occupation: string, cartoon: boolean, weapon: string }
+  // It returns the created character if there are no errors
+  // It returns the wrong fields if there is some error
+  // It returns JSON
+  createOneRegister = (char) => this.api.post(`/characters`, char);
 
-  getOneRegister () {
+  //   Verb: PATCH/PUT, Route: "/characters/:id"
+  // It receives the character id as a parameter (route)
+  // It receives an object as a parameter, with the following format: { name: string, occupation: string, cartoon: boolean, weapon: string }
+  // All the fields are optional
+  // It returns the updated character if there are no errors
+  // It returns "Character not found" if there is no character with the indicated id
+  // It returns JSON / text
+  updateOneRegister = (id, charData) => this.api.put(`/characters/${id}`, charData);
+  
+  deleteOneRegister = (id) => this.api.delete(`/characters/${id}`);
 
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,47 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-all').addEventListener('click', async (event) => {
+    const allCharacters = await charactersAPI.getFullList();
+    // json with all the characters
+    const result = allCharacters.data;
+    console.log(result)
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async (event) => {
+    const id = document.querySelector('#characterId').value;
+    console.log(id)
+    const char = await charactersAPI.getOneRegister(id);
+    console.log(char.data);
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async (event) => {
+    const id = document.querySelector('#deleteId').value;
+    console.log(id);
+    await charactersAPI.deleteOneRegister(id);
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('edit-character-form').addEventListener('submit', async  (event) => {
+    const id = document.querySelector('#edit-id').value;
 
+    const name = document.querySelector('#edit-name').value;
+    const occupation = document.querySelector('#edit-occupation').value;
+    const weapon = document.querySelector('#edit-weapon').value;
+    const cartoon = document.querySelector('#edit-cartoon').checked; 
+    
+    const charData = { name, occupation, weapon, cartoon };
+    const updated = await charactersAPI.updateOneRegister(id, charData);
+    console.log(updated);
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('new-character-form').addEventListener('submit', async (event) => {
+    const name = document.querySelector('#new-name').value;
+    const occupation = document.querySelector('#new-occupation').value;
+    const weapon = document.querySelector('#new-weapon').value;
+    const cartoon = document.querySelector('#new-cartoon').checked; 
+    
+    const charData = { name, occupation, weapon, cartoon };
+    const updated = await charactersAPI.createOneRegister(charData);
+    console.log(updated);
   });
 });
