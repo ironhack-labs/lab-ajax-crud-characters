9d86342645c4242b8fd6f7a666e46547ba178d7a
Corentin Aesch <corentin.aesch@gmail.com>
2022-02-10 19:59:03 +0100

    in progress

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,22 @@ class APIHandler {
   }
 
   getFullList () {
-
+    return axios.get(`${this.BASE_URL}/characters`);
   }
 
   getOneRegister () {
-
+    return axios.get(`${this.BASE_URL}/${characterId}`);
   }
 
   createOneRegister () {
-
+    return axios.post(`${this.BASE_URL}/characters`, characterObj);
   }
 
   updateOneRegister () {
-
+    return axios.patch(`${this.BASE_URL}/${characterId}`, characterObj);
   }
 
   deleteOneRegister () {
-
+    return axios.delete(`${this.BASE_URL}/characters/${characterId}`);
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,9 +1,12 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const charContainer = document.querySelector('.characters-container');
+const idSearchForm = document.querySelector('input[name="character-id"]');
+const idDeleteForm = document.querySelector('input[name="character-id-delete"]');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
-  });
+  
+});
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
