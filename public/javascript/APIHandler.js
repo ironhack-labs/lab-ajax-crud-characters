6df32d7e20b45e124d3fa463da137f856ce30052
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-23 11:07:56 +0100

    Iteration 3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -5,41 +5,41 @@ class APIHandler {
 
   async getFullList () {
     try{
-      console.log(await axios.get(`${this.BASE_URL}/characters`));
+      return await axios.get(`${this.BASE_URL}/characters`);
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 
   async getOneRegister (id) {
     try {
-      console.log(await axios.get(`${this.BASE_URL}/characters/${id}`));
+      return await axios.get(`${this.BASE_URL}/characters/${id}`);
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 
   async createOneRegister (register) {
     try {
-      console.log(await axios.post(`${this.BASE_URL}/characters`, register));
+      return await axios.post(`${this.BASE_URL}/characters`, register);
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 
   async updateOneRegister (id, register) {
     try {
-      console.log(await axios.put(`${this.BASE_URL}/characters/${id}`, register));
+      return await axios.put(`${this.BASE_URL}/characters/${id}`, register);
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 
   async deleteOneRegister (id) {
     try {
-      console.log(await axios.delete(`${this.BASE_URL}/characters/${id}`));
+      return await axios.delete(`${this.BASE_URL}/characters/${id}`);
     } catch (error) {
-      console.log(error);
+      return error;
     }
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,40 +1,114 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
-    const resFromApi = charactersAPI.getFullList();
+
+  function newContainer(container){
+    const { id, name , occupation , cartoon , weapon } = container;
+    return `<div class="characters-container">
+    <div class="character-info">
+      <div class="id">Id: <span>${id}</span></div>
+      <div class="name">Name: <span>${name}</span></div>
+      <div class="occupation">Occupation: <span>${occupation}</span></div>
+      <div class="cartoon">Is a Cartoon? <span>${cartoon}</span></div>
+      <div class="weapon">Weapon: <span>${weapon}</span></div>
+    </div>
+  </div>`;
+  }
+
+  document.getElementById('fetch-all').addEventListener('click', async function (event) { 
+    const buttonStyle = event.target.style;
+    const resFromApi = await charactersAPI.getFullList();
+    if('status' in resFromApi){
+      const section = document.getElementById('fetch-data');
+      section.innerHTML = "";
+      [...resFromApi.data].forEach(element => {
+        const html = newContainer(element);
+        section.innerHTML += html;
+      });
+      buttonStyle.backgroundColor = "green";
+    } else {
+      buttonStyle.backgroundColor = "red";
+    }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    const buttonStyle = event.target.style;
     const fetchOneId = document.getElementsByName('character-id')[0].value;
-    const resFromApi = charactersAPI.getOneRegister(fetchOneId);
+    const section = document.getElementById('fetch-data');
+    if(fetchOneId!==""){
+      const resFromApi = await charactersAPI.getOneRegister(fetchOneId);
+      if('status' in resFromApi){
+        const html = newContainer(resFromApi.data);
+        section.innerHTML = html;
+        buttonStyle.backgroundColor = "green";
+      } else {
+        buttonStyle.backgroundColor = "red";
+      }
+    } else {
+      section.innerHTML="";
+      buttonStyle.backgroundColor = "red";
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    const buttonStyle = event.target.style;
     const deleteOneId = document.getElementsByName('character-id-delete')[0].value;
-    charactersAPI.deleteOneRegister(deleteOneId);
+    if(deleteOneId!==""){
+      const resFromApi = await charactersAPI.deleteOneRegister(deleteOneId);
+      if('status' in resFromApi){  
+        buttonStyle.backgroundColor = "green";
+      } else {
+        buttonStyle.backgroundColor = "red";
+      }
+    } else {
+      buttonStyle.backgroundColor = "red";
+    }
   });
 
-  document.getElementById('send-data').addEventListener('click', function (event) {
-    const editForm = document.getElementById('edit-character-form');
-    const [ id, name, occupation, weapon, checkbox ] = editForm.getElementsByTagName('input');
-    charactersAPI.updateOneRegister(id.value, {
-      name: name.value,
-      occupation: occupation.value,
-      cartoon: checkbox.checked,
-      weapon: weapon.value
+  document.getElementById('edit-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const editForm = event.target;
+    const buttonStyle = editForm.getElementsByTagName('button')[0].style;
+    const [
+      { value: id },
+      { value: name },
+      { value: occupation },
+      { value: weapon },
+      { checked: cartoon }
+    ] = editForm.getElementsByTagName('input');
+    const resFromApi = await charactersAPI.updateOneRegister(id, {
+      name,
+      occupation,
+      cartoon,
+      weapon
     });
+    if('status' in resFromApi){  
+      buttonStyle.backgroundColor = "green";
+    } else {
+      buttonStyle.backgroundColor = "red";
+    }
   });
 
-  document.getElementById('create-data').addEventListener('click', function (event) {
-    const newForm = document.getElementById('new-character-form');
-    const [ name, occupation, weapon, checkbox ] = newForm.getElementsByTagName('input');
-    console.log(newForm);
-    charactersAPI.createOneRegister({
-      name: name.value,
-      occupation: occupation.value,
-      cartoon: checkbox.checked,
-      weapon: weapon.value
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const newForm = event.target;
+    const buttonStyle = newForm.getElementsByTagName('button')[0].style;
+    const [
+      { value: name },
+      { value: occupation },
+      { value: weapon },
+      { checked: cartoon }
+    ] = newForm.getElementsByTagName('input');
+    resFromApi = await charactersAPI.createOneRegister({
+      name,
+      occupation,
+      cartoon,
+      weapon
     });
+    if('status' in resFromApi){  
+      buttonStyle.backgroundColor = "green";
+    } else {
+      buttonStyle.backgroundColor = "red";
+    }
   });
 });
