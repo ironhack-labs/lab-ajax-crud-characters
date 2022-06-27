6c8b2a77afa84f8ff89b26f279772fcee8baecfd
zarahsara <sarahabbasi25@aol.com>
2022-06-27 22:16:21 +0100

    Lab done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,72 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  getFullList (character) {
+     
+    axios({
+     method: 'GET',
+    url: `https://characters.com/v2/name/${character}`,
+  }).then ((responseFromAxios) => {
+    const ThecharacterFromTheApi = responseFromAxios.data[0];
 
-  }
 
-  getOneRegister () {
+  
+    const characterName = document.querySelector('#character-name');
+    characterName.innerText = `The name of the character: ${TheCharacterFromTheApi.name}`;
 
-  }
+ 
+    const characterOccupation = document.querySelector('#character-occupation');
+    characterOccupation.innerText = `Occuption: ${TheCharacterFromTheApi.occupation}`;
+
+    const cartoon = document.querySelector('#isa-cartoon');
+    cartoon.innertext = ThecharacterFromTheApi.cartoon;
+    
+    const characterWeapon = document.querySelector('#character-Weapon');
+    characterWeapon.innerText = `Weapon: ${TheCharacterFromTheApi.Weapon}`;
+
+     return this.APIHandler.get('/characters');
+  });
+};
+ 
+
+  getOneRegister (id) {
+   axios.get('/characters')
+   .then(response => console.log(`All characters are: `, response.data))
+   .catch(error => console.log(error));
+
+  axios
+   .get('/characters/id')
+   .then(response => console.log(`Character with ID is:`, response.data))
+   .catch(error => console.log(error));
+
+   
+    return this.api.get(`/characters/${id}`);
+
+  };
 
   createOneRegister () {
+  axios.post('/characters');
+  .then(response => console.log(`Character with ID is:`, response.data ))
+  .catch(error => console.log(error));   
+   return this.api.post('/characters', NewUserInfoFromReqBody);
+ };
 
-  }
+  updateOneRegister (id, updateOneRegister) {
+    axios
+   .put('/characters/id')
+   .then(response => console.log(`Character with ID is:`, response.data))
+   .catch(error => console.log(error));
+   return this.api.put(`/characters/${id}`, updateOneRegister);
+  };
 
-  updateOneRegister () {
+  deleteOneRegister (id) {
 
-  }
-
-  deleteOneRegister () {
-
-  }
+    axios
+    .delete('/characters/id')
+    .then(response => console.log(`Character with ID is:`, response.data))
+    console.log('character delete');
+    return this.api.delete(`/characters/${id}`);
+  };
 }
+
+module.exports = APIHandler;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,21 +3,43 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
 
-  });
+    const input = document.querySelector('#character-name-input');
+    APIHandler.getFullList(input.value);
+    
+
+   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
-  });
+const input = document.querySelector('#character_id');
+APIHandler.getOneRegister(input.value);
+});
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    
+  const input = document.querySelector('#character_id');
+APIHandler.delete(input.value);
 
-  });
+
+ });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
-  });
+
+    
+      event.preventDefault();
+      document.getElementById('/characters/${id}`, updateOneRegister');
+    
+    });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    document.getElementById('/characters/${id}`, updateOneRegister');
 
-  });
+    const name = document.querySelector('.new-character-form input[type=text]');
+    const Occuption = document.querySelector('.new-character-form input[type=text]');
+    const cartoon = document.querySelector('.new-character-form input[type=text]');
+    const Weapon = document.querySelector('.new-character-form input[type=text]');
+   charactersAPI.createOneRegister();
+ });
 });
