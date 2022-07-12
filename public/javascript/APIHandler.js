b9c14ceef6df0bc50cff85903da8d91cc8c340ed
Moisés Vargas Rangel <vargasrangelaudio@gmail>
2022-07-12 11:10:01 -0500

    Iteracion pendiente

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,13 +1,24 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl
+    });
   }
 
   getFullList () {
-
+    return this.api.get("/characters")
+      .then(informacion => {
+          return informacion.data
+      })
+      .catch(console.log)
   }
 
-  getOneRegister () {
+  getOneRegister (id) {
+    return this.api.get(`/characters/${id}`)
+      .then(informacion => {
+          return informacion.data
+      })
 
   }
 
