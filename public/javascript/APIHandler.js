ba636a7d78bd003e9597a4955730ee1184f82e5f
Val Mejia <valemegaestrellalectora@gmail.com>
2022-07-19 18:50:16 -0500

    :tada:

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,14 +1,23 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl
+    });
   }
 
   getFullList () {
-
+   return this.api.get('/characters')
+    .then( informacion  => {
+      return informacion.data
+    }).catch(console.log)
   }
 
-  getOneRegister () {
-
+  getOneRegister (id) {
+    return this.api.get(`/characters/$(id`)
+    .then( informacion  => {
+      return informacion.data
+    }).catch(console.log)
   }
 
   createOneRegister () {
