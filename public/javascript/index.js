9b26f1425016a2f2b62c58361e4dd458059bbf08
lzaquine <zaquinelucas@gmail.com>
2022-08-05 09:06:26 +0100

    Not done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,30 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
+  
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+    .then((response) => {
+      
+    })
+      
+  
 
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    charactersAPI.getOneRegister()
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister()
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.updateOneRegister()
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    charactersAPI.createOneRegister()
   });
 });
