57aac289fae14dbaeee0447ee1873bf7cafb68b5
Jose Rios <jrbernabe@gmail.com>
2022-10-09 19:28:32 -0400

    Finish lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,50 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({ baseURL: this.BASE_URL });
   }
 
-  getFullList () {
-
+  async getFullList() {
+    try {
+      const fullList = await this.api.get(`/characters`);
+      return fullList;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  async getOneRegister(id) {
+    try {
+      const oneRegister = await this.api.get(`/characters/${id}`);
+      return oneRegister;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  getOneRegister () {
-
+  async createOneRegister(obj) {
+    try {
+      const oneRegister = await this.api.post(`/characters`, obj);
+      return oneRegister;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  createOneRegister () {
-
+  async updateOneRegister(id, obj) {
+    try {
+      const oneRegister = await this.api.put(`/characters/${id}`, obj);
+      return oneRegister;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  async deleteOneRegister(id) {
+    try {
+      const oneRegister = await this.api.delete(`/characters/${id}`);
+      return oneRegister;
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
