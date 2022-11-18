3c3b661df4a59ed75fe63667e324e23dfe81b83e
Ezequiel Benitez <ezequielbv@gmail.com>
2022-11-18 10:07:25 +0100

    Update card loop in the index.js

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,20 +1,5 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
-function createCard(character) {
-  
-  let { id, name, occupation, cartoon, weapon} = character;
-
-  let characterCard = `<div class="character-info">
-        <div class="name">Name: <span>${name}</span></div>
-        <div class="occupation">Occupation: <span>${occupation}</span></div>
-        <div class="cartoon">Is Cartoon?: <span>${cartoon}</span></div>
-        <div class="weapon">Weapon: <span>${weapon}</span></div>
-      </div>`
-  // console.log(characterCard);
-  return characterCard;
-}
-
-
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
     charactersAPI.getFullList()
@@ -22,22 +7,13 @@ window.addEventListener('load', () => {
         // console.log("Fetch all", response.data);
         const data = response.data;
         console.log("obj",data.length)
+
+        let card = ``;
         data.forEach(character => {
-          console.log("loop",character);
-
-          let { id, name, occupation, cartoon, weapon} = character;
-          let card = `<div class="character-card">
-          <p>Id: <span id="all-fetched-id" class="all-character fetched">${id}</span></p>
-          <p>Name: <span id="all-fetched-name" class="all-character fetched">${name}</span></p>
-          <p>Ocupation: <span id="all-fetched-occupation" class="all-character fetched">${occupation}</span></p>
-          <p>Is a Cartoon?: <span id="all-fetched-cartoon" class="all-character fetched">${cartoon}</span></p>
-          <p>Weapon: <span id="all-fetched-weapon" class="all-character fetched">${weapon}</span></p></div>`;
-
-          const cardContainer = document.createElement('div');
-          cardContainer.innerHTML = card;
-          document.getElementById('characters-container').appendChild(cardContainer);
-
+          // console.log("loop",character);
+          card += createCard(character);
         })
+        document.querySelector('.characters-container').innerHTML = card;
       })
       .catch(err => console.log(err));
   });
@@ -108,5 +84,14 @@ window.addEventListener('load', () => {
   });
 });
 
-
-
+function createCard(character) {
+  let { id, name, occupation, cartoon, weapon} = character;
+  let characterCard = `<div class="character-info">
+        <div class="name">Name: <span>${name}</span></div>
+        <div class="occupation">Occupation: <span>${occupation}</span></div>
+        <div class="cartoon">Is Cartoon?: <span>${cartoon}</span></div>
+        <div class="weapon">Weapon: <span>${weapon}</span></div>
+      </div>`
+  // console.log(characterCard);
+  return characterCard;
+}
