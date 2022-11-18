597f2a6d55b3e5ce1f7b771f62998e57759544f1
Vika Shcherbak <vikashcherbaka@gmail.com>
2022-11-18 10:15:15 +0100

    WIP

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,39 @@
+
+// const api = axios.create({
+//   baseURL: 'http://localhost:8000'
+// });
+
+
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor () {
+   this.api = axios.create({
+    baseURL: 'http://localhost:8000'
+  });
   }
 
   getFullList () {
-
+    this.api
+    .get('/characters')
+    .then(response => console.log(response.data))
+    .catch(error => console.log(error));
   }
 
-  getOneRegister () {
-
+  getOneRegister (characterId) {
+    return this.api.get(`/characters/${characterId}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister (charInf) {
+    console.log(charInf)
+    return this.api.post(`/characters`, charInf ); 
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (characterId, charInf) {
+    return this.api.put(`/characters/${characterId}`, charInf);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (characterId) {
+    return this.api.delete(`/characters/${characterId}`);
   }
 }
+
+//module.exports = APIHandler;
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,45 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler();
+
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
-
+    charactersAPI.getFullList()
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    let characterId = document.querySelector('#goya').value
+    console.log(characterId)
+    charactersAPI.getOneRegister(characterId)
+    .then(res => console.log(res.data))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    let a =
+    let b = 
+    let c =
 
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(event.target)
+    let a = document.querySelector('#goya-one').value;
+    let b = document.querySelector('#goya-two').value;
+    let c = document.querySelector('#goya-tree').value;
+    //let d = document.querySelector('#goya-four').value;
+    let newCar = {
+      "name": a,
+      "occupation": b,
+      "weapon": c, 
+    }
+    console.log(newCar)
+    charactersAPI.createOneRegister(newCar)
+    .then(res => console.log(res.data))
 
   });
 });
