0f6620874e7457723a3b87cd76de2a6debfe82d5
cursedxp <anilozsoy@gmail.com>
2022-11-24 15:32:32 +0100

    Fetch all characters

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,53 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI.getFullList().then((response) => {
+        response.data.forEach((element) => {
+          console.log("Hello");
+          charCard(element);
+        });
+      });
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      // const id = document.querySelector("[name=character-id]").value;
+      // charactersAPI.getOneRegister(id);
+    });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", function (event) {});
 
-  });
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", function (event) {});
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", function (event) {});
 });
+
+function charCard(element) {
+  const charactersContainer = document.querySelector(".characters-container");
+  const characterInfo = document.createElement("div");
+  const id = document.createElement("div");
+  const charName = document.createElement("div");
+  const occupation = document.createElement("div");
+  const isACartoon = document.createElement("div");
+  const weapon = document.createElement("div");
+
+  characterInfo.className = "character-info";
+
+  id.textContent = "id: " + element.id;
+  charName.textContent = "Name: " + element.name;
+  occupation.textContent = "Occupation: " + element.occupation;
+  isACartoon.textContent = "is a cartoon: " + element.cartoon;
+  weapon.textContent = "Weapon: " + element.weapon;
+  characterInfo.append(id, charName, occupation, isACartoon, weapon);
+  charactersContainer.appendChild(characterInfo);
+}
