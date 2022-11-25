94614c5177deac05fc258a591cb5489e87e89eaa
Antonio Gloria <angloria87@gmail.com>
2022-11-25 14:13:00 -0600

    Lab WIP - editCharacter pending

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,94 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
+// Top Section Inputs
+const charContainer = document.querySelector(".characters-container");
+const searchField = document.querySelector("[name='character-id']");
+const deleteField = document.querySelector("[name='character-id-delete']");
+
+// New Character Form Inputs
+const newName = document.querySelector("#new-character-form input[name='name']");
+const newOccupation = document.querySelector("#new-character-form input[name='occupation']");
+const newWeapon = document.querySelector("#new-character-form input[name='weapon']");
+const newCartoon = document.querySelector("#new-character-form input[name='cartoon']");
+
+// Delete Button
+const deleteButton = document.querySelector("#delete-one");
+
+function fillInfo(character) {
+    return `<div class="character-info">
+    <div class="id">ID:<span>${character.id}</span></div>
+    <div class="name">Character Name<span>${character.name}</span></div>
+    <div class="occupation">Character Occupation<span>${character.occupation}</span></div>
+    <div class="cartoon">Is a Cartoon? <span>${character.cartoon}</span></div>
+    <div class="weapon">Character Weapon <span>${character.weapon}</span></div>
+  </div>`;
+}
+
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    try {
+      const { data } = await charactersAPI.getFullList();
+      let dataHTML = ""
 
+      data.forEach(character => {
+        dataHTML += fillInfo(character)
+      });
+
+      charContainer.innerHTML = dataHTML;
+    }
+    catch (err) {
+      console.log(err);
+    }
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    try {
+      const id = searchField.value;
+      const { data } = await charactersAPI.getOneRegister(id);
+      charContainer.innerHTML = fillInfo(data);
+    }
+    catch (err) {
+      console.log(err);
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    event.preventDefault()
+    try {
+      const id = deleteField.value;
+      const deleteChar = await charactersAPI.deleteOneRegister(id);
+      console.log(deleteChar)
+      deleteButton.style.backgroundColor = "green";
+    }
+    catch (err) {
+      console.log(err);
+      deleteButton.style.backgroundColor = "red";
+    }
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document.getElementById('new-character-form').addEventListener('submit', async function (event) {
+    event.preventDefault()
+    try {
+      const name = newName.value;
+      const occupation = newOccupation.value;
+      const weapon = newWeapon.value;
+      const cartoon = newCartoon.checked;
 
+      const charObj = {
+        "name": name,
+        "occupation": occupation,
+        "weapon": weapon,
+        "cartoon": cartoon
+      };
+      const newChar = await charactersAPI.createOneRegister(charObj)
+      console.log(newChar);
+    }
+    catch (err) {
+      console.log(err)
+    }
   });
 });
