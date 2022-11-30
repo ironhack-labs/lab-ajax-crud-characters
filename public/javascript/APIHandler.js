45b708b6a4f9158e051453812bddaee797427c31
Eduardoo <eduardo.reyes19@yahoo.com>
2022-11-29 16:01:34 -0800

    Done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,29 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    constructor () 
+      this.BASE_URL = "http://localhost:8000";
+      this.api = axios.create({
+        baseURL: this.BASE_URL}) }
   }
 
-  getFullList () {
-
+  getFullList = () => {
+    return this.api.get('/characters');
   }
 
-  getOneRegister () {
-
+  getOneRegister = (characterId) => {
+    return this.api.get(`/characters/${characterId}`);
   }
 
-  createOneRegister () {
-
+  createOneRegister = (character) => {
+    return this.api.post(`/characters`, character);
   }
 
-  updateOneRegister () {
-
+  updateOneRegister = (characterId, character) => {
+    return this.api.put(`/characters/${characterId}`, character);
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister = (characterId) => {
+    return this.api.delete(`/characters/${characterId}`);
   }
-}
+
