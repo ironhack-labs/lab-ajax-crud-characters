ddfcb68c350017e6e470610cccade473cd1ba257
Alexandra <ss_sergeeva167@mail.ru>
2023-01-17 21:31:43 +0000

    still working

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -2,30 +2,30 @@ const axios = require("axios");
 
 class APIHandler {
   constructor(baseUrl) {
-    //this.BASE_URL = baseUrl;
-    this.api = axios.create({
-      baseUrl: "/api/db.json",
-    });
+    this.BASE_URL = baseUrl;
+    // this.api = axios.create({
+    //   baseUrl: "/api/db.json",
+    // });
   }
 
   getFullList = () => {
-    return this.api.get("/characters");
+    return this.BASE_URL.get("/characters");
   };
 
   getOneRegister = (registerId) => {
-    return this.api.get(`/characters/${registerId}`);
+    return this.BASE_URL.get(`/characters/${registerId}`);
   };
 
   createOneRegister = (registerInfo) => {
-    return this.api.post(`/characters`, registerInfo);
+    return this.BASE_URL.post(`/characters`, registerInfo);
   };
 
   updateOneRegister = (registerId, registerInfo) => {
-    return this.api.put(`/characters/${registerId}`, registerInfo)
+    return this.BASE_URL.put(`/characters/${registerId}`, registerInfo)
   }
 
   deleteOneRegister = (registerId) =>  {
-    return this.api.delete(`/characters/${registerId}`)
+    return this.BASE_URL.delete(`/characters/${registerId}`)
   }
 }
 
