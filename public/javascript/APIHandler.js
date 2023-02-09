cb4e58645f508184106dadb0a5ac217839d75dcf
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-09 16:09:33 +0100

    Iteration 2

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,31 @@
+const axios = require("axios");
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.api = axios.create({
+    })
   }
 
-  getFullList () {
+  getFullList = () => {
+    return this.api.get("/characters");
+  };
 
-  }
+  getOneRegister = (characterId) => {
+    return this.api.get(`/characters/${characterId}`);
+  };
 
-  getOneRegister () {
+  createOneRegister = (characterInfo) => {
+    return this.api.post(`/characters`, characterInfo);
+  };
 
-  }
+  updateOneRegister = (characterId, characterInfo) => {
+    return this.api.put(`/characters/${characterId}`, characterInfo);
+  };
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  deleteOneRegister = (characterId) => {
+    return this.api.delete(`/characters/${characterId}`);
+  };
 }
+
+module.exports = APIHandler;
