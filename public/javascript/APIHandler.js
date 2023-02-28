9d49181acb58f11b424fc01303ba4e55d33c169b
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-28 21:30:58 +0100

    Add stuff

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,33 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    this.BASE_URL = "http://localhost:8000";
+    this.api = axios.create({
+      baseURL: this.BASE_URL,
+    });
   }
 
   getFullList () {
+    return this.api.get('/characters');
 
   }
 
   getOneRegister () {
+    return this.api.get(`/characters/${id}`);
 
   }
 
   createOneRegister () {
+    return this.api.post(`/characters`, characterInfo);
 
   }
 
   updateOneRegister () {
+    return this.api.put(`/characters/${id}`, characterInfo);
 
   }
 
   deleteOneRegister () {
+    return this.api.delete(`/characters/${id}`);
 
   }
 }
