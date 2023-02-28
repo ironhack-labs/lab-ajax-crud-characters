5f06f5f5948fdd38a16b2775b57201c4f40b7449
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-28 21:30:36 +0100

    Solve iteration 1,2, start on 3

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,11 +1,27 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function  (event) {
+    try {
+        let response = await charactersAPI.getFullList()
+        let characters = await response.data
+        let html = ''
+        characters.forEach(character => {
+        html += `<div class="character-info">
+        <div class="name">${character.name}</div>
+        <div class="occupation">${character.occupation}</div>
+        <div class="cartoon">${character.cartoon}</div>
+        <div class="weapon">${character.weapon}</div>
+       </div>`
+      })
+      document.querySelector('.characters-container').innerHTML = html
+    } catch (error) {
+      console.log(err)
+    }
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
 
   });
 
