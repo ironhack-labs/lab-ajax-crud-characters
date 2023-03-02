caa179bcfe73bf0580bb4a4d1abc9faf0c1060b3
Yona <118460222+yosoyyona@users.noreply.github.com>
2023-03-02 23:13:26 +0100

    solved lab

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -14,7 +14,6 @@ window.addEventListener('load', () => {
     charactersAPI
     .getFullList()
     .then((res) => {
-      
       const characters = res.data
       charactersContainer.innerHTML = ""
 
@@ -27,17 +26,60 @@ window.addEventListener('load', () => {
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
 
+    const id = document.querySelector(`input[name="character-id"]`).value
+
+    charactersAPI
+    .getOneRegister(id)
+    .then((res) => {
+      const character = res.data
+      charactersContainer.innerHTML = ""
+
+      charactersContainer.appendChild(generateCharacterDOM(character))
+    })
+    .catch(error => console.log(error))
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
+    const id = document.querySelector(`input[name="character-id-delete"]`).value
+
+    charactersAPI
+    .deleteOneRegister(id)
 
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
 
+    event.preventDefault()
+
+    let editCharacterForm = document.getElementById("edit-character-form")
+    let id = editCharacterForm.querySelector(".chr-id").value
+
+    let characterInfo = {
+      name: editCharacterForm.name.value,
+      occupation: editCharacterForm.occupation.value,
+      weapon: editCharacterForm.weapon.value,
+      cartoon: editCharacterForm.cartoon.checked
+    }
+
+    charactersAPI
+    .updateOneRegister(id, characterInfo)
+    
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
+
+    event.preventDefault()
+
+    let newCharacterForm = document.getElementById("new-character-form")
+
+    let characterInfo = {
+      name: newCharacterForm.name.value,
+      occupation: newCharacterForm.occupation.value,
+      weapon: newCharacterForm.weapon.value,
+      cartoon: newCharacterForm.cartoon.checked
+    }
+
+    charactersAPI.createOneRegister(characterInfo)
 
   });
 });
