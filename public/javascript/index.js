515b3d95ad41eaaa59bfb943518bea661152b683
hussain <muhammadhussain170117@gmail.com>
2023-04-20 15:59:58 +0200

    completed

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,6 +2,28 @@ const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
   document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList()
+      .then(response => {
+        const characters = response.data
+
+        characters.forEach(character => {
+          const characterInfo = `
+          <div class="character-info">
+          <div class="id">Id: <span>${character.id}</span></div>
+          <div class="name">Name: <span>${character.name}</span></div>
+          <div class="occupation">Occupation: <span>${character.occupation}</span></div>
+          <div class="cartoon">Is a Cartoon?: <span>${character.cartoon}</span></div>
+          <div class="weapon">Weapon: <span>${character.weapon}</span></div>
+          </div>`
+
+          document.querySelector(".characters-container").innerHTML += characterInfo
+        })
+      })
+      .catch(err => console.log(err))
+
+
+
+
 
   });
 
