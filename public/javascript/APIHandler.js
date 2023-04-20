428c59fcb01004ee654b23229f3b2d01411c1b00
panagiotisliordos <takislio21@yahoo.gr>
2023-04-20 17:45:18 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,28 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
-  }
+    constructor(baseUrl) {
+        //this.BASE_URL = baseUrl;
+        this.api = axios.create({
+            baseURL: baseUrl,
+        });
+    }
 
-  getFullList () {
+    getFullList() {
+        return this.api.get("/characters");
+    }
 
-  }
+    getOneRegister(characterId) {
+        return this.api.get(`/characters/${characterId}`);
+    }
 
-  getOneRegister () {
+    createOneRegister(characterInfo) {
+        return this.api.post("/characters", characterInfo);
+    }
 
-  }
+    updateOneRegister(characterId, characterInfo) {
+        return this.api.put(`/characters/${characterId}`, characterInfo);
+    }
 
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+    deleteOneRegister(characterId) {
+        return this.api.delete(`/characters/${characterId}`);
+    }
 }
