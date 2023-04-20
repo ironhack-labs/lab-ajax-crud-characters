c986727eaf6b93989947236a1e842a7875dda0c1
Diana Kalstein <dianakalstein@gmail.com>
2023-04-20 17:32:19 +0200

    Solved lab

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,31 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    //this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl,
+    });
   }
 
-  getFullList () {
-
+  getFullList() {
+    return this.api.get("/characters")
   }
 
-  getOneRegister () {
-
-  }
-
-  createOneRegister () {
-
-  }
-
-  updateOneRegister () {
+  getOneRegister(characterId) {
+    return this.api.get(`/characters/${characterId}`)
 
   }
 
-  deleteOneRegister () {
+  createOneRegister(characterInfo) {
+    return this.api.post("/characters", characterInfo)
 
+  }
+
+  updateOneRegister(characterId, characterInfo) {
+    return this.api.put(`/characters/${characterId}`, characterInfo)
+
+  }
+
+  deleteOneRegister(characterId) {
+return this.api.delete(`/characters/${characterId}`)
   }
 }
