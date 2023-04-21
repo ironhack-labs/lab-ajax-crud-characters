74ac09d19f07f787cd77d179ed277bc8131eb7db
Nikolas <nihe@posteo.de>
2023-04-21 09:25:27 +0200

    done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,34 @@
 class APIHandler {
   constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+    // this.BASE_URL = baseUrl;
+    this.api = axios.create({
+      baseURL: baseUrl
+    })
   }
 
   getFullList () {
-
+    return this.api.get("/characters")
   }
 
-  getOneRegister () {
-
+  getOneRegister (characterId) {
+    return this.api.get(`/characters/${characterId}`)
   }
 
-  createOneRegister () {
-
+  createOneRegister (characterInfo) {
+    return this.api.post("/characters", characterInfo)
   }
-
-  updateOneRegister () {
-
+  
+  updateOneRegister (characterId, characterInfo) {
+    return this.api.put(`/characters/${characterId}`, characterInfo)
+    
   }
-
-  deleteOneRegister () {
-
+  // bei put und post wird second argument in den body geliefert (oder halt hier das Argument bei post lol)
+  
+  deleteOneRegister (characterId) {
+    return this.api.delete(`/characters/${characterId}`)
   }
 }
+
+// module.exports = ApiService brauchen wir hier nicht (weil kein nodejs, 
+// allerdings ist es ja bei der lecture von heute auch axios und nicht nodejs und
+// steht es noch (oder nur in meiner Version?))
