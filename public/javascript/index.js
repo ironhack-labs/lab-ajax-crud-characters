d7ac64532c2a1e5a61763e8b60b88fdc39403591
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-26 01:31:57 +0200

    added error messages

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -8,18 +8,22 @@ window.addEventListener('load', () => {
   const chCard = chTemplate.cloneNode(true);
   chTemplate.remove();
 
+  // function for creating result cards
   let createNewCards = (obj) => {
     for (let i=0; i < obj.length; i++) {
       if (i == 0) {
         // clears previous results
         document.querySelectorAll('.result-card').forEach(e => e.remove());
       }
+      let cartoonStatus;
+      obj[i].cartoon == true ? cartoonStatus='👍' : cartoonStatus='<em>nope</em>';
+
       let newCard = chCard.cloneNode(true);
       newCard.classList.add('result-card');
-      newCard.querySelector('.name').innerHTML = `<strong>Name:</strong> ${obj[i].name}`;
-      newCard.querySelector('.occupation').innerHTML = `<strong>Occupation:</strong> ${obj[i].occupation}`;
-      newCard.querySelector('.cartoon').innerHTML = `<strong>Is a Cartoon?:</strong> ${obj[i].cartoon}`;
-      newCard.querySelector('.weapon').innerHTML = `<strong>Weapon:</strong> ${obj[i].weapon}`;
+      newCard.querySelector('.name').innerHTML = `Name: <span>${obj[i].name}</span>`;
+      newCard.querySelector('.occupation').innerHTML = `Occupation: <span>${obj[i].occupation}</span>`;
+      newCard.querySelector('.cartoon').innerHTML = `Is a Cartoon?: <span>${cartoonStatus}</span>`;
+      newCard.querySelector('.weapon').innerHTML = `Weapon: <span>${obj[i].weapon}</span>`;
       chContainer.appendChild(newCard);
     }
   }
@@ -34,20 +38,37 @@ window.addEventListener('load', () => {
   document.getElementById('fetch-one').addEventListener('click', async (event) => {
     event.preventDefault();
     const requestedId = document.getElementById('character-id-search').value;
-    const result = await charactersAPI.getOneRegister(requestedId);
-    createNewCards([result]);
-    console.log(`Here that one ID: ${result}`);
+
+    // display result, display error if empty
+    if (requestedId) {
+      const result = await charactersAPI.getOneRegister(requestedId);
+      createNewCards([result]);
+      console.log(`Here that one ID: ${result.id}`);
+    } else {
+      const error = document.querySelector('.operation.search .error');
+      error.innerHTML = `No ID, no result, d\'oh!`;
+    }
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document.getElementById('delete-one').addEventListener('click', async (event) => {
+    event.preventDefault();
+    const requestedId = document.getElementById('character-id-delete').value;
 
+    // display result, display error if empty
+    if (requestedId) {
+      const result = await charactersAPI.getOneRegister(requestedId);
+      console.log(`ID to delete: ${result.id}`);
+    } else {
+      const error = document.querySelector('.operation.delete .error');
+      error.innerHTML = `No ID to delete, dude!`;
+    }
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('edit-character-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+  document.getElementById('new-character-form').addEventListener('submit', async (event) => {
+    event.preventDefault();
   });
 });
