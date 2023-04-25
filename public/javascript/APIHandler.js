273b43fd302dc7e2cb6e572cc21774bcd946dfcb
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-26 00:58:30 +0200

    solved getOneRegister()

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -9,9 +9,11 @@ class APIHandler {
     return fullList.data;
   }
 
-  // async getOneRegister () {
-
-  // }
+  async getOneRegister (id) {
+    const oneItem = await axios.get(`${this.BASE_URL}/characters/${id}`);
+    console.log(`Here's the one item: ${oneItem}`);
+    return oneItem.data;
+  }
 
   // async createOneRegister () {
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,38 +1,42 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
+  const chContainer = document.querySelector('.characters-container');
+  
+  // copy, then remove template card
+  const chTemplate = document.querySelector('.character-info');
+  const chCard = chTemplate.cloneNode(true);
+  chTemplate.remove();
 
-  // let createNewCard = () => {
-
-  // }
-
-  document.getElementById('fetch-all').addEventListener('click', async (event) => {
-    event.preventDefault();
-    const listOfAll = await charactersAPI.getFullList();
-    const chContainer = document.querySelector('.characters-container');
-    const chTemplate = document.querySelector('.character-info');
-    const chCard = chTemplate.cloneNode(true);
-
-    console.log(listOfAll);
-
-    for (let i=0; i < listOfAll.length; i++) {
+  let createNewCards = (obj) => {
+    for (let i=0; i < obj.length; i++) {
       if (i == 0) {
-        // clears previous potential results
-        chTemplate.remove();
+        // clears previous results
         document.querySelectorAll('.result-card').forEach(e => e.remove());
       }
       let newCard = chCard.cloneNode(true);
       newCard.classList.add('result-card');
-      newCard.querySelector('.name').innerHTML = `<strong>Name:</strong> ${listOfAll[i].name}`;
-      newCard.querySelector('.occupation').innerHTML = `<strong>Occupation:</strong> ${listOfAll[i].occupation}`;
-      newCard.querySelector('.cartoon').innerHTML = `<strong>Is a Cartoon?:</strong> ${listOfAll[i].cartoon}`;
-      newCard.querySelector('.weapon').innerHTML = `<strong>Weapon:</strong> ${listOfAll[i].weapon}`;
+      newCard.querySelector('.name').innerHTML = `<strong>Name:</strong> ${obj[i].name}`;
+      newCard.querySelector('.occupation').innerHTML = `<strong>Occupation:</strong> ${obj[i].occupation}`;
+      newCard.querySelector('.cartoon').innerHTML = `<strong>Is a Cartoon?:</strong> ${obj[i].cartoon}`;
+      newCard.querySelector('.weapon').innerHTML = `<strong>Weapon:</strong> ${obj[i].weapon}`;
       chContainer.appendChild(newCard);
     }
+  }
+
+  document.getElementById('fetch-all').addEventListener('click', async (event) => {
+    event.preventDefault();
+    const result = await charactersAPI.getFullList();
+    createNewCards(result);
+    // console.log(listOfAll);
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+  document.getElementById('fetch-one').addEventListener('click', async (event) => {
+    event.preventDefault();
+    const requestedId = document.getElementById('character-id-search').value;
+    const result = await charactersAPI.getOneRegister(requestedId);
+    createNewCards([result]);
+    console.log(`Here that one ID: ${result}`);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
