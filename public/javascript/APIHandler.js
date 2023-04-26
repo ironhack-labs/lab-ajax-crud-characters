93610c5118c7c347dfc915b7255ad8101a8c5e3b
Wiebke Lueneburg <git@fraulueneburg.de>
2023-04-26 13:29:49 +0200

    solved iteration 3 (add character)

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -16,17 +16,21 @@ class APIHandler {
   }
   
   async deleteOneRegister (id) {
-    const oneItem = await axios.delete(`${this.BASE_URL}/characters/${id}`);
-    console.log(`Item to delete: ${oneItem}`);
-    return oneItem.data;
+    const itemToDelete = await axios.delete(`${this.BASE_URL}/characters/${id}`);
+    console.log(`Item to delete: ${itemToDelete}`);
+    return itemToDelete.data;
   }
 
-  // async createOneRegister () {
+  async createOneRegister (newCharacter) {
+    const addedCharacter = await axios.post(`${this.BASE_URL}/characters/`, newCharacter);
+    console.log(`Newly created item: ${addedCharacter}`);
+    return addedCharacter.data;
+  }
 
-  // }
-
-  // async updateOneRegister () {
-
-  // }
+  async updateOneRegister (updatedCharacter) {
+    const itemToUpdate = await axios.put(`${this.BASE_URL}/characters/`, updatedCharacter);
+    console.log(`Item updated: ${itemToUpdate}`);
+    return itemToUpdate.data;
+  }
 }
 const ApiHandle = new APIHandler('http://localhost:8000');
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -28,6 +28,7 @@ window.addEventListener('load', () => {
     }
   }
 
+  // fetch all characters
   document.getElementById('fetch-all').addEventListener('click', async (event) => {
     event.preventDefault();
     const result = await charactersAPI.getFullList();
@@ -35,40 +36,66 @@ window.addEventListener('load', () => {
     // console.log(listOfAll);
   });
 
+  // fetch one character
   document.getElementById('fetch-one').addEventListener('click', async (event) => {
     event.preventDefault();
     const requestedId = document.getElementById('character-id-search').value;
-
+    const wrapper = document.querySelector('.operation.search');
+    const message = document.querySelector('.operation.search .message');
+    
     // display result, display error if empty
     if (requestedId) {
       const result = await charactersAPI.getOneRegister(requestedId);
       createNewCards([result]);
+      wrapper.classList.remove('error');
+      message.innerHTML = '';
       console.log(`Here that one ID: ${result.id}`);
     } else {
-      const error = document.querySelector('.operation.search .error');
-      error.innerHTML = `No ID, no result, d\'oh!`;
+      wrapper.classList.add('error');
+      message.innerHTML = `No ID, no result, d\'oh!`;
     }
   });
 
+  // delete character
   document.getElementById('delete-one').addEventListener('click', async (event) => {
     event.preventDefault();
     const idToDelete = document.getElementById('character-id-delete').value;
+    const wrapper = document.querySelector('.operation.delete');
+    const message = document.querySelector('.operation.delete .message');
 
     // display result, display error if empty
     if (idToDelete) {
       const result = await charactersAPI.deleteOneRegister(idToDelete);
+      wrapper.classList.remove('error');
+      message.innerHTML = `Successfully deleted item with ${result.id}`;
       console.log(`ID to delete: ${result.id}`);
     } else {
-      const error = document.querySelector('.operation.delete .error');
-      error.innerHTML = `No ID to delete, dude!`;
+      wrapper.classList.add('error');
+      message.innerHTML = `No ID to delete, dude!`;
     }
   });
-
-  document.getElementById('edit-character-form').addEventListener('submit', async (event) => {
+  
+  // add character
+  document.getElementById('send-data-new').addEventListener('click', async (event) => {
     event.preventDefault();
+    const name = document.querySelector('#new-character-form input[name="name"]').value;
+    const occupation = document.querySelector('#new-character-form input[name="occupation"]').value;
+    const cartoon = document.querySelector('#new-character-form input[name="cartoon"]').checked;
+    const weapon = document.querySelector('#new-character-form input[name="weapon"]').value;
+    const characterToAdd = { name, occupation, cartoon, weapon };
+    const addedCharacter = await charactersAPI.createOneRegister(characterToAdd);
+    console.log('Successfully added character', addedCharacter.data);
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', async (event) => {
+  // update character
+  document.getElementById('send-data-update').addEventListener('click', async (event) => {
     event.preventDefault();
+    const name = document.querySelector('#edit-character-form input[name="name"]').value;
+    const occupation = document.querySelector('#edit-character-form input[name="occupation"]').value;
+    const cartoon = document.querySelector('#edit-character-form input[name="cartoon"]').checked;
+    const weapon = document.querySelector('#edit-character-form input[name="weapon"]').value;
+    const characterToUpdate = { name, occupation, cartoon, weapon };
+    const updatedCharacter = await charactersAPI.updateOneRegister(characterToUpdate);
+    console.log('Successfully updated character', characterToUpdate.name);
   });
 });
