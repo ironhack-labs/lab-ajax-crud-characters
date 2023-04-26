3114e39cc850ca8fefdeb32a44e9a793b9cfe120
RonnySk <ronny_sk@hotmail.com>
2023-04-26 09:34:07 +0200

    2 iteration

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,28 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList (characters) {
+    const fullList = await axios.get(`${this.BASE_URL}/${characters}`);
+    return fullList.data;
   }
 
-  getOneRegister () {
-
+  getOneRegister (characters, id) {
+    const oneRegister = await axios.get(`${this.BASE_URL}/${characters}/${id}`);
+    return oneRegister.data;
   }
 
-  createOneRegister () {
-
+  createOneRegister (characters, newCharacter) {
+    const createOneRegister = await axios.post(`${this.BASE_URL}/${characters}`, newCharacter);
+    return 
   }
 
-  updateOneRegister () {
-
+  updateOneRegister (characters, id) {
+    const updateOneRegister = await axios.get(`${this.BASE_URL}/${characters}/${id}`);
+    return updateOneRegister.data;    
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister (characters, id) {
+    const deleteOneRegister = await axios.get(`${this.BASE_URL}/${characters}/${id}`);
+    return deleteOneRegister.data; 
   }
 }
