e0f51a41f6893ca7ee32cf939bc6515796daf166
MaaX59 <Maax.strandberg@gmail.com>
2023-04-26 16:24:35 +0200

    at iteration 3

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -3,23 +3,37 @@ class APIHandler {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  async getFullList () {
+    const fullList = await axios('${this.BASE_URL}/characters');
+    console.log(fullList.data);
+    return fullList.data;
   }
 
-  getOneRegister () {
-
+  async getOneRegister (id) {
+    const getOne = await axios('${this.BASE_URL}/characters/${:id}');
+    return getOne.data;
   }
 
-  createOneRegister () {
-
+  async createOneRegister (newChar) {
+    const addedChar = await axios.post('${this.BASE_URL}/characters', newChar);
   }
 
-  updateOneRegister () {
-
+  async updateOneRegister (id, info) {
+    try{
+    const updateChar = await axios.patch('${this.BASE_URL}/characters/${:id}', info);
+    console.log (updateChar);
+  } catch(error){
+    console.log('could not find character´s id', error);
+  };
   }
 
-  deleteOneRegister () {
+  async deleteOneRegister (id) {
+    try{
+          const deleteChar = await axios.delete('${this.BASE_URL}/characters/${:id}');
+          console.log('character has been deleted')
+    }catch(error){
+      console.log('could not delete character', error);
+    };
 
   }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,43 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler('http://localhost:8080');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+
+  document
+  .getElementById('fetch-all')
+  .addEventListener('click', async (event)=> {
+    const getAll = await charactersAPI.getFullList();
+    console.log(`here is all the characters `, getAll);
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+  .getElementById("fetch-one")
+  .addEventListener('click', async function (event)  {
+    const idInput = document.querySelector("#character-id").value;
+    const getOne = await charactersAPI.getOneRegister(idInput);
+    console.log('One character found ', getOne);
 
   });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+  .getElementById('delete-one')
+  .addEventListener('click', async (event) => {
+    const idInput = document.querySelector("#character-id-delete").value;
+    const deleteId = await charactersAPI.deleteOneRegister(idInput);
+    console.log('deleted character ', idInput);
+
 
   });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document
+  .getElementById('edit-character-form')
+  .addEventListener('submit', function (event) {
 
   });
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  document
+  .getElementById('new-character-form')
+  .addEventListener('submit', function (event) {
 
   });
 });
