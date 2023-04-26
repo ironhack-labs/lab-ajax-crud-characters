8a066df401f7f3dbef2a481cd9606d7b37c536f2
Michel Saber <michel-saber@hotmail.com>
2023-04-26 20:15:07 +0200

    Iteration 3 partially done

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,22 +6,49 @@ window.addEventListener("load", () => {
     .addEventListener("click", async (event) => {
       const response = await charactersAPI.getFullList("characters");
       console.log(response);
-      return response;
+
+      response.forEach((character) => {
+        const characterInfo = ` <div class="character-info">
+          <div class="id">Id: <span>${character.id}</span></div>
+          <div class="name">Name: <span>${character.name}</span></div>
+          <div class="occupation">Occupation: <span>${character.occupation}</span></div>
+          <div class="cartoon">Is a cartoon: <span>${character.cartoon}</span></div>
+          <div class="weapon">Weapon: <span>${character.weapon}</span></div>
+          </div>`;
+
+        document.querySelector(".characters-container").innerHTML +=
+          characterInfo;
+      });
     });
-
-  document
-    .getElementById("fetch-one")
-    .addEventListener("click", function (event) {});
-
-  document
-    .getElementById("delete-one")
-    .addEventListener("click", function (event) {});
-
-  document
-    .getElementById("edit-character-form")
-    .addEventListener("submit", function (event) {});
-
-  document
-    .getElementById("new-character-form")
-    .addEventListener("submit", function (event) {});
 });
+
+document
+  .getElementById("fetch-one")
+  .addEventListener("click", async (event) => {
+    const characterId = document.querySelector("[name=character-id]").value;
+    const response = await charactersAPI.getOneRegister(
+      "characters",
+      characterId
+    );
+    const characterInfo = ` <div class="character-info">
+        <div class="id">Id: <span>${response.id}</span></div>
+        <div class="name">Name: <span>${response.name}</span></div>
+        <div class="occupation">Occupation: <span>${response.occupation}</span></div>
+        <div class="cartoon">Is a cartoon: <span>${response.cartoon}</span></div>
+        <div class="weapon">Weapon: <span>${response.weapon}</span></div>
+        </div>`;
+
+    document.querySelector(".characters-container").innerHTML = characterInfo;
+  });
+
+document
+  .getElementById("delete-one")
+  .addEventListener("click", function (event) {});
+
+document
+  .getElementById("edit-character-form")
+  .addEventListener("submit", function (event) {});
+
+document
+  .getElementById("new-character-form")
+  .addEventListener("submit", function (event) {});
