999dd822ac23ac8930f355b4e428aeec8cb14813
Lena Imdahl <mail@lenaimdahl.de>
2023-04-27 18:18:40 +0200

    added error and color for button

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,38 +1,46 @@
+// work from Mattia and Lena
+
 const charactersAPI = new APIHandler("http://localhost:8000");
 
 window.addEventListener("load", () => {
   document
     .querySelector("#fetch-all")
     .addEventListener("click", async (event) => {
-      const allCharacters = await charactersAPI.getFullList();
-      const divContainer = document.querySelector(".characters-container");
+      try {
+        const allCharacters = await charactersAPI.getFullList();
+        const divContainer = document.querySelector(".characters-container");
 
-      for (const character of allCharacters) {
-        const divInfo = document.createElement("div");
-        divInfo.setAttribute("class", "character-info");
+        for (const character of allCharacters) {
+          const divInfo = document.createElement("div");
+          divInfo.setAttribute("class", "character-info");
 
-        const divId = document.createElement("div");
-        divId.innerHTML = character.id;
+          const divId = document.createElement("div");
+          divId.innerHTML = character.id;
 
-        const divName = document.createElement("div");
-        divName.innerHTML = character.name;
+          const divName = document.createElement("div");
+          divName.innerHTML = character.name;
 
-        const divOccupation = document.createElement("div");
-        divOccupation.innerHTML = character.occupation;
+          const divOccupation = document.createElement("div");
+          divOccupation.innerHTML = character.occupation;
 
-        const divCartoon = document.createElement("div");
-        divCartoon.innerHTML = character.cartoon;
+          const divCartoon = document.createElement("div");
+          divCartoon.innerHTML = character.cartoon;
 
-        const divWeapon = document.createElement("div");
-        divWeapon.innerHTML = character.weapon;
+          const divWeapon = document.createElement("div");
+          divWeapon.innerHTML = character.weapon;
 
-        divInfo.appendChild(divId);
-        divInfo.appendChild(divName);
-        divInfo.appendChild(divOccupation);
-        divInfo.appendChild(divCartoon);
-        divInfo.appendChild(divWeapon);
+          divInfo.appendChild(divId);
+          divInfo.appendChild(divName);
+          divInfo.appendChild(divOccupation);
+          divInfo.appendChild(divCartoon);
+          divInfo.appendChild(divWeapon);
 
-        divContainer.appendChild(divInfo);
+          divContainer.appendChild(divInfo);
+        }
+
+        document.querySelector("#fetch-all").style.backgroundColor = "green";
+      } catch (error) {
+        document.querySelector("#fetch-all").style.backgroundColor = "red";
       }
     });
 
@@ -40,27 +48,38 @@ window.addEventListener("load", () => {
     .getElementById("fetch-one")
     .addEventListener("click", async function () {
       const id = document.querySelector(".operation input").value;
-      const character = await charactersAPI.getOneRegister(id);
+      try {
+        const character = await charactersAPI.getOneRegister(id);
 
-      document.querySelector(
-        ".character-info .name"
-      ).innerHTML = `Character Name: ${character.name}`;
-      document.querySelector(
-        ".character-info .occupation"
-      ).innerHTML = `Character occupation: ${character.occupation}`;
-      document.querySelector(
-        ".character-info .cartoon"
-      ).innerHTML = `Is a cartoon?: ${character.cartoon}`;
-      document.querySelector(
-        ".character-info .weapon"
-      ).innerHTML = `Character weapon: ${character.weapon}`;
+        document.querySelector(
+          ".character-info .name"
+        ).innerHTML = `Character Name: ${character.name}`;
+        document.querySelector(
+          ".character-info .occupation"
+        ).innerHTML = `Character occupation: ${character.occupation}`;
+        document.querySelector(
+          ".character-info .cartoon"
+        ).innerHTML = `Is a cartoon?: ${character.cartoon}`;
+        document.querySelector(
+          ".character-info .weapon"
+        ).innerHTML = `Character weapon: ${character.weapon}`;
+
+        document.querySelector("#fetch-one").style.backgroundColor = "green";
+      } catch (error) {
+        document.querySelector("#fetch-one").style.backgroundColor = "red";
+      }
     });
 
   document
     .getElementById("delete-one")
-    .addEventListener("click", async function () {
+    .addEventListener("click", async function (event) {
       const id = document.querySelector(".operation.delete input").value;
-      await charactersAPI.deleteOneRegister(id);
+      try {
+        await charactersAPI.deleteOneRegister(id);
+        document.querySelector("#delete-one").style.backgroundColor = "green";
+      } catch (error) {
+        document.querySelector("#delete-one").style.backgroundColor = "red";
+      }
     });
 
   document
@@ -89,7 +108,17 @@ window.addEventListener("load", () => {
 
       const id = formProps.id;
       delete formProps.id;
-      await charactersAPI.updateOneRegister(id, formProps);
+
+      try {
+        await charactersAPI.updateOneRegister(id, formProps);
+        document.querySelector(
+          "#edit-character-form #send-data"
+        ).style.backgroundColor = "green";
+      } catch (error) {
+        document.querySelector(
+          "#edit-character-form #send-data"
+        ).style.backgroundColor = "red";
+      }
     });
 
   document
@@ -103,6 +132,15 @@ window.addEventListener("load", () => {
         formProps.cartoon = true;
       }
 
-      await charactersAPI.createOneRegister(formProps);
+      try {
+        await charactersAPI.createOneRegister(formProps);
+        document.querySelector(
+          "#new-character-form #send-data"
+        ).style.backgroundColor = "green";
+      } catch (error) {
+        document.querySelector(
+          "#new-character-form #send-data"
+        ).style.backgroundColor = "red";
+      }
     });
 });
