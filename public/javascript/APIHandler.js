b31e8782a20fa92ca47263c6ecbaf1db0765f8c0
Aykut Kav <ayktkav@gmail.com>
2023-06-29 22:25:29 +0200

    First Commit

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,67 @@
 class APIHandler {
-  constructor (baseUrl) {
-    this.BASE_URL = baseUrl;
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+  getFullList() {
+    return axios
+      .get(`${this.baseUrl}/characters`)
+      .then((response) => {
+        const characters = response.data;
+        console.log(characters);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  getFullList () {
-
+  getOneRegister(id) {
+    return axios
+      .get(`${this.baseUrl}/characters/${id}`)
+      .then((response) => {
+        const character = response.data;
+        console.log(character);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  getOneRegister () {
-
+  createOneRegister(characterData) {
+    return axios
+      .post(`${this.baseUrl}/characters`, characterData)
+      .then((response) => {
+        const newCharacter = response.data;
+        console.log(newCharacter);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
-  createOneRegister () {
+  updateOneRegister(characterId, updatedCharacter) {
+    // updateOneRegister(id, characterData) {
+    // return axios
+    //   .put(`${this.baseUrl}/characters/${id}`, characterData)
+    //   .then((response) => {
+    //     const updatedCharacter = response.data;
+    //     console.log(updatedCharacter);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    const url = `${this.baseUrl}/characters/${characterId}`;
 
+    return axios.put(url, updatedCharacter);
   }
 
-  updateOneRegister () {
+  deleteOneRegister(characterId) {
+    const url = `${this.baseUrl}/characters/${characterId}`;
 
+    return axios.delete(url);
   }
+  createOneRegister(newCharacter) {
+    const url = `${this.baseUrl}/characters`;
 
-  deleteOneRegister () {
-
+    return axios.post(url, newCharacter);
   }
 }
