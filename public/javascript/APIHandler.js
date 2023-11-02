3ff56b2666d5fd9e59b558605ec071a00615e241
chloe <marin.ducarme@gmail.com>
2023-11-02 06:25:30 +0100

    first part

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,46 @@
+const axios = require('axios');
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
   getFullList () {
-
+    return this.BASE_URL.get('/characters');
   }
 
   getOneRegister () {
-
+    return this.BASE_URL.get(`/characters/${characterId}`);
   }
 
   createOneRegister () {
-
-  }
+    return this.BASE_URL.post(`/characters`, characterInfo)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error(error.response.data);
+    });
+  };
 
   updateOneRegister () {
-
+    return this.BASE_URL.put(`/characters/${characterId}`, characterInfo)
+    .then(() => {
+      console.log('Character has been successfully updated');
+    })
+    .catch((error) => {
+      console.error(`Character not found ${id}:`, error);
+    });
   }
 
   deleteOneRegister () {
-
+    return this.BASE_URL.delete(`/characters/${characterId}`)
+    .then(() => {
+      console.log('Character has been successfully deleted');
+    })
+    .catch((error) => {
+      console.error(`Character not found ${id}:`, error);
+    });
   }
 }
+module.exports = APIHandler;
