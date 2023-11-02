77821db23a6abbf857c392e669144eea5ab881d9
EO <eo.code.wd@gmail.com>
2023-11-02 13:42:25 +0100

    delet-create-update

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,80 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+window.addEventListener("load", () => {
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      charactersAPI
+        .getFullList()
+        .then((characters) => {
+          console.log("All characters:", characters);
+        })
+        .catch((error) => {
+          console.error("Error fetching all characters:", error);
+        });
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {});
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", async function (event) {
+      try {
+        const characterId = document.getElementById("character-id").value;
+        console.log(characterId);
+        const response = await charactersAPI.deleteOneRegister(characterId);
+        //console.log(response);
 
-  });
+      } catch (error) {
+        console.error("Error deleting character by ID:", error);
+      }
+    });
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
+      const form = document.getElementById("edit-character-form");
+      const id = form.querySelector('input[name="chr-id"]').value;
+      const name = form.querySelector('input[name="name"]').value;
+      const occ = form.querySelector('input[name="occupation"]').value;
+      const weapon = form.querySelector('input[name="weapon"]').value;
+      const check = form.querySelector('input[name="cartoon"]').checked;
+      const data = {
+        "name": name,
+        "occupation": occ,
+        'weapon': weapon,
+        "check": check,
+      };
+      try {
+        await charactersAPI.updateOneRegister(id, data);
+      } catch (error) {
+        console.error("Error editing character by ID:", error);
+      }
+    });
 
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
-  });
-
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
-  });
+  document
+    .getElementById("new-character-form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
+      try {
+        const name = document.getElementById("add-name").value;
+        console.log(name);
+        const occ = document.getElementById("add-occ").value;
+        const weapon = document.getElementById("add-weapon").value;
+        const check = document.getElementById("add-check").checked;
+        const data = {
+          "name": name,
+          "occupation": occ,
+          "weapon": weapon,
+          "check": check,
+        };
+        await charactersAPI.createOneRegister(data);
+       
+      } catch (error) {
+        console.error("Error creating new character:", error);
+      }
+    });
 });
