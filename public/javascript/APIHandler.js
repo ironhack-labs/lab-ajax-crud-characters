f984d018f6f549a902ea6677db6b5d68fbefc902
Gabriel Fernandez <gfernandezareas@gmail.com>
2023-12-10 15:32:35 +0100

    lab done

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,80 @@
+// const axios = require('axios');
+
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
-
+  getFullList = () => {
+    return axios({
+      method: 'GET',
+      url: `${this.BASE_URL}/characters`,
+    })
+    .then(response => {
+      // console.log('Response from API is: ', response.data);
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
   }
 
-  getOneRegister () {
-
+  getOneRegister = (characterId) => {
+    return axios({
+      method: 'GET',
+      url: `${this.BASE_URL}/characters/${characterId}`,
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
   }
 
-  createOneRegister () {
-
+  createOneRegister = (characterInfo) => {
+    return axios.post(`${this.BASE_URL}/characters`, characterInfo)
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
   }
 
-  updateOneRegister () {
-
+  updateOneRegister = (characterId, characterInfo) => {
+    return axios.put(`${this.BASE_URL}/characters/${characterId}`, characterInfo)
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
   }
 
-  deleteOneRegister () {
-
+  deleteOneRegister = (characterId) => {
+    return axios({
+      method: 'DELETE',
+      url: `${this.BASE_URL}/characters/${characterId}`,
+    })
+    .then(response => {
+      return response.data;
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
   }
 }
+
+const charactersAPI = new APIHandler('http://localhost:8000');
+
+// const APITest = new APIHandler('http://localhost:8000');
+
+// APITest.getFullList();
+
+// module.exports = APIHandler;
